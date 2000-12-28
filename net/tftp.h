592ec2c500a36765b04e679c21413a9eccfe1e69
wdenk <wdenk>
2000-12-28 11:02:30 +0000

    Initial revision

---
+++ net/tftp.h
@@ -0,0 +1,21 @@
+/*
+ *	LiMon - BOOTP/TFTP.
+ *
+ *	Copyright 1994, 1995, 2000 Neil Russell.
+ *	(See License)
+ */
+
+#ifndef __TFTP_H__
+#define __TFTP_H__
+
+/**********************************************************************/
+/*
+ *	Global functions and variables.
+ */
+
+/* tftp.c */
+extern void	TftpStart (void);	/* Begin TFTP get */
+
+/**********************************************************************/
+
+#endif /* __TFTP_H__ */
