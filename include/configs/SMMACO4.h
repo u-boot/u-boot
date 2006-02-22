5f0be01356aa7dafc2e5e4ee07715d05bbe9013b
Wolfgang Denk <wd@pollux.(none)>
2006-02-22 02:39:31 +0100

    Disable CMD_BSP for SMMACO4 board.

--- include/configs/SMMACO4.h
+++ include/configs/SMMACO4.h
@@ -88,8 +88,7 @@
 				CFG_CMD_PING	| \
 				CFG_CMD_POST_DIAG | \
 				CFG_CMD_REGINFO | \
-				CFG_CMD_SNTP	| \
-				CFG_CMD_BSP)
+				CFG_CMD_SNTP	)
 
 /* this must be included AFTER the definition of CONFIG_COMMANDS (if any) */
 #include <cmd_confdefs.h>
