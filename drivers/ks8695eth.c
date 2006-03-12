7c54c7018e9c6f50931c07afe1805bef1be0def9
Wolfgang Denk <wd@pollux.denx.de>
2006-03-12 01:40:01 +0100

    Fix conditional for including ks8695eth driver Patch by Greg Ungerer, 04 Jun 2005

--- drivers/ks8695eth.c
+++ drivers/ks8695eth.c
@@ -18,8 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifdef	CONFIG_DRIVER_KS8695ETH
-
 /****************************************************************************/
 
 #include <common.h>
@@ -28,6 +26,7 @@
 #include <asm/io.h>
 #include <asm/arch/platform.h>
 
+#ifdef	CONFIG_DRIVER_KS8695ETH
 /****************************************************************************/
 
 /*
