278bc4b37fabba61209fab8704c75f4fa80cc6fa
Stefan Roese <sr@denx.de>
2006-05-10 15:06:58 +0200

    Update yosemite configuration to enable flash write buffer support Patch by Stefan Roese, 10 May 2006

--- include/configs/yosemite.h
+++ include/configs/yosemite.h
@@ -109,6 +109,8 @@
 #define CFG_FLASH_ERASE_TOUT	120000	/* Timeout for Flash Erase (in ms)	*/
 #define CFG_FLASH_WRITE_TOUT	500	/* Timeout for Flash Write (in ms)	*/
 
+#define CFG_FLASH_USE_BUFFER_WRITE 1	/* use buffered writes (20x faster)	*/
+
 #define CFG_FLASH_EMPTY_INFO		/* print 'E' for empty sector on flinfo */
 
 #ifdef CFG_ENV_IS_IN_FLASH
