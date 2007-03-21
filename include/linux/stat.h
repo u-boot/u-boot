9c0deb5ae3ea0189f2e08ac29ef1316f1fb8548d
Haavard Skinnemoen <hskinnemoen@atmel.com>
2007-03-21 19:44:48 +0100

    AVR32: Provide a definition of struct stat
    Copy the definition of struct stat from the Linux kernel.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>

--- include/linux/stat.h
+++ include/linux/stat.h
@@ -125,6 +125,31 @@ struct stat {
 
 #endif	/* __MIPS__ */
 
+#if defined(__AVR32__)
+
+struct stat {
+        unsigned long st_dev;
+        unsigned long st_ino;
+        unsigned short st_mode;
+        unsigned short st_nlink;
+        unsigned short st_uid;
+        unsigned short st_gid;
+        unsigned long  st_rdev;
+        unsigned long  st_size;
+        unsigned long  st_blksize;
+        unsigned long  st_blocks;
+        unsigned long  st_atime;
+        unsigned long  st_atime_nsec;
+        unsigned long  st_mtime;
+        unsigned long  st_mtime_nsec;
+        unsigned long  st_ctime;
+        unsigned long  st_ctime_nsec;
+        unsigned long  __unused4;
+        unsigned long  __unused5;
+};
+
+#endif /* __AVR32__ */
+
 #ifdef __cplusplus
 }
 #endif
