822af351ad2babc7d99033361a5fcacd30f6bc78
Rodolfo Giometti <giometti@enneenne.com>
2007-04-03 14:27:18 +0200

    Support for the Philips ISP116x HCD (Host Controller Driver)
    Signed-off-by: Rodolfo Giometti <giometti@enneenne.com>

--- include/usb.h
+++ include/usb.h
@@ -169,7 +169,10 @@ struct usb_device {
  * this is how the lowlevel part communicate with the outer world
  */
 
-#if defined(CONFIG_USB_UHCI) || defined(CONFIG_USB_OHCI) || defined (CONFIG_USB_SL811HS) || defined(CONFIG_USB_OHCI_NEW)
+#if defined(CONFIG_USB_UHCI) || defined(CONFIG_USB_OHCI) || \
+	defined(CONFIG_USB_OHCI_NEW) || defined (CONFIG_USB_SL811HS) || \
+	defined(CONFIG_USB_ISP116X_HCD)
+
 int usb_lowlevel_init(void);
 int usb_lowlevel_stop(void);
 int submit_bulk_msg(struct usb_device *dev, unsigned long pipe, void *buffer,int transfer_len);
