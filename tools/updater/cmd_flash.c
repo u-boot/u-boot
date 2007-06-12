5fcf543e0b6628c76ff48705b1b0566bfd11507b
Jon Loeliger <jdl@jdl.com>
2007-06-11 19:03:28 -0500

    tools/ : Augment CONFIG_COMMANDS tests with defined(CONFIG_CMD_*).
    This is a compatibility step that allows both the older form
    and the new form to co-exist for a while until the older can
    be removed entirely.
    
    All transformations are of the form:
    Before:
        #if (CONFIG_COMMANDS & CFG_CMD_AUTOSCRIPT)
    After:
        #if (CONFIG_COMMANDS & CFG_CMD_AUTOSCRIPT) || defined(CONFIG_CMD_AUTOSCRIPT)
    
    Signed-off-by: Jon Loeliger <jdl@freescale.com>

--- tools/updater/cmd_flash.c
+++ tools/updater/cmd_flash.c
@@ -28,7 +28,7 @@
 #include <command.h>
 #include <flash.h>
 
-#if (CONFIG_COMMANDS & CFG_CMD_FLASH)
+#if (CONFIG_COMMANDS & CFG_CMD_FLASH) || defined(CONFIG_CMD_FLASH)
 
 extern flash_info_t flash_info[];	/* info for FLASH chips */
 
