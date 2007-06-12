45cdb9b72c94655c7308b464a2666057c0b286e0
Jon Loeliger <jdl@jdl.com>
2007-06-11 19:02:34 -0500

    disk/: Augment CONFIG_COMMANDS tests with defined(CONFIG_CMD_*).
    This is a compatibility step that allows both the older form
    and the new form to co-exist for a while until the older can
    be removed entirely.
    
    All transformations are of the form:
    Before:
        #if (CONFIG_COMMANDS & CFG_CMD_AUTOSCRIPT)
    After:
        #if (CONFIG_COMMANDS & CFG_CMD_AUTOSCRIPT) || defined(CONFIG_CMD_AUTOSCRIPT)
    
    Signed-off-by: Jon Loeliger <jdl@freescale.com>

--- disk/part_dos.c
+++ disk/part_dos.c
@@ -35,9 +35,9 @@
 #include <ide.h>
 #include "part_dos.h"
 
-#if ((CONFIG_COMMANDS & CFG_CMD_IDE)	|| \
-     (CONFIG_COMMANDS & CFG_CMD_SCSI)	|| \
-     (CONFIG_COMMANDS & CFG_CMD_USB)	|| \
+#if ((CONFIG_COMMANDS & CFG_CMD_IDE)	 || defined(CONFIG_CMD_IDE) || \
+     (CONFIG_COMMANDS & CFG_CMD_SCSI)	 || defined(CONFIG_CMD_SCSI) || \
+     (CONFIG_COMMANDS & CFG_CMD_USB)	 || defined(CONFIG_CMD_USB) || \
      defined(CONFIG_MMC) || \
      defined(CONFIG_SYSTEMACE) ) && defined(CONFIG_DOS_PARTITION)
 
--- disk/part_mac.c
+++ disk/part_mac.c
@@ -34,9 +34,9 @@
 #include <ide.h>
 #include "part_mac.h"
 
-#if ((CONFIG_COMMANDS & CFG_CMD_IDE)	|| \
-     (CONFIG_COMMANDS & CFG_CMD_SCSI)	|| \
-     (CONFIG_COMMANDS & CFG_CMD_USB)	|| \
+#if ((CONFIG_COMMANDS & CFG_CMD_IDE)	 || defined(CONFIG_CMD_IDE) || \
+     (CONFIG_COMMANDS & CFG_CMD_SCSI)	 || defined(CONFIG_CMD_SCSI) || \
+     (CONFIG_COMMANDS & CFG_CMD_USB)	 || defined(CONFIG_CMD_USB) || \
      defined(CONFIG_MMC) || \
      defined(CONFIG_SYSTEMACE) ) && defined(CONFIG_MAC_PARTITION)
 
