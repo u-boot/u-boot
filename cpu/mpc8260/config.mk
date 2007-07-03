5fc59175b92883ed5d2666a04e6bc49e70a4a365
Grant Likely <grant.likely@secretlab.ca>
2007-07-03 00:33:13 -0600

    Consolidate mpc8260 linker scripts
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- cpu/mpc8260/config.mk
+++ cpu/mpc8260/config.mk
@@ -25,3 +25,6 @@ PLATFORM_RELFLAGS += -fPIC -ffixed-r14 -meabi
 
 PLATFORM_CPPFLAGS += -DCONFIG_8260 -DCONFIG_CPM2 -ffixed-r2 -ffixed-r29 \
 		     -mstring -mcpu=603e -mmultiple
+
+# Use default linker script.  Board port can override in board/*/config.mk
+LDSCRIPT := $(SRCTREE)/cpu/mpc8260/u-boot.lds
