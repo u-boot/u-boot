334043f601a90ac53e5ecc846fbb73a1ef38cb1f
Stefan Roese <sr@denx.de>
2007-07-06 12:26:51 +0200

    ppc4xx: Update lwmon5 default environment
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/lwmon5.h
+++ include/configs/lwmon5.h
@@ -184,6 +184,8 @@
 	"update=protect off FFF80000 FFFFFFFF;era FFF80000 FFFFFFFF;"	\
 		"cp.b 200000 FFF80000 80000\0"			        \
 	"upd=run load;run update\0"					\
+	"lwe_env=tftp 200000 /tftpboot.dev/lwmon5/env_uboot.bin;"	\
+		"autoscr 200000\0"					\
 	""
 #define CONFIG_BOOTCOMMAND	"run flash_self"
 
