921d4b19ad1be704df58725485d9292dc0414adf
Kim Phillips <kim.phillips@freescale.com>
2007-11-19 12:30:09 -0600

    mpc83xx: fix CFG_ENV_ADDR and CFG_ENV_SECT_SIZE definitions for 837x
    Fix the definitions of CFG_ENV_ADDR and CFG_ENV_SECT_SIZE for 837x.
    This change guarantees that the environment will be located on the
    first flash sector after the U-Boot image.
    
    Signed-off-by: Kim Phillips <kim.phillips@freescale.com>

--- include/configs/MPC837XEMDS.h
+++ include/configs/MPC837XEMDS.h
@@ -197,6 +197,7 @@
 #undef CFG_RAMBOOT
 #endif
 
+/* CFG_MONITOR_LEN must be a multiple of CFG_ENV_SECT_SIZE */
 #define CFG_MONITOR_LEN		(256 * 1024) /* Reserve 256 kB for Mon */
 #define CFG_MALLOC_LEN		(512 * 1024) /* Reserved for malloc */
 
@@ -386,7 +387,7 @@
  */
 #ifndef CFG_RAMBOOT
 	#define CFG_ENV_IS_IN_FLASH	1
-	#define CFG_ENV_ADDR		(CFG_MONITOR_BASE + 0x40000)
+	#define CFG_ENV_ADDR		(CFG_MONITOR_BASE + CFG_MONITOR_LEN)
 	#define CFG_ENV_SECT_SIZE	0x20000 /* 128K(one sector) for env */
 	#define CFG_ENV_SIZE		0x2000
 #else
