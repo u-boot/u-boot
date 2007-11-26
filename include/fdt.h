a5f601fd1b1278deae5aa9fc27a232b0d1c1c788
Wolfgang Denk <wd@denx.de>
2007-11-26 19:18:21 +0100

    Cleanup coding style; update CHANGELOG
    Signed-off-by: Wolfgang Denk <wd@denx.de>

--- include/fdt.h
+++ include/fdt.h
@@ -4,22 +4,22 @@
 #ifndef __ASSEMBLY__
 
 struct fdt_header {
-	uint32_t magic;                  /* magic word FDT_MAGIC */
-	uint32_t totalsize;              /* total size of DT block */
-	uint32_t off_dt_struct;          /* offset to structure */
-	uint32_t off_dt_strings;         /* offset to strings */
-	uint32_t off_mem_rsvmap;         /* offset to memory reserve map */
-	uint32_t version;                /* format version */
-	uint32_t last_comp_version;      /* last compatible version */
+	uint32_t magic;			 /* magic word FDT_MAGIC */
+	uint32_t totalsize;		 /* total size of DT block */
+	uint32_t off_dt_struct;		 /* offset to structure */
+	uint32_t off_dt_strings;	 /* offset to strings */
+	uint32_t off_mem_rsvmap;	 /* offset to memory reserve map */
+	uint32_t version;		 /* format version */
+	uint32_t last_comp_version;	 /* last compatible version */
 
-        /* version 2 fields below */
-	uint32_t boot_cpuid_phys;        /* Which physical CPU id we're
+	/* version 2 fields below */
+	uint32_t boot_cpuid_phys;	 /* Which physical CPU id we're
 					    booting on */
 	/* version 3 fields below */
-        uint32_t size_dt_strings;        /* size of the strings block */
+	uint32_t size_dt_strings;	 /* size of the strings block */
 
 	/* version 17 fields below */
-	uint32_t size_dt_struct;         /* size of the structure block */
+	uint32_t size_dt_struct;	 /* size of the structure block */
 };
 
 struct fdt_reserve_entry {
@@ -41,12 +41,12 @@ struct fdt_property {
 
 #endif /* !__ASSEMBLY */
 
-#define FDT_MAGIC	0xd00dfeed      /* 4: version, 4: total size */
+#define FDT_MAGIC	0xd00dfeed	/* 4: version, 4: total size */
 #define FDT_TAGSIZE	sizeof(uint32_t)
 
-#define FDT_BEGIN_NODE	0x1             /* Start node: full name */
-#define FDT_END_NODE	0x2             /* End node */
-#define FDT_PROP	0x3             /* Property: name off,
+#define FDT_BEGIN_NODE	0x1		/* Start node: full name */
+#define FDT_END_NODE	0x2		/* End node */
+#define FDT_PROP	0x3		/* Property: name off,
 					   size, content */
 #define FDT_NOP		0x4		/* nop */
 #define FDT_END		0x9
