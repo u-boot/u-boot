22abb2d2eaf7b795a6923c6273ec9cb53fda9a10
Kumar Gala <galak@kernel.crashing.org>
2007-11-29 10:34:28 -0600

    Update Freescale MPC85xx ADS/CDS/MDS board config
    * Removed some misc environment setup
    * Enabled CONFIG_CMDLINE_EDITING
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- include/configs/MPC8568MDS.h
+++ include/configs/MPC8568MDS.h
@@ -458,6 +458,7 @@ extern unsigned long get_clock_freq(void);
  * Miscellaneous configurable options
  */
 #define CFG_LONGHELP			/* undef to save memory	*/
+#define CONFIG_CMDLINE_EDITING		/* Command-line editing */
 #define CFG_LOAD_ADDR	0x2000000	/* default load address */
 #define CFG_PROMPT	"=> "		/* Monitor Command Prompt */
 #if defined(CONFIG_CMD_KGDB)
