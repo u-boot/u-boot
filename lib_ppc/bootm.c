7b325454fd231d4273de3fe373850f777fb086bf
Marian Balakowicz <m8@semihalf.com>
2008-01-31 13:58:20 +0100

    [new uImage] Cleanup FDT handling in PPC do_boot_linux()
    Move FDT blob finding and relocation to a dedicated
    get_fdt() routine. It increases code readability and
    will make adding support for new uImage format easier.
    
    Signed-off-by: Marian Balakowicz <m8@semihalf.com>

--- lib_ppc/bootm.c
+++ lib_ppc/bootm.c
@@ -41,6 +41,9 @@
 #include <fdt_support.h>
 
 static void fdt_error (const char *msg);
+static ulong get_fdt (ulong alloc_current, cmd_tbl_t *cmdtp, int flag,
+		int argc, char *argv[],
+		image_header_t *hdr, char **of_flat_tree);
 #endif
 
 #ifdef CFG_INIT_RAM_LOCK
@@ -69,9 +72,7 @@ do_bootm_linux(cmd_tbl_t *cmdtp, int flag,
 	void	(*kernel)(bd_t *, ulong, ulong, ulong, ulong);
 
 #if defined(CONFIG_OF_LIBFDT)
-	image_header_t *fdt_hdr;
-	char	*of_flat_tree = NULL;
-	ulong	of_data = 0;
+	char	*of_flat_tree;
 #endif
 
 	/*
@@ -111,131 +112,9 @@ do_bootm_linux(cmd_tbl_t *cmdtp, int flag,
 
 #if defined(CONFIG_OF_LIBFDT)
 	/* find flattened device tree */
-	if(argc > 3) {
-		of_flat_tree = (char *) simple_strtoul(argv[3], NULL, 16);
-		fdt_hdr = (image_header_t *)of_flat_tree;
+	alloc_current = get_fdt (alloc_current,
+			cmdtp, flag, argc, argv, hdr, &of_flat_tree);
 
-		if (fdt_check_header (of_flat_tree) == 0) {
-#ifndef CFG_NO_FLASH
-			if (addr2info((ulong)of_flat_tree) != NULL)
-				of_data = (ulong)of_flat_tree;
-#endif
-		} else if (image_check_magic (fdt_hdr)) {
-			ulong image_start, image_end;
-			ulong load_start, load_end;
-
-			printf ("## Flat Device Tree at %08lX\n", fdt_hdr);
-			print_image_hdr (fdt_hdr);
-
-			image_start = (ulong)fdt_hdr;
-			image_end = image_get_image_end (fdt_hdr);
-
-			load_start = image_get_load (fdt_hdr);
-			load_end = load_start + image_get_data_size (fdt_hdr);
-
-			if ((load_start < image_end) && (load_end > image_start)) {
-				fdt_error ("fdt overwritten");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-
-			puts ("   Verifying Checksum ... ");
-			if (!image_check_hcrc (fdt_hdr)) {
-				fdt_error ("fdt header checksum invalid");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-
-			if (!image_check_dcrc (fdt_hdr)) {
-				fdt_error ("fdt checksum invalid");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-			puts ("OK\n");
-
-			if (!image_check_type (fdt_hdr, IH_TYPE_FLATDT)) {
-				fdt_error ("uImage is not a fdt");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-			if (image_get_comp (fdt_hdr) != IH_COMP_NONE) {
-				fdt_error ("uImage is compressed");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-			if (fdt_check_header (of_flat_tree + image_get_header_size ()) != 0) {
-				fdt_error ("uImage data is not a fdt");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-
-			memmove ((void *)image_get_load (fdt_hdr),
-				(void *)(of_flat_tree + image_get_header_size ()),
-				image_get_data_size (fdt_hdr));
-
-			of_flat_tree = (char *)image_get_load (fdt_hdr);
-		} else {
-			fdt_error ("Did not find a Flattened Device Tree");
-			do_reset (cmdtp, flag, argc, argv);
-		}
-		printf ("   Booting using the fdt at 0x%x\n",
-				of_flat_tree);
-	} else if (image_check_type (hdr, IH_TYPE_MULTI)) {
-		ulong fdt_data, fdt_len;
-
-		image_multi_getimg (hdr, 2, &fdt_data, &fdt_len);
-		if (fdt_len) {
-
-			of_flat_tree = (char *)fdt_data;
-
-#ifndef CFG_NO_FLASH
-			/* move the blob if it is in flash (set of_data to !null) */
-			if (addr2info ((ulong)of_flat_tree) != NULL)
-				of_data = (ulong)of_flat_tree;
-#endif
-
-			if (fdt_check_header (of_flat_tree) != 0) {
-				fdt_error ("image is not a fdt");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-
-			if (be32_to_cpu (fdt_totalsize (of_flat_tree)) != fdt_len) {
-				fdt_error ("fdt size != image size");
-				do_reset (cmdtp, flag, argc, argv);
-			}
-		}
-	}
-#endif
-
-
-#if defined(CONFIG_OF_LIBFDT)
-
-#ifdef CFG_BOOTMAPSZ
-	/*
-	 * The blob must be within CFG_BOOTMAPSZ,
-	 * so we flag it to be copied if it is not.
-	 */
-	if (of_flat_tree >= (char *)CFG_BOOTMAPSZ)
-		of_data = (ulong)of_flat_tree;
-#endif
-
-	/* move of_flat_tree if needed */
-	if (of_data) {
-		int err;
-		ulong of_start, of_len;
-
-		of_len = be32_to_cpu(fdt_totalsize(of_data));
-
-		/* position on a 4K boundary before the alloc_current */
-		of_start  = alloc_current - of_len;
-		of_start &= ~(4096 - 1);	/* align on page */
-		debug ("## device tree at 0x%08lX ... 0x%08lX (len=%ld=0x%lX)\n",
-			of_data, of_data + of_len - 1, of_len, of_len);
-
-		of_flat_tree = (char *)of_start;
-		printf ("   Loading Device Tree to %08lx, end %08lx ... ",
-			of_start, of_start + of_len - 1);
-		err = fdt_open_into((void *)of_data, (void *)of_start, of_len);
-		if (err != 0) {
-			fdt_error ("fdt move failed");
-			do_reset (cmdtp, flag, argc, argv);
-		}
-		puts ("OK\n");
-	}
 	/*
 	 * Add the chosen node if it doesn't exist, add the env and bd_t
 	 * if the user wants it (the logic is in the subroutines).
@@ -341,5 +220,157 @@ static void fdt_error (const char *msg)
 	puts ("ERROR: ");
 	puts (msg);
 	puts (" - must RESET the board to recover.\n");
+}
+
+static ulong get_fdt (ulong alloc_current,
+		cmd_tbl_t *cmdtp, int flag,
+		int argc, char *argv[],
+		image_header_t *hdr, char **of_flat_tree)
+{
+	image_header_t	*fdt_hdr;
+	ulong		fdt_relocate = 0;
+	char		*fdt = NULL;
+	ulong		new_alloc_current;
+
+	if(argc > 3) {
+		fdt = (char *)simple_strtoul (argv[3], NULL, 16);
+		fdt_hdr = (image_header_t *)fdt;
+
+		if (fdt_check_header (fdt) == 0) {
+			printf ("## Flattened Device Tree blob at %08lx\n", fdt);
+#ifndef CFG_NO_FLASH
+			if (addr2info ((ulong)fdt) != NULL)
+				fdt_relocate = 1;
+#endif
+		} else if (image_check_magic (fdt_hdr)) {
+			ulong image_start, image_end;
+			ulong load_start, load_end;
+
+			printf ("## Flattened Device Tree Image at %08lx\n",
+					fdt_hdr);
+
+			print_image_hdr (fdt_hdr);
+
+			image_start = (ulong)fdt_hdr;
+			image_end = image_get_image_end (fdt_hdr);
+
+			load_start = image_get_load (fdt_hdr);
+			load_end = load_start + image_get_data_size (fdt_hdr);
+
+			if ((load_start < image_end) && (load_end > image_start)) {
+				fdt_error ("fdt overwritten");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+
+			puts ("   Verifying Checksum ... ");
+			if (!image_check_hcrc (fdt_hdr)) {
+				fdt_error ("fdt header checksum invalid");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+
+			if (!image_check_dcrc (fdt_hdr)) {
+				fdt_error ("fdt checksum invalid");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+			puts ("OK\n");
+
+			if (!image_check_type (fdt_hdr, IH_TYPE_FLATDT)) {
+				fdt_error ("uImage is not a fdt");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+			if (image_get_comp (fdt_hdr) != IH_COMP_NONE) {
+				fdt_error ("uImage is compressed");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+			if (fdt_check_header ((char *)image_get_data (fdt_hdr)) != 0) {
+				fdt_error ("uImage data is not a fdt");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+
+			memmove ((void *)image_get_load (fdt_hdr),
+				(void *)image_get_data (fdt_hdr),
+				image_get_data_size (fdt_hdr));
+
+			fdt = (char *)image_get_load (fdt_hdr);
+		} else {
+			fdt_error ("Did not find a Flattened Device Tree");
+			do_reset (cmdtp, flag, argc, argv);
+		}
+		printf ("   Booting using the fdt at 0x%x\n",
+				fdt);
+	} else if (image_check_type (hdr, IH_TYPE_MULTI)) {
+		ulong fdt_data, fdt_len;
+
+		printf ("## Flattened Device Tree from multi "
+			"component Image at %08lX\n", (ulong)hdr);
+
+		image_multi_getimg (hdr, 2, &fdt_data, &fdt_len);
+		if (fdt_len) {
+
+			fdt = (char *)fdt_data;
+			printf ("   Booting using the fdt at 0x%x\n", fdt);
+
+#ifndef CFG_NO_FLASH
+			/* move the blob if it is in flash (set of_relocte) */
+			if (addr2info ((ulong)fdt) != NULL)
+				fdt_relocate = 1;
+#endif
+
+			if (fdt_check_header (fdt) != 0) {
+				fdt_error ("image is not a fdt");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+
+			if (be32_to_cpu (fdt_totalsize (fdt)) != fdt_len) {
+				fdt_error ("fdt size != image size");
+				do_reset (cmdtp, flag, argc, argv);
+			}
+		} else {
+			debug ("   Did not find a Flattened Device Tree");
+		}
+	}
+
+#ifdef CFG_BOOTMAPSZ
+	/*
+	 * The blob must be within CFG_BOOTMAPSZ,
+	 * so we flag it to be copied if it is not.
+	 */
+	if (fdt >= (char *)CFG_BOOTMAPSZ)
+		fdt_relocate = 1;
+#endif
+
+	/* move flattend device tree if needed */
+	if (fdt_relocate) {
+		int err;
+		ulong of_start, of_len;
+
+		of_len = be32_to_cpu (fdt_totalsize (fdt));
+
+		/* position on a 4K boundary before the alloc_current */
+		of_start  = alloc_current - of_len;
+		of_start &= ~(4096 - 1);	/* align on page */
+
+		debug ("## device tree at 0x%08lX ... 0x%08lX (len=%ld=0x%lX)\n",
+			(ulong)fdt, (ulong)fdt + of_len - 1,
+			of_len, of_len);
+
+		printf ("   Loading Device Tree to %08lx, end %08lx ... ",
+			of_start, of_start + of_len - 1);
+
+		err = fdt_open_into (fdt, (void *)of_start, of_len);
+		if (err != 0) {
+			fdt_error ("fdt move failed");
+			do_reset (cmdtp, flag, argc, argv);
+		}
+		puts ("OK\n");
+
+		*of_flat_tree = (char *)of_start;
+		new_alloc_current = of_start;
+	} else {
+		*of_flat_tree = fdt;
+		new_alloc_current = alloc_current;
+	}
+
+	return new_alloc_current;
 }
 #endif
