b86b3416f874358acaf07519e7620cdb2145f75b
Mike Frysinger <vapier@gentoo.org>
2008-02-19 00:50:58 -0500

    Blackfin: cleanup lib_blackfin/cache.c
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- lib_blackfin/cache.c
+++ lib_blackfin/cache.c
@@ -1,45 +1,26 @@
 /*
  * U-boot - cache.c
  *
- * Copyright (c) 2005-2007 Analog Devices Inc.
+ * Copyright (c) 2005-2008 Analog Devices Inc.
  *
  * (C) Copyright 2000-2004
  * Wolfgang Denk, DENX Software Engineering, wd@denx.de.
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
- * MA 02110-1301 USA
+ * Licensed under the GPL-2 or later.
  */
 
-/* for now: just dummy functions to satisfy the linker */
-#include <config.h>
 #include <common.h>
 #include <asm/blackfin.h>
-#include "cache.h"
 
-void flush_cache(unsigned long dummy1, unsigned long dummy2)
+void flush_cache(unsigned long addr, unsigned long size)
 {
-	if (dummy1 >= 0xE0000000)
+	/* no need to flush stuff in on chip memory (L1/L2/etc...) */
+	if (addr >= 0xE0000000)
 		return;
 
 	if (icache_status())
-		blackfin_icache_flush_range((void*)dummy1, (void*)(dummy1 + dummy2));
-	if (dcache_status())
-		blackfin_dcache_flush_range((void*)dummy1, (void*)(dummy1 + dummy2));
+		blackfin_icache_flush_range((void *)addr, (void *)(addr + size));
 
-	return;
+	if (dcache_status())
+		blackfin_dcache_flush_range((void *)addr, (void *)(addr + size));
 }
