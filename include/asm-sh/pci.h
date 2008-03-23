28e5efde4d925fcb34901d0030d0648de2da7e89
Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
2008-03-24 01:53:01 +0900

    sh: Add support PCI host driver for SH7751/SH7751R
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- include/asm-sh/pci.h
+++ include/asm-sh/pci.h
@@ -1,7 +1,7 @@
 /*
  * SH4 PCI Controller (PCIC) for U-Boot.
  * (C) Dustin McIntire (dustin@sensoria.com)
- * (C) 2007 Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
+ * (C) 2007,2008 Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
  * (C) 2008 Yusuke Goda <goda.yusuke@renesas.com>
  *
  * u-boot/include/asm-sh/pci.h
@@ -28,8 +28,9 @@
 #define _ASM_PCI_H_
 
 #include <pci.h>
-
-#if defined(CONFIG_CPU_SH7780)
+#if defined(CONFIG_SH7751_PCI)
+int pci_sh7751_init(struct pci_controller *hose);
+#elif defined(CONFIG_SH7780_PCI)
 int pci_sh7780_init(struct pci_controller *hose);
 #else
 #error "Not support PCI."
