d0d91ae3acb4f29d1a2a3a766747478ed54e2848
Stefan Roese <sr@denx.de>
2008-04-25 13:59:03 +0200

    ppc4xx: Remove double defines in lwmon5.h
    introduced with latest lwmon5/POST merge
    
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/lwmon5.h
+++ include/configs/lwmon5.h
@@ -98,15 +98,6 @@
 #define CFG_WATCHDOG_MAGIC_MASK	0xFFFF0000
 #define CFG_DSPIC_TEST_MASK	0x00000001
 
-/* Additional registers for watchdog timer post test */
-
-#define CFG_DSPIC_TEST_ADDR	(CFG_PERIPHERAL_BASE + GPT0_COMP5)
-#define CFG_WATCHDOG_TIME_ADDR	(CFG_PERIPHERAL_BASE + GPT0_COMP4)
-#define CFG_WATCHDOG_FLAGS_ADDR	(CFG_PERIPHERAL_BASE + GPT0_COMP5)
-#define CFG_WATCHDOG_MAGIC	0x12480000
-#define CFG_WATCHDOG_MAGIC_MASK	0xFFFF0000
-#define CFG_DSPIC_TEST_MASK	0x00000001
-
 /*-----------------------------------------------------------------------
  * Serial Port
  *----------------------------------------------------------------------*/
