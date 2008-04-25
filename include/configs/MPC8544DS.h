292188e15523c165c4269403fdcd33c26d89176e
Roy Zang <tie-fei.zang@freescale.com>
2008-04-25 00:55:09 -0500

    MPC8544DS: Removes the unknown flash message information
    This patch removes the unknown flash message information:
    '## Unknown FLASH on Bank 1 - Size = 0xdeadbeef = -286261248 MB'
    This unknown flash message is caused by PromJet.
    Some of the board user is unhappy with this information.
    
    Signed-off-by: Roy Zang <tie-fei.zang@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- include/configs/MPC8544DS.h
+++ include/configs/MPC8544DS.h
@@ -165,6 +165,7 @@ extern unsigned long get_board_sys_clk(unsigned long dummy);
 
 #define CFG_FLASH_BANKS_LIST	{0xfe800000,CFG_FLASH_BASE}
 
+#define CFG_FLASH_QUIET_TEST
 #define CFG_MAX_FLASH_BANKS	2		/* number of banks */
 #define CFG_MAX_FLASH_SECT	128		/* sectors per device */
 #undef	CFG_FLASH_CHECKSUM
