70a0f81412b0b18a6fd0bea960451bc6c2cca49a
Kumar Gala <galak@kernel.crashing.org>
2008-04-29 12:54:59 -0500

    85xx: Add -mno-spe to e500/85xx builds
    Newer gcc's might be configured to enable autovectorization by default.
    If we happen to build with one of those compilers we will get SPE
    instructions in random code.
    
    -mno-spe disables the compiler for automatically generating SPE
    instructions without our knowledge.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- cpu/mpc85xx/config.mk
+++ cpu/mpc85xx/config.mk
@@ -25,3 +25,4 @@ PLATFORM_RELFLAGS += -fPIC -ffixed-r14 -meabi
 
 PLATFORM_CPPFLAGS += -DCONFIG_MPC85xx -DCONFIG_E500 -ffixed-r2 \
 			-Wa,-me500 -msoft-float -mno-string
+PLATFORM_CPPFLAGS +=$(call cc-option,-mno-spe)
