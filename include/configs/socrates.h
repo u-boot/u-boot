2f845dc2bdf461bfee9fa25823f769f5db9eba0b
Sergei Poselenov <sposelenov@emcraft.com>
2008-05-08 17:46:23 +0200

    socrates: fix second TSEC configuration (it is actually TSEC3)
    Signed-off-by: Sergei Poselenov <sposelenov@emcraft.com>

--- include/configs/socrates.h
+++ include/configs/socrates.h
@@ -242,19 +242,19 @@
 #define CONFIG_MII		1	/* MII PHY management */
 #define CONFIG_TSEC1	1
 #define CONFIG_TSEC1_NAME	"TSEC0"
-#define CONFIG_TSEC2	1
-#define CONFIG_TSEC2_NAME	"TSEC1"
+#define CONFIG_TSEC3	1
+#define CONFIG_TSEC3_NAME	"TSEC1"
 #undef CONFIG_MPC85XX_FEC
 
 #define TSEC1_PHY_ADDR		0
-#define TSEC2_PHY_ADDR		1
+#define TSEC3_PHY_ADDR		1
 
 #define TSEC1_PHYIDX		0
-#define TSEC2_PHYIDX		0
+#define TSEC3_PHYIDX		0
 #define TSEC1_FLAGS		TSEC_GIGABIT
-#define TSEC2_FLAGS		TSEC_GIGABIT
+#define TSEC3_FLAGS		TSEC_GIGABIT
 
-/* Options are: TSEC[0-1] */
+/* Options are: TSEC[0,1] */
 #define CONFIG_ETHPRIME		"TSEC0"
 #define CONFIG_PHY_GIGE		1	/* Include GbE speed/duplex detection */
 
