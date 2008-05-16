289011207d999b2e4085150d2aa30d547ad9b800
Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
2008-05-16 11:10:30 +0200

    Move definition of container_of() to common.h
    AVR32 and AT91SAM9 both have their own identical definitions of
    container_of() taken from the Linux kernel. Move it to common.h so
    that all architectures can use it.
    
    container_of() is already used by some drivers, and will be used
    extensively by the new and improved SPI API.
    
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- include/asm-arm/arch-at91sam9/hardware.h
+++ include/asm-arm/arch-at91sam9/hardware.h
@@ -51,15 +51,4 @@
 #error "Unsupported AT91 processor"
 #endif
 
-/*
- * container_of - cast a member of a structure out to the containing structure
- *
- * @ptr:	the pointer to the member.
- * @type:	the type of the container struct this is embedded in.
- * @member:	the name of the member within the struct.
- */
-#define container_of(ptr, type, member) ({			\
-	const typeof(((type *)0)->member) *__mptr = (ptr);	\
-	(type *)((char *)__mptr - offsetof(type, member)); })
-
 #endif
