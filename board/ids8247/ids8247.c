27269417ade432189b234d9fbac98b54e37b978c
Matvejchikov Ilya <matvejchikov@gmail.com>
2008-07-06 13:57:58 +0400

    Some copy-n-paste fixes in printf usage
    Signed-off-by: Matvejchikov Ilya <matvejchikov@gmail.com>

--- board/ids8247/ids8247.c
+++ board/ids8247/ids8247.c
@@ -334,7 +334,7 @@ void ft_blob_update(void *blob, bd_t *bd)
 	ret = fdt_fixup_memory(blob, (u64)bd->bi_memstart, (u64)bd->bi_memsize);
 
 	if (ret < 0) {
-		printf("ft_blob_update): cannot set /memory/reg "
+		printf("ft_blob_update(): cannot set /memory/reg "
 			"property err:%s\n", fdt_strerror(ret));
 	}
 }
