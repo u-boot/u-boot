98874ff329d4a5b32c467b43f6e966e1aa68479f
Andy Fleming <afleming@freescale.com>
2008-07-07 14:24:39 -0500

    Fix LMB type issues
    The LMB code now uses phys_addr_t and phys_size_t.  Also, there were a couple
    of casting problems in the bootm code that called the LMB functions.
    
    Signed-off-by: Andy Fleming <afleming@freescale.com>

--- lib_ppc/bootm.c
+++ lib_ppc/bootm.c
@@ -212,7 +212,8 @@ do_bootm_linux(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[],
 		}
 
 		/* Delete the old LMB reservation */
-		lmb_free(lmb, (uint64_t)(u32)of_flat_tree, fdt_totalsize(of_flat_tree));
+		lmb_free(lmb, (phys_addr_t)(u32)of_flat_tree,
+				(phys_size_t)fdt_totalsize(of_flat_tree));
 
 		/* Calculate the actual size of the fdt */
 		actualsize = fdt_off_dt_strings(of_flat_tree) +
