c3ca7e5e00a24451f20df3bded9a61ba541921df
Paul Gortmaker <paul.gortmaker@windriver.com>
2008-07-11 15:33:08 -0400

    sbc8560: enable CONFIG_OF_LIBFDT by default
    Make the default build for the sbc8560 board be powerpc
    capable with libfdt support.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/configs/sbc8560.h
+++ include/configs/sbc8560.h
@@ -200,6 +200,11 @@
 #define CFG_PROMPT_HUSH_PS2 "> "
 #endif
 
+/* pass open firmware flat tree */
+#define CONFIG_OF_LIBFDT                1
+#define CONFIG_OF_BOARD_SETUP           1
+#define CONFIG_OF_STDOUT_VIA_ALIAS      1
+
 /*
  * I2C
  */
