dbf3dfb386a2d5d2381814e39985ab2e21894550
Marcel Ziswiler <marcel@ziswiler.com>
2008-07-11 02:39:14 +0200

    Enable passing of ATAGs required by latest Linux kernel.

--- include/configs/pxa255_idp.h
+++ include/configs/pxa255_idp.h
@@ -208,6 +208,10 @@
 /*			"protect off"	*/
 
 
+#define CONFIG_CMDLINE_TAG		1	/* enable passing of ATAGs */
+#define CONFIG_SETUP_MEMORY_TAGS	1
+/* #define CONFIG_INITRD_TAG		1 */
+
 #if defined(CONFIG_CMD_KGDB)
 #define CONFIG_KGDB_BAUDRATE	115200		/* speed to run kgdb serial port */
 #define CONFIG_KGDB_SER_INDEX	2		/* which serial port to use */
