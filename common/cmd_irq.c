ffa4bafacaef67058463b3d7d0099ced57569dd2
Kumar Gala <galak@kernel.crashing.org>
2008-08-15 08:24:33 -0500

    Add command to enable/disable interrupts
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

---
+++ common/cmd_irq.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2008 Freescale Semiconductor, Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <config.h>
+#include <command.h>
+
+int do_interrupts(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
+{
+
+	if (argc != 2) {
+		printf ("Usage:\n%s\n", cmdtp->usage);
+		return 1;
+	}
+
+	/* on */
+	if (strncmp(argv[1], "on", 2) == 0) {
+		enable_interrupts();
+	} else {
+		disable_interrupts();
+	}
+
+	return 0;
+}
+
+U_BOOT_CMD(
+	interrupts, 5, 0, do_interrupts,
+	"interrupts - enable or disable interrupts\n",
+	"[on, off]\n"
+	"    - enable or disable interrupts\n"
+);
