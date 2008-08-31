652f7c2eef76a1340928bd660845441e932d86a2
Andy Fleming <afleming@freescale.com>
2008-08-31 16:33:28 -0500

    Add support for Freescale SGMII Riser Card
    The 8544DS and 8572DS systems have an optional SGMII riser card which
    exposes new ethernet ports which are connected to the eTSECs via an
    SGMII interface.  The SGMII PHYs for this board are offset from the standard
    PHY addresses, so this code modifies the passed in tsec_info structure to
    use the SGMII PHYs on the card, instead.
    
    Signed-off-by: Andy Fleming <afleming@freescale.com>
    Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

--- include/configs/MPC8544DS.h
+++ include/configs/MPC8544DS.h
@@ -369,6 +369,9 @@ extern unsigned long get_board_sys_clk(unsigned long dummy);
 #define CONFIG_TSEC3	1
 #define CONFIG_TSEC3_NAME	"eTSEC3"
 
+#define CONFIG_FSL_SGMII_RISER	1
+#define SGMII_RISER_PHY_OFFSET	0x1c
+
 #define TSEC1_PHY_ADDR		0
 #define TSEC3_PHY_ADDR		1
 
