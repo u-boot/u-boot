fecb5ade3b37f62981f2b05b621005850173aaa9
Jason Jin <Jason.jin@freescale.com>
2008-09-19 17:32:49 +0800

    Fix the NAND size overflow issue.
    When the total size of all NAND devices exceeds 4 GiB, the size  will
    overflow. This patch tries to fix this.
    
    Note that we still have a problem when a single NAND device is bigger
    than 4 GiB: then the overflow would actually happen earlier, i. e.
    when storing the size in nand_info[].size, as nand_info[].size is an
    "u_int32_t".
    
    Signed-off-by: Jason Jin <Jason.jin@freescale.com>
    Signed-off-by: Wolfgang Denk <wd@denx.de>

--- drivers/mtd/nand/nand.c
+++ drivers/mtd/nand/nand.c
@@ -63,11 +63,11 @@ void nand_init(void)
 	unsigned int size = 0;
 	for (i = 0; i < CFG_MAX_NAND_DEVICE; i++) {
 		nand_init_chip(&nand_info[i], &nand_chip[i], base_address[i]);
-		size += nand_info[i].size;
+		size += nand_info[i].size / 1024;
 		if (nand_curr_device == -1)
 			nand_curr_device = i;
 	}
-	printf("%u MiB\n", size / (1024 * 1024));
+	printf("%u MiB\n", size / 1024);
 
 #ifdef CFG_NAND_SELECT_DEVICE
 	/*
