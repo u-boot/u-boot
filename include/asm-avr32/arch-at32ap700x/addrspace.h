a38de083d2979db3680f0d0978c509a172c8fa00
Olav Morken <olavmrk@gmail.com>
2009-01-23 12:56:28 +0100

    AVR32: Move addrspace.h to arch-directory, and move some functions from io.h to addrspace.h
    The AVR32A architecture (which AT32UC3A-series is based on) has a
    different memory layout than the AVR32B-architecture. This patch moves
    addrspace.h to an arch-dependent directory in preparation for
    AT32UC3A-support. It also moves some address-space manipulation
    functions from io.h to addrspace.h.
    
    Signed-off-by: Gunnar Rangoy <gunnar@rangoy.com>
    Signed-off-by: Paul Driveklepp <pauldriveklepp@gmail.com>
    Signed-off-by: Olav Morken <olavmrk@gmail.com>
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- include/asm-avr32/arch-at32ap700x/addrspace.h
+++ include/asm-avr32/arch-at32ap700x/addrspace.h
@@ -22,6 +22,8 @@
 #ifndef __ASM_AVR32_ADDRSPACE_H
 #define __ASM_AVR32_ADDRSPACE_H
 
+#include <asm/types.h>
+
 /* Memory segments when segmentation is enabled */
 #define P0SEG		0x00000000
 #define P1SEG		0x80000000
@@ -42,5 +44,41 @@
 #define P2SEGADDR(a) ((__typeof__(a))(((unsigned long)(a) & 0x1fffffff) | P2SEG))
 #define P3SEGADDR(a) ((__typeof__(a))(((unsigned long)(a) & 0x1fffffff) | P3SEG))
 #define P4SEGADDR(a) ((__typeof__(a))(((unsigned long)(a) & 0x1fffffff) | P4SEG))
+
+/* virt_to_phys will only work when address is in P1 or P2 */
+static inline unsigned long virt_to_phys(volatile void *address)
+{
+	return PHYSADDR(address);
+}
+
+static inline void * phys_to_virt(unsigned long address)
+{
+	return (void *)P1SEGADDR(address);
+}
+
+#define cached(addr) ((void *)P1SEGADDR(addr))
+#define uncached(addr) ((void *)P2SEGADDR(addr))
+
+/*
+ * Given a physical address and a length, return a virtual address
+ * that can be used to access the memory range with the caching
+ * properties specified by "flags".
+ *
+ * This implementation works for memory below 512MiB (flash, etc.) as
+ * well as above 3.5GiB (internal peripherals.)
+ */
+#define MAP_NOCACHE	(0)
+#define MAP_WRCOMBINE	(1 << 7)
+#define MAP_WRBACK	(MAP_WRCOMBINE | (1 << 9))
+#define MAP_WRTHROUGH	(MAP_WRBACK | (1 << 0))
+
+static inline void *
+map_physmem(phys_addr_t paddr, unsigned long len, unsigned long flags)
+{
+	if (flags == MAP_WRBACK)
+		return (void *)P1SEGADDR(paddr);
+	else
+		return (void *)P2SEGADDR(paddr);
+}
 
 #endif /* __ASM_AVR32_ADDRSPACE_H */
