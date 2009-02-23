ff7dc067369e30066744f096995aef7d97574d15
Stefan Roese <sr@denx.de>
2009-02-23 16:52:36 +0100

    powerpc: Fix compilation warning in board.c
    Fix this warning:
    
    board.c: In function 'board_init_r':
    board.c:653: warning: unused variable 'i'
    board.c:651: warning: unused variable 'e'
    
    Signed-off-by: Stefan Roese <sr@denx.de>

--- lib_ppc/board.c
+++ lib_ppc/board.c
@@ -645,9 +645,8 @@ void board_init_f (ulong bootflag)
 void board_init_r (gd_t *id, ulong dest_addr)
 {
 	cmd_tbl_t *cmdtp;
-	char *s, *e;
+	char *s;
 	bd_t *bd;
-	int i;
 	extern void malloc_bin_reloc (void);
 #ifndef CONFIG_ENV_IS_NOWHERE
 	extern char * env_name_spec;
