2a325ce3c705ab2d82760afc541c511328472df8
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2009-03-27 23:26:42 +0100

    at91rm9200dk: Move conditional compilation to Makefile
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- board/atmel/at91rm9200dk/mux.c
+++ board/atmel/at91rm9200dk/mux.c
@@ -4,34 +4,26 @@
 #include <dataflash.h>
 
 int AT91F_GetMuxStatus(void) {
-#ifdef	CONFIG_DATAFLASH_MMC_SELECT
 	AT91C_BASE_PIOB->PIO_PER = CONFIG_SYS_DATAFLASH_MMC_PIO; /* Set in PIO mode */
 	AT91C_BASE_PIOB->PIO_OER = CONFIG_SYS_DATAFLASH_MMC_PIO; /* Configure in output */
 
 
-	if(AT91C_BASE_PIOB->PIO_ODSR & CONFIG_SYS_DATAFLASH_MMC_PIO) {
+	if(AT91C_BASE_PIOB->PIO_ODSR & CONFIG_SYS_DATAFLASH_MMC_PIO)
 		return 1;
-	} else {
-		return 0;
-	}
-#endif
+
 	return 0;
 }
 
 void AT91F_SelectMMC(void) {
-#ifdef	CONFIG_DATAFLASH_MMC_SELECT
 	AT91C_BASE_PIOB->PIO_PER = CONFIG_SYS_DATAFLASH_MMC_PIO; /* Set in PIO mode */
 	AT91C_BASE_PIOB->PIO_OER = CONFIG_SYS_DATAFLASH_MMC_PIO; /* Configure in output */
 	/* Set Output */
 	AT91C_BASE_PIOB->PIO_SODR = CONFIG_SYS_DATAFLASH_MMC_PIO;
-#endif
 }
 
 void AT91F_SelectSPI(void) {
-#ifdef	CONFIG_DATAFLASH_MMC_SELECT
 	AT91C_BASE_PIOB->PIO_PER = CONFIG_SYS_DATAFLASH_MMC_PIO; /* Set in PIO mode */
 	AT91C_BASE_PIOB->PIO_OER = CONFIG_SYS_DATAFLASH_MMC_PIO; /* Configure in output */
 	/* Clear Output */
 	AT91C_BASE_PIOB->PIO_CODR = CONFIG_SYS_DATAFLASH_MMC_PIO;
-#endif
 }
