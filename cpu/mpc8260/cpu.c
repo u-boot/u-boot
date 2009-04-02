3a671fc06ae1d9e8eba76600372317c75ae0eb3e
Scott Wood <scottwood@freescale.com>
2009-04-02 16:10:36 -0500

    mpc8260: Fill in brg's clock-frequency in device tree.
    Signed-off-by: Scott Wood <scottwood@freescale.com>

--- cpu/mpc8260/cpu.c
+++ cpu/mpc8260/cpu.c
@@ -311,6 +311,9 @@ void ft_cpu_setup (void *blob, bd_t *bd)
 	fdt_fixup_ethernet(blob);
 #endif
 
+	do_fixup_by_compat_u32(blob, "fsl,cpm2-brg",
+			       "clock-frequency", bd->bi_brgfreq, 1);
+
 	do_fixup_by_path_u32(blob, cpu_path, "bus-frequency", bd->bi_busfreq, 1);
 	do_fixup_by_path_u32(blob, cpu_path, "timebase-frequency", OF_TBCLK, 1);
 	do_fixup_by_path_u32(blob, cpu_path, "clock-frequency", bd->bi_intfreq, 1);
