7ce6031afc8671c8b47c6135b3678d43fcd02852
Prafulla Wadaskar <prafulla@marvell.com>
2009-04-06 21:24:43 +0530

    sf: new Macronix MX25xx SPI flash driver
    Added macronix SF driver for MTD framework
    MX25L12805D is supported and tested
    TBD: sector erase implementation, other deivces support
    
    Signed-off-by: Prafulla Wadaskar <prafulla@marvell.com>
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- drivers/mtd/spi/spi_flash.c
+++ drivers/mtd/spi/spi_flash.c
@@ -134,6 +134,11 @@ struct spi_flash *spi_flash_probe(unsigned int bus, unsigned int cs,
 		flash = spi_flash_probe_atmel(spi, idcode);
 		break;
 #endif
+#ifdef CONFIG_SPI_FLASH_MACRONIX
+	case 0xc2:
+		flash = spi_flash_probe_macronix(spi, idcode);
+		break;
+#endif
 #ifdef CONFIG_SPI_FLASH_STMICRO
 	case 0x20:
 		flash = spi_flash_probe_stmicro(spi, idcode);
