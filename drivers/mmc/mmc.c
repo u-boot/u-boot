b81830f6e3b3e6ed114d071eb107965e49fa9b5a
Ilya Yanok <yanok@emcraft.com>
2009-05-14 14:03:09 +0400

    mmc: it's safe to ignore mmc_send_if_cond() return value
    Return value of mmc_send_if_cond() can be safely ignored (as it is
    done in Linux). This makes older cards work with MXC MCI controller.
    
    Signed-off-by: Ilya Yanok <yanok@emcraft.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -867,10 +867,6 @@ int mmc_init(struct mmc *mmc)
 	/* Test for SD version 2 */
 	err = mmc_send_if_cond(mmc);
 
-	/* If we got an error other than timeout, we bail */
-	if (err && err != TIMEOUT)
-		return err;
-
 	/* Now try to get the SD card's operating condition */
 	err = sd_send_op_cond(mmc);
 
