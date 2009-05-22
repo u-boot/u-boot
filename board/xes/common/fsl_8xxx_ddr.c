25623937bb81cae788d767e6c59a11c96fc82866
Peter Tyser <ptyser@xes-inc.com>
2009-05-22 10:26:36 -0500

    xes: Update Freescale DDR code to work with 86xx processors
    Signed-off-by: Peter Tyser <ptyser@xes-inc.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- board/xes/common/fsl_8xxx_ddr.c
+++ board/xes/common/fsl_8xxx_ddr.c
@@ -32,9 +32,10 @@ phys_size_t initdram(int board_type)
 {
 	phys_size_t dram_size = fsl_ddr_sdram();
 
+#ifdef CONFIG_MPC85xx
 	dram_size = setup_ddr_tlbs(dram_size / 0x100000);
-
 	dram_size *= 0x100000;
+#endif
 
 #if defined(CONFIG_DDR_ECC) && !defined(CONFIG_ECC_INIT_VIA_DDRCONTROLLER)
 	/* Initialize and enable DDR ECC */
@@ -48,7 +49,12 @@ phys_size_t initdram(int board_type)
 void board_add_ram_info(int use_default)
 {
 #if (CONFIG_NUM_DDR_CONTROLLERS > 1)
+#if defined(CONFIG_MPC85xx)
 	volatile ccsr_ddr_t *ddr1 = (void *)(CONFIG_SYS_MPC85xx_DDR_ADDR);
+#elif defined(CONFIG_MPC86xx)
+	volatile immap_t *immap = (immap_t *)CONFIG_SYS_IMMR;
+	volatile ccsr_ddr_t *ddr1 = &immap->im_ddr1;
+#endif
 #endif
 
 	puts(" (");
