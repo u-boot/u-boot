8bf29b59fce8cc381114929082202d800e313ad5
Simon Kagstrom <simon.kagstrom@netinsight.net>
2009-07-07 13:58:51 +0200

    Add unaligned.h for arm
    This patch adds unaligned.h for ARM (needed to build with LZO
    compression). The file is taken from the linux kernel, but includes
    u-boot headers instead.
    
    Signed-off-by: Simon Kagstrom <simon.kagstrom@netinsight.net>
    Acked-by: Stefan Roese <sr@denx.de>

---
+++ include/asm-arm/unaligned.h
@@ -0,0 +1,18 @@
+#ifndef _ASM_ARM_UNALIGNED_H
+#define _ASM_ARM_UNALIGNED_H
+
+#include <linux/unaligned/access_ok.h>
+#include <linux/unaligned/generic.h>
+
+/*
+ * Select endianness
+ */
+#ifndef __ARMEB__
+#define get_unaligned	__get_unaligned_le
+#define put_unaligned	__put_unaligned_le
+#else
+#define get_unaligned	__get_unaligned_be
+#define put_unaligned	__put_unaligned_be
+#endif
+
+#endif /* _ASM_ARM_UNALIGNED_H */
