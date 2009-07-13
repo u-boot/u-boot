51d91e1a253c97713c7f3e5c0b910a4db4979283
Kumar Gala <galak@kernel.crashing.org>
2009-07-13 09:23:59 -0500

    drivers/bios_emulator: Fix gcc 4.4 compiler warning
    biosemu.c: In function 'BE_setVGA':
    biosemu.c:147: warning: dereferencing type-punned pointer will break strict-aliasing rules
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- drivers/bios_emulator/biosemu.c
+++ drivers/bios_emulator/biosemu.c
@@ -144,7 +144,8 @@ void X86API BE_setVGA(BE_VGAInfo * info)
 		_BE_env.biosmem_base = _BE_env.busmem_base + 0x20000;
 		_BE_env.biosmem_limit = 0xC7FFF;
 	}
-	if (*((u32 *) info->LowMem) == 0)
+	if ((info->LowMem[0] == 0) && (info->LowMem[1] == 0) &&
+	    (info->LowMem[2] == 0) && (info->LowMem[3] == 0))
 		_BE_bios_init((u32 *) info->LowMem);
 	memcpy((u8 *) M.mem_base, info->LowMem, sizeof(info->LowMem));
 }
