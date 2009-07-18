086ff34a3a7e5e595630d658c1c13778399452d1
Peter Tyser <ptyser@xes-inc.com>
2009-07-17 19:01:06 -0500

    xpedite1k: Remove support for reading MACs from EEPROM
    By default, the XPedite1000 comes installed with xMon, a proprietary
    bootloader.  xMon stores its MAC address in an onboard EEPROM.  Rather
    than requiring a non-standard location in the EEPROM to be reserved for
    MAC addresses, store the MAC addresses in U-Boot's standard environment.
    A U-Boot application or OS application can be used to migrate xMon MAC
    addresses to U-Boot's environment if necessary.
    
    Signed-off-by: Peter Tyser <ptyser@xes-inc.com>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/XPEDITE1K.h
+++ include/configs/XPEDITE1K.h
@@ -38,7 +38,6 @@
 #define CONFIG_440		1
 #define CONFIG_440GX		1		/* 440 GX */
 #define CONFIG_BOARD_EARLY_INIT_F 1		/* Call board_pre_init	*/
-#define CONFIG_MISC_INIT_R
 #define CONFIG_SYS_CLK_FREQ	33333333	/* external freq to pll */
 
 
