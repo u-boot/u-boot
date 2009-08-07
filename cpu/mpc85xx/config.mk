ec79d33b2c41ee8b6d1354cc0910217b769c5036
Kumar Gala <galak@kernel.crashing.org>
2009-08-07 13:00:55 -0500

    85xx: Move to a common linker script
    There are really no differences between all the 85xx linker scripts so
    we can just move to a single common one.  Board code is still able to
    override the common one if need be.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- cpu/mpc85xx/config.mk
+++ cpu/mpc85xx/config.mk
@@ -25,3 +25,6 @@ PLATFORM_RELFLAGS += -fPIC -ffixed-r14 -meabi
 
 PLATFORM_CPPFLAGS += -ffixed-r2 -Wa,-me500 -msoft-float -mno-string
 PLATFORM_CPPFLAGS +=$(call cc-option,-mno-spe)
+
+# Use default linker script.  Board port can override in board/*/config.mk
+LDSCRIPT := $(SRCTREE)/cpu/mpc85xx/u-boot.lds
