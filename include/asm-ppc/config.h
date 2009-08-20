3b1f243b8dad30a646a0f056b0268519eadbc3c5
Poonam Aggrwal <poonam.aggrwal@freescale.com>
2009-08-20 18:55:35 +0530

    85xx: Added CONFIG_MAX_CPUS for P1020
    Signed-off-by: Poonam Aggrwal <poonam.aggrwal@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- include/asm-ppc/config.h
+++ include/asm-ppc/config.h
@@ -38,8 +38,8 @@
 #endif
 #endif
 
-#if defined(CONFIG_MPC8572) || defined(CONFIG_P2020) \
-	|| defined(CONFIG_MPC8641)
+#if defined(CONFIG_MPC8572) || defined(CONFIG_P1020) || \
+	defined(CONFIG_P2020) || defined(CONFIG_MPC8641)
 #define CONFIG_MAX_CPUS        2
 #else
 #define CONFIG_MAX_CPUS        1
