d72871e1387094972569e4b77c25e88020f7b68f
Kumar Gala <galak@kernel.crashing.org>
2009-09-01 11:24:45 -0500

    arm: Remove -fno-strict-aliasing
    -fno-strict-aliasing is hidding warnings.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- cpu/ixp/config.mk
+++ cpu/ixp/config.mk
@@ -24,8 +24,7 @@
 
 BIG_ENDIAN = y
 
-PLATFORM_RELFLAGS += -fno-strict-aliasing  -fno-common -ffixed-r8 \
-	-msoft-float -mbig-endian
+PLATFORM_RELFLAGS += -fno-common -ffixed-r8 -msoft-float -mbig-endian
 
 PLATFORM_CPPFLAGS += -mbig-endian -march=armv5te -mtune=strongarm1100
 # =========================================================================
