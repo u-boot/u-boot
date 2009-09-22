0630535e2d062dd73c1ceca5c6125c86d1127a49
Peter Tyser <ptyser@xes-inc.com>
2009-09-22 09:27:55 -0500

    arm/microblaze/nios/nios2/sh: Remove relocation fixups
    These architectures don't need relocation fixups, so reduce their
    codesize a bit by defining CONFIG_RELOC_FIXUP_WORKS.
    
    Also remove the reloc_off field from their global data structures
    as it is no longer needed.
    
    Note that the location of the standalone application jump table pointer
    in the global data structure is affected by this change, breaking
    execution of standalone applications compiled for previous versions of
    U-Boot. We will therefore increment XF_VERSION in the next commit,
    which also touches this area.
    
    Signed-off-by: Peter Tyser <ptyser@xes-inc.com>
    Signed-off-by: Wolfgang Denk <wd@denx.de>

--- common/stdio.c
+++ common/stdio.c
@@ -201,7 +201,7 @@ int stdio_deregister(char *devname)
 
 int stdio_init (void)
 {
-#if !defined(CONFIG_ARM) && !defined(CONFIG_RELOC_FIXUP_WORKS)
+#if !defined(CONFIG_RELOC_FIXUP_WORKS)
 	/* already relocated for current ARM implementation */
 	ulong relocation_offset = gd->reloc_off;
 	int i;
@@ -211,7 +211,7 @@ int stdio_init (void)
 		stdio_names[i] = (char *) (((ulong) stdio_names[i]) +
 						relocation_offset);
 	}
-#endif /* !ARM && !CONFIG_RELOC_FIXUP_WORKS */
+#endif /* !CONFIG_RELOC_FIXUP_WORKS */
 
 	/* Initialize the list */
 	INIT_LIST_HEAD(&(devs.list));
