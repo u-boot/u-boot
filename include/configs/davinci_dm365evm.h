a4474ff8629be5f28aefb8a9f48d4411d62fb0d2
Sandeep Paulraj <s-paulraj@ti.com>
2009-10-13 19:35:11 -0400

    TI DaVinci: Adding Copyright for DM365 EVM
    Forgot to add Copyright while submitting the patch.
    This patch adds the copyright.
    
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/davinci_dm365evm.h
+++ include/configs/davinci_dm365evm.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2009 Texas Instruments Incorporated
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
