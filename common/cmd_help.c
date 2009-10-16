4e1ca93b6bae34b68be9280b43bf0289d994656c
Peter Tyser <ptyser@xes-inc.com>
2009-10-16 17:36:26 -0500

    cmd_help: General cleanup
    Shorten the overly-verbose help message of 'help' and clean up some
    redundant ifdefery while we're at it.
    
    Signed-off-by: Peter Tyser <ptyser@xes-inc.com>

--- common/cmd_help.c
+++ common/cmd_help.c
@@ -33,25 +33,18 @@ int do_help(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[])
 
 U_BOOT_CMD(
 	help,	CONFIG_SYS_MAXARGS,	1,	do_help,
-	"print online help",
-	"[command ...]\n"
-	"    - show help information (for 'command')\n"
-	"'help' prints online help for the monitor commands.\n\n"
-	"Without arguments, it prints a short usage message for all commands.\n\n"
-	"To get detailed help information for specific commands you can type\n"
-	"'help' with one or more command names as arguments."
+	"print command description/usage",
+	"\n"
+	"	- print brief description of all commands\n"
+	"help command ...\n"
+	"	- print detailed usage of 'command'"
 );
 
 /* This does not use the U_BOOT_CMD macro as ? can't be used in symbol names */
-#ifdef  CONFIG_SYS_LONGHELP
 cmd_tbl_t __u_boot_cmd_question_mark Struct_Section = {
 	"?",	CONFIG_SYS_MAXARGS,	1,	do_help,
 	"alias for 'help'",
+#ifdef  CONFIG_SYS_LONGHELP
 	""
-};
-#else
-cmd_tbl_t __u_boot_cmd_question_mark Struct_Section = {
-	"?",	CONFIG_SYS_MAXARGS,	1,	do_help,
-	"alias for 'help'"
-};
 #endif /* CONFIG_SYS_LONGHELP */
+};
