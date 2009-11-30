a52ad4f99486ce3f404f83f75263e321956bb6d5
Mike Frysinger <vapier@gentoo.org>
2009-11-30 13:51:24 -0500

    Blackfin: pull io funcs from linux
    Some common code uses more of the io.h funcs than we currently provide, so
    pull in all of the ones from the linux kernel.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- include/asm-blackfin/config-pre.h
+++ include/asm-blackfin/config-pre.h
@@ -71,4 +71,7 @@ static inline const char *get_bfin_boot_mode(int bfin_boot)
 # define BFIN_BOOT_SPI_SSEL 1
 #endif
 
+/* We rarely use interrupts, so favor throughput over latency */
+#define CONFIG_BFIN_INS_LOWOVERHEAD
+
 #endif
