8b432bebc205a959684559477472c048e31d137d
Grazvydas Ignotas <notasas@gmail.com>
2009-12-11 15:07:30 +0200

    pandora: don't enable VAUX3, VDAC and VPLL2 regulators
    These regulators are not needed to start the kernel and only
    cause "incomplete constraints" warnings from kernel, so don't
    turn them on to save power.
    
    Signed-off-by: Grazvydas Ignotas <notasas@gmail.com>

--- board/pandora/pandora.c
+++ board/pandora/pandora.c
@@ -65,7 +65,6 @@ int misc_init_r(void)
 	struct gpio *gpio5_base = (struct gpio *)OMAP34XX_GPIO5_BASE;
 	struct gpio *gpio6_base = (struct gpio *)OMAP34XX_GPIO6_BASE;
 
-	twl4030_power_init();
 	twl4030_led_init(TWL4030_LED_LEDEN_LEDBON);
 
 	/* Configure GPIOs to output */
