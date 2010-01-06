b416191a14770c6bcc6fd67be7decf8159b2baee
Chris Zhang <chris@seamicro.com>
2010-01-06 13:34:04 -0800

    Fix EHCI port reset.
    In USB ehci driver, the port reset is not terminated. EHCI spec says "A host
     controller must terminate the reset and stabilize the state of the port within
     2 milliseconds". Without termination, a port stays at reset state. This is
     observed on ppc4xx(sequoia) boards.
    
    Signed-off-by: Chris Zhang <chris@seamicro.com>

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -708,6 +708,9 @@ ehci_submit_root(struct usb_device *dev, unsigned long pipe, void *buffer,
 				 * root
 				 */
 				wait_ms(50);
+				/* terminate the reset */
+				ehci_writel(status_reg, reg & ~EHCI_PS_PR);
+				wait_ms(2);
 				portreset |= 1 << le16_to_cpu(req->index);
 			}
 			break;
