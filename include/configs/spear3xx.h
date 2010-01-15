7da692360414d07027c6cf564a15d79cd9dcf488
Vipin KUMAR <vipin.kumar@st.com>
2010-01-15 19:15:53 +0530

    SPEAr : Support added for SPEAr320 board
    SPEAr320 SoC support contains basic spear320 support along with the
    usage of following drivers
    - serial driver(UART)
    - i2c driver
    - smi driver
    - nand driver(FSMC)
    - usbd driver
    - emi driver(cfi support)
    
    Signed-off-by: Vipin <vipin.kumar@st.com>

--- include/configs/spear3xx.h
+++ include/configs/spear3xx.h
@@ -34,6 +34,9 @@
 #elif defined(CONFIG_MK_spear310)
 #define CONFIG_SPEAR3XX				1
 #define CONFIG_SPEAR310				1
+#elif defined(CONFIG_MK_spear320)
+#define CONFIG_SPEAR3XX				1
+#define CONFIG_SPEAR320				1
 #endif
 
 #include <configs/spear-common.h>
@@ -62,6 +65,18 @@
 						(void *)CONFIG_SYS_SERIAL3, \
 						(void *)CONFIG_SYS_SERIAL4, \
 						(void *)CONFIG_SYS_SERIAL5 }
+#elif defined(CONFIG_SPEAR320)
+
+#if (CONFIG_CONS_INDEX)
+#undef  CONFIG_PL011_CLOCK
+#define CONFIG_PL011_CLOCK			(83 * 1000 * 1000)
+#endif
+
+#define CONFIG_SYS_SERIAL1			0xA3000000
+#define CONFIG_SYS_SERIAL2			0xA4000000
+#define CONFIG_PL01x_PORTS			{(void *)CONFIG_SYS_SERIAL0, \
+						(void *)CONFIG_SYS_SERIAL1, \
+						(void *)CONFIG_SYS_SERIAL2 }
 #endif
 
 #if defined(CONFIG_SPEAR_EMI)
@@ -84,6 +99,17 @@
 						CONFIG_SYS_CS5_FLASH_BASE }
 #define CONFIG_SYS_MAX_FLASH_BANKS		6
 
+#elif defined(CONFIG_SPEAR320)
+#define CONFIG_SYS_FLASH_BASE			0x44000000
+#define CONFIG_SYS_CS1_FLASH_BASE		0x45000000
+#define CONFIG_SYS_CS2_FLASH_BASE		0x46000000
+#define CONFIG_SYS_CS3_FLASH_BASE		0x47000000
+#define CONFIG_SYS_FLASH_BANKS_LIST		{ CONFIG_SYS_FLASH_BASE,   \
+						CONFIG_SYS_CS1_FLASH_BASE, \
+						CONFIG_SYS_CS2_FLASH_BASE, \
+						CONFIG_SYS_CS3_FLASH_BASE }
+#define CONFIG_SYS_MAX_FLASH_BANKS		4
+
 #endif
 
 #define CONFIG_SYS_MAX_FLASH_SECT		(127 + 8)
@@ -96,6 +122,9 @@
 
 #elif defined(CONFIG_SPEAR310)
 #define CONFIG_SYS_NAND_BASE			(0x40000000)
+
+#elif defined(CONFIG_SPEAR320)
+#define CONFIG_SYS_NAND_BASE			(0x50000000)
 
 #endif
 
