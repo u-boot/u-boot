07c966066b977b24f4b08b1684eed130f463b4c6
Matthias Kaehlcke <matthias@kaehlcke.net>
2010-01-21 22:16:34 +0100

    cs8900_initialize() cleanup
    cs8900_initialize(): remove unecessary calls to free() and fix memory leak
    
    Signed-off-by: Matthias Kaehlcke <matthias@kaehlcke.net>
    Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

--- drivers/net/cs8900.c
+++ drivers/net/cs8900.c
@@ -308,14 +308,13 @@ int cs8900_initialize(u8 dev_num, int base_addr)
 
 	dev = malloc(sizeof(*dev));
 	if (!dev) {
-		free(dev);
 		return 0;
 	}
 	memset(dev, 0, sizeof(*dev));
 
 	priv = malloc(sizeof(*priv));
 	if (!priv) {
-		free(priv);
+		free(dev);
 		return 0;
 	}
 	memset(priv, 0, sizeof(*priv));
