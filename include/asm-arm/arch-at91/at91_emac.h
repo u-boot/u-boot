6799a80f7644d78988187390dfd37352f20ad710
Jens Scharsig <js_at_ng@scharsoft.de>
2010-02-03 22:47:57 +0100

    new at91_emac network driver (NET_MULTI api)
    * add's at91_emac (AT91RM9200) network driver (NET_MULTI api)
     * enable driver with CONFIG_DRIVER_AT91EMAC
     * generic PHY initialization
     * modify AT91RM9200 boards to use NET_MULTI driver
     * the drivers has been tested with LXT971 Phy and DM9161 Phy at MII and RMII
       interface
    
    Signed-off-by: Jens Scharsig <js_at_ng@scharsoft.de>

--- include/asm-arm/arch-at91/at91_emac.h
+++ include/asm-arm/arch-at91/at91_emac.h
@@ -1,6 +1,4 @@
 /*
- * Memory Setup stuff - taken from blob memsetup.S
- *
  * Copyright (C) 2009 Jens Scharsig (js_at_ng@scharsoft.de)
  *
  * based on AT91RM9200 datasheet revision I (36. Ethernet MAC (EMAC))
@@ -15,7 +13,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
