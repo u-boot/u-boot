6c71a8fec95a9e0f90fbc47469c389c6f35d96bc
Naveen Krishna CH <ch.naveen.samsung.com>
2010-02-04 14:17:38 +0900

    S5PC100: Moves the Macros to a common header file
    The get_pll_clk(int) API returns the PLL frequency based on
    the (int) argument which is defined locally in clock.c
    
    Moving that #define to common header file (clk.h) would
    be helpful when using the API from other files.
    
    Signed-off-by: Naveen Krishna Ch <ch.naveen@samsung.com>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- include/asm-arm/arch-s5pc1xx/clk.h
+++ include/asm-arm/arch-s5pc1xx/clk.h
@@ -23,6 +23,12 @@
 #ifndef __ASM_ARM_ARCH_CLK_H_
 #define __ASM_ARM_ARCH_CLK_H_
 
+#define APLL	0
+#define MPLL	1
+#define EPLL	2
+#define HPLL	3
+#define VPLL	4
+
 void s5pc1xx_clock_init(void);
 
 extern unsigned long (*get_pll_clk)(int pllreg);
