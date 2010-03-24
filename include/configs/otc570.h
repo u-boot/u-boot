fb508b8b39a52a4063d098f5a9b4355bc5b30c14
Matthias Fuchs <matthias.fuchs@esd.eu>
2010-03-24 10:16:20 +0100

    at91: Get rid of some warnings when building for otc570
    The soft-i2c code for AT91 defines I2C_SOFT_DECLARATIONS
    for direct access by dereferencing a pio pointer.
    The OTC570 platform uses the AT91 gpio API so it does not
    need the pio variable.
    
    Signed-off-by: Matthias Fuchs <matthias.fuchs@esd.eu>

--- include/configs/otc570.h
+++ include/configs/otc570.h
@@ -102,6 +102,7 @@
 	at91_set_pio_output(AT91_PIO_PORTB, 4, 0); \
 	at91_set_pio_output(AT91_PIO_PORTB, 5, 0); \
 }
+#define I2C_SOFT_DECLARATIONS
 /* Configure data pin as output */
 #define I2C_ACTIVE		at91_set_pio_output(AT91_PIO_PORTB, 4, 0)
 /* Configure data pin as input */
