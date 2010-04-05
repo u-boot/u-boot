55429a0323c8b39618b0005a7b8e496c5f8da997
Marek Vasut <marek.vasut@gmail.com>
2010-04-05 02:28:14 +0200

    PXA: PXAMMC: Drop different delays for PXA27X
    In case the delays were set to 10000, the MMC card on PXA27X boards (and PXA3xx
    boards) didn't initialize on first try. Increasing the delays and leaving just
    those for PXA25x and 26x (that is 200000) fixes this problem.
    
    Signed-off-by: Marek Vasut <marek.vasut@gmail.com>

--- drivers/mmc/pxa_mmc.c
+++ drivers/mmc/pxa_mmc.c
@@ -584,11 +584,7 @@ mmc_legacy_init(int verbose)
 			debug("Detected SD card\n");
 			break;
 		}
-#ifdef CONFIG_PXA27X
-		udelay(10000);
-#else
 		udelay(200000);
-#endif
 	}
 
 	if (retries <= 0 || !(IF_TYPE_SD == mmc_dev.if_type)) {
@@ -598,11 +594,7 @@ mmc_legacy_init(int verbose)
 
 		retries = 10;
 		while (retries-- && resp && !(resp[0] & 0x80000000)) {
-#ifdef CONFIG_PXA27X
-			udelay(10000);
-#else
 			udelay(200000);
-#endif
 			resp =
 			    mmc_cmd(MMC_CMD_SEND_OP_COND, 0x00ff, 0x8000,
 				    MMC_CMDAT_R3);
