c960b13ed22d9ea570957379f9f7f2f37d87ef08
Thomas Chou <thomas@wytron.com.tw>
2010-04-20 12:49:52 +0800

    net: add altera triple speeds ethernet mac driver
    This driver supports the Altera triple speeds 10/100/1000 ethernet
    mac.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>
    Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

--- include/netdev.h
+++ include/netdev.h
@@ -41,6 +41,8 @@ int board_eth_init(bd_t *bis);
 int cpu_eth_init(bd_t *bis);
 
 /* Driver initialization prototypes */
+int altera_tse_initialize(u8 dev_num, int mac_base,
+			  int sgdma_rx_base, int sgdma_tx_base);
 int au1x00_enet_initialize(bd_t*);
 int at91emac_register(bd_t *bis, unsigned long iobase);
 int bfin_EMAC_initialize(bd_t *bis);
