bd75db3feb9a8e4123b76006dbe582b71adbf22f
Valentin Yakovenkov <yakovenkov@niistt.ru>
2010-04-23 09:40:23 +0400

    smc911x driver frame alignment patch
    SMSC911x chips have alignment function to allow frame payload data
    (which comes after 14-bytes ethernet header) to be aligned at some
    boundary when reading it from fifo (usually - 4 bytes boundary).
    This is done by inserting fake zeros bytes BEFORE actual frame data when
    reading from SMSC's fifo.
    This function controlled by RX_CFG register. There are bits that
    represents amount of fake bytes to be inserted.
    
    Linux uses alignment of 4 bytes. Ethernet frame header is 14 bytes long,
    so we need to add 2 fake bytes to get payload data aligned at 4-bytes
    boundary.
    Linux driver does this by adding IP_ALIGNMENT constant (defined at
    skb.h) when calculating fifo data length. All network subsystem of Linux
    uses this constant too when calculating different offsets.
    
    But u-boot does not use any packet data alignment, so we don't need to
    add anything when calculating fifo data length.
    Moreover, driver zeros the RX_CFG register just one line up, so chip
    does not insert any fake data at the beginig. So calculated data length
    is always bigger by 1 word.
    
    It seems that at almost every packet read we get an underflow condition
    at fifo and possible corruption of data. Especially at continuous
    transfers, such as tftp.
    
    Just after removing this magic addition, I've got tftp transfer speed as
    it aught to be at 100Mbps. It was really slow before.
    
    It seems that fifo underflow occurs only when using byte packing on
    32-bit blackfin bus (may be because of very small delay between reads).
    
    Signed-off-by: Valentin Yakovenkov <yakovenkov@niistt.ru>
    Signed-off-by: Ben Warren <biggerbadderben@gmail.com>

--- drivers/net/smc911x.c
+++ drivers/net/smc911x.c
@@ -220,7 +220,7 @@ static int smc911x_rx(struct eth_device *dev)
 
 		smc911x_reg_write(dev, RX_CFG, 0);
 
-		tmplen = (pktlen + 2+ 3) / 4;
+		tmplen = (pktlen + 3) / 4;
 		while (tmplen--)
 			*data++ = pkt_data_pull(dev, RX_DATA_FIFO);
 
