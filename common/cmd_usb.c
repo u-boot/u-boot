65d342541e78ab9a22bf480cc4fe2f659f94bad4
Serge Ziryukin <ftrvxmtrx@gmail.com>
2010-04-25 21:32:36 +0300

    cmd_usb.c: show cmd usage if no args given
    Signed-off-by: Serge Ziryukin <ftrvxmtrx@gmail.com>

--- common/cmd_usb.c
+++ common/cmd_usb.c
@@ -516,6 +516,11 @@ int do_usb(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 	block_dev_desc_t *stor_dev;
 #endif
 
+	if (argc < 2) {
+		cmd_usage(cmdtp);
+		return 1;
+	}
+
 	if ((strncmp(argv[1], "reset", 5) == 0) ||
 		 (strncmp(argv[1], "start", 5) == 0)) {
 		usb_stop();
