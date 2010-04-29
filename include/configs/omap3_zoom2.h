8b6724aa5a2b53f59be513aa88a0300a4845aabb
Grazvydas Ignotas <notasas@gmail.com>
2010-04-29 17:19:13 +0300

    OMAP3: remove unused config macros
    Most OMAP3 boards have various flash related macros in their configs
    that are either not referenced anywhere in the code or are used by
    drivers that are not enabled. Remove them.
    
    Signed-off-by: Grazvydas Ignotas <notasas@gmail.com>
    Acked-by: Steve Sakoman <steve@sakoman.com>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/omap3_zoom2.h
+++ include/configs/omap3_zoom2.h
@@ -246,9 +246,6 @@
 #define PISMO1_NAND_SIZE		GPMC_SIZE_128M
 #define PISMO1_ONEN_SIZE		GPMC_SIZE_128M
 
-#define CONFIG_SYS_MAX_FLASH_SECT	520	/* max number of sectors on */
-						/* one chip */
-#define CONFIG_SYS_MAX_FLASH_BANKS	2	/* max number of flash banks */
 #define CONFIG_SYS_MONITOR_LEN		(256 << 10)	/* Reserve 2 sectors */
 
 #define CONFIG_SYS_FLASH_BASE		boot_flash_base
@@ -262,13 +259,6 @@
 #define CONFIG_SYS_ENV_SECT_SIZE	boot_flash_sec
 #define CONFIG_ENV_OFFSET		boot_flash_off
 #define CONFIG_ENV_ADDR			SMNAND_ENV_OFFSET
-
-/*-----------------------------------------------------------------------
- * CFI FLASH driver setup
- */
-/* timeout values are in ticks */
-#define CONFIG_SYS_FLASH_ERASE_TOUT	(100 * CONFIG_SYS_HZ)
-#define CONFIG_SYS_FLASH_WRITE_TOUT	(100 * CONFIG_SYS_HZ)
 
 #ifndef __ASSEMBLY__
 extern unsigned int boot_flash_base;
