551265f0df9e7a1c6098ff4eec435218423f365b
Thomas Chou <thomas@wytron.com.tw>
2010-04-30 11:34:18 +0800

    nios2: add spi flash support to nios2-generic board
    This patch enables the altera_spi and spi_flash drivers for the
    nios2-generic board. It allows access to the EPCS/SPI flash on
    the Altera EP1C20 board.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>
    Tested-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Scott McNutt <smcnutt@psyent.com>

--- board/altera/nios2-generic/custom_fpga.h
+++ board/altera/nios2-generic/custom_fpga.h
@@ -35,6 +35,16 @@
 #define CONFIG_SMC91111
 #define CONFIG_SMC_USE_32_BIT
 
+/* epcs_controller.epcs_control_port is a altera_avalon_epcs_flash_controller */
+#define EPCS_CONTROLLER_REG_BASE 0x82100200
+#define CONFIG_SYS_ALTERA_SPI_LIST { EPCS_CONTROLLER_REG_BASE }
+#define CONFIG_ALTERA_SPI
+#define CONFIG_CMD_SPI
+#define CONFIG_CMD_SF
+#define CONFIG_SF_DEFAULT_SPEED 30000000
+#define CONFIG_SPI_FLASH
+#define CONFIG_SPI_FLASH_STMICRO
+
 /* jtag_uart.avalon_jtag_slave is a altera_avalon_jtag_uart */
 #define CONFIG_SYS_JTAG_UART_BASE 0x821208b0
 
