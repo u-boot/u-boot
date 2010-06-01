f193e3da9817dc4892bc25967954d98838f84386
Kumar Gala <galak@kernel.crashing.org>
2010-06-01 10:29:11 -0500

    powerpc/p2040: Add various p2040 specific information
    Add P2040 SoC specific information:
    * SERDES Table
    * Added p2040 to cpu_type_list and SVR list
    * Added number of LAWs for p2040
    * Set CONFIG_MAX_CPUS to 4 for p2040
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/include/asm/config.h
+++ arch/powerpc/include/asm/config.h
@@ -47,6 +47,8 @@
 	defined(CONFIG_P1021) || defined(CONFIG_P1022) || \
 	defined(CONFIG_P2020) || defined(CONFIG_MPC8641)
 #define CONFIG_MAX_CPUS		2
+#elif defined(CONFIG_PPC_P2040)
+#define CONFIG_MAX_CPUS		4
 #elif defined(CONFIG_PPC_P3041)
 #define CONFIG_MAX_CPUS		4
 #elif defined(CONFIG_PPC_P4080)
