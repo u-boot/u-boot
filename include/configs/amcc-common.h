0ad7f0950a9bc0a69b3cd5f34ccf7da25fcf1c6d
Wolfgang Denk <wd@denx.de>
2010-07-23 21:16:26 +0200

    ppc4xx: cleanup default environment for AMCC boards
    None of the AMCC boards uses an embedded environment, so there is no
    need to run "saveenv" after updating U-Boot.  Drop the redundant
    commands from the default environment.
    
    Signed-off-by: Wolfgang Denk <wd@denx.de>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/amcc-common.h
+++ include/configs/amcc-common.h
@@ -278,15 +278,13 @@
 	"load=tftp 200000 ${u-boot}\0"					\
 	"update=protect off " xstr(CONFIG_SYS_MONITOR_BASE) " FFFFFFFF;"	\
 		"era " xstr(CONFIG_SYS_MONITOR_BASE) " FFFFFFFF;"		\
-		"cp.b ${fileaddr} " xstr(CONFIG_SYS_MONITOR_BASE) " ${filesize};" \
-		"setenv filesize;saveenv\0"				\
+		"cp.b ${fileaddr} " xstr(CONFIG_SYS_MONITOR_BASE) " ${filesize}" \
 	"upd=run load update\0"						\
 
 #define CONFIG_AMCC_DEF_ENV_NAND_UPD					\
 	"u-boot-nand=" xstr(CONFIG_HOSTNAME) "/u-boot-nand.bin\0"	\
 	"nload=tftp 200000 ${u-boot-nand}\0"				\
-	"nupdate=nand erase 0 100000;nand write 200000 0 100000;"	\
-		"setenv filesize;saveenv\0"				\
+	"nupdate=nand erase 0 100000;nand write 200000 0 100000"	\
 	"nupd=run nload nupdate\0"
 
 #endif /* __AMCC_COMMON_H */
