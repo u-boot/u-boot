c0ef51318813c062052f91ef7c15b75bbbf15767
Vitaly Kuzmichev <vkuzmichev@mvista.com>
2010-08-13 17:02:41 +0400

    USB-CDC: change simple_strtol to simple_strtoul
    The patch is based on commit bb9496c6f7e853e5d4edd5397c9d45f1968d623c
    (done by Julia Lawall <julia@diku.dk>) from linux-2.6.git.
    
    Since num is unsigned, it would seem better to use simple_strtoul that
    simple_strtol.
    
    Signed-off-by: Vitaly Kuzmichev <vkuzmichev@mvista.com>

--- drivers/usb/gadget/epautoconf.c
+++ drivers/usb/gadget/epautoconf.c
@@ -156,7 +156,7 @@ ep_matches (
 
 	/* report address */
 	if (isdigit (ep->name [2])) {
-		u8	num = simple_strtol (&ep->name [2], NULL, 10);
+		u8	num = simple_strtoul (&ep->name [2], NULL, 10);
 		desc->bEndpointAddress |= num;
 #ifdef	MANY_ENDPOINTS
 	} else if (desc->bEndpointAddress & USB_DIR_IN) {
