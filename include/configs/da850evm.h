97003756249bd790910417eb66f0039bbf06a02c
Ben Gardiner <bengardiner@nanometrics.ca>
2010-08-23 09:08:15 -0400

    da8xx: fixup ARM relocation support
    Split the existing dram_init for da8xx when ARM reloc is enabled, like the
    changes to arch/arm/cpu/arm926ejs/orion5x/dram.c in
    0f234d263b17ccf1b8fd776eb8c15b7cdb27a887 by Heiko Schocher <hs@denx.de>.
    
    Without these changes gd->ram_size is '0' which leads to incorrect relocation
    when CONFIG_SYS_ARM_WITHOUT_RELOC is defined and the board does not boot.
    
    We use get_ram_size to dynamically calculate the available RAM because it runs
    on different board version with different ram, as suggested by Heiko in private
    communication.
    
    Tested on a da850evm with 128M of DDR2 installed; with both
    CONFIG_SYS_ARM_WITHOUT_RELOC defined and undefined.
    
    Signed-off-by: Ben Gardiner <bengardiner@nanometrics.ca>
    Reviewed-by: Sudhakar Rajashekhara <sudhakar.raj@ti.com>
    CC: Sudhakar Rajashekhara <sudhakar.raj@ti.com>
    CC: Heiko Schocher <hs@denx.de>

--- include/configs/da850evm.h
+++ include/configs/da850evm.h
@@ -47,6 +47,7 @@
 #define CONFIG_SYS_GBL_DATA_SIZE	128 /* reserved for initial data */
 #define PHYS_SDRAM_1		DAVINCI_DDR_EMIF_DATA_BASE /* DDR Start */
 #define PHYS_SDRAM_1_SIZE	(64 << 20) /* SDRAM size 64MB */
+#define CONFIG_MAX_RAM_BANK_SIZE (512 << 20) /* max size from SPRS586*/
 
 /* memtest start addr */
 #define CONFIG_SYS_MEMTEST_START	(PHYS_SDRAM_1 + 0x2000000)
