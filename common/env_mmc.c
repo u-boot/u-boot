a2f69d35f977179a0c98812c47718cee5b36fc11
Steve Sakoman <steve@sakoman.com>
2010-10-05 15:31:38 -0700

    env_mmc: Fix broken build due to set_default_env() change
    Previously the function was set_default_env(void), it is now
    set_default_env(const char *s). This patch adds the required
    parameter. This fixes a broken build on OMAP4430 SDP.
    
    Signed-off-by: Steve Sakoman <steve.sakoman@linaro.org>

--- common/env_mmc.c
+++ common/env_mmc.c
@@ -147,7 +147,6 @@ void env_relocate_spec(void)
 #if !defined(ENV_IS_EMBEDDED)
 static void use_default()
 {
-	puts ("*** Warning - bad CRC or MMC, using default environment\n\n");
-	set_default_env();
+	set_default_env(NULL);
 }
 #endif
