e9319f111d366294b9a1e8b1f66875242a5de3dd
John Rigby <john.rigby@linaro.org>
2010-10-13 14:31:08 -0600

    drivers/net/fec_mxc.c: write mac address in init
    Call fec_set_hwaddr in init routine to setup MAC address so when ethaddr is set
    late via setenv the change will propagate to the hw.
    
    Signed-off-by: John Rigby <john.rigby@linaro.org>
    CC: Ben Warren <biggerbadderben@gmail.com>

--- drivers/net/fec_mxc.c
+++ drivers/net/fec_mxc.c
@@ -414,6 +414,9 @@ static int fec_init(struct eth_device *dev, bd_t* bd)
 	uint32_t base;
 	struct fec_priv *fec = (struct fec_priv *)dev->priv;
 
+	/* Initialize MAC address */
+	fec_set_hwaddr(dev);
+
 	/*
 	 * reserve memory for both buffer descriptor chains at once
 	 * Datasheet forces the startaddress of each chain is 16 byte
