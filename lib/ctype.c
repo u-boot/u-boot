543f0a3819a9af130f3f80a660099fad8d2d4b8e
Mike Frysinger <vapier@gentoo.org>
2010-10-20 07:17:45 -0400

    ctype: constify lookup table
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- lib/ctype.c
+++ lib/ctype.c
@@ -29,7 +29,7 @@
 
 #include <linux/ctype.h>
 
-unsigned char _ctype[] = {
+const unsigned char _ctype[] = {
 _C,_C,_C,_C,_C,_C,_C,_C,			/* 0-7 */
 _C,_C|_S,_C|_S,_C|_S,_C|_S,_C|_S,_C,_C,		/* 8-15 */
 _C,_C,_C,_C,_C,_C,_C,_C,			/* 16-23 */
