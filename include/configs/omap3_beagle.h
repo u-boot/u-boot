31bfcf1c5776df3d90286aa15104c45096d53dc6
Steve Sakoman <steve@sakoman.com>
2010-10-27 05:04:30 -0700

    ARMV7: OMAP: Fix build after introduction of GENERATED_GBL_DATA_SIZE
    This patch fixes the issue by defining and using CONFIG_SYS_INIT_RAM_SIZE and
    CONFIG_SYS_INIT_RAM_ADDR. Based on an email discussion with Wolfgang Denk and
    Heiko Schocher.
    
    Signed-off-by: Steve Sakoman <steve.sakoman@linaro.org>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/omap3_beagle.h
+++ include/configs/omap3_beagle.h
@@ -346,8 +346,11 @@ extern unsigned int boot_flash_sec;
 extern unsigned int boot_flash_type;
 #endif
 
-/* additions for new relocation code, must be added to all boards */
 #define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM_1
-#define CONFIG_SYS_INIT_SP_ADDR		(LOW_LEVEL_SRAM_STACK - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_RAM_ADDR	0x4020f800
+#define CONFIG_SYS_INIT_RAM_SIZE	0x800
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_INIT_RAM_ADDR + \
+					 CONFIG_SYS_INIT_RAM_SIZE - \
+					 GENERATED_GBL_DATA_SIZE)
 
 #endif /* __CONFIG_H */
