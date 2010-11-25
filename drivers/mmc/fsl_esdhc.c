63786d299defb7248932d551b38575d36c1f6a84
Jerry Huang <Changm-Ming.Huang@freescale.com>
2010-11-25 17:06:10 +0000

    fsl_esdhc: Fix max clock frequency
    The max clock of MMC is 52MHz
    
    Signed-off-by: Jerry Huang <Changm-Ming.Huang@freescale.com>
    Tested-by: Stefano Babic <sbabic@denx.de>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -477,7 +477,7 @@ int fsl_esdhc_initialize(bd_t *bis, struct fsl_esdhc_cfg *cfg)
 		mmc->host_caps |= MMC_MODE_HS_52MHz | MMC_MODE_HS;
 
 	mmc->f_min = 400000;
-	mmc->f_max = MIN(gd->sdhc_clk, 50000000);
+	mmc->f_max = MIN(gd->sdhc_clk, 52000000);
 
 	mmc_register(mmc);
 
