52f0aa835fd6caf5f094b551e435f5c9506d61df
Mike Frysinger <vapier@gentoo.org>
2010-12-20 18:57:09 -0500

    config_cmd_defaults.h: new header for common u-boot command defaults
    We have config_defaults.h which are random configuration settings that
    everyone gets by default.  We also have config_cmd_default.h which is a
    recommended list of defaults but boards have to opt into.  Now we have
    config_cmd_defaults.h which is a list of defaults that everyone gets
    and has to actively opt out of.
    
    For now, we populate it with the bootm command which previously was
    unable to be disabled.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

---
+++ include/config_cmd_defaults.h
@@ -0,0 +1,14 @@
+/*
+ * config_cmd_defaults.h - sane defaults for everyone
+ *
+ * Copyright (c) 2010 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2 or later.
+ */
+
+#ifndef _CONFIG_CMD_DEFAULTS_H_
+#define _CONFIG_CMD_DEFAULTS_H_
+
+#define CONFIG_CMD_BOOTM 1
+
+#endif
