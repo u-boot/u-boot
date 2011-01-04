56551362086adaee7afb08ed618ac8e46b5f53ae
Kumar Gala <galak@kernel.crashing.org>
2011-01-04 17:07:54 -0600

    powerpc/86xx: Enable common SRIO init code
    Add the needed defines and code to utilize the common 8xxx srio init
    code to setup LAWs and modify device tree if we have SRIO enabled on a
    board.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/cpu/mpc86xx/cpu_init.c
+++ arch/powerpc/cpu/mpc86xx/cpu_init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004,2009-2010 Freescale Semiconductor, Inc.
+ * Copyright 2004,2009-2011 Freescale Semiconductor, Inc.
  * Jeff Brown
  * Srikanth Srinivasan (srikanth.srinivasan@freescale.com)
  *
@@ -34,6 +34,7 @@
 #include <asm/fsl_serdes.h>
 #include <asm/mp.h>
 
+extern void srio_init(void);
 void setup_bats(void);
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -79,6 +80,10 @@ int cpu_init_r(void)
 {
 	/* needs to be in ram since code uses global static vars */
 	fsl_serdes_init();
+
+#ifdef CONFIG_SYS_SRIO
+	srio_init();
+#endif
 
 #if defined(CONFIG_MP)
 	setup_mp();
