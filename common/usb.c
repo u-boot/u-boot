cb44091fdfa1170f9c0a0a1d3396d6472fbf8911
Alexander Holler <holler@ahsoftware.de>
2011-01-27 22:50:07 +0100

    USB: Fix device stati for removable and powerctrl (typo)
    I currently don't know if the error could have other consequences
    than a wrong output when turning debug on.
    
    Signed-off-by: Alexander Holler <holler@ahsoftware.de>

--- common/usb.c
+++ common/usb.c
@@ -1220,7 +1220,7 @@ int usb_hub_configure(struct usb_device *dev)
 		hub->desc.DeviceRemovable[i] = descriptor->DeviceRemovable[i];
 
 	for (i = 0; i < ((hub->desc.bNbrPorts + 1 + 7)/8); i++)
-		hub->desc.DeviceRemovable[i] = descriptor->PortPowerCtrlMask[i];
+		hub->desc.PortPowerCtrlMask[i] = descriptor->PortPowerCtrlMask[i];
 
 	dev->maxchild = descriptor->bNbrPorts;
 	USB_HUB_PRINTF("%d ports detected\n", dev->maxchild);
