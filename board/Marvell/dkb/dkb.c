ea4bc120db7da3f80bdaca0c08b5fd404cfa0d3e
Lei Wen <[leiwen@marvell.com]>
2011-02-09 18:17:47 +0530

    Pantheon: Add Board Support for Marvell dkb board
    DKB is a Development Board for PANTHEON TD/TTC(pxa920/pxa910) with
    * Processor upto 806Mhz
    * LPDDR1/2
    * x8/x16 SLC/MLC NAND
    * Footprints for eMMC & MMC x8 card
    
    With Peripherals:
    * Parallel LCD I/F
    * Audio codecs (88PM8607)
    * MIPI CSI-2 camera
    * Marvell 88W8787 802.11n/BT module
    * Marvell 2G/3G RF
    * Dual analog mics & speakers, headset jack, LED, ambient
    * USB2.0 HS host, OTG (mini AB)
    * GPIO, GPIO expander with DIP switches for easier selection
    * UART serial over USB, CIR
    
    This patch adds basic board support with DRAM and UART functionality
    
    Signed-off-by: Lei Wen <leiwen@marvell.com>
    Signed-off-by: Prafulla Wadaskar <prafulla@marvell.com>

---
+++ board/Marvell/dkb/dkb.c
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright 2011
+ * Marvell Semiconductor <www.marvell.com>
+ * Written-by: Lei Wen <leiwen@marvell.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <common.h>
+#include <mvmfp.h>
+#include <asm/arch/mfp.h>
+#include <asm/arch/cpu.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_early_init_f(void)
+{
+	u32 mfp_cfg[] = {
+		/* Enable Console on UART2 */
+		MFP47_UART2_RXD,
+		MFP48_UART2_TXD,
+
+		MFP_EOC		/*End of configureation*/
+	};
+	/* configure MFP's */
+	mfp_config(mfp_cfg);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* arch number of Board */
+	gd->bd->bi_arch_number = MACH_TYPE_TTC_DKB;
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = panth_sdram_base(0) + 0x100;
+	return 0;
+}
