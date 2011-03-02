5103d7aa23b33b9c4c5b6221248109eb6672c7b1
Zhao Chenhui <b35336@freescale.com>
2011-03-02 16:44:52 +0800

    powerpc/85xx: Disable ECC in considering performance on MPC8572DS
    Signed-off-by: Zhao Chenhui <b35336@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- include/configs/MPC8572DS.h
+++ include/configs/MPC8572DS.h
@@ -741,7 +741,7 @@
 #define CONFIG_BAUDRATE	115200
 
 #define	CONFIG_EXTRA_ENV_SETTINGS				\
- "hwconfig=fsl_ddr:ctlr_intlv=bank\0"				\
+ "hwconfig=fsl_ddr:ctlr_intlv=bank,ecc=off\0"			\
  "netdev=eth0\0"						\
  "uboot=" MK_STR(CONFIG_UBOOTPATH) "\0"				\
  "tftpflash=tftpboot $loadaddr $uboot; "			\
