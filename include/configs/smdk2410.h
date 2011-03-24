4479fc5b2077a7ac4cb9cfc552ebdc3760e40675
David Müller (ELSOFT AG) <d.mueller@elsoft.ch>
2011-03-24 22:28:04 +0000

    SMDK2410: remove unneeded config.mk
    Signed-off-by: David Müller <d.mueller@elsoft.ch>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- include/configs/smdk2410.h
+++ include/configs/smdk2410.h
@@ -38,6 +38,8 @@
 #define CONFIG_S3C2410	1	/* specifically a SAMSUNG S3C2410 SoC	*/
 #define CONFIG_SMDK2410	1	/* on a SAMSUNG SMDK2410 Board  */
 
+#define CONFIG_SYS_TEXT_BASE	0x0
+
 /* input clock of PLL */
 #define CONFIG_SYS_CLK_FREQ	12000000/* the SMDK2410 has 12MHz input clock */
 
