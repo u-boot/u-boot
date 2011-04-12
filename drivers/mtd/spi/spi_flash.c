493c36072160d72d5a8fe5943539da47c0a702a6
Mike Frysinger <vapier@gentoo.org>
2011-04-12 02:09:28 -0400

    sf: use print_size() for sector_size output
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- drivers/mtd/spi/spi_flash.c
+++ drivers/mtd/spi/spi_flash.c
@@ -297,8 +297,8 @@ struct spi_flash *spi_flash_probe(unsigned int bus, unsigned int cs,
 		goto err_manufacturer_probe;
 	}
 
-	printf("SF: Detected %s with page size %u, total ",
-	       flash->name, flash->sector_size);
+	printf("SF: Detected %s with page size ", flash->name);
+	print_size(flash->sector_size, ", total ");
 	print_size(flash->size, "\n");
 
 	spi_release_bus(spi);
