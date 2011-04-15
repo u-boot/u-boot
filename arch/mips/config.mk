660da0947abff3bc98bb0baa37a6db5050ff46d6
Daniel Schwierzeck <daniel.schwierzeck@googlemail.com>
2011-04-15 17:16:44 +0200

    MIPS: Introduce --gc-sections for MIPS
    All architectures but MIPS are using --gc-sections on final linking.
    This patch introduces that feature for MIPS to reduce the memory and
    flash footprint.
    
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@googlemail.com>
    Cc: Wolfgang Denk <wd@denx.de>
    Cc: Stefan Roese <sr@denx.de>
    Cc: Thomas Lange <thomas@corelatus.se>
    Cc: Vlad Lungu <vlad.lungu@windriver.com>
    Signed-off-by: Shinya Kuribayashi <skuribay@pobox.com>

--- arch/mips/config.mk
+++ arch/mips/config.mk
@@ -50,3 +50,5 @@ PLATFORM_CPPFLAGS += -DCONFIG_MIPS -D__MIPS__
 PLATFORM_CPPFLAGS		+= -G 0 -mabicalls -fpic
 PLATFORM_CPPFLAGS		+= -msoft-float
 PLATFORM_LDFLAGS		+= -G 0 -static -n -nostdlib
+PLATFORM_RELFLAGS		+= -ffunction-sections -fdata-sections
+LDFLAGS_FINAL			+= --gc-sections
