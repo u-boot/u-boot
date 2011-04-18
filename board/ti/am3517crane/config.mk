915162daaad18f5cd1446187c846fec853e9ea87
Srinath <srinath@mistralsolutions.com>
2011-04-18 17:40:35 -0400

    ARMV7: AM3517/05: Add support for CraneBoard.
    It is a low cost reference design based on Sitara AM3517 SoC from Texas Instruments
    Please refer to <www.craneboard.org> for more details.
    
    Signed-off-by: Srinath <srinath@mistralsolutions.com>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

---
+++ board/ti/am3517crane/config.mk
@@ -0,0 +1,29 @@
+#
+# Author: Srinath R <srinath@mistralsolutions.com>
+#
+# Based on logicpd/am3517evm/config.mk
+#
+# Copyright (C) 2011 Mistral Solutions Pvt Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Physical Address:
+# 8000'0000 (bank0)
+# A000/0000 (bank1)
+# Linux-Kernel is expected to be at 8000'8000, entry 8000'8000
+# (mem base + reserved)
+
+# For use with external or internal boots.
+CONFIG_SYS_TEXT_BASE = 0x80008000
