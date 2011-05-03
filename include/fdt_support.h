b3606f141e56df972b5caf5e2316d652f0089ee6
Timur Tabi <timur@freescale.com>
2011-05-03 13:35:10 -0500

    fdt: add prototype for fdt_increase_size()
    Add a prototype for fdt_increase_size() so that anyone can call it.
    
    Signed-off-by: Timur Tabi <timur@freescale.com>

--- include/fdt_support.h
+++ include/fdt_support.h
@@ -79,6 +79,7 @@ void ft_pci_setup(void *blob, bd_t *bd);
 
 void set_working_fdt_addr(void *addr);
 int fdt_resize(void *blob);
+int fdt_increase_size(void *fdt, int add_len);
 
 int fdt_fixup_nor_flash_size(void *blob);
 
