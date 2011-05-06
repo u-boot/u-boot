486c1f6e7e47c144443a8317a7ce90b6d546cd4e
Holger Brunck <holger.brunck@keymile.com>
2011-05-06 00:41:55 +0000

    powerpc/km82xx: fix compile issue for mgcoge2ne
    commit 91a3c14c (ppc, mgcoge: add DIP switch detection)
    introduces an compile error due to an missing define in the
    mgcoge2ne.h. DIP switch detection is valid for both boards.
    
    Signed-off-by: Holger Brunck <holger.brunck@keymile.com>
    cc: Wolfgang Denk <wd@denx.de>
    cc: Heiko Schocher <hs@denx.de>

--- include/configs/mgcoge3ne.h
+++ include/configs/mgcoge3ne.h
@@ -90,4 +90,7 @@
 /* include further common stuff for all keymile 82xx boards */
 #include "km82xx-common.h"
 
+/* bfticu address */
+#define CONFIG_SYS_BFTICU_BASE          0x40000000
+
 #endif /* __MGCOGE3NE */
