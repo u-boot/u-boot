e4e7e42803ec2399211883ad7793406b7eafa776
Timur Tabi <timur@freescale.com>
2011-05-10 15:28:14 -0500

    powerpc/85xx: add support the ePAPR "phandle" property
    The ePAPR specification says that phandle properties should be called
    "phandle", and not "linux,phandle".  To facilitate the migration from
    "linux,phandle" to "phandle", we update fdt_qportal() to use the new
    function, fdt_create_phandle().  This function abstracts the creation of
    phandle properties.
    
    Signed-off-by: Timur Tabi <timur@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/cpu/mpc85xx/portals.c
+++ arch/powerpc/cpu/mpc85xx/portals.c
@@ -151,8 +151,10 @@ static int fdt_qportal(void *blob, int off, int id, char *name,
 			dev_handle = fdt_get_phandle(blob, dev_off);
 			if (dev_handle <= 0) {
 				dev_handle = fdt_alloc_phandle(blob);
-				fdt_setprop_cell(blob, dev_off,
-					"linux,phandle", dev_handle);
+				ret = fdt_create_phandle(blob, dev_off,
+							 dev_handle);
+				if (ret < 0)
+					return ret;
 			}
 
 			ret = fdt_setprop(blob, childoff, "dev-handle",
