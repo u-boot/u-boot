0e0996ef4fb340bd91ce6588a998cdaf10f43a42
Matthew McClintock <msm@freescale.com>
2011-05-24 05:48:26 +0000

    common/cmd_ximg.c: add ifdef protection for gzip uncompression
    Print a message if we do not have the ability to uncompress a gzip
    image. Before, u-boot would just assume the routines were available
    
    Signed-off-by: Matthew McClintock <msm@freescale.com>

--- common/cmd_ximg.c
+++ common/cmd_ximg.c
@@ -215,6 +215,7 @@ do_imgextract(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 			memmove ((char *) dest, (char *)data, len);
 #endif	/* CONFIG_HW_WATCHDOG || CONFIG_WATCHDOG */
 			break;
+#ifdef CONFIG_GZIP
 		case IH_COMP_GZIP:
 			printf ("   Uncompressing part %d ... ", part);
 			if (gunzip ((void *) dest, unc_len,
@@ -223,6 +224,7 @@ do_imgextract(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 				return 1;
 			}
 			break;
+#endif
 #if defined(CONFIG_BZIP2)
 		case IH_COMP_BZIP2:
 			{
