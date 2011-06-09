56ccd36fa1b0c59d1ded7f5e6641db9c2d700734
Aneesh V <aneesh@ti.com>
2011-06-09 08:54:51 -0400

    omap1510inn: fix build breaks
    Provide SDRAM base address and use SRAM for initial stack
    
    Signed-off-by: Aneesh V <aneesh@ti.com>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/omap1510inn.h
+++ include/configs/omap1510inn.h
@@ -161,6 +161,8 @@
 
 #define CONFIG_SYS_FLASH_BASE		PHYS_FLASH_1
 
+#define PHYS_SRAM		0x20000000
+
 /*-----------------------------------------------------------------------
  * FLASH and environment organization
  */
@@ -189,5 +191,8 @@
 #define CONFIG_ENV_SECT_SIZE	PHYS_FLASH_SECT_SIZE	/* Total Size of Environment Sector */
 #define CONFIG_ENV_SIZE		CONFIG_ENV_SECT_SIZE
 #define CONFIG_ENV_OFFSET		( CONFIG_SYS_MONITOR_BASE + CONFIG_SYS_MONITOR_LEN )	/* Environment after Monitor */
+
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM_1
+#define CONFIG_SYS_INIT_SP_ADDR 	PHYS_SRAM
 
 #endif	/* __CONFIG_H */
