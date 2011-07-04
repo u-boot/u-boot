4daea6fff34b071e010cbd1af93445634703981b
Stefan Bigler <stefan.bigler@keymile.com>
2011-07-04 22:24:01 +0000

    km/common: moved eeprom config to pbec specific part
    Moved eeprom config to specific part, to allow bigger eeprom write pages
    for km_kirkwood designs. Write page only used for env eeprom in std use
    cases. 24C128 has page size of 64bytes -> 8 time faster.
    
    Signed-off-by: Stefan Bigler <stefan.bigler@keymile.com>
    cc: Valentin Longchamp <valentin.longchamp@keymile.com>
    cc: Heiko Schocher <hs@denx.de>
    cc: Wolfgang Denk <wd@denx.de>

--- include/configs/km/km_arm.h
+++ include/configs/km/km_arm.h
@@ -195,6 +195,12 @@ int get_scl(void);
 
 #endif
 
+/* EEprom support 24C128, 24C256 valid for environment eeprom */
+#define CONFIG_SYS_I2C_MULTI_EEPROMS
+#define CONFIG_SYS_EEPROM_PAGE_WRITE_ENABLE
+#define CONFIG_SYS_EEPROM_PAGE_WRITE_BITS	6 /* 64 Byte write page */
+#define CONFIG_SYS_EEPROM_PAGE_WRITE_DELAY_MS	10
+
 #define CONFIG_SYS_I2C_EEPROM_ADDR	0x50
 #define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	2
 
