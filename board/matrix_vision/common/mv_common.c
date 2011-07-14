12b5723b3e599857812abfeeb2c241c700274542
Michael Jones <michael.jones@matrix-vision.de>
2011-07-14 23:09:44 +0000

    mv_common.c: don't compile reset_environment if ENV_IS_NOWHERE
    Doesn't make sense to provide this function to boards which defined
    CONFIG_ENV_IS_NOWHERE. Such a board gets a linking error because
    common/env_nowhere.c doesn't define saveenv().
    
    Signed-off-by: Michael Jones <michael.jones@matrix-vision.de>
    Acked-by: Mike Frysinger <vapier@gentoo.org>

--- board/matrix_vision/common/mv_common.c
+++ board/matrix_vision/common/mv_common.c
@@ -38,6 +38,7 @@ static char* entries_to_keep[] = {
 #define MV_MAX_ENV_ENTRY_LENGTH	64
 #define MV_KEEP_ENTRIES		ARRAY_SIZE(entries_to_keep)
 
+#ifndef CONFIG_ENV_IS_NOWHERE
 void mv_reset_environment(void)
 {
 	int i;
@@ -67,6 +68,7 @@ void mv_reset_environment(void)
 
 	saveenv();
 }
+#endif
 
 int mv_load_fpga(void)
 {
