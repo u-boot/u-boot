7130a579fdba5dd1bf99508fb0b1d13317542109
Andreas Pretzsch <apr@cn-eng.de>
2011-07-16 05:51:00 +0000

    add command fitupd to run an update from a FIT image
    Command calls update_tftp() analogous to automatic update described
    in doc/README.update.
    
    Usage:
    fitupd [addr]
            - run update from FIT image at addr
              or from tftp 'updatefile'
    
    Signed-off-by: Andreas Pretzsch <apr@cn-eng.de>

---
+++ common/cmd_fitupd.c
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2011
+ * Andreas Pretzsch, carpe noctem engineering, apr@cn-eng.de
+ *
+ * This file is released under the terms of GPL v2 and any later version.
+ * See the file COPYING in the root directory of the source tree for details.
+ */
+
+#include <common.h>
+#include <command.h>
+
+#if !defined(CONFIG_UPDATE_TFTP)
+#error "CONFIG_UPDATE_TFTP required"
+#endif
+
+extern int update_tftp(ulong addr);
+
+static int do_fitupd(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	ulong addr = 0UL;
+
+	if (argc > 2)
+		return cmd_usage(cmdtp);
+
+	if (argc == 2)
+		addr = simple_strtoul(argv[1], NULL, 16);
+
+	return update_tftp(addr);
+}
+
+U_BOOT_CMD(fitupd, 2, 0, do_fitupd,
+	"update from FIT image",
+	"[addr]\n"
+	"\t- run update from FIT image at addr\n"
+	"\t  or from tftp 'updatefile'"
+);
