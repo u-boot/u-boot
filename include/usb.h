80b350a7ab83653242033877c203940db7ecc0e5
Jason Cooper <u-boot@lakedaemon.net>
2011-07-31 20:09:58 +0000

    usb: increase non-bulk timeout for slow chipsets.
    If you take a look at 96820a35, you'll see the original timeout was
    CONFIG_SYS_HZ.  Which is 1000.  After the mentioned change, non-bulk timeout
    was changed to 100.  This causes timeout failures on the dreamplug platform
    when trying to initialize the usb microsd reader.
    
    Signed-off-by: Jason Cooper <u-boot@lakedaemon.net>
    Acked-by: Mike Frysinger <vapier@gentoo.org>

--- include/usb.h
+++ include/usb.h
@@ -46,7 +46,7 @@
  * This is the timeout to allow for submitting an urb in ms. We allow more
  * time for a BULK device to react - some are slow.
  */
-#define USB_TIMEOUT_MS(pipe) (usb_pipebulk(pipe) ? 5000 : 100)
+#define USB_TIMEOUT_MS(pipe) (usb_pipebulk(pipe) ? 5000 : 1000)
 
 /* device request (setup) */
 struct devrequest {
