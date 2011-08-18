45263ec4c06fd8f1201df0bc7a8ab37a639a41f3
Jason Jin <Jason.jin@freescale.com>
2011-08-18 14:41:14 +0800

    ColdFire: Update compile flags for each CPUs
    Remove compiler version check for gcc 4.1 in config.mk.
    
    Signed-off-by: TsiChung Liew <tsicliew@gmail.com>
    Signed-off-by: Jason Jin <Jason.jin@freescale.com>

--- arch/m68k/cpu/mcf523x/config.mk
+++ arch/m68k/cpu/mcf523x/config.mk
@@ -24,8 +24,4 @@
 #
 
 PLATFORM_RELFLAGS += -ffixed-d7 -msep-data
-ifneq ($(findstring 4.1,$(shell $(CC) --version)),4.1)
 PLATFORM_CPPFLAGS += -mcpu=5235 -fPIC
-else
-PLATFORM_CPPFLAGS += -m5307 -fPIC
-endif
