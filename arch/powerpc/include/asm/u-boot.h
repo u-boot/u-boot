25fb02abdf7af37b3b9f83891115ee94e9073708
Weirich, Bernhard <Bernhard.Weirich@riedel.net>
2011-09-08 18:27:38 +0200

    Fix incorrect array size of phy settings for 405EX
    Change bd_t->bi_phy* arrays from 1 to 2 for PPC405EX since
    405EX has 2 ethernet interfaces.
    
    Signed-off-by: Bernhard Weirich <bernhard.weirich@riedel.net>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- arch/powerpc/include/asm/u-boot.h
+++ arch/powerpc/include/asm/u-boot.h
@@ -132,7 +132,7 @@ typedef struct bd_info {
     defined(CONFIG_460EX) || defined(CONFIG_460GT)
 	int		bi_phynum[4];           /* Determines phy mapping */
 	int		bi_phymode[4];          /* Determines phy mode */
-#elif defined(CONFIG_405EP) || defined(CONFIG_440)
+#elif defined(CONFIG_405EP) || defined(CONFIG_405EX) || defined(CONFIG_440)
 	int		bi_phynum[2];           /* Determines phy mapping */
 	int		bi_phymode[2];          /* Determines phy mode */
 #else
