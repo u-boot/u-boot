7fcd9bbd6f0a69fb41b097cc8fd11d1ef21519e8
Marek Vasut <marek.vasut@gmail.com>
2011-09-26 02:26:03 +0200

    GCC4.6: Squash warning in cmd_nvedit.c
    cmd_nvedit.c: In function ‘do_env_edit’:
    cmd_nvedit.c:463:6: warning: variable ‘len’ set but not used
    [-Wunused-but-set-variable]
    
    Signed-off-by: Marek Vasut <marek.vasut@gmail.com>

--- common/cmd_nvedit.c
+++ common/cmd_nvedit.c
@@ -460,7 +460,6 @@ int do_env_edit(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	char buffer[CONFIG_SYS_CBSIZE];
 	char *init_val;
-	int len;
 
 	if (argc < 2)
 		return cmd_usage(cmdtp);
@@ -468,7 +467,7 @@ int do_env_edit(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	/* Set read buffer to initial value or empty sting */
 	init_val = getenv(argv[1]);
 	if (init_val)
-		len = sprintf(buffer, "%s", init_val);
+		sprintf(buffer, "%s", init_val);
 	else
 		buffer[0] = '\0';
 
