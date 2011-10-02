d90f0c107bdb9766aeaf11181b97f5d7fde3efda
Mike Frysinger <vapier@gentoo.org>
2011-10-02 10:01:26 +0000

    net: drop !NET_MULTI ns7520 driver
    This driver was never converted to NET_MULTI, and no board uses it.
    So punt it and be done.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- net/eth.c
+++ net/eth.c
@@ -541,7 +541,6 @@ char *eth_get_name (void)
 #warning Ethernet driver is deprecated.  Please update to use CONFIG_NET_MULTI
 
 extern int mcf52x2_miiphy_initialize(bd_t *bis);
-extern int ns7520_miiphy_initialize(bd_t *bis);
 
 
 int eth_initialize(bd_t *bis)
@@ -552,9 +551,6 @@ int eth_initialize(bd_t *bis)
 
 #if defined(CONFIG_MCF52x2)
 	mcf52x2_miiphy_initialize(bis);
-#endif
-#if defined(CONFIG_DRIVER_NS7520_ETHERNET)
-	ns7520_miiphy_initialize(bis);
 #endif
 	return 0;
 }
