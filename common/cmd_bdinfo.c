0c277ef9f92b41b41a78682330b6c35673a1819b
Timur Tabi <timur@freescale.com>
2011-10-05 17:08:07 -0500

    cmd_bdinfo: replace print_str() with print_mhz()
    The print_str() helper function for cmd_bdinfo can print any string, but it
    is only used to print MHz values.  Replace it with print_mhz() that takes
    a number and converts it to a string internally.
    
    Signed-off-by: Timur Tabi <timur@freescale.com>

--- common/cmd_bdinfo.c
+++ common/cmd_bdinfo.c
@@ -43,12 +43,11 @@ static void print_lnum(const char *, u64);
 #endif
 
 #if defined(CONFIG_PPC)
-static void print_str(const char *, const char *);
+static void print_mhz(const char *, unsigned long);
 
 int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	bd_t *bd = gd->bd;
-	char buf[32];
 
 #ifdef DEBUG
 	print_num("bd address",		(ulong)bd);
@@ -71,32 +70,32 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	defined(CONFIG_440GR) || defined(CONFIG_440GRX) || \
 	defined(CONFIG_440SP) || defined(CONFIG_440SPE) || \
 	defined(CONFIG_XILINX_405)
-	print_str("procfreq",		strmhz(buf, bd->bi_procfreq));
-	print_str("plb_busfreq",	strmhz(buf, bd->bi_plb_busfreq));
+	print_mhz("procfreq",		bd->bi_procfreq);
+	print_mhz("plb_busfreq",	bd->bi_plb_busfreq);
 #if	defined(CONFIG_405EP) || defined(CONFIG_405GP) || \
 	defined(CONFIG_440EP) || defined(CONFIG_440EPX) || \
 	defined(CONFIG_440GR) || defined(CONFIG_440GRX) || \
 	defined(CONFIG_440SPE) || defined(CONFIG_XILINX_405)
-	print_str("pci_busfreq",	strmhz(buf, bd->bi_pci_busfreq));
+	print_mhz("pci_busfreq",	bd->bi_pci_busfreq);
 #endif
 #else	/* ! CONFIG_405GP, CONFIG_405CR, CONFIG_405EP, CONFIG_XILINX_405, CONFIG_440EP CONFIG_440GR */
 #if defined(CONFIG_CPM2)
-	print_str("vco",		strmhz(buf, bd->bi_vco));
-	print_str("sccfreq",		strmhz(buf, bd->bi_sccfreq));
-	print_str("brgfreq",		strmhz(buf, bd->bi_brgfreq));
+	print_mhz("vco",		bd->bi_vco);
+	print_mhz("sccfreq",		bd->bi_sccfreq);
+	print_mhz("brgfreq",		bd->bi_brgfreq);
 #endif
-	print_str("intfreq",		strmhz(buf, bd->bi_intfreq));
+	print_mhz("intfreq",		bd->bi_intfreq);
 #if defined(CONFIG_CPM2)
-	print_str("cpmfreq",		strmhz(buf, bd->bi_cpmfreq));
+	print_mhz("cpmfreq",		bd->bi_cpmfreq);
 #endif
-	print_str("busfreq",		strmhz(buf, bd->bi_busfreq));
+	print_mhz("busfreq",		bd->bi_busfreq);
 #endif /* CONFIG_405GP, CONFIG_405CR, CONFIG_405EP, CONFIG_XILINX_405, CONFIG_440EP CONFIG_440GR */
 #if defined(CONFIG_MPC8220)
-	print_str("inpfreq",		strmhz(buf, bd->bi_inpfreq));
-	print_str("flbfreq",		strmhz(buf, bd->bi_flbfreq));
-	print_str("pcifreq",		strmhz(buf, bd->bi_pcifreq));
-	print_str("vcofreq",		strmhz(buf, bd->bi_vcofreq));
-	print_str("pevfreq",		strmhz(buf, bd->bi_pevfreq));
+	print_mhz("inpfreq",		bd->bi_inpfreq);
+	print_mhz("flbfreq",		bd->bi_flbfreq);
+	print_mhz("pcifreq",		bd->bi_pcifreq);
+	print_mhz("vcofreq",		bd->bi_vcofreq);
+	print_mhz("pevfreq",		bd->bi_pevfreq);
 #endif
 
 	print_eth(0);
@@ -117,7 +116,7 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 #endif
 
 #ifdef CONFIG_HERMES
-	print_str("ethspeed",		strmhz(buf, bd->bi_ethspeed));
+	print_mhz("ethspeed",		bd->bi_ethspeed);
 #endif
 	printf("IP addr     = %pI4\n", &bd->bi_ip_addr);
 	printf("baudrate    = %6ld bps\n", bd->bi_baudrate);
@@ -209,12 +208,11 @@ int do_bdinfo(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 
 #elif defined(CONFIG_M68K)
 
-static void print_str(const char *, const char *);
+static void print_mhz(const char *, unsigned long);
 
 int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	bd_t *bd = gd->bd;
-	char buf[32];
 
 	print_num("memstart",		(ulong)bd->bi_memstart);
 	print_lnum("memsize",		(u64)bd->bi_memsize);
@@ -228,15 +226,15 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 #if defined(CONFIG_SYS_MBAR)
 	print_num("mbar",		bd->bi_mbar_base);
 #endif
-	print_str("cpufreq",		strmhz(buf, bd->bi_intfreq));
-	print_str("busfreq",		strmhz(buf, bd->bi_busfreq));
+	print_mhz("cpufreq",		bd->bi_intfreq);
+	print_mhz("busfreq",		bd->bi_busfreq);
 #ifdef CONFIG_PCI
-	print_str("pcifreq",		strmhz(buf, bd->bi_pcifreq));
+	print_mhz("pcifreq",		bd->bi_pcifreq);
 #endif
 #ifdef CONFIG_EXTRA_CLOCK
-	print_str("flbfreq",		strmhz(buf, bd->bi_flbfreq));
-	print_str("inpfreq",		strmhz(buf, bd->bi_inpfreq));
-	print_str("vcofreq",		strmhz(buf, bd->bi_vcofreq));
+	print_mhz("flbfreq",		bd->bi_flbfreq);
+	print_mhz("inpfreq",		bd->bi_inpfreq);
+	print_mhz("vcofreq",		bd->bi_vcofreq);
 #endif
 #if defined(CONFIG_CMD_NET)
 	print_eth(0);
@@ -259,19 +257,18 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 #elif defined(CONFIG_BLACKFIN)
 
-static void print_str(const char *, const char *);
+static void print_mhz(const char *, unsigned long);
 
 int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	bd_t *bd = gd->bd;
-	char buf[32];
 
 	printf("U-Boot      = %s\n", bd->bi_r_version);
 	printf("CPU         = %s\n", bd->bi_cpu);
 	printf("Board       = %s\n", bd->bi_board_name);
-	print_str("VCO",	strmhz(buf, bd->bi_vco));
-	print_str("CCLK",	strmhz(buf, bd->bi_cclk));
-	print_str("SCLK",	strmhz(buf, bd->bi_sclk));
+	print_mhz("VCO",	bd->bi_vco);
+	print_mhz("CCLK",	bd->bi_cclk);
+	print_mhz("SCLK",	bd->bi_sclk);
 
 	print_num("boot_params",	(ulong)bd->bi_boot_params);
 	print_num("memstart",		(ulong)bd->bi_memstart);
@@ -380,13 +377,12 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 #elif defined(CONFIG_X86)
 
-static void print_str(const char *, const char *);
+static void print_mhz(const char *, unsigned long);
 
 int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	int i;
 	bd_t *bd = gd->bd;
-	char buf[32];
 
 	print_num("boot_params",	(ulong)bd->bi_boot_params);
 	print_num("bi_memstart",	bd->bi_memstart);
@@ -397,8 +393,8 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	print_num("bi_sramstart",	bd->bi_sramstart);
 	print_num("bi_sramsize",	bd->bi_sramsize);
 	print_num("bi_bootflags",	bd->bi_bootflags);
-	print_str("cpufreq",		strmhz(buf, bd->bi_intfreq));
-	print_str("busfreq",		strmhz(buf, bd->bi_busfreq));
+	print_mhz("cpufreq",		bd->bi_intfreq);
+	print_mhz("busfreq",		bd->bi_busfreq);
 
 	for (i = 0; i < CONFIG_NR_DRAM_BANKS; ++i) {
 		print_num("DRAM bank",	i);
@@ -409,7 +405,7 @@ int do_bdinfo(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 #if defined(CONFIG_CMD_NET)
 	print_eth(0);
 	printf("ip_addr     = %pI4\n", &bd->bi_ip_addr);
-	print_str("ethspeed",	    strmhz(buf, bd->bi_ethspeed));
+	print_mhz("ethspeed",	    bd->bi_ethspeed);
 #endif
 	printf("baudrate    = %d bps\n", bd->bi_baudrate);
 
@@ -474,9 +470,11 @@ static void print_lnum(const char *name, u64 value)
 	defined(CONFIG_M68K) || \
 	defined(CONFIG_BLACKFIN) || \
 	defined(CONFIG_X86)
-static void print_str(const char *name, const char *str)
+static void print_mhz(const char *name, unsigned long hz)
 {
-	printf("%-12s= %6s MHz\n", name, str);
+	char buf[32];
+
+	printf("%-12s= %6s MHz\n", name, strmhz(buf, hz));
 }
 #endif	/* CONFIG_PPC */
 
