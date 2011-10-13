f5ca20c6b60acfce81f45a0a0a92a5e72de5b008
Simon Glass <sjg@chromium.org>
2011-10-13 14:43:14 +0000

    x86: Use getenv_ulong() in place of getenv(), strtoul
    This changes the board code to use the new getenv_ulong() function.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/x86/lib/board.c
+++ arch/x86/lib/board.c
@@ -74,14 +74,8 @@ extern ulong __bss_end;
  */
 static int init_baudrate (void)
 {
-	char tmp[64];	/* long enough for environment variables */
-	int i = getenv_f("baudrate", tmp, 64);
-
-	gd->baudrate = (i != 0)
-			? (int) simple_strtoul (tmp, NULL, 10)
-			: CONFIG_BAUDRATE;
-
-	return (0);
+	gd->baudrate = getenv_ulong("baudrate", 10, CONFIG_BAUDRATE);
+	return 0;
 }
 
 static int display_banner (void)
@@ -360,9 +354,7 @@ void board_init_r(gd_t *id, ulong dest_addr)
 	udelay(20);
 
 	/* Initialize from environment */
-	if ((s = getenv ("loadaddr")) != NULL) {
-		load_addr = simple_strtoul (s, NULL, 16);
-	}
+	load_addr = getenv_ulong("loadaddr", 16, load_addr);
 #if defined(CONFIG_CMD_NET)
 	if ((s = getenv ("bootfile")) != NULL) {
 		copy_filename (BootFile, s, sizeof (BootFile));
