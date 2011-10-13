ae5c77dd908edaf55025fa3f41095bfb9cb1b973
Christian Riesch <christian.riesch@omicron.at>
2011-10-13 00:52:29 +0000

    da850evm: Move LPSC configuration to board_early_init_f()
    Since commit f1d2b313c9eb6808d30c16a9eb5251240452a56c the serial
    port of the da850evm is accessed before the UART2 peripheral of
    the SoC is powered on in the function board_init() in
    board/davinci/da8xxevm/da850evm.c.
    
    When u-boot is used in conjunction with the UBL (user boot loader, see
    doc/README.davinci) on this board, the UART2 peripheral is already
    turned on by UBL at the time u-boot is started. Hence, the wrong
    initialization sequence is not noticed by most users. However, if UBL is not
    used, u-boot must power on the peripheral before using it.
    
    This patch adds a board_early_init_f() function for the LPSC
    configuration to the da850evm board configuration.
    
    Signed-off-by: Christian Riesch <christian.riesch@omicron.at>
    Cc: Sudhakar Rajashekhara <sudhakar.raj@ti.com>
    Cc: Sandeep Paulraj <s-paulraj@ti.com>
    Cc: Heiko Schocher <hs@denx.de>
    Cc: Ben Gardiner <bengardiner@nanometrics.ca>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/da850evm.h
+++ include/configs/da850evm.h
@@ -160,6 +160,7 @@
  * U-Boot general configuration
  */
 #define CONFIG_MISC_INIT_R
+#define CONFIG_BOARD_EARLY_INIT_F
 #define CONFIG_BOOTFILE		"uImage" /* Boot file name */
 #define CONFIG_SYS_PROMPT	"U-Boot > " /* Command Prompt */
 #define CONFIG_SYS_CBSIZE	1024 /* Console I/O Buffer Size	*/
