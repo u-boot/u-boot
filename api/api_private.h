a2a5729fc1247bb45d794e9d731c0b03bf58096f
Che-Liang Chiou <clchiou@chromium.org>
2011-10-20 23:04:22 +0000

    api: export LCD device to external apps
    This patch exports LCD info-query and bitmap-rendering functions to
    external apps.
    
    This patch is tested on a Seaboard.  Because the LCD driver is not yet
    upstreamed, the test was done in a local downstream repo.
    
    Signed-off-by: Che-Liang Chiou <clchiou@chromium.org>

--- api/api_private.h
+++ api/api_private.h
@@ -45,4 +45,8 @@ int		dev_write_net(void *, void *, int);
 
 void dev_stor_init(void);
 
+int display_get_info(int type, struct display_info *di);
+int display_draw_bitmap(ulong bitmap, int x, int y);
+void display_clear(void);
+
 #endif /* _API_PRIVATE_H_ */
