30dca9ddadcd428a4b54457f93602306571b03b9
Luca Ceresoli <luca.ceresoli@comelit.it>
2011-11-04 13:42:09 -0400

    ARM: dig297: Define MACH_TYPE_OMAP3_CPS and CONFIG_MACH_TYPE
    MACH_TYPE_OMAP3_CPS was dropped in the latest mach-types sync (47af6f61bcd9)
    because it is not mainlined in Linux.
    
    Signed-off-by: Luca Ceresoli <luca.ceresoli@comelit.it>
    Cc: Sandeep Paulraj <s-paulraj@ti.com>
    Cc: Albert Aribaud <albert.u.boot@aribaud.net>
    Cc: Wolfgang Denk <wd@denx.de>
    CC: Tom Rini <tom.rini@gmail.com>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- board/comelit/dig297/dig297.c
+++ board/comelit/dig297/dig297.c
@@ -91,8 +91,6 @@ static const u32 gpmc_lan_config[] = {
 int board_init(void)
 {
 	gpmc_init();		/* in SRAM or SDRAM, finish GPMC */
-	/* board id for Linux */
-	gd->bd->bi_arch_number = MACH_TYPE_OMAP3_CPS;
 	/* boot param addr */
 	gd->bd->bi_boot_params = (OMAP34XX_SDRC_CS0 + 0x100);
 
