73ce5003734aae3a50e5429b404b5b8e00e5a98e
Alexander Holler <holler@ahsoftware.de>
2011-11-09 16:40:11 -0500

    BeagleBoard: config: Switch to ttyO2
    This is needed to support the latest kernel versions.
    
    Cc: Albert ARIBAUD <albert.u.boot@aribaud.net>
    Cc: Sandeep Paulraj <s-paulraj@ti.com>
    Signed-off-by: Alexander Holler <holler@ahsoftware.de>
    Signed-off-by: Dirk Behme <dirk.behme@gmail.com>
    Signed-off-by: Tom Rini <trini@ti.com>
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- include/configs/omap3_beagle.h
+++ include/configs/omap3_beagle.h
@@ -215,7 +215,7 @@
 	"rdaddr=0x81000000\0" \
 	"usbtty=cdc_acm\0" \
 	"bootfile=uImage.beagle\0" \
-	"console=ttyS2,115200n8\0" \
+	"console=tty02,115200n8\0" \
 	"mpurate=auto\0" \
 	"buddy=none "\
 	"optargs=\0" \
