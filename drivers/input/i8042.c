22e0f5a9ecab85f36a0fe69892d950c1ac212c91
Gabe Black <gabeblack@chromium.org>
2011-11-14 20:18:12 +0000

    x86: Fix some bugs in the i8402 driver when no controller is present
    If no controller is present, the i8402 driver should return immediately and
    not attempt to operate on the missing hardware.
    
    In kbd_input_empty, the status register is checked every millisecond to see
    whether the input buffer is empty, up to a timeout which is tracked by
    decrimenting a counter each time the check is performed. The decrement is
    performed with a postfix -- operator, and the value of the counter is
    checked in place. That means that when the counter reaches zero and the
    loop terminates, it will actually be decrimented one more time and become
    -1. That value is returned as the return value of the function. That would
    give the right answer if it wasn't for that extra decrement because a
    timeout would indicate that the buffer never became empty.
    
    This change fixes both of those bugs.
    
    Signed-off-by: Gabe Black <gabeblack@chromium.org>

--- drivers/input/i8042.c
+++ drivers/input/i8042.c
@@ -313,6 +313,13 @@ static unsigned char ext_key_map[] = {
 	0x00  /* map end */
 	};
 
+/******************************************************************************/
+
+static int kbd_controller_present(void)
+{
+	return in8(I8042_STATUS_REG) != 0xff;
+}
+
 /*******************************************************************************
  *
  * i8042_kbd_init - reset keyboard and init state flags
@@ -321,6 +328,9 @@ int i8042_kbd_init(void)
 {
 	int keymap, try;
 	char *penv;
+
+	if (!kbd_controller_present())
+		return -1;
 
 #ifdef CONFIG_USE_CPCIDVI
 	penv = getenv("console");
@@ -603,7 +613,7 @@ static int kbd_input_empty(void)
 	while ((in8(I8042_STATUS_REG) & 0x02) && kbdTimeout--)
 		udelay(1000);
 
-	return kbdTimeout;
+	return kbdTimeout != -1;
 }
 
 /******************************************************************************/
