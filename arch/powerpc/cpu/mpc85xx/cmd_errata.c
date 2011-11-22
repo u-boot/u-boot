43f082bb7fc1c24b32a4abc693869c7d14d42829
Kumar Gala <galak@kernel.crashing.org>
2011-11-22 06:51:15 -0600

    powerpc/85xx: Add workaround for erratum CPU-A003999
    Erratum A-003999: Running Floating Point instructions requires special
    initialization.
    
    Impact:
    Floating point arithmetic operations may result in an incorrect value.
    
    Workaround:
    Perform a read modify write to set bit 7 to a 1 in SPR 977 before
    executing any floating point arithmetic operation. This bit can be set
    when setting MSR[FP], and can be cleared when clearing MSR[FP].
    Alternatively, the bit can be set once at boot time, and never cleared.
    There will be no performance degradation due to setting this bit.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/cpu/mpc85xx/cmd_errata.c
+++ arch/powerpc/cpu/mpc85xx/cmd_errata.c
@@ -53,6 +53,9 @@ static int do_errata(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 #if defined(CONFIG_SYS_P4080_ERRATUM_CPU22)
 	puts("Work-around for Erratum CPU22 enabled\n");
 #endif
+#if defined(CONFIG_SYS_FSL_ERRATUM_CPU_A003999)
+	puts("Work-around for Erratum CPU-A003999 enabled\n");
+#endif
 #if defined(CONFIG_SYS_FSL_ERRATUM_DDR_MSYNC_IN)
 	puts("Work-around for DDR MSYNC_IN Erratum enabled\n");
 #endif
