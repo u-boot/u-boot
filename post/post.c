c90a4dd79cb17abb46689f27ff9f1c971362d6e2
Christian Riesch <christian.riesch@omicron.at>
2011-12-09 16:54:02 +0100

    post/post.c: Use lldiv for 64-bit divisions
    Signed-off-by: Christian Riesch <christian.riesch@omicron.at>
    Cc: Tom Rini <trini@ti.com>
    Cc: Heiko Schocher <hs@denx.de>
    Cc: Wolfgang Denk <wd@denx.de>

--- post/post.c
+++ post/post.c
@@ -24,6 +24,7 @@
 #include <common.h>
 #include <stdio_dev.h>
 #include <watchdog.h>
+#include <div64.h>
 #include <post.h>
 
 #ifdef CONFIG_SYS_POST_HOTKEYS_GPIO
@@ -495,7 +496,7 @@ void post_reloc(void)
 unsigned long post_time_ms(unsigned long base)
 {
 #if defined(CONFIG_PPC) || defined(CONFIG_ARM)
-	return (unsigned long)(get_ticks() / (get_tbclk() / CONFIG_SYS_HZ))
+	return (unsigned long)lldiv(get_ticks(), get_tbclk() / CONFIG_SYS_HZ)
 		- base;
 #else
 #warning "Not implemented yet"
