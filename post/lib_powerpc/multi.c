38081ff7a4c43cc77bac3f7e85b6592b50d136a8
Wolfgang Denk <wd@denx.de>
2011-12-23 01:29:11 +0000

    post/lib_powerpc/multi.c: code cleanup
    Clean up and document the code:
    
    - get rid of unneeded code block
    - add comment which code is generated
    
    Signed-off-by: Wolfgang Denk <wd@denx.de>
    Cc: Anatolij Gustschin <agust@denx.de>
    Cc: Stefan Roese <sr@denx.de>
    Cc: Kumar Gala <galak@kernel.crashing.org>
    Cc: Kim Phillips <kim.phillips@freescale.com>
    Cc: Andy Fleming <afleming@gmail.com>
    Acked-by: Anatolij Gustschin <agust@denx.de>

--- post/lib_powerpc/multi.c
+++ post/lib_powerpc/multi.c
@@ -44,26 +44,23 @@ int cpu_post_test_multi(void)
 {
 	int ret = 0;
 	unsigned int i;
+	ulong src[26], dst[26];
 	int flag = disable_interrupts();
 
-	if (ret == 0) {
-		ulong src[26], dst[26];
+	ulong code[] = {
+		ASM_LMW(5, 3, 0),	/* lmw	r5, 0(r3)	*/
+		ASM_STMW(5, 4, 0),	/* stmr	r5, 0(r4)	*/
+		ASM_BLR,		/* blr			*/
+	};
 
-		ulong code[] = {
-			ASM_LMW(5, 3, 0),
-			ASM_STMW(5, 4, 0),
-			ASM_BLR,
-		};
-
-		for (i = 0; i < ARRAY_SIZE(src); ++i) {
-			src[i] = i;
-			dst[i] = 0;
-		}
-
-		cpu_post_exec_02(code, (ulong) src, (ulong) dst);
-
-		ret = memcmp(src, dst, sizeof(dst)) == 0 ? 0 : -1;
+	for (i = 0; i < ARRAY_SIZE(src); ++i) {
+		src[i] = i;
+		dst[i] = 0;
 	}
+
+	cpu_post_exec_02(code, (ulong) src, (ulong) dst);
+
+	ret = memcmp(src, dst, sizeof(dst)) == 0 ? 0 : -1;
 
 	if (ret != 0)
 		post_log("Error at multi test !\n");
