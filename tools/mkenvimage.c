d1acdae98655af4a9ed1b138325ff172206d1c00
David Wagner <david.wagner@free-electrons.com>
2012-01-13 13:27:35 +0000

    mkenvimage: Correct an include and add a missing one
    compiler.h needs to be included from U-Boot's headers.
    Also, group U-Boot-specific includes together
    
    stdlib.h was missing.
    
    Signed-off-by: David Wagner <david.wagner@free-electrons.com>
    Acked-by: Mike Frysinger <vapier@gentoo.org>

--- tools/mkenvimage.c
+++ tools/mkenvimage.c
@@ -31,13 +31,14 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
-#include <compiler.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include "compiler.h"
 #include <u-boot/crc.h>
 #include <version.h>
 
