33e7e60c77ec89cd969ff5718f798f881c2832b7
Mike Frysinger <vapier@gentoo.org>
2012-02-16 01:10:43 -0500

    Blackfin: gd_t: relocate volatile markings
    This makes Blackfin behave the same as other ports, and fixes many gcc
    warnings that show up with 4.5+:
    	board.c:40:1: warning: optimization may eliminate reads and/or
    		writes to register variables
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/include/asm/global_data.h
+++ arch/blackfin/include/asm/global_data.h
@@ -73,6 +73,6 @@ typedef struct global_data {
 #define GD_FLG_DISABLE_CONSOLE	0x00040	/* Disable console (in & out)		*/
 #define GD_FLG_ENV_READY	0x00080	/* Environment imported into hash table	*/
 
-#define DECLARE_GLOBAL_DATA_PTR     register gd_t * volatile gd asm ("P3")
+#define DECLARE_GLOBAL_DATA_PTR     register volatile gd_t *gd asm ("P3")
 
 #endif
