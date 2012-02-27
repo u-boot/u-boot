0291091c9e4dffce1f278d6b87ac123f389595df
Simon Glass <sjg@chromium.org>
2012-02-27 10:52:51 +0000

    tegra: usb: Add common USB defines for tegra2 boards
    All Tegra2 boards should include tegra2-common. This adds the required
    USB config to that file.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- include/configs/tegra2-common.h
+++ include/configs/tegra2-common.h
@@ -84,6 +84,16 @@
 #define CONFIG_SYS_BAUDRATE_TABLE	{4800, 9600, 19200, 38400, 57600,\
 					115200}
 
+/*
+ * This parameter affects a TXFILLTUNING field that controls how much data is
+ * sent to the latency fifo before it is sent to the wire. Without this
+ * parameter, the default (2) causes occasional Data Buffer Errors in OUT
+ * packets depending on the buffer address and size.
+ */
+#define CONFIG_USB_EHCI_TXFIFO_THRESH	10
+#define CONFIG_EHCI_IS_TDI
+#define CONFIG_EHCI_DCACHE
+
 /* include default commands */
 #include <config_cmd_default.h>
 
