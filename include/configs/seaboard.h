d9fdfe0aa598978054f108ba42ca928aceb8c090
Simon Glass <sjg@chromium.org>
2012-02-27 10:52:53 +0000

    tegra: fdt: Enable FDT support for Seaboard
    This switches Seaboard over to use FDT for run-time config instead of
    CONFIG options. USB is the only user at present.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- include/configs/seaboard.h
+++ include/configs/seaboard.h
@@ -27,6 +27,11 @@
 #include <asm/sizes.h>
 #include "tegra2-common.h"
 
+/* Enable fdt support for Seaboard. Flash the image in u-boot-dtb.bin */
+#define CONFIG_DEFAULT_DEVICE_TREE	tegra2-seaboard
+#define CONFIG_OF_CONTROL
+#define CONFIG_OF_SEPARATE
+
 /* High-level configuration options */
 #define TEGRA2_SYSMEM		"mem=384M@0M nvmem=128M@384M mem=512M@512M"
 #define V_PROMPT		"Tegra2 (SeaBoard) # "
