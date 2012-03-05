696df71b5f564916db6762b8f21605676abc6baf
Marek Vasut <marex@denx.de>
2012-03-06 00:00:12 +0100

    IXP: Make IXP buildable with arm-linux- toolchains
    Add -EB flag to LD to switch endianness of the linker. This should make armeb
    targets buildable again. Also, make use of U-Boot's internal libgcc instead of
    toolchain's one, this works around the use of libraries from GCC, which might be
    little endian.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Bryan Hundven <bryanhundven@gmail.com>
    Cc: Michael Schwingen <rincewind@discworld.dascon.de>

--- arch/arm/cpu/ixp/config.mk
+++ arch/arm/cpu/ixp/config.mk
@@ -28,6 +28,9 @@ PLATFORM_RELFLAGS += -fno-common -ffixed-r8 -msoft-float -mbig-endian
 
 PLATFORM_CPPFLAGS += -mbig-endian -march=armv5te -mtune=strongarm1100
 
+PLATFORM_LDFLAGS += -EB
+USE_PRIVATE_LIBGCC = yes
+
 # -fdata-sections triggers "section .bss overlaps section .rel.dyn" linker error
 PLATFORM_RELFLAGS += -ffunction-sections
 LDFLAGS_u-boot += --gc-sections
