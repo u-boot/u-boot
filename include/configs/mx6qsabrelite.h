f4ac6cb6af107ce62754dd3bfc7ff31a92e7e2c8
Fabio Estevam <fabio.estevam@freescale.com>
2012-03-28 09:45:26 +0000

    mx6qsabrelite: Fix the serial console port
    On mx6qsabrelite the console is ttymxc1.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>

--- include/configs/mx6qsabrelite.h
+++ include/configs/mx6qsabrelite.h
@@ -116,7 +116,7 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
        "script=boot.scr\0" \
        "uimage=uImage\0" \
-       "console=ttymxc3\0" \
+	"console=ttymxc1\0" \
 	"fdt_high=0xffffffff\0"	  \
 	"initrd_high=0xffffffff\0" \
        "mmcdev=0\0" \
