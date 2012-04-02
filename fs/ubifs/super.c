f75325e1927398f7e18e15f186b1fc52174cc19f
Bernhard Walle <walle@corscience.de>
2012-04-02 01:58:34 +0000

    UBIFS: Improve error message when reading superblock failed
    In addition to the error message also display the error code. I had the
    problem that my malloc memory was not enough (ENOMEM), and if u-boot
    had displayed the error code immediately that would have saved me some
    debugging.
    
    Signed-off-by: Bernhard Walle <walle@corscience.de>
    
    Use ubifs_err instead of printf.
    Add "errno=%d" in output as suggested by Albert Aribaud.
    
    Signed-off-by: Thomas Weber <weber@corscience.de>

--- fs/ubifs/super.c
+++ fs/ubifs/super.c
@@ -1191,7 +1191,7 @@ int ubifs_mount(char *vol_name)
 	mnt = NULL;
 	ret = ubifs_get_sb(&ubifs_fs_type, flags, name, data, mnt);
 	if (ret) {
-		printf("Error reading superblock on volume '%s'!\n", name);
+		ubifs_err("Error reading superblock on volume '%s' errno=%d!\n", name, ret);
 		return -1;
 	}
 
