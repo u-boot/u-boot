d519b4bc0a1216eb273c76c129b779fa1ba2beb5
Fabio Estevam <fabio.estevam@freescale.com>
2012-04-02 11:19:45 +0000

    ARM: introduce arch_early_init_r()
    Introduce arch_early_init_r() function, which can be useful for doing
    early initialization after relocation has happened.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>

--- arch/arm/lib/board.c
+++ arch/arm/lib/board.c
@@ -500,6 +500,10 @@ void board_init_r(gd_t *id, ulong dest_addr)
 	malloc_start = dest_addr - TOTAL_MALLOC_LEN;
 	mem_malloc_init (malloc_start, TOTAL_MALLOC_LEN);
 
+#ifdef CONFIG_ARCH_EARLY_INIT_R
+	arch_early_init_r();
+#endif
+
 #if !defined(CONFIG_SYS_NO_FLASH)
 	puts("Flash: ");
 
