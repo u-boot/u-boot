c5179da9f3746f4ef59e8b5458b06b8fa2af422c
Yen Lin <yelin@nvidia.com>
2012-04-02 13:18:56 +0000

    tegra: Setup PMC scratch info from ap20 setup
    Save SDRAM parameters into the warmboot scratch registers
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Yen Lin <yelin@nvidia.com>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- arch/arm/cpu/armv7/tegra2/ap20.c
+++ arch/arm/cpu/armv7/tegra2/ap20.c
@@ -31,6 +31,7 @@
 #include <asm/arch/pmc.h>
 #include <asm/arch/pinmux.h>
 #include <asm/arch/scu.h>
+#include <asm/arch/warmboot.h>
 #include <common.h>
 
 int tegra_get_chip_type(void)
@@ -322,6 +323,11 @@ void init_pmc_scratch(void)
 
 	/* ODMDATA is for kernel use to determine RAM size, LP config, etc. */
 	writel(CONFIG_SYS_BOARD_ODMDATA, &pmc->pmc_scratch20);
+
+#ifdef CONFIG_TEGRA2_LP0
+	/* save Sdram params to PMC 2, 4, and 24 for WB0 */
+	warmboot_save_sdram_params();
+#endif
 }
 
 void tegra2_start(void)
