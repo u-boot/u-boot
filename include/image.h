314f634b488ff6345d20a0ed05f95a990021adb8
Mike Frysinger <vapier@gentoo.org>
2012-04-22 06:59:06 +0000

    image/fit: drop inline markings on parser code
    Putting "inline" on extern funcs makes no sense, so drop them.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- include/image.h
+++ include/image.h
@@ -531,9 +531,9 @@ static inline int image_check_target_arch(const image_header_t *hdr)
 #define FIT_MAX_HASH_LEN	20	/* max(crc32_len(4), sha1_len(20)) */
 
 /* cmdline argument format parsing */
-inline int fit_parse_conf(const char *spec, ulong addr_curr,
+int fit_parse_conf(const char *spec, ulong addr_curr,
 		ulong *addr, const char **conf_name);
-inline int fit_parse_subimage(const char *spec, ulong addr_curr,
+int fit_parse_subimage(const char *spec, ulong addr_curr,
 		ulong *addr, const char **image_name);
 
 void fit_print_contents(const void *fit);
