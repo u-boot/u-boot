f1e445c3c94aaab910b4588ec4c8934fd9edd86a
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2012-04-25 02:34:31 +0000

    igep00x0: change mpurate from 500 to auto
    This patch changes the default mpurate variable from 500 to auto on
    all IGEP boards, with this the default rate is autoselected.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@iseebcn.com>
    Signed-off-by: Anatolij Gustschin <agust@denx.de>

--- include/configs/igep00x0.h
+++ include/configs/igep00x0.h
@@ -136,7 +136,7 @@
 	"loadaddr=0x82000000\0" \
 	"usbtty=cdc_acm\0" \
 	"console=ttyS2,115200n8\0" \
-	"mpurate=500\0" \
+	"mpurate=auto\0" \
 	"vram=12M\0" \
 	"dvimode=1024x768MR-16@60\0" \
 	"defaultdisplay=dvi\0" \
