6f3b300c0a324725e468f496f89372388ff5ee66
Nikita Kiryanov <nikita@compulab.co.il>
2012-05-24 04:01:22 +0000

    cm-t35: fix incorrect BOARD_REV_SIZE value
    Non-legacy layouts have an extended revision field,
    but only the first 2 bytes are the PCB revision.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- board/cm_t35/eeprom.c
+++ board/cm_t35/eeprom.c
@@ -27,8 +27,7 @@
 #define BOARD_SERIAL_OFFSET_LEGACY	8
 #define BOARD_REV_OFFSET		0
 #define BOARD_REV_OFFSET_LEGACY		6
-#define BOARD_REV_SIZE			4
-#define BOARD_REV_SIZE_LEGACY		2
+#define BOARD_REV_SIZE			2
 #define MAC_ADDR_OFFSET			4
 #define MAC_ADDR_OFFSET_LEGACY		0
 
@@ -107,17 +106,14 @@ u32 get_board_rev(void)
 {
 	u32 rev = 0;
 	uint offset = BOARD_REV_OFFSET_LEGACY;
-	int len = BOARD_REV_SIZE_LEGACY;
 
 	if (eeprom_setup_layout())
 		return 0;
 
-	if (eeprom_layout != LAYOUT_LEGACY) {
+	if (eeprom_layout != LAYOUT_LEGACY)
 		offset = BOARD_REV_OFFSET;
-		len = BOARD_REV_SIZE;
-	}
 
-	if (cm_t3x_eeprom_read(offset, (uchar *)&rev, len))
+	if (cm_t3x_eeprom_read(offset, (uchar *)&rev, BOARD_REV_SIZE))
 		return 0;
 
 	return rev;
