f3f98bb0b8cc520e08ea2bdfc3f9cbe4e4ac29f5
SRICHARAN R <r.sricharan@ti.com>
2012-06-12 19:53:33 +0000

    ARM: OMAP4/5: Do not configure non essential pads, clocks, dplls.
    Currently on OMAP4/5 platforms, many kernel drivers are dependent
    upon the bootloaders for mux, dpll and clock configurations.
    This should not be the case and bootloaders should set only the
    minimum required for the uboot functionality and kernel boot.
    
    Note that this is going to break the kernel drivers. But this
    is the only way to get things fixed in the kernel.
    
    Signed-off-by: R Sricharan <r.sricharan@ti.com>

--- include/configs/omap4_common.h
+++ include/configs/omap4_common.h
@@ -120,9 +120,6 @@
 /* Flash */
 #define CONFIG_SYS_NO_FLASH	1
 
-/* clocks */
-#define CONFIG_SYS_CLOCKS_ENABLE_ALL
-
 /* commands to include */
 #include <config_cmd_default.h>
 
@@ -282,8 +279,6 @@
 #define CONFIG_SPL_LIBGENERIC_SUPPORT
 #define CONFIG_SPL_SERIAL_SUPPORT
 #define CONFIG_SPL_LDSCRIPT "$(CPUDIR)/omap-common/u-boot-spl.lds"
-
-#define CONFIG_SYS_ENABLE_PADS_ALL
 
 #define CONFIG_SYS_THUMB_BUILD
 
