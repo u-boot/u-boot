4fb44e22ec3ddc48b730865cf2c00e98224cee99
Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
2012-06-13 16:29:47 +0900

    arm: rmobile: Add basic support for Renesas R-Mobile
    This patch adds minimum support for R-Mobile. Only minimal support with timer.
    This CPU can uses the peripheral of Renesas SuperH.
    
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

---
+++ arch/arm/include/asm/arch-rmobile/rmobile.h
@@ -0,0 +1,8 @@
+#ifndef __ASM_ARCH_RMOBILE_H
+#define __ASM_ARCH_RMOBILE_H
+
+#if defined(CONFIG_RMOBILE)
+#error "SOC Name not defined"
+#endif /* CONFIG_RMOBILE */
+
+#endif /* __ASM_ARCH_RMOBILE_H */
