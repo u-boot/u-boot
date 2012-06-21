8d811ca36a2a4096dd3ed0d64f2a22a247400737
Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
2012-06-21 14:55:07 +0900

    arm: rmobile: Add supoprt for KMC KZM-A9-GT board
    The KZM-A9-GT board has Renesas R-Mobile SH73A0, 512MB DDR2-SDRAM,
    USB, Ethernet, and more.
    
    This patch supports the following functions:
    	- 512MB DDR2-SDRAM
    	- 16MB NOR Flash memory
    	- Serial console (SCIF)
    	- Ethernet (SMSC)
    	- I2C
    
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -1105,6 +1105,7 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_UBISYS_P9D_EVP       3493
 #define MACH_TYPE_ATDGP318             3494
 #define MACH_TYPE_OMAP5_SEVM           3777
+#define MACH_TYPE_KZM9G                4140
 
 #ifdef CONFIG_ARCH_EBSA110
 # ifdef machine_arch_type
@@ -14220,6 +14221,18 @@ extern unsigned int __machine_arch_type;
 # define machine_is_omap5_sevm()      (machine_arch_type == MACH_TYPE_OMAP5_SEVM)
 #else
 # define machine_is_omap5_sevm()      (0)
+#endif
+
+#ifdef CONFIG_MACH_KZM9G
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type __machine_arch_type
+# else
+#  define machine_arch_type MACH_TYPE_KZM9G
+# endif
+# define machine_is_kzm9g()	(machine_arch_type == MACH_TYPE_KZM9G)
+#else
+# define machine_is_kzm9g()	(0)
 #endif
 
 /*
