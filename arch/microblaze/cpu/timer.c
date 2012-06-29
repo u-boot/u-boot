779bf42c2a278b2a2f8a4d9e4869c237d0a8742a
Michal Simek <monstr@monstr.eu>
2012-06-29 13:42:28 +0200

    microblaze: Move __udelay implementation
    Move __udelay to the timer code because of unification.
    And clean coding style because of checkpatch.pl.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>
    Acked-by: Simon Glass <sjg@chromium.org>

--- arch/microblaze/cpu/timer.c
+++ arch/microblaze/cpu/timer.c
@@ -42,6 +42,25 @@ ulong get_timer (ulong base)
 
 #ifdef CONFIG_SYS_INTC_0
 #ifdef CONFIG_SYS_TIMER_0
+void __udelay(unsigned long usec)
+{
+	int i;
+
+	i = get_timer(0);
+	while ((get_timer(0) - i) < (usec / 1000))
+		;
+}
+#else
+void __udelay(unsigned long usec)
+{
+	unsigned int i;
+
+	for (i = 0; i < (usec * CONFIG_XILINX_CLOCK_FREQ / 10000000); i++)
+		;
+}
+#endif
+
+#ifdef CONFIG_SYS_TIMER_0
 microblaze_timer_t *tmr = (microblaze_timer_t *) (CONFIG_SYS_TIMER_0_ADDR);
 
 void timer_isr (void *arg)
