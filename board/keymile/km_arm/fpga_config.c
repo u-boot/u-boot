dbdee4ca591a3bf19446d8ab89285835e210b378
Valentin Longchamp <valentin.longchamp@keymile.com>
2012-07-05 05:05:09 +0000

    arm/km: support the 2 PCIe fpga resets
    The PCIe FPGAs now have to support 2 resets: one for the non traffic
    affecting part (PCIe) and one for the traffic affecting part.
    
    When the FPGA is not reconfigured, we only reset the PCIe part.
    
    Signed-off-by: Valentin Longchamp <valentin.longchamp@keymile.com>

--- board/keymile/km_arm/fpga_config.c
+++ board/keymile/km_arm/fpga_config.c
@@ -99,7 +99,7 @@ static int boco_set_bits(u8 reg, u8 flags)
 #define FPGA_INIT_B	0x10
 #define FPGA_DONE	0x20
 
-static int fpga_done()
+static int fpga_done(void)
 {
 	int ret = 0;
 	u8 regval;
@@ -206,25 +206,30 @@ int wait_for_fpga_config(void)
 }
 
 #define PRST1		0x4
-#define BRIDGE_RST	0x4
+#define PCIE_RST	0x10
+#define TRAFFIC_RST	0x04
 
 int fpga_reset(void)
 {
 	int ret = 0;
+	u8 resets;
 
 	if (!check_boco2()) {
 		/* we do not have BOCO2, this is not really used */
 		return 0;
 	}
 
-	ret = boco_clear_bits(PRST1, BRIDGE_RST);
+	/* if we have skipped, we only want to reset the PCIe part */
+	resets = skip ? PCIE_RST : PCIE_RST | TRAFFIC_RST;
+
+	ret = boco_clear_bits(PRST1, resets);
 	if (ret)
 		return ret;
 
 	/* small delay for the pulse */
 	udelay(10);
 
-	ret = boco_set_bits(PRST1, BRIDGE_RST);
+	ret = boco_set_bits(PRST1, resets);
 	if (ret)
 		return ret;
 
