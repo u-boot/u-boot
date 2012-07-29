312693c3dd280a73824c6403e249775ccb7c3547
Jim Lin <jilin@nvidia.com>
2012-07-29 20:53:29 +0000

    tegra: nand: Add Tegra NAND driver
    A device tree is used to configure the NAND, including memory
    timings and block/pages sizes.
    
    If this node is not present or is disabled, then NAND will not
    be initialized.
    
    Signed-off-by: Jim Lin <jilin@nvidia.com>
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- lib/fdtdec.c
+++ lib/fdtdec.c
@@ -42,6 +42,7 @@ static const char * const compat_names[COMPAT_COUNT] = {
 	COMPAT(NVIDIA_TEGRA20_EMC, "nvidia,tegra20-emc"),
 	COMPAT(NVIDIA_TEGRA20_EMC_TABLE, "nvidia,tegra20-emc-table"),
 	COMPAT(NVIDIA_TEGRA20_KBC, "nvidia,tegra20-kbc"),
+	COMPAT(NVIDIA_TEGRA20_NAND, "nvidia,tegra20-nand"),
 };
 
 const char *fdtdec_get_compatible(enum fdt_compat_id id)
