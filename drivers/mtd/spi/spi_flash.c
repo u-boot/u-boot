58c08da6192497b2fb681ca119ee39c0ce061f8a
Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
2012-08-06 20:55:35 +0530

    Xilinx: ARM: sf: Enable prints on erase and write functions
    This patch provides to enabled the prints on erase and write
    functions to make sure that user to know the functionality.
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>

--- drivers/mtd/spi/spi_flash.c
+++ drivers/mtd/spi/spi_flash.c
@@ -122,7 +122,7 @@ int spi_flash_cmd_write_multi(struct spi_flash *flash, u32 offset,
 		byte_addr = 0;
 	}
 
-	debug("SF: program %s %zu bytes @ %#x\n",
+	printf("SF: program %s %zu bytes @ %#x\n",
 	      ret ? "failure" : "success", len, offset);
 
 	spi_release_bus(flash->spi);
@@ -249,7 +249,7 @@ int spi_flash_cmd_erase(struct spi_flash *flash, u8 erase_cmd,
 			goto out;
 	}
 
-	debug("SF: Successfully erased %zu bytes @ %#x\n", len, start);
+	printf("SF: Successfully erased %zu bytes @ %#x\n", len, start);
 
  out:
 	spi_release_bus(flash->spi);
