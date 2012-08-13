f34819960692ef00c363abd98af5fea96f41c7dc
Otavio Salvador <otavio@ossystems.com.br>
2012-08-13 09:53:09 +0000

    mxs: Remove not required explicit iomux-mx28.h include
    The iomux header is included on sys_proto.h so to avoid SoC specific
    header inclusion.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
    Acked-by: Marek Vasut <marex@denx.de>

--- arch/arm/cpu/arm926ejs/mxs/spl_boot.c
+++ arch/arm/cpu/arm926ejs/mxs/spl_boot.c
@@ -26,7 +26,6 @@
 #include <common.h>
 #include <config.h>
 #include <asm/io.h>
-#include <asm/arch/iomux-mx28.h>
 #include <asm/arch/imx-regs.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/gpio.h>
