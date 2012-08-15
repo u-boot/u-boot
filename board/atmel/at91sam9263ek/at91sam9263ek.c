82c4c6452604546a022c4cbb90cd7162c04557d2
Andreas Bießmann <andreas.devel@googlemail.com>
2012-08-15 22:18:56 +0000

    at91sam9263ek: remove unnecessary console_init_f
    Signed-off-by: Andreas Bießmann <andreas.devel@googlemail.com>
    cc: Stelian Pop <stelian@popies.net>

--- board/atmel/at91sam9263ek/at91sam9263ek.c
+++ board/atmel/at91sam9263ek/at91sam9263ek.c
@@ -260,9 +260,6 @@ int board_early_init_f(void)
 
 int board_init(void)
 {
-	/* Enable Ctrlc */
-	console_init_f();
-
 	/* arch number of AT91SAM9263EK-Board */
 	gd->bd->bi_arch_number = MACH_TYPE_AT91SAM9263EK;
 	/* adress of boot parameters */
