11e6e95774ca352cf85d678e806657a21324a9e0
Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
2012-08-17 10:02:19 +0530

    Xilinx: ARM: Add license notes on board configs
    This patch adds license notes on all zynq board configurations
    Now the license notes resemble the license notes as on xilinx linux git.
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>

--- include/configs/zynq_ep107.h
+++ include/configs/zynq_ep107.h
@@ -1,8 +1,18 @@
 /*
- * Hacked together,
- * hopefully functional.
+ * (C) Copyright 2012 Xilinx
  *
+ * Configuration settings for the Xilinx Zynq EP107 board.
  * See zynq_common.h for Zynq common configs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
  */
 
 #ifndef __CONFIG_ZYNQ_EP107_H
--- include/configs/zynq_zed.h
+++ include/configs/zynq_zed.h
@@ -1,7 +1,18 @@
 /*
- * Configuration for Zynq Evaluation and Development Board - ZedBoard
+ * (C) Copyright 2012 Xilinx
  *
+ * Configuration for Zynq Evaluation and Development Board - ZedBoard
  * See zynq_common.h for Zynq common configs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
  */
 
 #ifndef __CONFIG_ZYNQ_ZED_H
