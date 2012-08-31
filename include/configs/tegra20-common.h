7e5fd8fb805f553d2399f0afa8cdf14da838afce
Allen Martin <amartin@nvidia.com>
2012-08-31 08:30:02 +0000

    tegra20: rename CONFIG_MACH_TEGRA_GENERIC
    Rename CONFIG_MACH_TEGRA_GENERIC to the less confusing CONFIG_TEGRA.
    The meaning of the config options is now:
    
    CONFIG_TEGRA - Any tegra chip
    CONFIG_TEGRA20 - A tegra20 family chip
    CONFIG_TEGRA30 - A tegra30 family chip (not added yet)
    
    Signed-off-by: Allen Martin <amartin@nvidia.com>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Tested-by: Thierry Reding <thierry.reding@avionic-design.de>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- include/configs/tegra20-common.h
+++ include/configs/tegra20-common.h
@@ -38,7 +38,7 @@
  */
 #define CONFIG_ARMCORTEXA9		/* This is an ARM V7 CPU core */
 #define CONFIG_TEGRA20			/* in a NVidia Tegra20 core */
-#define CONFIG_MACH_TEGRA_GENERIC	/* which is a Tegra generic machine */
+#define CONFIG_TEGRA			/* which is a Tegra generic machine */
 #define CONFIG_SYS_L2CACHE_OFF		/* No L2 cache */
 
 #define CONFIG_SYS_CACHELINE_SIZE	32
