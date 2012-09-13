185f7d9afc9b5c77bbedc386a43019e07e5d0c04
Michal Simek <monstr@monstr.eu>
2012-09-13 20:23:34 +0000

    net: Add driver for Zynq Gem IP
    Device driver for Zynq Gem IP.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>
    CC: Joe Hershberger <joe.hershberger@gmail.com>
    CC: Marek Vasut <marex@denx.de>
    Acked-by: Marek Vasut <marex@denx.de>

--- include/netdev.h
+++ include/netdev.h
@@ -104,7 +104,7 @@ int xilinx_emaclite_initialize(bd_t *bis, unsigned long base_addr,
 							int txpp, int rxpp);
 int xilinx_ll_temac_eth_init(bd_t *bis, unsigned long base_addr, int flags,
 						unsigned long ctrl_addr);
-
+int zynq_gem_initialize(bd_t *bis, int base_addr);
 /*
  * As long as the Xilinx xps_ll_temac ethernet driver has not its own interface
  * exported by a public hader file, we need a global definition at this point.
