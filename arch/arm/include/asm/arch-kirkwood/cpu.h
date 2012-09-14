7029394d822a7d9e8ccb12d9679030b462e1052f
Dinh Nguyen <dinguyen@altera.com>
2012-09-14 11:08:28 +0000

    ARM: kirkwood/orion5x: Use reset_cpu definition in include/common.h
    include/common.h has the reset_cpu defined already. No need to
    re-define here.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    Signed-off-by: Pavel Machek <pavel@denx.de>
    Acked-By: Prafulla Wadaskar <Prafulla@marvell.com>

--- arch/arm/include/asm/arch-kirkwood/cpu.h
+++ arch/arm/include/asm/arch-kirkwood/cpu.h
@@ -155,7 +155,6 @@ struct kwgpio_registers {
 /*
  * functions
  */
-void reset_cpu(unsigned long ignored);
 unsigned char get_random_hex(void);
 unsigned int kw_sdram_bar(enum memory_bank bank);
 unsigned int kw_sdram_bs(enum memory_bank bank);
