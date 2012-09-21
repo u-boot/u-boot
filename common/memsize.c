3ab270d5df7ae7bf8d7b5d225ab1abb37297d77e
Tom Rini <trini@ti.com>
2012-09-21 08:14:05 -0700

    Revert "memsize: Fix for bug in memory sizing code"
    After further investigation this change is not correct and results in
    get_ram_size returning incorrectly large values.  Reported first by
    Gerlando Falauto.
    
    This reverts commit b8496cced856ff411f1eb2e4eff20f5abe7080b0.
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- common/memsize.c
+++ common/memsize.c
@@ -73,7 +73,6 @@ long get_ram_size(long *base, long maxsize)
 		}
 		return (0);
 	}
-	*addr = save[i];
 
 	for (cnt = 1; cnt < maxsize / sizeof (long); cnt <<= 1) {
 		addr = base + cnt;	/* pointer arith! */
