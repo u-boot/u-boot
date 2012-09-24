7dd6545da75d9194b4e59a183012c1117e6241cc
Fabio Estevam <fabio.estevam@freescale.com>
2012-09-24 08:09:33 +0000

    mx6q: Add basic support for mx6qsabreauto
    mx6qsabreauto is a board based on mx6q SoC with the following features:
    - 2GB of DDR3
    - 2 USB ports
    - 1 HDMI output port
    - SPI NOR
    - 2 LVDS LCD ports
    - Gigabit Ethernet
    - Camera
    - eMMC and SD card slot
    - Multichannel Audio
    - CAN
    - SATA
    - NAND
    - PCIE
    - Video Input
    
    Add very basic support for it.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>

---
+++ include/configs/mx6qsabreauto.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2012 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Freescale i.MX6Q SabreSD board.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+#ifndef __MX6QSABREAUTO_CONFIG_H
+#define __MX6QSABREAUTO_CONFIG_H
+#include "mx6qsabre_common.h"
+
+#define CONFIG_MACH_TYPE	3529
+#define CONFIG_MXC_UART_BASE	UART4_BASE
+#define PHYS_SDRAM_SIZE		(2u * 1024 * 1024 * 1024)
+
+#endif                         /* __MX6QSABREAUTO_CONFIG_H */
