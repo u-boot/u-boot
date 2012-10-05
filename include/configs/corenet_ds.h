11860d888cdc2be5da7b8738a067b84cd76e71c0
Timur Tabi <timur@freescale.com>
2012-10-05 09:48:53 +0000

    powerpc/85xx: move SRIO configuration out of corenet_ds.h
    The P5040 does not have SRIO, so don't put the SRIO definitions in
    corenet_ds.h.  They belong in the board-specific header files.
    
    Signed-off-by: Timur Tabi <timur@freescale.com>
    Signed-off-by: Andy Fleming <afleming@freescale.com>

--- include/configs/corenet_ds.h
+++ include/configs/corenet_ds.h
@@ -76,10 +76,6 @@
 #define CONFIG_FSL_PCI_INIT		/* Use common FSL init code */
 #define CONFIG_SYS_PCI_64BIT		/* enable 64-bit PCI resources */
 
-#define CONFIG_SYS_SRIO
-#define CONFIG_SRIO1			/* SRIO port 1 */
-#define CONFIG_SRIO2			/* SRIO port 2 */
-
 #define CONFIG_FSL_LAW			/* Use common FSL init code */
 
 #define CONFIG_ENV_OVERWRITE
