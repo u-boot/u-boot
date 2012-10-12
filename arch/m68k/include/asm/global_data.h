f5a5b3c5eef09785ec3d079e84e9f92725a13cd8
Simon Glass <sjg@chromium.org>
2012-10-12 14:21:10 +0000

    m68k: Change bi_baudrate and global data baudrate to int
    These don't need to be longs, so change them.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Rini <trini@ti.com>

--- arch/m68k/include/asm/global_data.h
+++ arch/m68k/include/asm/global_data.h
@@ -34,7 +34,7 @@
 typedef	struct	global_data {
 	bd_t		*bd;
 	unsigned long	flags;
-	unsigned long	baudrate;
+	unsigned int	baudrate;
 	unsigned long	cpu_clk;	/* CPU clock in Hz!		*/
 	unsigned long	bus_clk;
 #ifdef CONFIG_PCI
