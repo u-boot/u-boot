4576c6e6f85c2d8a14e0219a87f54b4c59f55395
Mayuresh Kulkarni <mkulkarni@nvidia.com>
2012-10-17 13:25:00 +0000

    tegra: Enable display/lcd support on Seaboard
    Enable the Seaboard's 16-bit LCD and use it as the console.
    
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- include/configs/seaboard.h
+++ include/configs/seaboard.h
@@ -54,6 +54,7 @@
 #define CONFIG_MACH_TYPE		MACH_TYPE_SEABOARD
 
 #define CONFIG_BOARD_EARLY_INIT_F
+#define CONFIG_BOARD_LATE_INIT		/* Make sure LCD init is complete */
 
 /* I2C */
 #define CONFIG_TEGRA_I2C
@@ -101,8 +102,16 @@
 
 #undef TEGRA_DEVICE_SETTINGS
 #define TEGRA_DEVICE_SETTINGS	"stdin=serial,tegra-kbc\0" \
-				"stdout=serial\0" \
-				"stderr=serial\0"
+				"stdout=serial,lcd\0" \
+				"stderr=serial,lcd\0"
+
+/* LCD support */
+#define CONFIG_LCD
+#define CONFIG_PWM_TEGRA
+#define CONFIG_VIDEO_TEGRA
+#define LCD_BPP				LCD_COLOR16
+#define CONFIG_SYS_WHITE_ON_BLACK
+#define CONFIG_CONSOLE_SCROLL_LINES	10
 
 #include "tegra-common-post.h"
 
