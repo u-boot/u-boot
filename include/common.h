c4fa493d3a822757af8fbcdc516e12bb7f72d071
Simon Glass <sjg@chromium.org>
2012-10-29 05:23:47 +0000

    Support setenv_ulong() and setenv_addr() for powerpc
    This includes were outside an #ifdef CONFIG_PPC, but there is not reason
    to exclude powerpc from using them.
    
    Move the declaration outside the #ifdef.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/common.h
+++ include/common.h
@@ -345,9 +345,9 @@ int	saveenv	     (void);
 int inline setenv    (const char *, const char *);
 #else
 int	setenv	     (const char *, const char *);
+#endif /* CONFIG_PPC */
 int setenv_ulong(const char *varname, ulong value);
 int setenv_addr(const char *varname, const void *addr);
-#endif /* CONFIG_PPC */
 #ifdef CONFIG_ARM
 # include <asm/mach-types.h>
 # include <asm/setup.h>
