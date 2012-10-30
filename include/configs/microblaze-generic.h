c2219d787bf9eca259517a52e363aa36d2a2329e
John Linn <john.linn@xilinx.com>
2012-10-30 09:20:25 -0700

    microblaze: Do not use device tree cfg as default
    The generic configuration for Microblaze was using device tree
    to configure u-boot such that it required a device tree by
    the user. This is an experimental feature still and not ready
    for production such that it makes it harder for users.
    
    This is not to be confused with the ability to load a device
    tree and pass it to Linux as that ability still exists.
    
    Signed-off-by: John Linn <john.linn@xilinx.com>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- include/configs/microblaze-generic.h
+++ include/configs/microblaze-generic.h
@@ -31,11 +31,6 @@
 #define	CONFIG_MICROBLAZE	1
 #define	MICROBLAZE_V5		1
 
-/* Open Firmware DTS */
-#define CONFIG_OF_CONTROL	1
-#define CONFIG_OF_EMBED		1
-#define CONFIG_DEFAULT_DEVICE_TREE microblaze
-
 /* linear and spi flash memory */
 #ifdef XILINX_FLASH_START
 #define	FLASH
