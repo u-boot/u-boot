418b7f3aff3bf4c508b2a57ffc6dc7ca10f811aa
Stephen Warren <swarren@nvidia.com>
2012-10-31 11:17:28 +0000

    Add board_name to CONFIG_ENV_VARS_UBOOT_CONFIG
    CONFIG_ENV_VARS_UBOOT_CONFIG creates environment variables indicating
    which configuration U-Boot was built for. Some U-Boot binaries run on
    multiple boards, and hence this information may not uniquley describe
    the HW that U-Boot is actually running on. Another patch introduces
    environment variable board_name to represent that. In order to avoid
    scripts having to check $board_name, use it if set, and then fall back
    to using $board, make CONFIG_ENV_VARS_UBOOT_CONFIG also set a default
    value for board_name, so that variable is always available.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- include/env_default.h
+++ include/env_default.h
@@ -118,6 +118,7 @@ const uchar default_environment[] = {
 	"arch="		CONFIG_SYS_ARCH			"\0"
 	"cpu="		CONFIG_SYS_CPU			"\0"
 	"board="	CONFIG_SYS_BOARD		"\0"
+	"board_name="	CONFIG_SYS_BOARD		"\0"
 #ifdef CONFIG_SYS_VENDOR
 	"vendor="	CONFIG_SYS_VENDOR		"\0"
 #endif
