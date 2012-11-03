688c2d140bcd457210a279dc489825e75ab1743a
Mela Custodio <sessyargc@gmail.com>
2012-11-03 17:40:16 +0000

    mmc: add no simultaenous power and vdd
    Bring in the code from Linux kernel.
    
    Added to Linux kernel by:
    commit e08c1694d9e2138204f2b79b73f0f159074ce2f5
    Author: Andres Salomon <dilinger@queued.net>
    Date:   Fri Jul 4 10:00:03 2008 -0700
    
    Some HW balks when writing both voltage setting and power up at the same
    time to SDHCI_POWER_CONTROL register.
    
    Signed-off-by: Rommel G Custodio <sessyargc@gmail.com>
    CC: Andy Fleming <afleming@freescale.com>
    
    v2: fix attribution and SOB
    Signed-off-by: Andy Fleming <afleming@freescale.com>

--- drivers/mmc/sdhci.c
+++ drivers/mmc/sdhci.c
@@ -340,6 +340,9 @@ static void sdhci_set_power(struct sdhci_host *host, unsigned short power)
 		return;
 	}
 
+	if (host->quirks & SDHCI_QUIRK_NO_SIMULT_VDD_AND_POWER)
+		sdhci_writeb(host, pwr, SDHCI_POWER_CONTROL);
+
 	pwr |= SDHCI_POWER_ON;
 
 	sdhci_writeb(host, pwr, SDHCI_POWER_CONTROL);
