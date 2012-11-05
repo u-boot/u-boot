25920757765e22bdebf8e6fb1f777e21a31c9c21
Simon Glass <sjg@chromium.org>
2012-11-05 12:16:24 +0000

    cbfs: Remove mention of CREDITS files
    As requested by Wolfgang, remove references to CREDITS in the CBFS
    files.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- fs/cbfs/cbfs.c
+++ fs/cbfs/cbfs.c
@@ -1,9 +1,6 @@
 /*
  * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
