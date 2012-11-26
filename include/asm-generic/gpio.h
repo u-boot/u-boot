94740e473920f5f0c71f949281db8dcb132e27cd
Nikita Kiryanov <nikita@compulab.co.il>
2012-11-26 23:06:32 +0000

    gpio: remove duplicate function signature
    gpio_request() appears twice in asm-generic/gpio.h
    Remove one of the definitions.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>

--- include/asm-generic/gpio.h
+++ include/asm-generic/gpio.h
@@ -42,10 +42,11 @@
  */
 
 /**
- * Request ownership of a GPIO.
+ * Request a gpio. This should be called before any of the other functions
+ * are used on this gpio.
  *
- * @param gpio	GPIO number
- * @param label	Name given to the GPIO
+ * @param gp	GPIO number
+ * @param label	User label for this GPIO
  * @return 0 if ok, -1 on error
  */
 int gpio_request(unsigned gpio, const char *label);
@@ -93,14 +94,4 @@ int gpio_get_value(unsigned gpio);
  * @return 0 if ok, -1 on error
  */
 int gpio_set_value(unsigned gpio, int value);
-
-/**
- * Request a gpio. This should be called before any of the other functions
- * are used on this gpio.
- *
- * @param gp	GPIO number
- * @param label	User label for this GPIO
- * @return 0 if ok, -1 on error
- */
-int gpio_request(unsigned gpio, const char *label);
 #endif	/* _ASM_GENERIC_GPIO_H_ */
