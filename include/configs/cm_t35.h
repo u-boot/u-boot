854a7836dc10d9ff818ba9eb4265538acd037cd9
Nikita Kiryanov <nikita@compulab.co.il>
2012-12-02 13:59:19 +0200

    cm-t35: add USB host support
    Add support for USB host ports on cm-t3530 and cm-t3730.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- include/configs/cm_t35.h
+++ include/configs/cm_t35.h
@@ -114,9 +114,15 @@
 #define CONFIG_DOS_PARTITION
 
 /* USB */
-#define CONFIG_MUSB_UDC
 #define CONFIG_USB_OMAP3
+#define CONFIG_USB_EHCI
+#define CONFIG_USB_EHCI_OMAP
+#define CONFIG_USB_ULPI
+#define CONFIG_USB_ULPI_VIEWPORT_OMAP
+#define CONFIG_USB_STORAGE
+#define CONFIG_MUSB_UDC
 #define CONFIG_TWL4030_USB
+#define CONFIG_CMD_USB
 
 /* USB device configuration */
 #define CONFIG_USB_DEVICE
