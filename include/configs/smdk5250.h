f1cbca2be56e53f4cb7d613de13bb311c1d79730
Hatim RV <hatim.rv@samsung.com>
2012-12-11 00:52:49 +0000

    SMDK5250: config: Add configuration file for SMDK5250 board
    Add the configuration file for exynos5250 based SMDK5250 board.
    
    Signed-off-by: Hatim Ali <hatim.rv@samsung.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

---
+++ include/configs/smdk5250.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2012 Samsung Electronics
+ *
+ * Configuration settings for the SAMSUNG SMDK5250 board.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_SMDK_H
+#define __CONFIG_SMDK_H
+
+#include <configs/exynos5250-dt.h>
+
+#undef CONFIG_DEFAULT_DEVICE_TREE
+#define CONFIG_DEFAULT_DEVICE_TREE	exynos5250-smdk5250
+
+#endif	/* __CONFIG_SMDK_H */
