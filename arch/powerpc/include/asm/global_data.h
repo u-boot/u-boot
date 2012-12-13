923a662f2fb09aa67c1ec0de25474c218fad2690
Simon Glass <sjg@chromium.org>
2012-12-13 20:49:02 +0000

    ppc: Move fpga_state to arch_global_data
    Move this field into arch_global_data and tidy up.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/powerpc/include/asm/global_data.h
+++ arch/powerpc/include/asm/global_data.h
@@ -122,6 +122,9 @@ struct arch_global_data {
 #if defined(CONFIG_SYS_GT_6426x)
 	unsigned int mirror_hack[16];
 #endif
+#ifdef CONFIG_SYS_FPGA_COUNT
+	unsigned fpga_state[CONFIG_SYS_FPGA_COUNT];
+#endif
 };
 
 /*
@@ -169,9 +172,6 @@ typedef	struct	global_data {
 #endif
 #if defined(CONFIG_LWMON) || defined(CONFIG_LWMON5)
 	unsigned long kbd_status;
-#endif
-#ifdef CONFIG_SYS_FPGA_COUNT
-	unsigned fpga_state[CONFIG_SYS_FPGA_COUNT];
 #endif
 #if defined(CONFIG_WD_MAX_RATE)
 	unsigned long long wdt_last;	/* trace watch-dog triggering rate */
