e3c52f2b8779469c843eb79282396f1a5ca3fef5
Tom Rini <trini@ti.com>
2012-12-20 07:30:27 -0700

    Revert "fw_env: fix building w/out a config.h"
    I had missed Joe's NAK on this patch, so...
    
    This reverts commit 92ace272d06cec1d7f1533bb9edf914fb5845fba.
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- tools/env/fw_env.h
+++ tools/env/fw_env.h
@@ -21,6 +21,15 @@
  * MA 02111-1307 USA
  */
 
+/* Pull in the current config to define the default environment */
+#ifndef __ASSEMBLY__
+#define __ASSEMBLY__ /* get only #defines from config.h */
+#include <config.h>
+#undef	__ASSEMBLY__
+#else
+#include <config.h>
+#endif
+
 /*
  * To build the utility with the static configuration
  * comment out the next line.
@@ -41,6 +50,22 @@
 #define ENV2_SIZE         0x4000
 #define DEVICE2_ESIZE     0x4000
 #define DEVICE2_ENVSECTORS     2
+#endif
+
+#ifndef CONFIG_BAUDRATE
+#define CONFIG_BAUDRATE		115200
+#endif
+
+#ifndef CONFIG_BOOTDELAY
+#define CONFIG_BOOTDELAY	5	/* autoboot after 5 seconds	*/
+#endif
+
+#ifndef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND							\
+	"bootp; "								\
+	"setenv bootargs root=/dev/nfs nfsroot=${serverip}:${rootpath} "	\
+	"ip=${ipaddr}:${serverip}:${gatewayip}:${netmask}:${hostname}::off; "	\
+	"bootm"
 #endif
 
 extern int   fw_printenv(int argc, char *argv[]);
