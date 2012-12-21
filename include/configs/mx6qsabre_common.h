2f994fe622dcb01a87d83eacee5df0b29f33d306
Otavio Salvador <otavio@ossystems.com.br>
2012-12-21 06:59:07 +0000

    mx6qsabre_common: Change default loadaddr to 0x12000000
    This allow use of mainline and Freescale BSP Linux kernel with same
    environment.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx6qsabre_common.h
+++ include/configs/mx6qsabre_common.h
@@ -78,7 +78,7 @@
 
 #define CONFIG_BOOTDELAY               1
 
-#define CONFIG_LOADADDR                        0x10800000
+#define CONFIG_LOADADDR                        0x12000000
 #define CONFIG_SYS_TEXT_BASE           0x17800000
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
