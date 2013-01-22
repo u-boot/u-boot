f10b4c0e650e9a7e8ff4631c3dd16e84b0d6fb6a
Rajeshwari Shinde <rajeshwari.s@samsung.com>
2013-01-22 20:30:18 +0000

    SF: Add driver for Gigabyte device GD25LQ and GD25Q64B
    This patch adds driver for the gigabyte devices
    GD25LQ and GD25Q64B required for Snow Board.
    
    Signed-off-by: Rajeshwari Shinde <rajeshwari.s@samsung.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Tom Rini <trini@ti.com>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- drivers/mtd/spi/spi_flash_internal.h
+++ drivers/mtd/spi/spi_flash_internal.h
@@ -106,3 +106,4 @@ struct spi_flash *spi_flash_probe_sst(struct spi_slave *spi, u8 *idcode);
 struct spi_flash *spi_flash_probe_stmicro(struct spi_slave *spi, u8 *idcode);
 struct spi_flash *spi_flash_probe_winbond(struct spi_slave *spi, u8 *idcode);
 struct spi_flash *spi_fram_probe_ramtron(struct spi_slave *spi, u8 *idcode);
+struct spi_flash *spi_flash_probe_gigadevice(struct spi_slave *spi, u8 *idcode);
