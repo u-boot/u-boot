e70fb539a634ab86153353aec8a9c1fede818eb0
Michal Simek <michal.simek@xilinx.com>
2013-01-22 23:40:06 +0000

    serial: arm_dcc: Remove CONFIG_ARM_DCC_MULTI option
    CONFIG_ARM_DCC_MULTI should be also removed in the patch
    "serial: Remove CONFIG_SERIAL_MULTI from serial drivers"
    (sha1: a3827250606895ec2dd4b8d867342b7cabf3692f)
    Because the driver defines serial_* functions
    which cause conflict with serial.c (multiple definition of serial_*)
    
    Removing CONFIG_SERIAL_MULTI function also require to define
    default_serial_console for cases where another serial driver
    is not available in the system.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Marek Vasut <marex@denx.de>

--- include/stdio_dev.h
+++ include/stdio_dev.h
@@ -99,7 +99,7 @@ struct list_head* stdio_get_list(void);
 struct stdio_dev* stdio_get_by_name(const char* name);
 struct stdio_dev* stdio_clone(struct stdio_dev *dev);
 
-#ifdef CONFIG_ARM_DCC_MULTI
+#ifdef CONFIG_ARM_DCC
 int drv_arm_dcc_init(void);
 #endif
 #ifdef CONFIG_LCD
