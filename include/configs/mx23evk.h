c4c25940b9bf61526862ec7f1d2e99141c3a4c0a
Fabio Estevam <fabio.estevam@freescale.com>
2013-01-28 01:41:00 +0000

    mx23evk: Remove CONFIG_SYS_BAUDRATE_TABLE
    The baudrate is already defined by CONFIG_BAUDRATE and there is no need
    to keep CONFIG_SYS_BAUDRATE_TABLE.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx23evk.h
+++ include/configs/mx23evk.h
@@ -112,7 +112,6 @@
 #define CONFIG_PL01x_PORTS		{ (void *)MXS_UARTDBG_BASE }
 #define CONFIG_CONS_INDEX		0
 #define CONFIG_BAUDRATE			115200	/* Default baud rate */
-#define CONFIG_SYS_BAUDRATE_TABLE	{ 9600, 19200, 38400, 57600, 115200 }
 
 /* DMA */
 #define CONFIG_APBH_DMA
