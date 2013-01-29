d62ef5619c9249772247d6af3b8e65207ae0c871
Jagannadha Sutradharudu Teki <jagannadha.sutradharudu-teki@xilinx.com>
2013-01-29 17:13:38 +0530

    sf: stmicro: Add support for N25Q256A
    Add support for Numonyx N25Q256A SPI flash.
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/spi/stmicro.c
+++ drivers/mtd/spi/stmicro.c
@@ -134,6 +134,12 @@ static const struct stmicro_spi_flash_params stmicro_spi_flash_table[] = {
 		.nr_sectors = 512,
 		.name = "N25Q256",
 	},
+	{
+		.id = 0xbb19,
+		.pages_per_sector = 256,
+		.nr_sectors = 512,
+		.name = "N25Q256A",
+	},
 };
 
 struct spi_flash *spi_flash_probe_stmicro(struct spi_slave *spi, u8 * idcode)
