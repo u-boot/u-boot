ecdbf419f9c979cb6417df261c324c5beb63300d
Simon Glass <sjg@chromium.org>
2013-02-24 20:29:23 +0000

    sandbox: config: Enable hash functions and mtest
    Enable the hash command and sha1/256 hashing for sandbox. Also use a
    better address for memory testing (since the existing one is set up
    for linux host memory space).
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/sandbox.h
+++ include/configs/sandbox.h
@@ -63,8 +63,8 @@
 #define CONFIG_SYS_HZ			1000
 
 /* Memory things - we don't really want a memory test */
-#define CONFIG_SYS_LOAD_ADDR		0x10000000
-#define CONFIG_SYS_MEMTEST_START	0x10000000
+#define CONFIG_SYS_LOAD_ADDR		0x00000000
+#define CONFIG_SYS_MEMTEST_START	0x00100000
 #define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x1000)
 #define CONFIG_PHYS_64BIT
 
@@ -84,6 +84,11 @@
 /* We don't have networking support yet */
 #undef CONFIG_CMD_NET
 #undef CONFIG_CMD_NFS
+
+#define CONFIG_CMD_HASH
+#define CONFIG_HASH_VERIFY
+#define CONFIG_SHA1
+#define CONFIG_SHA256
 
 #define CONFIG_BOOTARGS ""
 
