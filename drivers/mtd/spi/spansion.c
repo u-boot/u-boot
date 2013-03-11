c0f87dd4ffa0c012d1dc8f737412c58200a93c93
Simon Glass <sjg@chromium.org>
2013-03-11 06:08:03 +0000

    sf: Use spi_flash_alloc() in each SPI flash driver
    Rather than each device having its own way to allocate a SPI flash
    structure, use the new allocation function everywhere. This will make it
    easier to extend the interface without breaking devices.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/mtd/spi/spansion.c
+++ drivers/mtd/spi/spansion.c
@@ -128,18 +128,12 @@ struct spi_flash *spi_flash_probe_spansion(struct spi_slave *spi, u8 *idcode)
 		return NULL;
 	}
 
-	flash = malloc(sizeof(*flash));
+	flash = spi_flash_alloc_base(spi, params->name);
 	if (!flash) {
 		debug("SF: Failed to allocate memory\n");
 		return NULL;
 	}
 
-	flash->spi = spi;
-	flash->name = params->name;
-
-	flash->write = spi_flash_cmd_write_multi;
-	flash->erase = spi_flash_cmd_erase;
-	flash->read = spi_flash_cmd_read_fast;
 	flash->page_size = 256;
 	flash->sector_size = 256 * params->pages_per_sector;
 	flash->size = flash->sector_size * params->nr_sectors;
