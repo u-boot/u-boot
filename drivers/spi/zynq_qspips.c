9ac988bcb2a9a46e32e157d053565e99aa47e15c
Jagannadha Sutradharudu Teki <jagannadha.sutradharudu-teki@xilinx.com>
2013-03-12 09:44:50 +0530

    spi: zynq: driver code cleanups
    - Moved the structures from zynq_qspips.h to zynq_qspips.c
    - Removed typedef bool and zynq_qspips.h
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>

--- drivers/spi/zynq_qspips.c
+++ drivers/spi/zynq_qspips.c
@@ -18,9 +18,8 @@
 #include <malloc.h>
 #include <ubi_uboot.h>
 #include <spi.h>
+#include <asm/io.h>
 #include <asm/arch/hardware.h>
-
-#include "zynq_qspips.h"
 
 /* Register offset definitions */
 #define XQSPIPS_CONFIG_OFFSET		0x00 /* Configuration  Register, RW */
@@ -132,6 +131,44 @@ enum xqspips_con_topology {
 /* Macros for the QSPI controller read/write */
 #define xqspips_write(off, val)		__raw_writel(val, off)
 #define xqspips_read(off)		__raw_readl(off)
+
+/* Few mtd flash functions */
+extern int spi_flash_cmd(struct spi_slave *spi, u8 cmd,
+		void *response, size_t len);
+extern int spi_flash_cmd_read(struct spi_slave *spi, const u8 *cmd,
+		size_t cmd_len, void *data, size_t data_len);
+
+struct xqspips {
+	void *regs;
+	u32 input_clk_hz;
+	u32 speed_hz;
+	const void *txbuf;
+	void *rxbuf;
+	int bytes_to_transfer;
+	int bytes_to_receive;
+	struct xqspips_inst_format *curr_inst;
+	u8 inst_response;
+	unsigned int is_inst;
+	unsigned int is_dual;
+};
+
+struct spi_device {
+	struct xqspips	master;
+	u32		max_speed_hz;
+	u8		chip_select;
+	u8		mode;
+	u8		bits_per_word;
+};
+
+struct spi_transfer {
+	const void	*tx_buf;
+	void		*rx_buf;
+	unsigned	len;
+	unsigned	cs_change:1;
+	u8		bits_per_word;
+	u16		delay_usecs;
+	u32		speed_hz;
+};
 
 struct zynq_spi_slave {
 	struct spi_slave  slave;
