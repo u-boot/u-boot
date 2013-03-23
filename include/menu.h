fc9d64ffcd94c1e22675830eae13956cc4fd822d
Pali Rohár <pali.rohar@gmail.com>
2013-03-23 14:50:40 +0000

    menu: Add support for user defined item choice function
    Selecting menu items is currently done in menu_interactive_choice()
    by reading the user input strings from standard input.
    
    Extend menu_interactive_choice() to support user defined function
    for selecting menu items. This function and its argument can be
    specified when creating the menu.
    
    Signed-off-by: Pali Rohár <pali.rohar@gmail.com>
    Signed-off-by: Anatolij Gustschin <agust@denx.de>

--- include/menu.h
+++ include/menu.h
@@ -21,7 +21,9 @@
 struct menu;
 
 struct menu *menu_create(char *title, int timeout, int prompt,
-				void (*item_data_print)(void *));
+				void (*item_data_print)(void *),
+				char *(*item_choice)(void *),
+				void *item_choice_data);
 int menu_default_set(struct menu *m, char *item_key);
 int menu_get_choice(struct menu *m, void **choice);
 int menu_item_add(struct menu *m, char *item_key, void *item_data);
