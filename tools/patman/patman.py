ca706e768d01ee30bb0e6f6af7ca65b5a244b56d
Simon Glass <sjg@chromium.org>
2013-03-26 13:09:45 +0000

    patman: Minor help message/README fixes
    A few of the help messages are not quite right, and there is a typo
    in the README. Fix these.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- tools/patman/patman.py
+++ tools/patman/patman.py
@@ -49,7 +49,7 @@ parser.add_option('-i', '--ignore-errors', action='store_true',
        dest='ignore_errors', default=False,
        help='Send patches email even if patch errors are found')
 parser.add_option('-n', '--dry-run', action='store_true', dest='dry_run',
-       default=False, help="Do a try run (create but don't email patches)")
+       default=False, help="Do a dry run (create but don't email patches)")
 parser.add_option('-p', '--project', default=project.DetectProject(),
                   help="Project name; affects default option values and "
                   "aliases [default: %default]")
@@ -72,7 +72,7 @@ parser.add_option('--no-tags', action='store_false', dest='process_tags',
 parser.usage = """patman [options]
 
 Create patches from commits in a branch, check them and email them as
-specified by tags you place in the commits. Use -n to """
+specified by tags you place in the commits. Use -n to do a dry run first."""
 
 
 # Parse options twice: first to get the project and second to handle
