12a46a384dac2eb0f879394ab39e89dec66df130
Padmavathi Venna <padma.v@samsung.com>
2013-03-28 04:32:21 +0000

    Exynos: clock: Add generic api to get the clk freq
    Add generic api to get the frequency of the required peripherial. This
    API gets the source clock frequency and returns the required frequency
    by dividing with first and second dividers based on the requirement.
    
    Test with command "sf probe 1:0; time sf read 40008000 0 1000".
    Try with different numbers of bytes and see that sane values are obtained
    Build and boot U-boot with this patch, backlight works properly.
    
    Signed-off-by: Padmavathi Venna <padma.v@samsung.com>
    Signed-off-by: Akshay Saraswat <akshay.s@samsung.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- arch/arm/include/asm/arch-exynos/clk.h
+++ arch/arm/include/asm/arch-exynos/clk.h
@@ -29,6 +29,12 @@
 #define VPLL	4
 #define BPLL	5
 
+enum pll_src_bit {
+	EXYNOS_SRC_MPLL = 6,
+	EXYNOS_SRC_EPLL,
+	EXYNOS_SRC_VPLL,
+};
+
 unsigned long get_pll_clk(int pllreg);
 unsigned long get_arm_clk(void);
 unsigned long get_i2c_clk(void);
@@ -43,5 +49,14 @@ void set_i2s_clk_source(void);
 int set_i2s_clk_prescaler(unsigned int src_frq, unsigned int dst_frq);
 int set_epll_clk(unsigned long rate);
 int set_spi_clk(int periph_id, unsigned int rate);
+
+/**
+ * get the clk frequency of the required peripheral
+ *
+ * @param peripheral	Peripheral id
+ *
+ * @return frequency of the peripheral clk
+ */
+unsigned long clock_get_periph_rate(int peripheral);
 
 #endif
