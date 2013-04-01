773d56d40da16ebec63da88c63cc7bc25b391350
Fabio Estevam <fabio.estevam@freescale.com>
2013-04-01 16:03:37 +0000

    mx6qsabrelite: Remove duplicate 'mmc dev'
    No need to call 'mmc dev' twice.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx6qsabrelite.h
+++ include/configs/mx6qsabrelite.h
@@ -216,7 +216,6 @@
 		"fi;\0"
 
 #define CONFIG_BOOTCOMMAND \
-	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \
 			   "run bootscript; " \
