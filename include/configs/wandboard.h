69089245c27eeabfce5736bded168938ecf51739
Fabio Estevam <fabio.estevam@freescale.com>
2013-04-01 16:03:36 +0000

    wandboard: Remove duplicate 'mmc dev'
    No need to call 'mmc dev' twice.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/wandboard.h
+++ include/configs/wandboard.h
@@ -150,7 +150,6 @@
 		"fi;\0"
 
 #define CONFIG_BOOTCOMMAND \
-	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \
 			   "run bootscript; " \
