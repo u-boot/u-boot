fe8e4dbad1d31c452af3fbabba21e72b210381b3
Kuan-Yu Kuo <ken.kuoky@gmail.com>
2013-04-23 07:47:47 +0000

    nds32: Use sections header to obtain link symbols
    Include this header to get access to link symbols, which are otherwise
    removed.
    
    Signed-off-by: Kuan-Yu Kuo <ken.kuoky@gmail.com>
    Cc: Macpaul Lin <macpaul@gmail.com>

--- arch/nds32/include/asm/u-boot-nds32.h
+++ arch/nds32/include/asm/u-boot-nds32.h
@@ -30,11 +30,6 @@
 #define _U_BOOT_NDS32_H_	1
 
 /* for the following variables, see start.S */
-extern char __bss_start[];	/* BSS start relative to _start */
-extern ulong __bss_end;		/* BSS end relative to _start */
-extern char _end[];		/* end of image relative to _start */
-extern void _start(void);	/* start of image relative to _start */
-extern ulong _TEXT_BASE;	/* code start */
 extern ulong IRQ_STACK_START;	/* top of IRQ stack */
 extern ulong FIQ_STACK_START;	/* top of FIQ stack */
 
