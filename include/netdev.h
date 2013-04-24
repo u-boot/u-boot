45a1693a312453dcb5d26cd03c57569c50872cc6
Roberto Cerati <roberto.cerati@bticino.it>
2013-04-24 10:46:17 +0800

    net: ks8851_mll: add ethernet support
    The device interface is 16 bits wide.
    All the available packets are read from the incoming fifo.
    
    Signed-off-by: Roberto Cerati <roberto.cerati@bticino.it>
    Signed-off-by: Raffaele Recalcati <raffaele.recalcati@bticino.it>
    [voice.shen@atmel.com: address comments from review results]
    [voice.shen@atmel.com: clean up for submit]
    Signed-off-by: Bo Shen <voice.shen@atmel.com>
    Tested-by: Raffaele Recalcati <raffaele.recalcati@bticino.it>

--- include/netdev.h
+++ include/netdev.h
@@ -71,6 +71,7 @@ int greth_initialize(bd_t *bis);
 void gt6426x_eth_initialize(bd_t *bis);
 int inca_switch_initialize(bd_t *bis);
 int ks8695_eth_initialize(void);
+int ks8851_mll_initialize(u8 dev_num, int base_addr);
 int lan91c96_initialize(u8 dev_num, int base_addr);
 int macb_eth_initialize(int id, void *regs, unsigned int phy_addr);
 int mcdmafec_initialize(bd_t *bis);
