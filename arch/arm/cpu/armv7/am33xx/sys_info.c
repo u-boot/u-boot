30bba0175122150c330ff95132e048a39bf978eb
Tom Rini <trini@ti.com>
2013-04-25 16:46:04 -0400

    am33xx: Fix warning with CONFIG_DISPLAY_CPUINFO
    The arm_freq and ddr_freq variables are unused, so remove.  Fixup
    whitespace slightly while in here.
    
    Reviewed-by: Peter Korsgaard <jacmet@sunsite.dk>
    Signed-off-by: Tom Rini <trini@ti.com>

--- arch/arm/cpu/armv7/am33xx/sys_info.c
+++ arch/arm/cpu/armv7/am33xx/sys_info.c
@@ -92,7 +92,6 @@ u32 get_sysboot_value(void)
 int print_cpuinfo(void)
 {
 	char *cpu_s, *sec_s;
-	int arm_freq, ddr_freq;
 
 	switch (get_cpu_type()) {
 	case AM335X:
@@ -123,10 +122,7 @@ int print_cpuinfo(void)
 		sec_s = "?";
 	}
 
-	printf("%s-%s rev %d\n",
-			cpu_s, sec_s, get_cpu_rev());
-
-	/* TODO: Print ARM and DDR frequencies  */
+	printf("%s-%s rev %d\n", cpu_s, sec_s, get_cpu_rev());
 
 	return 0;
 }
