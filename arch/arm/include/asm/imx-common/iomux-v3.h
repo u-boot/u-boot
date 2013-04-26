79a34d3ccc7bb08848412877b6935e596fef1d29
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2013-04-26 01:34:45 +0000

    imx: iomux-v3: cosmetic: Reorganize definitions
    Keep pad control definitions together, and organize definitions in a more
    legible way.
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- arch/arm/include/asm/imx-common/iomux-v3.h
+++ arch/arm/include/asm/imx-common/iomux-v3.h
@@ -86,31 +86,29 @@ typedef u64 iomux_v3_cfg_t;
 	((iomux_v3_cfg_t)(sel_input_ofs) << MUX_SEL_INPUT_OFS_SHIFT)|	\
 	((iomux_v3_cfg_t)(sel_input)     << MUX_SEL_INPUT_SHIFT))
 
+#define __NA_			0x000
+#define NO_MUX_I		0
+#define NO_PAD_I		0
+
 #define NO_PAD_CTRL		(1 << 17)
-#define GPIO_PIN_MASK		0x1f
-#define GPIO_PORT_SHIFT		5
-#define GPIO_PORT_MASK		(0x7 << GPIO_PORT_SHIFT)
-#define GPIO_PORTA		(0 << GPIO_PORT_SHIFT)
-#define GPIO_PORTB		(1 << GPIO_PORT_SHIFT)
-#define GPIO_PORTC		(2 << GPIO_PORT_SHIFT)
-#define GPIO_PORTD		(3 << GPIO_PORT_SHIFT)
-#define GPIO_PORTE		(4 << GPIO_PORT_SHIFT)
-#define GPIO_PORTF		(5 << GPIO_PORT_SHIFT)
 
 #ifdef CONFIG_MX6
 
 #define PAD_CTL_HYS		(1 << 16)
+
 #define PAD_CTL_PUS_100K_DOWN	(0 << 14)
 #define PAD_CTL_PUS_47K_UP	(1 << 14)
 #define PAD_CTL_PUS_100K_UP	(2 << 14)
 #define PAD_CTL_PUS_22K_UP	(3 << 14)
-
 #define PAD_CTL_PUE		(1 << 13)
 #define PAD_CTL_PKE		(1 << 12)
+
 #define PAD_CTL_ODE		(1 << 11)
+
 #define PAD_CTL_SPEED_LOW	(1 << 6)
 #define PAD_CTL_SPEED_MED	(2 << 6)
 #define PAD_CTL_SPEED_HIGH	(3 << 6)
+
 #define PAD_CTL_DSE_DISABLE	(0 << 3)
 #define PAD_CTL_DSE_240ohm	(1 << 3)
 #define PAD_CTL_DSE_120ohm	(2 << 3)
@@ -142,13 +140,20 @@ typedef u64 iomux_v3_cfg_t;
 
 #endif
 
-#define PAD_CTL_SRE_FAST	(1 << 0)
 #define PAD_CTL_SRE_SLOW	(0 << 0)
+#define PAD_CTL_SRE_FAST	(1 << 0)
 
 #define IOMUX_CONFIG_SION	0x10
-#define __NA_			0x000
-#define NO_MUX_I		0
-#define NO_PAD_I		0
+
+#define GPIO_PIN_MASK		0x1f
+#define GPIO_PORT_SHIFT		5
+#define GPIO_PORT_MASK		(0x7 << GPIO_PORT_SHIFT)
+#define GPIO_PORTA		(0 << GPIO_PORT_SHIFT)
+#define GPIO_PORTB		(1 << GPIO_PORT_SHIFT)
+#define GPIO_PORTC		(2 << GPIO_PORT_SHIFT)
+#define GPIO_PORTD		(3 << GPIO_PORT_SHIFT)
+#define GPIO_PORTE		(4 << GPIO_PORT_SHIFT)
+#define GPIO_PORTF		(5 << GPIO_PORT_SHIFT)
 
 void imx_iomux_v3_setup_pad(iomux_v3_cfg_t pad);
 void imx_iomux_v3_setup_multiple_pads(iomux_v3_cfg_t const *pad_list,
