83284a1a6ee451cef80658583db1a346fa844a67
Fabio Estevam <fabio.estevam@freescale.com>
2013-05-03 04:37:09 +0000

    mx23_olinuxino: Fix DDR pin iomux settings
    Change MUX_CONFIG_EMI to use the same drive strength as the bootlets code from
    Freescale, which results in much better stability.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>

--- board/olimex/mx23_olinuxino/spl_boot.c
+++ board/olimex/mx23_olinuxino/spl_boot.c
@@ -29,7 +29,7 @@
 #include <asm/arch/imx-regs.h>
 #include <asm/arch/sys_proto.h>
 
-#define	MUX_CONFIG_EMI	(MXS_PAD_3V3 | MXS_PAD_16MA | MXS_PAD_PULLUP)
+#define	MUX_CONFIG_EMI	(MXS_PAD_3V3 | MXS_PAD_12MA | MXS_PAD_PULLUP)
 #define	MUX_CONFIG_SSP	(MXS_PAD_3V3 | MXS_PAD_8MA | MXS_PAD_PULLUP)
 
 const iomux_cfg_t iomux_setup[] = {
