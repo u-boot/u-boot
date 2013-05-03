0ede8272d0f0c3baf07fa23a2a576a5e59abe489
Fabio Estevam <fabio.estevam@freescale.com>
2013-05-03 04:37:08 +0000

    mx23evk: Fix DDR pin iomux settings
    Change MUX_CONFIG_EMI to use the same drive strength as the bootlets code from
    Freescale, which results in much better stability.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>

--- board/freescale/mx23evk/spl_boot.c
+++ board/freescale/mx23evk/spl_boot.c
@@ -26,7 +26,7 @@
 #include <asm/arch/sys_proto.h>
 
 #define	MUX_CONFIG_SSP1	(MXS_PAD_3V3 | MXS_PAD_8MA | MXS_PAD_PULLUP)
-#define	MUX_CONFIG_EMI	(MXS_PAD_3V3 | MXS_PAD_16MA | MXS_PAD_PULLUP)
+#define	MUX_CONFIG_EMI	(MXS_PAD_3V3 | MXS_PAD_12MA | MXS_PAD_PULLUP)
 
 const iomux_cfg_t iomux_setup[] = {
 	/* DUART */
