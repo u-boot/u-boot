68088ceed7eb059d1aa50ffe7c372cf8b058797e
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2013-05-06 01:33:51 +0000

    imx: mx35pdk: Fix MUX2_CTR GPIO
    MUX2_CTR is on GPIO1[5], not GPIO2[5], and it needs to be set high in order to
    connect the FEC.
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- board/freescale/mx35pdk/mx35pdk.c
+++ board/freescale/mx35pdk/mx35pdk.c
@@ -245,7 +245,7 @@ int board_late_init(void)
 
 		imx_iomux_v3_setup_pad(MX35_PAD_COMPARE__GPIO1_5);
 
-		gpio_direction_output(IMX_GPIO_NR(2, 5), 1);
+		gpio_direction_output(IMX_GPIO_NR(1, 5), 1);
 	}
 
 	val = mc9sdz60_reg_read(MC9SDZ60_REG_GPIO_1) | 0x04;
