3a01799b35218137b9051929a74eb2a6d06c5d26
Liu Gang <Gang.Liu@freescale.com>
2013-05-07 16:30:47 +0800

    powerpc/b4860qds: Enable master module for boot from SRIO and PCIE
    B4860QDS can support the feature of Boot from SRIO/PCIE, and the macro
    "CONFIG_SRIO_PCIE_BOOT_MASTER" will enable the master module of this feature
    when building the u-boot image.
    
    You can get some description about this macro in README file, and for more
    information about the feature of Boot from SRIO/PCIE, please refer to the
    document doc/README.srio-pcie-boot-corenet.
    
    Signed-off-by: Liu Gang <Gang.Liu@freescale.com>
    Signed-off-by: Andy Fleming <afleming@freescale.com>

--- include/configs/B4860QDS.h
+++ include/configs/B4860QDS.h
@@ -63,6 +63,7 @@
 #define CONFIG_SYS_SRIO
 #define CONFIG_SRIO1			/* SRIO port 1 */
 #define CONFIG_SRIO2			/* SRIO port 2 */
+#define CONFIG_SRIO_PCIE_BOOT_MASTER
 #endif
 
 #define CONFIG_FSL_LAW			/* Use common FSL init code */
