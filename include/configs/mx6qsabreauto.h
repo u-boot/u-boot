195781657cccdda947555668aca5854af84f5fdf
Renato Frias <b13784@freescale.com>
2013-05-13 18:01:12 +0000

    mx6qsabreauto: Add i2c to mx6qsabreauto board
    Add i2c2 and 3 to mx6qsabreauto board, i2c3 is multiplexed
    use gpio to set steering.
    
    Signed-off-by: Renato Frias <b13784@freescale.com>
    Reviewed-by: Otavio Salvador <otavio@ossystems.com.br>
    Reviewed-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Stefano Babic <sbabic@denx.de>

--- include/configs/mx6qsabreauto.h
+++ include/configs/mx6qsabreauto.h
@@ -38,4 +38,10 @@
 #define CONFIG_SYS_MMC_ENV_PART		1	/* Boot partition 1 */
 #endif
 
+/* I2C Configs */
+#define CONFIG_CMD_I2C
+#define CONFIG_I2C_MULTI_BUS
+#define CONFIG_I2C_MXC
+#define CONFIG_SYS_I2C_SPEED		100000
+
 #endif                         /* __MX6QSABREAUTO_CONFIG_H */
