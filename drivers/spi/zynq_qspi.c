3407b636b3e6c5b59a19fd374978db6f70d929c8
Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
2013-05-20 15:10:51 +0530

    zynq: qspi: Update driver description
    Updated the driver description on header.
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>

--- drivers/spi/zynq_qspi.c
+++ drivers/spi/zynq_qspi.c
@@ -1,7 +1,7 @@
 /*
  * (C) Copyright 2011 - 2013 Xilinx
  *
- * Xilinx PS Quad-SPI (QSPI) controller driver (master mode only)
+ * Xilinx Zynq Quad-SPI(QSPI) controller driver (master mode only)
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by the
