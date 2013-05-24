994aca0924795160745fd90ee6de0a69eff298c0
Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
2013-05-24 15:24:00 +0530

    zynq: nand: do not wait on erase completion in cmd_func
    The chip command function dont have to wait on ERASE completion
    as the dev_ready chip function is defined and use of the default
    nand_erase is made.
    
    This fixes the "Timeout!" error while doing "nand erase" and
    also the very low NAND erase performace.
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>

--- drivers/mtd/nand/zynq_nand.c
+++ drivers/mtd/nand/zynq_nand.c
@@ -876,7 +876,6 @@ static void zynq_nand_cmd_function(struct mtd_info *mtd, unsigned int command,
 	ndelay(100);
 
 	if ((command == NAND_CMD_READ0) ||
-	    (command == NAND_CMD_ERASE1) ||
 	    (command == NAND_CMD_RESET) ||
 	    (command == NAND_CMD_PARAM) ||
 	    (command == NAND_CMD_GET_FEATURES)) {
