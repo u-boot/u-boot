bc3442aaed26f16a0b9d1caa55cbe3bd94d16a86
Simon Glass <sjg@chromium.org>
2013-06-11 11:14:33 -0700

    pci: Convert extern inline functions to static inline
    I am not sure of the meaning of extern inline, but this gives errors
    when building with function instrumenting enabled. Change these functions
    to static inline.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/pci.h
+++ include/pci.h
@@ -462,7 +462,7 @@ struct pci_region {
 #define PCI_REGION_SYS_MEMORY	0x00000100	/* System memory */
 #define PCI_REGION_RO		0x00000200	/* Read-only memory */
 
-extern __inline__ void pci_set_region(struct pci_region *reg,
+static inline void pci_set_region(struct pci_region *reg,
 				      pci_addr_t bus_start,
 				      phys_addr_t phys_start,
 				      pci_size_t size,
@@ -548,7 +548,7 @@ struct pci_controller {
 	void *priv_data;
 };
 
-extern __inline__ void pci_set_ops(struct pci_controller *hose,
+static inline void pci_set_ops(struct pci_controller *hose,
 				   int (*read_byte)(struct pci_controller*,
 						    pci_dev_t, int where, u8 *),
 				   int (*read_word)(struct pci_controller*,
