df84502edffd1da48588a212b402602f032ed53d
Albert ARIBAUD <albert.u.boot@aribaud.net>
2013-06-11 14:17:32 +0200

    arm: generalize lib/bss.c into lib/sections.c
    File arch/arm/lib/bss.c was initially defined for BSS only,
    but is now going to also contain definitions for other
    section-boundary-related symbols, so rename it for better
    accuracy.
    
    Also, remove useless 'used' attributes.
    
    Signed-off-by: Albert ARIBAUD <albert.u.boot@aribaud.net>
    Tested-by: Lubomir Popov <lpopov@mm-sol.com>
    Tested-by: Jeroen Hofstee <jeroen@myspectrum.nl>
    Reviewed-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- arch/arm/lib/sections.c
+++ arch/arm/lib/sections.c
@@ -35,5 +35,5 @@
  * aliasing warnings.
  */
 
-char __bss_start[0] __attribute__((used, section(".__bss_start")));
-char __bss_end[0] __attribute__((used, section(".__bss_end")));
+char __bss_start[0] __attribute__((section(".__bss_start")));
+char __bss_end[0] __attribute__((section(".__bss_end")));
