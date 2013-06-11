d026dec8751378473b4f12eb87717c8059472b3e
Albert ARIBAUD <albert.u.boot@aribaud.net>
2013-06-11 14:17:33 +0200

    arm: make __image_copy_{start, end} compiler-generated
    This change is only done where needed: some linker
    scripts may contain __image_copy_{start,end} yet
    remain unchanged.
    
    Also, __image_copy_end needs its own section; putting
    it in relocation sections changes their flags and makes
    relocation break.
    
    Signed-off-by: Albert ARIBAUD <albert.u.boot@aribaud.net>
    Tested-by: Lubomir Popov <lpopov@mm-sol.com>
    Tested-by: Jeroen Hofstee <jeroen@myspectrum.nl>
    Reviewed-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>

--- arch/arm/lib/sections.c
+++ arch/arm/lib/sections.c
@@ -37,3 +37,5 @@
 
 char __bss_start[0] __attribute__((section(".__bss_start")));
 char __bss_end[0] __attribute__((section(".__bss_end")));
+char __image_copy_start[0] __attribute__((section(".__image_copy_start")));
+char __image_copy_end[0] __attribute__((section(".__image_copy_end")));
