e97721c41a808b8078b686e23eb80dd65264ed55
Otavio Salvador <otavio@ossystems.com.br>
2013-06-28 18:52:43 -0300

    mx51evk: Change default environment to cope with OE changes
    OpenEmbedded has change partitioning layout of generated image so it
    does not raise warnings during the boot regarding unkown partition
    being used for U-Boot.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
    Acked-by: Stefano Babic <sbabic@denx.de>

--- include/configs/mx51evk.h
+++ include/configs/mx51evk.h
@@ -162,8 +162,8 @@
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
 	"mmcdev=0\0" \
-	"mmcpart=2\0" \
-	"mmcroot=/dev/mmcblk0p3 rootwait rw\0" \
+	"mmcpart=1\0" \
+	"mmcroot=/dev/mmcblk0p2 rootwait rw\0" \
 	"mmcargs=setenv bootargs console=ttymxc0,${baudrate} " \
 		"root=${mmcroot}\0" \
 	"loadbootscript=" \
