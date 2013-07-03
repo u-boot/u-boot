2eb60902a03758133b027934b8710c7ff730cb06
Stefan Roese <sr@denx.de>
2013-07-03 18:34:54 +0200

    net: smsc95xx: Add support for another SMSC95xx variant
    This patch adds support for the SMSC9500 with product id 0x9900 which is
    equipped in the "EXSYS USB 2.0" etherner USB adapter.
    
    Tested on omap3_beagle.
    
    Signed-off-by: Stefan Roese <sr@denx.de>

--- drivers/usb/eth/smsc95xx.c
+++ drivers/usb/eth/smsc95xx.c
@@ -784,6 +784,7 @@ static const struct smsc95xx_dongle smsc95xx_dongles[] = {
 	{ 0x0424, 0xec00 },	/* LAN9512/LAN9514 Ethernet */
 	{ 0x0424, 0x9500 },	/* LAN9500 Ethernet */
 	{ 0x0424, 0x9730 },	/* LAN9730 Ethernet (HSIC) */
+	{ 0x0424, 0x9900 },	/* SMSC9500 USB Ethernet Device (SAL10) */
 	{ 0x0000, 0x0000 }	/* END - Do not remove */
 };
 
