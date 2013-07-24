0b17998e509e5614633aee0a3c5d6248f8bc580b
Tom Rini <trini@ti.com>
2013-07-24 09:34:30 -0400

    qemu-malta: Update for SPDX license identifiers
    Signed-off-by: Tom Rini <trini@ti.com>

--- drivers/pci/pci_gt64120.c
+++ drivers/pci/pci_gt64120.c
@@ -6,9 +6,7 @@
  *   Authors: Carsten Langgaard <carstenl@mips.com>
  *            Maciej W. Rozycki <macro@mips.com>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation.
+ * SPDX-License-Identifier:	GPL-2.0
  */
 
 #include <common.h>
--- include/pci_gt64120.h
+++ include/pci_gt64120.h
@@ -1,9 +1,7 @@
 /*
  * Copyright (C) 2013 Gabor Juhos <juhosg@openwrt.org>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation.
+ * SPDX-License-Identifier:	GPL-2.0
  */
 
 #ifndef _PCI_GT64120_H
