e633ac019642689e3a2dd95bd644b3a10c7bbbb4
Taras Kondratiuk <taras@ti.com>
2013-08-06 15:18:51 +0300

    ARM: OMAP4460: sdp: Limit TPS mux config to 4460
    TPS mux config is 4460 specific, so it should be limited to 4460 only.
    
    Signed-off-by: Taras Kondratiuk <taras@ti.com>

--- board/ti/sdp4430/sdp.c
+++ board/ti/sdp4430/sdp.c
@@ -65,7 +65,8 @@ void set_muxconf_regs_essential(void)
 		   sizeof(wkup_padconf_array_essential) /
 		   sizeof(struct pad_conf_entry));
 
-	if (omap_revision() >= OMAP4460_ES1_0)
+	if ((omap_revision() >= OMAP4460_ES1_0) &&
+			(omap_revision() < OMAP4470_ES1_0))
 		do_set_mux((*ctrl)->control_padconf_wkup_base,
 				 wkup_padconf_array_essential_4460,
 				 sizeof(wkup_padconf_array_essential_4460) /
