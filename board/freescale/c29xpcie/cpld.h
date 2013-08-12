3aab0cd852d7c9565c2559a7983cbb73852bac28
York Sun <yorksun@freescale.com>
2013-08-12 14:57:12 -0700

    powerpc/mpc85xx: Cleanup license header in source files
    Fix the license header introduced by the following patches
    
    Add TWR-P10xx board support
    Add T4240EMU target
    IDT8T49N222A configuration code
    Add C29x SoC support
    Add support for C29XPCIE board
    
    Signed-off-by: York Sun <yorksun@freescale.com>

--- board/freescale/c29xpcie/cpld.h
+++ board/freescale/c29xpcie/cpld.h
@@ -3,10 +3,7 @@
  * Author: Mingkai Hu <Mingkai.Hu@freescale.com>
  *         Po Liu <Po.Liu@freescale.com>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * SPDX-License-Identifier:	GPL-2.0+
  *
  * This file provides support for the ngPIXIS, a board-specific FPGA used on
  * some Freescale reference boards.
