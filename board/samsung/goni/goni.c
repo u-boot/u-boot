2936df1f1183f0bb884489e993a0381c3f179ea6
Łukasz Majewski <l.majewski@samsung.com>
2013-08-16 15:33:33 +0200

    i2c:samsung: Adjust Trats, GONI and Universal_c210 boards to work with new I2C framework
    New I2C framework, introduced after v2013.07 final release, imposed I2C
    code adjustment for some Samsung boards - namely Trats, GONI and Universal_c210.
    
    Those boards were using schematic based I2C enumeration (I2C_5, I2C_9).
    However, new I2C framework imposes usage of logical I2C adapters numbering
    (e.g. I2C_0, I2C_1, etc).
    
    Additionally, I2C_GET_* macros were replaced with i2c_*_bus_num() functions.
    
    Trats board gained definition of second soft I2C adapter.
    
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>
    Cc: Minkyu Kang <mk7.kang@samsung.com>
    Cc: Heiko Schocher <hs@denx.de>

--- board/samsung/goni/goni.c
+++ board/samsung/goni/goni.c
@@ -32,7 +32,11 @@ int power_init_board(void)
 {
 	int ret;
 
-	ret = pmic_init(I2C_5);
+	/*
+	 * For PMIC the I2C bus is named as I2C5, but it is connected
+	 * to logical I2C adapter 0
+	 */
+	ret = pmic_init(I2C_0);
 	if (ret)
 		return ret;
 
