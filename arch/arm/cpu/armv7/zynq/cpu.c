505747bb88c7ca70beb6fcef58e4488ddc326316
Michal Simek <michal.simek@xilinx.com>
2013-08-22 14:36:47 +0200

    zynq: Use arch_cpu_init() instead of lowlevel_init()
    Zynq lowlevel_init() was implemented in C but stack
    pointer is setup after function call in _main().
    Move architecture setup to arch_cpu_init() which is call
    as the first function in board_init_f() which
    already have correct stack pointer.
    
    Reported-by: Sven Schwermer <sven.schwermer@tuhh.de>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/cpu/armv7/zynq/cpu.c
+++ arch/arm/cpu/armv7/zynq/cpu.c
@@ -27,6 +27,10 @@
 
 void lowlevel_init(void)
 {
+}
+
+int arch_cpu_init(void)
+{
 	zynq_slcr_unlock();
 	/* remap DDR to zero, FILTERSTART */
 	writel(0, &scu_base->filter_start);
@@ -47,6 +51,8 @@ void lowlevel_init(void)
 	writel(0xC, &slcr_base->ddr_urgent);
 
 	zynq_slcr_lock();
+
+	return 0;
 }
 
 void reset_cpu(ulong addr)
