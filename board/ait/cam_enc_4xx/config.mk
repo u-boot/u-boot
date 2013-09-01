1affd4d4a3fe512050e1ad1636d9360c670da531
Masahiro Yamada <yamada.m@jp.panasonic.com>
2013-09-01 15:04:27 +0900

    cam_enc_4xx: Move CONFIG_SPL_PAD_TO to a config header
    For most boards which define CONFIG_SPL_PAD_TO,
    it is defined in config header files.
    Currently, there exists only one exception, cam_enc_4xx board.
    
    This patch moves CONFIG_SPL_PAD_TO definition
    from board/ait/cam_enc_4xx/config.mk
    to include/configs/cam_enc_4xx.h.
    
    With this modification, we can delete a glue code
    in the top level config.mk:
    
    ifneq ($(CONFIG_SPL_PAD_TO),)
    CPPFLAGS += -DCONFIG_SPL_PAD_TO=$(CONFIG_SPL_PAD_TO)
    endif
    
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>
    Cc: Heiko Schocher <hs@denx.de>

--- board/ait/cam_enc_4xx/config.mk
+++ board/ait/cam_enc_4xx/config.mk
@@ -7,8 +7,6 @@
 # (mem base + reserved)
 #
 
-#Provide at least 16MB spacing between us and the Linux Kernel image
-CONFIG_SPL_PAD_TO := 12320
 UBL_CONFIG = $(SRCTREE)/board/$(BOARDDIR)/ublimage.cfg
 ifndef CONFIG_SPL_BUILD
 ALL-y += $(obj)u-boot.ubl
