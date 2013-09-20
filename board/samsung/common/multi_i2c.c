ecda331463d75fe085cddccf3a935333db770079
Piotr Wilczek <p.wilczek@samsung.com>
2013-09-20 15:01:26 +0200

    samsung:common:i2c: add definions for third soft I2C adapter for Trats2 board
    Signed-off-by: Piotr Wilczek <p.wilczek@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    CC: Lukasz Majewski <l.majewski@samsung.com>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- board/samsung/common/multi_i2c.c
+++ board/samsung/common/multi_i2c.c
@@ -8,6 +8,14 @@
 #include <common.h>
 #include <i2c.h>
 
+#ifndef CONFIG_SOFT_I2C_I2C10_SCL
+#define CONFIG_SOFT_I2C_I2C10_SCL 0
+#endif
+
+#ifndef CONFIG_SOFT_I2C_I2C10_SDA
+#define CONFIG_SOFT_I2C_I2C10_SDA 0
+#endif
+
 /* Handle multiple I2C buses instances */
 int get_multi_scl_pin(void)
 {
@@ -18,6 +26,8 @@ int get_multi_scl_pin(void)
 		return CONFIG_SOFT_I2C_I2C5_SCL;
 	case I2C_1:
 		return CONFIG_SOFT_I2C_I2C9_SCL;
+	case I2C_2:
+		return CONFIG_SOFT_I2C_I2C10_SCL;
 	default:
 		printf("I2C_%d not supported!\n", bus);
 	};
@@ -34,6 +44,8 @@ int get_multi_sda_pin(void)
 		return CONFIG_SOFT_I2C_I2C5_SDA;
 	case I2C_1:
 		return CONFIG_SOFT_I2C_I2C9_SDA;
+	case I2C_2:
+		return CONFIG_SOFT_I2C_I2C10_SDA;
 	default:
 		printf("I2C_%d not supported!\n", bus);
 	};
