0ab449beec5287ca600eca8c9bc7fe052af99162
Priyanka Jain <Priyanka.Jain@freescale.com>
2013-10-03 21:07:00 +0530

    sf: probe: Add support for EN25S64
    Add support for EON EN25S64 SPI flash.
    
    Signed-off-by: Priyanka Jain <Priyanka.Jain@freescale.com>
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>

--- drivers/mtd/spi/sf_probe.c
+++ drivers/mtd/spi/sf_probe.c
@@ -51,6 +51,7 @@ static const struct spi_flash_params spi_flash_params_table[] = {
 	{"EN25Q32B",	   0x1c3016, 0x0,	64 * 1024,    64,	             0},
 	{"EN25Q64",	   0x1c3017, 0x0,	64 * 1024,   128,	       SECT_4K},
 	{"EN25Q128B",	   0x1c3018, 0x0,       64 * 1024,   256,	             0},
+	{"EN25S64",	   0x1c3817, 0x0,	64 * 1024,   128,		     0},
 #endif
 #ifdef CONFIG_SPI_FLASH_GIGADEVICE	/* GIGADEVICE */
 	{"GD25Q64B",	   0xc84017, 0x0,	64 * 1024,   128,	       SECT_4K},
