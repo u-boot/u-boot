f1a74918e1d3d9988525c12b8af2eebc1910ccf7
Nikita Kiryanov <nikita@compulab.co.il>
2013-10-16 17:23:27 +0300

    lcd: add DataImage SCF0403x LCD panel support
    Add SPI-based driver for DataImage SCF0403852GGU04 and SCF0403526GGU20
    LCD panels.
    
    Cc: Tom Rini <trini@ti.com>
    Cc: Anatolij Gustschin <agust@denx.de>
    Cc: Igor Grinberg <grinberg@compulab.co.il>
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>
    Acked-by: Anatolij Gustschin <agust@denx.de>
    Signed-off-by: Anatolij Gustschin <agust@denx.de>

---
+++ include/scf0403_lcd.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2013, Compulab Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef SCF0403_LCD_H_
+#define SCF0403_LCD_H_
+
+int scf0403_init(int reset_gpio);
+
+#endif
