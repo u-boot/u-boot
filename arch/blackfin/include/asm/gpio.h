cae4d0403c0863176f228d410d6a29b3f4b9d595
Steven Miao <realmz6@gmail.com>
2013-10-23 14:44:09 +0800

    blackfin: Move machine specific gpio_port_t structure back to blackfin arch folder.
    The gpio register mappings are different among blackfin processors.
    
    Signed-off-by: Steven Miao <realmz6@gmail.com>
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- arch/blackfin/include/asm/gpio.h
+++ arch/blackfin/include/asm/gpio.h
@@ -8,6 +8,7 @@
 #define __ARCH_BLACKFIN_GPIO_H__
 
 #include <asm-generic/gpio.h>
+#include <asm/portmux.h>
 
 #define gpio_bank(x)	((x) >> 4)
 #define gpio_bit(x)	(1<<((x) & 0xF))
@@ -141,6 +142,8 @@ struct gpio_port_t {
 	unsigned short dummy16;
 	unsigned short inen;
 };
+#else
+extern struct gpio_port_t * const gpio_array[];
 #endif
 
 #ifdef ADI_SPECIAL_GPIO_BANKS
