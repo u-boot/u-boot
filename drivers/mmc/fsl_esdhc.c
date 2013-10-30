5f9b9f867d8a804de4980d2901a9c756a8eb5473
Haijun.Zhang <Haijun.Zhang@freescale.com>
2013-10-30 11:37:54 +0800

    powerpc/esdhc: Map register for eSDHC Host Controller V3.0
    eSDHC host controller has new register to support SD Spec 3.0.
    And the according host controller version was Freescale eSDHC
    Version 3.0.
    
    Signed-off-by: Haijun Zhang <haijun.zhang@freescale.com>
    Acked-by: Pantelis Antoniou <panto@antoniou-consulting.com>

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -45,9 +45,21 @@ struct fsl_esdhc {
 	uint    mixctrl;
 	char    reserved1[4];
 	uint	fevt;
-	char	reserved2[168];
+	uint	admaes;
+	uint	adsaddr;
+	char	reserved2[160];
 	uint	hostver;
-	char	reserved3[780];
+	char	reserved3[4];
+	uint	dmaerraddr;
+	char	reserved4[4];
+	uint	dmaerrattr;
+	char	reserved5[4];
+	uint	hostcapblt2;
+	char	reserved6[8];
+	uint	tcr;
+	char	reserved7[28];
+	uint	sddirctl;
+	char	reserved8[712];
 	uint	scr;
 };
 
