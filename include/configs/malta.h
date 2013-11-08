14b4e1a63e580b1993866ec783d958619d485d9c
Paul Burton <paul.burton@imgtec.com>
2013-11-08 11:18:53 +0000

    malta: remove cache size definitions
    These will now be detected at runtime, allowing a single U-boot
    configuration to function correctly with different bitstreams. Without
    this you may need to re-configure, re-build and re-flash U-boot to your
    Malta if you flash a new bitstream with a different cache configuration
    to your old bitstream.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- include/configs/malta.h
+++ include/configs/malta.h
@@ -29,10 +29,6 @@
 #define CONFIG_SYS_MHZ			250	/* arbitrary value */
 #define CONFIG_SYS_MIPS_TIMER_FREQ	(CONFIG_SYS_MHZ * 1000000)
 
-#define CONFIG_SYS_DCACHE_SIZE		16384	/* arbitrary value */
-#define CONFIG_SYS_ICACHE_SIZE		16384	/* arbitrary value */
-#define CONFIG_SYS_CACHELINE_SIZE	32	/* arbitrary value */
-
 #define CONFIG_SWAP_IO_SPACE
 
 /*
