477fe65832e0251e5a077aa15a3a7b97660db4f4
Masahiro Yamada <yamada.m@jp.panasonic.com>
2013-11-11 12:11:12 +0900

    nios2: remove unnecessary header include path
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>
    Cc: Thomas Chou <thomas@wytron.com.tw>

--- board/altera/nios2-generic/config.mk
+++ board/altera/nios2-generic/config.mk
@@ -5,11 +5,7 @@
 # SPDX-License-Identifier:	GPL-2.0+
 #
 
-# we get text_base from board config header, so do not use this
-#CONFIG_SYS_TEXT_BASE = do-not-use-me
-
 PLATFORM_CPPFLAGS += -mno-hw-div -mno-hw-mul
-PLATFORM_CPPFLAGS += -I$(TOPDIR)/board/$(VENDOR)/include
 
 ifeq ($(debug),1)
 PLATFORM_CPPFLAGS += -DDEBUG
