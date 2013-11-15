184c551b8582fee6b685d513d026b44f558285b5
Bo Shen <voice.shen@atmel.com>
2013-11-15 11:12:32 +0800

    arm: atmel: sama5d3: correct the ID for DBGU and PIT
    As the DBGU and PIT has its own ID on sama5d3 SoC, while not share
    with SYS ID. So, correct them.
    
    Signed-off-by: Bo Shen <voice.shen@atmel.com>
    Signed-off-by: Andreas Bießmann <andreas.devel@googlemail.com>

--- arch/arm/cpu/armv7/at91/sama5d3_devices.c
+++ arch/arm/cpu/armv7/at91/sama5d3_devices.c
@@ -84,7 +84,7 @@ void at91_seriald_hw_init(void)
 	at91_set_a_periph(AT91_PIO_PORTB, 30, 0);	/* DRXD */
 
 	/* Enable clock */
-	at91_periph_clk_enable(ATMEL_ID_SYS);
+	at91_periph_clk_enable(ATMEL_ID_DBGU);
 }
 
 #if defined(CONFIG_ATMEL_SPI)
