eecbc5e5167ffb0afd6b7f0396550bbb955e923d
Michal Simek <michal.simek@xilinx.com>
2013-11-21 13:39:02 -0800

    common: Add new clk command
    Command provides just dump subcommand for showing clock
    frequencies in a soc.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/clk.h
@@ -0,0 +1,6 @@
+#ifndef _CLK_H_
+#define _CLK_H_
+
+int soc_clk_dump(void);
+
+#endif /* _CLK_H_ */
--- include/config_cmd_all.h
+++ include/config_cmd_all.h
@@ -23,6 +23,7 @@
 #define CONFIG_CMD_BSP		/* Board Specific functions	*/
 #define CONFIG_CMD_CACHE	/* icache, dcache		*/
 #define CONFIG_CMD_CDP		/* Cisco Discovery Protocol	*/
+#define CONFIG_CMD_CLK		/* Clock support		*/
 #define CONFIG_CMD_CONSOLE	/* coninfo			*/
 #define CONFIG_CMD_DATE		/* support for RTC, date/time...*/
 #define CONFIG_CMD_DHCP		/* DHCP Support			*/
