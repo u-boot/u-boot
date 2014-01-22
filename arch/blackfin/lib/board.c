861aa63d6b914006256a283a5276b245b6f71a64
Sonic Zhang <sonic.zhang@analog.com>
2014-01-22 16:04:19 +0800

    blackfin: Change SMC dcplb entry flag to cover 16M address region
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- arch/blackfin/lib/board.c
+++ arch/blackfin/lib/board.c
@@ -142,7 +142,8 @@ void init_cplbtables(void)
 	++i;
 #if defined(__ADSPBF60x__)
 	icplb_add(0x0, 0x0);
-	dcplb_add(CONFIG_SYS_FLASH_BASE, SDRAM_EBIU);
+	dcplb_add(CONFIG_SYS_FLASH_BASE, PAGE_SIZE_16MB | CPLB_DIRTY |
+		CPLB_SUPV_WR | CPLB_USER_WR | CPLB_USER_RD | CPLB_VALID);
 	++i;
 #endif
 
