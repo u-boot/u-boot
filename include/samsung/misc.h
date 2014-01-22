9d29a682a8dbe40b8d1af45197a87d49472fc105
Przemyslaw Marczak <p.marczak@samsung.com>
2014-01-22 11:24:17 +0100

    samsung: misc: Add LCD download menu.
    This simple LCD menu allows run one of download mode on device
    without writing on console or for fast and easy upgrade.
    
    This feature check user keys combination at boot:
    - power key + volume up - download menu
    - power key + volume down - thor mode (without menu)
    
    New configs:
    - CONFIG_LCD_MENU
    - CONFIG_LCD_MENU_BOARD
    
    For proper effect this feature needs following definitions:
    
    Power key:
    - KEY_PWR_PMIC_NAME - (string) pmic which supports power key check
    
    Register address:
    - KEY_PWR_STATUS_REG
    - KEY_PWR_INTERRUPT_REG
    
    Register power key mask:
    - KEY_PWR_STATUS_MASK
    - KEY_PWR_INTERRUPT_MASK
    
    Gpio numbers:
    - KEY_PWR_INTERRUPT_MASK
    - KEY_VOL_DOWN_GPIO
    
    Functions needs to be called:
    - keys_init() - for set proper gpio direction
    - check_boot_mode() - menu - main function
    
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- include/samsung/misc.h
+++ include/samsung/misc.h
@@ -1,6 +1,23 @@
 #ifndef __SAMSUNG_MISC_COMMON_H__
 #define __SAMSUNG_MISC_COMMON_H__
 
+#ifdef CONFIG_LCD_MENU
+enum {
+	BOOT_MODE_INFO,
+	BOOT_MODE_THOR,
+	BOOT_MODE_UMS,
+	BOOT_MODE_DFU,
+	BOOT_MODE_EXIT,
+};
+
+#ifdef CONFIG_REVISION_TAG
+u32 get_board_rev(void);
+#endif
+
+void keys_init(void);
+void check_boot_mode(void);
+#endif /* CONFIG_LCD_MENU */
+
 #ifdef CONFIG_CMD_BMP
 void draw_logo(void);
 #endif
