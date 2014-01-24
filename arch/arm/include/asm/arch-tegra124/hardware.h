999c6baf79d74f6ec5c6c96334ad8d653b4081a7
Tom Warren <twarren.nvidia@gmail.com>
2014-01-24 12:46:13 -0700

    ARM: tegra: add/edit headers for Tegra124
    These headers define the Tegra124 hardware. Add them to the usual
    place.
    
    Add Tegra124 chip ID/SKU ID definitions to common headers.
    
    There's no real HW change on Tegra124 for 90% of the toys, so it might
    make sense for a future patch to unify some of the content of these
    files in a common location.
    
    Signed-off-by: Tom Warren <twarren@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Tested-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

---
+++ arch/arm/include/asm/arch-tegra124/hardware.h
@@ -0,0 +1,16 @@
+/*
+ * (C) Copyright 2013
+ * NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _TEGRA124_HARDWARE_H_
+#define _TEGRA124_HARDWARE_H_
+
+/*
+ * Include Tegra-specific hardware definitions
+ * Nothing needed currently for Tegra124
+ */
+
+#endif /* _TEGRA124_HARDWARE_H_ */
---
+++ arch/arm/include/asm/arch-tegra124/pmu.h
@@ -0,0 +1,14 @@
+/*
+ * (C) Copyright 2010-2013
+ * NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _TEGRA124_PMU_H_
+#define _TEGRA124_PMU_H_
+
+/* Set core and CPU voltages to nominal levels */
+int pmu_set_nominal(void);
+
+#endif	/* _TEGRA124_PMU_H_ */
