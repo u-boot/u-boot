a35ad51efe363abdc72473b32aea2c1bb271995f
Mugunthan V N <mugunthanvnm@ti.com>
2014-02-18 07:31:55 -0500

    ARM: AM335x: add support for reading cpsw 2nd mac address from efuse
    Adding support for reading cpsw 2nd mac address from efuse and pass it
    to kernel via dtb which will be used in dual emac mode of cpsw.
    Also adding mii command support to am335x common config.
    
    Acked-by: Tom Rini <trini@ti.com>
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- board/ti/am335x/board.c
+++ board/ti/am335x/board.c
@@ -602,6 +602,21 @@ int board_eth_init(bd_t *bis)
 	}
 
 #ifdef CONFIG_DRIVER_TI_CPSW
+
+	mac_lo = readl(&cdev->macid1l);
+	mac_hi = readl(&cdev->macid1h);
+	mac_addr[0] = mac_hi & 0xFF;
+	mac_addr[1] = (mac_hi & 0xFF00) >> 8;
+	mac_addr[2] = (mac_hi & 0xFF0000) >> 16;
+	mac_addr[3] = (mac_hi & 0xFF000000) >> 24;
+	mac_addr[4] = mac_lo & 0xFF;
+	mac_addr[5] = (mac_lo & 0xFF00) >> 8;
+
+	if (!getenv("eth1addr")) {
+		if (is_valid_ether_addr(mac_addr))
+			eth_setenv_enetaddr("eth1addr", mac_addr);
+	}
+
 	if (read_eeprom(&header) < 0)
 		puts("Could not get board ID.\n");
 
