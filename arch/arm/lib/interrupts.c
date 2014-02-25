1551df35f296f0a8df32f4f2054254f46e8be252
Tom Rini <trini@ti.com>
2014-02-25 10:27:01 -0500

    arm: Switch to -mno-unaligned-access when supported by the compiler
    When we tell the compiler to optimize for ARMv7 (and ARMv6 for that
    matter) it assumes a default of SCTRL.A being cleared and unaligned
    accesses being allowed and fast at the hardware level.  We set this bit
    and must pass along -mno-unaligned-access so that the compiler will
    still breakdown accesses and not trigger a data abort.
    
    To better help understand the requirements of the project with respect
    to unaligned memory access, the
    Documentation/unaligned-memory-access.txt file has been added as
    doc/README.unaligned-memory-access.txt and is taken from the v3.14-rc1
    tag of the kernel.
    
    Cc: Albert ARIBAUD <albert.u.boot@aribaud.net>
    Cc: Mans Rullgard <mans@mansr.com>
    Signed-off-by: Tom Rini <trini@ti.com>

--- arch/arm/lib/interrupts.c
+++ arch/arm/lib/interrupts.c
@@ -153,7 +153,7 @@ void do_prefetch_abort (struct pt_regs *pt_regs)
 
 void do_data_abort (struct pt_regs *pt_regs)
 {
-	printf ("data abort\n\n    MAYBE you should read doc/README.arm-unaligned-accesses\n\n");
+	printf ("data abort\n");
 	show_regs (pt_regs);
 	bad_mode ();
 }
