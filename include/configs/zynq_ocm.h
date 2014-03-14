59981d20462859d6ed7f2e84c9cca8157325afd2
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2014-03-14 18:51:21 +0530

    zynq_ocm: Add support to save environment to EEPROM
    Added support to save enironment to EEPROM for zynq OCM.
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/zynq_ocm.h
+++ include/configs/zynq_ocm.h
@@ -14,12 +14,12 @@
 #define CONFIG_ZYNQ_SERIAL_UART1
 #define _CONFIG_CMD_DEFAULT_H
 #define CONFIG_SKIP_LOWLEVEL_INIT
-#define CONFIG_ENV_IS_NOWHERE
 #define CONFIG_SYS_DCACHE_OFF
 #define CONFIG_SYS_ICACHE_OFF
 #define CONFIG_ZYNQ_OCM
 #define CONFIG_ZYNQ_SDHCI0
 #define CONFIG_ZYNQ_QSPI
+#define CONFIG_ZYNQ_EEPROM
 
 #include <configs/zynq-common.h>
 
@@ -49,15 +49,21 @@
 #undef CONFIG_FAT_WRITE
 #undef CONFIG_CMD_EXT4
 #undef CONFIG_CMD_EXT4_WRITE
+#undef CONFIG_ENV_IS_IN_SPI_FLASH
+#undef CONFIG_ENV_OFFSET
+#undef CONFIG_ENV_SECT_SIZE
 
 /* Define needed configs */
 #define CONFIG_CMD_MEMORY
 #define CONFIG_BOOTDELAY	-1 /* -1 to Disable autoboot */
 #define CONFIG_SYS_MALLOC_LEN	0x4000
+#define CONFIG_ENV_IS_IN_EEPROM
+#define CONFIG_ENV_OFFSET		0x0
+#define CONFIG_ENV_SECT_SIZE		255
+#define CONFIG_ENV_SIZE			1000
 
 #define CONFIG_SYS_SDRAM_SIZE		((256 * 1024) - 32)
 #define CONFIG_SYS_SDRAM_BASE		0xFFFC0000
-#define CONFIG_ENV_SIZE			1400
 
 #define FAT_BUFF_PTR_OCM		0xFFFC5000
 /* Define the cluster size fat filesystem */
