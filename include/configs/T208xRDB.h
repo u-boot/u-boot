4feac1c661d13b86bca8eef289c55c81ed2989ca
Shengzhou Liu <Shengzhou.Liu@freescale.com>
2014-04-02 14:28:35 +0800

    powerpc/T208xRDB: add mtdparts suppport
    We use dynamical mtdparts partition instead of directly puting
    mtd partitions nodes in device tree.
    
    Signed-off-by: Shengzhou Liu <Shengzhou.Liu@freescale.com>
    Reviewed-by: York Sun <yorksun@freescale.com>

--- include/configs/T208xRDB.h
+++ include/configs/T208xRDB.h
@@ -616,6 +616,22 @@ unsigned long get_board_ddr_clk(void);
 #endif
 
 /*
+ * Dynamic MTD Partition support with mtdparts
+ */
+#ifndef CONFIG_SYS_NO_FLASH
+#define CONFIG_MTD_DEVICE
+#define CONFIG_MTD_PARTITIONS
+#define CONFIG_CMD_MTDPARTS
+#define CONFIG_FLASH_CFI_MTD
+#define MTDIDS_DEFAULT "nor0=fe8000000.nor,nand0=fff800000.flash," \
+			"spi0=spife110000.1"
+#define MTDPARTS_DEFAULT "mtdparts=fe8000000.nor:1m(uboot),5m(kernel)," \
+			"128k(dtb),96m(fs),-(user);fff800000.flash:1m(uboot)," \
+			"5m(kernel),128k(dtb),96m(fs),-(user);spife110000.1:" \
+			"1m(uboot),5m(kernel),128k(dtb),-(user)"
+#endif
+
+/*
  * Environment
  */
 
