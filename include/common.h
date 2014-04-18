c85bb5a01a5a87c4937665816b17e281f9552413
Masahiro Yamada <yamada.m@jp.panasonic.com>
2014-04-18 19:09:47 +0900

    rand: do not surround function declarations by #ifdef
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>

--- include/common.h
+++ include/common.h
@@ -820,12 +820,10 @@ char *	strmhz(char *buf, unsigned long hz);
 #include <u-boot/crc.h>
 
 /* lib/rand.c */
-#if defined(CONFIG_LIB_RAND) || defined(CONFIG_LIB_HW_RAND)
 #define RAND_MAX -1U
 void srand(unsigned int seed);
 unsigned int rand(void);
 unsigned int rand_r(unsigned int *seedp);
-#endif
 
 /* common/console.c */
 int	console_init_f(void);	/* Before relocation; uses the serial  stuff	*/
