4067815998a45ed18e4e42ac9cf655ad07d1d4df
Aneesh Bansal <aneesh.bansal@freescale.com>
2014-04-22 15:16:48 +0530

    powerpc/mpc85xx: SECURE BOOT- secure boot target for t1040rdb
    T1040RDB.h file is removed and a unified file T104xRDB.h is created.
    Hence macro CONFIG_T1040 is renamed to CONFIG_T104x.
    
    Signed-off-by: Gaurav Kumar Rana <gaurav.rana@freescale.com>
    Signed-off-by: Aneesh Bansal <aneesh.bansal@freescale.com>

--- arch/powerpc/include/asm/fsl_secure_boot.h
+++ arch/powerpc/include/asm/fsl_secure_boot.h
@@ -21,7 +21,7 @@
 	defined(CONFIG_T4240QDS) || \
 	defined(CONFIG_T2080QDS) || \
 	defined(CONFIG_T1040QDS) || \
-	defined(CONFIG_T1040RDB)
+	defined(CONFIG_T104xRDB)
 #define CONFIG_SYS_CPC_REINIT_F
 #undef CONFIG_SYS_INIT_L3_ADDR
 #define CONFIG_SYS_INIT_L3_ADDR			0xbff00000
