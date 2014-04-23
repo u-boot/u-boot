56d83d1c046c693b65ab09c0e960d922ec639c2b
Stefan Agner <stefan@agner.ch>
2014-04-23 18:17:51 +0200

    arm: vf610: add DDR_SEL_PAD_CONTR register
    Set DDR_SEL_PAD_CONTR register explicitly to DDR3 which solves RAM
    issues with newer silicon (1.1). This register was added in revision
    4 of the Vybrid Reference Manual.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- board/freescale/vf610twr/vf610twr.c
+++ board/freescale/vf610twr/vf610twr.c
@@ -217,7 +217,8 @@ void ddr_ctrl_init(void)
 		&ddrmr->cr[139]);
 
 	writel(DDRMC_CR154_PAD_ZQ_EARLY_CMP_EN_TIMER(13) |
-		DDRMC_CR154_PAD_ZQ_MODE(1), &ddrmr->cr[154]);
+		DDRMC_CR154_PAD_ZQ_MODE(1) |
+		DDRMC_CR154_DDR_SEL_PAD_CONTR(3), &ddrmr->cr[154]);
 	writel(DDRMC_CR155_AXI0_AWCACHE | DDRMC_CR155_PAD_ODT_BYTE1(2),
 		&ddrmr->cr[155]);
 	writel(DDRMC_CR158_TWR(6), &ddrmr->cr[158]);
