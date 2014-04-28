488c47a141179fc3fa10779be6268e56f88ec18d
Masahiro Yamada <yamada.m@jp.panasonic.com>
2014-04-28 10:18:34 +0900

    config: remove platform CONFIG_SYS_HZ definition part 4
    Some new boards define CONFIG_SYS_HZ again! Remove.
    
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>
    Acked-by: Bo Shen <voice.shen@atmel.com>

--- include/configs/T208xRDB.h
+++ include/configs/T208xRDB.h
@@ -750,7 +750,6 @@ unsigned long get_board_ddr_clk(void);
 #define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE+sizeof(CONFIG_SYS_PROMPT)+16)
 #define CONFIG_SYS_MAXARGS	16	/* max number of command args */
 #define CONFIG_SYS_BARGSIZE	CONFIG_SYS_CBSIZE/* Boot Argument Buffer Size */
-#define CONFIG_SYS_HZ		1000	/* decrementer freq: 1ms ticks*/
 
 /*
  * For booting Linux, the board info and command line data
