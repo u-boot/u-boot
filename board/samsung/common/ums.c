dc1e746c42d440fcd70aaaf02a1bbce5a4b2a5e6
Stephen Warren <swarren@nvidia.com>
2014-05-05 10:40:13 -0600

    ums: remove error-checking of MMC device size
    There's no reason to believe that an MMC device will incorrectly report
    its capacity. Remove error checking of this value from ums_disk_init()
    to simplify it.
    
    Cc: Lukasz Majewski <l.majewski@samsung.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Przemyslaw Marczak <p.marczak@samsung.com>

--- board/samsung/common/ums.c
+++ board/samsung/common/ums.c
@@ -38,16 +38,9 @@ static struct ums ums_dev = {
 
 static struct ums *ums_disk_init(struct mmc *mmc)
 {
-	uint64_t mmc_end_sector = mmc->capacity / SECTOR_SIZE;
-
-	if (!mmc_end_sector) {
-		error("MMC capacity is not valid");
-		return NULL;
-	}
-
 	ums_dev.block_dev = &mmc->block_dev;
 	ums_dev.start_sector = 0;
-	ums_dev.num_sectors = mmc_end_sector;
+	ums_dev.num_sectors = mmc->capacity / SECTOR_SIZE;
 
 	printf("UMS: disk start sector: %#x, count: %#x\n",
 	       ums_dev.start_sector, ums_dev.num_sectors);
