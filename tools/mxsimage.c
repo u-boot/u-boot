25308f45e11527cbfc7ff6d9dced7800e4b976e4
Charles Manning <cdhmanning@gmail.com>
2014-05-14 14:45:01 +1200

    tools: Refactor mxsimage to use pbl_crc32
    Both pblimage and mxsimage use the same crc algorithm, so refactor.
    
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>

--- tools/mxsimage.c
+++ tools/mxsimage.c
@@ -19,6 +19,7 @@
 
 #include "imagetool.h"
 #include "mxsimage.h"
+#include "pbl_crc32.h"
 #include <image.h>
 
 
@@ -228,29 +229,6 @@ static int sb_aes_reinit(struct sb_image_ctx *ictx, int enc)
 	if (!ret)
 		return ret;
 	return sb_aes_init(ictx, iv, enc);
-}
-
-/*
- * CRC32
- */
-static uint32_t crc32(uint8_t *data, uint32_t len)
-{
-	const uint32_t poly = 0x04c11db7;
-	uint32_t crc32 = 0xffffffff;
-	unsigned int byte, bit;
-
-	for (byte = 0; byte < len; byte++) {
-		crc32 ^= data[byte] << 24;
-
-		for (bit = 8; bit > 0; bit--) {
-			if (crc32 & (1UL << 31))
-				crc32 = (crc32 << 1) ^ poly;
-			else
-				crc32 = (crc32 << 1);
-		}
-	}
-
-	return crc32;
 }
 
 /*
@@ -998,7 +976,9 @@ static int sb_build_command_load(struct sb_image_ctx *ictx,
 
 	ccmd->load.address	= dest;
 	ccmd->load.count	= cctx->length;
-	ccmd->load.crc32	= crc32(cctx->data, cctx->length);
+	ccmd->load.crc32	= pbl_crc32(0,
+					    (const char *)cctx->data,
+					    cctx->length);
 
 	cctx->size = sizeof(*ccmd) + cctx->length;
 
@@ -1834,7 +1814,9 @@ static int sb_verify_command(struct sb_image_ctx *ictx,
 		EVP_DigestUpdate(&ictx->md_ctx, cctx->data, asize);
 		sb_aes_crypt(ictx, cctx->data, cctx->data, asize);
 
-		if (ccmd->load.crc32 != crc32(cctx->data, asize)) {
+		if (ccmd->load.crc32 != pbl_crc32(0,
+						  (const char *)cctx->data,
+						  asize)) {
 			fprintf(stderr,
 				"ERR: SB LOAD command payload CRC32 invalid!\n");
 			return -EINVAL;
