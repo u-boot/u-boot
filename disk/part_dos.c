e04350d2991ed628587e94b5b6d89c24f439e172
Steve Rae <srae@broadcom.com>
2014-05-26 11:52:23 -0700

    disk: part_efi: clarify lbaint_t usage
    - update the comments regarding lbaint_t usage
    - cleanup casting of values related to the lbaint_t type
    - cleanup of a type that requires a u64
    
    Tested on little endian ARMv7 and ARMv8 configurations
    
    Signed-off-by: Steve Rae <srae@broadcom.com>

--- disk/part_dos.c
+++ disk/part_dos.c
@@ -199,8 +199,9 @@ static int get_partition_info_extended (block_dev_desc_t *dev_desc, int ext_part
 		    (part_num == which_part) &&
 		    (is_extended(pt->sys_ind) == 0)) {
 			info->blksz = 512;
-			info->start = ext_part_sector + le32_to_int (pt->start4);
-			info->size  = le32_to_int (pt->size4);
+			info->start = (lbaint_t)(ext_part_sector +
+					le32_to_int(pt->start4));
+			info->size  = (lbaint_t)le32_to_int(pt->size4);
 			switch(dev_desc->if_type) {
 				case IF_TYPE_IDE:
 				case IF_TYPE_SATA:
