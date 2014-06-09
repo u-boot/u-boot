745325a97d172a71dea4ec7528224ed63973d601
Hans de Goede <hdegoede@redhat.com>
2014-06-09 11:36:57 +0200

    sunxi: Add sun4i support
    Add support for the Allwinner A10 SoC also known as the Allwinner sun4i family,
    and add the Cubieboard board which uses the A10 SoC.
    
    Compared to sun7 only the DRAM controller is a bit different:
    -Controller reset bits are inverted, but only for Rev. A
    -Different hpcr values
    -No MBUS on sun4i
    -Various other initialization changes
    
    Signed-off-by: Henrik Nordstrom <henrik@henriknordstrom.net>
    Signed-off-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Oliver Schinagl <oliver@schinagl.nl>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Ian Campbell <ijc@hellion.org.uk>

--- arch/arm/cpu/armv7/sunxi/cpu_info.c
+++ arch/arm/cpu/armv7/sunxi/cpu_info.c
@@ -13,7 +13,14 @@
 #ifdef CONFIG_DISPLAY_CPUINFO
 int print_cpuinfo(void)
 {
+#ifdef CONFIG_SUN4I
+	puts("CPU:   Allwinner A10 (SUN4I)\n");
+#elif defined CONFIG_SUN7I
 	puts("CPU:   Allwinner A20 (SUN7I)\n");
+#else
+#warning Please update cpu_info.c with correct CPU information
+	puts("CPU:   SUNXI Family\n");
+#endif
 	return 0;
 }
 #endif
