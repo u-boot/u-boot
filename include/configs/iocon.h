7d2357c1999ff1f93f795282526230a8bd176106
Michal Simek <michal.simek@xilinx.com>
2014-06-17 00:36:14 +0200

    configs: iocon: Enabling CONFIG_CMD_FPGAD again
    This is a MIME GnuPG-signed message.  If you see this text, it means that
    your E-mail or Usenet software does not support MIME signed messages.
    The Internet standard for MIME PGP messages, RFC 2015, was published in 1996.
    To open this message correctly you will need to install E-mail or Usenet
    software that supports modern Internet standards.
    
    Revert changes in iocon.h config file caused by
    these two commits:
    "configs: iocom: Fix typo on CMD_FPGA command"
    (sha1: d0db28f94034ef02c1d6737895766fb3c19de47f)
    and
    "fpga: Guard the LOADMK functionality with CMD_FPGA_LOADMK"
    (sha1: 64e809afeaf1572c3246a5bca198a77d0498fd89)
    
    CONFIG_CMD_FPGAD is own command.
    
    Reported-by: Dirk Eibach <dirk.eibach@gdsys.cc>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/iocon.h
+++ include/configs/iocon.h
@@ -62,8 +62,7 @@
  * Commands additional to the ones defined in amcc-common.h
  */
 #define CONFIG_CMD_CACHE
-#define CONFIG_CMD_FPGA
-#define CONFIG_CMD_FPGA_LOADMK
+#define CONFIG_CMD_FPGAD
 #undef CONFIG_CMD_EEPROM
 
 /*
