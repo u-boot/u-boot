9d0456822c4fec302608d4161ae9d9cfe81100b1
York Sun <yorksun@freescale.com>
2014-06-24 21:16:20 -0700

    powerpc/mpc85xx: Check return value of find_tlb_idx
    find_tlb_idx() is called in board_early_init_r() on multiple boards.
    The return value is not checked before being used to disable a TLB.
    In normal case the return value wouldn't be -1. In case of a mis-
    configuration during porting to a new board, checking the return value
    may be helpful to reveal some user errors.
    
    Signed-off-by: York Sun <yorksun@freescale.com>

--- board/freescale/p1_twr/p1_twr.c
+++ board/freescale/p1_twr/p1_twr.c
@@ -155,7 +155,7 @@ void pci_init_board(void)
 int board_early_init_r(void)
 {
 	const unsigned int flashbase = CONFIG_SYS_FLASH_BASE;
-	const u8 flash_esel = find_tlb_idx((void *)flashbase, 1);
+	int flash_esel = find_tlb_idx((void *)flashbase, 1);
 
 	/*
 	 * Remap Boot flash region to caching-inhibited
@@ -166,8 +166,14 @@ int board_early_init_r(void)
 	flush_dcache();
 	invalidate_icache();
 
-	/* invalidate existing TLB entry for flash */
-	disable_tlb(flash_esel);
+	if (flash_esel == -1) {
+		/* very unlikely unless something is messed up */
+		puts("Error: Could not find TLB for FLASH BASE\n");
+		flash_esel = 2;	/* give our best effort to continue */
+	} else {
+		/* invalidate existing TLB entry for flash */
+		disable_tlb(flash_esel);
+	}
 
 	set_tlb(1, flashbase, CONFIG_SYS_FLASH_BASE_PHYS, /* tlb, epn, rpn */
 		MAS3_SW|MAS3_SR, MAS2_I|MAS2_G,           /* perms, wimge */
