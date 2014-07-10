072f210a0d394ba9083323b8bbce6822f1cc2ecb
Jeroen Hofstee <jeroen@myspectrum.nl>
2014-07-10 22:21:11 +0200

    lcd: remove unused lcd_puts_xy
    prevents a clang warning that the function is
    never used.
    
    cc: agust@denx.de
    Signed-off-by: Jeroen Hofstee <jeroen@myspectrum.nl>

--- common/lcd.c
+++ common/lcd.c
@@ -110,7 +110,6 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 static void lcd_drawchars(ushort x, ushort y, uchar *str, int count);
-static inline void lcd_puts_xy(ushort x, ushort y, uchar *s);
 static inline void lcd_putc_xy(ushort x, ushort y, uchar  c);
 
 static int lcd_init(void *lcdbase);
@@ -372,15 +371,6 @@ static void lcd_drawchars(ushort x, ushort y, uchar *str, int count)
 #endif
 	}
 }
-
-/*----------------------------------------------------------------------*/
-
-static inline void lcd_puts_xy(ushort x, ushort y, uchar *s)
-{
-	lcd_drawchars(x, y, s, strlen((char *)s));
-}
-
-/*----------------------------------------------------------------------*/
 
 static inline void lcd_putc_xy(ushort x, ushort y, uchar c)
 {
