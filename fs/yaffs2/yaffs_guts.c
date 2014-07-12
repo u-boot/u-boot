e803fa2c4b9313a9efc7b9f87a49839d542d9615
Jeroen Hofstee <jeroen@myspectrum.nl>
2014-07-12 15:16:51 +0200

    yaffs2: cosmetic: remove self assignments
    Remove self assignments which is just dead code to prevent
    compiler warnings about non used arguments. For u-boot this
    does not prevent any warning though, on the contrary it actual
    introduces warnings when compiling with clang. Remove them.
    
    Signed-off-by: Jeroen Hofstee <jeroen@myspectrum.nl>

--- fs/yaffs2/yaffs_guts.c
+++ fs/yaffs2/yaffs_guts.c
@@ -219,18 +219,11 @@ static void yaffs_handle_chunk_wr_ok(struct yaffs_dev *dev, int nand_chunk,
 				     const u8 *data,
 				     const struct yaffs_ext_tags *tags)
 {
-	dev = dev;
-	nand_chunk = nand_chunk;
-	data = data;
-	tags = tags;
 }
 
 static void yaffs_handle_chunk_update(struct yaffs_dev *dev, int nand_chunk,
 				      const struct yaffs_ext_tags *tags)
 {
-	dev = dev;
-	nand_chunk = nand_chunk;
-	tags = tags;
 }
 
 void yaffs_handle_chunk_error(struct yaffs_dev *dev,
@@ -813,8 +806,6 @@ struct yaffs_tnode *yaffs_find_tnode_0(struct yaffs_dev *dev,
 	u32 i;
 	int required_depth;
 	int level = file_struct->top_level;
-
-	dev = dev;
 
 	/* Check sane level and chunk Id */
 	if (level < 0 || level > YAFFS_TNODES_MAX_LEVEL)
@@ -3214,7 +3205,6 @@ static void yaffs_load_name_from_oh(struct yaffs_dev *dev, YCHAR *name,
 		}
 	} else {
 #else
-	dev = dev;
 	{
 #endif
 		yaffs_strncpy(name, oh_name, buff_size - 1);
@@ -3258,7 +3248,6 @@ static void yaffs_load_oh_from_name(struct yaffs_dev *dev, YCHAR *oh_name,
 		}
 	} else {
 #else
-	dev = dev;
 	{
 #endif
 		yaffs_strncpy(oh_name, name, YAFFS_MAX_NAME_LENGTH - 1);
