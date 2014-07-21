842d9d81a9173ae2edae51059536b667051d4d31
Michal Simek <michal.simek@xilinx.com>
2014-07-21 13:22:10 +0200

    zynqmp: Provide actual ttc and clk frequency
    Based on platform selection provides clock settings.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/cpu/armv8/zynqmp/clk.c
+++ arch/arm/cpu/armv8/zynqmp/clk.c
@@ -5,12 +5,25 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
+#include <common.h>
+#include <asm/arch/hardware.h>
+#include <asm/arch/sys_proto.h>
+
 unsigned long get_uart_clk(int dev_id)
 {
-	return 25000000;
+	u32 ver = zynqmp_get_silicon_version();
+
+	switch (ver) {
+	case ZYNQMP_CSU_VERSION_VELOCE:
+		return 400000;
+	case ZYNQMP_CSU_VERSION_EP108:
+		return 25000000;
+	}
+
+	return 133000000;
 }
 
 unsigned long get_ttc_clk(int dev_id)
 {
-	return 25000000;
+	return get_uart_clk(dev_id);
 }
