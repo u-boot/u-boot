906f609681bdf8341fbf313ee2455a4f56906f08
Hans de Goede <hdegoede@redhat.com>
2014-07-26 19:08:11 +0200

    sun4i: add USB EHCI settings
    Specific USB EHCI settings to be set for sun4i if CONFIG_USB_EHCI is enabled.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Ian Campbell <ijc@hellion.org.uk>

--- include/configs/sun4i.h
+++ include/configs/sun4i.h
@@ -16,6 +16,18 @@
 
 #define CONFIG_SYS_PROMPT		"sun4i# "
 
+#ifdef CONFIG_USB_EHCI
+#define CONFIG_USB_EHCI_SUNXI
+
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	2
+#ifndef CONFIG_SUNXI_USB_VBUS0_GPIO
+#define CONFIG_SUNXI_USB_VBUS0_GPIO	SUNXI_GPH(6)
+#endif
+#ifndef CONFIG_SUNXI_USB_VBUS1_GPIO
+#define CONFIG_SUNXI_USB_VBUS1_GPIO	SUNXI_GPH(3)
+#endif
+#endif
+
 /*
  * Include common sunxi configuration where most the settings are
  */
