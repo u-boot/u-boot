3400655e0f3cb6a8ca2b486137e96f576c9cd2fc
Vasili Galka <vvv444@gmail.com>
2014-08-26 14:05:31 +0300

    mpc5xxx: Add stub implementation of cache functions
    Some drivers (e.g. net/e1000) reference these functions. So, this
    fixes the build of MVBC_P board.
    
    I'm not familiar with the MPC5xxx platform, maybe a full
    implementation shall be implemented instead of this stub in the
    future.
    
    Signed-off-by: Vasili Galka <vvv444@gmail.com>
    Cc: Wolfgang Denk <wd@denx.de>, Marek Vasut <marex@denx.de>
    Acked-by: Wolfgang Denk <wd@denx.de>

---
+++ arch/powerpc/cpu/mpc5xxx/cache.c
@@ -0,0 +1,15 @@
+/*
+ * This file contains stub implementation of
+ *   invalidate_dcache_range()
+ *   flush_dcache_range()
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+void invalidate_dcache_range(unsigned long start, unsigned long stop)
+{
+}
+
+void flush_dcache_range(unsigned long start, unsigned long stop)
+{
+}
