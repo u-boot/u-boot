c69d2e57616f20eb1989cfe235ee036a26c78d5a
Thomas Chou <thomas@wytron.com.tw>
2014-08-28 17:29:06 +0800

    nios2: link to CONFIG_SYS_MONITOR_BASE and remove text_base hook
    This patch changes the link script to base at CONFIG_SYS_MONITOR_BASE.
    Then we can remove the text_base hook in nios2-generic board.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- board/altera/nios2-generic/nios2-generic.c
+++ board/altera/nios2-generic/nios2-generic.c
@@ -14,8 +14,6 @@
 #include <asm/io.h>
 #include <asm/gpio.h>
 
-void text_base_hook(void); /* nop hook for text_base.S */
-
 #if defined(CONFIG_ENV_IS_IN_FLASH) && defined(CONFIG_ENV_ADDR) && \
     defined(CONFIG_CFI_FLASH_MTD)
 static void __early_flash_cmd_reset(void)
@@ -30,7 +28,6 @@ void early_flash_cmd_reset(void)
 
 int board_early_init_f(void)
 {
-	text_base_hook();
 #ifdef CONFIG_ALTERA_PIO
 #ifdef LED_PIO_BASE
 	altera_pio_init(LED_PIO_BASE, LED_PIO_WIDTH, 'o',
