1de271b487d60c613568ad61fc005ff850f2ed71
Shaveta Leekha <shaveta@freescale.com>
2014-09-04 16:17:09 +0530

    B4860QDS: Enable mac command support
    One of the I2C EEPROM is used to store/save and edit mac
    addresses of ports.
    this patch add required CONFIG to support the same
    
    Signed-off-by: Shaveta Leekha <shaveta@freescale.com>

--- include/configs/B4860QDS.h
+++ include/configs/B4860QDS.h
@@ -227,6 +227,7 @@ unsigned long get_board_ddr_clk(void);
 #endif
 
 /* EEPROM */
+#define CONFIG_ID_EEPROM
 #define CONFIG_SYS_I2C_EEPROM_NXID
 #define CONFIG_SYS_EEPROM_BUS_NUM	0
 #define CONFIG_SYS_I2C_EEPROM_ADDR	0x57
