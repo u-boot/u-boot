1f7278851ea359063dabc235ff690a5010467956
Vadim Bendebury <vbendeb@chromium.org>
2014-09-04 10:45:13 -0700

    patman: make run results better visible
    For an occasional user of patman some failures are not obvious: for
    instance when checkpatch reports warnings, the dry run still reports
    that the email would be sent. If it is not dry run, the warnings are
    shown on the screen, but it is not clear that the email was not sent.
    
    Add some code to report failure to send email explicitly.
    
    Tested by running the script on a patch with style violations,
    observed error messages in the script output.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Acked-by: Simon Glass <sjg@chromium.org>

--- tools/patman/patman.py
+++ tools/patman/patman.py
@@ -146,13 +146,18 @@ else:
 
     # Email the patches out (giving the user time to check / cancel)
     cmd = ''
-    if ok or options.ignore_errors:
+    its_a_go = ok or options.ignore_errors
+    if its_a_go:
         cmd = gitutil.EmailPatches(series, cover_fname, args,
                 options.dry_run, not options.ignore_bad_tags, cc_file,
                 in_reply_to=options.in_reply_to)
+    else:
+        print col.Color(col.RED, "Not sending emails due to errors/warnings")
 
     # For a dry run, just show our actions as a sanity check
     if options.dry_run:
         series.ShowActions(args, cmd, options.process_tags)
+        if not its_a_go:
+            print col.Color(col.RED, "Email would not be sent")
 
     os.remove(cc_file)
