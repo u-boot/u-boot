9e8f664ecb25110c623d0385735db27596330ee7
Vadim Bendebury <vbendeb@chromium.org>
2014-09-05 16:53:34 +0530

    video: Add driver for Parade PS8625 dP to LVDS bridge
    The initialization table comes from the "Illustration of I2C command
    for initialing PS8625" document supplied by Parade.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Signed-off-by: Ajay Kumar <ajaykumar.rs@samsung.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Tested-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- lib/fdtdec.c
+++ lib/fdtdec.c
@@ -71,6 +71,7 @@ static const char * const compat_names[COMPAT_COUNT] = {
 	COMPAT(TI_TPS65090, "ti,tps65090"),
 	COMPAT(COMPAT_NXP_PTN3460, "nxp,ptn3460"),
 	COMPAT(SAMSUNG_EXYNOS_SYSMMU, "samsung,sysmmu-v3.3"),
+	COMPAT(PARADE_PS8625, "parade,ps8625"),
 };
 
 const char *fdtdec_get_compatible(enum fdt_compat_id id)
