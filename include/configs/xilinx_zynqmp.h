33f66ec81e4013f7ce3adf11f7e39bcd17ccd1e0
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2014-09-10 11:33:49 +0530

    zynqmp: Add FAT and EXT4 write support
    Add FAT and EXT4 write support for zynqmp.
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp.h
+++ include/configs/xilinx_zynqmp.h
@@ -75,6 +75,8 @@
 #define CONFIG_CMD_CACHE
 #define CONFIG_CMD_ECHO
 #define CONFIG_CMD_ENV
+#define CONFIG_CMD_EXT2
+#define CONFIG_CMD_EXT4
 #define CONFIG_CMD_FAT
 #define CONFIG_CMD_FLASH
 #define CONFIG_CMD_IMI
@@ -117,6 +119,11 @@
 # define CONFIG_SDHCI
 # define CONFIG_ZYNQ_SDHCI
 # define CONFIG_CMD_MMC
+#endif
+
+#if defined (CONFIG_ZYNQ_SDHCI) || defined(CONFIG_ZYNQ_USB)
+# define CONFIG_FAT_WRITE
+# define CONFIG_CMD_EXT4_WRITE
 #endif
 
 /* Miscellaneous configurable options */
