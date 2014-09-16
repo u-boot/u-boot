b0928da648d440778512ddd81e690717239b8f1f
Masahiro Yamada <yamada.m@jp.panasonic.com>
2014-09-16 16:32:59 +0900

    kconfig: move CONFIG_CC_OPTIMIZE_LIBS_FOR_SPEED to Kconfig
    If this option is enabled, the objects under lib/ directory
    are compiled with speed optimization, not size optimization.
    (Currently, only used by some Blackfin boards.)
    
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Tested-by: Simon Glass <sjg@chromium.org>

--- include/configs/bf533-stamp.h
+++ include/configs/bf533-stamp.h
@@ -186,7 +186,6 @@
  */
 #define CONFIG_RTC_BFIN
 #define CONFIG_UART_CONSOLE	0
-#define CONFIG_CC_OPTIMIZE_LIBS_FOR_SPEED
 
 /* FLASH/ETHERNET uses the same async bank */
 #define SHARED_RESOURCES 	1
