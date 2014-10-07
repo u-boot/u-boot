7ac1a24a85b9e961f8f26a9c2a23a438a849f183
Ashish Kumar <Ashish.Kumar@freescale.com>
2014-10-07 18:02:23 +0530

    powerpc/BSC9131RDB: Enable creation of dynamic partitions for NAND
    * fdt_fixup_mtdparts is called from ft_board_setup
     * Run "mtdparts default" to create NAND partition on uboot
     * Use mtdparts to create partitions dynamically rather
        than using static partitions in device tree
    
    Signed-off-by: Ashish Kumar <Ashish.Kumar@freescale.com>
    Reviewed-by: York Sun <yorksun@freescale.com>

--- include/configs/BSC9131RDB.h
+++ include/configs/BSC9131RDB.h
@@ -400,6 +400,23 @@ extern unsigned long get_sdram_size(void);
 #endif
 
 /*
+ * Dynamic MTD Partition support with mtdparts
+ */
+#define CONFIG_MTD_DEVICE
+#define CONFIG_MTD_PARTITIONS
+#define CONFIG_CMD_MTDPARTS
+#define MTDIDS_DEFAULT "nand0=ff800000.flash,"
+#define MTDPARTS_DEFAULT "mtdparts=ff800000.flash:1m(uboot)," \
+			"8m(kernel),512k(dtb),-(fs)"
+/*
+ * Override partitions in device tree using info
+ * in "mtdparts" environment variable
+ */
+#ifdef CONFIG_CMD_MTDPARTS
+#define CONFIG_FDT_FIXUP_PARTITIONS
+#endif
+
+/*
  * Environment Configuration
  */
 
