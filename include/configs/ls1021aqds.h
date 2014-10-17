8776cb205e25c4fdba05bd3fb13fe71973cd4c71
Nikhil Badola <nikhil.badola@freescale.com>
2014-10-17 11:37:25 +0530

    ls1: config: Enable USB EHCI Host on LS1021AQDS
    Enable USB EHCI Host on LS1021AQDS
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Reviewed-by: York Sun <yorksun@freescale.com>

--- include/configs/ls1021aqds.h
+++ include/configs/ls1021aqds.h
@@ -279,6 +279,23 @@ unsigned long get_board_ddr_clk(void);
 #define CONFIG_GENERIC_MMC
 
 /*
+ * USB
+ */
+#define CONFIG_HAS_FSL_DR_USB
+
+#ifdef CONFIG_HAS_FSL_DR_USB
+#define CONFIG_USB_EHCI
+
+#ifdef CONFIG_USB_EHCI
+#define CONFIG_CMD_USB
+#define CONFIG_USB_STORAGE
+#define CONFIG_USB_EHCI_FSL
+#define CONFIG_EHCI_HCD_INIT_AFTER_RESET
+#define CONFIG_CMD_EXT2
+#endif
+#endif
+
+/*
  * eTSEC
  */
 #define CONFIG_TSEC_ENET
