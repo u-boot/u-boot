0b03a931abcc7572489be07bee169a6d9a23832a
Igor Grinberg <grinberg@compulab.co.il>
2014-11-03 11:32:23 +0200

    omap: hsmmc: assume cd gpio is active low
    Switch the default CD GPIO polarity to active low.
    
    The current hsmmc driver assumption that the CD GPIO is active high, but
    in the real hardware, usually the opposite holds.
    The usual SD card socket has a mechanical switch which is grounded as
    soon as a card is inserted.
    Of course there might be some board logic which inverts the signal, but
    as far as current users are concerned, there is no such logic.
    
    Current U-Boot users either not using the CD functionality, or have a
    different way (e.g. external to SoC GPIO controller) for checking the
    card presence.
    
    This patch also brings the polarity assumption in line with the Linux
    kernel and adds appropriate comments.
    
    This patch also might spare issues once the TWL GPIO driver will be
    converted to the DM.
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Cc: Pantelis Antoniou <panto@antoniou-consulting.com>
    Cc: Dmitry Lifshitz <lifshitz@compulab.co.il>
    Reviewed-by: Tom Rini <trini@ti.com>

--- board/compulab/cm_t54/cm_t54.c
+++ board/compulab/cm_t54/cm_t54.c
@@ -100,16 +100,11 @@ uint mmc_get_env_part(struct mmc *mmc)
 #define SB_T54_CD_GPIO 228
 #define SB_T54_WP_GPIO 229
 
-int board_mmc_getcd(struct mmc *mmc)
-{
-	return !gpio_get_value(SB_T54_CD_GPIO);
-}
-
 int board_mmc_init(bd_t *bis)
 {
 	int ret0, ret1;
 
-	ret0 = omap_mmc_init(0, 0, 0, -1, SB_T54_WP_GPIO);
+	ret0 = omap_mmc_init(0, 0, 0, SB_T54_CD_GPIO, SB_T54_WP_GPIO);
 	if (ret0)
 		printf("cm_t54: failed to initialize mmc0\n");
 
