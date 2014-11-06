a70e157f304f578db7c99f5fba2ebeb882d736ba
Franklin S Cooper Jr <fcooper@ti.com>
2014-11-06 08:28:47 -0600

    configs: omap5_common : Boot rootfs from sd card by default
    * Since the emmc isn't always programed trying to load the fs from the
      emmc causes boot failures/kernel panic.
    
    * The current bootcmd is set to:
      bootcmd=run findfdt; run mmcboot;setenv mmcdev 1; setenv bootpart 1:2; \
      setenv mmcroot /dev/mmcblk0p2 rw; run mmcboot;
    
    My guess is the env variables should be set so that sd card boot
    (dt,kernel,fs) is the default and then fallback to emmc if it fails (no
    sd card detected)
    
    The current bootcmd attempts to set mmcroot to the sd card rootfs but
    that code doesn't run due to mmcboot being ran early on.
    
    Signed-off-by: Franklin Cooper Jr. <fcooper@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Reviewed-by: Tom Rini <trini@ti.com>

--- include/configs/ti_omap5_common.h
+++ include/configs/ti_omap5_common.h
@@ -78,7 +78,7 @@
 	"partitions=" PARTS_DEFAULT "\0" \
 	"optargs=\0" \
 	"mmcdev=0\0" \
-	"mmcroot=/dev/mmcblk1p2 rw\0" \
+	"mmcroot=/dev/mmcblk0p2 rw\0" \
 	"mmcrootfstype=ext4 rootwait\0" \
 	"mmcargs=setenv bootargs console=${console} " \
 		"${optargs} " \
