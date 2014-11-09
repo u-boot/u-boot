cf3c03b173038c51c19b1d871f225fc0c62f2a85
Ian Campbell <ian.campbell@citrix.com>
2014-11-09 10:44:33 +0000

    exynos: Increase command line buffer size (CONFIG_SYS_CBSIZE)
    I was running into this limit with a not overly long PXE append line.
    
    Since the PXE code wants to print the resulting command line increase
    CONFIG_SYS_PBSIZE too.
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- include/configs/exynos-common.h
+++ include/configs/exynos-common.h
@@ -74,8 +74,8 @@
 #define CONFIG_PARTITION_UUIDS
 
 /* Miscellaneous configurable options */
-#define CONFIG_SYS_CBSIZE		256	/* Console I/O Buffer Size */
-#define CONFIG_SYS_PBSIZE		384	/* Print Buffer Size */
+#define CONFIG_SYS_CBSIZE		1024	/* Console I/O Buffer Size */
+#define CONFIG_SYS_PBSIZE		1024	/* Print Buffer Size */
 #define CONFIG_SYS_MAXARGS		16	/* max number of command args */
 
 /* Boot Argument Buffer Size */
