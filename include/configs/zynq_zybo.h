a03d5c114a5e4a0bd696b0135a65746eb46c84f4
Michal Simek <michal.simek@xilinx.com>
2014-11-13 13:22:40 +0100

    zynq: zybo: Move CONFIG_DEFAULT_DEVICE_TREE to defconfig
    This is a MIME GnuPG-signed message.  If you see this text, it means that
    your E-mail or Usenet software does not support MIME signed messages.
    The Internet standard for MIME PGP messages, RFC 2015, was published in 1996.
    To open this message correctly you will need to install E-mail or Usenet
    software that supports modern Internet standards.
    
    Move CONFIG_DEFAULT_DEVICE_TREE from board config
    directly to defconfig.
    
    This patch removes compilation warning:
    include/configs/zynq_zybo.h:24:0: warning: "CONFIG_DEFAULT_DEVICE_TREE"
    redefined [enabled by default]
     #define CONFIG_DEFAULT_DEVICE_TREE zynq-zybo
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/zynq_zybo.h
+++ include/configs/zynq_zybo.h
@@ -21,7 +21,6 @@
 
 #define CONFIG_ZYNQ_SDHCI0
 #define CONFIG_ZYNQ_BOOT_FREEBSD
-#define CONFIG_DEFAULT_DEVICE_TREE	zynq-zybo
 
 /* Define ZYBO PS Clock Frequency to 50MHz */
 #define CONFIG_ZYNQ_PS_CLK_FREQ	50000000UL
