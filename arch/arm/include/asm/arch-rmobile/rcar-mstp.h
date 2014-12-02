11d902c6a6769dc5db41de534310377efe5179a9
Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
2014-12-02 16:52:21 +0900

    arm: rmobile: rcar: Add mstp_setclrbits and mstp_setclrbits_le32
    This addes macro for set and clear bit control for module control register.
    This is used when user want to disable the function of the devices
    corresponding to register.
    
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- arch/arm/include/asm/arch-rmobile/rcar-mstp.h
+++ arch/arm/include/asm/arch-rmobile/rcar-mstp.h
@@ -14,9 +14,13 @@
 		out_##type((saddr), in_##type(addr) | (set))
 #define mstp_clrbits(type, addr, saddr, clear) \
 		out_##type((saddr), in_##type(addr) & ~(clear))
+#define mstp_setclrbits(type, addr, set, clear) \
+		out_##type((addr), (in_##type(addr) | (set)) & ~(clear))
 #define mstp_setbits_le32(addr, saddr, set) \
 		mstp_setbits(le32, addr, saddr, set)
 #define mstp_clrbits_le32(addr, saddr, clear) \
 		mstp_clrbits(le32, addr, saddr, clear)
+#define mstp_setclrbits_le32(addr, set, clear) \
+		mstp_setclrbits(le32, addr, set, clear)
 
 #endif /* __ASM_ARCH_RCAR_MSTP_H */
