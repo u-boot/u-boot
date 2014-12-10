3183c2a0cbdd11989112faa13a1582f797ade51f
Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
2014-12-10 14:12:43 +0900

    arm: rmobile: Add mmc.h for sh_mmcif of rmobile
    R-Mobile and R-Car ARM SoCs use sh_mmcif as MMC host driver.
    This adds arch-rmobile/mmc.h that defines mmcif_mmc_init().
    
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

---
+++ arch/arm/include/asm/arch-rmobile/mmc.h
@@ -0,0 +1,14 @@
+/*
+ * Renesas SuperH MMCIF driver.
+ *
+ * Copyright (C)  2014 Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
+ * Copyright (C)  2014 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+#ifndef _RMOBILE_MMC_H_
+#define _RMOBILE_MMC_H_
+
+int mmcif_mmc_init(void);
+
+#endif /* _RMOBILE_MMC_H_ */
