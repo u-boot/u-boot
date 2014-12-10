d67be7c965949b88e2bae40ebb9de263a4f441e0
Ruchika Gupta <ruchika.gupta@freescale.com>
2014-12-10 11:52:50 +0530

    powerpc: mpc85xx: Add dummy gpio.h to enable CONFIG_OF_CONTROL
    If CONFIG_OF_CONTROL is enabled, lib/fdtdec.c is compiled.
    It includes <asm/gpio.h> and then <asm/gpio.h> includes
    <asm/arch/gpio.h>.  As a result, all the SoCs that enable
    CONFIG_OF_CONTROL must have <asm/arch/gpio.h>.
    
    The right fix would be to split the lib/fdtdec.c to remove
    dependency on GPIO.
    
    This commit adds a dummy <asm/arch/gpio.h> to support OF_CONTROL
    for mpc85xx platform. A file mpc85xx_gpio.h exists in
    arch/powerpc/include/asm. The defintions in that file conflict
    with the ones in asm-generic/gpio.h. Hence a dummy header file
    has been added. This will be removed after FDT-GPIO stuff is
    fixed correctly.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@freescale.com>
    Reviewed-by: York Sun <yorksun@freescale.com>

---
+++ arch/powerpc/include/asm/arch-mpc85xx/gpio.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+/*
+ * Dummy header file to enable CONFIG_OF_CONTROL.
+ * If CONFIG_OF_CONTROL is enabled, lib/fdtdec.c is compiled.
+ * It includes <asm/arch/gpio.h> via <asm/gpio.h>, so those SoCs that enable
+ * OF_CONTROL must have arch/gpio.h.
+ */
+
+#ifndef __ASM_ARCH_MX85XX_GPIO_H
+#define __ASM_ARCH_MX85XX_GPIO_H
+
+#endif
