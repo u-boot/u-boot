f315828b0d31cac2559af626f75bcb7f5a0ac524
Thierry Reding <treding@nvidia.com>
2014-12-09 22:25:12 -0700

    pci: tegra: Add Tegra PCIe driver
    Add support for the PCIe controller found on some generations of Tegra.
    Tegra20 has 2 root ports with a total of 4 lanes, Tegra30 has 3 root
    ports with a total of 6 lanes and Tegra124 has 2 root ports with a total
    of 5 lanes.
    
    This is based on the Linux kernel driver, originally submitted upstream
    by Mike Rapoport.
    
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- include/fdtdec.h
+++ include/fdtdec.h
@@ -86,6 +86,9 @@ enum fdt_compat_id {
 	COMPAT_NVIDIA_TEGRA20_SFLASH,	/* Tegra 2 SPI flash controller */
 	COMPAT_NVIDIA_TEGRA20_SLINK,	/* Tegra 2 SPI SLINK controller */
 	COMPAT_NVIDIA_TEGRA114_SPI,	/* Tegra 114 SPI controller */
+	COMPAT_NVIDIA_TEGRA124_PCIE,	/* Tegra 124 PCIe controller */
+	COMPAT_NVIDIA_TEGRA30_PCIE,	/* Tegra 30 PCIe controller */
+	COMPAT_NVIDIA_TEGRA20_PCIE,	/* Tegra 20 PCIe controller */
 	COMPAT_NVIDIA_TEGRA124_XUSB_PADCTL,
 					/* Tegra124 XUSB pad controller */
 	COMPAT_SMSC_LAN9215,		/* SMSC 10/100 Ethernet LAN9215 */
