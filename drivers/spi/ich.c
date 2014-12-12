7e7740397dc9405a8ed6299e8ebc8170142ccae7
Bin Meng <bmeng.cn@gmail.com>
2014-12-12 21:05:27 +0800

    x86: ich-spi: Add Intel Tunnel Creek SPI controller support
    Add Intel Tunnel Creek SPI controller support which is an ICH7
    compatible device.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- drivers/spi/ich.c
+++ drivers/spi/ich.c
@@ -167,7 +167,8 @@ void spi_free_slave(struct spi_slave *slave)
  */
 static int get_ich_version(uint16_t device_id)
 {
-	if (device_id == PCI_DEVICE_ID_INTEL_TGP_LPC)
+	if (device_id == PCI_DEVICE_ID_INTEL_TGP_LPC ||
+	    device_id == PCI_DEVICE_ID_INTEL_ITC_LPC)
 		return 7;
 
 	if ((device_id >= PCI_DEVICE_ID_INTEL_COUGARPOINT_LPC_MIN &&
