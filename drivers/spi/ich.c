fa388bca3eb3f81f6412c69f0e15ab05ad4918a7
Bin Meng <bmeng.cn@gmail.com>
2014-12-12 19:36:15 +0530

    x86: ich-spi: Set the rx operation mode for ich 7
    ICH 7 SPI controller only supports array read command (03h).
    Fast array read command (0Bh) is not supported.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Tested-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Jagannadha Sutradharudu Teki <jagannadh.teki@gmail.com>

--- drivers/spi/ich.c
+++ drivers/spi/ich.c
@@ -141,6 +141,10 @@ struct spi_slave *spi_setup_slave(unsigned int bus, unsigned int cs,
 	ich->slave.max_write_size = ctlr.databytes;
 	ich->speed = max_hz;
 
+	/* ICH 7 SPI controller only supports array read command */
+	if (ctlr.ich_version == 7)
+		ich->slave.op_mode_rx = SPI_OPM_RX_AS;
+
 	return &ich->slave;
 }
 
