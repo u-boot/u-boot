3c5b20f1b7c6687813d193033adddf0f93a3335d
Masahiro Yamada <yamada.m@jp.panasonic.com>
2014-12-15 23:26:08 +0900

    mpc8xx: remove ELPT860 board support
    This board is still a non-generic board.
    
    Signed-off-by: Masahiro Yamada <yamada.m@jp.panasonic.com>
    Cc: The LEOX team <team@leox.org>

--- include/commproc.h
+++ include/commproc.h
@@ -456,32 +456,6 @@ typedef struct scc_enet {
 #define SICR_ENET_CLKRT	((uint)0x00002c00)
 #endif	/* CONFIG_BSEIP */
 
-/***  ELPT860 *********************************************************/
-
-#ifdef CONFIG_ELPT860
-/* Bits in parallel I/O port registers that have to be set/cleared
- * to configure the pins for SCC1 use.
- */
-#  define PROFF_ENET        PROFF_SCC1
-#  define CPM_CR_ENET       CPM_CR_CH_SCC1
-#  define SCC_ENET          0
-
-#  define PA_ENET_RXD       ((ushort)0x0001)	/* PA 15 */
-#  define PA_ENET_TXD       ((ushort)0x0002)	/* PA 14 */
-#  define PA_ENET_RCLK      ((ushort)0x0100)	/* PA  7 */
-#  define PA_ENET_TCLK      ((ushort)0x0200)	/* PA  6 */
-
-#  define PC_ENET_TENA      ((ushort)0x0001)	/* PC 15 */
-#  define PC_ENET_CLSN      ((ushort)0x0010)	/* PC 11 */
-#  define PC_ENET_RENA      ((ushort)0x0020)	/* PC 10 */
-
-/* Control bits in the SICR to route TCLK (CLK2) and RCLK (CLK1) to
- * SCC1.  Also, make sure GR1 (bit 24) and SC1 (bit 25) are zero.
- */
-#  define SICR_ENET_MASK    ((uint)0x000000FF)
-#  define SICR_ENET_CLKRT   ((uint)0x00000025)
-#endif	/* CONFIG_ELPT860 */
-
 /***  ESTEEM 192E  **************************************************/
 #ifdef CONFIG_ESTEEM192E
 /* ESTEEM192E
