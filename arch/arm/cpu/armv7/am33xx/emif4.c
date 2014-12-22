fc46bae2ae38c8d0b1570427b5c9520281eaae4f
James Doublesin <doublesin@ti.com>
2014-12-22 16:26:11 -0600

    arm: am437x: Enable hardware leveling for EMIF
    Switch to using hardware leveling for certain parameters on the EMIF
    rather than using precalculated values.  Doing this also means we have a
    common place now between am437x and am335x for setting
    emif_sdram_ref_ctrl with a value for the correct delay length.
    
    Tested-by: Felipe Balbi <balbi@ti.com>
    Tested-by: Tom Rini <trini@ti.com>
    Signed-off-by: James Doublesin <doublesin@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- arch/arm/cpu/armv7/am33xx/emif4.c
+++ arch/arm/cpu/armv7/am33xx/emif4.c
@@ -112,17 +112,20 @@ void config_ddr(unsigned int pll, const struct ctrl_ioregs *ioregs,
 
 	/* Set CKE to be controlled by EMIF/DDR PHY */
 	writel(DDR_CKE_CTRL_NORMAL, &ddrctrl->ddrckectrl);
+
 #endif
 #ifdef CONFIG_AM43XX
 	writel(readl(&cm_device->cm_dll_ctrl) & ~0x1, &cm_device->cm_dll_ctrl);
 	while ((readl(&cm_device->cm_dll_ctrl) & CM_DLL_READYST) == 0)
 		;
-	writel(0x80000000, &ddrctrl->ddrioctrl);
 
 	config_io_ctrl(ioregs);
 
 	/* Set CKE to be controlled by EMIF/DDR PHY */
 	writel(DDR_CKE_CTRL_NORMAL, &ddrctrl->ddrckectrl);
+
+	/* Allow EMIF to control DDR_RESET */
+	writel(0x00000000, &ddrctrl->ddrioctrl);
 #endif
 
 	/* Program EMIF instance */
