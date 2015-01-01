d1a5d3c133a07d6035b9f97a6315afed7d73514a
Simon Glass <sjg@chromium.org>
2015-01-01 16:17:58 -0700

    x86: config: Always scroll the display by 5 lines, for speed
    Scrolling a line at a time is very slow for reasons that I don't understand.
    It seems to take about 100ms to copy 4MB of RAM in the frame buffer. To cope
    with this, scroll 5 lines each time.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/x86-common.h
+++ include/configs/x86-common.h
@@ -179,6 +179,7 @@
 #define VIDEO_FB_16BPP_WORD_SWAP
 #define CONFIG_I8042_KBD
 #define CONFIG_CFB_CONSOLE
+#define CONFIG_CONSOLE_SCROLL_LINES 5
 
 /*-----------------------------------------------------------------------
  * CPU Features
