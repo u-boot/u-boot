a5ee5c6949fce12cdaae6016f04f0964512e5051
Matthias Fuchs <matthias.fuchs@esd.eu>
2015-01-12 22:47:33 +0100

    ppc4xx: switch PLU405 to generic board
    Signed-off-by: Matthias Fuchs <matthias.fuchs@esd.eu>

--- include/configs/PLU405.h
+++ include/configs/PLU405.h
@@ -21,6 +21,8 @@
 #define CONFIG_PLU405		1	/* ...on a PLU405 board		*/
 
 #define	CONFIG_SYS_TEXT_BASE	0xFFF80000
+#define CONFIG_SYS_GENERIC_BOARD
+#define CONFIG_DISPLAY_BOARDINFO
 
 #define CONFIG_BOARD_EARLY_INIT_F 1	/* call board_early_init_f()	*/
 #define CONFIG_MISC_INIT_R	1	/* call misc_init_r()		*/
