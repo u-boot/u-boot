ee74fec845e76fbc30a480b84f6dc8be62b73b36
Hans de Goede <hdegoede@redhat.com>
2015-01-13 19:22:21 +0100

    sun9i: Add cpu_sun9i.h with iomem defines
    Add a headerfile with all the base addresses from the sun9i blocks.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Ian Campbell <ijc@hellion.org.uk>

--- arch/arm/include/asm/arch-sunxi/cpu.h
+++ arch/arm/include/asm/arch-sunxi/cpu.h
@@ -7,6 +7,10 @@
 #ifndef _SUNXI_CPU_H
 #define _SUNXI_CPU_H
 
+#if defined(CONFIG_MACH_SUN9I)
+#include <asm/arch/cpu_sun9i.h>
+#else
 #include <asm/arch/cpu_sun4i.h>
+#endif
 
 #endif /* _SUNXI_CPU_H */
