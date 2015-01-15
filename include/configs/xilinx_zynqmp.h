0ed92360c753feefbb7ab096174f1f0e4f607d49
Michal Simek <michal.simek@xilinx.com>
2015-01-15 11:37:21 +0100

    zynqmp: Unify console I/O Buffer size with Zynq
    The reason for using bigger value is user configuration with
    scripts on SD.
    Also unification with Zynq.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp.h
+++ include/configs/xilinx_zynqmp.h
@@ -148,7 +148,7 @@
 
 /* Monitor Command Prompt */
 /* Console I/O Buffer Size */
-#define CONFIG_SYS_CBSIZE		512
+#define CONFIG_SYS_CBSIZE		2048
 #define CONFIG_SYS_PROMPT		"ZynqMP> "
 #define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
 					sizeof(CONFIG_SYS_PROMPT) + 16)
