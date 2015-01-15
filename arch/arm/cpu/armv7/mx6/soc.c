1f516faa45611aedc8c2e3f303b3866f615d481e
Peng Fan <Peng.Fan@freescale.com>
2015-01-15 14:22:32 +0800

    ARM: imx6: disable bandgap self-bias after boot
    The self-bias circuit is used by the bandgap during startup.
    Once the bandgap has stabilized, the self-bias circuit should
    be disabled for best noise performance of analog blocks.
    Also this bit should be disabled before the chip enters STOP mode or
    when ever the regular bandgap is disabled.
    
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>
    Signed-off-by: Ranjani Vaidyanathan <Ranjani.Vaidyanathan@freescale.com>

--- arch/arm/cpu/armv7/mx6/soc.c
+++ arch/arm/cpu/armv7/mx6/soc.c
@@ -255,6 +255,23 @@ static void clear_mmdc_ch_mask(void)
 	writel(0, &mxc_ccm->ccdr);
 }
 
+static void init_bandgap(void)
+{
+	struct anatop_regs *anatop = (struct anatop_regs *)ANATOP_BASE_ADDR;
+	/*
+	 * Ensure the bandgap has stabilized.
+	 */
+	while (!(readl(&anatop->ana_misc0) & 0x80))
+		;
+	/*
+	 * For best noise performance of the analog blocks using the
+	 * outputs of the bandgap, the reftop_selfbiasoff bit should
+	 * be set.
+	 */
+	writel(BM_ANADIG_ANA_MISC0_REFTOP_SELBIASOFF, &anatop->ana_misc0_set);
+}
+
+
 #ifdef CONFIG_MX6SL
 static void set_preclk_from_osc(void)
 {
@@ -273,6 +290,13 @@ int arch_cpu_init(void)
 
 	/* Need to clear MMDC_CHx_MASK to make warm reset work. */
 	clear_mmdc_ch_mask();
+
+	/*
+	 * Disable self-bias circuit in the analog bandap.
+	 * The self-bias circuit is used by the bandgap during startup.
+	 * This bit should be set after the bandgap has initialized.
+	 */
+	init_bandgap();
 
 	/*
 	 * When low freq boot is enabled, ROM will not set AHB
