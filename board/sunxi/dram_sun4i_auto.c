d133647af4d4fdaaf2dfb6a9e757c7aebd00a29e
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2015-02-01 00:27:05 +0200

    sunxi: dram: Optionally use standard JEDEC timings for sun[457]i
    In addition to the current Android magic settings, allow to optionally use
    DDR3 timing parameters, which are tailored for different clock frequencies
    and JEDEC speed bins. This should improve reliability and performance.
    
    Adding '+S:CONFIG_DRAM_TIMINGS_DDR3_1066F_1333H=y' to the board defconfig
    allows to use timings, which are calculated for the DDR3-1066F speed bin.
    A lot of DDR3 chips, which are used in real Allwinner based devices,
    support DDR3-1066F speed bin timings.
    
    And adding '+S:CONFIG_DRAM_TIMINGS_DDR3_800E_1066G_1333J=y' should work
    with any DDR3 chips, because this targets the slowest JEDEC speed bins.
    
    The vendor magic values are still used by default for DRAM, but board
    maintainers now have more flexibility in DRAM timings selection.
    
    Signed-off-by: Siarhei Siamashka <siarhei.siamashka@gmail.com>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- board/sunxi/dram_sun4i_auto.c
+++ board/sunxi/dram_sun4i_auto.c
@@ -8,18 +8,22 @@ static struct dram_para dram_para = {
 	.density = 0,
 	.io_width = 0,
 	.bus_width = 0,
-	.cas = 6,
 	.zq = CONFIG_DRAM_ZQ,
 	.odt_en = 0,
 	.size = 0,
+#ifdef CONFIG_DRAM_TIMINGS_VENDOR_MAGIC
+	.cas = 6,
 	.tpr0 = 0x30926692,
 	.tpr1 = 0x1090,
 	.tpr2 = 0x1a0c8,
+	.emr2 = 0,
+#else
+#	include "dram_timings_sun4i.h"
+#endif
 	.tpr3 = 0,
 	.tpr4 = 0,
 	.tpr5 = 0,
 	.emr1 = CONFIG_DRAM_EMR1,
-	.emr2 = 0,
 	.emr3 = 0,
 };
 
