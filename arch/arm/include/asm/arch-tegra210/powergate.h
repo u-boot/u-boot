6c43f6c8d920caa1db01f5d0571a4d9ba720be15
Tom Warren <twarren@nvidia.com>
2015-02-02 13:22:29 -0700

    ARM: Tegra210: Add SoC code/include files for T210
    All based off of Tegra124. As a Tegra210 board is brought
    up, these may change a bit to match the HW more closely,
    but probably 90% of this is identical to T124.
    
    Note that since T210 is a 64-bit build, it has no SPL
    component, and hence no cpu.c for Tegra210.
    
    Signed-off-by: Tom Warren <twarren@nvidia.com>

---
+++ arch/arm/include/asm/arch-tegra210/powergate.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2014-2015 NVIDIA CORPORATION. All rights reserved.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef _TEGRA210_POWERGATE_H_
+#define _TEGRA210_POWERGATE_H_
+
+#include <asm/arch-tegra/powergate.h>
+
+#endif /* _TEGRA210_POWERGATE_H_ */
