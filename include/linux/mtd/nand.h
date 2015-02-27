cd6e8a25202dde0fc14cd8383a2e752781d55e7b
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2015-02-27 15:02:17 +0530

    mtd: nand: Increase max sizes of OOB and Page size
    Increase max sizes for OOB, Page size and eccpos to
    suit for Micron MT29F32G08 part
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/mtd/nand.h
+++ include/linux/mtd/nand.h
@@ -64,8 +64,8 @@ extern int nand_unlock(struct mtd_info *mtd, loff_t ofs, uint64_t len);
  * is supported now. If you add a chip with bigger oobsize/page
  * adjust this accordingly.
  */
-#define NAND_MAX_OOBSIZE       744
-#define NAND_MAX_PAGESIZE      8192
+#define NAND_MAX_OOBSIZE       1216
+#define NAND_MAX_PAGESIZE      16384
 #endif
 
 /*
