37220efab846ece7cdfc7a8fba3d8294adc6093d
Simon Glass <sjg@chromium.org>
2015-03-09 19:12:57 -0600

    tegra: seaboard: Correct the gpio_request() call
    Requesting a GPIO without a name is not supposed anymore. This causes the
    request to fail. Add a name so that the serial console works on seaboard.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reported-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- board/nvidia/seaboard/seaboard.c
+++ board/nvidia/seaboard/seaboard.c
@@ -20,7 +20,7 @@
 void gpio_early_init_uart(void)
 {
 	/* Enable UART via GPIO_PI3 (port 8, bit 3) so serial console works */
-	gpio_request(GPIO_PI3, NULL);
+	gpio_request(GPIO_PI3, "uart_en");
 	gpio_direction_output(GPIO_PI3, 0);
 }
 #endif
