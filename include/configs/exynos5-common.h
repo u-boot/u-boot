f44ef7d60c7349c0eed5d2363a2649badaae2610
Inha Song <ideal.song@samsung.com>
2015-03-13 17:48:35 +0900

    exynos5: add trace feature #ifdef in exynos5-common.h
    We can enable / disable trace feature from the FTRACE config options.
    To enable, compile U-Boot with FTRACE=1.
    
    This patch add #ifdef FTRACE in exynos5-common.h for enable/disable
    to use FTRACE configs instead of having to change board config files.
    
    Signed-off-by: Inha Song <ideal.song@samsung.com>
    Acked-by: Simon Glass <sjg@chroimum.org>
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- include/configs/exynos5-common.h
+++ include/configs/exynos5-common.h
@@ -16,14 +16,14 @@
 #define CONFIG_SYS_CACHELINE_SIZE	64
 #define CONFIG_EXYNOS_SPL
 
-/* Allow tracing to be enabled */
+#ifdef FTRACE
 #define CONFIG_TRACE
 #define CONFIG_CMD_TRACE
 #define CONFIG_TRACE_BUFFER_SIZE	(16 << 20)
 #define CONFIG_TRACE_EARLY_SIZE		(8 << 20)
 #define CONFIG_TRACE_EARLY
 #define CONFIG_TRACE_EARLY_ADDR		0x50000000
-
+#endif
 
 /* Enable ACE acceleration for SHA1 and SHA256 */
 #define CONFIG_EXYNOS_ACE_SHA
