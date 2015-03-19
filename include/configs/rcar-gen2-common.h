326a682358c16afcf2c7a9617e9811e72a1f0929
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-03-19 19:42:55 +0900

    malloc_f: enable SYS_MALLOC_F by default if DM is on
    This option has a bool type, not hex.
    Fix it and enable it if CONFIG_DM is on because Driver Model always
    requires malloc memory.  Devices are scanned twice, before/after
    relocation.  CONFIG_SYS_MALLOC_F should be enabled to use malloc
    memory before relocation.  As it is board-independent, handle it
    globally.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Acked-by: Robert Baldyga <r.baldyga@samsung.com>

--- include/configs/rcar-gen2-common.h
+++ include/configs/rcar-gen2-common.h
@@ -35,8 +35,6 @@
 #define CONFIG_SYS_THUMB_BUILD
 #define CONFIG_SYS_GENERIC_BOARD
 
-#define CONFIG_SYS_MALLOC_F_LEN	(1 << 10)
-
 /* Support File sytems */
 #define CONFIG_FAT_WRITE
 #define CONFIG_DOS_PARTITION
