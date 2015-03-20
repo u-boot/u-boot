e2a5d55642712f3c5a95b148cd1c94be801c4c4f
Thierry Reding <treding@nvidia.com>
2015-03-20 12:56:17 +0100

    config: Use booti instead of bootz on 64-bit ARM
    The bootz command doesn't work with Linux kernel images on 64-bit ARM.
    The replacement command with the same interface and functionality is
    booti.
    
    Cc: Dennis Gilmore <dennis@ausil.us>
    Cc: Tom Rini <trini@konsulko.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/config_distro_defaults.h
+++ include/config_distro_defaults.h
@@ -31,7 +31,11 @@
 
 #define CONFIG_OF_LIBFDT
 
+#ifdef CONFIG_ARM64
+#define CONFIG_CMD_BOOTI
+#else
 #define CONFIG_CMD_BOOTZ
+#endif
 #define CONFIG_CMD_DHCP
 #define CONFIG_CMD_ELF
 #define CONFIG_CMD_EXT2
