060ef094600373ea2b25111006edc56c072d9bd7
York Sun <yorksun@freescale.com>
2015-03-20 19:28:05 -0700

    armv8/fsl-lsch3: Implement workaround for erratum A008585
    Generic Timer may contain an erroneous value. The workaround is to
    read it twice until getting the same value.
    
    Signed-off-by: York Sun <yorksun@freescale.com>

--- arch/arm/cpu/armv8/generic_timer.c
+++ arch/arm/cpu/armv8/generic_timer.c
@@ -25,7 +25,18 @@ unsigned long get_tbclk(void)
 unsigned long timer_read_counter(void)
 {
 	unsigned long cntpct;
+#ifdef CONFIG_SYS_FSL_ERRATUM_A008585
+	/* This erratum number needs to be confirmed to match ARM document */
+	unsigned long temp;
+#endif
 	isb();
 	asm volatile("mrs %0, cntpct_el0" : "=r" (cntpct));
+#ifdef CONFIG_SYS_FSL_ERRATUM_A008585
+	asm volatile("mrs %0, cntpct_el0" : "=r" (temp));
+	while (temp != cntpct) {
+		asm volatile("mrs %0, cntpct_el0" : "=r" (cntpct));
+		asm volatile("mrs %0, cntpct_el0" : "=r" (temp));
+	}
+#endif
 	return cntpct;
 }
