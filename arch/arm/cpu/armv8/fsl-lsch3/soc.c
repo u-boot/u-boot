d746fef40653d17a64ff81a8ff57c55d122d8d5e
Scott Wood <scottwood@freescale.com>
2015-03-20 19:28:13 -0700

    armv8/ls2085a: Add workaround for USB erratum A-008751
    Without this "USB may not work" according to the erratum text, though I
    did not notice a problem without it.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: York Sun <yorksun@freescale.com>

--- arch/arm/cpu/armv8/fsl-lsch3/soc.c
+++ arch/arm/cpu/armv8/fsl-lsch3/soc.c
@@ -7,8 +7,19 @@
 #include <common.h>
 #include <fsl_ifc.h>
 #include <asm/arch-fsl-lsch3/soc.h>
+#include <asm/io.h>
+
+static void erratum_a008751(void)
+{
+#ifdef CONFIG_SYS_FSL_ERRATUM_A008751
+	u32 __iomem *scfg = (u32 __iomem *)SCFG_BASE;
+
+	writel(0x27672b2a, scfg + SCFG_USB3PRM1CR / 4);
+#endif
+}
 
 void fsl_lsch3_early_init_f(void)
 {
+	erratum_a008751();
 	init_early_memctl_regs();	/* tighten IFC timing */
 }
