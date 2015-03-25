e00cb2232b0e6c2f41f49f3535a7874067a60c3a
Simon Glass <sjg@chromium.org>
2015-03-25 12:23:05 -0600

    dm: usb: Add tests for the USB uclass
    This adds a simple test for probing and a functional test using the flash
    stick emulator, which tests a large chunk of the USB stack.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Marek Vasut <marex@denx.de>

--- test/dm/test-dm.sh
+++ test/dm/test-dm.sh
@@ -10,5 +10,8 @@ dtc -I dts -O dtb test/dm/test.dts -o test/dm/test.dtb
 make O=sandbox sandbox_config || die "Cannot configure U-Boot"
 make O=sandbox -s -j${NUM_CPUS} || die "Cannot build U-Boot"
 dd if=/dev/zero of=spi.bin bs=1M count=2
+echo -n "this is a test" > testflash.bin
+dd if=/dev/zero bs=1M count=4 >>testflash.bin
 ./sandbox/u-boot -d test/dm/test.dtb -c "dm test"
 rm spi.bin
+rm testflash.bin
