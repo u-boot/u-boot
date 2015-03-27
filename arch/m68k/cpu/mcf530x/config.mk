bdde65951659fb78d3b4bbf99743a74522f03a05
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-03-27 17:01:07 +0900

    m68k: mcf530x: move CPU type to Kconfig and refactor config.mk
    This commit intends to stop grepping CPU type in
    arch/m68k/cpu/mcf530x/config.mk.
    
    Move the CPU type config options from include/configs/amcore.h
    to arch/m68k/Kconfig and refactor the CPU flags select in
    arch/m68k/cpu/mcf530x/config.mk.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Alison Wang <alison.wang@freescale.com>
    Cc: Angelo Dureghello <angelo@sysam.it>

--- arch/m68k/cpu/mcf530x/config.mk
+++ arch/m68k/cpu/mcf530x/config.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier:     GPL-2.0+
 #
 
-cfg=$(srctree)/include/configs/$(CONFIG_SYS_CONFIG_NAME:"%"=%).h
-is5307:=$(shell grep CONFIG_M5307 $(cfg))
+cpuflags-$(CONFIG_M5307) := -mcpu=5307
 
-ifneq (,$(findstring CONFIG_M5307,$(is5307)))
-PLATFORM_CPPFLAGS += -mcpu=5307
-endif
+PLATFORM_CPPFLAGS += $(cpuflags-y)
