aa63387a399bdf9e06e4918322b23a112a5b8f97
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-03-27 17:01:13 +0900

    m68k: merge per-CPU config.mk into arch/m68k/Makefile
    Collect CPU specific flags into the single place.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Alison Wang <alison.wang@freescale.com>
    Cc: Angelo Dureghello <angelo@sysam.it>

--- arch/m68k/cpu/mcf530x/config.mk
+++
@@ -1,9 +0,0 @@
-#
-# (C) Copyright 2014  Angelo Dureghello <angelo@sysam.it>
-#
-# SPDX-License-Identifier:     GPL-2.0+
-#
-
-cpuflags-$(CONFIG_M5307) := -mcpu=5307
-
-PLATFORM_CPPFLAGS += $(cpuflags-y)
