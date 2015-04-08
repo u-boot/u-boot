a34f2075633d7928e2988193b0caba5854e6ef20
Joe Hershberger <joe.hershberger@ni.com>
2015-04-08 01:41:20 -0500

    net: cosmetic: Fix checkpatch.pl failures in net.h
    There were still a few remaining complains in the legacy eth_device
    definition that hadn't been addressed.
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- include/net.h
+++ include/net.h
@@ -149,14 +149,14 @@ struct eth_device {
 	phys_addr_t iobase;
 	int state;
 
-	int  (*init) (struct eth_device *, bd_t *);
-	int  (*send) (struct eth_device *, void *packet, int length);
-	int  (*recv) (struct eth_device *);
-	void (*halt) (struct eth_device *);
+	int (*init)(struct eth_device *, bd_t *);
+	int (*send)(struct eth_device *, void *packet, int length);
+	int (*recv)(struct eth_device *);
+	void (*halt)(struct eth_device *);
 #ifdef CONFIG_MCAST_TFTP
-	int (*mcast) (struct eth_device *, const u8 *enetaddr, u8 set);
+	int (*mcast)(struct eth_device *, const u8 *enetaddr, u8 set);
 #endif
-	int  (*write_hwaddr) (struct eth_device *);
+	int (*write_hwaddr)(struct eth_device *);
 	struct eth_device *next;
 	int index;
 	void *priv;
