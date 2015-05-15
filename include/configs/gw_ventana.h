e889e23a1de63bf198af710b20af9c9a5ee1a90a
Tim Harvey <tharvey@gateworks.com>
2015-05-15 09:14:49 -0700

    imx: ventana: config: use MMC SPL RAW support
    Switch to MMC RAW support for SPL. We will place the uboot.img at 69KB.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- include/configs/gw_ventana.h
+++ include/configs/gw_ventana.h
@@ -11,10 +11,6 @@
 #define CONFIG_SPL_BOARD_INIT
 #define CONFIG_SPL_NAND_SUPPORT
 #define CONFIG_SPL_MMC_SUPPORT
-#define CONFIG_SPL_FAT_SUPPORT
-/*
-#define CONFIG_SPL_SATA_SUPPORT
-*/
 /* Location in NAND to read U-Boot from */
 #define CONFIG_SYS_NAND_U_BOOT_OFFS     (14 * 1024 * 1024)
 
