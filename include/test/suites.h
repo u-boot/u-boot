c617ede08ad0583e0e015e76ebda2c5d2f6b5854
Joe Hershberger <joe.hershberger@ni.com>
2015-05-20 14:27:28 -0500

    test: Add a common unit test command
    Add a command that all other unit tests should be a sub-command of.
    Also include a command that will run all tests.
    
    Signed-off-by: Joe Hershberger <joe.hershberger@ni.com>
    Acked-by: Simon Glass <sjg@chromium.org>

---
+++ include/test/suites.h
@@ -0,0 +1,11 @@
+/*
+ * (C) Copyright 2015
+ * Joe Hershberger, National Instruments, joe.hershberger@ni.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#ifndef __TEST_SUITES_H__
+#define __TEST_SUITES_H__
+
+#endif /* __TEST_SUITES_H__ */
