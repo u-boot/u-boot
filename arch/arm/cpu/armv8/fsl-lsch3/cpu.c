226296656c6efe4eb2b26dbbebc417455bdfd99d
Prabhakar Kushwaha <prabhakar at freescale.com>
2015-05-28 14:54:06 +0530

    armv8/fsl-ch3: Add support to print SoC personality
    This patch adds support to print out the SoC personality.
    Freescale LS20xx SoCs (compliant to Chassis-3 specifications) can
    have 6 personalities: LS2045AE, LS2045A, LS2080AE, LS2080A,
    LS2085AE and LS2085A
    
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma at freescale.com>
    Signed-off-by: Prabhakar Kushwaha <prabhakar at freescale.com>
    Reviewed-by: York Sun <yorksun@freescale.com>

--- arch/arm/cpu/armv8/fsl-lsch3/cpu.c
+++ arch/arm/cpu/armv8/fsl-lsch3/cpu.c
@@ -9,6 +9,7 @@
 #include <asm/system.h>
 #include <asm/armv8/mmu.h>
 #include <asm/io.h>
+#include <asm/arch-fsl-lsch3/soc.h>
 #include <asm/arch-fsl-lsch3/immap_lsch3.h>
 #include <fsl_debug_server.h>
 #include <fsl-mc/fsl_mc.h>
@@ -21,6 +22,35 @@
 #include "speed.h"
 
 DECLARE_GLOBAL_DATA_PTR;
+
+static struct cpu_type cpu_type_list[] = {
+#ifdef CONFIG_LS2085A
+	CPU_TYPE_ENTRY(LS2085, LS2085, 8),
+	CPU_TYPE_ENTRY(LS2080, LS2080, 8),
+	CPU_TYPE_ENTRY(LS2045, LS2045, 4),
+#endif
+};
+
+void cpu_name(char *name)
+{
+	struct ccsr_gur __iomem *gur = (void *)(CONFIG_SYS_FSL_GUTS_ADDR);
+	unsigned int i, svr, ver;
+
+	svr = in_le32(&gur->svr);
+	ver = SVR_SOC_VER(svr);
+
+	for (i = 0; i < ARRAY_SIZE(cpu_type_list); i++)
+		if ((cpu_type_list[i].soc_ver & SVR_WO_E) == ver) {
+			strcpy(name, cpu_type_list[i].name);
+
+			if (IS_E_PROCESSOR(svr))
+				strcat(name, "E");
+			break;
+		}
+
+	if (i == ARRAY_SIZE(cpu_type_list))
+		strcpy(name, "unknown");
+}
 
 #ifndef CONFIG_SYS_DCACHE_OFF
 /*
@@ -379,6 +409,13 @@ int print_cpuinfo(void)
 	char buf[32];
 	unsigned int i, core;
 	u32 type;
+
+	puts("SoC: ");
+
+	cpu_name(buf);
+	printf(" %s (0x%x)\n", buf, in_le32(&gur->svr));
+
+	memset((u8 *)buf, 0x00, ARRAY_SIZE(buf));
 
 	get_sys_info(&sysinfo);
 	puts("Clock Configuration:");
