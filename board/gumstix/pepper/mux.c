5e90470a866875984820dbc48c9b881e0ce773ba
Adam YH Lee <adam.yh.lee@gmail.com>
2015-06-01 14:29:09 -0700

    pepper: Implement Board Detection mechanism
    AM335x-based 'Gumstix Pepper' SBCs and variants use different types of
    RAM (DDR2 vs DDR3 with DDR3 being the default).  Detect the board type
    by reading the factory-programmed EEPROM [1] and use this to select any
    runtime boot options such as RAM type.
    
    [1] http://elinux.org/BeagleBoardPinMux#List_of_Vendor_and_Device_IDs
    
    Signed-off-by: Adam YH Lee <adam.yh.lee@gmail.com>
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- board/gumstix/pepper/mux.c
+++ board/gumstix/pepper/mux.c
@@ -64,6 +64,11 @@ void enable_uart0_pin_mux(void)
 	configure_module_pin_mux(uart0_pin_mux);
 }
 
+void enable_i2c0_pin_mux(void)
+{
+	configure_module_pin_mux(i2c0_pin_mux);
+}
+
 /*
  * Do board-specific muxes.
  */
