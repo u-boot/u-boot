0859b49d127665e507300c56fb279183f13321e9
Simon Glass <sjg@chromium.org>
2015-06-05 14:39:40 -0600

    tegra: Increase maximum arguments to 32
    When setting up large environment variables we can exceed 16 arguemnts.
    Increase this to avoid problems.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- include/configs/tegra-common.h
+++ include/configs/tegra-common.h
@@ -104,7 +104,7 @@
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
 					sizeof(CONFIG_SYS_PROMPT) + 16)
-#define CONFIG_SYS_MAXARGS		16	/* max number of command args */
+#define CONFIG_SYS_MAXARGS		32	/* max number of command args */
 /* Boot Argument Buffer Size */
 #define CONFIG_SYS_BARGSIZE		(CONFIG_SYS_CBSIZE)
 
