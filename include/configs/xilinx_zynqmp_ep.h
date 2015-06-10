0b54a9dd09b8a06a1923c3f62f940626b062ccf7
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2015-06-10 15:50:57 +0530

    zynqmp: Define ep config for ZynqMP
    Define a new config "zynqmp_ep" for ZynqMP instead
    of xilinx_zynqmp. This defconfig supports all emulation
    platforms of ZynqMP. Also renamed TARGET_XILINX_ZYNQMP
    to ARCH_ZYNQMP.
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/configs/xilinx_zynqmp_ep.h
@@ -0,0 +1,26 @@
+/*
+ * Configuration for Xilinx ZynqMP emulation
+ * platforms. See zynqmp-common.h for ZynqMP
+ * common configs
+ *
+ * (C) Copyright 2014 - 2015 Xilinx, Inc.
+ * Michal Simek <michal.simek@xilinx.com>
+ * Siva Durga Prasad Paladugu <sivadur@xilinx.com>
+ *
+ * Based on Configuration for Versatile Express
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQMP_EP_H
+#define __CONFIG_ZYNQMP_EP_H
+
+#define CONFIG_ZYNQ_SERIAL_UART0
+#define CONFIG_ZYNQ_SDHCI0
+#define CONFIG_ZYNQ_I2C0
+#define CONFIG_SYS_I2C_ZYNQ
+#define CONFIG_ZYNQ_EEPROM
+
+#include <configs/xilinx_zynqmp.h>
+
+#endif /* __CONFIG_ZYNQMP_EP_H */
