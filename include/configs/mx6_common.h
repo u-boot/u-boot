959da4ecd024c52be939b74241f93d02166013b8
Peter Robinson <pbrobinson@gmail.com>
2015-06-16 10:20:01 +0100

    Drop duplicate CONFIG_SYS_NO_FLASH from mx6_common
    It's defind earlier in the file
    
    Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

--- include/configs/mx6_common.h
+++ include/configs/mx6_common.h
@@ -87,7 +87,6 @@
 #define CONFIG_CMD_FAT
 
 /* Miscellaneous configurable options */
-#define CONFIG_SYS_NO_FLASH
 #undef CONFIG_CMD_IMLS
 #define CONFIG_SYS_LONGHELP
 #define CONFIG_SYS_HUSH_PARSER
