da229e4e57b7d028cf01d52ac70832a90453e289
Simon Glass <sjg@chromium.org>
2015-06-23 15:38:34 -0600

    sandbox: Drop special-case sandbox console code
    At present printf() skips output if it can see there is no console. This
    is really just an optimisation, and is not necessary. Also it is currently
    incorrect in some cases. Rather than update the logic, just remove it so
    that we don't need to keep it in sync.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- common/console.c
+++ common/console.c
@@ -456,6 +456,7 @@ static inline void print_pre_console_buffer(int flushpoint) {}
 void putc(const char c)
 {
 #ifdef CONFIG_SANDBOX
+	/* sandbox can send characters to stdout before it has a console */
 	if (!gd || !(gd->flags & GD_FLG_SERIAL_READY)) {
 		os_putc(c);
 		return;
@@ -539,11 +540,6 @@ int printf(const char *fmt, ...)
 	va_list args;
 	uint i;
 	char printbuffer[CONFIG_SYS_PBSIZE];
-
-#if !defined(CONFIG_SANDBOX) && !defined(CONFIG_PRE_CONSOLE_BUFFER)
-	if (!gd->have_console)
-		return 0;
-#endif
 
 	va_start(args, fmt);
 
