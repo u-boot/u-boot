e506889c9690b672a601ac84a31bb8dbc40d7e35
Chen-Yu Tsai <wens@csie.org>
2015-06-23 19:57:25 +0800

    sunxi: Add support for UART0 in PB pin group on A33
    The A33 adds a pinmux function for UART0 in the PB pin group.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- arch/arm/include/asm/arch-sunxi/gpio.h
+++ arch/arm/include/asm/arch-sunxi/gpio.h
@@ -156,6 +156,7 @@ enum sunxi_gpio_number {
 #define SUN4I_GPB_UART0		2
 #define SUN5I_GPB_UART0		2
 #define SUN8I_GPB_UART2		2
+#define SUN8I_A33_GPB_UART0	3
 
 #define SUNXI_GPC_SDC2		3
 #define SUN6I_GPC_SDC3		4
