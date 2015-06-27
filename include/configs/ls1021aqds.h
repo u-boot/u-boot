6812484a40bf18ec8e0021278613caf6a0c46bd7
Jagan Teki <jteki@openedev.com>
2015-06-27 22:04:55 +0530

    sf: dataflash: Rename to CONFIG_SPI_FLASH_DATAFLASH
    Rename CONFIG_SF_DATAFLASH into CONFIG_SPI_FLASH_DATAFLASH
    as it follows the naming convention same as remaining configs.
    
    Signed-off-by: Jagan Teki <jteki@openedev.com>

--- include/configs/ls1021aqds.h
+++ include/configs/ls1021aqds.h
@@ -423,7 +423,7 @@ unsigned long get_board_ddr_clk(void);
 #if defined(CONFIG_FSL_DSPI) || defined(CONFIG_FSL_QSPI)
 #define CONFIG_CMD_SF
 #define CONFIG_DM_SPI_FLASH
-#define CONFIG_SF_DATAFLASH
+#define CONFIG_SPI_FLASH_DATAFLASH
 #endif
 #endif
 
