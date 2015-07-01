b11c5d1dc29e81326d1215011d19377737082aeb
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2015-07-01 16:36:43 +0200

    MIPS: change 'extern inline' to 'static inline'
    The kernel changed it a long time ago. Also this is now broken
    on gcc-5.x.
    
    Reported-by: Andy Kennedy <andy.kennedy@adtran.com>
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/include/asm/system.h
+++ arch/mips/include/asm/system.h
@@ -22,7 +22,7 @@
 #include <linux/kernel.h>
 #endif
 
-extern __inline__ void
+static __inline__ void
 __sti(void)
 {
 	__asm__ __volatile__(
@@ -46,7 +46,7 @@ __sti(void)
  * R4000/R4400 need three nops, the R4600 two nops and the R10000 needs
  * no nops at all.
  */
-extern __inline__ void
+static __inline__ void
 __cli(void)
 {
 	__asm__ __volatile__(
@@ -207,7 +207,7 @@ do { \
  * For 32 and 64 bit operands we can take advantage of ll and sc.
  * FIXME: This doesn't work for R3000 machines.
  */
-extern __inline__ unsigned long xchg_u32(volatile int * m, unsigned long val)
+static __inline__ unsigned long xchg_u32(volatile int * m, unsigned long val)
 {
 #ifdef CONFIG_CPU_HAS_LLSC
 	unsigned long dummy;
