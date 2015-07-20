0ca54023ab1a3cb5c504630f454f092dcca7e6d0
Peng Fan <Peng.Fan@freescale.com>
2015-07-20 19:28:22 +0800

    imx: mx6ul: Add pins IOMUX head file
    Add i.MX6UL pins IOMUX file which defines the IOMUX settings for choose.
    
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>
    Signed-off-by: Ye.Li <B37916@freescale.com>

--- arch/arm/include/asm/arch-mx6/mx6-pins.h
+++ arch/arm/include/asm/arch-mx6/mx6-pins.h
@@ -37,6 +37,8 @@ enum {
 #include "mx6sl_pins.h"
 #elif defined(CONFIG_MX6SX)
 #include "mx6sx_pins.h"
+#elif defined(CONFIG_MX6UL)
+#include "mx6ul_pins.h"
 #else
 #error "Please select cpu"
 #endif	/* CONFIG_MX6Q */
