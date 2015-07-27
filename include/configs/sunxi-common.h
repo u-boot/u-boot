fa5e102019e28a5936e52d6aa9f5624cf1744a35
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-07-27 16:21:26 +0200

    sunxi: increase SYS_MONITOR_LEN
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -179,7 +179,7 @@
 
 #define CONFIG_SYS_NO_FLASH
 
-#define CONFIG_SYS_MONITOR_LEN		(512 << 10)	/* 512 KiB */
+#define CONFIG_SYS_MONITOR_LEN		(768 << 10)	/* 768 KiB */
 #define CONFIG_IDENT_STRING		" Allwinner Technology"
 
 #define CONFIG_ENV_OFFSET		(544 << 10) /* (8 + 24 + 512) KiB */
