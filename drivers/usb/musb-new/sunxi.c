1feda63ed988b2c21f758d27e9332f4ab46b1ce9
Maxime Ripard <maxime.ripard@free-electrons.com>
2015-08-04 17:04:10 +0200

    musb: sunxi: Force EP0 on re-enable
    Currently, the second use of a gadget will fail, while the first one works.
    
    Forcing the EP0 at every enable fix this issue.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/usb/musb-new/sunxi.c
+++ drivers/usb/musb-new/sunxi.c
@@ -200,6 +200,9 @@ static int sunxi_musb_enable(struct musb *musb)
 
 	pr_debug("%s():\n", __func__);
 
+	musb_ep_select(musb->mregs, 0);
+	musb_writeb(musb->mregs, MUSB_FADDR, 0);
+
 	if (enabled)
 		return 0;
 
