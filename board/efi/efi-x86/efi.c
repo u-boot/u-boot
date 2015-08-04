3e9aa3205529c3838e8e92d65e748c27012cca88
Ben Stoltz <stoltz@google.com>
2015-08-04 12:33:47 -0600

    x86: Add EFI board code
    Add support for the efi-x86 board, which supports running U-Boot as an
    EFI 32-bit application.
    
    Signed-off-by: Ben Stoltz <stoltz@google.com>
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Tested-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ board/efi/efi-x86/efi.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2015 Google, Inc
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <asm/gpio.h>
+
+int arch_early_init_r(void)
+{
+	return 0;
+}
+
+void setup_pch_gpios(u16 gpiobase, const struct pch_gpio_map *gpio)
+{
+	return;
+}
