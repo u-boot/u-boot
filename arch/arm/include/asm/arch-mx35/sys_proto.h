fc684e87a1d1342cecbaf68ad8690482e4baff76
Peng Fan <Peng.Fan@freescale.com>
2015-08-13 10:55:34 +0800

    imx-common: consolidate macros and prototypes into sys_proto.h
    Move most macro definitions and prototypes into
    "arch/arm/include/asm/imx-common/sys_proto.h" to avoid duplicated
    function prototypes and marco definitions for different i.MX SoCs.
    
    This patch do not remove the sys_proto.h for different i.MX SoCs,
    because we need to modify lots of driver code and others. This patch
    remove duplicated macros and prototypes and incude "sys_proto.h"
    of imx-common for each sys_proto.h of different i.MX platforms.
    Then later we should avoid add stuff in sys_proto.h of each platform,
    and modify driver to include common sys_proto.h.
    
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>
    Cc: Stefano Babic <sbabic@denx.de>
    Reviewed-by: Stefano Babic <sbabic@denx.de>

--- arch/arm/include/asm/arch-mx35/sys_proto.h
+++ arch/arm/include/asm/arch-mx35/sys_proto.h
@@ -5,12 +5,12 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef _SYS_PROTO_H_
-#define _SYS_PROTO_H_
+#ifndef _MX35_SYS_PROTO_H_
+#define _MX35_SYS_PROTO_H_
 
-u32 get_cpu_rev(void);
-void mx3_setup_sdram_bank(u32 start_address, u32 ddr2_config,
-	u32 row, u32 col, u32 dsize, u32 refresh);
-#define is_soc_rev(rev)	((get_cpu_rev() & 0xFF) - rev)
+#include <asm/imx-common/sys_proto.h>
+
+void mx3_setup_sdram_bank(u32 start_address, u32 ddr2_config, u32 row,
+			  u32 col, u32 dsize, u32 refresh);
 
 #endif
