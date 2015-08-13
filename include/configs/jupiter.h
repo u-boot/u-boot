50301a5a1912409c9b5c865f72dfb42a63bcdd87
Anatolij Gustschin <agust@denx.de>
2015-08-13 23:57:56 +0200

    powerpc: jupiter: convert to generic board
    Signed-off-by: Anatolij Gustschin <agust@denx.de>
    Cc: Heiko Schocher <hs@denx.de>
    Cc: Wolfgang Denk <wd@denx.de>

--- include/configs/jupiter.h
+++ include/configs/jupiter.h
@@ -15,6 +15,8 @@
 
 #define CONFIG_MPC5200		1	/* This is an MPC5200 CPU */
 #define CONFIG_JUPITER		1	/* ... on Jupiter board */
+#define CONFIG_SYS_GENERIC_BOARD
+#define CONFIG_DISPLAY_BOARDINFO
 
 /*
  * Valid values for CONFIG_SYS_TEXT_BASE are:
