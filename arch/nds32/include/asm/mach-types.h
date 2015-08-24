b3537c08e14b9d7e3b3207de2d280dab2dc499fa
Kun-Hua Huang <kunhua@andestech.com>
2015-08-24 14:52:36 +0800

    NDS32: Generic Board Support and Unsupport
    	Remove ag101 and ag102 support
    
    Signed-off-by: Kun-Hua Huang <kunhua@andestech.com>

--- arch/nds32/include/asm/mach-types.h
+++ arch/nds32/include/asm/mach-types.h
@@ -12,20 +12,6 @@ extern unsigned int __machine_arch_type;
 #endif
 
 /* see arch/arm/kernel/arch.c for a description of these */
-#define MACH_TYPE_ADPAG101             0
-
-#ifdef CONFIG_ARCH_ADPAG101
-# ifdef machine_arch_type
-#  undef machine_arch_type
-#  define machine_arch_type	__machine_arch_type
-# else
-#  define machine_arch_type	MACH_TYPE_ADPAG101
-# endif
-# define machine_is_adpag101()	(machine_arch_type == MACH_TYPE_ADPAG101)
-#else
-# define machine_is_adpag101()	(0)
-#endif
-
 #define MACH_TYPE_ADPAG101P            1
 
 #ifdef CONFIG_ARCH_ADPAG101P
@@ -38,20 +24,6 @@ extern unsigned int __machine_arch_type;
 # define machine_is_adpag101p()	(machine_arch_type == MACH_TYPE_ADPAG101P)
 #else
 # define machine_is_adpag101p()	(1)
-#endif
-
-#define MACH_TYPE_ADPAG102             2
-
-#ifdef CONFIG_ARCH_ADPAG102
-# ifdef machine_arch_type
-#  undef machine_arch_type
-#  define machine_arch_type	__machine_arch_type
-# else
-#  define machine_arch_type	MACH_TYPE_ADPAG102
-# endif
-# define machine_is_adpag102()	(machine_arch_type == MACH_TYPE_ADPAG102)
-#else
-# define machine_is_adpag102()	(2)
 #endif
 
 #endif /* __ASM_NDS32_MACH_TYPE_H */
