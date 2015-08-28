f4e190e317b860bb15e221a29cbf16976a04e3ca
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-08-28 20:13:18 +0900

    ARM: uniphier: enable SPL_OF_CONTROL
    Device Tree really improves code maintainability and is now
    available for SPL too.
    
    This is the state-of-the-art implementation in U-boot.
    
    The board files (platform data) are no longer needed.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/configs/uniphier.h
+++ include/configs/uniphier.h
@@ -288,7 +288,7 @@
 	"netdev=eth0\0"						\
 	"verify=n\0"						\
 	"nandupdate=nand erase 0 0x00100000 &&"			\
-		"tftpboot u-boot-spl.bin &&"			\
+		"tftpboot u-boot-spl-dtb.bin &&"		\
 		"nand write $loadaddr 0 0x00010000 &&"		\
 		"tftpboot u-boot-dtb.img &&"			\
 		"nand write $loadaddr 0x00010000 0x000f0000\0"	\
