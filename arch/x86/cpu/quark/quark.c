31b5aebd5e00af84709711d8b9a60ebaf9deeed6
Bin Meng <bmeng.cn@gmail.com>
2015-09-03 05:37:26 -0700

    x86: Convert to use driver model pci on quark/galileo
    Move to driver model pci for Intel quark/galileo.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- arch/x86/cpu/quark/quark.c
+++ arch/x86/cpu/quark/quark.c
@@ -136,7 +136,6 @@ static void quark_enable_legacy_seg(void)
 
 int arch_cpu_init(void)
 {
-	struct pci_controller *hose;
 	int ret;
 
 	post_code(POST_CPU_INIT);
@@ -145,10 +144,6 @@ int arch_cpu_init(void)
 #endif
 
 	ret = x86_cpu_init_f();
-	if (ret)
-		return ret;
-
-	ret = pci_early_init_hose(&hose);
 	if (ret)
 		return ret;
 
