720e5e54ebc81ddbee171dbd1bab4e394b97fb81
Jagan Teki <jteki@openedev.com>
2015-09-04 18:29:12 +0530

    sf: params: Add IS25LP128 part support
    Added support for IS25LP128 flash part.
    
    Signed-off-by: Jagan Teki <jteki@openedev.com>
    Cc: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Cc: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/spi/sf_params.c
+++ drivers/mtd/spi/sf_params.c
@@ -38,6 +38,7 @@ const struct spi_flash_params spi_flash_params_table[] = {
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	{"IS25LP032",	   0x9d6016, 0x0,	64 * 1024,    64, RD_NORM,			  0},
 	{"IS25LP064",	   0x9d6017, 0x0,	64 * 1024,   128, RD_NORM,			  0},
+	{"IS25LP128",	   0x9d6018, 0x0,	64 * 1024,   256, RD_NORM,			  0},
 #endif
 #ifdef CONFIG_SPI_FLASH_MACRONIX	/* MACRONIX */
 	{"MX25L2006E",	   0xc22012, 0x0,	64 * 1024,     4, RD_NORM,			  0},
