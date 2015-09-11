5841c5b0a7967364a8ec10d54ab77ad62f3ab164
Bin Meng <bmeng.cn@gmail.com>
2015-09-11 03:24:36 -0700

    x86: Convert to use driver model eth on quark/galileo
    Convert to use DM version of Designware ethernet driver on Intel
    quark/galileo.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- arch/x86/cpu/quark/quark.c
+++ arch/x86/cpu/quark/quark.c
@@ -6,8 +6,6 @@
 
 #include <common.h>
 #include <mmc.h>
-#include <netdev.h>
-#include <phy.h>
 #include <asm/io.h>
 #include <asm/irq.h>
 #include <asm/pci.h>
@@ -229,23 +227,6 @@ int cpu_mmc_init(bd_t *bis)
 {
 	return pci_mmc_init("Quark SDHCI", mmc_supported,
 			    ARRAY_SIZE(mmc_supported));
-}
-
-int cpu_eth_init(bd_t *bis)
-{
-	u32 base;
-	int ret0, ret1;
-
-	qrk_pci_read_config_dword(QUARK_EMAC0, PCI_BASE_ADDRESS_0, &base);
-	ret0 = designware_initialize(base, PHY_INTERFACE_MODE_RMII);
-
-	qrk_pci_read_config_dword(QUARK_EMAC1, PCI_BASE_ADDRESS_0, &base);
-	ret1 = designware_initialize(base, PHY_INTERFACE_MODE_RMII);
-
-	if (ret0 < 0 && ret1 < 0)
-		return -1;
-	else
-		return 0;
 }
 
 void cpu_irq_init(void)
