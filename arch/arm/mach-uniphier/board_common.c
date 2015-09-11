cf88affab6c46bc728d47fe2590c465734d78efb
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-09-11 20:17:49 +0900

    ARM: uniphier: parse device tree to determine DRAM base and size
    Device tree specifies the available memory ranges in its "/memory"
    node.  Use it to simplify the CONFIG defines.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- arch/arm/mach-uniphier/board_common.c
+++ arch/arm/mach-uniphier/board_common.c
@@ -18,15 +18,3 @@ int board_init(void)
 
 	return 0;
 }
-
-#if CONFIG_NR_DRAM_BANKS >= 2
-void dram_init_banksize(void)
-{
-	DECLARE_GLOBAL_DATA_PTR;
-
-	gd->bd->bi_dram[0].start = CONFIG_SDRAM0_BASE;
-	gd->bd->bi_dram[0].size  = CONFIG_SDRAM0_SIZE;
-	gd->bd->bi_dram[1].start = CONFIG_SDRAM1_BASE;
-	gd->bd->bi_dram[1].size  = CONFIG_SDRAM1_SIZE;
-}
-#endif
