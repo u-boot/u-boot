04386f656bbc35abd7718a4bbbba7fcfb4f59aef
Stefan Roese <sr@denx.de>
2015-10-02 08:20:35 +0200

    Revert "powerpc: ppc4xx: remove lwmon5 support"
    This reverts commit 8fe11b8901a31d11990488c82bc23612589d57be.
    
    I'll add support to lwmon5 in the next patch and will remove
    support for the broken lcd4_lwmon5 as well.
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Cc: Masahiro Yamada <yamada.masahiro@socionext.com>

---
+++ board/lwmon5/config.mk
@@ -0,0 +1,18 @@
+#
+# (C) Copyright 2002
+# Wolfgang Denk, DENX Software Engineering, wd@denx.de.
+#
+# SPDX-License-Identifier:	GPL-2.0+
+#
+# lwmon5 (440EPx)
+#
+
+PLATFORM_CPPFLAGS += -DCONFIG_440=1
+
+ifeq ($(debug),1)
+PLATFORM_CPPFLAGS += -DDEBUG
+endif
+
+ifeq ($(dbcr),1)
+PLATFORM_CPPFLAGS += -DCONFIG_SYS_INIT_DBCR=0x8cff0000
+endif
