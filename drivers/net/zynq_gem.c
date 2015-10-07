24b1fd291646d10fb989151883808481bdae9f62
Michal Simek <michal.simek@xilinx.com>
2015-10-07 16:42:56 +0200

    net: zynq: Add support for different PHY interface types
    MII is setup by default for all cases. The most of boards are using
    RGMII but PHY drivers are not doing any specific setting that's why MII
    setting was working file. With TI DP83867 is necessary to setup
    paramaters based on interface type.
    
    Use one setting per board for it which is something what will be removed
    when driver is moved to DM.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/zynq_gem.c
+++ drivers/net/zynq_gem.c
@@ -168,6 +168,7 @@ struct zynq_gem_priv {
 	int phyaddr;
 	u32 emio;
 	int init;
+	phy_interface_t interface;
 	struct phy_device *phydev;
 	struct mii_dev *bus;
 };
@@ -391,7 +392,7 @@ static int zynq_gem_init(struct eth_device *dev, bd_t * bis)
 
 	/* interface - look at tsec */
 	phydev = phy_connect(priv->bus, priv->phyaddr, dev,
-			     PHY_INTERFACE_MODE_MII);
+			     priv->interface);
 
 	phydev->supported = supported | ADVERTISED_Pause |
 			    ADVERTISED_Asym_Pause;
@@ -622,6 +623,12 @@ int zynq_gem_initialize(bd_t *bis, phys_addr_t base_addr,
 
 	priv->phyaddr = phy_addr;
 	priv->emio = emio;
+
+#ifndef CONFIG_ZYNQ_GEM_INTERFACE
+	priv->interface = PHY_INTERFACE_MODE_MII;
+#else
+	priv->interface = CONFIG_ZYNQ_GEM_INTERFACE;
+#endif
 
 	sprintf(dev->name, "Gem.%lx", base_addr);
 
