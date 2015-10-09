6368c3e75fdeaf5e183acc704409b3b5b6d682b5
Thomas Chou <thomas@wytron.com.tw>
2015-10-09 09:32:00 +0800

    nios2: remove asm/psr.h
    Remove asm/psr.h, which is not used.
    Also clean up asm/sections.h and unaligned.h.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>
    Reviewed-by: Marek Vasut <marex@denx.de>

--- arch/nios2/include/asm/sections.h
+++ arch/nios2/include/asm/sections.h
@@ -1,11 +1,1 @@
-/*
- * Copyright (c) 2012 The Chromium OS Authors.
- * SPDX-License-Identifier:	GPL-2.0+
- */
-
-#ifndef __ASM_NIOS2_SECTIONS_H
-#define __ASM_NIOS2_SECTIONS_H
-
 #include <asm-generic/sections.h>
-
-#endif
