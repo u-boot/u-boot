ecdc3df611385625dc3902b3de118dfcbfd8fb0c
Paul Gortmaker <paul.gortmaker@windriver.com>
2015-10-17 16:40:31 -0400

    sbc8641d: increase monitor size from 256k to 384k
    Between v2015.07-rc1 and v2015.07-rc2 this board started
    silent boot failure.  A bisect led to commit 6eed3786c68c8a49d
    ("net: Move the CMD_NET config to defconfigs").  This commit
    looks harmless in itself, but it did implicitly add a feature
    to the image which led to this:
    
     u-boot$git describe 6eed3786c68c8a49d
     v2015.07-rc1-412-g6eed3786c68c
                  ^^^
    
     u-boot$ls -l ../41*/u-boot.bin
     -rwxrwxr-x 1 paul paul 261476 Oct 16 16:47 ../411/u-boot.bin
     -rwxrwxr-x 1 paul paul 266392 Oct 16 16:43 ../412/u-boot.bin
     u-boot$bc
     bc 1.06.95
     Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006 Free Software Foundation, Inc.
     This is free software with ABSOLUTELY NO WARRANTY.
     For details type `warranty'.
     256*1024
     262144
    
    i.e. we finally broke through the 256k monitor size.  Jump it
    up to 384k and fix the hard coded value used in the env offset
    at the same time.
    
    We were probably flirting with the 256k size issue without
    knowing it when testing on different baselines in earlier
    commits, but since this is all board specific, a rebase or
    reorder to put this commit 1st is of little value.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/configs/sbc8641d.h
+++ include/configs/sbc8641d.h
@@ -241,7 +241,7 @@
 #define CONFIG_SYS_GBL_DATA_OFFSET	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
 #define CONFIG_SYS_INIT_SP_OFFSET	CONFIG_SYS_GBL_DATA_OFFSET
 
-#define CONFIG_SYS_MONITOR_LEN		(256 * 1024)    /* Reserve 256 kB for Mon */
+#define CONFIG_SYS_MONITOR_LEN		(384 * 1024)    /* Reserve 384 kB for Mon */
 #define CONFIG_SYS_MALLOC_LEN		(1024 * 1024)   /* Reserved for malloc */
 
 /* Serial Port */
@@ -472,7 +472,7 @@
  * Environment
  */
 #define CONFIG_ENV_IS_IN_FLASH	1
-#define CONFIG_ENV_ADDR		(CONFIG_SYS_MONITOR_BASE + 0x40000)
+#define CONFIG_ENV_ADDR		(CONFIG_SYS_MONITOR_BASE + CONFIG_SYS_MONITOR_LEN)
 #define CONFIG_ENV_SECT_SIZE	0x20000	/* 128k(one sector) for env */
 #define CONFIG_ENV_SIZE		0x2000
 
