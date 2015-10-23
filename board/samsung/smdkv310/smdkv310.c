6b949ba8243342906a1f4a2e6f1f8c307de40ffd
Minkyu Kang <mk7.kang@samsung.com>
2015-10-23 15:59:37 +0900

    smdkv310: clean up checkpatch issues
    This patch will fix these checkpatch issues.
    
    WARNING: Avoid unnecessary line continuations
    +	gd->bd->bi_dram[0].size = get_ram_size((long *)PHYS_SDRAM_1, \
    
    WARNING: Avoid unnecessary line continuations
    +	gd->bd->bi_dram[1].size = get_ram_size((long *)PHYS_SDRAM_2, \
    
    WARNING: Avoid unnecessary line continuations
    +	gd->bd->bi_dram[2].size = get_ram_size((long *)PHYS_SDRAM_3, \
    
    WARNING: Avoid unnecessary line continuations
    +	gd->bd->bi_dram[3].size = get_ram_size((long *)PHYS_SDRAM_4, \
    
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

--- board/samsung/smdkv310/smdkv310.c
+++ board/samsung/smdkv310/smdkv310.c
@@ -55,16 +55,16 @@ int dram_init(void)
 void dram_init_banksize(void)
 {
 	gd->bd->bi_dram[0].start = PHYS_SDRAM_1;
-	gd->bd->bi_dram[0].size = get_ram_size((long *)PHYS_SDRAM_1, \
+	gd->bd->bi_dram[0].size = get_ram_size((long *)PHYS_SDRAM_1,
 							PHYS_SDRAM_1_SIZE);
 	gd->bd->bi_dram[1].start = PHYS_SDRAM_2;
-	gd->bd->bi_dram[1].size = get_ram_size((long *)PHYS_SDRAM_2, \
+	gd->bd->bi_dram[1].size = get_ram_size((long *)PHYS_SDRAM_2,
 							PHYS_SDRAM_2_SIZE);
 	gd->bd->bi_dram[2].start = PHYS_SDRAM_3;
-	gd->bd->bi_dram[2].size = get_ram_size((long *)PHYS_SDRAM_3, \
+	gd->bd->bi_dram[2].size = get_ram_size((long *)PHYS_SDRAM_3,
 							PHYS_SDRAM_3_SIZE);
 	gd->bd->bi_dram[3].start = PHYS_SDRAM_4;
-	gd->bd->bi_dram[3].size = get_ram_size((long *)PHYS_SDRAM_4, \
+	gd->bd->bi_dram[3].size = get_ram_size((long *)PHYS_SDRAM_4,
 							PHYS_SDRAM_4_SIZE);
 }
 
