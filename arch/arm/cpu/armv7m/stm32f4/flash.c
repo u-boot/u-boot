4cd3246f2a3a3a5032edcaed3cf807dd916f0aea
Vadzim Dambrouski <pftbest@gmail.com>
2015-10-23 21:14:06 +0300

    arm: stm32f4: fix a bug when only first sector gets erased
    flash_lock call is inside a for loop, so after the first iteration flash
    is locked and no more sectors can be erased.
    Move flash_lock out of the loop.
    
    Signed-off-by: Vadzim Dambrouski <pftbest@gmail.com>

--- arch/arm/cpu/armv7m/stm32f4/flash.c
+++ arch/arm/cpu/armv7m/stm32f4/flash.c
@@ -114,9 +114,9 @@ int flash_erase(flash_info_t *info, int first, int last)
 			;
 
 		clrbits_le32(&STM32_FLASH->cr, STM32_FLASH_CR_SER);
-		stm32f4_flash_lock(1);
 	}
 
+	stm32f4_flash_lock(1);
 	return 0;
 }
 
