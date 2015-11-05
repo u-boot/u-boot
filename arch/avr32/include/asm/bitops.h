61a177767cfcaa231b9e16d1d462a12ad472d777
Fabio Estevam <fabio.estevam@freescale.com>
2015-11-05 12:43:37 -0200

    avr32: Use the generic bitops headers
    The generic bitops headers are required when calling logarithmic
    functions, such as ilog2().
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>

--- arch/avr32/include/asm/bitops.h
+++ arch/avr32/include/asm/bitops.h
@@ -6,4 +6,9 @@
 #ifndef __ASM_AVR32_BITOPS_H
 #define __ASM_AVR32_BITOPS_H
 
+#include <asm-generic/bitops/fls.h>
+#include <asm-generic/bitops/__fls.h>
+#include <asm-generic/bitops/fls64.h>
+#include <asm-generic/bitops/__ffs.h>
+
 #endif /* __ASM_AVR32_BITOPS_H */
