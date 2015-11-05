f86411b18d69b03ca90c3864b8bffbff350f54e9
Fabio Estevam <fabio.estevam@freescale.com>
2015-11-05 12:43:28 -0200

    sh: Use the generic bitops headers
    The generic bitops headers are required when calling logarithmic
    functions, such as ilog2().
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Heiko Schocher <hs@denx.de>

--- arch/sh/include/asm/bitops.h
+++ arch/sh/include/asm/bitops.h
@@ -1,6 +1,11 @@
 #ifndef __ASM_SH_BITOPS_H
 #define __ASM_SH_BITOPS_H
 
+#include <asm-generic/bitops/fls.h>
+#include <asm-generic/bitops/__fls.h>
+#include <asm-generic/bitops/fls64.h>
+#include <asm-generic/bitops/__ffs.h>
+
 #ifdef __KERNEL__
 #include <asm/irqflags.h>
 /* For __swab32 */
