079bfc5db419337f800996f8a49fc4039392dda2
Thomas Chou <thomas@wytron.com.tw>
2015-11-14 11:26:51 +0800

    altera_pio: change ioremap to map_physmem
    Change ioremap() to map_physmem(), as it is more used in u-boot.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>
    Reviewed-by: Marek Vasut <marex@denx.de>

--- drivers/gpio/altera_pio.c
+++ drivers/gpio/altera_pio.c
@@ -89,8 +89,9 @@ static int altera_pio_ofdata_to_platdata(struct udevice *dev)
 {
 	struct altera_pio_platdata *plat = dev_get_platdata(dev);
 
-	plat->regs = ioremap(dev_get_addr(dev),
-		sizeof(struct altera_pio_regs));
+	plat->regs = map_physmem(dev_get_addr(dev),
+				 sizeof(struct altera_pio_regs),
+				 MAP_NOCACHE);
 	plat->gpio_count = fdtdec_get_int(gd->fdt_blob, dev->of_offset,
 		"altr,gpio-bank-width", 32);
 	plat->bank_name = fdt_getprop(gd->fdt_blob, dev->of_offset,
