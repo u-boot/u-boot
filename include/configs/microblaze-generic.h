a8cb39921557946538c34a5baf44217a8af92a25
Michal Simek <michal.simek@xilinx.com>
2015-11-24 13:30:21 +0100

    microblaze: Do not compile CMD_GPIO when driver is missing
    Do not compile CMD_GPIO when GPIO driver is not enabled.
    It is causing compilation error.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/microblaze-generic.h
+++ include/configs/microblaze-generic.h
@@ -247,7 +247,9 @@
 #define CONFIG_CMD_ASKENV
 #define CONFIG_CMD_IRQ
 #define CONFIG_CMD_MFSL
-#define CONFIG_CMD_GPIO
+#ifdef CONFIG_XILINX_GPIO
+# define CONFIG_CMD_GPIO
+#endif
 
 #if defined(CONFIG_DCACHE) || defined(CONFIG_ICACHE)
 # define CONFIG_CMD_CACHE
