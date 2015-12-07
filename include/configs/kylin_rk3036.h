47b4c228b5ae72598dc38ca2764c11ea1c6693cc
huang lin <hl@rock-chips.com>
2015-12-07 11:08:57 +0800

    rockchip: Add basic support for kylin board
    kylin board use rk3036 SOC, 512M sdram, 8G emmc.
    This add some basic files required to allow the board
    to output serial message and can run command(mmc info etc).
    
    Signed-off-by: Lin Huang <hl@rock-chips.com>
    Acked-by: Simon Glass <sjg@chromium.org>

---
+++ include/configs/kylin_rk3036.h
@@ -0,0 +1,12 @@
+/*
+ * (C) Copyright 2015 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <configs/rk3036_common.h>
+
+#endif
