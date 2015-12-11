61a4392a363875d393b520faa3031c46e96887fe
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-12-11 12:22:25 +0900

    m68k: M54418TWR: drop board/freescale/m54418twr/config.mk
    The board/freescale/m54418twr/config.mk defined TEXT_BASE, which has
    the same value as CONFIG_SYS_TEXT_BASE.  The TEXT_BASE is referenced
    by two files:
     - arch/m68k/cpu/mcf5445x/start.S and include/
     - include/configs/M54418TWR.h
    
    Replace the references with CONFIG_SYS_TEXT_BASE and delete
    board/freescale/m54418twr/config.mk.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Angelo Dureghello <angelo at sysam.it>

--- board/freescale/m54418twr/config.mk
+++
@@ -1,7 +0,0 @@
-# Copyright 2010-2012 Freescale Semiconductor, Inc.
-# TsiChung Liew (Tsi-Chung.Liew@freescale.com)
-#
-# SPDX-License-Identifier:	GPL-2.0+
-#
-
-PLATFORM_CPPFLAGS += -DTEXT_BASE=$(CONFIG_SYS_TEXT_BASE)
