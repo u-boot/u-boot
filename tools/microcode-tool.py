aefba6f1b5681f124b5dce4908219eb6374533c9
Bin Meng <bmeng.cn@gmail.com>
2015-12-11 02:55:46 -0800

    tools: microcode-tool: Support parsing header file with a license block
    The microcode header files in the Intel Chief River FSP package have
    a license comment block. Update the microcode-tool to support parsing
    it and extract the license text to the .dtsi file.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- tools/microcode-tool.py
+++ tools/microcode-tool.py
@@ -95,9 +95,23 @@ def ParseHeaderFiles(fname_list):
         name = os.path.splitext(name)[0]
         data = []
         with open(fname) as fd:
+            license_start = False
+            license_end = False
             for line in fd:
                 line = line.rstrip()
 
+                if len(line) >= 2:
+                    if line[0] == '/' and line[1] == '*':
+                        license_start = True
+                        continue
+                    if line[0] == '*' and line[1] == '/':
+                        license_end = True
+                        continue
+                if license_start and not license_end:
+                    # Ignore blank line
+                    if len(line) > 0:
+                        license_text.append(line)
+                    continue
                 # Omit anything after the last comma
                 words = line.split(',')[:-1]
                 data += [word + ',' for word in words]
