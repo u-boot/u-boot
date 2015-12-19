5b6f357fe73a3fab11870cceeac5f2fecd6d7e02
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2015-12-19 20:20:46 +0100

    MIPS: create .text sub-sections for assembler functions
    Put all functions coded in assembly in sub-sections of
    section .text. This allows the linker to garbage collect
    unused assembly functions too.
    
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/include/asm/asm.h
+++ arch/mips/include/asm/asm.h
@@ -59,6 +59,7 @@ symbol:
 		.align	2;                              \
 		.type	symbol, @function;              \
 		.ent	symbol, 0;                      \
+		.section .text.symbol, "x";             \
 symbol:		.frame	sp, 0, ra
 
 /*
@@ -68,7 +69,8 @@ symbol:		.frame	sp, 0, ra
 		.globl	symbol;                         \
 		.align	2;                              \
 		.type	symbol, @function;              \
-		.ent	symbol, 0;                       \
+		.ent	symbol, 0;                      \
+		.section .text.symbol, "x";             \
 symbol:		.frame	sp, framesize, rpc
 
 /*
