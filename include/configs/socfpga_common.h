eba522a0f7e0bae819633977aa9ad905775a1b68
Marek Vasut <marex@denx.de>
2015-12-20 04:00:45 +0100

    arm: socfpga: Enable DFU MMC support only if DM_MMC is enabled
    It is not possible to compile DFU MMC support if the MMC support is not
    compiled into U-Boot. Secure the code with an ifdef to prevent compiler
    splat.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Dinh Nguyen <dinguyen@opensource.altera.com>
    Cc: Chin Liang See <clsee@altera.com>

--- include/configs/socfpga_common.h
+++ include/configs/socfpga_common.h
@@ -248,7 +248,9 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 #define CONFIG_USB_FUNCTION_MASS_STORAGE
 
 #define CONFIG_USB_FUNCTION_DFU
+#ifdef CONFIG_DM_MMC
 #define CONFIG_DFU_MMC
+#endif
 #define CONFIG_SYS_DFU_DATA_BUF_SIZE	(32 * 1024 * 1024)
 #define DFU_DEFAULT_POLL_TIMEOUT	300
 
