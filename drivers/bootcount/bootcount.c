76765375d7aceab50ba7311612d8e5540cd7519f
Robert P. J. Day <rpjday@crashcourse.ca>
2015-12-22 07:15:14 -0500

    bootcount: Correct #endif comment to match, other aesthetics
    Correct mismatched comment on #endif to agree with its #if defined().
    
    Also add some missing #endif comments for consistency, take out
    extraneous blank lines for readability.
    
    rday

--- drivers/bootcount/bootcount.c
+++ drivers/bootcount/bootcount.c
@@ -31,16 +31,14 @@
 
 #if defined(CONFIG_MPC8260)
 #include <asm/cpm_8260.h>
-
 #define CONFIG_SYS_BOOTCOUNT_ADDR	(CONFIG_SYS_IMMR + CPM_BOOTCOUNT_ADDR)
 #endif /* defined(CONFIG_MPC8260) */
 
 #if defined(CONFIG_QE)
 #include <linux/immap_qe.h>
-
 #define CONFIG_SYS_BOOTCOUNT_ADDR	(CONFIG_SYS_IMMR + 0x110000 + \
 					 QE_MURAM_SIZE - 2 * sizeof(u32))
-#endif /* defined(CONFIG_MPC8360) */
+#endif /* defined(CONFIG_QE) */
 
 #if defined(CONFIG_4xx)
 #define CONFIG_SYS_BOOTCOUNT_ADDR	(CONFIG_SYS_OCM_DATA_ADDR + \
@@ -60,7 +58,7 @@ __weak void bootcount_store(ulong a)
 #else
 	raw_bootcount_store(reg, a);
 	raw_bootcount_store(reg + 4, BOOTCOUNT_MAGIC);
-#endif
+#endif /* defined(CONFIG_SYS_BOOTCOUNT_SINGLEWORD */
 }
 
 __weak ulong bootcount_load(void)
@@ -79,6 +77,6 @@ __weak ulong bootcount_load(void)
 		return 0;
 	else
 		return raw_bootcount_load(reg);
-#endif
+#endif /* defined(CONFIG_SYS_BOOTCOUNT_SINGLEWORD) */
 }
-#endif
+#endif /* defined(CONFIG_SYS_BOOTCOUNT_ADDR) */
