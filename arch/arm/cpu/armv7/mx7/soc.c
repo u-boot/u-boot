d9699de85c44cb23280ca4b61a9fd846dd3508ce
Peng Fan <van.freenix@gmail.com>
2016-01-04 13:16:41 +0800

    imx: mx7: default enable MDIO open drain
    The management data input/output (MDIO) requires open-drain,
    i.MX7D TO1.0 ENET MDIO pin has no open drain, but TO1.1 supports
    this feature. So to TO1.1, need to enable open drain by setting
    bits GPR0[8:7] for TO1.1.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>

--- arch/arm/cpu/armv7/mx7/soc.c
+++ arch/arm/cpu/armv7/mx7/soc.c
@@ -130,6 +130,24 @@ static void init_csu(void)
 		writel(CSU_INIT_SEC_LEVEL0, CSU_IPS_BASE_ADDR + i * 4);
 }
 
+static void imx_enet_mdio_fixup(void)
+{
+	struct iomuxc_gpr_base_regs *gpr_regs =
+		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+
+	/*
+	 * The management data input/output (MDIO) requires open-drain,
+	 * i.MX7D TO1.0 ENET MDIO pin has no open drain, but TO1.1 supports
+	 * this feature. So to TO1.1, need to enable open drain by setting
+	 * bits GPR0[8:7].
+	 */
+
+	if (soc_rev() >= CHIP_REV_1_1) {
+		setbits_le32(&gpr_regs->gpr[0],
+			     IOMUXC_GPR_GPR0_ENET_MDIO_OPEN_DRAIN_MASK);
+	}
+}
+
 int arch_cpu_init(void)
 {
 	init_aips();
@@ -137,6 +155,8 @@ int arch_cpu_init(void)
 	init_csu();
 	/* Disable PDE bit of WMCR register */
 	imx_set_wdog_powerdown(false);
+
+	imx_enet_mdio_fixup();
 
 #ifdef CONFIG_APBH_DMA
 	/* Start APBH DMA */
