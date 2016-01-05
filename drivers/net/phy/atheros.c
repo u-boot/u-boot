d584c68ce0f5bf2f430ccfb2ba00bd506206fb91
Fabio Estevam <fabio.estevam@nxp.com>
2016-01-05 17:02:52 -0200

    phy: atheros: Use ar8035_config for AR8031
    Commit 08ad9b068afb88 (" ar8031: modify the config func of ar8031 to
    ar8021_config") selected 'ar8021_config' as the configuration function
    for AR8031.
    
    The correct would be to use 'ar8035_config' instead as AR8031/AR8035
    have the same programming model and even share the same phy driver
    in the linux kernel: drivers/net/phy/at803x.c.
    
    Tested on a mx6qsabresd and wandboard, which now can work without
    any PHY setup code in the board files.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- drivers/net/phy/atheros.c
+++ drivers/net/phy/atheros.c
@@ -51,7 +51,7 @@ static struct phy_driver AR8031_driver =  {
 	.uid = 0x4dd074,
 	.mask = 0xffffffef,
 	.features = PHY_GBIT_FEATURES,
-	.config = ar8021_config,
+	.config = ar8035_config,
 	.startup = genphy_startup,
 	.shutdown = genphy_shutdown,
 };
