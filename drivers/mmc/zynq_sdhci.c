a57a4a5d830cb81bf73f241532d7867606ed61fb
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2016-01-05 12:21:04 +0530

    sdhci: zynq: Remove hardcoded value zero as min frequency
    Remove hardcoded value zero as min frequency and
    use config option CONFIG_ZYNQ_SDHCI_MIN_FREQ
    defined in board config
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mmc/zynq_sdhci.c
+++ drivers/mmc/zynq_sdhci.c
@@ -13,6 +13,10 @@
 #include <malloc.h>
 #include <sdhci.h>
 
+#ifndef CONFIG_ZYNQ_SDHCI_MIN_FREQ
+# define CONFIG_ZYNQ_SDHCI_MIN_FREQ	0
+#endif
+
 static int arasan_sdhci_probe(struct udevice *dev)
 {
 	struct mmc_uclass_priv *upriv = dev_get_uclass_priv(dev);
@@ -22,7 +26,8 @@ static int arasan_sdhci_probe(struct udevice *dev)
 		       SDHCI_QUIRK_BROKEN_R1B;
 	host->version = sdhci_readw(host, SDHCI_HOST_VERSION);
 
-	add_sdhci(host, CONFIG_ZYNQ_SDHCI_MAX_FREQ, 0);
+	add_sdhci(host, CONFIG_ZYNQ_SDHCI_MAX_FREQ,
+		  CONFIG_ZYNQ_SDHCI_MIN_FREQ);
 
 	upriv->mmc = host->mmc;
 
