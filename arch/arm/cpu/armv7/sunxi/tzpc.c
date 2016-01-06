5823664fb84612ef511136b66059710cccd71bed
Chen-Yu Tsai <wens@csie.org>
2016-01-06 15:13:06 +0800

    sunxi: Support Secure Memory Touch Arbiter (SMTA) in sun8i H3
    Secure Memory Touch Arbiter is the same thing as the TrustZone
    Protection Controller found on A31/A31s.
    
    Access to many peripherals on the H3 can be controlled by the SMTA,
    and the settings default to secure access only.
    
    This patch supports the new settings, and sets them to allow non-secure
    access.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- arch/arm/cpu/armv7/sunxi/tzpc.c
+++ arch/arm/cpu/armv7/sunxi/tzpc.c
@@ -13,6 +13,15 @@ void tzpc_init(void)
 {
 	struct sunxi_tzpc *tzpc = (struct sunxi_tzpc *)SUNXI_TZPC_BASE;
 
+#ifdef CONFIG_MACH_SUN6I
 	/* Enable non-secure access to the RTC */
-	writel(SUNXI_TZPC_DECPORT0_RTC, &tzpc->decport0_set);
+	writel(SUN6I_TZPC_DECPORT0_RTC, &tzpc->decport0_set);
+#endif
+
+#ifdef CONFIG_MACH_SUN8I_H3
+	/* Enable non-secure access to all peripherals */
+	writel(SUN8I_H3_TZPC_DECPORT0_ALL, &tzpc->decport0_set);
+	writel(SUN8I_H3_TZPC_DECPORT1_ALL, &tzpc->decport1_set);
+	writel(SUN8I_H3_TZPC_DECPORT2_ALL, &tzpc->decport2_set);
+#endif
 }
