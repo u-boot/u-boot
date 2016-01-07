a5f88877af9ae7d9f462efcbc5cb6fd7be9af106
Stefan Roese <sr@denx.de>
2016-01-07 14:09:09 +0100

    arm: mvebu: Add runtime boot-device detection
    This patch adds runtime boot-device detection to SPL U-Boot.
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Cc: Luka Perkov <luka.perkov@sartura.hr>
    Cc: Dirk Eibach <dirk.eibach@gdsys.cc>
    Cc: Phil Sutter <phil@nwl.cc>
    Cc: Kevin Smith <kevin.smith@elecsyscorp.com>

--- arch/arm/mach-mvebu/spl.c
+++ arch/arm/mach-mvebu/spl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2015 Stefan Roese <sr@denx.de>
+ * Copyright (C) 2014-2016 Stefan Roese <sr@denx.de>
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -15,14 +15,30 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+static u32 get_boot_device(void)
+{
+	u32 val;
+	u32 boot_device;
+
+	val = readl(CONFIG_SAR_REG);	/* SAR - Sample At Reset */
+	boot_device = (val & BOOT_DEV_SEL_MASK) >> BOOT_DEV_SEL_OFFS;
+	switch (boot_device) {
+#ifdef CONFIG_SPL_MMC_SUPPORT
+	case BOOT_FROM_MMC:
+	case BOOT_FROM_MMC_ALT:
+		return BOOT_DEVICE_MMC1;
+#endif
+	case BOOT_FROM_UART:
+		return BOOT_DEVICE_UART;
+	case BOOT_FROM_SPI:
+	default:
+		return BOOT_DEVICE_SPI;
+	};
+}
+
 u32 spl_boot_device(void)
 {
-#if defined(CONFIG_SPL_SPI_FLASH_SUPPORT)
-	return BOOT_DEVICE_SPI;
-#endif
-#if defined(CONFIG_SPL_MMC_SUPPORT)
-	return BOOT_DEVICE_MMC1;
-#endif
+	return get_boot_device();
 }
 
 #ifdef CONFIG_SPL_MMC_SUPPORT
