23ff8633fd8ca75d2ffd4595b9c72bb1a5fdbd20
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-01-12 21:48:25 +0100

    MIPS: sync I/O related header files with linux-4.4
    Mainly sync asm/io.h to get a working ioremap() implementation
    as well as the full set of I/O accessors. Pull in additional
    header files to make this work.
    
    Furthermore port over the directory 'arch/mips/include/asm/mach-generic/'
    with contains default definitions for I/O and memory spaces and default
    implementations for mapping those spaces. All files in that directory
    can be overwritten by a SoC/machine.
    
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/include/asm/addrspace.h
+++ arch/mips/include/asm/addrspace.h
@@ -10,6 +10,8 @@
 #ifndef _ASM_ADDRSPACE_H
 #define _ASM_ADDRSPACE_H
 
+#include <spaces.h>
+
 /*
  *  Configure language
  */
@@ -50,7 +52,7 @@
  */
 #define CPHYSADDR(a)		((_ACAST32_(a)) & 0x1fffffff)
 #define XPHYSADDR(a)		((_ACAST64_(a)) &			\
-				 _CONST64_(0x000000ffffffffff))
+				 _CONST64_(0x0000ffffffffffff))
 
 #ifdef CONFIG_64BIT
 
