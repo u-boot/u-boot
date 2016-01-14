197e14ea7ae76cc0bc18d601495ca01b097b0b2f
Jeffy Chen <jeffy.chen@rock-chips.com>
2016-01-14 10:19:38 +0800

    rockchip: kylin: Add default gpt partition table
    Add default android gpt partition table for kylin board.
    
    Use "gpt write mmc 0 $partitions" to apply.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Acked-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- include/configs/kylin_rk3036.h
+++ include/configs/kylin_rk3036.h
@@ -9,4 +9,33 @@
 
 #include <configs/rk3036_common.h>
 
+#ifndef CONFIG_SPL_BUILD
+
+/* Enable gpt partition table */
+#define CONFIG_CMD_GPT
+#define CONFIG_RANDOM_UUID
+#define CONFIG_EFI_PARTITION
+#define PARTS_DEFAULT \
+	"uuid_disk=${uuid_gpt_disk};" \
+	"name=loader,start=32K,size=4000K,uuid=${uuid_gpt_loader};" \
+	"name=reserved,size=64K,uuid=${uuid_gpt_reserved};" \
+	"name=misc,size=4M,uuid=${uuid_gpt_misc};" \
+	"name=recovery,size=32M,uuid=${uuid_gpt_recovery};" \
+	"name=boot_a,size=32M,uuid=${uuid_gpt_boot_a};" \
+	"name=boot_b,size=32M,uuid=${uuid_gpt_boot_b};" \
+	"name=system_a,size=818M,uuid=${uuid_gpt_system_a};" \
+	"name=system_b,size=818M,uuid=${uuid_gpt_system_b};" \
+	"name=vendor_a,size=50M,uuid=${uuid_gpt_vendor_a};" \
+	"name=vendor_b,size=50M,uuid=${uuid_gpt_vendor_b};" \
+	"name=cache,size=100M,uuid=${uuid_gpt_cache};" \
+	"name=metadata,size=16M,uuid=${uuid_gpt_metadata};" \
+	"name=persist,size=4M,uuid=${uuid_gpt_persist};" \
+	"name=userdata,size=-,uuid=${uuid_gpt_userdata};\0" \
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"partitions=" PARTS_DEFAULT \
+
+#endif
+
 #endif
