b6517a0d024f7430dac6bed97fac296f42d5c66b
Jeffy Chen <jeffy.chen@rock-chips.com>
2016-01-14 10:19:41 +0800

    rockchip: kylin: Store env in emmc
    There's a 64K reserved area at the end of the first 4M.
    Store env there, so we can use fastboot to flash it.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- include/configs/kylin_rk3036.h
+++ include/configs/kylin_rk3036.h
@@ -7,9 +7,21 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
+#include <linux/sizes.h>
 #include <configs/rk3036_common.h>
 
 #ifndef CONFIG_SPL_BUILD
+
+/* Store env in emmc */
+#undef CONFIG_ENV_SIZE
+#define CONFIG_ENV_SIZE			SZ_32K
+#undef CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_IS_IN_MMC
+#define CONFIG_SYS_MMC_ENV_DEV		0 /* emmc */
+#define CONFIG_SYS_MMC_ENV_PART		0 /* user area */
+#define CONFIG_ENV_OFFSET		(SZ_4M - SZ_64K) /* reserved area */
+#define CONFIG_ENV_OFFSET_REDUND	(CONFIG_ENV_OFFSET + CONFIG_ENV_SIZE)
+#define CONFIG_SYS_REDUNDAND_ENVIRONMENT
 
 /* Enable gpt partition table */
 #define CONFIG_CMD_GPT
