472040d5cbd1bbf9f5f325508b37feb7f129c125
Stephen Warren <swarren@wwwdotorg.org>
2016-01-15 11:15:25 -0700

    test/py: test that sandbox exits when asked
    Test the sandbox port's implementation of the reset command and SIGHUP
    handling. These should both cause the U-Boot process to exit gracefully.
    
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Simon Glass <sjg@chromium.org>

---
+++ test/py/tests/test_sandbox_exit.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2015 Stephen Warren
+# Copyright (c) 2015-2016, NVIDIA CORPORATION. All rights reserved.
+#
+# SPDX-License-Identifier: GPL-2.0
+
+import pytest
+import signal
+
+@pytest.mark.boardspec('sandbox')
+@pytest.mark.buildconfigspec('reset')
+def test_reset(u_boot_console):
+    '''Test that the "reset" command exits sandbox process.'''
+
+    u_boot_console.run_command('reset', wait_for_prompt=False)
+    assert(u_boot_console.validate_exited())
+    u_boot_console.ensure_spawned()
+
+@pytest.mark.boardspec('sandbox')
+def test_ctrl_c(u_boot_console):
+    '''Test that sending SIGINT to sandbox causes it to exit.'''
+
+    u_boot_console.kill(signal.SIGINT)
+    assert(u_boot_console.validate_exited())
+    u_boot_console.ensure_spawned()
