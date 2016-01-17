b2a6235920e2c0cd77b6826e9508401426f4ad92
Simon Glass <sjg@chromium.org>
2016-01-17 16:11:56 -0700

    x86: ivybridge: Drop XHCI support
    This is not used on link which is the only ivybridge board. Drop this code.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/x86/include/asm/arch-ivybridge/bd82x6x.h
+++ arch/x86/include/asm/arch-ivybridge/bd82x6x.h
@@ -7,7 +7,6 @@
 #ifndef _ASM_ARCH_BD82X6X_H
 #define _ASM_ARCH_BD82X6X_H
 
-void bd82x6x_usb_xhci_init(pci_dev_t dev);
 int gma_func0_init(struct udevice *dev, const void *blob, int node);
 
 #endif
