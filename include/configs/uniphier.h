cd5d95653c34723f99c2c712e7a9455687d36e9f
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-01-21 14:56:08 +0900

    ARM: uniphier: factor out common part of boot commands
    The environment "bootm_low" is updated before the "bootz" command.
    This is common for all the boot modes (NOR, NAND, TFTP, etc.), so
    can be factored out.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- include/configs/uniphier.h
+++ include/configs/uniphier.h
@@ -201,22 +201,21 @@
 	"ramdisk_addr_r=0x84a00000\0" \
 	"ramdisk_size=0x00600000\0" \
 	"ramdisk_file=rootfs.cpio.uboot\0" \
-	"norboot=setexpr bootm_low $kernel_addr_r '&' fe000000 &&" \
-		"setexpr kernel_addr $nor_base + $kernel_addr &&" \
+	"boot_common=setexpr bootm_low $kernel_addr_r '&' fe000000 &&" \
+		"bootz $kernel_addr_r $ramdisk_addr_r $fdt_addr_r\0" \
+	"norboot=setexpr kernel_addr $nor_base + $kernel_addr &&" \
 		"cp.b $kernel_addr $kernel_addr_r $kernel_size &&" \
-		"setexpr ramdisk_addr $nor_base + $ramdisk_addr &&" \
-		"setexpr fdt_addr $nor_base + $fdt_addr &&" \
-		"bootz $kernel_addr_r $ramdisk_addr $fdt_addr\0" \
-	"nandboot=setexpr bootm_low $kernel_addr_r '&' fe000000 &&" \
-		"nand read $kernel_addr_r $kernel_addr $kernel_size &&" \
+		"setexpr ramdisk_addr_r $nor_base + $ramdisk_addr &&" \
+		"setexpr fdt_addr_r $nor_base + $fdt_addr &&" \
+		"run boot_common\0" \
+	"nandboot=nand read $kernel_addr_r $kernel_addr $kernel_size &&" \
 		"nand read $ramdisk_addr_r $ramdisk_addr $ramdisk_size &&" \
 		"nand read $fdt_addr_r $fdt_addr $fdt_size &&" \
-		"bootz $kernel_addr_r $ramdisk_addr_r $fdt_addr_r\0" \
-	"tftpboot=setexpr bootm_low $kernel_addr_r '&' fe000000 &&" \
-		"tftpboot $kernel_addr_r $bootfile &&" \
+		"run boot_common\0" \
+	"tftpboot=tftpboot $kernel_addr_r $bootfile &&" \
 		"tftpboot $ramdisk_addr_r $ramdisk_file &&" \
 		"tftpboot $fdt_addr_r $fdt_file &&" \
-		"bootz $kernel_addr_r $ramdisk_addr_r $fdt_addr_r\0"
+		"run boot_common\0"
 #endif
 
 #define	CONFIG_EXTRA_ENV_SETTINGS				\
