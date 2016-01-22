74336f7daa4b1a45d04ddc9ef05737af54ae4836
Simon Glass <sjg@chromium.org>
2016-01-21 19:45:19 -0700

    rockchip: jerry: Enable EDP and HDMI video output
    Enable these devices using the VOPL video output device. We explicitly
    disable VOPB in the device tree to avoid it taking over. Since this device
    has an LCD display this comes up by default. If the display fails for some
    reason then it will attempt to use HDMI. It is possible to force it to fail
    (and thus fall back to HDMI) by puting 'return -EPERM' at the top of
    rk_edp_probe(). For now there is no easy way to select between the two.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/chromebook_jerry.h
+++ include/configs/chromebook_jerry.h
@@ -9,8 +9,8 @@
 
 #define ROCKCHIP_DEVICE_SETTINGS \
 		"stdin=serial,cros-ec-keyb\0" \
-		"stdout=serial\0" \
-		"stderr=serial\0"
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
 
 #include <configs/rk3288_common.h>
 
@@ -29,5 +29,9 @@
 
 #define CONFIG_SPL_POWER_SUPPORT
 #define CONFIG_SPL_I2C_SUPPORT
+
+#define CONFIG_I2C_EDID
+#define CONFIG_SYS_WHITE_ON_BLACK
+#define CONFIG_CONSOLE_SCROLL_LINES		10
 
 #endif
