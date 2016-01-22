889b8a3147e5ea453297c117c911efb61f798c0f
Simon Glass <sjg@chromium.org>
2016-01-21 19:45:20 -0700

    rockchip: firefly-rk3288: Enable HDMI output
    Enable HDMI output and a console on firefly.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/firefly-rk3288.h
+++ include/configs/firefly-rk3288.h
@@ -7,7 +7,10 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=serial,cros-ec-keyb\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
 
 #include <configs/rk3288_common.h>
 
@@ -20,5 +23,9 @@
  * u-boot @ 128K
  */
 #define CONFIG_ENV_OFFSET (96 * 1024)
+
+#define CONFIG_I2C_EDID
+#define CONFIG_SYS_WHITE_ON_BLACK
+#define CONFIG_CONSOLE_SCROLL_LINES		10
 
 #endif
