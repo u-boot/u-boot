28e92109c7b2e3eb2820f7bb6c339ba0f42a2a01
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2016-01-26 11:24:21 +0100

    ppc: xilinx_ppc440_generic: Remove weak attributes
    Now that the specific boards have been removed there is no need to
    maintain the weak functions.
    
    Fix also CamelCase to make checkpatch happy
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Acked-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Michal Simek <michal.simek@xilinx.com>

--- board/xilinx/ppc440-generic/xilinx_ppc440_generic.c
+++ board/xilinx/ppc440-generic/xilinx_ppc440_generic.c
@@ -10,35 +10,31 @@
 #include <common.h>
 #include <asm/processor.h>
 
-int __board_pre_init(void)
+int board_pre_init(void)
 {
 	return 0;
 }
-int board_pre_init(void) __attribute__((weak, alias("__board_pre_init")));
 
-int __checkboard(void)
+int checkboard(void)
 {
 	puts("Xilinx PPC440 Generic Board\n");
 	return 0;
 }
-int checkboard(void) __attribute__((weak, alias("__checkboard")));
 
-phys_size_t __initdram(int board_type)
+phys_size_t initdram(int board_type)
 {
 	return get_ram_size(XPAR_DDR2_SDRAM_MEM_BASEADDR,
 			    CONFIG_SYS_SDRAM_SIZE_MB * 1024 * 1024);
 }
-phys_size_t initdram(int) __attribute__((weak, alias("__initdram")));
 
-void __get_sys_info(sys_info_t *sysInfo)
+void get_sys_info(sys_info_t *sys_info)
 {
-	sysInfo->freqProcessor = XPAR_CORE_CLOCK_FREQ_HZ;
-	sysInfo->freqPLB = XPAR_PLB_CLOCK_FREQ_HZ;
-	sysInfo->freqPCI = 0;
+	sys_info->freqProcessor = XPAR_CORE_CLOCK_FREQ_HZ;
+	sys_info->freqPLB = XPAR_PLB_CLOCK_FREQ_HZ;
+	sys_info->freqPCI = 0;
 
 	return;
 }
-void get_sys_info(sys_info_t *) __attribute__((weak, alias("__get_sys_info")));
 
 int get_serial_clock(void){
 	return XPAR_UARTNS550_0_CLOCK_FREQ_HZ;
