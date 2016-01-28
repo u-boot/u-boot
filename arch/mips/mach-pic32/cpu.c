32c1a6eef8b467bb776f989caf0480fce6218598
Purna Chandra Mandal <purna.mandal@microchip.com>
2016-01-28 15:30:10 +0530

    MIPS: initial infrastructure for Microchip PIC32 architecture
    Create initial directory, Kconfigs needed for PIC32 architecture
    support. Also add PIC32 specific register definition required for drivers.
    
    Signed-off-by: Purna Chandra Mandal <purna.mandal@microchip.com>
    Reviewed-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>

---
+++ arch/mips/mach-pic32/cpu.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2015
+ * Purna Chandra Mandal <purna.mandal@microchip.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ */
+#include <common.h>
+
+phys_size_t initdram(int board_type)
+{
+	return 0;
+}
