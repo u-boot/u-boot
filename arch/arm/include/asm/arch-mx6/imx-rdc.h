d08607e1e7b05f86f81e21601f6012d20ae1c397
Peng Fan <peng.fan@nxp.com>
2016-01-28 16:55:01 +0800

    imx: mx6sx Add RDC mappings of masters and peripherals
    Add the definitions for the RDC mappings for i.MX6 SoloX.
    
    Signed-off-by: Ye.Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/include/asm/arch-mx6/imx-rdc.h
+++ arch/arm/include/asm/arch-mx6/imx-rdc.h
@@ -7,6 +7,10 @@
 #ifndef __IMX_RDC_H__
 #define __IMX_RDC_H__
 
+#if defined(CONFIG_MX6SX)
+#include "mx6sx_rdc.h"
+#else
 #error "Please select cpu"
+#endif /* CONFIG_MX6SX */
 
 #endif	/* __IMX_RDC_H__*/
