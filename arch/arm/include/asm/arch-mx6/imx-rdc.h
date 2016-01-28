de09c43bbfeeae767a8561d2810df55675e8b1a4
Peng Fan <peng.fan@nxp.com>
2016-01-28 16:55:00 +0800

    imx: imx-common: introduce Resource Domain Controller support
    Introduce Resource Domain Controller support for i.MX.
    Now i.MX6SX and i.MX7D supports this feature to assign masters
    and peripherals to different domains.
    
    Signed-off-by: Ye.Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

---
+++ arch/arm/include/asm/arch-mx6/imx-rdc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ *
+ * SPDX-License-Identifier:  GPL-2.0+
+ */
+
+#ifndef __IMX_RDC_H__
+#define __IMX_RDC_H__
+
+#error "Please select cpu"
+
+#endif	/* __IMX_RDC_H__*/
