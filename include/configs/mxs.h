baf7125acab7d7b56cf4e623d2a2b1204518bd37
Simon Glass <sjg@chromium.org>
2016-01-31 09:16:12 -0700

    dm: freescale: Drop mxs_auart serial driver
    This does not appear to be used, and has not been converted to driver model
    by the deadline (doc/driver-model/serial-howto.txt).
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/mxs.h
+++ include/configs/mxs.h
@@ -121,12 +121,10 @@
  * DUART Serial Driver.
  * Conflicts with AUART driver which can be set by board.
  */
-#ifndef CONFIG_MXS_AUART
 #define CONFIG_PL011_SERIAL
 #define CONFIG_PL011_CLOCK		24000000
 #define CONFIG_PL01x_PORTS		{ (void *)MXS_UARTDBG_BASE }
 #define CONFIG_CONS_INDEX		0
-#endif
 /* Default baudrate can be overriden by board! */
 #ifndef CONFIG_BAUDRATE
 #define CONFIG_BAUDRATE			115200
