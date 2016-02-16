ca94f161f340b2c39aeb1cd2a1b1ae445c990ba5
Michal Simek <michal.simek@xilinx.com>
2016-02-16 09:56:01 +0100

    ARM64: zynqmp: Remove earlyconX helper variables
    There is no need to specify earlycon for certain because only earlycon
    can be add to bootargs. Linux kernel read information about console from
    stdout-path property.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp.h
+++ include/configs/xilinx_zynqmp.h
@@ -218,8 +218,6 @@
 	"nfsroot=setenv bootargs $bootargs root=/dev/nfs nfsroot=$serverip:/mnt/sata,tcp ip=$ipaddr:$serverip:$serverip:255.255.255.0:zynqmp:eth0:off rw\0" \
 	"sdroot=setenv bootargs $bootargs root=/dev/mmcblk0p2 rw rootwait\0" \
 	"sdroot1=setenv bootargs $bootargs root=/dev/mmcblk1p2 rw rootwait\0" \
-	"earlycon0=setenv bootargs $bootargs earlycon=cdns,mmio,0xff000000,115200n8\0" \
-	"earlycon1=setenv bootargs $bootargs earlycon=cdns,mmio,0xff001000,115200n8\0" \
 	"android=setenv bootargs $bootargs init=/init androidboot.selinux=disabled androidboot.hardware=$board\0" \
 	"android_debug=run android && setenv bootargs $bootargs video=DP-1:1024x768@60 drm.debug=0xf\0" \
 	"usbhostboot=usb start && load usb 0 $fdt_addr system.dtb && " \
