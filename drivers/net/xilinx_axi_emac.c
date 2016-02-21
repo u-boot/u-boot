39579875c8c128e723e2cafcc4c63eed3b1a12e1
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2016-02-21 15:46:15 +0530

    net: xilinx_axi: Clear Isolate bit if found set suring phy
    In SGMII cases the isolate bit might set after DMA and
    ethernet resets and hence check and clear during
    setup_phy if it was set.
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/xilinx_axi_emac.c
+++ drivers/net/xilinx_axi_emac.c
@@ -269,6 +269,23 @@ static int setup_phy(struct udevice *dev)
 	struct axi_regs *regs = priv->iobase;
 	struct phy_device *phydev = priv->phydev;
 
+	if (priv->interface == PHY_INTERFACE_MODE_SGMII) {
+		/*
+		 * In SGMII cases the isolate bit might set
+		 * after DMA and ethernet resets and hence
+		 * check and clear if set.
+		 */
+		ret = phyread(priv, priv->phyaddr, MII_BMCR, &temp);
+		if (ret)
+			return 0;
+		if (temp & BMCR_ISOLATE) {
+			temp &= ~BMCR_ISOLATE;
+			ret = phywrite(priv, priv->phyaddr, MII_BMCR, temp);
+			if (ret)
+				return 0;
+		}
+	}
+
 	if (phy_startup(phydev)) {
 		printf("axiemac: could not initialize PHY %s\n",
 		       phydev->dev->name);
