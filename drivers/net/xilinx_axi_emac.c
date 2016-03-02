f0a4eaa7c2866bc79c59a78bc6e9f6651f701df0
Alistair Francis <alistair.francis@xilinx.com>
2016-03-02 09:37:28 -0800

    net: xilinx_axi: Declare another missing variable
    Since commit
    "net: xilinx_axi: Clear Isolate bit if found set suring phy"
    (sha1: 39579875c) u-boot fails to build as the temp variable is
    never declared. This patch declares the variable in the setup_phy()
    function.
    
    Signed-off-by: Alistair Francis <alistair.francis@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/xilinx_axi_emac.c
+++ drivers/net/xilinx_axi_emac.c
@@ -264,6 +264,7 @@ static int axiemac_phy_init(struct udevice *dev)
 /* Setting axi emac and phy to proper setting */
 static int setup_phy(struct udevice *dev)
 {
+	u16 temp;
 	u32 speed, emmc_reg, ret;
 	struct axidma_priv *priv = dev_get_priv(dev);
 	struct axi_regs *regs = priv->iobase;
