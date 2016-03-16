374e78efb0c6739c8c700325acca769d7a9e47de
Simon Glass <sjg@chromium.org>
2016-03-16 07:44:43 -0600

    x86: Add support for the samus chromebook
    This adds basic support for chromebook_samus. This is the 2015 Pixel and
    is based on an Intel broadwell platform.
    
    Supported so far are:
    - Serial
    - SPI flash
    - SDRAM init (with MRC cache)
    - SATA
    - Video (on the internal LCD panel)
    - Keyboard
    
    Various less-visible drivers are provided to make the above work (e.g. PCH,
    power control and LPC).
    
    The platform requires various binary blobs which are documented in the
    README. The major missing feature is USB3 since the existing U-Boot support
    does not work correctly with Intel XHCI controllers.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ include/configs/chromebook_samus.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors.
+ * (C) Copyright 2008
+ * Graeme Russ, graeme.russ@gmail.com.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+/*
+ * board/config.h - configuration options, board specific
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <configs/x86-common.h>
+#include <configs/x86-chromebook.h>
+
+#undef CONFIG_CFB_CONSOLE
+
+#undef CONFIG_STD_DEVICES_SETTINGS
+#define CONFIG_STD_DEVICES_SETTINGS     "stdin=usbkbd,i8042-kbd,serial\0" \
+					"stdout=vidconsole,serial\0" \
+					"stderr=vidconsole,serial\0"
+
+#define CONFIG_ENV_SECT_SIZE		0x1000
+#define CONFIG_ENV_OFFSET		0x003f8000
+
+#endif	/* __CONFIG_H */
