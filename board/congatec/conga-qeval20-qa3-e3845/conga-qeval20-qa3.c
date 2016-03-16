82ceba2ca2487c4967419cf7053e1301709219e5
Stefan Roese <sr@denx.de>
2016-03-16 08:48:21 +0100

    x86: Add congatec conga-QA3/E3845-4G (Bay Trail) support
    This patch adds support for the congatec conga-QA3/E3845-4G eMMC8 SoM,
    installed on the congatec Qseven 2.0 evaluation carrier board
    (conga-QEVAL).
    
    Its port is very similar to the MinnowboardMAX port and also uses
    the Intel FSP as described in doc/README.x86.
    
    Currently supported are the following interfaces / devices:
    - UART (via Winbond legacy SuperIO chip on carrier board)
    - Ethernet (PCIe Intel I210 / E1000)
    - SPI including SPI NOR as boot-device
    - USB 2.0
    - SATA via U-Boot SCSI IF
    - eMMC
    - Video (HDMI output @ 800x600)
    - PCIe
    
    Not supported yet is:
    - I2C
    - USB 3.0
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ board/congatec/conga-qeval20-qa3-e3845/conga-qeval20-qa3.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Stefan Roese <sr@denx.de>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <winbond_w83627.h>
+#include <asm/gpio.h>
+#include <asm/ibmpc.h>
+#include <asm/pnp_def.h>
+
+int board_early_init_f(void)
+{
+	/*
+	 * The FSP enables the BayTrail internal legacy UART (again).
+	 * Disable it again, so that the Winbond one can be used.
+	 */
+	setup_internal_uart(0);
+
+	/* Enable the legacy UART in the Winbond W83627 Super IO chip */
+	winbond_enable_serial(PNP_DEV(WINBOND_IO_PORT, W83627DHG_SP1),
+			      UART0_BASE, UART0_IRQ);
+
+	return 0;
+}
+
+int arch_early_init_r(void)
+{
+	return 0;
+}
