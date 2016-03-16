1eae8f66ff749409eb96e2f3f3387c56232d0b8a
Hans de Goede <hdegoede@redhat.com>
2016-03-16 13:46:22 +0100

    sunxi: Enable realtek phy support
    Enable building of drivers/net/phy/realtek.c so that realtek phys
    get properly initialized.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Ian Campbell <ijc@hellion.org.uk>

--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -310,6 +310,7 @@ extern int soft_i2c_gpio_scl;
 #define CONFIG_PHY_GIGE			/* GMAC can use gigabit PHY	*/
 #define CONFIG_PHY_ADDR		1
 #define CONFIG_MII			/* MII PHY management		*/
+#define CONFIG_PHY_REALTEK
 #endif
 
 #ifdef CONFIG_USB_EHCI_HCD
