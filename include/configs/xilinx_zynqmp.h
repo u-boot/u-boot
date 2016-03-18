ec98200e8b1e68df1e7de76b15c258f97573bdc4
Michal Simek <michal.simek@xilinx.com>
2016-03-18 18:09:15 +0100

    ARM64: zynqmp: Enable fat/ext4 write for USB too
    Fix USB config option.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp.h
+++ include/configs/xilinx_zynqmp.h
@@ -122,7 +122,7 @@
 # endif
 #endif
 
-#if defined(CONFIG_ZYNQ_SDHCI) || defined(CONFIG_ZYNQ_USB)
+#if defined(CONFIG_ZYNQ_SDHCI) || defined(CONFIG_ZYNQMP_USB)
 # define CONFIG_FAT_WRITE
 # define CONFIG_CMD_EXT4_WRITE
 #endif
