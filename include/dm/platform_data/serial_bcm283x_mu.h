3917c26909e4021c73073672865ec456eb51d640
Stephen Warren <swarren@wwwdotorg.org>
2016-03-18 21:41:38 -0600

    serial: add BCM283x mini UART driver
    The RPi3 typically uses the regular UART for high-speed communication with
    the Bluetooth device, leaving us the mini UART to use for the serial
    console. Add support for this UART so we can use it.
    
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>

---
+++ include/dm/platform_data/serial_bcm283x_mu.h
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 2016 Stephen Warren <swarren@wwwdotorg.org>
+ *
+ * Derived from pl01x code:
+ * Copyright (c) 2014 Google, Inc
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __serial_bcm283x_mu_h
+#define __serial_bcm283x_mu_h
+
+/*
+ *Information about a serial port
+ *
+ * @base: Register base address
+ */
+struct bcm283x_mu_serial_platdata {
+	unsigned long base;
+	unsigned int clock;
+	bool skip_init;
+};
+
+#endif
