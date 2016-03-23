16cee521384aa901a652fa3e4703e2d55518a9fe
Mingkai Hu <mingkai.hu@nxp.com>
2016-03-23 19:10:42 +0800

    armv8: fsl-layerscape: Fix LS1043A SVR register
    Signed-off-by: Mingkai Hu <mingkai.hu@nxp.com>
    Signed-off-by: Gong Qianyu <Qianyu.Gong@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- arch/arm/include/asm/arch-fsl-layerscape/soc.h
+++ arch/arm/include/asm/arch-fsl-layerscape/soc.h
@@ -41,7 +41,7 @@ struct cpu_type {
 	{ .name = #n, .soc_ver = SVR_##v, .num_cores = (nc)}
 
 #define SVR_WO_E		0xFFFFFE
-#define SVR_LS1043		0x879204
+#define SVR_LS1043		0x879200
 #define SVR_LS2045		0x870120
 #define SVR_LS2080		0x870110
 #define SVR_LS2085		0x870100
