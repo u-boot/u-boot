24ae3961f811ee79e6c98474e21e07f8ce222dfc
Kevin Smith <kevin.smith@elecsyscorp.com>
2016-03-31 19:33:12 +0000

    net: phy: Add PHY driver for mv88e61xx switches
    The previous mv88e61xx driver was a driver for configuring the
    switch, but did not integrate with the PHY/networking system, so
    it could not be used as a PHY by U-boot.  This is a complete
    rework to support this device as a PHY.
    
    Signed-off-by: Kevin Smith <kevin.smith@elecsyscorp.com>
    Acked-by: Prafulla Wadaskar <prafulla@marvell.com>
    Cc: Albert ARIBAUD <albert.u.boot@aribaud.net>
    Cc: Joe Hershberger <joe.hershberger@ni.com>
    Cc: Stefan Roese <sr@denx.de>
    Cc: Marek Vasut <marex@denx.de>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- include/phy.h
+++ include/phy.h
@@ -249,6 +249,7 @@ int gen10g_startup(struct phy_device *phydev);
 int gen10g_shutdown(struct phy_device *phydev);
 int gen10g_discover_mmds(struct phy_device *phydev);
 
+int phy_mv88e61xx_init(void);
 int phy_aquantia_init(void);
 int phy_atheros_init(void);
 int phy_broadcom_init(void);
