7a91e1a3835413aac3e7c894ca1ba205e6ade887
Wenyou Yang <wenyou.yang@atmel.com>
2016-04-11 16:41:32 +0800

    ARM: at91: clock: complete the GCK's clock sources
    Add the UPLL clock and master clock as a clock source for getting
    the generated clock frequency to complete its clock sources support.
    
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>
    Reviewed-by: Andreas Bießmann <andreas@biessmann.org>

--- arch/arm/mach-at91/armv7/clock.c
+++ arch/arm/mach-at91/armv7/clock.c
@@ -236,6 +236,12 @@ u32 at91_get_periph_generated_clk(u32 id)
 	case AT91_PMC_PCR_GCKCSS_PLLA_CLK:
 		freq = gd->arch.plla_rate_hz;
 		break;
+	case AT91_PMC_PCR_GCKCSS_UPLL_CLK:
+		freq = AT91_UTMI_PLL_CLK_FREQ;
+		break;
+	case AT91_PMC_PCR_GCKCSS_MCK_CLK:
+		freq = gd->arch.mck_rate_hz;
+		break;
 	default:
 		printf("Improper GCK clock source selection!\n");
 		freq = 0;
