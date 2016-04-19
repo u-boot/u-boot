c3a23e8b5f7c13d9de389d25d756a7da64bc5144
Andreas Fenkart <andreas.fenkart@digitalstrom.com>
2016-04-19 22:43:40 +0200

    tools/env: remove 'extern' from function prototype in fw_env.h
    checkpatch complains about in succeding patch. Prefer to fix all
    declarations in a dedicated patch.
    
    Signed-off-by: Andreas Fenkart <andreas.fenkart@digitalstrom.com>

--- tools/env/fw_env.h
+++ tools/env/fw_env.h
@@ -78,12 +78,12 @@ extern struct setenv_args setenv_args;
 
 int parse_aes_key(char *key, uint8_t *bin_key);
 
-extern int   fw_printenv(int argc, char *argv[]);
-extern char *fw_getenv  (char *name);
-extern int fw_setenv  (int argc, char *argv[]);
-extern int fw_parse_script(char *fname);
-extern int fw_env_open(void);
-extern int fw_env_write(char *name, char *value);
-extern int fw_env_close(void);
+int   fw_printenv(int argc, char *argv[]);
+char *fw_getenv(char *name);
+int fw_setenv(int argc, char *argv[]);
+int fw_parse_script(char *fname);
+int fw_env_open(void);
+int fw_env_write(char *name, char *value);
+int fw_env_close(void);
 
-extern unsigned	long  crc32	 (unsigned long, const unsigned char *, unsigned);
+unsigned long crc32(unsigned long, const unsigned char *, unsigned);
