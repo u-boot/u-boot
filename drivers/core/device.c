35732098db382b48e3fb4f3595fc108f69ea4457
Stephen Warren <swarren@nvidia.com>
2016-04-28 16:04:15 -0600

    fdt: fix dev_get_addr_name node offset
    Use the device's own DT offset, not the device's parent's.
    
    Fixes: 43c4d44e3330 ("fdt: implement dev_get_addr_name()")
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- drivers/core/device.c
+++ drivers/core/device.c
@@ -657,8 +657,8 @@ fdt_addr_t dev_get_addr_name(struct udevice *dev, const char *name)
 #if CONFIG_IS_ENABLED(OF_CONTROL)
 	int index;
 
-	index = fdt_find_string(gd->fdt_blob, dev->parent->of_offset,
-				"reg-names", name);
+	index = fdt_find_string(gd->fdt_blob, dev->of_offset, "reg-names",
+				name);
 	if (index < 0)
 		return index;
 
