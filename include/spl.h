304f05bba7d800d48824088f6137ff9e0d24f70b
Michal Simek <michal.simek@xilinx.com>
2016-05-10 07:54:20 +0200

    spl: Introduce new function spl_board_prepare_for_boot
    Call this function before passing control from SPL.
    For fpga case it is necessary to enable for example level shifters.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/spl.h
+++ include/spl.h
@@ -40,6 +40,7 @@ u32 spl_boot_mode(void);
 void spl_set_header_raw_uboot(void);
 void spl_parse_image_header(const struct image_header *header);
 void spl_board_prepare_for_linux(void);
+void spl_board_prepare_for_boot(void);
 void __noreturn jump_to_image_linux(void *arg);
 int spl_start_uboot(void);
 void spl_display_print(void);
