5a5f07a6af6c655cb89b7950813346c7903694b7
Michal Simek <michal.simek@xilinx.com>
2016-05-18 14:04:06 +0200

    ARM64: zynqmp: Add support for zc1751-dc3 and enable SPL
    Enable DC3 and SPL.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/configs/xilinx_zynqmp_zc1751_xm017_dc3.h
@@ -0,0 +1,26 @@
+/*
+ * Configuration for Xilinx ZynqMP zc1751 XM017 DC3
+ *
+ * (C) Copyright 2015 Xilinx, Inc.
+ * Michal Simek <michal.simek@xilinx.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQMP_ZC1751_XM017_DC3_H
+#define __CONFIG_ZYNQMP_ZC1751_XM017_DC3_H
+
+#define CONFIG_ZYNQ_SDHCI1
+#define CONFIG_ZYNQ_I2C0
+#define CONFIG_ZYNQ_I2C1
+
+#define CONFIG_ZYNQMP_XHCI_LIST {ZYNQMP_USB0_XHCI_BASEADDR, ZYNQMP_USB1_XHCI_BASEADDR}
+
+#define CONFIG_IDENT_STRING	" Xilinx ZynqMP ZC1751 xm017 dc3"
+
+#define CONFIG_AHCI
+#define CONFIG_SATA_CEVA
+
+#include <configs/xilinx_zynqmp.h>
+
+#endif /* __CONFIG_ZYNQMP_ZC1751_XM017_DC3_H */
