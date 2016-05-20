62a09f0535b223c8acbf85da8d4b1acf1667826e
Daniel Allred <d-allred@ti.com>
2016-05-19 19:10:54 -0500

    ARM: omap5: add ft_board_setup for dra7xx/am57xx
    Adds the board specific ft_board_setup() functions that
    are called when CONFIG_OF_BOARD_SETUP is defined. These functions
    will currently just call the ft_cpu_setup() function.
    
    Adds CONFIG_OF_BOARD_SETUP to the defconfig files
    for dra72_evm, dra74_evm, and am57xx_evm.
    
    Signed-off-by: Daniel Allred <d-allred@ti.com>
    Signed-off-by: Madan Srinivas <madans@ti.com>
    
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- board/ti/am57xx/board.c
+++ board/ti/am57xx/board.c
@@ -686,3 +686,12 @@ int board_early_init_f(void)
 	return 0;
 }
 #endif
+
+#if defined(CONFIG_OF_LIBFDT) && defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	ft_cpu_setup(blob, bd);
+
+	return 0;
+}
+#endif
