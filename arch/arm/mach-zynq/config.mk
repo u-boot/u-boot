b6ea3f5217990e2c5bd794c7827e61a3d6ebbfe0
Michal Simek <michal.simek@xilinx.com>
2016-05-24 14:05:57 +0200

    ARM: zynq: Remove -mno-unaligned-access flag duplication
    Patch:
    "arm: Switch to -mno-unaligned-access when supported by the compiler"
    (sha1: 1551df35f296f0a8df32f4f2054254f46e8be252)
    enables -mno-unaligned-access for all platforms. Remove it for Zynq.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/config.mk
+++ arch/arm/mach-zynq/config.mk
@@ -4,5 +4,3 @@
 # SPDX-License-Identifier:      GPL-2.0
 #
 PLATFORM_RELFLAGS += -fno-strict-aliasing
-# Xilinx, added to prevent unaligned accesses which started happening # with GCC 4.5.2 tools
-PLATFORM_RELFLAGS += -mno-unaligned-access
