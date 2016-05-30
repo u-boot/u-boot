53e796e152c1d69df19865cd387479dcf9a4b0d1
Michal Simek <michal.simek@xilinx.com>
2016-05-30 10:43:11 +0200

    net: gem: Add support for more PHYs on MDIO bus
    Find out MDIO bus and enable MDIO access to it if this is done via
    different controller.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/zynq_gem.c
+++ drivers/net/zynq_gem.c
@@ -743,12 +743,14 @@ static int zynq_gem_ofdata_to_platdata(struct udevice *dev)
 
 	pdata->iobase = (phys_addr_t)dev_read_addr(dev);
 	priv->iobase = (struct zynq_gem_regs *)pdata->iobase;
-	priv->mdiobase = priv->iobase;
 	/* Hardcode for now */
 	priv->phyaddr = -1;
 
 	if (!dev_read_phandle_with_args(dev, "phy-handle", NULL, 0, 0,
 					&phandle_args)) {
+		fdt_addr_t addr;
+		ofnode parent;
+
 		debug("phy-handle does exist %s\n", dev->name);
 		priv->phyaddr = ofnode_read_u32_default(phandle_args.node,
 							"reg", -1);
@@ -756,6 +758,16 @@ static int zynq_gem_ofdata_to_platdata(struct udevice *dev)
 		priv->max_speed = ofnode_read_u32_default(phandle_args.node,
 							  "max-speed",
 							  SPEED_1000);
+
+		parent = ofnode_get_parent(phandle_args.node);
+		addr = ofnode_get_addr(parent);
+		if (addr == FDT_ADDR_T_NONE) {
+			printf("MDIO bus not found %s\n", dev->name);
+			return -ENODEV;
+		}
+		priv->mdiobase = (struct zynq_gem_regs *)addr;
+	} else {
+		priv->mdiobase = priv->iobase;
 	}
 
 	phy_mode = dev_read_prop(dev, "phy-mode", NULL);
