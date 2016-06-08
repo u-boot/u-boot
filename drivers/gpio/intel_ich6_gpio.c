8142340ee3847c6422294fad0f1adc01c9d27e2b
Bin Meng <bmeng.cn@gmail.com>
2016-06-08 05:07:32 -0700

    x86: ich6_gpio: Output return value of syscon_get_by_driver_data()
    The call to syscon_get_by_driver_data() does not save its return value.
    Print it out to aid debugging.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/gpio/intel_ich6_gpio.c
+++ drivers/gpio/intel_ich6_gpio.c
@@ -114,9 +114,11 @@ static int ich6_gpio_probe(struct udevice *dev)
 	struct gpio_dev_priv *uc_priv = dev_get_uclass_priv(dev);
 	struct ich6_bank_priv *bank = dev_get_priv(dev);
 	struct udevice *pinctrl;
+	int ret;
 
 	/* Set up pin control if available */
-	syscon_get_by_driver_data(X86_SYSCON_PINCONF, &pinctrl);
+	ret = syscon_get_by_driver_data(X86_SYSCON_PINCONF, &pinctrl);
+	debug("%s, pinctrl=%p, ret=%d\n", __func__, pinctrl, ret);
 
 	uc_priv->gpio_count = GPIO_PER_BANK;
 	uc_priv->bank_name = plat->bank_name;
