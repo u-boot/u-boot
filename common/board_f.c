ba169d981f80517f057bf635df7e3dab203b9cea
Anatolij Gustschin <agust@denx.de>
2016-06-13 14:24:24 +0200

    board_f: prevent misleading "Watchdog enabled" output
    Output the "Watchdog enabled" message only if hw_watchdog_init()
    call really happened.
    
    Signed-off-by: Anatolij Gustschin <agust@denx.de>

--- common/board_f.c
+++ common/board_f.c
@@ -120,8 +120,8 @@ static int init_func_watchdog_init(void)
 	defined(CONFIG_DESIGNWARE_WATCHDOG) || \
 	defined(CONFIG_IMX_WATCHDOG))
 	hw_watchdog_init();
-# endif
 	puts("       Watchdog enabled\n");
+# endif
 	WATCHDOG_RESET();
 
 	return 0;
