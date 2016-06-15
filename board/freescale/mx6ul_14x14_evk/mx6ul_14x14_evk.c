288683280f18b267615d9afa86e1378727fe3405
Peng Fan <van.freenix@gmail.com>
2016-06-15 14:18:38 +0800

    imx6ulevk: fix LCD_nPWREN setting
    Q901 is PMOS, LCD_nPWREN should be at low voltage then output is 3V3.
    If LCD_nPWREN is high, output is 2.4V which is not correct.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>
    Cc: Fabio Estevam <fabio.estevam@nxp.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>

--- board/freescale/mx6ul_14x14_evk/mx6ul_14x14_evk.c
+++ board/freescale/mx6ul_14x14_evk/mx6ul_14x14_evk.c
@@ -117,7 +117,7 @@ static enum qn_level seq[3][2] = {
 
 static enum qn_func qn_output[8] = {
 	qn_reset, qn_reset, qn_reset, qn_enable, qn_disable, qn_reset,
-	qn_disable, qn_enable
+	qn_disable, qn_disable
 };
 
 static void iox74lv_init(void)
