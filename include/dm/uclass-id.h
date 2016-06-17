89c1e2da78f82a09685006291ce8bb44f635fa25
Stephen Warren <swarren@nvidia.com>
2016-06-17 09:43:58 -0600

    Add a reset driver framework/uclass
    A reset controller is a hardware module that controls reset signals that
    affect other hardware modules or chips.
    
    This patch defines a standard API that connects reset clients (i.e. the
    drivers for devices affected by reset signals) to drivers for reset
    controllers/providers. Initially, DT is the only supported method for
    connecting the two.
    
    The DT binding specification (reset.txt) was taken from Linux kernel
    v4.5's Documentation/devicetree/bindings/reset/reset.txt.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -63,6 +63,7 @@ enum uclass_id {
 	UCLASS_PWRSEQ,		/* Power sequence device */
 	UCLASS_REGULATOR,	/* Regulator device */
 	UCLASS_REMOTEPROC,	/* Remote Processor device */
+	UCLASS_RESET,		/* Reset controller device */
 	UCLASS_RTC,		/* Real time clock device */
 	UCLASS_SERIAL,		/* Serial UART */
 	UCLASS_SPI,		/* SPI bus */
