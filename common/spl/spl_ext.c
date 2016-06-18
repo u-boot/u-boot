9ab165d8b05706284de0ee9bd402346c54ae6b15
Petr Kulhavy <brain@jikos.cz>
2016-06-18 12:21:17 +0200

    SPL ext: cosmetic: correct error message in spl_load_image_ext()
    Correct the error message in spl_load_image_ext() when image parsing
    fails. Instead of "ext4fs_read failed" print "failed to parse image
    header".
    
    Signed-off-by: Petr Kulhavy <brain@jikos.cz>
    CC: Guillaume GARDET <guillaume.gardet@free.fr>
    CC: Tom Rini <trini@konsulko.com>

--- common/spl/spl_ext.c
+++ common/spl/spl_ext.c
@@ -50,7 +50,7 @@ int spl_load_image_ext(struct blk_desc *block_dev,
 
 	err = spl_parse_image_header(header);
 	if (err < 0) {
-		puts("spl: ext4fs_read failed\n");
+		puts("spl: ext: failed to parse image header\n");
 		goto end;
 	}
 
