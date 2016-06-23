61520ac4d5545cc8d2e1792092e46ab8043d5f36
Marek Vasut <marex@denx.de>
2016-06-23 18:14:50 +0200

    arm: socfpga: Fix "improve raw MMC SPL boot"
    This fixes commit d31e9c575f24f4b7f5f382ccae70d7a86bbc379d ,
    which broke booting from SD card on all SoCFPGA boards. The
    patch assumes the bootloader partition to be partition 3, at
    the end of the SD card, which doesn't make any sense. U-Boot
    assumes the bootloader partition is partition 1 or that the
    bootloader image is at offset +1 MiB from the start of SD card.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Sylvain Lesne <lesne@alse-fr.com>

--- include/configs/socfpga_common.h
+++ include/configs/socfpga_common.h
@@ -346,7 +346,7 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 #define CONFIG_SPL_FS_LOAD_PAYLOAD_NAME		"u-boot-dtb.img"
 #define CONFIG_SPL_LIBDISK_SUPPORT
 #else
-#define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_PARTITION	3
+#define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_PARTITION	1
 #define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR	0x200 /* offset 512 sect (256k) */
 #define CONFIG_SPL_LIBDISK_SUPPORT
 #endif
