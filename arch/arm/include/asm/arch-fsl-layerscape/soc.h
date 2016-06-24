49cdce163519ad760ca7769be64dd2a6133a5c11
Prabhakar Kushwaha <prabhakar.kushwaha@nxp.com>
2016-06-24 13:48:13 +0530

    armv8: fsl-layerscape: Append "A" in SoC name for ARM based SoCs
    Freescale ARMv8 SoC name ends with "A" to represent ARM SoCs.
    like LS2080A, LS1043A, LS1012A.
    
    So append "A" to SoC names.
    
    Signed-off-by: Pratiyush Mohan Srivastava <pratiyush.srivastava@nxp.com>
    Signed-off-by: Prabhakar Kushwaha <prabhakar.kushwaha@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- arch/arm/include/asm/arch-fsl-layerscape/soc.h
+++ arch/arm/include/asm/arch-fsl-layerscape/soc.h
@@ -41,13 +41,13 @@ struct cpu_type {
 	{ .name = #n, .soc_ver = SVR_##v, .num_cores = (nc)}
 
 #define SVR_WO_E		0xFFFFFE
-#define SVR_LS1012		0x870400
-#define SVR_LS1043		0x879200
-#define SVR_LS1023		0x879208
-#define SVR_LS2045		0x870120
-#define SVR_LS2080		0x870110
-#define SVR_LS2085		0x870100
-#define SVR_LS2040		0x870130
+#define SVR_LS1012A		0x870400
+#define SVR_LS1043A		0x879200
+#define SVR_LS1023A		0x879208
+#define SVR_LS2045A		0x870120
+#define SVR_LS2080A		0x870110
+#define SVR_LS2085A		0x870100
+#define SVR_LS2040A		0x870130
 
 #define SVR_MAJ(svr)		(((svr) >> 4) & 0xf)
 #define SVR_MIN(svr)		(((svr) >> 0) & 0xf)
