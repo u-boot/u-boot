ec6f61003b6cda13b20878a990e5f9b70496d0ad
Daniel Allred <d-allred@ti.com>
2016-06-27 09:19:16 -0500

    arm: cache: add missing dummy functions for when dcache disabled
    Adds missing flush_dcache_range and invalidate_dcache_range dummy
    (empty) placeholder functions to the #else portion of the #ifndef
    CONFIG_SYS_DCACHE_OFF, where full implementations of these functions
    are defined.
    
    Signed-off-by: Daniel Allred <d-allred@ti.com>
    Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- arch/arm/cpu/armv7/cache_v7.c
+++ arch/arm/cpu/armv7/cache_v7.c
@@ -195,6 +195,14 @@ void flush_dcache_all(void)
 {
 }
 
+void invalidate_dcache_range(unsigned long start, unsigned long stop)
+{
+}
+
+void flush_dcache_range(unsigned long start, unsigned long stop)
+{
+}
+
 void arm_init_before_mmu(void)
 {
 }
