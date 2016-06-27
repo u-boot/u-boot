1bb0a21b465427088ad623826fe23066399cb2d9
Andreas Dannenberg <dannenberg@ti.com>
2016-06-27 09:19:19 -0500

    arm: omap-common: secure ROM signature verify API
    Adds an API that verifies a signature attached to an image (binary
    blob). This API is basically a entry to a secure ROM service provided by
    the device and accessed via an SMC call, using a particular calling
    convention.
    
    Signed-off-by: Daniel Allred <d-allred@ti.com>
    Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- arch/arm/include/asm/omap_sec_common.h
+++ arch/arm/include/asm/omap_sec_common.h
@@ -18,4 +18,13 @@
  */
 u32 secure_rom_call(u32 service, u32 proc_id, u32 flag, ...);
 
+/*
+ * Invoke a secure ROM API on high-secure (HS) device variants that can be used
+ * to verify a secure blob by authenticating and optionally decrypting it. The
+ * exact operation performed depends on how the certificate that was embedded
+ * into the blob during the signing/encryption step when the secure blob was
+ * first created.
+ */
+int secure_boot_verify_image(void **p_image, size_t *p_size);
+
 #endif /* _OMAP_SEC_COMMON_H_ */
