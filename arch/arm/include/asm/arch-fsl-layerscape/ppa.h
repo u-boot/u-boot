f1dd4cadd20c3b6b2454b7f293265caa66f27fee
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2016-06-28 20:18:14 +0800

    ARMv8/layerscape: Add FSL PPA support
    The FSL Primary Protected Application (PPA) is a software component
    loaded during boot which runs in TrustZone and remains resident
    after boot.
    
    Use the secure firmware framework to integrate FSL PPA into U-Boot.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

---
+++ arch/arm/include/asm/arch-fsl-layerscape/ppa.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2016 NXP Semiconductor, Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __FSL_PPA_H_
+#define __FSL_PPA_H_
+
+#define SEC_FIRMWARE_FIT_IMAGE		"firmware"
+#define SEC_FIRMEWARE_FIT_CNF_NAME	"config@1"
+#define SEC_FIRMWARE_TARGET_EL		2
+
+int ppa_init(void);
+
+#endif
