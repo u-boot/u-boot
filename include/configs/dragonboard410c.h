a3e2efcb42be378659a5443346ca5c77317c30b9
Ricardo Salveti de Araujo <rsalveti@rsalveti.net>
2016-07-03 14:16:03 -0300

    dragonboard410c: adding missing default addr for script and pxe boot
    Cc: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Ricardo Salveti <rsalveti@rsalveti.net>

--- include/configs/dragonboard410c.h
+++ include/configs/dragonboard410c.h
@@ -126,6 +126,8 @@ REFLASH(dragonboard/u-boot.img, 8)\
 	"fdtfile=apq8016-sbc.dtb\0" \
 	"fdt_addr_r=0x83000000\0"\
 	"ramdisk_addr_r=0x84000000\0"\
+	"scriptaddr=0x90000000\0"\
+	"pxefile_addr_r=0x90100000\0"\
 	BOOTENV
 
 #define CONFIG_ENV_IS_NOWHERE
