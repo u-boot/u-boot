2372e14f1937fceea54d698342e5a4240b58a893
Vignesh R <vigneshr@ti.com>
2016-07-06 10:20:56 +0530

    spi: cadence_quadspi: Enable QUAD mode based on DT data
    Instead of relying on CONFIG_SPI_FLASH_QUAD to be defined to enable QUAD
    mode, make use of mode_rx field of dm_spi_slave_platdata to determine
    whether to enable or disable QUAD mode. This is necessary to support
    muliple SPI controllers where one of them may not support QUAD mode.
    
    Signed-off-by: Vignesh R <vigneshr@ti.com>
    Tested-by: Marek Vasut <marex@denx.de>
    Acked-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Jagan Teki <jteki@openedev.com>

--- drivers/spi/cadence_qspi.h
+++ drivers/spi/cadence_qspi.h
@@ -53,7 +53,7 @@ int cadence_qspi_apb_command_write(void *reg_base_addr,
 	unsigned int txlen,  const u8 *txbuf);
 
 int cadence_qspi_apb_indirect_read_setup(struct cadence_spi_platdata *plat,
-	unsigned int cmdlen, const u8 *cmdbuf);
+	unsigned int cmdlen, unsigned int rx_width, const u8 *cmdbuf);
 int cadence_qspi_apb_indirect_read_execute(struct cadence_spi_platdata *plat,
 	unsigned int rxlen, u8 *rxbuf);
 int cadence_qspi_apb_indirect_write_setup(struct cadence_spi_platdata *plat,
