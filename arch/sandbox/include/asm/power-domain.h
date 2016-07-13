61f5ddcb7a997f7b7bca3680cd6f67e60e616841
Stephen Warren <swarren@nvidia.com>
2016-07-13 13:45:31 -0600

    Add a power domain framework/uclass
    Many SoCs allow power to be applied to or removed from portions of the SoC
    (power domains). This may be used to save power. This API provides the
    means to control such power management hardware.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Simon Glass <sjg@chromium.org>

---
+++ arch/sandbox/include/asm/power-domain.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016, NVIDIA CORPORATION.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef __SANDBOX_POWER_DOMAIN_H
+#define __SANDBOX_POWER_DOMAIN_H
+
+#include <common.h>
+
+struct udevice;
+
+int sandbox_power_domain_query(struct udevice *dev, unsigned long id);
+
+int sandbox_power_domain_test_get(struct udevice *dev);
+int sandbox_power_domain_test_on(struct udevice *dev);
+int sandbox_power_domain_test_off(struct udevice *dev);
+int sandbox_power_domain_test_free(struct udevice *dev);
+
+#endif
