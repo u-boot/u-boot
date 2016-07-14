b6f4048b547f576d7534533fc17b3b257a252e26
Michal Simek <michal.simek@xilinx.com>
2016-07-14 11:13:04 +0200

    ARM64: zynqmp: Ignore warnings from autogenerated files
    Autogenerated files contain casting issues and missing function
    declaration and even usleep implementation. Suppress them for now
    till these files are fixed.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- board/xilinx/zynqmp/xil_io.h
+++ board/xilinx/zynqmp/xil_io.h
@@ -7,6 +7,7 @@
 
 /* FIXME remove this when vivado is fixed */
 #include <asm/io.h>
+#include <common.h>
 
 #define xil_printf(...)
 
@@ -31,5 +32,13 @@ int Xil_In32(unsigned long addr)
 {
 	return readl(addr);
 }
+
+void mask_delay(u32 delay);
+void usleep(u32 sleep)
+{
+	udelay(sleep);
+}
+int mask_poll(u32 add, u32 mask);
+int mask_pollOnValue(u32 add, u32 mask, u32 value);
 
 #endif /* XIL_IO_H */
