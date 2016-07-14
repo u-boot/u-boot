313fb12ead72b74d4e4e5f6d2487dfd512e63bcc
Michal Simek <michal.simek@xilinx.com>
2016-07-14 13:11:37 +0200

    dm: clk: Remove simple version of clk_get_by_index/name()
    Simple version of clk_get_by_index() added by:
    "dm: clk: Add a simple version of clk_get_by_index()"
    (sha1: a4b10c088c4f6ef2e2bba33e8cfea369bcbbce44)
    is only working for #clock-cells=<1> but not for
    any other values. Fixed clocks is using #clock-cells=<0>
    which requires full implementation.
    
    Remove simplified versions of clk_get_by_index() and use full version.
    Also remove empty clk_get_by_name() which is failing when it is called
    which is useless.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/clk/clk-uclass.c
+++ drivers/clk/clk-uclass.c
@@ -20,31 +20,6 @@ static inline struct clk_ops *clk_dev_ops(struct udevice *dev)
 }
 
 #if CONFIG_IS_ENABLED(OF_CONTROL)
-#ifdef CONFIG_SPL_BUILD
-int clk_get_by_index(struct udevice *dev, int index, struct clk *clk)
-{
-	int ret;
-	u32 cell[2];
-
-	if (index != 0)
-		return -ENOSYS;
-	assert(clk);
-	ret = uclass_get_device(UCLASS_CLK, 0, &clk->dev);
-	if (ret)
-		return ret;
-	ret = fdtdec_get_int_array(gd->fdt_blob, dev->of_offset, "clocks",
-				   cell, 2);
-	if (ret)
-		return ret;
-	clk->id = cell[1];
-	return 0;
-}
-
-int clk_get_by_name(struct udevice *dev, const char *name, struct clk *clk)
-{
-	return -ENOSYS;
-}
-#else
 static int clk_of_xlate_default(struct clk *clk,
 				struct fdtdec_phandle_args *args)
 {
@@ -117,7 +92,6 @@ int clk_get_by_name(struct udevice *dev, const char *name, struct clk *clk)
 
 	return clk_get_by_index(dev, index, clk);
 }
-#endif
 #endif
 
 int clk_request(struct udevice *dev, struct clk *clk)
