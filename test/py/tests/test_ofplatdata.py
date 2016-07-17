f60d0603edca472c4458b30956f38c6c1a836d66
Simon Glass <sjg@chromium.org>
2016-07-16 18:36:44 -0600

    test: Adjust the of-platdata test run condition
    This should be spl_of_platdata, since otherwise it will try to run on boards
    that don't support of-platdata.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/py/tests/test_ofplatdata.py
+++ test/py/tests/test_ofplatdata.py
@@ -34,7 +34,7 @@ string <NULL>
 stringarray "one" "" ""
 '''
 
-@pytest.mark.buildconfigspec('spl')
+@pytest.mark.buildconfigspec('spl_of_platdata')
 def test_ofplatdata(u_boot_console):
     """Test that of-platdata can be generated and used in sandbox"""
     cons = u_boot_console
