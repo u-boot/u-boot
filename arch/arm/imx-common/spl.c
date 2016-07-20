68b09b891323b6f44e145164dfe3e1cd3322cc36
Breno Lima <breno.lima@nxp.com>
2016-07-20 16:55:32 -0300

    Revert "imx_common: Return MMCSD_MODE_FS in spl_boot_mode() also for EXTFS"
    Commit c1ebf54868359005 ("imx_common: Return MMCSD_MODE_FS in spl_boot_mode()
    also for EXTFS") causes SPL breakage on wandboard:
    
    ERROR: v7_dcache_inval_range - start address is not aligned - 0x1820006c
    ERROR: v7_dcache_inval_range - stop address is not aligned - 0x1820086c
    ERROR: v7_dcache_inval_range - start address is not aligned - 0x1820006c
    ERROR: v7_dcache_inval_range - stop address is not aligned - 0x1820086c
    ** First descriptor is NOT a primary desc on 0:1 **
    spl: no partition table found
    SPL: failed to boot from all boot devices
    ### ERROR ### Please RESET the board ###
    
    This error is seen when SPL and u-boot.img are stored in the raw SD card
    partition.
    
    This reverts commit c1ebf54868359005c32944c1473668d5fcaca158.
    
    Signed-off-by: Breno Lima <breno.lima@nxp.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>

--- arch/arm/imx-common/spl.c
+++ arch/arm/imx-common/spl.c
@@ -76,7 +76,7 @@ u32 spl_boot_mode(const u32 boot_device)
 	/* for MMC return either RAW or FAT mode */
 	case BOOT_DEVICE_MMC1:
 	case BOOT_DEVICE_MMC2:
-#if defined(CONFIG_SPL_FAT_SUPPORT) || defined(CONFIG_SPL_EXT_SUPPORT)
+#if defined(CONFIG_SPL_FAT_SUPPORT)
 		return MMCSD_MODE_FS;
 #elif defined(CONFIG_SUPPORT_EMMC_BOOT)
 		return MMCSD_MODE_EMMCBOOT;
