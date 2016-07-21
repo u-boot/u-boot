de8499de2cbed7e84ad9f75e346d0aaf20feca80
Mike Looijmans <mike.looijmans@topic.nl>
2016-07-21 15:55:02 +0200

    ARM64: zynqmp: Make booting from SD possible without uEnv.txt
    When booting from SD card, the kernel cannot find any rootfs.
    It only works if you provide a "uEnv.txt" file on the SD card that
    patches the bootargs.
    
    If uEnv.txt does not exist, assume that the rootfs is on the second
    partition of the SD card (or mmc device). To make this work, rename
    "sdroot" to "sdroot0" so a simple script can switch between sd0 and sd1.
    
    Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp.h
+++ include/configs/xilinx_zynqmp.h
@@ -187,7 +187,7 @@
 			"echo Loaded environment from ${bootenv}; " \
 			"run importbootenv; " \
 		"fi\0" \
-	"sdboot=mmc dev $sdbootdev && mmcinfo && run uenvboot; " \
+	"sdboot=mmc dev $sdbootdev && mmcinfo && run uenvboot || run sdroot$sdbootdev; " \
 		"load mmc $sdbootdev:$partid $fdt_addr system.dtb && " \
 		"load mmc $sdbootdev:$partid $kernel_addr Image && " \
 		"booti $kernel_addr - $fdt_addr\0" \
@@ -202,7 +202,7 @@
 		 "tftpboot 6000000 rootfs.cpio.ub && booti 80000 6000000 $fdt_addr\0" \
 	"nosmp=setenv bootargs $bootargs maxcpus=1\0" \
 	"nfsroot=setenv bootargs $bootargs root=/dev/nfs nfsroot=$serverip:/mnt/sata,tcp ip=$ipaddr:$serverip:$serverip:255.255.255.0:zynqmp:eth0:off rw\0" \
-	"sdroot=setenv bootargs $bootargs root=/dev/mmcblk0p2 rw rootwait\0" \
+	"sdroot0=setenv bootargs $bootargs root=/dev/mmcblk0p2 rw rootwait\0" \
 	"sdroot1=setenv bootargs $bootargs root=/dev/mmcblk1p2 rw rootwait\0" \
 	"android=setenv bootargs $bootargs init=/init androidboot.selinux=disabled androidboot.hardware=$board\0" \
 	"android_debug=run android && setenv bootargs $bootargs video=DP-1:1024x768@60 drm.debug=0xf\0" \
