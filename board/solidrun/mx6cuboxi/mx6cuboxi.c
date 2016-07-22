4a2f9014e82e73caeb09b39e4bf02f1460279882
Breno Lima <breno.lima@nxp.com>
2016-07-22 09:11:30 -0300

    mx6cuboxi: Replace is_mx6q() for macro
    It's not necessary to implement the is_mx6q function, there is a macro in
    sys_proto.h already implemented.
    
    Signed-off-by: Breno Lima <breno.lima@nxp.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>

--- board/solidrun/mx6cuboxi/mx6cuboxi.c
+++ board/solidrun/mx6cuboxi/mx6cuboxi.c
@@ -367,14 +367,6 @@ int checkboard(void)
 	return 0;
 }
 
-static bool is_mx6q(void)
-{
-	if (is_cpu_type(MXC_CPU_MX6Q) || is_cpu_type(MXC_CPU_MX6D))
-		return true;
-	else
-		return false;
-}
-
 int board_late_init(void)
 {
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
@@ -383,7 +375,7 @@ int board_late_init(void)
 	else
 		setenv("board_name", "CUBOXI");
 
-	if (is_mx6q())
+	if (is_mx6dq())
 		setenv("board_rev", "MX6Q");
 	else
 		setenv("board_rev", "MX6DL");
@@ -615,7 +607,7 @@ static void spl_dram_init(int width)
 		.ddr_type = DDR_TYPE_DDR3,
 	};
 
-	if (is_cpu_type(MXC_CPU_MX6D) || is_cpu_type(MXC_CPU_MX6Q))
+	if (is_mx6dq())
 		mx6dq_dram_iocfg(width, &mx6q_ddr_ioregs, &mx6q_grp_ioregs);
 	else
 		mx6sdl_dram_iocfg(width, &mx6dl_ddr_ioregs, &mx6sdl_grp_ioregs);
