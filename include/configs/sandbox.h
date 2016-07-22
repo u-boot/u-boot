5923c843ba0e06a90e0c0c5cc6894ac8641377e1
Simon Glass <sjg@chromium.org>
2016-07-22 09:22:45 -0600

    sandbox: Add instructions about building on 32-bit machines
    Sandbox is built with 64-bit ints by default. This doesn't work properly on
    32-bit machines.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- include/configs/sandbox.h
+++ include/configs/sandbox.h
@@ -27,7 +27,10 @@
 
 #define CONFIG_SYS_STDIO_DEREGISTER
 
-/* Number of bits in a C 'long' on this architecture */
+/*
+ * Number of bits in a C 'long' on this architecture. Set this to 32 when
+ * building on a 32-bit machine.
+ */
 #define CONFIG_SANDBOX_BITS_PER_LONG	64
 
 #define CONFIG_LMB
