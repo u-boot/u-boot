63326e6f0b48616ec708faec5fab55887fa8bf51
Fabio Estevam <fabio.estevam@nxp.com>
2016-07-23 13:23:42 -0300

    cgtqmx6eval: Remove uneeded PHYS_SDRAM_SIZE
    cgtqmx6eval uses the imx_ddr_size() function to calculate the DDR size in
    runtime, so there is no need to define PHYS_SDRAM_SIZE.
    
    Remove the unneeded definition.
    
    Cc: Otavio Salvador <otavio@ossystems.com.br>
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Acked-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/cgtqmx6eval.h
+++ include/configs/cgtqmx6eval.h
@@ -251,7 +251,6 @@
 /* Physical Memory Map */
 #define CONFIG_NR_DRAM_BANKS	       1
 #define PHYS_SDRAM		       MMDC0_ARB_BASE_ADDR
-#define PHYS_SDRAM_SIZE			       (1u * 1024 * 1024 * 1024)
 
 #define CONFIG_SYS_SDRAM_BASE	       PHYS_SDRAM
 #define CONFIG_SYS_INIT_RAM_ADDR       IRAM_BASE_ADDR
