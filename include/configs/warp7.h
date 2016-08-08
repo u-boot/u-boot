ed39522680873db35edabdb8325575d9fd08c756
Breno Lima <breno.lima@nxp.com>
2016-08-08 09:57:38 -0300

    warp7: Modify fdt_file environment variable
    Use imx7s-warp.dts as fdt_file because this is the name that upstream
    kernel will deploy.
    
    Signed-off-by: Breno Lima <breno.lima@nxp.com>
    Acked-by: Fabio Estevam <fabio.estevam@nxp.com>

--- include/configs/warp7.h
+++ include/configs/warp7.h
@@ -39,7 +39,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx7d-warp.dtb\0" \
+	"fdt_file=imx7s-warp.dtb\0" \
 	"fdt_addr=0x83000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
