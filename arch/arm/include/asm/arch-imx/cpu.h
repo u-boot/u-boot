65ce54be8e0566f9ccaaccce19f6be69ce51d874
Peng Fan <van.freenix@gmail.com>
2016-08-11 14:02:38 +0800

    imx: mx6ull: add mx6ull major cpu type
    Add i.MX6ULL major cpu type.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>
    Reviewed-by: Stefano Babic <sbabic@denx.de>

--- arch/arm/include/asm/arch-imx/cpu.h
+++ arch/arm/include/asm/arch-imx/cpu.h
@@ -17,7 +17,8 @@
 #define MXC_CPU_MX6SX		0x62
 #define MXC_CPU_MX6Q		0x63
 #define MXC_CPU_MX6UL		0x64
-#define MXC_CPU_MX6SOLO		0x65 /* dummy ID */
+#define MXC_CPU_MX6ULL		0x65
+#define MXC_CPU_MX6SOLO		0x66 /* dummy */
 #define MXC_CPU_MX6D		0x67
 #define MXC_CPU_MX6DP		0x68
 #define MXC_CPU_MX6QP		0x69
