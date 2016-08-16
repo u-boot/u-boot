ad0513828e30210450a43526eccbd98bfd232953
Kever Yang <kever.yang@rock-chips.com>
2016-08-16 17:58:13 +0800

    rk3399: enable the pwm2/3 pinctrl in board init
    There is no interrupt line for each PWM which used by pinctrl to get the
    periph_id, so it's not able to enable the default pinctrl setting by pinctrl
    framework, let's enable it at board_init().
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- board/rockchip/evb_rk3399/evb-rk3399.c
+++ board/rockchip/evb_rk3399/evb-rk3399.c
@@ -4,12 +4,41 @@
  * SPDX-License-Identifier:     GPL-2.0+
  */
 #include <common.h>
-#include <asm/armv8/mmu.h>
+#include <dm.h>
+#include <dm/pinctrl.h>
+#include <asm/arch/periph.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
+	struct udevice *pinctrl;
+	int ret;
+
+	/*
+	 * The PWM do not have decicated interrupt number in dts and can
+	 * not get periph_id by pinctrl framework, so let's init them here.
+	 * The PWM2 and PWM3 are for pwm regulater.
+	 */
+	ret = uclass_get_device(UCLASS_PINCTRL, 0, &pinctrl);
+	if (ret) {
+		debug("%s: Cannot find pinctrl device\n", __func__);
+		goto out;
+	}
+
+	ret = pinctrl_request_noflags(pinctrl, PERIPH_ID_PWM2);
+	if (ret) {
+		debug("%s PWM2 pinctrl init fail!\n", __func__);
+		goto out;
+	}
+
+	ret = pinctrl_request_noflags(pinctrl, PERIPH_ID_PWM3);
+	if (ret) {
+		debug("%s PWM3 pinctrl init fail!\n", __func__);
+		goto out;
+	}
+
+out:
 	return 0;
 }
 
