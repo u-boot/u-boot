1befb38b868248dda4e84a57aa779f7e8c0b85c0
Alexander Graf <agraf@suse.de>
2016-08-19 01:23:21 +0200

    x86: Move table csum into separate file
    We need the checksum function without all the other table functionality
    soon, so let's split it out into its own C file.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ include/tables_csum.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2015, Bin Meng <bmeng.cn@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _TABLES_CSUM_H_
+#define _TABLES_CSUM_H_
+
+u8 table_compute_checksum(void *v, int len);
+
+#endif
