217f92bb79ea43df4d0fe47e31c28c01c721ad87
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-08-30 16:22:22 +0900

    ARM: armv7: move CONFIG_ARMV7_PSCI to Kconfig
    Add ARCH_SUPPORT_PSCI as a non-configurable option that platforms
    can select.  Then, move CONFIG_ARMV7_PSCI, which is automatically
    enabled if both ARMV7_NONSEC and ARCH_SUPPORT_PSCI are enabled.
    
    Reviewed-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- include/configs/sun8i.h
+++ include/configs/sun8i.h
@@ -27,7 +27,6 @@
 #endif
 
 #ifndef CONFIG_MACH_SUN8I_A83T
-#define CONFIG_ARMV7_PSCI		1
 #if defined(CONFIG_MACH_SUN8I_A23)
 #define CONFIG_ARMV7_PSCI_NR_CPUS	2
 #elif defined(CONFIG_MACH_SUN8I_A33)
