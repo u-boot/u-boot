4a79ececebdaee108a6f18ddb5684e2590fa2de6
Kever Yang <kever.yang@rock-chips.com>
2016-09-01 10:14:20 +0800

    rk3399: add a empty "sys_proto.h" header file
    driver/usb/dwc3/gadget.c need a "sys_proto.h" header file, add a
    empty one to make compile success.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Simon Glass <sjg@chromium.org>

---
+++ arch/arm/include/asm/arch-rockchip/sys_proto.h
@@ -0,0 +1,10 @@
+/*
+ * (C) Copyright 2016 Rockchip Electronics Co.,Ltd
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _ASM_ARCH_SYS_PROTO_H
+#define _ASM_ARCH_SYS_PROTO_H
+
+#endif /* _ASM_ARCH_SYS_PROTO_H */
