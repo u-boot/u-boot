95689da5a7301e7727d58e9f44e990d4aa18ea1c
Simon Glass <sjg@chromium.org>
2016-09-12 23:18:46 -0600

    Convert CONFIG_SPL_MTD_SUPPORT to Kconfig
    Move this option to Kconfig and tidy up existing uses.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/configs/am3517_evm.h
+++ include/configs/am3517_evm.h
@@ -349,7 +349,6 @@
 #define CONFIG_SPL_NAND_BASE
 #define CONFIG_SPL_NAND_DRIVERS
 #define CONFIG_SPL_NAND_ECC
-#define CONFIG_SPL_MTD_SUPPORT
 #define CONFIG_SPL_POWER_SUPPORT
 #define CONFIG_SPL_LDSCRIPT		"$(CPUDIR)/omap-common/u-boot-spl.lds"
 
