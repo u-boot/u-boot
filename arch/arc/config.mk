7c8d81605302e7d7fdd3e7d8eb69302bddc64a2c
Alexey Brodkin <abrodkin@synopsys.com>
2016-09-16 12:12:26 +0300

    arc: Use -mcpu=XXX instead of obsolete -marcXXX
    With newer ARC tools old way of CPU specification gets obsolete,
    so we're switching to newer and more common way of setting "-mcpu".
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- arch/arc/config.mk
+++ arch/arc/config.mk
@@ -31,15 +31,15 @@ CONFIG_MMU = 1
 endif
 
 ifdef CONFIG_CPU_ARC750D
-PLATFORM_CPPFLAGS += -marc700
+PLATFORM_CPPFLAGS += -mcpu=arc700
 endif
 
 ifdef CONFIG_CPU_ARC770D
-PLATFORM_CPPFLAGS += -marc700 -mlock -mswape
+PLATFORM_CPPFLAGS += -mcpu=arc700 -mlock -mswape
 endif
 
 ifdef CONFIG_CPU_ARCEM6
-PLATFORM_CPPFLAGS += -marcem
+PLATFORM_CPPFLAGS += -mcpu=arcem
 endif
 
 ifdef CONFIG_CPU_ARCHS34
