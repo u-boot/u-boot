b2b135d980a7171c9b0eefbb38861ccbe9a47f97
Paul Burton <paul.burton@imgtec.com>
2016-09-21 11:18:53 +0100

    MIPS: Map CM Global Control Registers
    Map the Global Control Registers (GCRs) provided by the MIPS Coherence
    Manager (CM) in preparation for using some of them in later patches.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

---
+++ arch/mips/include/asm/cm.h
@@ -0,0 +1,19 @@
+/*
+ * MIPS Coherence Manager (CM) Register Definitions
+ *
+ * Copyright (c) 2016 Imagination Technologies Ltd.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __MIPS_ASM_CM_H__
+#define __MIPS_ASM_CM_H__
+
+/* Global Control Register (GCR) offsets */
+#define GCR_BASE			0x0008
+#define GCR_BASE_UPPER			0x000c
+#define GCR_REV				0x0030
+
+/* GCR_REV CM versions */
+#define GCR_REV_CM3			0x0800
+
+#endif /* __MIPS_ASM_CM_H__ */
