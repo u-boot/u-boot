57718f017b10871cd9dc03dca56c3bd690fc2614
Simon Glass <sjg@chromium.org>
2016-09-25 21:33:10 -0600

    mmc: Fix cast for 64-bit compilation
    Fix a cast that causes warnings on 64-bit machines.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- drivers/mmc/pci_mmc.c
+++ drivers/mmc/pci_mmc.c
@@ -30,7 +30,7 @@ int pci_mmc_init(const char *name, struct pci_device_id *mmc_supported)
 
 		mmc_host->name = name;
 		dm_pci_read_config32(dev, PCI_BASE_ADDRESS_0, &iobase);
-		mmc_host->ioaddr = (void *)iobase;
+		mmc_host->ioaddr = (void *)(ulong)iobase;
 		mmc_host->quirks = 0;
 		ret = add_sdhci(mmc_host, 0, 0);
 		if (ret)
