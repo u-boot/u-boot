d7608aba38d760be4c40cb8f4591057e5e40c2d4
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-24 10:47:01 -0700

    efi: Use device device path type Messaging for network interface node
    When adding network interface node use Messaging device path with
    subtype MAC Address and device's MAC address as a value instead
    of Media Device path type with subtype File Path and path "Net"
    
    Signed-off-by: Oleksandr Tymoshenko <gonzo@bluezbox.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- include/efi_api.h
+++ include/efi_api.h
@@ -268,6 +268,19 @@ struct efi_device_path {
 	u16 length;
 };
 
+struct efi_mac_addr {
+	u8 addr[32];
+};
+
+#define DEVICE_PATH_TYPE_MESSAGING_DEVICE	0x03
+#  define DEVICE_PATH_SUB_TYPE_MSG_MAC_ADDR	0x0b
+
+struct efi_device_path_mac_addr {
+	struct efi_device_path dp;
+	struct efi_mac_addr mac;
+	u8 if_type;
+};
+
 #define DEVICE_PATH_TYPE_MEDIA_DEVICE		0x04
 #  define DEVICE_PATH_SUB_TYPE_FILE_PATH	0x04
 
