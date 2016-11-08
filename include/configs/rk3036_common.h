1e352124b94f977fb7bbb516ea09ef345a70ee09
Kever Yang <kever.yang@rock-chips.com>
2016-11-08 18:13:39 +0800

    config: rk3036: enable configs for USB HOST
    rk3036 using dwc2 usb controller, need enable relate configs for it.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- include/configs/rk3036_common.h
+++ include/configs/rk3036_common.h
@@ -79,6 +79,13 @@
 #define CONFIG_G_DNL_VENDOR_NUM		0x2207
 #define CONFIG_G_DNL_PRODUCT_NUM	0x310a
 
+/* usb host */
+#ifdef CONFIG_CMD_USB
+#define CONFIG_USB_DWC2
+#define CONFIG_USB_HOST_ETHER
+#define CONFIG_USB_ETHER_SMSC95XX
+#define CONFIG_USB_ETHER_ASIX
+#endif
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"scriptaddr=0x60000000\0" \
 	"pxefile_addr_r=0x60100000\0" \
