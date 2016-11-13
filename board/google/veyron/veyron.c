38ffcb679b1694f8ebf4e5670614fb830ae0c2f2
Simon Glass <sjg@chromium.org>
2016-11-13 14:22:11 -0700

    rockchip: veyron: Add a note about the SDRAM voltage
    Add a comment to indicate that we are not supporting the PWM regulator
    yet.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- board/google/veyron/veyron.c
+++ board/google/veyron/veyron.c
@@ -5,3 +5,9 @@
  */
 
 #include <common.h>
+
+/*
+ * We should increase the DDR voltage to 1.2V using the PWM regulator.
+ * There is a U-Boot driver for this but it may need to add support for the
+ * 'voltage-table' property.
+ */
