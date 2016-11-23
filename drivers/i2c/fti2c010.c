2852709676c89e45b564ff0d64ce53e921c99c1d
Simon Glass <sjg@chromium.org>
2016-11-23 06:34:44 -0700

    dm: i2c: Add a note to I2C drivers which need conversion
    Maintainers need to be notified more directly of the need to convert these
    drivers. Add a note to the top each affected file.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Acked-by: Heiko Schocher <hs@denx.de>

--- drivers/i2c/fti2c010.c
+++ drivers/i2c/fti2c010.c
@@ -5,6 +5,9 @@
  * Dante Su <dantesu@faraday-tech.com>
  *
  * SPDX-License-Identifier:	GPL-2.0+
+ *
+ * NOTE: This driver should be converted to driver model before June 2017.
+ * Please see doc/driver-model/i2c-howto.txt for instructions.
  */
 
 #include <common.h>
