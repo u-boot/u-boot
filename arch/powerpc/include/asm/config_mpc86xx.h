6c691a1c7efa9f43e7b9fac94e6377b9dc7067a8
York Sun <york.sun@nxp.com>
2016-11-23 14:16:01 -0800

    powerpc: mpc86xx: Remove macro CONFIG_MAX_CPUS
    This macro CONFIG_MAX_CPUS is not used for MPC86xx SoCs.
    
    Signed-off-by: York Sun <york.sun@nxp.com>

--- arch/powerpc/include/asm/config_mpc86xx.h
+++ arch/powerpc/include/asm/config_mpc86xx.h
@@ -12,11 +12,9 @@
 /* SoC specific defines for Freescale MPC86xx processors */
 
 #if defined(CONFIG_ARCH_MPC8610)
-#define CONFIG_MAX_CPUS			1
 #define CONFIG_SYS_FSL_NUM_LAWS		10
 
 #elif defined(CONFIG_ARCH_MPC8641)
-#define CONFIG_MAX_CPUS			2
 #define CONFIG_SYS_FSL_NUM_LAWS		10
 
 #else
