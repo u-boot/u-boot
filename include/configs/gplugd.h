cb71c6d85409715f541261f0bdda4a8060128689
Jagan Teki <jagan@openedev.com>
2016-11-25 00:58:58 +0530

    spi: Zap armada100_spi.c and env
    armada100_spi.c and related env is zapping becuase
    of "no DM conversion".
    
    Cc: Ajay Bhargav <ajay.bhargav@einfochips.com>
    Signed-off-by: Jagan Teki <jagan@openedev.com>

--- include/configs/gplugd.h
+++ include/configs/gplugd.h
@@ -72,11 +72,6 @@
 /* GPIO Configuration for PHY */
 #define CONFIG_SYS_GPIO_PHY_RST		104	/* GPIO104 */
 
-/* SPI Support */
-#define CONFIG_ARMADA100_SPI
-#define CONFIG_ENV_SPI_CS		110
-#define CONFIG_SYS_SSP_PORT		2
-
 /* Flash Support */
 
 /*
@@ -95,10 +90,8 @@
 /*
  * Environment variables configurations
  */
-#define CONFIG_ENV_IS_IN_SPI_FLASH
-#define CONFIG_ENV_SECT_SIZE		0x4000
+#define CONFIG_ENV_IS_NOWHERE
 #define CONFIG_ENV_SIZE			0x4000
-#define CONFIG_ENV_OFFSET		0x07C000
 
 #ifdef CONFIG_CMD_USB
 #define CONFIG_USB_EHCI
