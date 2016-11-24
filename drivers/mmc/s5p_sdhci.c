f0ecfc5e7e412c2098627cab2133fb2e284edfea
Seung-Woo Kim <sw0312.kim@samsung.com>
2016-11-24 15:05:51 +0900

    mmc: s5p_sdhci: fix to check proper pinmux id
    At sdhci_get_config(), there was wrong condition to check pimux
    id, so this patch fixes to check proper pinmux id.
    
    Signed-off-by: Seung-Woo Kim <sw0312.kim@samsung.com>

--- drivers/mmc/s5p_sdhci.c
+++ drivers/mmc/s5p_sdhci.c
@@ -152,7 +152,7 @@ static int sdhci_get_config(const void *blob, int node, struct sdhci_host *host)
 
 	/* Get device id */
 	dev_id = pinmux_decode_periph_id(blob, node);
-	if (dev_id < PERIPH_ID_SDMMC0 && dev_id > PERIPH_ID_SDMMC3) {
+	if (dev_id < PERIPH_ID_SDMMC0 || dev_id > PERIPH_ID_SDMMC3) {
 		debug("MMC: Can't get device id\n");
 		return -EINVAL;
 	}
