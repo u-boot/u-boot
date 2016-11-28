e389033f72b55f1f30952e419a8505efcdd4c91d
Peng Fan <peng.fan@nxp.com>
2016-11-28 17:49:50 +0800

    imx: mx6sxsabreauto: enable more dm drivers
    Enable MMC/I2C/GPIO/PMIC/REGULATOR/PCA953X DM drivers
    for mx6sxsabreauto board. Drop non-DM code.
    
    Note:
    The i.MX DM drivers has such dependency.
      MXC GPIO -> MXC I2C -> PFUZE/REGULATOR
      MXC GPIO -> PCA953X
      MXC GPIO -> FSL_USDHC
    
    So the drivers needs to be enabled all to avoid
    compiling error.
    
    The uboot dm tree log:
    => dm tree
     Class       Probed   Name
     ----------------------------------------
      root        [ + ]    root_driver
      thermal     [   ]    |-- imx_thermal
      simple_bus  [ + ]    |-- soc
      simple_bus  [ + ]    |   |-- aips-bus@02000000
      simple_bus  [   ]    |   |   |-- spba-bus@02000000
      gpio        [ + ]    |   |   |-- gpio@0209c000
      gpio        [ + ]    |   |   |-- gpio@020a0000
      gpio        [ + ]    |   |   |-- gpio@020a4000
      gpio        [ + ]    |   |   |-- gpio@020a8000
      gpio        [ + ]    |   |   |-- gpio@020ac000
      gpio        [ + ]    |   |   |-- gpio@020b0000
      gpio        [ + ]    |   |   |-- gpio@020b4000
      simple_bus  [   ]    |   |   |-- anatop@020c8000
      simple_bus  [   ]    |   |   |-- snvs@020cc000
      pinctrl     [ + ]    |   |   `-- iomuxc@020e0000
      pinconfig   [ + ]    |   |       `-- imx6x-sabreauto
      pinconfig   [ + ]    |   |           |-- i2c2grp-1
      pinconfig   [ + ]    |   |           |-- i2c3grp-2
      pinconfig   [   ]    |   |           |-- uart1grp
      pinconfig   [ + ]    |   |           |-- usdhc3grp
      pinconfig   [   ]    |   |           |-- usdhc3grp-100mhz
      pinconfig   [   ]    |   |           |-- usdhc3grp-200mhz
      pinconfig   [ + ]    |   |           |-- usdhc4grp
      pinconfig   [ + ]    |   |           `-- vccsd3grp
      simple_bus  [ + ]    |   |-- aips-bus@02100000
      mmc         [ + ]    |   |   |-- usdhc@02198000
      mmc         [ + ]    |   |   |-- usdhc@0219c000
      i2c         [ + ]    |   |   |-- i2c@021a4000
      i2c_generic [ + ]    |   |   |   |-- generic_8
      i2c_generic [ + ]    |   |   |   `-- generic_4e
      i2c         [ + ]    |   |   `-- i2c@021a8000
      gpio        [ + ]    |   |       |-- gpio@30
      gpio        [ + ]    |   |       `-- gpio@32
      simple_bus  [   ]    |   `-- aips-bus@02200000
      simple_bus  [   ]    |       `-- spba-bus@02200000
      simple_bus  [ + ]    `-- regulators
      regulator   [ + ]        `-- regulator@0
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>

--- include/configs/mx6sxsabreauto.h
+++ include/configs/mx6sxsabreauto.h
@@ -114,18 +114,11 @@
 #define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC3_BASE_ADDR
 
 /* I2C Configs */
-#define CONFIG_SYS_I2C
 #define CONFIG_SYS_I2C_MXC
 #define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
 #define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
 #define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
 #define CONFIG_SYS_I2C_SPEED		  100000
-
-/* PMIC */
-#define CONFIG_POWER
-#define CONFIG_POWER_I2C
-#define CONFIG_POWER_PFUZE100
-#define CONFIG_POWER_PFUZE100_I2C_ADDR	0x08
 
 /* NAND flash command */
 #define CONFIG_CMD_NAND
