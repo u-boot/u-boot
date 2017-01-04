bb5854c4f459b1180dd7c43f4745d5dbbc04d3c9
Adam Ford <aford173@gmail.com>
2017-01-04 09:26:43 -0600

    ARM: omap3_logic: Use DEFAULT_LINUX_BOOT_ENV from ti_armv7_common
    Since we're including ti_armv7_common, let's pull in DEFAULT_LINUX_BOOT_ENV
    and remove unnecessary duplicative definitions.  This patch also renames a
    few environmental variables to match what is inside ti_armv7_common. This
    should help future-proof any subsequent memory or memory location changes.
    
    Signed-off-by: Adam Ford <aford173@gmail.com>

--- include/configs/omap3_logic.h
+++ include/configs/omap3_logic.h
@@ -31,7 +31,6 @@
 #define CONFIG_SETUP_MEMORY_TAGS
 #define CONFIG_INITRD_TAG
 #define CONFIG_REVISION_TAG
-#define CONFIG_CMDLINE_EDITING		/* cmd line edit/history */
 
 /* Hardware drivers */
 
@@ -124,9 +123,7 @@
 	"saveenv;"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"loadaddr=0x81000000\0" \
-	"uimage=uImage\0" \
-	"zimage=zImage\0" \
+	DEFAULT_LINUX_BOOT_ENV \
 	"mtdids=" MTDIDS_DEFAULT "\0"	\
 	"mtdparts=" MTDPARTS_DEFAULT "\0" \
 	"mmcdev=0\0" \
@@ -156,18 +153,17 @@
 		"${optargs};" \
 		"run addmtdparts; " \
 		"run vrfb_arg\0" \
-	"loadbootscript=fatload mmc ${mmcdev} ${loadaddr} boot.scr\0" \
+	"loadbootscript=load mmc ${mmcdev} ${loadaddr} boot.scr\0" \
 	"bootscript=echo 'Running bootscript from mmc ...'; " \
 		"source ${loadaddr}\0" \
 	"loaduimage=mmc rescan; " \
-		"fatload mmc ${mmcdev} ${loadaddr} ${uimage}\0" \
+		"load mmc ${mmcdev} ${loadaddr} uImage\0" \
 	"loadzimage=mmc rescan; " \
-		"fatload mmc ${mmcdev} ${loadaddr} ${zimage}\0" \
+		"load mmc ${mmcdev} ${loadaddr} zImage\0" \
 	"ramdisksize=64000\0" \
-	"ramdiskaddr=0x82000000\0" \
 	"ramdiskimage=rootfs.ext2.gz.uboot\0" \
 	"loadramdisk=mmc rescan; " \
-		"fatload mmc ${mmcdev} ${ramdiskaddr} ${ramdiskimage}\0" \
+		"load mmc ${mmcdev} ${rdaddr} ${ramdiskimage}\0" \
 	"ramargs=run setconsole; setenv bootargs console=${console} " \
 		"root=/dev/ram rw ramdisk_size=${ramdisksize}\0" \
 	"mmcargs=run setconsole; setenv bootargs console=${console} " \
@@ -184,15 +180,14 @@
 		"ip=${ipaddr}:${tftpserver}:${gatewayip}:${netmask}::eth0:off\0" \
 	"nfsrootpath=/opt/nfs-exports/omap\0" \
 	"autoload=no\0" \
-	"fdtaddr=0x86000000\0" \
-	"loadfdtimage=mmc rescan; " \
-		"fatload mmc ${mmcdev} ${fdtaddr} ${fdtimage}\0" \
+	"loadfdt=mmc rescan; " \
+		"load mmc ${mmcdev} ${fdtaddr} ${fdtimage}\0" \
 	"mmcbootz=echo Booting with DT from mmc${mmcdev} ...; " \
 		"run mmcargs; " \
 		"run common_bootargs; " \
 		"run dump_bootargs; " \
 		"run loadzimage; " \
-		"run loadfdtimage; " \
+		"run loadfdt; " \
 		"bootz ${loadaddr} - ${fdtaddr}\0" \
 	"mmcramboot=echo 'Booting uImage kernel from mmc w/ramdisk...'; " \
 		"run ramargs; " \
@@ -200,22 +195,22 @@
 		"run dump_bootargs; " \
 		"run loaduimage; " \
 		"run loadramdisk; " \
-		"bootm ${loadaddr} ${ramdiskaddr}\0" \
+		"bootm ${loadaddr} ${rdaddr}\0" \
 	"mmcrambootz=echo 'Booting zImage kernel from mmc w/ramdisk...'; " \
 		"run ramargs; " \
 		"run common_bootargs; " \
 		"run dump_bootargs; " \
 		"run loadzimage; " \
 		"run loadramdisk; " \
-		"run loadfdtimage; " \
-		"bootz ${loadaddr} ${ramdiskaddr} ${fdtaddr};\0" \
+		"run loadfdt; " \
+		"bootz ${loadaddr} ${rdaddr} ${fdtaddr};\0" \
 	"tftpboot=echo 'Booting kernel/ramdisk rootfs from tftp...'; " \
 		"run ramargs; " \
 		"run common_bootargs; " \
 		"run dump_bootargs; " \
 		"tftpboot ${loadaddr} ${zimage}; " \
-		"tftpboot ${ramdiskaddr} ${ramdiskimage}; " \
-		"bootm ${loadaddr} ${ramdiskaddr}\0" \
+		"tftpboot ${rdaddr} ${ramdiskimage}; " \
+		"bootm ${loadaddr} ${rdaddr}\0" \
 	"tftpbootz=echo 'Booting kernel NFS rootfs...'; " \
 		"dhcp;" \
 		"run nfsargs;" \
@@ -228,7 +223,6 @@
 	"run autoboot"
 
 /* Miscellaneous configurable options */
-#define CONFIG_AUTO_COMPLETE
 
 /* memtest works on */
 #define CONFIG_SYS_MEMTEST_START	(OMAP34XX_SDRC_CS0)
