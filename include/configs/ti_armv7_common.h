2170652d9861a3ab0eac11d44a948062ee493ede
Andrew F. Davis <afd@ti.com>
2017-01-11 10:28:08 -0600

    ti_armv7_common: env: Use FIT image configs by default
    This allows us to specify a FIT configuration that will automatically
    use the correct images from the FIT blob.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>

--- include/configs/ti_armv7_common.h
+++ include/configs/ti_armv7_common.h
@@ -124,8 +124,7 @@
 	"fit_bootfile=fitImage.itb\0" \
 	"update_to_fit=setenv loadaddr ${fit_loadaddr}; setenv bootfile ${fit_bootfile}\0" \
 	"args_fit=setenv bootargs console=${console} \0" \
-	"loadfit=run args_fit; bootm ${loadaddr}:kernel@1 " \
-		"${loadaddr}:ramdisk@1 ${loadaddr}:${fdtfile};\0" \
+	"loadfit=run args_fit; bootm ${loadaddr}#${fdtfile};\0" \
 
 /*
  * DDR information.  If the CONFIG_NR_DRAM_BANKS is not defined,
