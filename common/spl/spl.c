d308c38f1427afdafe720b16d7b9ddeacb97fb94
Michal Simek <michal.simek@xilinx.com>
2017-01-16 12:57:19 +0100

    spl: Add missing line ending to SPL print
    One print requires line ending to be aligned with SW running
    after it. Prints look weird without it.
    
    For example:
    U-Boot SPL 2017.01-01801-g1fb1292c7b9d (Jan 16 2017 - 12:50:53)
    EL Level:	EL3
    Trying to boot from SPINOTICE:  ATF running on XCZU15EG/silicon
    v2/RTL5.1 at 0xfffe5000, with PMU firmware
    NOTICE:  BL31: Secure code at 0x0
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -316,7 +316,7 @@ static int boot_from_devices(struct spl_image_info *spl_image,
 		loader = spl_ll_find_loader(spl_boot_list[i]);
 #if defined(CONFIG_SPL_SERIAL_SUPPORT) && defined(CONFIG_SPL_LIBCOMMON_SUPPORT)
 		if (loader)
-			printf("Trying to boot from %s", loader->name);
+			printf("Trying to boot from %s\n", loader->name);
 		else
 			puts("SPL: Unsupported Boot Device!\n");
 #endif
