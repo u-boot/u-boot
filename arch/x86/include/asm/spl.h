4bbc02454fb84310eb755f68fa17d2eb36d976fe
Simon Glass <sjg@chromium.org>
2017-01-16 07:03:56 -0700

    x86: Add an SPL implementation
    SPL needs to set up the machine ready for loading 64-bit U-Boot and jumping
    to it. Call the existing init routines in order to accomplish this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ arch/x86/include/asm/spl.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2017 Google, Inc
+ * Written by Simon Glass <sjg@chromium.org>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ * This file is required for SPL to build, but is empty.
+ */
