f59f07ece5ec78a8fc2f0e06a80ff66029002629
Ladislav Michl <ladis@linux-mips.org>
2017-01-19 11:45:35 +0100

    cmd: ubi: allow '-' to specify maximum volume size
    Currently maximum volume size can be specified only if no other
    arguments are used. Use '-' placeholder as volume size to allow
    maximum volume size to be specified together with volume id and
    type.
    
    Signed-off-by: Ladislav Michl <ladis@linux-mips.org>

--- cmd/ubi.c
+++ cmd/ubi.c
@@ -600,7 +600,8 @@ static int do_ubi(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		}
 		/* E.g., create volume size */
 		if (argc == 4) {
-			size = simple_strtoull(argv[3], NULL, 16);
+			if (argv[3][0] != '-')
+				size = simple_strtoull(argv[3], NULL, 16);
 			argc--;
 		}
 		/* Use maximum available size */
@@ -691,8 +692,9 @@ U_BOOT_CMD(
 		" - Display volume and ubi layout information\n"
 	"ubi check volumename"
 		" - check if volumename exists\n"
-	"ubi create[vol] volume [size] [type] [id]"
-		" - create volume name with size\n"
+	"ubi create[vol] volume [size] [type] [id]\n"
+		" - create volume name with size ('-' for maximum"
+		" available size)\n"
 	"ubi write[vol] address volume size"
 		" - Write volume from address with size\n"
 	"ubi write.part address volume size [fullsize]\n"
