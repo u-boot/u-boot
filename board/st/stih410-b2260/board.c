5cc16d886ee1ec9841a9ec76365d7b841495c075
Patrice Chotard <patrice.chotard@st.com>
2017-02-21 13:37:12 +0100

    board: Add STMicroelectronics STiH410-B2260 support
    This is a 96Board compliant board based on STiH410 SoC:
      - 1GB DDR
      - On-Board USB combo WiFi/Bluetooth RTL8723BU
        with PCB soldered antenna
      - Ethernet 1000-BaseT
      - SATA
      - HDMI
      - 2 x USB2.0 type A
      - 1 x USB2.0 type micro-AB
      - SD card slot
      - High speed connector (SD/I2C/USB interfaces)
      - Low speed connector (UART/I2C/GPIO/SPI/PCM interfaces)
    
    Signed-off-by: Patrice Chotard <patrice.chotard@st.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ board/st/stih410-b2260/board.c
@@ -0,0 +1,28 @@
+/*
+ * Board init file for STiH410-B2260
+ *
+ * (C) Copyright 2017 Patrice Chotard <patrice.chotard@st.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init(void)
+{
+	gd->ram_size = PHYS_SDRAM_1_SIZE;
+	return 0;
+}
+
+void dram_init_banksize(void)
+{
+	gd->bd->bi_dram[0].start = PHYS_SDRAM_1;
+	gd->bd->bi_dram[0].size = PHYS_SDRAM_1_SIZE;
+}
+
+int board_init(void)
+{
+	return 0;
+}
---
+++ include/configs/stih410-b2260.h
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2017
+ * Patrice Chotard, <patrice.chotard@st.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <config.h>
+
+/* ram memory-related information */
+#define CONFIG_NR_DRAM_BANKS		1
+#define PHYS_SDRAM_1			0x40000000
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM_1
+#define PHYS_SDRAM_1_SIZE		0x3FE00000
+#define CONFIG_SYS_TEXT_BASE		0x7D600000
+#define CONFIG_SYS_LOAD_ADDR		PHYS_SDRAM_1	/* default load addr */
+
+#define CONFIG_BAUDRATE			115200
+
+#define CONFIG_SYS_HZ_CLOCK		1000000000	/* 1 GHz */
+
+/* Libraries */
+#define CONFIG_MD5
+
+#define CONFIG_BOOTARGS							\
+	"console=ttyS0,115200 earlyprintk consoleblank=0 ignore_loglevel"
+
+/* Environment */
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"board= B2260" \
+	"load_addr= #CONFIG_SYS_LOAD_ADDR \0"
+
+#define CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_SIZE 0x4000
+
+/* Extra Commands */
+#define CONFIG_CMD_ASKENV
+#define CONFIG_SYS_LONGHELP
+
+#define CONFIG_SETUP_MEMORY_TAGS
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		0x1800000
+#define CONFIG_SYS_GBL_DATA_SIZE	1024	/* Global data structures */
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_TEXT_BASE - \
+					 CONFIG_SYS_MALLOC_LEN - \
+					 CONFIG_SYS_GBL_DATA_SIZE)
+
+/* Monitor Command Prompt */
+#define CONFIG_SYS_CBSIZE		1024	/* Console I/O Buffer Size */
+
+#define CONFIG_SYS_MAXARGS		16	/* max number of command args */
+#define CONFIG_SYS_MAX_FLASH_BANKS	1
+
+#define CONFIG_SKIP_LOWLEVEL_INIT
+
+#endif /* __CONFIG_H */
