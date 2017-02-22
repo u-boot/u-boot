15f05610438981297ff628bb87e6d5789ee3fad0
Dirk Eibach <dirk.eibach@gdsys.cc>
2017-02-22 16:07:23 +0100

    arm: mvebu: Add gdsys ControlCenter-Compact board
    The gdsys ControlCenter Digital board is based on a Marvell Armada 38x
    SOC.
    
    It boots from SPI-Flash but can be configured to boot from SD-card for
    factory programming and testing.
    
    On board peripherals include:
    - 2 x GbE
    - Xilinx Kintex-7 FPGA connected via PCIe
    - mSATA
    - USB3 host
    - Atmel TPM
    
    Signed-off-by: Dirk Eibach <dirk.eibach@gdsys.cc>
    Signed-off-by: Mario Six <mario.six@gdsys.cc>
    Signed-off-by: Stefan Roese <sr@denx.de>

---
+++ board/gdsys/a38x/keyprogram.h
@@ -0,0 +1,14 @@
+/*
+ * (C) Copyright 2016
+ * Mario Six, Guntermann & Drunck GmbH, mario.six@gdsys.cc
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __KEYPROGRAM_H
+#define __KEYPROGRAM_H
+
+int load_and_run_keyprog(void);
+int flush_keys(void);
+
+#endif /* __KEYPROGRAM_H */
