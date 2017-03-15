483a8014e98b699c9a918137f7124702b3ec29c1
Jacob Chen <jacob2.chen@rock-chips.com>
2017-03-15 17:28:43 +0800

    rockchip: configs: correct mmc env dev for rk3288 based boards
    we are using mmc alias , so mmc index have been changed.
    now mmc dev 0 is emmc and mmc dev 1 is sdmmc.
    
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- include/configs/evb_rk3288.h
+++ include/configs/evb_rk3288.h
@@ -11,7 +11,7 @@
 #include <configs/rk3288_common.h>
 
 #define CONFIG_ENV_IS_IN_MMC
-#define CONFIG_SYS_MMC_ENV_DEV 1
+#define CONFIG_SYS_MMC_ENV_DEV 0
 
 #define CONFIG_SYS_WHITE_ON_BLACK
 
--- include/configs/tinker_rk3288.h
+++ include/configs/tinker_rk3288.h
@@ -16,7 +16,7 @@
 	func(MMC, mmc, 1)
 
 #define CONFIG_ENV_IS_IN_MMC
-#define CONFIG_SYS_MMC_ENV_DEV 0
+#define CONFIG_SYS_MMC_ENV_DEV 1
 
 #define CONFIG_SYS_WHITE_ON_BLACK
 
