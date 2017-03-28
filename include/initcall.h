2d986c0f54fba2b0e25898976d8ceb16a78ad8cb
Simon Glass <sjg@chromium.org>
2017-03-28 10:27:17 -0600

    board_f: initcall: Add a header guard
    This file is missing the usual header guard. Add it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/initcall.h
+++ include/initcall.h
@@ -4,6 +4,11 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
+#ifndef __INITCALL_H
+#define __INITCALL_H
+
 typedef int (*init_fnc_t)(void);
 
 int initcall_run_list(const init_fnc_t init_sequence[]);
+
+#endif
