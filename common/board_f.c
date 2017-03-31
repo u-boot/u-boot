abf7f4c7040b3914bea327243dcfac0ef840fbaf
Simon Glass <sjg@chromium.org>
2017-03-31 08:40:31 -0600

    board_f: Drop CONFIG_SPL_BUILD check
    This is never defined when building this file, so drop it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- common/board_f.c
+++ common/board_f.c
@@ -106,10 +106,6 @@ __weak void blue_led_off(void) {}
  * a structure...
  */
 
-/*
- * Could the CONFIG_SPL_BUILD infection become a flag in gd?
- */
-
 #if defined(CONFIG_WATCHDOG) || defined(CONFIG_HW_WATCHDOG)
 static int init_func_watchdog_init(void)
 {
@@ -464,7 +460,6 @@ static int reserve_uboot(void)
 	return 0;
 }
 
-#ifndef CONFIG_SPL_BUILD
 /* reserve memory for malloc() area */
 static int reserve_malloc(void)
 {
@@ -486,7 +481,6 @@ static int reserve_board(void)
 	}
 	return 0;
 }
-#endif
 
 static int setup_machine(void)
 {
@@ -890,10 +884,8 @@ static const init_fnc_t init_sequence_f[] = {
 	reserve_video,
 	reserve_trace,
 	reserve_uboot,
-#ifndef CONFIG_SPL_BUILD
 	reserve_malloc,
 	reserve_board,
-#endif
 	setup_machine,
 	reserve_global_data,
 	reserve_fdt,
