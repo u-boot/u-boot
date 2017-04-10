27255fe82163c2459431e1a270338df3bf76e1ec
Peng Fan <peng.fan@nxp.com>
2017-04-10 19:44:33 +0800

    net: fec: do not access reserved register for i.MX6ULL
    The MIB RAM and FIFO receive start register does not exist on
    i.MX6ULL. Accessing these register will cause enet not work well or
    cause system report fault.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Cc: Joe Hershberger <joe.hershberger@ni.com>

--- drivers/net/fec_mxc.c
+++ drivers/net/fec_mxc.c
@@ -563,7 +563,7 @@ static int fec_init(struct eth_device *dev, bd_t *bd)
 	writel(0x00000000, &fec->eth->gaddr2);
 
 	/* Do not access reserved register for i.MX6UL */
-	if (!is_mx6ul()) {
+	if (!is_mx6ul() && !is_mx6ull()) {
 		/* clear MIB RAM */
 		for (i = mib_ptr; i <= mib_ptr + 0xfc; i += 4)
 			writel(0, i);
