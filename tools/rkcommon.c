68d12600b155b67a38854c7d64ce7c8e9e82b63c
Kever Yang <kever.yang@rock-chips.com>
2017-04-14 14:55:05 +0800

    mkimage: rockchip: add support for rk3328
    Add support for rk3328 package header in mkimage tool.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- tools/rkcommon.c
+++ tools/rkcommon.c
@@ -71,6 +71,7 @@ static struct spl_info spl_infos[] = {
 	{ "rk3036", "RK30", 0x1000, false, false },
 	{ "rk3188", "RK31", 0x8000 - 0x800, true, false },
 	{ "rk3288", "RK32", 0x8000, false, false },
+	{ "rk3328", "RK32", 0x8000 - 0x1000, false, false },
 	{ "rk3399", "RK33", 0x20000, false, true },
 };
 
