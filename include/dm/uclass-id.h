0753bc2d30d7ca4a0ea4ef7f97083961c3a9d0e0
maxims@google.com <maxims@google.com>
2017-04-17 12:00:21 -0700

    dm: Simple Watchdog uclass
    This is a simple uclass for Watchdog Timers. It has four operations:
    start, restart, reset, stop. Drivers must implement start, restart and
    stop operations, while implementing reset is optional: It's default
    implementation expires watchdog timer in one clock tick.
    
    Signed-off-by: Maxim Sloyko <maxims@google.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -84,6 +84,7 @@ enum uclass_id {
 	UCLASS_VIDEO,		/* Video or LCD device */
 	UCLASS_VIDEO_BRIDGE,	/* Video bridge, e.g. DisplayPort to LVDS */
 	UCLASS_VIDEO_CONSOLE,	/* Text console driver for video device */
+	UCLASS_WDT,		/* Watchdot Timer driver */
 
 	UCLASS_COUNT,
 	UCLASS_INVALID = -1,
