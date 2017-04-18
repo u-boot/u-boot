eaa59b3404cdcfc190810d5083c164ac18eb8ab7
Wenyou Yang <wenyou.yang@atmel.com>
2017-04-18 15:31:02 +0800

    board: at91sam9263ek: Enable early debug UART
    Enable the early debug UART to debug problems when an ICE or other
    debug mechanism is not available.
    
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- board/atmel/at91sam9263ek/at91sam9263ek.c
+++ board/atmel/at91sam9263ek/at91sam9263ek.c
@@ -7,6 +7,7 @@
  */
 
 #include <common.h>
+#include <debug_uart.h>
 #include <linux/sizes.h>
 #include <asm/arch/at91sam9263.h>
 #include <asm/arch/at91sam9_smc.h>
@@ -176,10 +177,22 @@ void lcd_show_board_info(void)
 #endif /* CONFIG_LCD_INFO */
 #endif
 
+#ifdef CONFIG_DEBUG_UART_BOARD_INIT
+void board_debug_uart_init(void)
+{
+	at91_seriald_hw_init();
+}
+#endif
+
+#ifdef CONFIG_BOARD_EARLY_INIT_F
 int board_early_init_f(void)
 {
+#ifdef CONFIG_DEBUG_UART
+	debug_uart_init();
+#endif
 	return 0;
 }
+#endif
 
 int board_init(void)
 {
