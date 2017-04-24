fa9aee3aa379d15d4fbb225bc3d6b48521678182
Álvaro Fernández Rojas <noltari@gmail.com>
2017-04-24 19:03:33 +0200

    MIPS: tl-wdr4300: remove debug_uart_init call
    In order to add a generic MIPS debug_uart_init call right before the call to
    board_early_init_f, we need to remove all calls to debug_uart_init from every
    MIPS boards.
    WDR4300 doesn't provide a board_debug_uart_init and configures pinmux in
    board_early_init_f instead. Since I have no idead of what's the needed uart
    pinmux config, I copied the whole pinmux config to a new function that is
    called from board_early_init_f if CONFIG_DEBUG_UART_BOARD_INIT is not enabled.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- board/tplink/wdr4300/wdr4300.c
+++ board/tplink/wdr4300/wdr4300.c
@@ -34,8 +34,7 @@ static void wdr4300_usb_start(void)
 static inline void wdr4300_usb_start(void) {}
 #endif
 
-#ifdef CONFIG_BOARD_EARLY_INIT_F
-int board_early_init_f(void)
+void wdr4300_pinmux_config(void)
 {
 	void __iomem *regs;
 
@@ -56,9 +55,20 @@ int board_early_init_f(void)
 	writel(0x00000000, regs + AR934X_GPIO_REG_OUT_FUNC3);
 	writel(0x0000004d, regs + AR934X_GPIO_REG_OUT_FUNC4);
 	writel(0x00000000, regs + AR934X_GPIO_REG_OUT_FUNC5);
+}
 
-#ifdef CONFIG_DEBUG_UART
-	debug_uart_init();
+#ifdef CONFIG_DEBUG_UART_BOARD_INIT
+void board_debug_uart_init(void)
+{
+	wdr4300_pinmux_config();
+}
+#endif
+
+#ifdef CONFIG_BOARD_EARLY_INIT_F
+int board_early_init_f(void)
+{
+#ifndef CONFIG_DEBUG_UART_BOARD_INIT
+	wdr4300_pinmux_config();
 #endif
 
 #ifndef CONFIG_SKIP_LOWLEVEL_INIT
