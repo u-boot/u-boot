8bd06ba91af529f3e5746bac94a3308181f69d09
Jagan Teki <jagan@amarulasolutions.com>
2017-05-12 17:18:25 +0530

    icorem6: Remove unused FEC configs
    - IMX_FEC_BASE:  icorem6 using dts, no need for explicit base.
    - CONFIG_ETHPRIME: ethprime env not using anywhere in the board.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- include/configs/imx6qdl_icore.h
+++ include/configs/imx6qdl_icore.h
@@ -168,10 +168,8 @@
 
 /* Ethernet */
 #ifdef CONFIG_FEC_MXC
-# define IMX_FEC_BASE			ENET_BASE_ADDR
 # define CONFIG_FEC_MXC_PHYADDR		0
 # define CONFIG_FEC_XCV_TYPE		RMII
-# define CONFIG_ETHPRIME		"FEC"
 
 # define CONFIG_MII
 #endif
