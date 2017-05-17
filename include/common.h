3ff240c92f516841d8aff3b7d9268763711f2afd
Simon Glass <sjg@chromium.org>
2017-05-17 08:22:56 -0600

    x86: Don't include asm/u-boot.h in common
    With a small fixup to u-boot-x86.h, this is not actually needed anywhere,
    so drop it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/common.h
+++ include/common.h
@@ -430,9 +430,6 @@ static inline int setenv_addr(const char *varname, const void *addr)
 # include <asm/setup.h>
 # include <asm/u-boot.h>
 #endif /* CONFIG_ARM */
-#ifdef CONFIG_X86
-# include <asm/u-boot.h>
-#endif /* CONFIG_X86 */
 #ifdef CONFIG_NDS32
 # include <asm/mach-types.h>
 # include <asm/setup.h>
