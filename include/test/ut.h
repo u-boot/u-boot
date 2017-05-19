fe3c0b5b9306f957a6c25551a73ab8a70735699b
Simon Glass <sjg@chromium.org>
2017-05-18 20:10:00 -0600

    dm: test: Fix nit with position of backslash
    Line up this backslash with all the others.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/test/ut.h
+++ include/test/ut.h
@@ -104,7 +104,7 @@ void ut_failf(struct unit_test_state *uts, const char *fname, int line,
 }
 
 /* Assert that a pointer is not an error pointer */
-#define ut_assertok_ptr(expr) {					\
+#define ut_assertok_ptr(expr) {						\
 	const void *val = (expr);					\
 									\
 	if (IS_ERR(val)) {						\
