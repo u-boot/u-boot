020b3ce8aeec1f452c2a3a7d37c53b4d1574ec35
Alison Wang <b18965@freescale.com>
2017-06-08 16:15:14 +0800

    armv8: Remove duplicate definition for IH_ARCH_ARM and IH_ARCH_ARM64
    The duplicate definitions for IH_ARCH_ARM and IH_ARCH_ARM64 are removed.
    The definitions in <image.h> are used.
    
    According to this modification, the comparison between os arch and cpu
    arch is done in C programming instead of ASM programming.
    
    Signed-off-by: Alison Wang <alison.wang@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/mp.c
+++ arch/arm/cpu/armv8/fsl-layerscape/mp.c
@@ -29,9 +29,14 @@ void update_os_arch_secondary_cores(uint8_t os_arch)
 	u64 *table = get_spin_tbl_addr();
 	int i;
 
-	for (i = 1; i < CONFIG_MAX_CPUS; i++)
-		table[i * WORDS_PER_SPIN_TABLE_ENTRY +
-			SPIN_TABLE_ELEM_OS_ARCH_IDX] = os_arch;
+	for (i = 1; i < CONFIG_MAX_CPUS; i++) {
+		if (os_arch == IH_ARCH_DEFAULT)
+			table[i * WORDS_PER_SPIN_TABLE_ENTRY +
+				SPIN_TABLE_ELEM_ARCH_COMP_IDX] = OS_ARCH_SAME;
+		else
+			table[i * WORDS_PER_SPIN_TABLE_ENTRY +
+				SPIN_TABLE_ELEM_ARCH_COMP_IDX] = OS_ARCH_DIFF;
+	}
 }
 
 #ifdef CONFIG_FSL_LSCH3
