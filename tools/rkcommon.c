915e09814a83128fee8b87b2ee2e5f4a17e04a01
Kever Yang <kever.yang@rock-chips.com>
2017-06-14 14:54:20 +0800

    rockchip: mkimage: correct spl_size for rk3399
    The maximum spl_size for rk3399 is the internal memory size minus
    the size used in bootrom (which usually can get from SPL_TEXT_BASE).
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- tools/rkcommon.c
+++ tools/rkcommon.c
@@ -76,7 +76,7 @@ static struct spl_info spl_infos[] = {
 	{ "rk3188", "RK31", 0x8000 - 0x800, true, false },
 	{ "rk3288", "RK32", 0x8000, false, false },
 	{ "rk3328", "RK32", 0x8000 - 0x1000, false, false },
-	{ "rk3399", "RK33", 0x20000, false, true },
+	{ "rk3399", "RK33", 0x30000 - 0x2000, false, true },
 	{ "rv1108", "RK11", 0x1800, false, false},
 };
 
