70b2c6e6ae37dfc886f16bedebb36074cff9cb4a
Michal Simek <michal.simek@xilinx.com>
2017-06-20 14:26:01 +0200

    arm64: zynqmp: Add support for Xilinx zcu104 board
    Add also psu_init files build with v2017.3 daily latest tools
    for da6-c1156.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/configs/xilinx_zynqmp_zcu104.h
@@ -0,0 +1,42 @@
+/*
+ * Configuration for Xilinx ZynqMP zcu104
+ *
+ * (C) Copyright 2017 Xilinx, Inc.
+ * Michal Simek <michal.simek@xilinx.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQMP_ZCU104_H
+#define __CONFIG_ZYNQMP_ZCU104_H
+
+#define CONFIG_ZYNQ_SDHCI1
+#define CONFIG_ZYNQ_I2C1
+#define CONFIG_SYS_I2C_MAX_HOPS		1
+#define CONFIG_SYS_NUM_I2C_BUSES	9
+#define CONFIG_SYS_I2C_BUSES	{ \
+				{0, {I2C_NULL_HOP} }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 0} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 1} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 2} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 3} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 4} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 5} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 6} } }, \
+				{0, {{I2C_MUX_PCA9544, 0x74, 7} } }, \
+				}
+
+#define CONFIG_SYS_I2C_ZYNQ
+#define CONFIG_PCA953X
+#define CONFIG_CMD_PCA953X
+#define CONFIG_CMD_PCA953X_INFO
+
+#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
+#define CONFIG_CMD_EEPROM
+#define CONFIG_ZYNQ_EEPROM_BUS		1
+#define CONFIG_ZYNQ_GEM_EEPROM_ADDR	0x54
+#define CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET	0x20
+
+#include <configs/xilinx_zynqmp.h>
+
+#endif /* __CONFIG_ZYNQMP_ZCU104_H */
