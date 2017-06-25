47a52cd1e4c2b1e5f205e337bf3e1ae83993b5a4
Emmanuel Vadot <manu@bidouilliste.com>
2017-06-25 09:43:33 +0200

    dtc: mkimage: Add the possibility to specify DTC
    FreeBSD recently switch to it's BSDL dtc. While it support most of the
    features of the GPL one it still lacks the incbin directive.
    Add the possibility to specify which dtc we want to use for compiling dts
    and generating fit image.
    
    Signed-off-by: Emmanuel Vadot <manu@bidouilliste.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- Makefile
+++ Makefile
@@ -349,7 +349,7 @@ OBJDUMP		= $(CROSS_COMPILE)objdump
 AWK		= awk
 PERL		= perl
 PYTHON		?= python
-DTC		= dtc
+DTC		?= dtc
 CHECK		= sparse
 
 CHECKFLAGS     := -D__linux__ -Dlinux -D__STDC__ -Dunix -D__unix__ \
--- tools/mkimage.h
+++ tools/mkimage.h
@@ -44,6 +44,5 @@ static inline ulong map_to_sysmem(void *ptr)
 #define MKIMAGE_MAX_TMPFILE_LEN		256
 #define MKIMAGE_DEFAULT_DTC_OPTIONS	"-I dts -O dtb -p 500"
 #define MKIMAGE_MAX_DTC_CMDLINE_LEN	512
-#define MKIMAGE_DTC			"dtc"   /* assume dtc is in $PATH */
 
 #endif /* _MKIIMAGE_H_ */
