c93bef9323f5a9ede9a4d140d030afb22330b550
Meng Dongyang <daniel.meng@rock-chips.com>
2017-06-28 19:22:44 +0800

    rockchip: rk3328: board: add support of dwc2 gadget
    Probe dwc2 udc in the function of board_usb_start to enable
    usb gadget function.
    
    Signed-off-by: Meng Dongyang <daniel.meng@rock-chips.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Acked-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- board/rockchip/evb_rk3328/evb-rk3328.c
+++ board/rockchip/evb_rk3328/evb-rk3328.c
@@ -16,7 +16,49 @@ int board_init(void)
 	return 0;
 }
 
+#if defined(CONFIG_USB_GADGET) && defined(CONFIG_USB_GADGET_DWC2_OTG)
+#include <usb.h>
+#include <usb/dwc2_udc.h>
+
+static struct dwc2_plat_otg_data rk3328_otg_data = {
+	.rx_fifo_sz	= 512,
+	.np_tx_fifo_sz	= 16,
+	.tx_fifo_sz	= 128,
+};
+
 int board_usb_init(int index, enum usb_init_type init)
+{
+	int node;
+	const char *mode;
+	bool matched = false;
+	const void *blob = gd->fdt_blob;
+
+	/* find the usb_otg node */
+	node = fdt_node_offset_by_compatible(blob, -1,
+					"rockchip,rk3328-usb");
+
+	while (node > 0) {
+		mode = fdt_getprop(blob, node, "dr_mode", NULL);
+		if (mode && strcmp(mode, "otg") == 0) {
+			matched = true;
+			break;
+		}
+
+		node = fdt_node_offset_by_compatible(blob, node,
+					"rockchip,rk3328-usb");
+	}
+	if (!matched) {
+		debug("Not found usb_otg device\n");
+		return -ENODEV;
+	}
+
+	rk3328_otg_data.regs_otg = fdtdec_get_addr(blob, node, "reg");
+
+	return dwc2_udc_probe(&rk3328_otg_data);
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
 {
 	return 0;
 }
+#endif
