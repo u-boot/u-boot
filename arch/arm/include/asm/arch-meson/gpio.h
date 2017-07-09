2009a8d03fe5aa660c4da04a731a4d75785d4b6b
Beniamino Galvani <b.galvani@gmail.com>
2017-07-10 00:30:04 +0200

    pinctrl: meson: add GPIO support
    This commit adds GPIO support to the Amlogic Meson pin controller
    driver, based on code from Linux kernel.
    
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Beniamino Galvani <b.galvani@gmail.com>

---
+++ arch/arm/include/asm/arch-meson/gpio.h
@@ -0,0 +1,11 @@
+/*
+ * (C) Copyright 2017 - Beniamino Galvani <b.galvani@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_MESON_GPIO_H
+#define __ASM_ARCH_MESON_GPIO_H
+
+
+#endif	/* __ASM_ARCH_MESON_GPIO_H */
