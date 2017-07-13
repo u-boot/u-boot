a076789efe60ba9a9e22e47c278e03040812cde7
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2017-07-13 19:01:09 +0530

    arm64: zynqmp: Define a way to intialize TCM
    TCM on ZynqMP needs to be intialized in a sequence
    and this patch provides a global routine to perform
    this as per requirement.
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/include/asm/arch-zynqmp/sys_proto.h
+++ arch/arm/include/asm/arch-zynqmp/sys_proto.h
@@ -10,6 +10,11 @@
 
 #define PAYLOAD_ARG_CNT		5
 
+enum {
+	TCM_LOCK,
+	TCM_SPLIT,
+};
+
 int zynq_slcr_get_mio_pin_status(const char *periph);
 
 unsigned int zynqmp_get_silicon_version(void);
@@ -23,5 +28,7 @@ int zynqmp_mmio_write(const u32 address, const u32 mask, const u32 value);
 int zynqmp_mmio_read(const u32 address, u32 *value);
 int invoke_smc(u32 pm_api_id, u32 arg0, u32 arg1, u32 arg2, u32 arg3,
 	       u32 *ret_payload);
+
+void initialize_tcm(bool mode);
 
 #endif /* _ASM_ARCH_SYS_PROTO_H */
