77c17f43543735a6b8e74e49793320ae82b25d6d
Andy Yan <andy.yan@rock-chips.com>
2017-07-24 17:52:01 +0800

    rockchip: use puts instead of printf when back to bootrom
    printf will increase the code size more than 1kb, but platform
    like rk3036 has no enough space for it.
    
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>
    Acked-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- arch/arm/mach-rockchip/bootrom.c
+++ arch/arm/mach-rockchip/bootrom.c
@@ -9,8 +9,8 @@
 
 void back_to_bootrom(void)
 {
-#if defined(CONFIG_SPL_LIBGENERIC_SUPPORT) && !defined(CONFIG_TPL_BUILD)
-	printf("Returning to boot ROM...");
+#if defined(CONFIG_SPL_LIBCOMMON_SUPPORT) && !defined(CONFIG_TPL_BUILD)
+	puts("Returning to boot ROM...");
 #endif
 	_back_to_bootrom_s();
 }
