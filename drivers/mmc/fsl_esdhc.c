d6eb25e9878617f9a1d7f06ec21c9b981bb0a4e5
Simon Glass <sjg@chromium.org>
2017-07-29 11:35:22 -0600

    dm: mmc: fsl_esdhc: Drop mmc_init() call from fsl_esdhc_init()
    We want to use fsl_esdhc_init() with driver model. Move the mmc_init() out
    of this function so that we can use it for our common init.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -765,7 +765,6 @@ static int fsl_esdhc_init(struct fsl_esdhc_priv *priv,
 {
 	struct mmc_config *cfg;
 	struct fsl_esdhc *regs;
-	struct mmc *mmc;
 	u32 caps, voltage_caps;
 	int ret;
 
@@ -856,12 +855,6 @@ static int fsl_esdhc_init(struct fsl_esdhc_priv *priv,
 
 	cfg->b_max = CONFIG_SYS_MMC_MAX_BLK_COUNT;
 
-	mmc = mmc_create(cfg, priv);
-	if (mmc == NULL)
-		return -1;
-
-	priv->mmc = mmc;
-
 	return 0;
 }
 
@@ -885,6 +878,7 @@ int fsl_esdhc_initialize(bd_t *bis, struct fsl_esdhc_cfg *cfg)
 {
 	struct fsl_esdhc_plat *plat;
 	struct fsl_esdhc_priv *priv;
+	struct mmc *mmc;
 	int ret;
 
 	if (!cfg)
@@ -914,6 +908,12 @@ int fsl_esdhc_initialize(bd_t *bis, struct fsl_esdhc_cfg *cfg)
 		free(priv);
 		return ret;
 	}
+
+	mmc = mmc_create(&plat->cfg, priv);
+	if (!mmc)
+		return -EIO;
+
+	priv->mmc = mmc;
 
 	return 0;
 }
