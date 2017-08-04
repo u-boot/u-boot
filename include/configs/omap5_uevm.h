90d99e5936d4bcef31fd19a5339fbb1f1ca188a0
Simon Glass <sjg@chromium.org>
2017-08-04 16:34:54 -0600

    Convert CONFIG_CMD_TCA642X to Kconfig
    This converts the following to Kconfig:
       CONFIG_CMD_TCA642X
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- include/configs/omap5_uevm.h
+++ include/configs/omap5_uevm.h
@@ -46,7 +46,6 @@
 
 /* Required support for the TCA642X GPIO we have on the uEVM */
 #define CONFIG_TCA642X
-#define CONFIG_CMD_TCA642X
 #define CONFIG_SYS_I2C_TCA642X_BUS_NUM 4
 #define CONFIG_SYS_I2C_TCA642X_ADDR 0x22
 
