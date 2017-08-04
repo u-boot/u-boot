00805d7ad83b410660fc17a06374874a81e39874
Simon Glass <sjg@chromium.org>
2017-08-04 16:34:52 -0600

    Convert CONFIG_CMD_STRINGS to Kconfig
    This converts the following to Kconfig:
       CONFIG_CMD_STRINGS
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- include/configs/vct.h
+++ include/configs/vct.h
@@ -239,7 +239,6 @@ int vct_gpio_get(int pin);
  * (NOR/OneNAND) usage and Linux kernel booting.
  */
 #if defined(CONFIG_VCT_SMALL_IMAGE)
-#undef CONFIG_CMD_STRINGS
 #undef CONFIG_CMD_TERMINAL
 
 #undef CONFIG_SMC911X
