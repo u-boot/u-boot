2ddb1a177ada3b9edd327fe7e74e4891e462a6f0
Bin Meng <bmeng.cn@gmail.com>
2017-08-17 01:10:42 -0700

    x86: Convert CPU_INTEL_TURBO_NOT_PACKAGE_SCOPED to Kconfig
    This converts CPU_INTEL_TURBO_NOT_PACKAGE_SCOPED to a Kconfig option.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/x86/cpu/turbo.c
+++ arch/x86/cpu/turbo.c
@@ -14,7 +14,7 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#if CONFIG_CPU_INTEL_TURBO_NOT_PACKAGE_SCOPED
+#ifdef CONFIG_CPU_INTEL_TURBO_NOT_PACKAGE_SCOPED
 static inline int get_global_turbo_state(void)
 {
 	return TURBO_UNKNOWN;
