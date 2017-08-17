347e30e1720ea6c0231f81d278b076a39280a314
Andy Yan <andy.yan@rock-chips.com>
2017-08-17 15:55:50 +0800

    armv8: mmu: add space around operator
    Add space around operator "+", make it
    match the coding style.
    
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- arch/arm/include/asm/armv8/mmu.h
+++ arch/arm/include/asm/armv8/mmu.h
@@ -16,7 +16,7 @@
 #undef  PAGE_SIZE
 #define PAGE_SHIFT		12
 #define PAGE_SIZE		(1 << PAGE_SHIFT)
-#define PAGE_MASK		(~(PAGE_SIZE-1))
+#define PAGE_MASK		(~(PAGE_SIZE - 1))
 
 /***************************************************************/
 
