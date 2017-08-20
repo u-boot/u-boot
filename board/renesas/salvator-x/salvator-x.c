ddb39a075b9ea020e71b8fce234332f4f00df290
Marek Vasut <marek.vasut@gmail.com>
2017-08-20 17:13:47 +0200

    ARM: rmobile: Remove SH SDHI probing from Salvator-X board
    Drop the SH SDHI bit from the board since SD now probes from DT instead.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- board/renesas/salvator-x/salvator-x.c
+++ board/renesas/salvator-x/salvator-x.c
@@ -164,8 +164,6 @@ int board_init(void)
 #ifdef CONFIG_SH_SDHI
 int board_mmc_init(bd_t *bis)
 {
-	int ret = -ENODEV;
-
 	/* SDHI0 */
 	gpio_request(GPIO_GFN_SD0_DAT0, NULL);
 	gpio_request(GPIO_GFN_SD0_DAT1, NULL);
@@ -180,11 +178,6 @@ int board_mmc_init(bd_t *bis)
 	gpio_request(GPIO_GP_5_1, NULL);
 	gpio_direction_output(GPIO_GP_5_2, 1);	/* power on */
 	gpio_direction_output(GPIO_GP_5_1, 1);	/* 1: 3.3V, 0: 1.8V */
-
-	ret = sh_sdhi_init(CONFIG_SYS_SH_SDHI0_BASE, 0,
-			   SH_SDHI_QUIRK_64BIT_BUF);
-	if (ret)
-		return ret;
 
 	/* SDHI1/SDHI2 eMMC */
 	gpio_request(GPIO_GFN_SD1_DAT0, NULL);
@@ -207,11 +200,6 @@ int board_mmc_init(bd_t *bis)
 	gpio_request(GPIO_GP_5_9, NULL);
 	gpio_direction_output(GPIO_GP_5_3, 0);	/* 1: 3.3V, 0: 1.8V */
 	gpio_direction_output(GPIO_GP_5_9, 0);	/* 1: 3.3V, 0: 1.8V */
-
-	ret = sh_sdhi_init(CONFIG_SYS_SH_SDHI2_BASE, 1,
-			   SH_SDHI_QUIRK_64BIT_BUF);
-	if (ret)
-		return ret;
 
 #if defined(CONFIG_R8A7795)
 	/* SDHI3 */
@@ -240,9 +228,7 @@ int board_mmc_init(bd_t *bis)
 	gpio_direction_output(GPIO_GP_3_15, 1);	/* power on */
 	gpio_direction_output(GPIO_GP_3_14, 1);	/* 1: 3.3V, 0: 1.8V */
 
-	ret = sh_sdhi_init(CONFIG_SYS_SH_SDHI3_BASE, 2,
-			   SH_SDHI_QUIRK_64BIT_BUF);
-	return ret;
+	return 0;
 }
 #endif
 
