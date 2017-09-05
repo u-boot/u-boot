0a91e1cce4497a50af00dc134af6bc19ec87fe34
Chris Packham <judge.packham@gmail.com>
2017-09-05 17:03:26 +1200

    ARM: mvebu: add SAR frequency values for 1.8/2.0GHz
    The Armada-38x has 1.8GHz and 2.0GHz variants. Add entries for these
    variants to the sar_freq_tab.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- arch/arm/mach-mvebu/cpu.c
+++ arch/arm/mach-mvebu/cpu.c
@@ -112,13 +112,15 @@ static const struct sar_freq_modes sar_freq_tab[] = {
 #elif defined(CONFIG_ARMADA_38X)
 /* SAR frequency values for Armada 38x */
 static const struct sar_freq_modes sar_freq_tab[] = {
-	{  0x0,  0x0,  666, 333, 333 },
-	{  0x2,  0x0,  800, 400, 400 },
-	{  0x4,  0x0, 1066, 533, 533 },
-	{  0x6,  0x0, 1200, 600, 600 },
-	{  0x8,  0x0, 1332, 666, 666 },
-	{  0xc,  0x0, 1600, 800, 800 },
-	{ 0xff, 0xff,    0,   0,   0 }	/* 0xff marks end of array */
+	{  0x0,  0x0,  666,  333, 333 },
+	{  0x2,  0x0,  800,  400, 400 },
+	{  0x4,  0x0, 1066,  533, 533 },
+	{  0x6,  0x0, 1200,  600, 600 },
+	{  0x8,  0x0, 1332,  666, 666 },
+	{  0xc,  0x0, 1600,  800, 800 },
+	{ 0x10,  0x0, 1866,  933, 933 },
+	{ 0x13,  0x0, 2000, 1000, 933 },
+	{ 0xff, 0xff,    0,    0,   0 }	/* 0xff marks end of array */
 };
 #else
 /* SAR frequency values for Armada XP */
