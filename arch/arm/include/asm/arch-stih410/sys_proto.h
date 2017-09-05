40d1a31e6329da8feecc555f2bdfb8da463bdd40
Patrice Chotard <patrice.chotard@st.com>
2017-09-05 11:04:24 +0200

    usb: dwc3: Add dwc3 glue driver support for STi
    This patch adds the ST glue logic to manage the DWC3 HC
    on STiH407 SoC family. It configures the internal glue
    logic and syscfg registers.
    
    Part of this code been extracted from kernel.org driver
    (drivers/usb/dwc3/dwc3-st.c)
    
    Signed-off-by: Patrice Chotard <patrice.chotard@st.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ arch/arm/include/asm/arch-stih410/sys_proto.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017
+ * Patrice Chotard <patrice.chotard@st.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _ASM_ARCH_SYS_PROTO_H
+#define _ASM_ARCH_SYS_PROTO_H
+
+#endif /* _ASM_ARCH_SYS_PROTO_H */
