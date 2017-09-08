5a86ec13b57c527c7ec71f39bb63a4e3d9b7e523
Michal Simek <michal.simek@xilinx.com>
2017-09-08 14:10:33 +0200

    spl: Call spl_start_uboot also in case of RAM based boot
    For Xilinx ZynqMP this is filling handoff structure for ATF.
    Without this patch handoff structure is not filled and ATF doesn't know
    what to do.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -204,6 +204,7 @@ static int spl_ram_load_image(struct spl_image_info *spl_image,
 		load.bl_len = 1;
 		load.read = spl_ram_load_read;
 		spl_load_simple_fit(spl_image, &load, 0, header);
+		spl_start_uboot();
 	} else {
 		debug("Legacy image\n");
 		/*
