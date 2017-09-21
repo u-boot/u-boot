9815e3ba807ddf395631be35629498e55af02fa0
Jean-Jacques Hiblot <jjhiblot@ti.com>
2017-09-21 16:30:12 +0200

    mmc: add a library function to send tuning command
    HS200/SDR104 requires tuning command to be sent to the card.
    Add a simple function to send tuning command and to read and
    compare the received data with the tuning block pattern.
    This function can be used by platform driver to perform DLL
    tuning.
    This patch is similar to
    commit 996903de92f0 ("mmc: core: add core-level function for
    sending tuning commands") added in linux kernel.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- include/mmc.h
+++ include/mmc.h
@@ -650,6 +650,7 @@ void mmc_destroy(struct mmc *mmc);
 int mmc_unbind(struct udevice *dev);
 int mmc_initialize(bd_t *bis);
 int mmc_init(struct mmc *mmc);
+int mmc_send_tuning(struct mmc *mmc, u32 opcode, int *cmd_error);
 int mmc_read(struct mmc *mmc, u64 src, uchar *dst, int size);
 
 /**
