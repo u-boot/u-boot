0c19efa7e13c87faa967d905c0aff318ebe411d4
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2017-10-08 22:44:05 +0300

    ARM: tegra: Remove unused define CONFIG_ARMCORTEXA9
    This macro isn't used by anything, and in fact hasn't ever been used
    according to the Git logs.
    
    Signed-off-by: Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>

--- include/configs/tegra-common.h
+++ include/configs/tegra-common.h
@@ -13,7 +13,6 @@
 /*
  * High Level Configuration Options
  */
-#define CONFIG_ARMCORTEXA9		/* This is an ARM V7 CPU core */
 #define CONFIG_SYS_L2CACHE_OFF		/* No L2 cache */
 
 #include <asm/arch/tegra.h>		/* get chip and board defs */
