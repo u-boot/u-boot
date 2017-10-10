d962e5dadc2cbc21bffd375f29665e5042879e66
Kever Yang <kever.yang@rock-chips.com>
2017-10-10 16:21:04 +0200

    rockchip: mkimage: use spl_boot0 for all Rockchip SoCs
    Enable the spl_boot0 in SPL and use the pre-padding TAG memory,
    the mkimage do not need to pad it but only need to replace the value
    with correct TAG value.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    [Updated:]
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- tools/rkcommon.c
+++ tools/rkcommon.c
@@ -72,15 +72,15 @@ struct spl_info {
 };
 
 static struct spl_info spl_infos[] = {
-	{ "rk3036", "RK30", 0x1000, false, false },
-	{ "rk3128", "RK31", 0x1800, false, false },
-	{ "rk3188", "RK31", 0x8000 - 0x800, true, false },
-	{ "rk322x", "RK32", 0x8000 - 0x1000, false, false },
-	{ "rk3288", "RK32", 0x8000, false, false },
-	{ "rk3328", "RK32", 0x8000 - 0x1000, false, false },
+	{ "rk3036", "RK30", 0x1000, false, true },
+	{ "rk3128", "RK31", 0x1800, false, true },
+	{ "rk3188", "RK31", 0x8000 - 0x800, true, true },
+	{ "rk322x", "RK32", 0x8000 - 0x1000, false, true },
+	{ "rk3288", "RK32", 0x8000, false, true },
+	{ "rk3328", "RK32", 0x8000 - 0x1000, false, true },
 	{ "rk3368", "RK33", 0x8000 - 0x1000, false, true },
 	{ "rk3399", "RK33", 0x30000 - 0x2000, false, true },
-	{ "rv1108", "RK11", 0x1800, false, false},
+	{ "rv1108", "RK11", 0x1800, false, true },
 };
 
 static unsigned char rc4_key[16] = {
