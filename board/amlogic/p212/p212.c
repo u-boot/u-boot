cade865a43daf19df8ac972f44d6bc40639e81c0
Neil Armstrong <narmstrong@baylibre.com>
2017-10-12 15:50:32 +0200

    arm: add initial support for Amlogic P212 based on Meson GXL family
    This adds platform code for the Amlogic P212 reference board based on a
    Meson GXL (S905X) SoC with the Meson GXL configuration.
    
    This initial submission only supports UART and MMC/SDCard, support for the
    internal Ethernet PHY in Work In Progress.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Reviewed-by: Beniamino Galvani <b.galvani@gmail.com>

---
+++ board/amlogic/p212/p212.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2016 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <asm/io.h>
+#include <asm/arch/gxbb.h>
+
+int board_init(void)
+{
+	return 0;
+}
+
+int misc_init_r(void)
+{
+       return 0;
+}
