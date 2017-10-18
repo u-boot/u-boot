df172e117d1d9a24a116cbd84bbb6034f260c889
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-10-18 18:13:17 +0200

    test/py: test reboot by EFI watchdog
    Clear environment variable efi_selftest before executing the
    default tests.
    
    Provide a test verifying that the EFI watchdog
    reboots the system upon timeout.
    
    The test depends on CONFIG_CMD_EFI_SELFTEST=y.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- test/py/tests/test_efi_selftest.py
+++ test/py/tests/test_efi_selftest.py
@@ -1,4 +1,3 @@
-# Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
 # Copyright (c) 2017, Heinrich Schuchardt <xypron.glpk@gmx.de>
 #
 # SPDX-License-Identifier: GPL-2.0
@@ -14,6 +13,7 @@ def test_efi_selftest(u_boot_console):
 	Run bootefi selftest
 	"""
 
+	u_boot_console.run_command(cmd='setenv efi_selftest')
 	u_boot_console.run_command(cmd='bootefi selftest', wait_for_prompt=False)
 	m = u_boot_console.p.expect(['Summary: 0 failures', 'Press any key'])
 	if m != 0:
@@ -22,4 +22,16 @@ def test_efi_selftest(u_boot_console):
 	m = u_boot_console.p.expect(['resetting', 'U-Boot'])
 	if m != 0:
 		raise Exception('Reset failed during the EFI selftest')
+	u_boot_console.restart_uboot();
+
+@pytest.mark.buildconfigspec('cmd_bootefi_selftest')
+def test_efi_selftest_watchdog_reboot(u_boot_console):
+	u_boot_console.run_command(cmd='setenv efi_selftest list')
+	output = u_boot_console.run_command('bootefi selftest')
+	assert '\'watchdog reboot\'' in output
+	u_boot_console.run_command(cmd='setenv efi_selftest watchdog reboot')
+	u_boot_console.run_command(cmd='bootefi selftest', wait_for_prompt=False)
+	m = u_boot_console.p.expect(['resetting', 'U-Boot'])
+	if m != 0:
+		raise Exception('Reset failed in \'watchdog reboot\' test')
 	u_boot_console.restart_uboot();
