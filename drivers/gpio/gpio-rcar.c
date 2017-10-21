f122c13bf55d1162e9a69933489fb01a24b5b959
Marek Vasut <marek.vasut+renesas@gmail.com>
2017-10-21 11:28:06 +0200

    gpio: rmobile: Add R8A77995 D3 compatible
    Add new compatible to the GPIO driver for R8A77995 D3 SoC.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- drivers/gpio/gpio-rcar.c
+++ drivers/gpio/gpio-rcar.c
@@ -175,6 +175,7 @@ static const struct udevice_id rcar_gpio_ids[] = {
 	{ .compatible = "renesas,gpio-r8a7795" },
 	{ .compatible = "renesas,gpio-r8a7796" },
 	{ .compatible = "renesas,gpio-r8a77970" },
+	{ .compatible = "renesas,gpio-r8a77995" },
 	{ /* sentinel */ }
 };
 
