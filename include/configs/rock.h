5d2f1d8271e52e24e63e52ac7011466d2b3cdd02
Kever Yang <kever.yang@rock-chips.com>
2017-11-03 16:11:04 +0800

    rockchip: rock: remove CONFIG_ENV_OFFSET
    We use the same default ENV setting in rockchip_common.h for all SoC.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Acked-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- include/configs/rock.h
+++ include/configs/rock.h
@@ -12,18 +12,4 @@
 
 #define CONFIG_SYS_MMC_ENV_DEV 0
 
-#if CONFIG_IS_ENABLED(ROCKCHIP_BACK_TO_BROM)
-/* SPL @ 32k for 34k
- * u-boot directly after @ 68k for 400k or so
- * ENV @ 992k
- */
-#define CONFIG_ENV_OFFSET ((1024-32) * 1024)
-#else
-/* SPL @ 32k for ~36k
- * ENV @ 96k
- * u-boot @ 128K
- */
-#define CONFIG_ENV_OFFSET (96 * 1024)
-#endif
-
 #endif
