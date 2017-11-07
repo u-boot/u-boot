8ea754da602b49a5200e6c3e874b1f7166516df6
Lukasz Majewski <lukma@denx.de>
2017-11-07 11:10:29 +0100

    board: imx6: marsboard: Remove doubled #include <asm/arch/sys_proto.h>
    The sys_proto.h file has been included earlier in this file.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- board/embest/mx6boards/mx6boards.c
+++ board/embest/mx6boards/mx6boards.c
@@ -36,7 +36,7 @@
 #include <linux/fb.h>
 #include <ipu_pixfmt.h>
 #include <asm/io.h>
-#include <asm/arch/sys_proto.h>
+
 DECLARE_GLOBAL_DATA_PTR;
 
 #define UART_PAD_CTRL  (PAD_CTL_PUS_100K_UP |			\
