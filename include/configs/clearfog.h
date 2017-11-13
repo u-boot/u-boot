962b8fef91fbfa58c1045bee4d9a73c72b46c829
Jon Nettleton <jon@solid-run.com>
2017-11-13 07:04:30 +0200

    arm: mvebu: clearfog: Fix SPI-NOR flash access
    The production variant of the SPI flash used by the clearfog
    devices are based on winbond chips.  Additionally enable
    SPI_FLASH_BAR since some variants will have 16MB of flash
    that requires this to be enabled.
    
    Remove the default speed and mode; these values are taken from the
    device tree when CONFIG_DM_SPI_FLASH is enabled.
    
    Add default bus, so that 'sf' detects the SPI flash by default.
    
    Signed-off-by: Jon Nettleton <jon@solid-run.com>
    [baruch: remove speed/mode; add bus; move winbond to defconfig]
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/clearfog.h
+++ include/configs/clearfog.h
@@ -33,9 +33,7 @@
 #define CONFIG_SYS_I2C_SPEED		100000
 
 /* SPI NOR flash default params, used by sf commands */
-#define CONFIG_SF_DEFAULT_SPEED		1000000
-#define CONFIG_SF_DEFAULT_MODE		SPI_MODE_3
-#define CONFIG_SPI_FLASH_STMICRO
+#define CONFIG_SF_DEFAULT_BUS		1
 
 /*
  * SDIO/MMC Card Configuration
