00ebd1f74d94a0fa50545d59a4e61b3a28c82b07
Simon Glass <sjg@chromium.org>
2017-11-12 21:52:07 -0700

    binman: Add a Makefile for test-program compilation
    These test programs are includedd as binary files in U-Boot to avoid
    having to build them (and associated toolchain differences). Instructions
    on building are in the files themselves, but it seems better to provide
    a Makefile which can be manually run when desired.
    
    Add a Makefile, separate from the normal build system, to handle this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/test/u_boot_no_ucode_ptr.c
+++ tools/binman/test/u_boot_no_ucode_ptr.c
@@ -5,10 +5,6 @@
  *
  * Simple program to create a bad _dt_ucode_base_size symbol to create an
  * error when it is used. This is used by binman tests.
- *
- * Build with:
- * cc -march=i386 -m32 -o u_boot_no_ucode_ptr -T u_boot_ucode_ptr.lds \
-	-nostdlib u_boot_no_ucode_ptr.c
  */
 
 static unsigned long not__dt_ucode_base_size[2]
--- tools/binman/test/u_boot_ucode_ptr.c
+++ tools/binman/test/u_boot_ucode_ptr.c
@@ -5,10 +5,6 @@
  *
  * Simple program to create a _dt_ucode_base_size symbol which can be read
  * by 'nm'. This is used by binman tests.
- *
- * Build with:
- * cc -march=i386 -m32 -o u_boot_ucode_ptr -T u_boot_ucode_ptr.lds -nostdlib \
-	u_boot_ucode_ptr.c
  */
 
 static unsigned long _dt_ucode_base_size[2]
