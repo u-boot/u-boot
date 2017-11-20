a1797beed262b171645af867303ecf76aad66178
Jagan Teki <jagannadh.teki@gmail.com>
2017-11-21 00:02:11 +0530

    i.MX6Q: icorem6: Move spl load fit to common/spl
    Move spl load fit code into common/spl
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- board/engicam/common/spl.c
+++ board/engicam/common/spl.c
@@ -39,6 +39,22 @@ static iomux_v3_cfg_t const uart_pads[] = {
 #endif
 };
 
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+        if (is_mx6dq() && !strcmp(name, "imx6q-icore"))
+                return 0;
+        else if (is_mx6dq() && !strcmp(name, "imx6q-icore-rqs"))
+                return 0;
+        else if ((is_mx6dl() || is_mx6solo()) && !strcmp(name, "imx6dl-icore"))
+                return 0;
+        else if ((is_mx6dl() || is_mx6solo()) && !strcmp(name, "imx6dl-icore-rqs"))
+                return 0;
+        else
+                return -1;
+}
+#endif
+
 #ifdef CONFIG_SPL_OS_BOOT
 int spl_start_uboot(void)
 {
