bb0297ccbd47de6bafc7bf54b31f7181bb346f69
Jagan Teki <jagannadh.teki@gmail.com>
2017-11-21 00:02:13 +0530

    pinctrl: imx6ul: Fix pinctrl data overlapped with DT area
    before relocation pinctrl data BSS is overlapping DT area,
    when .data is using uninitialized global variable,
    imx6_pinctrl_soc_info. So assign them flags ZERO_OFFSET_VALID
    to prevent BSS overlap
    
    Suggested-by: Lokesh Vutla <lokeshvutla@ti.com>
    Reported-by: Jagan Teki <jagan@amarulasolutions.com>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/pinctrl/nxp/pinctrl-imx6.c
+++ drivers/pinctrl/nxp/pinctrl-imx6.c
@@ -13,6 +13,11 @@
 
 static struct imx_pinctrl_soc_info imx6_pinctrl_soc_info;
 
+/* FIXME Before reloaction, BSS is overlapped with DT area */
+static struct imx_pinctrl_soc_info imx6ul_pinctrl_soc_info = {
+	.flags = ZERO_OFFSET_VALID,
+};
+
 static struct imx_pinctrl_soc_info imx6_snvs_pinctrl_soc_info = {
 	.flags = ZERO_OFFSET_VALID,
 };
@@ -32,7 +37,7 @@ static const struct udevice_id imx6_pinctrl_match[] = {
 	{ .compatible = "fsl,imx6sll-iomuxc-snvs", .data = (ulong)&imx6_snvs_pinctrl_soc_info },
 	{ .compatible = "fsl,imx6sll-iomuxc", .data = (ulong)&imx6_pinctrl_soc_info },
 	{ .compatible = "fsl,imx6sx-iomuxc", .data = (ulong)&imx6_pinctrl_soc_info },
-	{ .compatible = "fsl,imx6ul-iomuxc", .data = (ulong)&imx6_pinctrl_soc_info },
+	{ .compatible = "fsl,imx6ul-iomuxc", .data = (ulong)&imx6ul_pinctrl_soc_info },
 	{ .compatible = "fsl,imx6ull-iomuxc-snvs", .data = (ulong)&imx6_snvs_pinctrl_soc_info },
 	{ /* sentinel */ }
 };
