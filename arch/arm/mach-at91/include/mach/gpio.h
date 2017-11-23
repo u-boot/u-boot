5aaef600776fb2d9f186c12e30975e6afaa276a6
Ben Whitten <ben.whitten@lairdtech.com>
2017-11-23 13:47:47 +0000

    board: laird: add WB45N CPU module
    This board is based on the Atmel 9x5 eval board.
    Supporting the following features:
     - Boot from NAND Flash
     - Ethernet
     - FIT
     - SPL
    
    Signed-off-by: Ben Whitten <ben.whitten@lairdtech.com>
    Signed-off-by: Dan Kephart <dan.kephart@lairdtech.com>

--- arch/arm/mach-at91/include/mach/gpio.h
+++ arch/arm/mach-at91/include/mach/gpio.h
@@ -219,6 +219,8 @@ static inline unsigned pin_to_mask(unsigned pin)
 	at91_set_a_periph((x - PIN_BASE) / 32,(x % 32), y)
 #define at91_set_B_periph(x, y) \
 	at91_set_b_periph((x - PIN_BASE) / 32,(x % 32), y)
+#define at91_set_gpio_deglitch(x, y) \
+	at91_set_pio_deglitch((x - PIN_BASE) / 32,(x % 32), y)
 #define at91_set_gpio_output(x, y) \
 	at91_set_pio_output((x - PIN_BASE) / 32,(x % 32), y)
 #define at91_set_gpio_input(x, y) \
