23ecca2cd829485c305d9a49c25508d74e041216
Fabio Estevam <fabio.estevam@nxp.com>
2017-11-23 09:18:53 -0200

    mx6ull: Fix WDOG3 base address
    i.MX6ULL has the same WDOG3 base address as i.MX6UL, so take this
    into account in the base address definition.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Stefano Babic <sbabic@denx.de>

--- arch/arm/include/asm/arch-mx6/imx-regs.h
+++ arch/arm/include/asm/arch-mx6/imx-regs.h
@@ -367,7 +367,7 @@
 #include <asm/types.h>
 
 /* only for i.MX6SX/UL */
-#define WDOG3_BASE_ADDR ((is_mx6ul() ?	\
+#define WDOG3_BASE_ADDR (((is_mx6ul() || is_mx6ull()) ?	\
 			 MX6UL_WDOG3_BASE_ADDR :  MX6SX_WDOG3_BASE_ADDR))
 #define LCDIF1_BASE_ADDR ((is_cpu_type(MXC_CPU_MX6SLL)) ?	\
 			  MX6SLL_LCDIF_BASE_ADDR :		\
