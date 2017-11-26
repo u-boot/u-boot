ab61e175713a0400c6ece6348e8f655998cf574d
Marek Vasut <marek.vasut+renesas@gmail.com>
2017-11-26 20:32:44 +0100

    ARM: rmobile: Rework the ULCB CPLD driver
    Rework the ULCB CPLD driver and make it into a sysreset driver,
    since that is what the ULCB CPLD driver is mostly for.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- include/configs/ulcb.h
+++ include/configs/ulcb.h
@@ -25,19 +25,6 @@
 /* Generic Timer Definitions (use in assembler source) */
 #define COUNTER_FREQUENCY	0xFE502A	/* 16.66MHz from CPclk */
 
-/* CPLD SPI */
-#define CONFIG_CMD_SPI
-#define CONFIG_SOFT_SPI
-#define SPI_DELAY	udelay(0)
-#define SPI_SDA(val)	ulcb_softspi_sda(val)
-#define SPI_SCL(val)	ulcb_softspi_scl(val)
-#define SPI_READ	ulcb_softspi_read()
-#ifndef	__ASSEMBLY__
-void ulcb_softspi_sda(int);
-void ulcb_softspi_scl(int);
-unsigned char ulcb_softspi_read(void);
-#endif
-
 /* Environment in eMMC, at the end of 2nd "boot sector" */
 #define CONFIG_ENV_OFFSET		(-CONFIG_ENV_SIZE)
 #define CONFIG_SYS_MMC_ENV_DEV		1
