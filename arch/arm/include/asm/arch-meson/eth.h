6915b1033105b23ebc725be772bec00838830667
Neil Armstrong <narmstrong@baylibre.com>
2017-11-27 10:16:16 +0100

    ARM: arch-meson: add ethernet common init function
    Introduce a generic common Ethernet Hardware init function
    common to all Amlogic GX SoCs with support for the
    Internal PHY enable for GXL SoCs.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

---
+++ arch/arm/include/asm/arch-meson/eth.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2016 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __MESON_ETH_H__
+#define __MESON_ETH_H__
+
+#include <phy.h>
+
+enum {
+	/* Use GXL Internal RMII PHY */
+	MESON_GXL_USE_INTERNAL_RMII_PHY = 1,
+};
+
+/* Configure the Ethernet MAC with the requested interface mode
+ * with some optional flags.
+ */
+void meson_gx_eth_init(phy_interface_t mode, unsigned int flags);
+
+#endif /* __MESON_ETH_H__ */
