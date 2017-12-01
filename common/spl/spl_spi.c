33640697882b094775535fabcbc309c6fc2b09f6
Michal Simek <michal.simek@xilinx.com>
2017-12-01 15:26:23 +0100

    Revert "spl: spl_spi: Fix warnings"
    This reverts commit 76e59ba9fcc171c4f4d300b199c60457cb9bbff9.
    This issue is not there anymore.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- common/spl/spl_spi.c
+++ common/spl/spl_spi.c
@@ -40,8 +40,7 @@ static int spi_load_image_os(struct spl_image_info *spl_image,
 		return err;
 
 	spi_flash_read(flash, CONFIG_SYS_SPI_KERNEL_OFFS,
-		       spl_image->size,
-		       (void *)(uintptr_t)spl_image->load_addr);
+		       spl_image->size, (void *)spl_image->load_addr);
 
 	/* Read device tree. */
 	spi_flash_read(flash, CONFIG_SYS_SPI_ARGS_OFFS,
@@ -131,8 +130,7 @@ static int spl_spi_load_image(struct spl_image_info *spl_image,
 				return err;
 			err = spi_flash_read(flash, payload_offs,
 					     spl_image->size,
-					     (void *)
-					     (uintptr_t)spl_image->load_addr);
+					     (void *)spl_image->load_addr);
 		}
 	}
 
