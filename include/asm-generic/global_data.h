e9c8d49d54cbbc7b219a1637d2994de7448b767d
Simon Glass <sjg@chromium.org>
2017-12-04 13:48:24 -0700

    log: Add an implementation of logging
    Add the logging header file and implementation with some configuration
    options to control it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/asm-generic/global_data.h
+++ include/asm-generic/global_data.h
@@ -114,6 +114,11 @@ typedef struct global_data {
 	struct bootstage_data *bootstage;	/* Bootstage information */
 	struct bootstage_data *new_bootstage;	/* Relocated bootstage info */
 #endif
+#ifdef CONFIG_LOG
+	int log_drop_count;		/* Number of dropped log messages */
+	int default_log_level;		/* For devices with no filters */
+	struct list_head log_head;	/* List of struct log_device */
+#endif
 } gd_t;
 #endif
 
