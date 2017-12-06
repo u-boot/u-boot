146be1095036f0eeef8fc33ec8d25d8a44897a63
Michal Simek <michal.simek@xilinx.com>
2017-12-06 16:28:23 +0100

    arm64: zynqmp: Remove zynq_slcr_gem_clk_setup()
    This function is not used anywhere.
    Reference was removed by:
    "zynq: Move zynq to clock framework"
    (sha1: 781745bd87710c5604a0ae005153d0c9578d616a)
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/include/asm/arch-zynqmp/sys_proto.h
+++ arch/arm/include/asm/arch-zynqmp/sys_proto.h
@@ -30,13 +30,6 @@ enum {
 	TCM_SPLIT,
 };
 
-#ifndef CONFIG_CLK_ZYNQMP
-/* Setup clk for network */
-static inline void zynq_slcr_gem_clk_setup(u32 gem_id, unsigned long clk_rate)
-{
-}
-#endif
-
 int zynq_board_read_rom_ethaddr(unsigned char *ethaddr);
 unsigned int zynqmp_get_silicon_version(void);
 
