1e2d2597a667e16adfe0a8a9be22e904cee84727
Zhao Qiang <qiang.zhao@nxp.com>
2017-12-14 09:50:46 +0800

    phy: atheros: set auto-negotiation for AR8021
    Signed-off-by: Zhao Qiang <qiang.zhao@nxp.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- drivers/net/phy/atheros.c
+++ drivers/net/phy/atheros.c
@@ -19,6 +19,7 @@
 
 static int ar8021_config(struct phy_device *phydev)
 {
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x00, 0x1200);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x05);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x3D47);
 
