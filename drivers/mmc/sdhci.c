be165fbbf18ad3926337fe51a337dca730fa7278
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-12-30 02:00:08 +0900

    mmc: sdhci: do not overwrite host_caps in sdhci_setup_cfg()
    This line overwrites host_cap that has been set by drivers and/or
    helpers like mmc_of_parse().  Accumulate capabilities flags.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- drivers/mmc/sdhci.c
+++ drivers/mmc/sdhci.c
@@ -594,7 +594,7 @@ int sdhci_setup_cfg(struct mmc_config *cfg, struct sdhci_host *host,
 	if (host->quirks & SDHCI_QUIRK_BROKEN_VOLTAGE)
 		cfg->voltages |= host->voltages;
 
-	cfg->host_caps = MMC_MODE_HS | MMC_MODE_HS_52MHz | MMC_MODE_4BIT;
+	cfg->host_caps |= MMC_MODE_HS | MMC_MODE_HS_52MHz | MMC_MODE_4BIT;
 
 	/* Since Host Controller Version3.0 */
 	if (SDHCI_GET_VERSION(host) >= SDHCI_SPEC_300) {
