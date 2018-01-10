2ad341ed7d90e007af8e878424f7b6e4f24d90ac
Michal Simek <michal.simek@xilinx.com>
2018-01-10 09:36:09 +0100

    arm64: zynqmp: Prepare psu_init rework
    Move generic functions to common location psu_spl_init.c. Function
    declarations are added to private header.
    These changes are done in connection to the fact that still files from
    HDF can be copied over and compilation should pass.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/cpu/armv8/zynqmp/spl.c
+++ arch/arm/cpu/armv8/zynqmp/spl.c
@@ -129,15 +129,6 @@ u32 spl_boot_mode(const u32 boot_device)
 	}
 }
 
-__weak int psu_init(void)
-{
-	/*
-	 * This function is overridden by the one in
-	 * board/xilinx/zynqmp/(platform)/psu_init_gpl.c, if it exists.
-	 */
-	return -1;
-}
-
 #ifdef CONFIG_SPL_OS_BOOT
 int spl_start_uboot(void)
 {
