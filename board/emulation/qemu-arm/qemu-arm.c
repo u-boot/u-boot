ddcca73051060f11802da7ddaa5e1d66b12025c4
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2018-01-11 16:11:23 +0200

    ARM: qemu-arm: Add support for AArch64
    This adds support for '-machine virt' on AArch64. This is rather simple:
    we just add TARGET_QEMU_ARM_xxBIT to select a few different Kconfig
    symbols, provide the ARMv8 memory map from the board file and add a new
    defconfig based on the 32-bit defconfig.
    
    Signed-off-by: Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- board/emulation/qemu-arm/qemu-arm.c
+++ board/emulation/qemu-arm/qemu-arm.c
@@ -6,6 +6,41 @@
 #include <common.h>
 #include <fdtdec.h>
 
+#ifdef CONFIG_ARM64
+#include <asm/armv8/mmu.h>
+
+static struct mm_region qemu_arm64_mem_map[] = {
+	{
+		/* Flash */
+		.virt = 0x00000000UL,
+		.phys = 0x00000000UL,
+		.size = 0x08000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		/* Peripherals */
+		.virt = 0x08000000UL,
+		.phys = 0x08000000UL,
+		.size = 0x38000000,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		/* RAM */
+		.virt = 0x40000000UL,
+		.phys = 0x40000000UL,
+		.size = 0xc0000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		/* List terminator */
+		0,
+	}
+};
+
+struct mm_region *mem_map = qemu_arm64_mem_map;
+#endif
+
 int board_init(void)
 {
 	return 0;
