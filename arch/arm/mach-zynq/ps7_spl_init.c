5a60a548f7dfc3c792ca0e24c0427f72b31e7c9f
Michal Simek <michal.simek@xilinx.com>
2018-01-12 14:24:50 +0100

    arm: zynq: Fix types in ps7_spl_init
    The patch is fixing the following Warning:
    arch/arm/mach-zynq/ps7_spl_init.c:133:24: warning: comparison between
    signed and unsigned integer expressions [-Wsign-compare]
        while (ioread(addr) < delay)
                            ^
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/ps7_spl_init.c
+++ arch/arm/mach-zynq/ps7_spl_init.c
@@ -58,7 +58,7 @@ static void perf_start_clock(void)
 }
 
 /* Compute mask for given delay in miliseconds*/
-static int get_number_of_cycles_for_delay(unsigned int delay)
+static unsigned long get_number_of_cycles_for_delay(unsigned long delay)
 {
 	return (APU_FREQ / (2 * 1000)) * delay;
 }
@@ -92,7 +92,7 @@ int __weak ps7_config(unsigned long *ps7_config_init)
 	unsigned long mask;
 	unsigned int numargs;
 	int i;
-	int delay;
+	unsigned long delay;
 
 	for (;;) {
 		opcode = ptr[0];
