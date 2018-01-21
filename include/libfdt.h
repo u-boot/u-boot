25ad202dd3268c3bda692de83485ca7649f2a57c
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-21 19:19:16 +0900

    libfdt: move working_fdt and FDT_RAMDISK_OVERHEAD to include/libfdt.h
    libfdt_env.h exists to contain system-dependent defines:
    
      - typedef of fdt*_t
      - fdt*_to_cpu(), cpu_to_fdt*
    
    working_fdt and FDT_RAMDISK_OVERHEAD are unrelated to the environment,
    so they must get out of this header.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/libfdt.h
+++ include/libfdt.h
@@ -1,1 +1,14 @@
+#ifndef UBOOT_LIBFDT_H
+#define UBOOT_LIBFDT_H
+/*
+ * SPDX-License-Identifier:     GPL-2.0+ BSD-2-Clause
+ */
+
 #include "../lib/libfdt/libfdt.h"
+
+extern struct fdt_header *working_fdt;	/* Pointer to the working fdt */
+
+/* adding a ramdisk needs 0x44 bytes in version 2008.10 */
+#define FDT_RAMDISK_OVERHEAD	0x80
+
+#endif /* UBOOT_LIBFDT_H */
--- include/libfdt_env.h
+++ include/libfdt_env.h
@@ -12,8 +12,6 @@
 #include "compiler.h"
 #include "linux/types.h"
 
-extern struct fdt_header *working_fdt;  /* Pointer to the working fdt */
-
 typedef __be16 fdt16_t;
 typedef __be32 fdt32_t;
 typedef __be64 fdt64_t;
@@ -22,8 +20,5 @@ typedef __be64 fdt64_t;
 #define cpu_to_fdt32(x)		cpu_to_be32(x)
 #define fdt64_to_cpu(x)		be64_to_cpu(x)
 #define cpu_to_fdt64(x)		cpu_to_be64(x)
-
-/* adding a ramdisk needs 0x44 bytes in version 2008.10 */
-#define FDT_RAMDISK_OVERHEAD	0x80
 
 #endif /* _LIBFDT_ENV_H */
