b0bd96c858afc273b6030a4c19c5074e6163b6f8
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-21 19:19:14 +0900

    libfdt: fix <linux/libfdt.h>
    I do not remember why, but this is apparently a file-copy mistake.
    The file name is libfdt.h, but its content is that of libfdt_env.h
    
    Re-import it from upstream Linux.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/linux/libfdt.h
+++ include/linux/libfdt.h
@@ -1,17 +1,8 @@
-#ifndef _LIBFDT_ENV_H
-#define _LIBFDT_ENV_H
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _INCLUDE_LIBFDT_H_
+#define _INCLUDE_LIBFDT_H_
 
-#include <linux/string.h>
+#include <linux/libfdt_env.h>
+#include "../../scripts/dtc/libfdt/libfdt.h"
 
-#include <asm/byteorder.h>
-
-typedef __be16 fdt16_t;
-typedef __be32 fdt32_t;
-typedef __be64 fdt64_t;
-
-#define fdt32_to_cpu(x) be32_to_cpu(x)
-#define cpu_to_fdt32(x) cpu_to_be32(x)
-#define fdt64_to_cpu(x) be64_to_cpu(x)
-#define cpu_to_fdt64(x) cpu_to_be64(x)
-
-#endif /* _LIBFDT_ENV_H */
+#endif /* _INCLUDE_LIBFDT_H_ */
