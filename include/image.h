6808ef9ac2a66a261bf341a99c3edb1e69f1cbdd
Bin Chen <bin.chen@linaro.org>
2018-01-27 16:59:09 +1100

    move booti_setup to arch/arm/lig/image.c
    Follow bootz's pattern by moving the booti_setup to arch/arm/lib.
    This allows to use booti_setup in other paths, e.g booting
    an Android image containing Image format.
    
    Note that kernel relocation is move out of booti_setup and it is the
    caller's responsibility to do it and allows them do it differently. say,
    cmd/booti.c just do a manually, while in the bootm path, we can use
    bootm_load_os(with some changes).
    
    Signed-off-by: Bin Chen <bin.chen@linaro.org>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- include/image.h
+++ include/image.h
@@ -871,6 +871,15 @@ int image_setup_linux(bootm_headers_t *images);
  */
 int bootz_setup(ulong image, ulong *start, ulong *end);
 
+/**
+ * Return the correct start address and size of a Linux aarch64 Image.
+ *
+ * @image: Address of image
+ * @start: Returns start address of image
+ * @size : Returns size image
+ * @return 0 if OK, 1 if the image was not recognised
+ */
+int booti_setup(ulong image, ulong *relocated_addr, ulong *size);
 
 /*******************************************************************/
 /* New uImage format specific code (prefixed with fit_) */
