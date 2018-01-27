f39bfec230828a843bb53d0213709d47db1574df
Tom Rini <trini@konsulko.com>
2018-01-27 17:23:21 -0500

    fs: btrfs: Fix printf format character warning
    When printing a size_t value we need to use %zu for portability between
    32bit and 64bit targets.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Marek Behun <marek.behun@nic.cz>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -147,8 +147,8 @@ static int btrfs_check_super(struct btrfs_super_block *sb)
 
 	if (sb->sys_chunk_array_size < sizeof(struct btrfs_key) +
 	    sizeof(struct btrfs_chunk)) {
-		printf("%s: system chunk array too small %u < %lu\n", __func__,
-		       sb->sys_chunk_array_size, (u32) sizeof(struct btrfs_key)
+		printf("%s: system chunk array too small %u < %zu\n", __func__,
+		       sb->sys_chunk_array_size, sizeof(struct btrfs_key)
 		       + sizeof(struct btrfs_chunk));
 		ret = -1;
 	}
