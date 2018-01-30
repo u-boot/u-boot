f29aa23b54e26d212cc61c07bcbb66ed5d6f9704
Justin Hibbits <chmeeedalf@gmail.com>
2018-01-29 22:23:36 -0600

    Fix FreeBSD endian checks
    FreeBSD, like OpenBSD, uses BIG_ENDIAN, LITTLE_ENDIAN, and BYTE_ORDER,
    whereas Linux and compatibles use __-prefixed names.  Define the names
    the same as the OpenBSD block below it.

--- include/compiler.h
+++ include/compiler.h
@@ -50,6 +50,9 @@ typedef unsigned long ulong;
 #endif
 #ifdef __FreeBSD__
 # include <sys/endian.h> /* htole32 and friends */
+# define __BYTE_ORDER BYTE_ORDER
+# define __LITTLE_ENDIAN LITTLE_ENDIAN
+# define __BIG_ENDIAN BIG_ENDIAN
 #elif defined(__OpenBSD__)
 # include <endian.h>
 # define __BYTE_ORDER BYTE_ORDER
