9d466f2fe11a88da9f31f260296be042ed378b9d
Álvaro Fernández Rojas <noltari@gmail.com>
2018-02-03 10:30:28 +0100

    MIPS: add BMIPS Netgear DGND3700v2 board
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ include/configs/netgear_dgnd3700v2.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018 Ãlvaro FernÃ¡ndez Rojas <noltari@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <configs/bmips_common.h>
+#include <configs/bmips_bcm6362.h>
+
+#define CONFIG_REMAKE_ELF
+
+#define CONFIG_ENV_SIZE			(8 * 1024)
+
