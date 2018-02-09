585beebd05d0561fc1113ee6904b4fe385e5348f
Patrice Chotard <patrice.chotard@st.com>
2018-02-09 13:09:57 +0100

    arch-stm32: Clean arch-stm32f7/syscfg.h
    Remove all unused defines
    
    Signed-off-by: Patrice Chotard <patrice.chotard@st.com>

--- arch/arm/include/asm/arch-stm32f7/syscfg.h
+++ arch/arm/include/asm/arch-stm32f7/syscfg.h
@@ -23,16 +23,7 @@ struct stm32_syscfg_regs {
  */
 #define STM32_SYSCFG		((struct stm32_syscfg_regs *)STM32_SYSCFG_BASE)
 
-/* SYSCFG memory remap register */
-#define SYSCFG_MEMRMP_MEM_BOOT	BIT(0)
-#define SYSCFG_MEMRMP_SWP_FMC	BIT(10)
-
 /* SYSCFG peripheral mode configuration register */
-#define SYSCFG_PMC_ADCXDC2	BIT(16)
 #define SYSCFG_PMC_MII_RMII_SEL	BIT(23)
-
-/* Compensation cell control register */
-#define SYSCFG_CMPCR_CMP_PD	BIT(0)
-#define SYSCFG_CMPCR_READY	BIT(8)
 
 #endif
