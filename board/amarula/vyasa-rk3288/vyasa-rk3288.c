1e84e44cfec952006f30c42bbab5d7e170776549
Jagan Teki <jagannadh.teki@gmail.com>
2018-02-14 21:26:22 +0530

    rk3288: vyasa: Add eMMC boot support
    RK3288 Vyasa has eMMC boot support, with JP4 open.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Acked-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- board/amarula/vyasa-rk3288/vyasa-rk3288.c
+++ board/amarula/vyasa-rk3288/vyasa-rk3288.c
@@ -9,6 +9,13 @@
 #ifndef CONFIG_TPL_BUILD
 #include <spl.h>
 
+void board_boot_order(u32 *spl_boot_list)
+{
+	/* eMMC prior to sdcard. */
+	spl_boot_list[0] = BOOT_DEVICE_MMC2;
+	spl_boot_list[1] = BOOT_DEVICE_MMC1;
+}
+
 int spl_start_uboot(void)
 {
         /* break into full u-boot on 'c' */
