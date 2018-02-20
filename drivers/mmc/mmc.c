76584e33988fc9f6c80199e04ca6e249baf7289a
Alexander Kochetkov <al.kochet@gmail.com>
2018-02-20 14:35:55 +0300

    mmc: fix off-by-one bug in mmc_startup_v4()
    MMC card with EXT_CSD_REV value 9 will trigger off-by-one
    bug while accessing mmc_versions array. The patch fix that.
    
    Signed-off-by: Alexander Kochetkov <al.kochet@gmail.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -1969,7 +1969,7 @@ static int mmc_startup_v4(struct mmc *mmc)
 		return -ENOMEM;
 	memcpy(mmc->ext_csd, ext_csd, MMC_MAX_BLOCK_LEN);
 
-	if (ext_csd[EXT_CSD_REV] > ARRAY_SIZE(mmc_versions))
+	if (ext_csd[EXT_CSD_REV] >= ARRAY_SIZE(mmc_versions))
 		return -EINVAL;
 
 	mmc->version = mmc_versions[ext_csd[EXT_CSD_REV]];
