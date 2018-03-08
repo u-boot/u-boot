cf4c34486d633c9e168475bd318ef67ff821063b
Calvin Johnson <calvin.johnson@nxp.com>
2018-03-08 15:30:27 +0530

    drivers: net: pfe_eth: LS1012A PFE headers
    Contains all the pfe header files.
    
    Signed-off-by: Calvin Johnson <calvin.johnson@nxp.com>
    Signed-off-by: Anjaneyulu Jagarlmudi <anji.jagarlmudi@nxp.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

---
+++ include/net/pfe_eth/pfe_mdio.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2015-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _PFE_MDIO_H_
+#define _PFE_MDIO_H_
+
+int pfe_phy_configure(struct pfe_eth_dev *priv, int dev_id, int phy_id);
+
+#endif /* _PFE_MDIO_H_ */
