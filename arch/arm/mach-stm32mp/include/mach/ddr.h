e70f70aa6504bd6387c0a6d117e48383c5048b6b
Patrick Delaunay <patrick.delaunay@st.com>
2018-03-12 10:46:11 +0100

    ram: stm32mp1: add driver
    Add driver and binding for stm32mp1 ddr controller and phy
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>

---
+++ arch/arm/mach-stm32mp/include/mach/ddr.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018, STMicroelectronics - All Rights Reserved
+ *
+ * SPDX-License-Identifier:	GPL-2.0+	BSD-3-Clause
+ */
+
+#ifndef __MACH_STM32MP_DDR_H_
+#define __MACH_STM32MP_DDR_H_
+
+int board_ddr_power_init(void);
+
+#endif
