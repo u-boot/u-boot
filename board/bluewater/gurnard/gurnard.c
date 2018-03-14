08afabc157de51d76c313a2b2606384a3d94f0a6
Jagan Teki <jagan@amarulasolutions.com>
2018-03-14 18:46:32 +0530

    at91: gurnard: Enable DM_SPI
    Enable DM_SPI for atmel SPI driver on gurnard board.
    
    Cc: Simon Glass <sjg@chromium.org>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- board/bluewater/gurnard/gurnard.c
+++ board/bluewater/gurnard/gurnard.c
@@ -18,7 +18,6 @@
 #ifndef CONFIG_DM_ETH
 #include <netdev.h>
 #endif
-#include <spi.h>
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <asm/mach-types.h>
@@ -414,25 +413,6 @@ int dram_init(void)
 void reset_phy(void)
 {
 }
-
-/* SPI chip select control - only used for FPGA programming */
-#ifdef CONFIG_ATMEL_SPI
-
-int spi_cs_is_valid(unsigned int bus, unsigned int cs)
-{
-	return bus == 0 && cs == 0;
-}
-
-void spi_cs_activate(struct spi_slave *slave)
-{
-	/* We don't use chipselects for FPGA programming */
-}
-
-void spi_cs_deactivate(struct spi_slave *slave)
-{
-	/* We don't use chipselects for FPGA programming */
-}
-#endif /* CONFIG_ATMEL_SPI */
 
 static struct atmel_serial_platdata at91sam9260_serial_plat = {
 	.base_addr = ATMEL_BASE_DBGU,
