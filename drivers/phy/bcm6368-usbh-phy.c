25d8380f221ecb6948ea98c2ac67ccc60892efc8
Álvaro Fernández Rojas <noltari@gmail.com>
2018-03-22 19:39:42 +0100

    phy: bcm6368-usbh: convert to use live dt
    Also fix bad accents in my name.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Reviewed-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- drivers/phy/bcm6368-usbh-phy.c
+++ drivers/phy/bcm6368-usbh-phy.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2018 Ãlvaro FernÃ¡ndez Rojas <noltari@gmail.com>
+ * Copyright (C) 2018 Álvaro Fernández Rojas <noltari@gmail.com>
  *
  * Derived from linux/arch/mips/bcm63xx/usb-common.c:
  *	Copyright 2008 Maxime Bizon <mbizon@freebox.fr>
@@ -116,15 +116,12 @@ static int bcm6368_usbh_probe(struct udevice *dev)
 #endif
 	struct reset_ctl rst_ctl;
 	struct clk clk;
-	fdt_addr_t addr;
-	fdt_size_t size;
 	int ret;
 
-	addr = devfdt_get_addr_size_index(dev, 0, &size);
-	if (addr == FDT_ADDR_T_NONE)
+	priv->regs = dev_remap_addr(dev);
+	if (!priv->regs)
 		return -EINVAL;
 
-	priv->regs = ioremap(addr, size);
 	priv->hw = hw;
 
 	/* enable usbh clock */
