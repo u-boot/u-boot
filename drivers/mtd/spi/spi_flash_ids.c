571910ca1944be309034629737040e02ced601a7
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2018-03-28 18:06:19 +0530

    spi: Add support for macronix devices mx25u12835f and mx25u25635f
    This patch add macronix flash devices mx25u12835f and mx25u25635f
    to spi flahs ids list.
    
    Signed-off-by: Prashant Mehta <pmeh@xilinx.com>
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -85,6 +85,8 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"mx25l25635f",	   INFO(0xc22019, 0x0, 64 * 1024,   512, RD_FULL | WR_QPP) },
 	{"mx25l51235f",	   INFO(0xc2201a, 0x0, 64 * 1024,  1024, RD_FULL | WR_QPP) },
 	{"mx25u6435f",	   INFO(0xc22537, 0x0, 64 * 1024,   128, RD_FULL | WR_QPP) },
+	{"mx25u12835f",	   INFO(0xc22538, 0x0, 64 * 1024,   256, RD_FULL | WR_QPP) },
+	{"mx25u25635f",	   INFO(0xc22539, 0x0, 64 * 1024,   512, RD_FULL | WR_QPP) },
 	{"mx25l12855e",	   INFO(0xc22618, 0x0, 64 * 1024,   256, RD_FULL | WR_QPP) },
 	{"mx25u1635e",     INFO(0xc22535, 0x0, 64 * 1024,  32, SECT_4K) },
 	{"mx66u51235f",    INFO(0xc2253a, 0x0, 64 * 1024,  1024, RD_FULL | WR_QPP) },
