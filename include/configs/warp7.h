206426aa0b636446bfe874f8a5cd3fc0a8a95119
Pierre-Jean TEXIER <texier.pj2@gmail.com>
2018-04-01 00:49:04 +0200

    warp7: include/configs: use generic fs commands in CONFIG_EXTRA_ENV_SETTINGS
    use the generic filesystem command 'load' rather
    than 'fatload' to avoid per-fs specific commands.
    
    Signed-off-by: Pierre-Jean TEXIER <texier.pj2@gmail.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>
    Acked-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- include/configs/warp7.h
+++ include/configs/warp7.h
@@ -48,11 +48,11 @@
 	"mmcargs=setenv bootargs console=${console},${baudrate} " \
 		"root=PARTUUID=${uuid} rootwait rw\0" \
 	"loadbootscript=" \
-		"fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
+		"load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"loadimage=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
+	"loadfdt=load mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run finduuid; " \
 		"run mmcargs; " \
