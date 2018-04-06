ebca6013c086356419a462dc1adbef858a5ada67
Vanessa Maegima <vanessa.maegima@nxp.com>
2018-04-06 16:54:41 -0300

    pico-imx7d: Replace fatload command
    Replace fatload with the fs generic loading interface ('load' command).
    
    Signed-off-by: Vanessa Maegima <vanessa.maegima@nxp.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>

--- include/configs/pico-imx7d.h
+++ include/configs/pico-imx7d.h
@@ -50,8 +50,8 @@
 	"finduuid=part uuid mmc 0:2 uuid\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} " \
 		"root=PARTUUID=${uuid} rootwait rw\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"loadimage=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
+	"loadfdt=load mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run finduuid; " \
 		"run mmcargs; " \
