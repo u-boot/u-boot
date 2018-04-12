b6bb93de75033d6d52392fff1d3f512b2feeadb6
Ruchika Gupta <ruchika.gupta@nxp.com>
2018-04-12 16:24:35 +0530

    armv8: sec_firmware: Remove JR3 from device tree node in all cases
    JR3 was getting removed from device tree only if random number
    generation was successful. However, if SEC firmware is present,
    JR3 should be removed from device tree node irrespective of the
    random seed generation as SEC firmware reserves it for it's use.
    Not removing it in case of random number generation failure causes
    the kernel to crash.
    
    Random number generation was being called twice. This is not
    required. If SEC firmware is running, SIP call can be made to the SEC
    firmware to get the random number. This call itself would return
    failure if function is not supported. Duplicate calling of random
    number generation function has been removed.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- arch/arm/cpu/armv8/sec_firmware.c
+++ arch/arm/cpu/armv8/sec_firmware.c
@@ -317,9 +317,7 @@ unsigned int sec_firmware_support_psci_version(void)
  */
 bool sec_firmware_support_hwrng(void)
 {
-	uint8_t rand[8];
 	if (sec_firmware_addr & SEC_FIRMWARE_RUNNING) {
-		if (!sec_firmware_get_random(rand, 8))
 			return true;
 	}
 
@@ -428,8 +426,10 @@ int fdt_fixup_kaslr(void *fdt)
 
 #if defined(CONFIG_ARMV8_SEC_FIRMWARE_SUPPORT)
 	/* Check if random seed generation is  supported */
-	if (sec_firmware_support_hwrng() == false)
+	if (sec_firmware_support_hwrng() == false) {
+		printf("WARNING: SEC firmware not running, no kaslr-seed\n");
 		return 0;
+	}
 
 	ret = sec_firmware_get_random(rand, 8);
 	if (ret < 0) {
