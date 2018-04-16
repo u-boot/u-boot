41c79775f08ad5836ae875cd21ea7ed952fa1237
Patrick Delaunay <patrick.delaunay@st.com>
2018-04-16 10:13:24 +0200

    arm: stm32mp1: add PSCI support
    Add PSCI v1.0 support for Linux and manage PSCI state
    for each CPU (affinity 0 level) with all mandatory functions:
    - PSCI_VERSION
    - CPU_SUSPEND
    - CPU_OFF
    - CPU_ON
    - AFFINITY_INFO
    - SYSTEM_OFF
    - SYSTEM_RESET
    - PSCI_FEATURES
    and 1 optional to avoid Linux warning
    - MIGRATE_INFO_TYPE
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>

--- include/configs/stm32mp1.h
+++ include/configs/stm32mp1.h
@@ -17,6 +17,11 @@
  */
 #define CONFIG_SYS_HZ				1000
 
+/* PSCI support */
+#define CONFIG_ARMV7_PSCI_1_0
+#define CONFIG_ARMV7_SECURE_BASE		STM32_SYSRAM_BASE
+#define CONFIG_ARMV7_SECURE_MAX_SIZE		STM32_SYSRAM_SIZE
+
 /*
  * malloc() pool size
  */
