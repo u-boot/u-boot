9622c7e65d9b9fe4703d3bf35c472518aedba404
Patrick Delaunay <patrick.delaunay@st.com>
2018-04-16 10:15:11 +0200

    sunxi: psci: save context id in cpu_on command
    Replace the psci_save_target_pc call by the new function
    psci_save(cpu, pc,context_id)
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>

--- arch/arm/cpu/armv7/sunxi/psci.c
+++ arch/arm/cpu/armv7/sunxi/psci.c
@@ -242,14 +242,15 @@ out:
 	cp15_write_scr(scr);
 }
 
-int __secure psci_cpu_on(u32 __always_unused unused, u32 mpidr, u32 pc)
+int __secure psci_cpu_on(u32 __always_unused unused, u32 mpidr, u32 pc,
+			 u32 context_id)
 {
 	struct sunxi_cpucfg_reg *cpucfg =
 		(struct sunxi_cpucfg_reg *)SUNXI_CPUCFG_BASE;
 	u32 cpu = (mpidr & 0x3);
 
-	/* store target PC */
-	psci_save_target_pc(cpu, pc);
+	/* store target PC and context id */
+	psci_save(cpu, pc, context_id);
 
 	/* Set secondary core power on PC */
 	sunxi_set_entry_address(&psci_cpu_entry);
