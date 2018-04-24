e4514051fca3a89850d7e62c29dff4e1194d225d
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-04-24 18:46:43 +0100

    warp7: Define the name of a signed boot-script file
    We need to know the name of a signed boot-script, its better to have a
    separate variable for this then to simply append some fixed string to an
    existing image name.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Tested-by: Breno Lima <breno.lima@nxp.com>
    Reviewed-by: Fabio Estevam <fabio.estevam@nxp.com>

--- include/configs/warp7.h
+++ include/configs/warp7.h
@@ -33,6 +33,7 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	CONFIG_DFU_ENV_SETTINGS \
 	"script=boot.scr\0" \
+	"script_signed=boot.scr.imx-signed\0" \
 	"image=zImage\0" \
 	"console=ttymxc0\0" \
 	"ethact=usb_ether\0" \
