d629152a9ae544401e2e657e8ce597fe61abc27b
Marek Vasut <marek.vasut+renesas@gmail.com>
2018-04-26 13:19:29 +0200

    mmc: renesas-sdhi: Add R8A77990 E3 compatible
    Add new compatible to the Uniphier SD driver for R8A77990 E3 SoC.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- drivers/mmc/renesas-sdhi.c
+++ drivers/mmc/renesas-sdhi.c
@@ -323,6 +323,7 @@ static const struct udevice_id renesas_sdhi_match[] = {
 	{ .compatible = "renesas,sdhi-r8a7796", .data = RENESAS_GEN3_QUIRKS },
 	{ .compatible = "renesas,sdhi-r8a77965", .data = RENESAS_GEN3_QUIRKS },
 	{ .compatible = "renesas,sdhi-r8a77970", .data = RENESAS_GEN3_QUIRKS },
+	{ .compatible = "renesas,sdhi-r8a77990", .data = RENESAS_GEN3_QUIRKS },
 	{ .compatible = "renesas,sdhi-r8a77995", .data = RENESAS_GEN3_QUIRKS },
 	{ /* sentinel */ }
 };
