04ca871459da53930a81509c53724ccfa7101118
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-04-27 01:02:02 +0900

    test: ofnode: test ofnode_device_is_compatible()
    Test ofnode_device_is_compatible(), and also ofnode_path().
    
    Requested-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ test/dm/ofnode.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+#include <common.h>
+#include <dm.h>
+#include <dm/test.h>
+#include <test/ut.h>
+
+static int dm_test_ofnode_compatible(struct unit_test_state *uts)
+{
+	ofnode root_node = ofnode_path("/");
+
+	ut_assert(ofnode_valid(root_node));
+	ut_assert(ofnode_device_is_compatible(root_node, "sandbox"));
+
+	return 0;
+}
+DM_TEST(dm_test_ofnode_compatible, DM_TESTF_SCAN_PDATA | DM_TESTF_SCAN_FDT);
