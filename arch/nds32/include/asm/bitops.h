76b916f233d65ac934999c9b19a052874ff7c1d9
Bryan O'Donoghue <pure.logic@nexus-software.ie>
2018-04-30 15:56:07 +0100

    nds32: Define PLATFORM__SET_BIT for generic_set_bit()
    nds32 bitops.h provides a __set_bit() but does not define PLATFORM__SET_BIT
    as a result generic_set_bit() is used instead of the architecturally
    provided __set_bit().
    
    This patch defines PLATFORM__SET_BIT which means that __set_bit() in nds32
    bitops.h will be called whenever generic_set_bit() is called - as opposed
    to the default cross-platform generic_set_bit().
    
    Signed-off-by: Bryan O'Donoghue <pure.logic@nexus-software.ie>
    Cc: Macpaul Lin <macpaul@andestech.com>

--- arch/nds32/include/asm/bitops.h
+++ arch/nds32/include/asm/bitops.h
@@ -44,6 +44,8 @@ static inline void __set_bit(int nr, void *addr)
 	*a |= mask;
 }
 
+#define PLATFORM__SET_BIT
+
 extern void clear_bit(int nr, void *addr);
 
 static inline void __clear_bit(int nr, void *addr)
