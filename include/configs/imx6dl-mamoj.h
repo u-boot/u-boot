129ceda20217b2ac0adae16d9cce4710319c4849
Jagan Teki <jagan@amarulasolutions.com>
2018-05-07 11:21:38 +0530

    configs: imx6dl-mamoj: Add DFU support
    Add DFU support for BTicino Mamoj board and update
    the same steps in README.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Signed-off-by: Simone CIANNI <simone.cianni@bticino.it>
    Signed-off-by: Raffaele RECALCATI <raffaele.recalcati@bticino.it>

--- include/configs/imx6dl-mamoj.h
+++ include/configs/imx6dl-mamoj.h
@@ -15,7 +15,7 @@
 #include "mx6_common.h"
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(16 * SZ_1M)
+#define CONFIG_SYS_MALLOC_LEN		(35 * SZ_1M)
 
 /* Total Size of Environment Sector */
 #define CONFIG_ENV_SIZE			SZ_128K
@@ -37,6 +37,8 @@
 	"fdt_addr_r=0x13000000\0"	\
 	"kernel_addr_r=0x10008000\0"	\
 	"fdt_high=0xffffffff\0"		\
+	"dfu_alt_info_spl=spl raw 0x2 0x400\0" \
+	"dfu_alt_info_uboot=u-boot raw 0x8a 0x11400\0" \
 	BOOTENV
 
 #define BOOT_TARGET_DEVICES(func) \
