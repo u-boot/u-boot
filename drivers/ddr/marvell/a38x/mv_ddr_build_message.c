0315d6959fdd9d2a4d89016c311e9c8c8d239a10
Chris Packham <judge.packham@gmail.com>
2018-05-15 13:31:11 +1200

    ARM: mvebu: a38x: Add missing SPDX license identfier
    mv_ddr_build_message.c is generated in Marvell's standalone mv_ddr code.
    When imported into u-boot we need to add the appropriate SPDX tag and
    re-format it slightly.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>

--- drivers/ddr/marvell/a38x/mv_ddr_build_message.c
+++ drivers/ddr/marvell/a38x/mv_ddr_build_message.c
@@ -1,1 +1,3 @@
-const char mv_ddr_build_message[] = "";       const char mv_ddr_version_string[] = "mv_ddr: mv_ddr-armada-17.10.4";
+// SPDX-License-Identifier: GPL-2.0
+const char mv_ddr_build_message[] = "";
+const char mv_ddr_version_string[] = "mv_ddr: mv_ddr-armada-17.10.4";
