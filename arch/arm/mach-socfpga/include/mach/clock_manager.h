508791a035ea8e2698c52e1547f77048c49c4391
Ley Foon Tan <ley.foon.tan@intel.com>
2018-05-18 22:05:22 +0800

    arm: socfpga: stratix10: Add clock manager driver for Stratix10 SoC
    Add Clock Manager driver support for Stratix SoC
    
    Signed-off-by: Chin Liang See <chin.liang.see@intel.com>
    Signed-off-by: Ley Foon Tan <ley.foon.tan@intel.com>

--- arch/arm/mach-socfpga/include/mach/clock_manager.h
+++ arch/arm/mach-socfpga/include/mach/clock_manager.h
@@ -16,6 +16,8 @@ void cm_print_clock_quick_summary(void);
 #include <asm/arch/clock_manager_gen5.h>
 #elif defined(CONFIG_TARGET_SOCFPGA_ARRIA10)
 #include <asm/arch/clock_manager_arria10.h>
+#elif defined(CONFIG_TARGET_SOCFPGA_STRATIX10)
+#include <asm/arch/clock_manager_s10.h>
 #endif
 
 #endif /* _CLOCK_MANAGER_H_ */
