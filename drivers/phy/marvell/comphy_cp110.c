f596b01b9a69a8fd486468a11f6b2dbf07984c5f
Christine Gharzuzi <chrisg@marvell.com>
2018-05-23 12:10:36 +0300

    phy: marvell: fix handling of unconnected comphy
    - the default value of comphy pipe selector is set to PCIe (x4)
      in case of unconnected comphy the default value remains 0x4
      which may lead to several issues with comphy initialization.
    
    - this patch adds SMC call that powers off the comphy lane in case of
      unconnected comphy.
    
    Change-Id: I196b2916518dd8df3b159ffa85e2989b8e483087
    Signed-off-by: Christine Gharzuzi <chrisg@marvell.com>
    Signed-off-by: Igal Liberman <igall@marvell.com>
    Reviewed-by: Grzegorz Jaszczyk <jaz@semihalf.com>

--- drivers/phy/marvell/comphy_cp110.c
+++ drivers/phy/marvell/comphy_cp110.c
@@ -553,6 +553,10 @@ int comphy_cp110_init(struct chip_serdes_phy_config *ptr_chip_cfg,
 		}
 		switch (ptr_comphy_map->type) {
 		case COMPHY_TYPE_UNCONNECTED:
+			ret = comphy_smc(MV_SIP_COMPHY_POWER_OFF,
+					 ptr_chip_cfg->comphy_base_addr,
+					 lane,
+					 ptr_comphy_map->type);
 		case COMPHY_TYPE_IGNORE:
 			continue;
 			break;
