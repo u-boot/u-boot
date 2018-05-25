680a52c35088dc30a4ca18870ec89ff3e1ac0e52
Tom Rini <trini@konsulko.com>
2018-05-25 08:28:45 -0400

    tests: Make test_log.py tests depend on cmd_log not log
    While the tests in this testcase are for the log subsystem they are only
    able to be run if CONFIG_CMD_LOG is enabled as well as CONFIG_LOG, so
    update the buildconfigspec requirement.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- test/py/tests/test_log.py
+++ test/py/tests/test_log.py
@@ -12,7 +12,7 @@ import pytest
 
 LOGL_FIRST, LOGL_WARNING, LOGL_INFO = (0, 4, 6)
 
-@pytest.mark.buildconfigspec('log')
+@pytest.mark.buildconfigspec('cmd_log')
 def test_log(u_boot_console):
     """Test that U-Boot logging works correctly."""
     def check_log_entries(lines, mask, max_level=LOGL_INFO):
@@ -98,7 +98,7 @@ def test_log(u_boot_console):
     test8()
     test9()
 
-@pytest.mark.buildconfigspec('log')
+@pytest.mark.buildconfigspec('cmd_log')
 def test_log_format(u_boot_console):
     """Test the 'log format' and 'log rec' commands"""
     def run_with_format(fmt, expected_output):
