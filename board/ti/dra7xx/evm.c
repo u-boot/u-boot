413b90777f8d9949083688e491e3f4e329d8a5b9
Alex Kiernan <alex.kiernan@gmail.com>
2018-05-29 15:30:49 +0000

    ti: fastboot: Move weak overrides to board files
    Overriding fastboot_set_reboot_flag() in arch/arm/mach-omap2/boot-common.c
    leaves it applying all boards that derive from this, not just the ones which
    have support for Android bootloader flow. Move the weak function override to
    the relevant board files.
    
    Signed-off-by: Alex Kiernan <alex.kiernan@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- board/ti/dra7xx/evm.c
+++ board/ti/dra7xx/evm.c
@@ -1188,5 +1188,15 @@ void board_tee_image_process(ulong tee_image, size_t tee_size)
 	secure_tee_install((u32)tee_image);
 }
 
+#if CONFIG_IS_ENABLED(FASTBOOT) && !CONFIG_IS_ENABLED(ENV_IS_NOWHERE)
+int fastboot_set_reboot_flag(void)
+{
+	printf("Setting reboot to fastboot flag ...\n");
+	env_set("dofastboot", "1");
+	env_save();
+	return 0;
+}
+#endif
+
 U_BOOT_FIT_LOADABLE_HANDLER(IH_TYPE_TEE, board_tee_image_process);
 #endif
