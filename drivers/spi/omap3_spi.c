9cddf70ead79f7b7c113d7481860a14b61aa6e0b
Hannes Schmelzer <oe5hpm@oevsv.at>
2018-06-02 08:06:48 +0200

    spi: omap3: fix set_speed and set_mode dm callbacks
    commit 84807922874e03895bbf15c4472a2dcee8fbbd03
    ("spi: omap3: Skip set_mode, set_speed from claim") did break SPI
    support on my AM335x board.
    
    The named commit:
    
    - ignored the responsible arguments (speed, mode)
    The set speed/mode function must use the supplied function arguments to
    work properly. With this commit we take those arguments and transfer
    them to the priv-data.
    
    - used wrong udevice pointer for getting priv data
    the udevice-pointer within function argument is already the spi-bus
    device, so it is wrong looking here for some parent (ocp-bus in this
    case) and getting priv-pointer from there.
    
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>
    Reviewed-by: Jagan Teki <jagan@openedev.com>

--- drivers/spi/omap3_spi.c
+++ drivers/spi/omap3_spi.c
@@ -650,12 +650,10 @@ static int omap3_spi_xfer(struct udevice *dev, unsigned int bitlen,
 
 static int omap3_spi_set_speed(struct udevice *dev, unsigned int speed)
 {
-	struct udevice *bus = dev->parent;
-	struct omap3_spi_priv *priv = dev_get_priv(bus);
-	struct dm_spi_slave_platdata *slave_plat = dev_get_parent_platdata(dev);
 
-	priv->cs = slave_plat->cs;
-	priv->freq = slave_plat->max_hz;
+	struct omap3_spi_priv *priv = dev_get_priv(dev);
+
+	priv->freq = speed;
 	_omap3_spi_set_speed(priv);
 
 	return 0;
@@ -663,12 +661,10 @@ static int omap3_spi_set_speed(struct udevice *dev, unsigned int speed)
 
 static int omap3_spi_set_mode(struct udevice *dev, uint mode)
 {
-	struct udevice *bus = dev->parent;
-	struct omap3_spi_priv *priv = dev_get_priv(bus);
-	struct dm_spi_slave_platdata *slave_plat = dev_get_parent_platdata(dev);
+	struct omap3_spi_priv *priv = dev_get_priv(dev);
 
-	priv->cs = slave_plat->cs;
-	priv->mode = slave_plat->mode;
+	priv->mode = mode;
+
 	_omap3_spi_set_mode(priv);
 
 	return 0;
