a8542ad81f544e754a7173b43b13f9db1354de0f
Bin Meng <bmeng.cn@gmail.com>
2018-06-03 19:04:19 -0700

    x86: ivybridge: Drop CONFIG_USBDEBUG
    This is not used anywhere. Clean this up.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- arch/x86/cpu/intel_common/mrc.c
+++ arch/x86/cpu/intel_common/mrc.c
@@ -242,11 +242,6 @@ static int sdram_initialise(struct udevice *dev, struct udevice *me_dev,
 	      version >> 24 , (version >> 16) & 0xff,
 	      (version >> 8) & 0xff, version & 0xff);
 
-#if CONFIG_USBDEBUG
-	/* mrc.bin reconfigures USB, so reinit it to have debug */
-	early_usbdebug_init();
-#endif
-
 	return 0;
 }
 
