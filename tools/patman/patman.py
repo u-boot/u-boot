4fb3502972db0faeb278f62effc0a4b07a355531
Chris Packham <judge.packham@gmail.com>
2018-06-07 20:45:06 +1200

    patman: add option for limiting the Cc list
    Many mailing-lists consider a long Cc list a sign of spam and will
    either drop the message or mark it for moderation. Because patman
    automatically invokes get_maintainer.pl the Cc list can expand
    unexpectedly. Allow the user to specify a limit for the Cc list.
    
    This limit is applied after removing any known bouncing addresses. By
    default no limit is applied.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- tools/patman/patman.py
+++ tools/patman/patman.py
@@ -38,6 +38,8 @@ parser.add_option('-i', '--ignore-errors', action='store_true',
 parser.add_option('-m', '--no-maintainers', action='store_false',
        dest='add_maintainers', default=True,
        help="Don't cc the file maintainers automatically")
+parser.add_option('-l', '--limit-cc', dest='limit', type='int',
+       default=None, help='Limit the cc list to LIMIT entries [default: %default]')
 parser.add_option('-n', '--dry-run', action='store_true', dest='dry_run',
        default=False, help="Do a dry run (create but don't email patches)")
 parser.add_option('-p', '--project', default=project.DetectProject(),
@@ -157,7 +159,7 @@ else:
 
     cc_file = series.MakeCcFile(options.process_tags, cover_fname,
                                 not options.ignore_bad_tags,
-                                options.add_maintainers)
+                                options.add_maintainers, options.limit)
 
     # Email the patches out (giving the user time to check / cancel)
     cmd = ''
