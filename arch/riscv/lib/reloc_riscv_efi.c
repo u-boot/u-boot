b71bb87af90f54ff109fa0449d07d91764c0794a
Ivan Gorinov <ivan.gorinov@intel.com>
2018-06-28 14:50:17 -0700

    riscv: Remove unused _relocate arguments
    EFI image handle and system table are not used in _relocate().
    
    Signed-off-by: Ivan Gorinov <ivan.gorinov@intel.com>

--- arch/riscv/lib/reloc_riscv_efi.c
+++ arch/riscv/lib/reloc_riscv_efi.c
@@ -50,8 +50,7 @@
 #define ELF_R_TYPE	ELF32_R_TYPE
 #endif
 
-efi_status_t _relocate(long ldbase, Elf_Dyn *dyn, efi_handle_t image,
-		       struct efi_system_table *systab)
+efi_status_t EFIAPI _relocate(long ldbase, Elf_Dyn *dyn)
 {
 	long relsz = 0, relent = 0;
 	Elf_Rela *rel = 0;
