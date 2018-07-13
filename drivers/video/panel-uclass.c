28576f81823d9fbb9edc525aba5cb130882b8b1f
Yannick Fertré <yannick.fertre@st.com>
2018-07-13 14:11:09 +0200

    dm: panel: get timings from panel
    Get timings from panel instead of read device tree.
    
    Signed-off-by: Yannick Fertré <yannick.fertre@st.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/video/panel-uclass.c
+++ drivers/video/panel-uclass.c
@@ -18,6 +18,17 @@ int panel_enable_backlight(struct udevice *dev)
 	return ops->enable_backlight(dev);
 }
 
+int panel_get_display_timing(struct udevice *dev,
+			     struct display_timing *timings)
+{
+	struct panel_ops *ops = panel_get_ops(dev);
+
+	if (!ops->get_display_timing)
+		return -ENOSYS;
+
+	return ops->get_display_timing(dev, timings);
+}
+
 UCLASS_DRIVER(panel) = {
 	.id		= UCLASS_PANEL,
 	.name		= "panel",
--- include/panel.h
+++ include/panel.h
@@ -15,6 +15,15 @@ struct panel_ops {
 	 * @return 0 if OK, -ve on error
 	 */
 	int (*enable_backlight)(struct udevice *dev);
+	/**
+	 * get_timings() - Get display timings from panel.
+	 *
+	 * @dev:	Panel device containing the display timings
+	 * @tim:	Place to put timings
+	 * @return 0 if OK, -ve on error
+	 */
+	int (*get_display_timing)(struct udevice *dev,
+				  struct display_timing *timing);
 };
 
 #define panel_get_ops(dev)	((struct panel_ops *)(dev)->driver->ops)
@@ -26,5 +35,14 @@ struct panel_ops {
  * @return 0 if OK, -ve on error
  */
 int panel_enable_backlight(struct udevice *dev);
+
+/**
+ * panel_get_display_timing() - Get display timings from panel.
+ *
+ * @dev:	Panel device containing the display timings
+ * @return 0 if OK, -ve on error
+ */
+int panel_get_display_timing(struct udevice *dev,
+			     struct display_timing *timing);
 
 #endif
