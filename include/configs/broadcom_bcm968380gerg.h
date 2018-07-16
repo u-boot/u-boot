990cebf0a2c2e4dd9033c56acf7b1404a0c0f698
Philippe Reynes <philippe.reynes@softathome.com>
2018-07-16 19:06:15 +0200

    bcm968380gerg: add initial support
    This add the initial support of the broadcom reference
    board bcm968380gerg with a bcm68380 SoC.
    
    This board has 512 MB of RAM, 128 MB of flash (nand),
    2 USB port, 1 UART, 4 ethernet ports and BCM43217 (wifi).
    
    Signed-off-by: Philippe Reynes <philippe.reynes@softathome.com>
    Reviewed-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/broadcom_bcm968380gerg.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2018 Philippe Reynes <philippe.reynes@softathome.com>
+ */
+
+#include <configs/bmips_common.h>
+#include <configs/bmips_bcm6838.h>
+
+#define CONFIG_ENV_SIZE			(8 * 1024)
