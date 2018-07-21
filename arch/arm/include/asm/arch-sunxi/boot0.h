80197801f4786fef257119981c977ed6debdf539
Icenowy Zheng <icenowy@aosc.io>
2018-07-21 16:20:22 +0800

    sunxi: change RMR64's RVBAR address for H6
    Allwinner H6 has a different RVBAR address with A64/H5.
    
    Add conditional RVBAR configuration into the code which does RMR switch.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>
    Reviewed-by: Andre Przywara <andre.przywara@arm.com>
    Acked-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Reviewed-by: Jagan Teki <jagan@openedev.com>
    Tested-by: Jagan Teki <jagan@amarulasolutions.com>

--- arch/arm/include/asm/arch-sunxi/boot0.h
+++ arch/arm/include/asm/arch-sunxi/boot0.h
@@ -26,7 +26,11 @@
 	.word	0xf57ff06f	// isb     sy
 	.word	0xe320f003	// wfi
 	.word	0xeafffffd	// b       @wfi
+#ifndef CONFIG_MACH_SUN50I_H6
 	.word	0x017000a0	// writeable RVBAR mapping address
+#else
+	.word	0x09010040	// writeable RVBAR mapping address
+#endif
 #ifdef CONFIG_SPL_BUILD
 	.word	CONFIG_SPL_TEXT_BASE
 #else
