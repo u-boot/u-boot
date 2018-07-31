3d186cf3f3ab5c18ede0f0a4ff85ffe99839bc7b
Stephen Warren <swarren@nvidia.com>
2018-07-31 12:39:07 -0600

    ARM: tegra: avoid more operations in non-secure world
    A secure monitor that runs before U-Boot, and hence causes U-Boot to run
    in non-secure world, must implement a few operations that U-Boot
    otherwise implements when running in secure world. Fix U-Boot to skip
    these operations when running in non-secure world. In particular:
    
    - The secure monitor must provide the LP0 resume code and own LP0
      configuration in order to maintain security, so must initialize all
      the PMC scratch registers used by the boot ROM during LP0 resume.
      Consequently, U-Boot should not attempt to clear those registers,
      since the register accesses will fail or cause an error.
    
    - The secure monitor owns system security, and so is responsible for
      configuring security-related items such as the VPR.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- arch/arm/mach-tegra/ap.c
+++ arch/arm/mach-tegra/ap.c
@@ -155,8 +155,13 @@ static void init_pmc_scratch(void)
 	int i;
 
 	/* SCRATCH0 is initialized by the boot ROM and shouldn't be cleared */
-	for (i = 0; i < 23; i++)
-		writel(0, &pmc->pmc_scratch1+i);
+#if defined(CONFIG_TEGRA_SUPPORT_NON_SECURE)
+	if (!tegra_cpu_is_non_secure())
+#endif
+	{
+		for (i = 0; i < 23; i++)
+			writel(0, &pmc->pmc_scratch1 + i);
+	}
 
 	/* ODMDATA is for kernel use to determine RAM size, LP config, etc. */
 	odmdata = get_odmdata();
