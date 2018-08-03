841f3216c298dbc5cf6802eac9ffe1e4da1167f4
Bin Meng <bmeng.cn@gmail.com>
2018-08-03 01:14:49 -0700

    pci: sandbox: emul: Rename priv structure
    We have "struct sandbox_pci_priv" in pci_sandbox driver. To avoid
    confusion, rename the emul's priv to "struct sandbox_pci_emul_priv".
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/pci/pci-emul-uclass.c
+++ drivers/pci/pci-emul-uclass.c
@@ -11,7 +11,7 @@
 #include <pci.h>
 #include <dm/lists.h>
 
-struct sandbox_pci_priv {
+struct sandbox_pci_emul_priv {
 	int dev_count;
 };
 
@@ -43,7 +43,7 @@ int sandbox_pci_get_emul(struct udevice *bus, pci_dev_t find_devfn,
 
 static int sandbox_pci_emul_post_probe(struct udevice *dev)
 {
-	struct sandbox_pci_priv *priv = dev->uclass->priv;
+	struct sandbox_pci_emul_priv *priv = dev->uclass->priv;
 
 	priv->dev_count++;
 	sandbox_set_enable_pci_map(true);
@@ -53,7 +53,7 @@ static int sandbox_pci_emul_post_probe(struct udevice *dev)
 
 static int sandbox_pci_emul_pre_remove(struct udevice *dev)
 {
-	struct sandbox_pci_priv *priv = dev->uclass->priv;
+	struct sandbox_pci_emul_priv *priv = dev->uclass->priv;
 
 	priv->dev_count--;
 	sandbox_set_enable_pci_map(priv->dev_count > 0);
@@ -66,5 +66,5 @@ UCLASS_DRIVER(pci_emul) = {
 	.name		= "pci_emul",
 	.post_probe	= sandbox_pci_emul_post_probe,
 	.pre_remove	= sandbox_pci_emul_pre_remove,
-	.priv_auto_alloc_size	= sizeof(struct sandbox_pci_priv),
+	.priv_auto_alloc_size	= sizeof(struct sandbox_pci_emul_priv),
 };
