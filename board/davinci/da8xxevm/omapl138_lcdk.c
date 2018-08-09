21af33ed0319b1a5133ea78a4261b5464e1a6a1b
Adam Ford <aford173@gmail.com>
2018-08-09 06:15:14 -0500

    ARM: davinci: omapl138_lcdk: Enable DM_MMC
    With DM_MMC now available, this patch enables DM_MMC for the
    omapl138_lcdk in U-Boot and keeps the older style for SPL.
    
    Signed-off-by: Peter Howard <phoward@gme.net.au>
    Signed-off-by: Adam Ford <aford173@gmail.com>

--- board/davinci/da8xxevm/omapl138_lcdk.c
+++ board/davinci/da8xxevm/omapl138_lcdk.c
@@ -353,6 +353,7 @@ int misc_init_r(void)
 	return 0;
 }
 
+#ifndef CONFIG_DM_MMC
 #ifdef CONFIG_MMC_DAVINCI
 static struct davinci_mmc mmc_sd0 = {
 	.reg_base = (struct davinci_mmc_regs *)DAVINCI_MMC_SD0_BASE,
@@ -368,4 +369,5 @@ int board_mmc_init(bd_t *bis)
 	/* Add slot-0 to mmc subsystem */
 	return davinci_mmc_init(bis, &mmc_sd0);
 }
+#endif
 #endif
