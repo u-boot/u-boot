600e46b08c6ac00aef2077a7467ae586d59f3250
Lokesh Vutla <lokeshvutla@ti.com>
2018-08-27 15:57:47 +0530

    mailbox: Allow attaching private data for mbox_chan
    Sometimes mbox controllers wants to store private data in
    mbox_chan so that it can be used at a later point of time.
    Adding support for hooking private data.
    
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- include/mailbox.h
+++ include/mailbox.h
@@ -44,6 +44,7 @@ struct udevice;
  *
  * @dev: The device which implements the mailbox.
  * @id: The mailbox channel ID within the provider.
+ * @con_priv: Hook for controller driver to attach private data
  *
  * Currently, the mailbox API assumes that a single integer ID is enough to
  * identify and configure any mailbox channel for any mailbox provider. If this
@@ -56,11 +57,9 @@ struct udevice;
  */
 struct mbox_chan {
 	struct udevice *dev;
-	/*
-	 * Written by of_xlate. We assume a single id is enough for now. In the
-	 * future, we might add more fields here.
-	 */
+	/* Written by of_xlate.*/
 	unsigned long id;
+	void *con_priv;
 };
 
 /**
