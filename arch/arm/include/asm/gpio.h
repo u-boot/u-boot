3980b16137341930ce4116225d42e35a996479c7
Lokesh Vutla <lokeshvutla@ti.com>
2018-08-27 15:57:55 +0530

    gpio: do not include <asm/arch/gpio.h> for ARCH_K3
    As no gpio.h is defined in arch/arm/mach-k3/include/,
    to avoid compilation failure, do not include asm/arch/gpio.h.
    
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- arch/arm/include/asm/gpio.h
+++ arch/arm/include/asm/gpio.h
@@ -1,4 +1,5 @@
-#if !defined(CONFIG_ARCH_UNIPHIER) && !defined(CONFIG_ARCH_STI)
+#if !defined(CONFIG_ARCH_UNIPHIER) && !defined(CONFIG_ARCH_STI) && \
+	!defined(CONFIG_ARCH_K3)
 #include <asm/arch/gpio.h>
 #endif
 #include <asm-generic/gpio.h>
