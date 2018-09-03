a92e52137d3cdad60d5984936330ad0fca1b4a2b
Jasper kcoding <jasperkcoding@gmail.com>
2018-09-03 22:14:53 +0200

    amlogic: board p212: Fix Ethernet PHY init
    Without this patch the Ethernet PHY on the p212 board does not get
    fully configured.
    When this happens Ethernet does not function.
    
    The similar libretech-cc and khadas-vim boards have this code already.
    That's why the Ethernet on these boards do work.
    
    Signed-off-by: Jasper Kcoding <jasperkcoding@gmail.com>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>

--- board/amlogic/p212/p212.c
+++ board/amlogic/p212/p212.c
@@ -29,7 +29,8 @@ int misc_init_r(void)
 	char serial[EFUSE_SN_SIZE];
 	ssize_t len;
 
-	meson_gx_eth_init(PHY_INTERFACE_MODE_RMII, 0);
+	meson_gx_eth_init(PHY_INTERFACE_MODE_RMII,
+			  MESON_GXL_USE_INTERNAL_RMII_PHY);
 
 	if (!eth_env_get_enetaddr("ethaddr", mac_addr)) {
 		len = meson_sm_read_efuse(EFUSE_MAC_OFFSET,
