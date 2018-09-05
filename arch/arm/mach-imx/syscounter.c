be277c3a89fcec9f837bf4fb562c606155b0999f
Rui Miguel Silva <rui.silva@linaro.org>
2018-09-05 11:56:05 +0100

    imx: mx7: avoid some initialization if low level is skipped
    We can have the case where u-boot is launched after some other low level
    enabler, like for example when u-boot runs after arm-trusted-firmware
    and/or optee. So, because of that we may need to jump the initialization of
    some IP blocks even because we may no longer have the permission for that.
    
    So, if the config option to skip low level init is set disable also timer,
    board and csu initialization.
    
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Cc: Stefano Babic <sbabic@denx.de>
    Cc: Fabio Estevam <fabio.estevam@nxp.com>
    Cc: Albert Aribaud <albert.u.boot@aribaud.net>
    Cc: Peng Fan <peng.fan@nxp.com>
    Cc: u-boot@lists.denx.de
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/syscounter.c
+++ arch/arm/mach-imx/syscounter.c
@@ -55,6 +55,7 @@ static inline unsigned long long us_to_tick(unsigned long long usec)
 	return usec;
 }
 
+#ifndef CONFIG_SKIP_LOWLEVEL_INIT
 int timer_init(void)
 {
 	struct sctr_regs *sctr = (struct sctr_regs *)SCTR_BASE_ADDR;
@@ -76,6 +77,7 @@ int timer_init(void)
 
 	return 0;
 }
+#endif
 
 unsigned long long get_ticks(void)
 {
