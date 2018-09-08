47cef9c80587e3ec8d3f3cbd546c0539c372a995
Chris Packham <judge.packham@gmail.com>
2018-09-08 21:39:07 +1200

    xtensa: use asm-generic/atomic.h
    Make use of asm-generic/atomic.h.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Acked-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/include/asm/atomic.h
+++ arch/xtensa/include/asm/atomic.h
@@ -7,48 +7,6 @@
 #define _XTENSA_ATOMIC_H
 
 #include <asm/system.h>
-
-typedef struct { volatile int counter; } atomic_t;
-
-#define ATOMIC_INIT(i)	{ (i) }
-
-#define atomic_read(v)		((v)->counter)
-#define atomic_set(v, i)	((v)->counter = (i))
-
-static inline void atomic_add(int i, atomic_t *v)
-{
-	unsigned long flags;
-
-	local_irq_save(flags);
-	v->counter += i;
-	local_irq_restore(flags);
-}
-
-static inline void atomic_sub(int i, atomic_t *v)
-{
-	unsigned long flags;
-
-	local_irq_save(flags);
-	v->counter -= i;
-	local_irq_restore(flags);
-}
-
-static inline void atomic_inc(atomic_t *v)
-{
-	unsigned long flags;
-
-	local_irq_save(flags);
-	++v->counter;
-	local_irq_restore(flags);
-}
-
-static inline void atomic_dec(atomic_t *v)
-{
-	unsigned long flags;
-
-	local_irq_save(flags);
-	--v->counter;
-	local_irq_restore(flags);
-}
+#include <asm-generic/atomic.h>
 
 #endif
