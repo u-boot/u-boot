d4b751e9f3efc5f230c0596c082ba13ca89618e1
AKASHI Takahiro <takahiro.akashi@linaro.org>
2018-09-11 15:59:15 +0900

    cmd: fat: add fatrm command
    In this patch, a new command, fatrm, is added so as to delete a file
    or directory.
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- cmd/fat.c
+++ cmd/fat.c
@@ -152,6 +152,18 @@ U_BOOT_CMD(
 	"      to 'dev' on 'interface'"
 );
 
+static int do_fat_rm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	return do_rm(cmdtp, flag, argc, argv, FS_TYPE_FAT);
+}
+
+U_BOOT_CMD(
+	fatrm,	4,	1,	do_fat_rm,
+	"delete a file",
+	"<interface> [<dev[:part]>] <filename>\n"
+	"    - delete a file from 'dev' on 'interface'"
+);
+
 static int do_fat_mkdir(cmd_tbl_t *cmdtp, int flag, int argc,
 			char * const argv[])
 {
