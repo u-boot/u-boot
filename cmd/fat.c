0349da51009211b540a8653795dee5d4b872fd3b
AKASHI Takahiro <takahiro.akashi@linaro.org>
2018-09-11 15:59:11 +0900

    cmd: fat: add fatmkdir command
    In this patch, a new command, fatmkdir, is added.
    
    Please note that, as there is no notion of "current directory" on u-boot,
    a directory name specified must contains an absolute directory path as
    a parent directory. Otherwise, "/" (root directory) is assumed.
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- cmd/fat.c
+++ cmd/fat.c
@@ -151,4 +151,17 @@ U_BOOT_CMD(
 	"    - write file 'filename' from the address 'addr' in RAM\n"
 	"      to 'dev' on 'interface'"
 );
+
+static int do_fat_mkdir(cmd_tbl_t *cmdtp, int flag, int argc,
+			char * const argv[])
+{
+	return do_mkdir(cmdtp, flag, argc, argv, FS_TYPE_FAT);
+}
+
+U_BOOT_CMD(
+	fatmkdir,	4,	1,	do_fat_mkdir,
+	"create a directory",
+	"<interface> [<dev[:part]>] <directory>\n"
+	"    - create a directory in 'dev' on 'interface'"
+);
 #endif
