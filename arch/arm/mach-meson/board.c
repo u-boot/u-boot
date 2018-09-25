c45414b542ee31283bd5954211facbee217f0322
Loic Devulder <ldevulder@suse.de>
2018-09-25 16:30:35 +0200

    ARM: meson: Extend mem_map to support 3GiB of RAM
    The current mem_map definition for Meson SoCs has support for up
    to 2GiB of RAM. According to S905, S905X, S912 and S805X datasheets
    the DDR region is set from 0x00000000 to 0xBFFFFFFF, so mem_map's
    definition should be changed accordingly.
    
    It is also needed to be able to boot Khadas VIM2 board with S912
    SoC.
    
    Signed-off-by: Loic Devulder <ldevulder@suse.de>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Acked-by: Marek Vasut <marek.vasut@gmail.com>

--- arch/arm/mach-meson/board.c
+++ arch/arm/mach-meson/board.c
@@ -111,13 +111,13 @@ static struct mm_region gx_mem_map[] = {
 	{
 		.virt = 0x0UL,
 		.phys = 0x0UL,
-		.size = 0x80000000UL,
+		.size = 0xc0000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 			 PTE_BLOCK_INNER_SHARE
 	}, {
-		.virt = 0x80000000UL,
-		.phys = 0x80000000UL,
-		.size = 0x80000000UL,
+		.virt = 0xc0000000UL,
+		.phys = 0xc0000000UL,
+		.size = 0x30000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
