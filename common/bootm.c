eaac4fb296b1899369e49d941f2c0d346c7f5c7a
Alexander Graf <agraf@suse.de>
2018-09-26 14:07:41 +0200

    sandbox: Fix set_working_fdt_addr users
    When running sandbox with the new pointer sanitization we just recently
    introduced, we're running into a case with FIT images where we end up
    interpreting pointers as addresses.
    
    What happened is that most callers of set_working_fdt_addr() simply
    convert pointers into addresses without taking into account that they
    might be 2 separate address spaces. Fix the callers up to map their
    pointers into addresses.
    
    This makes sandbox tests pass for me again.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- common/bootm.c
+++ common/bootm.c
@@ -262,7 +262,7 @@ int bootm_find_images(int flag, int argc, char * const argv[])
 		puts("Could not find a valid device tree\n");
 		return 1;
 	}
-	set_working_fdt_addr((ulong)images.ft_addr);
+	set_working_fdt_addr(map_to_sysmem(images.ft_addr));
 #endif
 
 #if IMAGE_ENABLE_FIT
