510e379c49ba8e0d90960d8cbc2ffb91091e229a
Bin Meng <bmeng.cn@gmail.com>
2018-09-26 06:55:21 -0700

    riscv: Add QEMU virt board support
    This adds QEMU RISC-V 'virt' board target support, with the hope of
    helping people easily test U-Boot on RISC-V.
    
    The QEMU virt machine models a generic RISC-V virtual machine with
    support for the VirtIO standard networking and block storage devices.
    It has CLINT, PLIC, 16550A UART devices in addition to VirtIO and
    it also uses device-tree to pass configuration information to guest
    software. It implements RISC-V privileged architecture spec v1.10.
    
    Both 32-bit and 64-bit builds are supported. Support is pretty much
    preliminary, only booting to U-Boot shell with the UART driver on
    a single core. Booting Linux is not supported yet.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>

---
+++ arch/riscv/cpu/qemu/cpu.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2018, Bin Meng <bmeng.cn@gmail.com>
+ */
+
+#include <common.h>
+#include <command.h>
+
+/*
+ * cleanup_before_linux() is called just before we call linux
+ * it prepares the processor for linux
+ *
+ * we disable interrupt and caches.
+ */
+int cleanup_before_linux(void)
+{
+	disable_interrupts();
+
+	/* turn off I/D-cache */
+
+	return 0;
+}
+
+int do_reset(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	printf("reset unsupported yet\n");
+
+	return 0;
+}
---
+++ include/configs/qemu-riscv.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2018, Bin Meng <bmeng.cn@gmail.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SDRAM_BASE + SZ_2M)
+
+#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE + SZ_2M)
+
+#define CONFIG_SYS_MALLOC_LEN		SZ_8M
+
+/* Environment options */
+#define CONFIG_ENV_SIZE			SZ_4K
+
+#endif /* __CONFIG_H */
