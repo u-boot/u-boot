362612df53284e72ca7945aaf29d37c14a1ad3ca
Patrice Chotard <patrice.chotard@st.com>
2018-10-02 09:03:10 +0200

    mach-stm32: Set MPU SDRAM size to 512MB for STM32F7/H7
    This allows to boot all STM32F7 and STM32H7 boards independently
    of the amount of embedded SDRAM.
    
    Signed-off-by: Patrice Chotard <patrice.chotard@st.com>
    Reviewed-by: Vikas Manocha <vikas.manocha@st.com>

--- arch/arm/mach-stm32/soc.c
+++ arch/arm/mach-stm32/soc.c
@@ -21,14 +21,9 @@ int arch_cpu_init(void)
 		O_I_WB_RD_WR_ALLOC, REGION_16MB },
 #endif
 
-#if defined(CONFIG_STM32F7)
+#if defined(CONFIG_STM32F7) || defined(CONFIG_STM32H7)
 		{ 0xC0000000, REGION_0, XN_DIS, PRIV_RW_USR_RW,
-		O_I_WB_RD_WR_ALLOC, REGION_16MB },
-#endif
-
-#if defined(CONFIG_STM32H7)
-		{ 0xD0000000, REGION_0, XN_DIS, PRIV_RW_USR_RW,
-		O_I_WB_RD_WR_ALLOC, REGION_32MB },
+		O_I_WB_RD_WR_ALLOC, REGION_512MB },
 #endif
 	};
 
