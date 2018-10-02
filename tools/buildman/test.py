4b4bc06ef4e37f79604ff8fa2142057e2cb05437
Simon Glass <sjg@chromium.org>
2018-10-01 21:12:43 -0600

    buildman: dtoc: Suppress unwanted output from test
    There are a few test cases which print output. Suppress this so that tests
    can run silently in the normal case.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/buildman/test.py
+++ tools/buildman/test.py
@@ -20,6 +20,7 @@ import control
 import command
 import commit
 import terminal
+import test_util
 import toolchain
 
 use_network = True
@@ -422,7 +423,8 @@ class TestBuild(unittest.TestCase):
     def testToolchainDownload(self):
         """Test that we can download toolchains"""
         if use_network:
-            url = self.toolchains.LocateArchUrl('arm')
+            with test_util.capture_sys_output() as (stdout, stderr):
+                url = self.toolchains.LocateArchUrl('arm')
             self.assertRegexpMatches(url, 'https://www.kernel.org/pub/tools/'
                     'crosstool/files/bin/x86_64/.*/'
                     'x86_64-gcc-.*-nolibc_arm-.*linux-gnueabi.tar.xz')
