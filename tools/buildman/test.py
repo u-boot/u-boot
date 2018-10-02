da753e35cbd9a7fdad2e9be13a9642fb1d9af968
Simon Glass <sjg@chromium.org>
2018-10-01 21:12:35 -0600

    buildman: Make the toolchain test more forgiving
    The filenames of the toolchains on kernel.org changes every now and then.
    Fix it for the current change, and make the test use a regex so that it
    has a better chance of passing with future changes too.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/buildman/test.py
+++ tools/buildman/test.py
@@ -422,8 +422,10 @@ class TestBuild(unittest.TestCase):
     def testToolchainDownload(self):
         """Test that we can download toolchains"""
         if use_network:
-            self.assertEqual('https://www.kernel.org/pub/tools/crosstool/files/bin/x86_64/4.9.0/x86_64-gcc-4.9.0-nolibc_arm-unknown-linux-gnueabi.tar.xz',
-                self.toolchains.LocateArchUrl('arm'))
+            url = self.toolchains.LocateArchUrl('arm')
+            self.assertRegexpMatches(url, 'https://www.kernel.org/pub/tools/'
+                    'crosstool/files/bin/x86_64/.*/'
+                    'x86_64-gcc-.*-nolibc_arm-.*linux-gnueabi.tar.xz')
 
 
 if __name__ == "__main__":
