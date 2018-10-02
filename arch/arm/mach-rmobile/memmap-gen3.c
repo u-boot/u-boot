c1ec34763811d2465c030bfa2fa8e240b36d60ce
Marek Vasut <marek.vasut+renesas@gmail.com>
2018-10-02 21:42:49 +0200

    ARM: rmobile: Mark 4-64GiB as DRAM on Gen3
    Mark area 0x1_0000_0000 - 0x10_0000_0000 as DRAM on Gen3 as the
    chip is capable of addressing that and U-Boot can make use of it.
    This patch prevents exception when accessing those areas.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- arch/arm/mach-rmobile/memmap-gen3.c
+++ arch/arm/mach-rmobile/memmap-gen3.c
@@ -30,6 +30,12 @@ static struct mm_region gen3_mem_map[] = {
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	}, {
+		.virt = 0x100000000UL,
+		.phys = 0x100000000UL,
+		.size = 0xf00000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
 		/* List terminator */
 		0,
 	}
