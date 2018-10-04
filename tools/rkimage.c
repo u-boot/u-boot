f25c1755a715d0d1794bd0827549367c20182501
Daniel Gröber <daniel@dps.uibk.ac.at>
2018-10-04 15:32:42 +0200

    rockchip: Fix rkimage format for SPL boot over USB
    The 'rkimage' format used for booting rockchip boards over USB seems to
    have been broken since commit 7bf274b9caab ("rockchip: mkimage: use
    imagename to select spl hdr & spl size"). That commit adds an offset of
    RK_SPL_HDR_START(=2048) to the location the 'RKxx' header is written
    at. However the bootrom expects this header to be the first four bytes of
    the image, not at offset 2048. This appears to have been a copy paste
    error since the 'rksd' and 'rkspi' image types do require this offset.
    
    Furthermore commit 111bcc4fb6cb ("rockchip: mkimage: pad the header to
    8-bytes (using a 'nop') for RK3399"), commit 3d54eabcafec9 ("rockchip:
    spl: RK3399: use boot0 hook to create space for SPL magic") and
    commit 308277569229 ("rockchip: mkimage: update rkimage to support
    pre-padded payloads") changed the way the space for the 'RKxx' header is
    allocated and written to the image without adjusting 'rkimage'.
    
    This commit fixes those mistakes and makes it possible to load u-boot SPL
    over USB once more.
    
    (Tested on RK3399)
    
    Signed-off-by: Daniel Gröber <daniel@dps.uibk.ac.at>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- tools/rkimage.c
+++ tools/rkimage.c
@@ -15,8 +15,7 @@ static uint32_t header;
 static void rkimage_set_header(void *buf, struct stat *sbuf, int ifd,
 			       struct image_tool_params *params)
 {
-	memcpy(buf + RK_SPL_HDR_START, rkcommon_get_spl_hdr(params),
-	       RK_SPL_HDR_SIZE);
+	memcpy(buf, rkcommon_get_spl_hdr(params), RK_SPL_HDR_SIZE);
 
 	if (rkcommon_need_rc4_spl(params))
 		rkcommon_rc4_encode_spl(buf, 4, params->file_size);
@@ -36,7 +35,7 @@ static int rkimage_check_image_type(uint8_t type)
 U_BOOT_IMAGE_TYPE(
 	rkimage,
 	"Rockchip Boot Image support",
-	4,
+	0,
 	&header,
 	rkcommon_check_params,
 	NULL,
