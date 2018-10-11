5fcae59741d95f946b45e0c48dfaeb16f3b617ae
Peng Ma <peng.ma@nxp.com>
2018-10-11 10:34:19 +0000

    scsi: ceva: add ls1046a soc support
    Add ahci compatible support for ls1046a soc.
    
    Signed-off-by: Peng Ma <peng.ma@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- drivers/ata/sata_ceva.c
+++ drivers/ata/sata_ceva.c
@@ -99,6 +99,7 @@ enum ceva_soc {
 	CEVA_LS1012A,
 	CEVA_LS1021A,
 	CEVA_LS1043A,
+	CEVA_LS1046A,
 };
 
 struct ceva_sata_priv {
@@ -138,6 +139,7 @@ static int ceva_init_sata(struct ceva_sata_priv *priv)
 
 	case CEVA_LS1012A:
 	case CEVA_LS1043A:
+	case CEVA_LS1046A:
 		writel(ECC_DIS_ADDR_CH2, ECC_DIS_VAL_CH2);
 		writel(CEVA_PHY1_CFG, base + AHCI_VEND_PPCFG);
 		writel(CEVA_TRANS_CFG, base + AHCI_VEND_PTC);
@@ -170,6 +172,7 @@ static const struct udevice_id sata_ceva_ids[] = {
 	{ .compatible = "fsl,ls1012a-ahci", .data = CEVA_LS1012A },
 	{ .compatible = "fsl,ls1021a-ahci", .data = CEVA_LS1021A },
 	{ .compatible = "fsl,ls1043a-ahci", .data = CEVA_LS1043A },
+	{ .compatible = "fsl,ls1046a-ahci", .data = CEVA_LS1046A },
 	{ }
 };
 
