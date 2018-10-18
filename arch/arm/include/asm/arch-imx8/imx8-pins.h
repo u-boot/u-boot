0468be6e7c51296e8f3dcc4ba0bad84a204cc91a
Peng Fan <peng.fan@nxp.com>
2018-10-18 14:28:15 +0200

    imx8: pins: include i.MX8QXP pin header when CONFIG_IMX8QXP defined
    Include i.MX8QXP pin header when CONFIG_IMX8QXP defined,
    if no SoC macro defined, report error.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Anatolij Gustschin <agust@denx.de>
    Cc: Stefano Babic <sbabic@denx.de>

---
+++ arch/arm/include/asm/arch-imx8/imx8-pins.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2018 NXP
+ */
+
+#ifndef __ASM_ARCH_IMX8_PINS_H__
+#define __ASM_ARCH_IMX8_PINS_H__
+
+#if defined(CONFIG_IMX8QXP)
+#include <dt-bindings/pinctrl/pads-imx8qxp.h>
+#else
+#error "No pin header"
+#endif
+
+#endif	/* __ASM_ARCH_IMX8_PINS_H__ */
