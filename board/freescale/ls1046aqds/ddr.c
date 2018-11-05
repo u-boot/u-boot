50e2d41f6c9b252ec00b3ed33264d091db7ed479
Rajesh Bhagat <rajesh.bhagat@nxp.com>
2018-11-05 18:02:40 +0000

    armv8: ls1046aqds: Add TFABOOT support
    TFABOOT support includes:
     - ls1046aqds_tfa_defconfig to be loaded by trusted firmware
     - environment address and size changes for TFABOOT
     - FMAN address changes for TFABOOT
     - define BOOTCOMMAND for TFABOOT
    
    Signed-off-by: Pankit Garg <pankit.garg@nxp.com>
    Signed-off-by: Vinitha V Pillai <vinitha.pillai@nxp.com>
    Signed-off-by: Rajesh Bhagat <rajesh.bhagat@nxp.com>
    Reviewed-by: York Sun <york.sun@nxp.com>

--- board/freescale/ls1046aqds/ddr.c
+++ board/freescale/ls1046aqds/ddr.c
@@ -92,6 +92,16 @@ found:
 	popts->cpo_sample = 0x70;
 }
 
+#ifdef CONFIG_TFABOOT
+int fsl_initdram(void)
+{
+	gd->ram_size = tfa_get_dram_size();
+	if (!gd->ram_size)
+		gd->ram_size = fsl_ddr_sdram_size();
+
+	return 0;
+}
+#else
 int fsl_initdram(void)
 {
 	phys_size_t dram_size;
@@ -116,3 +126,4 @@ int fsl_initdram(void)
 
 	return 0;
 }
+#endif
