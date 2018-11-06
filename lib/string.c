96794a3eaee9bf368efe475f0a040d0e08b09dc9
Simon Glass <sjg@chromium.org>
2018-11-06 15:21:38 -0700

    string: Include the config header
    At present the config header is not included in this file, but it does use
    a CONFIG option. Fix it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- lib/string.c
+++ lib/string.c
@@ -15,6 +15,7 @@
  *    reentrant and should be faster). Use only strsep() in new code, please.
  */
 
+#include <config.h>
 #include <linux/types.h>
 #include <linux/string.h>
 #include <linux/ctype.h>
