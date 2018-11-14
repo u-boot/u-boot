3deb1f731d9b122c86c246a7ec53bcd1d67af66f
Jagan Teki <jagan@amarulasolutions.com>
2018-11-14 15:28:05 +0530

    spi: pl022: Simplify platdata code
    pl022 spi driver support both OF_CONTROL and PLATDATA, this
    patch is trying to simplify the code that differentiating
    platdata vs of_control.
    - Move OF_CONTROL code at one place
    - Handle clock setup code directly in pl022_spi_ofdata_to_platdata
    
    Acked-by: Quentin Schulz <quentin.schulz@bootlin.com>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- include/dm/platform_data/pl022_spi.h
+++ include/dm/platform_data/pl022_spi.h
@@ -10,19 +10,12 @@
 #ifndef __PL022_SPI_H__
 #define __PL022_SPI_H__
 
-#if !CONFIG_IS_ENABLED(OF_PLATDATA)
-#include <clk.h>
-#endif
 #include <fdtdec.h>
 
 struct pl022_spi_pdata {
 	fdt_addr_t addr;
 	fdt_size_t size;
-#if !CONFIG_IS_ENABLED(OF_PLATDATA)
-	struct clk clk;
-#else
 	unsigned int freq;
-#endif
 };
 
 #endif
