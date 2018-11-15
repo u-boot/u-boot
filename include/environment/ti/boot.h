1e4fb783b32437049baed460b3b899f3f5df77a5
Sam Protsenko <semen.protsenko@linaro.org>
2018-11-15 22:26:17 +0200

    arm: ti: boot: Fix U-Boot environment partition address
    Recent removal of obsolete partition led to shifting of starting address
    of "reserved" partition (which reflects U-Boot environment). Fix its
    start address to keep it in sync with ENV_OFFSET and DFU environment.
    
    This patch also provides fixed start address for "reserved" partition,
    so that we can track when it's shifted next time.
    
    While at it, move it before "misc" partition to keep all Android
    partitions together.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>
    Acked-By: Praneeth Bajjuri <praneeth@ti.com>

--- include/environment/ti/boot.h
+++ include/environment/ti/boot.h
@@ -34,9 +34,9 @@
 	"partitions_android=" \
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=xloader,start=128K,size=256K,uuid=${uuid_gpt_xloader};" \
-	"name=bootloader,size=1792K,uuid=${uuid_gpt_bootloader};" \
+	"name=bootloader,size=2048K,uuid=${uuid_gpt_bootloader};" \
+	"name=reserved,start=2432K,size=256K,uuid=${uuid_gpt_reserved};" \
 	"name=misc,size=128K,uuid=${uuid_gpt_misc};" \
-	"name=reserved,size=256K,uuid=${uuid_gpt_reserved};" \
 	"name=efs,size=16M,uuid=${uuid_gpt_efs};" \
 	"name=crypto,size=16K,uuid=${uuid_gpt_crypto};" \
 	"name=recovery,size=40M,uuid=${uuid_gpt_recovery};" \
