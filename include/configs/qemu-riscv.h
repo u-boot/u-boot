111ab36fb66f3de92d2c12c5c1cfef30ef7a507c
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2018-11-22 11:26:34 +0100

    riscv: qemu: enable distro boot
    Enable distro boot on the qemu-riscv32/64 boards. Supported boot target
    devices are VirtIO and DHCP.
    
    Signed-off-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Alexander Graf <agraf@suse.de>

--- include/configs/qemu-riscv.h
+++ include/configs/qemu-riscv.h
@@ -20,8 +20,20 @@
 /* Environment options */
 #define CONFIG_ENV_SIZE			SZ_4K
 
+#define BOOT_TARGET_DEVICES(func) \
+	func(VIRTIO, virtio, 0) \
+	func(DHCP, dhcp, na)
+
+#include <config_distro_bootcmd.h>
+
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"fdt_high=0xffffffffffffffff\0" \
-	"initrd_high=0xffffffffffffffff\0"
+	"initrd_high=0xffffffffffffffff\0" \
+	"kernel_addr_r=0x81000000\0" \
+	"fdt_addr_r=0x82000000\0" \
+	"scriptaddr=0x82100000\0" \
+	"pxefile_addr_r=0x82200000\0" \
+	"ramdisk_addr_r=0x82300000\0" \
+	BOOTENV
 
 #endif /* __CONFIG_H */
