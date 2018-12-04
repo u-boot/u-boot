b0b1768e1b138204ab07d46cbe7d24d3770f0a12
Simon Goldschmidt <simon.k.r.goldschmidt@gmail.com>
2018-12-04 21:30:08 +0100

    test: hexdump: fix misplaced return
    One of the hexdump tests in test/lib/hexdump.c returns right at the
    start of the function without testing anything.
    
    Fix this by moving the 'return 0;' statement to the end of the function.
    
    Signed-off-by: Simon Goldschmidt <simon.k.r.goldschmidt@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- test/lib/hexdump.c
+++ test/lib/hexdump.c
@@ -11,8 +11,6 @@
 
 static int lib_test_hex_to_bin(struct unit_test_state *uts)
 {
-	return 0;
-
 	ut_asserteq(0x0, hex_to_bin('0'));
 	ut_asserteq(0x1, hex_to_bin('1'));
 	ut_asserteq(0x2, hex_to_bin('2'));
@@ -30,6 +28,8 @@ static int lib_test_hex_to_bin(struct unit_test_state *uts)
 	ut_asserteq(0xe, hex_to_bin('e'));
 	ut_asserteq(0xf, hex_to_bin('f'));
 	ut_asserteq(-1, hex_to_bin('g'));
+
+	return 0;
 }
 
 DM_TEST(lib_test_hex_to_bin, DM_TESTF_SCAN_PDATA | DM_TESTF_SCAN_FDT);
