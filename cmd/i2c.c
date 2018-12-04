19f8c4dfb6e744a31da59bdd23b24d144152f1dc
Christoph Muellner <christoph.muellner@theobroma-systems.com>
2018-12-04 11:27:18 +0100

    cmd: i2c: Fix help output of i2c command.
    In case SYS_I2C or DM_I2C are defined, then the "i2c " prefix
    of the "i2c crc32" command is missing.
    This patch addresses this, so that users can't get confused
    by the "crc32" command.
    
    Without the patch we get
    
        => i2c help
        i2c - I2C sub-system
    
        Usage:
        i2c bus [muxtype:muxaddr:muxchannel] - show I2C bus info
        crc32 chip address[.0, .1, .2] count - compute CRC32 checksum
        i2c dev [dev] - show or set current I2C bus
        [...]
    
    With the patch we get
    
        => i2c help
        i2c - I2C sub-system
    
        Usage:
        i2c bus [muxtype:muxaddr:muxchannel] - show I2C bus info
        i2c crc32 chip address[.0, .1, .2] count - compute CRC32 checksum
        i2c dev [dev] - show or set current I2C bus
        ...
    
    Signed-off-by: Christoph Muellner <christoph.muellner@theobroma-systems.com>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: Heiko Schocher <hs@denx.de>

--- cmd/i2c.c
+++ cmd/i2c.c
@@ -2023,6 +2023,7 @@ static int do_i2c(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 static char i2c_help_text[] =
 #if defined(CONFIG_SYS_I2C) || defined(CONFIG_DM_I2C)
 	"bus [muxtype:muxaddr:muxchannel] - show I2C bus info\n"
+	"i2c " /* That's the prefix for the crc32 command below. */
 #endif
 	"crc32 chip address[.0, .1, .2] count - compute CRC32 checksum\n"
 #if defined(CONFIG_SYS_I2C) || \
