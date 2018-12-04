16b6aefafdabe1c8733e876f02f54cdaaad5f409
Aditya Prayoga <aditya@kobol.io>
2018-12-05 00:57:23 +0800

    arm: mvebu: helios4: Reduce U-Boot image size
    Remove unused I2C support in SPL and use simple_malloc functions to
    reduce SPL image size.
    Since Helios4 does not have any PCIe allocated on SerDes, remove PCI
    support. MTD layer on top of SPI flash is not needed, remove it also.
    
    Signed-off-by: Aditya Prayoga <aditya@kobol.io>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Tested-By: Dennis Gilmore <dgilmore@redhat.com>
    Reviewed-By: Dennis Gilmore <dgilmore@redhat.com>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/helios4.h
+++ include/configs/helios4.h
@@ -54,12 +54,6 @@
 #define CONFIG_PHY_MARVELL		/* there is a marvell phy */
 #define PHY_ANEG_TIMEOUT	8000	/* PHY needs a longer aneg time */
 
-/* PCIe support */
-#ifndef CONFIG_SPL_BUILD
-#define CONFIG_PCI_MVEBU
-#define CONFIG_PCI_SCAN_SHOW
-#endif
-
 /* Keep device tree and initrd in lower memory so the kernel can access them */
 #define RELOCATION_LIMITS_ENV_SETTINGS	\
 	"fdt_high=0x10000000\0"		\
