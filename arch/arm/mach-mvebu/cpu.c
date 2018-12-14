1670a154f5f4845c3dc646fd505cd01083ad6683
Chris Packham <judge.packham@gmail.com>
2018-12-14 16:27:57 +1300

    ARM: mvebu: remove out of date comment
    The Marvell DDR3 training code is now part of the U-Boot SPL so the
    comment saying it needs porting is no longer correct.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- arch/arm/mach-mvebu/cpu.c
+++ arch/arm/mach-mvebu/cpu.c
@@ -283,10 +283,8 @@ int print_cpuinfo(void)
  * and sets the correct windows sizes and base addresses accordingly.
  *
  * These values are set in the scratch registers by the Marvell
- * DDR3 training code, which is executed by the BootROM before the
- * main payload (U-Boot) is executed. This training code is currently
- * only available in the Marvell U-Boot version. It needs to be
- * ported to mainline U-Boot SPL at some point.
+ * DDR3 training code, which is executed by the SPL before the
+ * main payload (U-Boot) is executed.
  */
 static void update_sdram_window_sizes(void)
 {
