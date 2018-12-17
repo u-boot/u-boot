eef05fd3ba68220156f33ffe6a9e68e42a6a5b53
Andre Przywara <andre.przywara@arm.com>
2018-12-17 10:05:45 +0000

    mmc: bring back partition init for non-DM MMC drivers
    Commit d0851c893706 ("blk: Call part_init() in the post_probe() method")
    removed the call to part_init() in mmc.c, as this is done by the DM_MMC
    framework.
    However Allwinner is (still) relying on a non-DM MMC driver, so we are
    now missing the implicit partition init, leading to failing MMC accesses
    due to the missing partition information.
    
    Bring the call back just for non-DM MMC driver to fix this regression.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested-by: Soeren Moch <smoch@web.de>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -2449,6 +2449,10 @@ static int mmc_startup(struct mmc *mmc)
 	bdesc->revision[0] = 0;
 #endif
 
+#if !defined(CONFIG_DM_MMC) && (!defined(CONFIG_SPL_BUILD) || defined(CONFIG_SPL_LIBDISK_SUPPORT))
+	part_init(bdesc);
+#endif
+
 	return 0;
 }
 
