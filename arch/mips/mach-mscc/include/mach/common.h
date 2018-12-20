3098ade229af721c8b64b423e2e11f5896b45710
Lars Povlsen <lars.povlsen@microchip.com>
2018-12-20 09:56:02 +0100

    mips: mscc: Add generic PHY MIIM utility functions
    The PHY MIIM utility functions can/will be used for board detection
    purposes.
    
    Signed-off-by: Lars Povlsen <lars.povlsen@microchip.com>

--- arch/mips/mach-mscc/include/mach/common.h
+++ arch/mips/mach-mscc/include/mach/common.h
@@ -9,10 +9,12 @@
 #if defined(CONFIG_SOC_OCELOT)
 #include <mach/ocelot/ocelot.h>
 #include <mach/ocelot/ocelot_devcpu_gcb.h>
+#include <mach/ocelot/ocelot_devcpu_gcb_miim_regs.h>
 #include <mach/ocelot/ocelot_icpu_cfg.h>
 #elif defined(CONFIG_SOC_LUTON)
 #include <mach/luton/luton.h>
 #include <mach/luton/luton_devcpu_gcb.h>
+#include <mach/luton/luton_devcpu_gcb_miim_regs.h>
 #include <mach/luton/luton_icpu_cfg.h>
 #else
 #error Unsupported platform
@@ -24,5 +26,23 @@
 #define MSCC_FLASH_TO	MSCC_MEMCTL1_TO	/* Flash base offset */
 
 #define VCOREIII_TIMER_DIVIDER 25	/* Clock tick ~ 0.1 us */
+
+/* Common utility functions */
+
+int mscc_phy_rd_wr(u8 read,
+		   u32 miim_controller,
+		   u8 miim_addr,
+		   u8 addr,
+		   u16 *value);
+
+int mscc_phy_rd(u32 miim_controller,
+		u8 miim_addr,
+		u8 addr,
+		u16 *value);
+
+int mscc_phy_wr(u32 miim_controller,
+		u8 miim_addr,
+		u8 addr,
+		u16 value);
 
 #endif				/* __ASM_MACH_COMMON_H */
