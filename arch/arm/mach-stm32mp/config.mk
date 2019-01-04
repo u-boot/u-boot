29e1a64c06da729418629ca1df8ed11bb34f71b9
Sean Nyekjær <Sean.Nyekjaer@prevas.dk>
2019-01-04 11:37:24 +0000

    arm: stm32mp1: deploy spl in root folder
    Deploy u-boot-spl.stm32 binary in u-boot root folder like
    the rest of the boards.
    This makes it more streamlined when building in Yocto, Buildroot etc..
    
    Signed-off-by: Sean Nyekjaer <sean.nyekjaer@prevas.dk>

--- arch/arm/mach-stm32mp/config.mk
+++ arch/arm/mach-stm32mp/config.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018, STMicroelectronics - All Rights Reserved
 #
 
-ALL-$(CONFIG_SPL_BUILD) += spl/u-boot-spl.stm32
+ALL-$(CONFIG_SPL_BUILD) += u-boot-spl.stm32
 
 MKIMAGEFLAGS_u-boot-spl.stm32 = -T stm32image -a $(CONFIG_SPL_TEXT_BASE) -e $(CONFIG_SPL_TEXT_BASE)
 
-spl/u-boot-spl.stm32: MKIMAGEOUTPUT = spl/u-boot-spl.stm32.log
+u-boot-spl.stm32: MKIMAGEOUTPUT = u-boot-spl.stm32.log
 
-spl/u-boot-spl.stm32: spl/u-boot-spl.bin FORCE
+u-boot-spl.stm32: spl/u-boot-spl.bin FORCE
 	$(call if_changed,mkimage)
