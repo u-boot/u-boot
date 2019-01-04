35e3d2e8a30321195e4f5cd8aafba6aefe818b3d
Sam Protsenko <semen.protsenko@linaro.org>
2019-01-04 22:09:08 +0200

    arm: ti: boot: Remove legacy Android partitions
    Remove unused Android partitions:
      - efs, crypto, cache: we don't use it anymore (images are not built
        in AOSP
      - ipu1, ipu2: IPU firmware is now a part of vendor image and doesn't
        reside as a separate partition
    
    While at it, rename "reserved" partition to "uboot-env", as it's
    actually stores U-Boot environment.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

--- include/environment/ti/boot.h
+++ include/environment/ti/boot.h
@@ -35,17 +35,12 @@
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=xloader,start=128K,size=256K,uuid=${uuid_gpt_xloader};" \
 	"name=bootloader,size=2048K,uuid=${uuid_gpt_bootloader};" \
-	"name=reserved,start=2432K,size=256K,uuid=${uuid_gpt_reserved};" \
+	"name=uboot-env,start=2432K,size=256K,uuid=${uuid_gpt_reserved};" \
 	"name=misc,size=128K,uuid=${uuid_gpt_misc};" \
-	"name=efs,size=16M,uuid=${uuid_gpt_efs};" \
-	"name=crypto,size=16K,uuid=${uuid_gpt_crypto};" \
 	"name=recovery,size=40M,uuid=${uuid_gpt_recovery};" \
 	"name=boot,size=10M,uuid=${uuid_gpt_boot};" \
 	"name=system,size=1024M,uuid=${uuid_gpt_system};" \
 	"name=vendor,size=256M,uuid=${uuid_gpt_vendor};" \
-	"name=cache,size=256M,uuid=${uuid_gpt_cache};" \
-	"name=ipu1,size=1M,uuid=${uuid_gpt_ipu1};" \
-	"name=ipu2,size=1M,uuid=${uuid_gpt_ipu2};" \
 	VBMETA_PART \
 	"name=userdata,size=-,uuid=${uuid_gpt_userdata}"
 #endif /* PARTS_DEFAULT */
