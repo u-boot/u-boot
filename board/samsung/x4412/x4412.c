b1d4971082468475e9a4be502e4dfc2102fbf4ac
chasinglulu <wangkartx@gmail.com>
2019-01-12 19:40:25 +0800

    reset usb3503 after port power on

--- board/samsung/x4412/x4412.c
+++ board/samsung/x4412/x4412.c
@@ -27,6 +27,37 @@ int board_usb_init(int index, enum usb_init_type init)
 	return 0;
 }
 
+void usb_hub_reset_devices(struct usb_hub_device *hub, int port)
+{
+    struct udevice *dev = hub->pusb_dev->dev;
+
+    if(usb_hub_is_root_hub(dev) == false)
+        return;
+
+    if (port == 2) {
+        struct gpio_desc desc;
+        int ret;
+        ret = dm_gpio_lookup_name("gpc00", &desc);
+        if (ret < 0) {
+            printf("Failed to find out gpc00\n");
+            return;
+        }
+        ret = dm_gpio_request(&desc, "usb3503_reset");
+        if (ret < 0 && ret != -EBUSY) {
+            printf("%s: Failed to request gpc00\n", __func__);
+            return;
+        }
+        ret = dm_gpio_set_dir_flags(&desc, GPIOD_IS_OUT|GPIOD_IS_OUT_ACTIVE);
+        if (ret < 0) {
+            printf("gpio dir\n");
+            return;
+        }
+        udelay(100);
+        dm_gpio_set_value(&desc, 0);
+        udelay(5000);
+    }
+}
+
 #ifdef CONFIG_BOARD_EARLY_INIT_F
 int exynos_early_init_f(void)
 {
