0fd1359c5a9ba79a6bb408859e3376242a513ae2
Andrew F. Davis <afd@ti.com>
2019-01-17 13:43:05 -0600

    ARM: mach-omap2: Kconfig: Allow OMAP5 devices to set entry point
    Like AM33xx and AM43xx, DRA7xx and AM57xx devices may need to
    have an non-standard boot address in memory. This may be due
    to the device being a high security variant, which place the
    Initial SoftWare (ISW) after certificates and secure software.
    
    Allow these devices to set this from Kconfig.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>

--- include/configs/ti_omap5_common.h
+++ include/configs/ti_omap5_common.h
@@ -81,7 +81,7 @@
  * RAM from address 0x40301350 (0x40300000+0x1000(reserved)+0x350(cert)).
  */
 #define TI_OMAP5_SECURE_BOOT_RESV_SRAM_SZ	0x1000
-#define CONFIG_SPL_TEXT_BASE	0x40301350
+#define CONFIG_SPL_TEXT_BASE	CONFIG_ISW_ENTRY_ADDR
 /* If no specific start address is specified then the secure EMIF
  * region will be placed at the end of the DDR space. In order to prevent
  * the main u-boot relocation from clobbering that memory and causing a
