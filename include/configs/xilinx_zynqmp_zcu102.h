6a6f9b9a11192939cd3a7e11d9fa352fb57ae33e
Michal Simek <michal.simek@xilinx.com>
2019-01-18 09:10:39 +0100

    arm64: zynqmp: Switch all platfroms to DM_I2C
    CONFIG_PCA953X is not needed because of PCA953X is integrated in gpio
    subsystem already. That's why also remove CMD_PCA953X which is only for
    this driver.
    
    zcu102/zcu104-revC/zcu106/zcu111 contain links to eeprom which stores MAC address.
    
    DM_I2C is not enabled for the whole SoC because it increase size for
    mini configurations and there is no I2C symbol present to setup
    dependencies.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp_zcu102.h
+++ include/configs/xilinx_zynqmp_zcu102.h
@@ -10,35 +10,6 @@
 #define __CONFIG_ZYNQMP_ZCU102_H
 
 #define CONFIG_ZYNQ_SDHCI1
-#define CONFIG_SYS_I2C_MAX_HOPS		1
-#define CONFIG_SYS_NUM_I2C_BUSES	18
-#define CONFIG_SYS_I2C_BUSES	{ \
-				{0, {I2C_NULL_HOP} }, \
-				{0, {{I2C_MUX_PCA9544, 0x75, 0} } }, \
-				{0, {{I2C_MUX_PCA9544, 0x75, 1} } }, \
-				{0, {{I2C_MUX_PCA9544, 0x75, 2} } }, \
-				{1, {I2C_NULL_HOP} }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 0} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 1} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 2} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 3} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x74, 4} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 0} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 1} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 2} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 3} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 4} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 5} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 6} } }, \
-				{1, {{I2C_MUX_PCA9548, 0x75, 7} } }, \
-				}
-
-#define CONFIG_PCA953X
-
-#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
-#define CONFIG_ZYNQ_EEPROM_BUS		5
-#define CONFIG_ZYNQ_GEM_EEPROM_ADDR	0x54
-
 #define CONFIG_SPD_EEPROM
 #define CONFIG_DDR_SPD
 
--- include/configs/xilinx_zynqmp_zcu104.h
+++ include/configs/xilinx_zynqmp_zcu104.h
@@ -10,23 +10,6 @@
 #define __CONFIG_ZYNQMP_ZCU104_H
 
 #define CONFIG_ZYNQ_SDHCI1
-#define CONFIG_SYS_I2C_MAX_HOPS		1
-#define CONFIG_SYS_NUM_I2C_BUSES	9
-#define CONFIG_SYS_I2C_BUSES	{ \
-				{0, {I2C_NULL_HOP} }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 0} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 1} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 2} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 3} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 4} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 5} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 6} } }, \
-				{0, {{I2C_MUX_PCA9548, 0x74, 7} } }, \
-				}
-
-#define CONFIG_PCA953X
-
-#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
 
 #include <configs/xilinx_zynqmp.h>
 
