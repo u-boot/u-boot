8a0c6aa33f0a077fee52b5b0e68190c7f8339ff0
Simon Glass <sjg@chromium.org>
2019-01-21 14:53:32 -0700

    rockchip: rk3399: Add ROCKCHIP_DEVICE_SETTINGS to set env
    Some boards use different stdio environment variables from the default.
    Provide a #define for this which can be set before including the header
    file.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>

--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -49,11 +49,16 @@
 	"kernel_addr_r=0x02080000\0" \
 	"ramdisk_addr_r=0x04000000\0"
 
+#ifndef ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS
+#endif
+
 #include <config_distro_bootcmd.h>
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	"partitions=" PARTS_DEFAULT \
+	ROCKCHIP_DEVICE_SETTINGS \
 	BOOTENV
 
 #endif
