829e8c73dd75eb49c00a6916963ec7b0b1a771c5
Michal Simek <michal.simek@xilinx.com>
2019-01-21 16:29:07 +0100

    xilinx: common: Add support for DM_I2C zynq_board_read_rom_ethaddr()
    It is much easier to point to eeprom which stores information like MAC
    address directly via DT. eeprom which contains this information is
    pointed by /chosen/xlnx,eeprom parameter.
    
    For example:
            chosen {
                    bootargs = "earlycon";
                    stdout-path = "serial0:115200n8";
    +               xlnx,eeprom = &eeprom;
            };
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- board/xilinx/common/board.c
+++ board/xilinx/common/board.c
@@ -8,6 +8,7 @@
 #include <dm/uclass.h>
 #include <i2c.h>
 
+#if !defined(CONFIG_DM_I2C)
 int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
 {
 #if defined(CONFIG_ZYNQ_GEM_EEPROM_ADDR) && \
@@ -23,3 +24,34 @@ int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
 
 	return 0;
 }
+
+#else
+int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
+{
+	int ret = -EINVAL;
+
+#if defined(CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET)
+	struct udevice *dev;
+	ofnode eeprom;
+
+	eeprom = ofnode_get_chosen_node("xlnx,eeprom");
+	if (!ofnode_valid(eeprom))
+		return -ENODEV;
+
+	debug("%s: Path to EEPROM %s\n", __func__,
+	      ofnode_get_chosen_prop("xlnx,eeprom"));
+
+	ret = uclass_get_device_by_ofnode(UCLASS_I2C_EEPROM, eeprom, &dev);
+	if (ret)
+		return ret;
+
+	ret = dm_i2c_read(dev, CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET, ethaddr, 6);
+	if (ret)
+		debug("%s: I2C EEPROM MAC address read failed\n", __func__);
+	else
+		debug("%s: I2C EEPROM MAC %pM\n", __func__, ethaddr);
+#endif
+
+	return ret;
+}
+#endif
