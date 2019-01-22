027b1134b20b253b71de306738ed8d2d3f8ddc21
Michal Simek <michal.simek@xilinx.com>
2019-01-22 15:55:46 +0100

    xilinx: common: Remove !DM_i2C code for reading mac from eeprom
    All platforms are converted to DM_I2C that's why there is no reason to
    keep this code here.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Heiko Schocher <hs@denx.de>

--- board/xilinx/common/board.c
+++ board/xilinx/common/board.c
@@ -8,24 +8,6 @@
 #include <dm/uclass.h>
 #include <i2c.h>
 
-#if !defined(CONFIG_DM_I2C)
-int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
-{
-#if defined(CONFIG_ZYNQ_GEM_EEPROM_ADDR) && \
-    defined(CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET) && \
-    defined(CONFIG_ZYNQ_EEPROM_BUS)
-	i2c_set_bus_num(CONFIG_ZYNQ_EEPROM_BUS);
-
-	if (eeprom_read(CONFIG_ZYNQ_GEM_EEPROM_ADDR,
-			CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET,
-			ethaddr, 6))
-		printf("I2C EEPROM MAC address read failed\n");
-#endif
-
-	return 0;
-}
-
-#else
 int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
 {
 	int ret = -EINVAL;
@@ -54,4 +36,3 @@ int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
 
 	return ret;
 }
-#endif
