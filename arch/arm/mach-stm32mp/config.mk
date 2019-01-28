1b35c90836e5660a37ed33581f06ebb0b36b01ad
Patrick Delaunay <patrick.delaunay@st.com>
2019-01-28 11:13:27 +0100

    arm: stm32mp1: deploy spl in root folder
    Update generation of spl binaries
    - continue to generate all SPL files in spl sub-directory
    - copy in root folder the needed file for user (YOCTO, buildroot):
      u-boot-spl.stm32
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>

--- arch/arm/mach-stm32mp/config.mk
+++ arch/arm/mach-stm32mp/config.mk
@@ -7,7 +7,10 @@ ALL-$(CONFIG_SPL_BUILD) += u-boot-spl.stm32
 
 MKIMAGEFLAGS_u-boot-spl.stm32 = -T stm32image -a $(CONFIG_SPL_TEXT_BASE) -e $(CONFIG_SPL_TEXT_BASE)
 
-u-boot-spl.stm32: MKIMAGEOUTPUT = u-boot-spl.stm32.log
+spl/u-boot-spl.stm32: MKIMAGEOUTPUT = spl/u-boot-spl.stm32.log
 
-u-boot-spl.stm32: spl/u-boot-spl.bin FORCE
+spl/u-boot-spl.stm32: spl/u-boot-spl.bin FORCE
 	$(call if_changed,mkimage)
+
+u-boot-spl.stm32 : spl/u-boot-spl.stm32
+	$(call if_changed,copy)
