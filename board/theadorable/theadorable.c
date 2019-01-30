6d9a98c58390156033e31a3a4078347bec618202
Stefan Roese <sr@denx.de>
2019-01-30 08:54:11 +0100

    video: Armada XP: Move driver to DM_VIDEO
    This patch moves the Armada XP video / LCD driver to DM_VIDEO. With this
    move, the legacy interface board_video_init() is removed from the
    theadorable board code (only user of this video driver). The support
    via DT will be added in a separate patch.
    
    This patch also enables DM_VIDEO for the theadorable board, as this is
    needed to not break git bisect'ability.
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Reviewed-by: Anatolij Gustschin <agust@denx.de>
    Acked-by: Anatolij Gustschin <agust@denx.de>

--- board/theadorable/theadorable.c
+++ board/theadorable/theadorable.c
@@ -218,22 +218,6 @@ int board_eth_init(bd_t *bis)
 }
 #endif
 
-int board_video_init(void)
-{
-	struct mvebu_lcd_info lcd_info;
-
-	/* Reserved memory area via CONFIG_SYS_MEM_TOP_HIDE */
-	lcd_info.fb_base	= gd->ram_size;
-	lcd_info.x_res		= 240;
-	lcd_info.x_fp		= 1;
-	lcd_info.x_bp		= 45;
-	lcd_info.y_res		= 320;
-	lcd_info.y_fp		= 1;
-	lcd_info.y_bp		= 3;
-
-	return mvebu_lcd_register_init(&lcd_info);
-}
-
 #ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
