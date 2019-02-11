5f40810b6606e18ebfea232b57fd32a834b891b0
T Karthik Reddy <t.karthik.reddy@xilinx.com>
2019-02-11 09:43:52 +0530

    drivers: mtd: spi: Add support for ISSI flash parts
    This patch adds support for IS25LP512M, IS25WP512M flash parts.
    
    Signed-off-by: T Karthik Reddy <t.karthik.reddy@xilinx.com>
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -79,10 +79,12 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"is25wp064d",	   INFO(0x9d7017, 0x0, 64 * 1024,   128, RD_QUAD) },
 	{"is25lp128",	   INFO(0x9d6018, 0x0, 64 * 1024,   256, 0) },
 	{"is25lp256d",	   INFO(0x9d6019, 0x0, 64 * 1024,   512, RD_QUAD) },
+	{"is25lp512",	   INFO(0x9d601a, 0x0, 64 * 1024,   1024, RD_QUAD) },
 	{"is25wp032",	   INFO(0x9d7016, 0x0, 64 * 1024,    64, RD_FULL | SECT_4K) },
 	{"is25wp064",	   INFO(0x9d7017, 0x0, 64 * 1024,   128, RD_FULL | SECT_4K) },
 	{"is25wp128",	   INFO(0x9d7018, 0x0, 64 * 1024,   256, RD_FULL | SECT_4K) },
 	{"is25wp256d",	   INFO(0x9d7019, 0x0, 64 * 1024,   512, RD_QUAD) },
+	{"is25wp512",	   INFO(0x9d701a, 0x0, 64 * 1024,   1024, RD_QUAD) },
 #endif
 #ifdef CONFIG_SPI_FLASH_MACRONIX	/* MACRONIX */
 	{"mx25l2006e",	   INFO(0xc22012, 0x0, 64 * 1024,     4, 0) },
