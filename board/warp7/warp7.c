c6235ef3cbc73a4340e08018696a37b5056948fe
Fabio Estevam <festevam@gmail.com>
2019-02-14 10:36:27 -0200

    warp7: Remove unneeded headers after DM conversion
    After DM conversion the I2C and MMC related board codes have been
    removed, so remove the corresponding header files as well.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>

--- board/warp7/warp7.c
+++ board/warp7/warp7.c
@@ -11,12 +11,8 @@
 #include <asm/gpio.h>
 #include <asm/mach-imx/hab.h>
 #include <asm/mach-imx/iomux-v3.h>
-#include <asm/mach-imx/mxc_i2c.h>
 #include <asm/io.h>
 #include <common.h>
-#include <fsl_esdhc.h>
-#include <i2c.h>
-#include <mmc.h>
 #include <asm/arch/crm_regs.h>
 #include <usb.h>
 #include <netdev.h>
