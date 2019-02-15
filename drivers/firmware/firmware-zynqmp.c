14723ed544f42c4ea8913eb151c1e9c52dd70c88
Rajan Vaja <rajan.vaja@xilinx.com>
2019-02-15 04:45:32 -0800

    zynqmp: firmware: Add Xilinx ZynqMP firmware driver
    Add simple ZynqMP firmware drive to populate child nodes under
    zynqmp_firmware DT node.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ drivers/firmware/firmware-zynqmp.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <dm.h>
+
+static const struct udevice_id zynqmp_firmware_ids[] = {
+	{ .compatible = "xlnx,zynqmp-firmware" },
+	{ }
+};
+
+U_BOOT_DRIVER(zynqmp_firmware) = {
+	.id = UCLASS_FIRMWARE,
+	.name = "zynqmp-firmware",
+	.probe = dm_scan_fdt_dev,
+	.of_match = zynqmp_firmware_ids,
+};
