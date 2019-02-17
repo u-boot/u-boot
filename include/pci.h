7d38db55f70a30eaa64f91b35720abc5f595783f
Simon Glass <sjg@chromium.org>
2019-02-16 20:24:41 -0700

    pci: Fix comment in struct pci_child_platdata
    This is platdata, not private data, so the comment is currently incorrect.
    Fix it to avoid confusion.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- include/pci.h
+++ include/pci.h
@@ -822,7 +822,7 @@ struct udevice;
  *
  * Every device on a PCI bus has this per-child data.
  *
- * It can be accessed using dev_get_parent_priv(dev) if dev->parent is a
+ * It can be accessed using dev_get_parent_platdata(dev) if dev->parent is a
  * PCI bus (i.e. UCLASS_PCI)
  *
  * @devfn:	Encoded device and function index - see PCI_DEVFN()
