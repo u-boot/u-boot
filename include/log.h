f9811e8575eb42e75c615ba9b44f0481cdb78330
Simon Glass <sjg@chromium.org>
2019-02-16 20:24:37 -0700

    log: Allow #define LOG_DEBUG to enable logging in a file
    At present it is possible to '#define DEBUG' at the top of a file which
    causes all debug() statements in that file to become active. There is
    currently no equivalent with logging, but this is a useful function.
    
    Add a LOG_DEBUG define along with documentation.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- include/log.h
+++ include/log.h
@@ -111,11 +111,16 @@ int _log(enum log_category_t cat, enum log_level_t level, const char *file,
 #endif
 
 #if CONFIG_IS_ENABLED(LOG)
+#ifdef LOG_DEBUG
+#define _LOG_DEBUG	1
+#else
+#define _LOG_DEBUG	0
+#endif
 
 /* Emit a log record if the level is less that the maximum */
 #define log(_cat, _level, _fmt, _args...) ({ \
 	int _l = _level; \
-	if (CONFIG_IS_ENABLED(LOG) && _l <= _LOG_MAX_LEVEL) \
+	if (CONFIG_IS_ENABLED(LOG) && (_l <= _LOG_MAX_LEVEL || _LOG_DEBUG)) \
 		_log((enum log_category_t)(_cat), _l, __FILE__, __LINE__, \
 		      __func__, \
 		      pr_fmt(_fmt), ##_args); \
