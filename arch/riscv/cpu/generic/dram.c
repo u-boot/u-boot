26f4fd1cb4f6808328e8eab2afe7454172fe6d2c
Anup Patel <Anup.Patel@wdc.com>
2019-02-25 08:14:30 +0000

    riscv: generic: Ensure that U-Boot runs within 4GB for 64bit systems
    On 64bit systems, the DRAM top can be easily beyond 4GB and U-Boot
    DMA mapping APIs will generate DMA addresses beyond 4GB. This
    breaks DMA programming in 32bit DMA capable devices (such as
    Cadence MACB ethernet). For example, If DRAM is more then 2GB
    on QEMU sifive_u machine then Cadence MACB ethernet stops working
    for U-Boot because it is a 32bit DMA capable device.
    
    To handle 32bit DMA capable devices on 64bit systems, we provide
    custom implementation of board_get_usable_ram_top() which ensures
    that usable ram top is not more then 4GB. This in-turn ensures
    that U-Boot always runs within 4GB hence DMA addresses generated
    by DMA mapping APIs will be within 4GB too.
    
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Alexander Graf <agraf@suse.de>
    Reviewed-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/riscv/cpu/generic/dram.c
+++ arch/riscv/cpu/generic/dram.c
@@ -5,6 +5,9 @@
 
 #include <common.h>
 #include <fdtdec.h>
+#include <linux/sizes.h>
+
+DECLARE_GLOBAL_DATA_PTR;
 
 int dram_init(void)
 {
@@ -14,4 +17,21 @@ int dram_init(void)
 int dram_init_banksize(void)
 {
 	return fdtdec_setup_memory_banksize();
+}
+
+ulong board_get_usable_ram_top(ulong total_size)
+{
+#ifdef CONFIG_64BIT
+	/*
+	 * Ensure that we run from first 4GB so that all
+	 * addresses used by U-Boot are 32bit addresses.
+	 *
+	 * This in-turn ensures that 32bit DMA capable
+	 * devices work fine because DMA mapping APIs will
+	 * provide 32bit DMA addresses only.
+	 */
+	if (gd->ram_top > SZ_4G)
+		return SZ_4G;
+#endif
+	return gd->ram_top;
 }
