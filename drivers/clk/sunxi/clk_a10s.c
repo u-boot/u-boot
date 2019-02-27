3d83c4a1d4dd7ea230d5b5c073e503dca883e409
Jagan Teki <jagan@amarulasolutions.com>
2019-02-28 00:26:49 +0530

    clk: sunxi: Implement A10 EMAC clocks
    Implement EMAC clocks via ccu_clk_gate for Allwinner A10 SoC.
    
    Which would eventually used in sunxi_emac.c driver.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/clk/sunxi/clk_a10s.c
+++ drivers/clk/sunxi/clk_a10s.c
@@ -19,6 +19,7 @@ static struct ccu_clk_gate a10s_gates[] = {
 	[CLK_AHB_MMC0]		= GATE(0x060, BIT(8)),
 	[CLK_AHB_MMC1]		= GATE(0x060, BIT(9)),
 	[CLK_AHB_MMC2]		= GATE(0x060, BIT(10)),
+	[CLK_AHB_EMAC]		= GATE(0x060, BIT(17)),
 	[CLK_AHB_SPI0]		= GATE(0x060, BIT(20)),
 	[CLK_AHB_SPI1]		= GATE(0x060, BIT(21)),
 	[CLK_AHB_SPI2]		= GATE(0x060, BIT(22)),
