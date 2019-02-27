41cdb28549d21855bb79d06c5ce6dec5da4a50e1
Jagan Teki <jagan@amarulasolutions.com>
2019-02-28 00:26:56 +0530

    test/dm: reset: Add reset_get_by_index[_nodev] test
    Add sample dm reset test for reset_get_by_index and
    reset_get_by_index_nodev functionality code.
    
    Cc: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- test/dm/reset.c
+++ test/dm/reset.c
@@ -5,6 +5,7 @@
 
 #include <common.h>
 #include <dm.h>
+#include <reset.h>
 #include <dm/test.h>
 #include <asm/reset.h>
 #include <test/ut.h>
@@ -14,6 +15,28 @@
 
 /* This is the other reset phandle specifier handled by bulk */
 #define OTHER_RESET_ID 2
+
+/* Base test of the reset uclass */
+static int dm_test_reset_base(struct unit_test_state *uts)
+{
+	struct udevice *dev;
+	struct reset_ctl reset_method1;
+	struct reset_ctl reset_method2;
+
+	/* Get the device using the reset device */
+	ut_assertok(uclass_get_device_by_name(UCLASS_MISC, "reset-ctl-test",
+					      &dev));
+
+	/* Get the same reset port in 2 different ways and compare */
+	ut_assertok(reset_get_by_index(dev, 1, &reset_method1));
+	ut_assertok(reset_get_by_index_nodev(dev_ofnode(dev), 1,
+					     &reset_method2));
+	ut_asserteq(reset_method1.id, reset_method2.id);
+
+	return 0;
+}
+
+DM_TEST(dm_test_reset_base, DM_TESTF_SCAN_FDT);
 
 static int dm_test_reset(struct unit_test_state *uts)
 {
