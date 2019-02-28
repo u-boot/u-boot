15b7e91f5cbd9e9518879aba8c7c702df205dab1
Fabio Estevam <festevam@gmail.com>
2019-02-28 08:46:14 -0300

    imx8mq_evk: Move CONFIG_ENV_IS_IN_MMC to Kconfig
    Currently the command "saveenv" is not available.
    
    The CONFIG_ENV_IS_IN_MMC symbol has been converted to Kconfig,
    so fix the problem by moving it to the defconfig.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- include/configs/imx8mq_evk.h
+++ include/configs/imx8mq_evk.h
@@ -187,7 +187,6 @@
 #define CONFIG_ENV_OVERWRITE
 #define CONFIG_ENV_OFFSET               (64 * SZ_64K)
 #define CONFIG_ENV_SIZE			0x1000
-#define CONFIG_ENV_IS_IN_MMC
 #define CONFIG_SYS_MMC_ENV_DEV		1   /* USDHC2 */
 #define CONFIG_MMCROOT			"/dev/mmcblk1p2"  /* USDHC2 */
 
