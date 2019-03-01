ed784ac3822b7d7019679a41a17907296e2dadbe
Simon Goldschmidt <simon.k.r.goldschmidt@gmail.com>
2019-03-01 20:12:34 +0100

    mtd: rawnand: denali: add reset handling
    This adds reset handling to the devicetree-enabled Denali NAND driver.
    
    For backwards compatibility, only a warning is printed when failing to
    get reset handles.
    
    Signed-off-by: Simon Goldschmidt <simon.k.r.goldschmidt@gmail.com>

--- drivers/mtd/nand/raw/denali.h
+++ drivers/mtd/nand/raw/denali.h
@@ -10,6 +10,7 @@
 #include <linux/bitops.h>
 #include <linux/mtd/rawnand.h>
 #include <linux/types.h>
+#include <reset.h>
 
 #define DEVICE_RESET				0x0
 #define     DEVICE_RESET__BANK(bank)			BIT(bank)
@@ -315,6 +316,7 @@ struct denali_nand_info {
 	void (*host_write)(struct denali_nand_info *denali, u32 addr, u32 data);
 	void (*setup_dma)(struct denali_nand_info *denali, dma_addr_t dma_addr,
 			  int page, int write);
+	struct reset_ctl_bulk resets;
 };
 
 #define DENALI_CAP_HW_ECC_FIXUP			BIT(0)
