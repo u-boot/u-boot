d96e7ed802fcef019a22292cdef3f2fab75fe199
Philippe Reynes <philippe.reynes@softathome.com>
2019-03-07 11:36:36 +0100

    gpio: do not include <asm/arch/gpio.h> on ARCH_BCM6858
    As no gpio.h is defined for this architecture, to avoid
    compilation failure, do not include <asm/arch/gpio.h> for
    arch bcm6858.
    
    Signed-off-by: Philippe Reynes <philippe.reynes@softathome.com>

--- arch/arm/include/asm/gpio.h
+++ arch/arm/include/asm/gpio.h
@@ -1,5 +1,5 @@
 #if !defined(CONFIG_ARCH_UNIPHIER) && !defined(CONFIG_ARCH_STI) && \
-	!defined(CONFIG_ARCH_K3)
+	!defined(CONFIG_ARCH_K3) && !defined(CONFIG_ARCH_BCM6858)
 #include <asm/arch/gpio.h>
 #endif
 #include <asm-generic/gpio.h>
