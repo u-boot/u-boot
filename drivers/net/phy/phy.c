4b04f472971e1a31f71d84b2eb9390a949819b70
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2019-03-15 17:46:47 +0530

    net: phy: Define init routine and register generic phy driver
    This patch define init routine for generic phy driver and registers it
    using phy_register as this generic phy driver also needs to be relocated
    incase of manual reloc.
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -458,6 +458,11 @@ static struct phy_driver genphy_driver = {
 	.shutdown	= genphy_shutdown,
 };
 
+int genphy_init(void)
+{
+	return phy_register(&genphy_driver);
+}
+
 static LIST_HEAD(phy_drivers);
 
 int phy_init(void)
@@ -543,6 +548,8 @@ int phy_init(void)
 #ifdef CONFIG_PHY_XILINX_GMII2RGMII
 	phy_xilinx_gmii2rgmii_init();
 #endif
+	genphy_init();
+
 	return 0;
 }
 
