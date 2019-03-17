fa33f08fd657b8568ede929df8a79bd113e9c5b1
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2019-03-17 19:28:32 +0100

    riscv: add infrastructure for calling functions on other harts
    Harts on RISC-V boot independently, U-Boot is responsible for managing
    them. Functions are called on other harts with smp_call_function(),
    which sends inter-processor interrupts (IPIs) to all other available
    harts. Available harts are those marked as available in the device tree
    and present in the available_harts mask stored in global data. The
    available_harts mask is used to register all harts that have entered
    U-Boot. Functions are specified with their address and two function
    arguments (argument 2 and 3). The first function argument is always the
    hart ID of the hart calling the function. On the other harts, the IPI
    interrupt handler handle_ipi() must be called on software interrupts to
    handle the request and call the specified function.
    
    Functions are stored in the ipi_data data structure. Every hart has its
    own data structure in global data. While this is not required at the
    moment (all harts are expected to boot Linux), this does allow future
    expansion, where other harts may be used for monitoring or other tasks.
    
    Signed-off-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>
    Reviewed-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Tested-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/riscv/include/asm/global_data.h
+++ arch/riscv/include/asm/global_data.h
@@ -10,12 +10,18 @@
 #ifndef	__ASM_GBL_DATA_H
 #define __ASM_GBL_DATA_H
 
+#include <asm/smp.h>
+
 /* Architecture-specific global data */
 struct arch_global_data {
 	long boot_hart;		/* boot hart id */
 #ifdef CONFIG_SIFIVE_CLINT
 	void __iomem *clint;	/* clint base address */
 #endif
+#ifdef CONFIG_SMP
+	struct ipi_data ipi[CONFIG_NR_CPUS];
+#endif
+	ulong available_harts;
 };
 
 #include <asm-generic/global_data.h>
