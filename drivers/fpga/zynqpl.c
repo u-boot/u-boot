31f7ce7f9bfd41c996b33f7568718462bbfe5b45
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2019-03-23 16:01:36 +0530

    arm: zynq: Add an info message about post config
    Post configuration cant be run at u-boot as u-boot
    didn't has any info about the design.So,this patch
    adds an info message that post config was not run
    and needs to be run manually if needed.
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/fpga/zynqpl.c
+++ drivers/fpga/zynqpl.c
@@ -408,6 +408,8 @@ static int zynq_load(xilinx_desc *desc, const void *buf, size_t bsize,
 	if (bstype != BIT_PARTIAL)
 		zynq_slcr_devcfg_enable();
 
+	puts("INFO:post config was not run, please run manually if needed\n");
+
 	return FPGA_SUCCESS;
 }
 
