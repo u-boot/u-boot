aaf29c99b4445c5449d845938b9c32fbb8d59a9a
Michal Simek <michal.simek@xilinx.com>
2019-04-08 13:43:51 +0200

    arm64: versal: Add missing dt-bindings headers for clk and power
    Add missing files.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/dt-bindings/power/xlnx-versal-power.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Copyright (C) 2019 Xilinx, Inc.
+ */
+
+#ifndef _DT_BINDINGS_VERSAL_POWER_H
+#define _DT_BINDINGS_VERSAL_POWER_H
+
+#define PD_SWDT_FPD		0x18224029
+#define PD_USB_0		0x18224018
+#define PD_GEM_0		0x18224019
+#define PD_GEM_1		0x1822401a
+#define PD_SPI_0		0x1822401b
+#define PD_SPI_1		0x1822401c
+#define PD_I2C_0		0x1822401d
+#define PD_I2C_1		0x1822401e
+#define PD_CAN_FD_0		0x1822401f
+#define PD_CAN_FD_1		0x18224020
+#define PD_UART_0		0x18224021
+#define PD_UART_1		0x18224022
+#define PD_GPIO			0x18224023
+#define PD_TTC_0		0x18224024
+#define PD_TTC_1		0x18224025
+#define PD_TTC_2		0x18224026
+#define PD_TTC_3		0x18224027
+#define PD_OSPI			0x1822402a
+#define PD_QSPI			0x1822402b
+#define PD_SDIO_0		0x1822402e
+#define PD_SDIO_1		0x1822402f
+#define PD_RTC			0x18224034
+#define PD_ADMA_0		0x18224035
+#define PD_ADMA_1		0x18224036
+#define PD_ADMA_2		0x18224037
+#define PD_ADMA_3		0x18224038
+#define PD_ADMA_4		0x18224039
+#define PD_ADMA_5		0x1822403a
+#define PD_ADMA_6		0x1822403b
+#define PD_ADMA_7		0x1822403c
+
+#endif
