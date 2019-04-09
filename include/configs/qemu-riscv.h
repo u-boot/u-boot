ac12c61909273be0806afae42963f426925ad111
David Abdurachmanov <david.abdurachmanov@gmail.com>
2019-04-09 12:42:46 +0200

    riscv: set CONFIG_SYS_BOOTM_LEN to SZ_64M
    After updating Fedora/RISCV kernel to 5.1-rc3+ the size increased above
    the current threshold. Looking into HiKey, Dragonboards, etc. seems that
    SZ_64M is a popular option.
    
    This sucessfully boots Fedora/RISCV with 5.1-rc3+ kernel on QEMU 4.0 (master)
    with OpenSBI -> U-Boot (S-mode) [extlinux] -> Kernel setup.
    
    Signed-off-by: David Abdurachmanov <david.abdurachmanov@gmail.com>
    Reviewed-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- include/configs/qemu-riscv.h
+++ include/configs/qemu-riscv.h
@@ -15,7 +15,7 @@
 
 #define CONFIG_SYS_MALLOC_LEN		SZ_8M
 
-#define CONFIG_SYS_BOOTM_LEN		SZ_16M
+#define CONFIG_SYS_BOOTM_LEN		SZ_64M
 
 #define CONFIG_STANDALONE_LOAD_ADDR	0x80200000
 
@@ -41,11 +41,11 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"fdt_high=0xffffffffffffffff\0" \
 	"initrd_high=0xffffffffffffffff\0" \
-	"kernel_addr_r=0x81000000\0" \
-	"fdt_addr_r=0x82000000\0" \
-	"scriptaddr=0x82100000\0" \
-	"pxefile_addr_r=0x82200000\0" \
-	"ramdisk_addr_r=0x82300000\0" \
+	"kernel_addr_r=0x84000000\0" \
+	"fdt_addr_r=0x88000000\0" \
+	"scriptaddr=0x88100000\0" \
+	"pxefile_addr_r=0x88200000\0" \
+	"ramdisk_addr_r=0x88300000\0" \
 	BOOTENV
 
 #endif /* __CONFIG_H */
