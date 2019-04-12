30b9a28a3f2dc599dc9c6d6d6c015fb89bed95bf
Neil Armstrong <narmstrong@baylibre.com>
2019-04-12 11:50:10 +0200

    mtd: spi-nor-ids: Add Gigadevice gd25lq128 ID
    This adds support for the Gigadevice gd25lq128 ID needed on the
    upcoming libretech-ac board.
    
    SPI_NOR_QUAD_READ is not set since it has not been tested in Quad
    mode.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Reviewed-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -106,6 +106,11 @@ const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
+	{
+		INFO("gd25lq128", 0xc86018, 0, 64 * 1024, 256,
+			SECT_4K | SPI_NOR_DUAL_READ |
+			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
+	},
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	/* ISSI */
