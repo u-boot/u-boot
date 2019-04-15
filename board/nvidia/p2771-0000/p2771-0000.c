8317189737a93753f634e0f14d984226b7a2da4c
Thierry Reding <treding@nvidia.com>
2019-04-15 11:32:34 +0200

    p2771-0000: Pass Ethernet MAC to the kernel
    Pass the ethernet MAC address to the kernel upon boot. This passes both
    the local-mac-address property (as passed to U-Boot from cboot) and the
    currently set MAC address via the mac-address property. The latter will
    only be set if it is different from the address that was already passed
    via the local-mac-address property.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- board/nvidia/p2771-0000/p2771-0000.c
+++ board/nvidia/p2771-0000/p2771-0000.c
@@ -4,7 +4,10 @@
  */
 
 #include <common.h>
+#include <environment.h>
 #include <i2c.h>
+#include <linux/libfdt.h>
+#include <asm/arch-tegra/cboot.h>
 #include "../p2571/max77620_init.h"
 
 void pin_mux_mmc(void)
@@ -52,3 +55,43 @@ int tegra_pcie_board_init(void)
 	return 0;
 }
 #endif
+
+int ft_board_setup(void *fdt, bd_t *bd)
+{
+	const void *cboot_fdt = (const void *)cboot_boot_x0;
+	uint8_t mac[ETH_ALEN], local_mac[ETH_ALEN];
+	const char *path;
+	int offset, err;
+
+	err = cboot_get_ethaddr(cboot_fdt, local_mac);
+	if (err < 0)
+		memset(local_mac, 0, ETH_ALEN);
+
+	path = fdt_get_alias(fdt, "ethernet");
+	if (!path)
+		return 0;
+
+	debug("ethernet alias found: %s\n", path);
+
+	offset = fdt_path_offset(fdt, path);
+	if (offset < 0)
+		return 0;
+
+	if (is_valid_ethaddr(local_mac)) {
+		err = fdt_setprop(fdt, offset, "local-mac-address", local_mac,
+				  ETH_ALEN);
+		if (!err)
+			debug("Local MAC address set: %pM\n", local_mac);
+	}
+
+	if (eth_env_get_enetaddr("ethaddr", mac)) {
+		if (memcmp(local_mac, mac, ETH_ALEN) != 0) {
+			err = fdt_setprop(fdt, offset, "mac-address", mac,
+					  ETH_ALEN);
+			if (!err)
+				debug("MAC address set: %pM\n", mac);
+		}
+	}
+
+	return 0;
+}
