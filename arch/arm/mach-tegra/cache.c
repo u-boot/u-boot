9e57819294a1b2b247f66a6dbfa380137e166ec5
Thierry Reding <treding@nvidia.com>
2019-04-15 11:32:19 +0200

    ARM: tegra: Guard GP pad control code with a Kconfig symbol
    The GP pad control code is not relevant on all Tegra SoC generations, so
    guard it with a Kconfig symbol that can be selected by the generations
    that need it.
    
    This is in preparation for unifying Tegra186 code with the code used on
    older generations.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Tom Warren <twarren@nvidia.com>

--- arch/arm/mach-tegra/cache.c
+++ arch/arm/mach-tegra/cache.c
@@ -8,7 +8,9 @@
 #include <common.h>
 #include <asm/io.h>
 #include <asm/arch-tegra/ap.h>
+#if IS_ENABLED(CONFIG_TEGRA_GP_PADCTRL)
 #include <asm/arch/gp_padctrl.h>
+#endif
 
 #ifndef CONFIG_ARM64
 void config_cache(void)
