9e03632c1f955f2cdb35cce18ef2470d033e7a82
Fabio Estevam <festevam@gmail.com>
2019-05-08 09:02:07 -0300

    imx8qm_mek: Remove unneeded config options settings
    Remove such unneeded custom config options because the
    provided default values are good enough.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>

--- include/configs/imx8qm_mek.h
+++ include/configs/imx8qm_mek.h
@@ -155,15 +155,6 @@
 /* Serial */
 #define CONFIG_BAUDRATE			115200
 
-/* Monitor Command Prompt */
-#define CONFIG_HUSH_PARSER
-#define CONFIG_SYS_PROMPT_HUSH_PS2     "> "
-#define CONFIG_SYS_CBSIZE              2048
-#define CONFIG_SYS_MAXARGS             64
-#define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
-#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
-					sizeof(CONFIG_SYS_PROMPT) + 16)
-
 /* Generic Timer Definitions */
 #define COUNTER_FREQUENCY		8000000	/* 8MHz */
 
