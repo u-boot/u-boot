0ab85fe68f34ce06d8baed0f56dd7cd7c6d426f2
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2019-05-14 13:33:46 +0200

    opos6ul: set REFSEL and REFR fields
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>

--- arch/arm/mach-imx/mx6/opos6ul.c
+++ arch/arm/mach-imx/mx6/opos6ul.c
@@ -192,6 +192,8 @@ struct mx6_ddr_sysinfo ddr_sysinfo = {
 	.sde_to_rst = 0x10,	/* 14 cycles, 200us (JEDEC default) */
 	.rst_to_cke = 0x23,	/* 33 cycles, 500us (JEDEC default) */
 	.ddr_type = DDR_TYPE_DDR3,
+	.refsel = 1,		/* Refresh cycles at 32KHz */
+	.refr = 7,		/* 8 refreshes commands per refresh cycle */
 };
 
 static struct mx6_ddr3_cfg mem_ddr = {
