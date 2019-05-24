422237ee5953927ba54946448d7f525a301dbec1
Marek Behún <marek.behun@nic.cz>
2019-05-24 14:57:51 +0200

    arm: mvebu: turris_omnia: remove unneeded macro from board config
    This is not needed here since Omnia is using DM_PCI now.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/turris_omnia.h
+++ include/configs/turris_omnia.h
@@ -28,11 +28,6 @@
 
 #define PHY_ANEG_TIMEOUT	8000	/* PHY needs a longer aneg time */
 
-/* PCIe support */
-#ifndef CONFIG_SPL_BUILD
-#define CONFIG_PCI_SCAN_SHOW
-#endif
-
 /* Keep device tree and initrd in lower memory so the kernel can access them */
 #define RELOCATION_LIMITS_ENV_SETTINGS	\
 	"fdt_high=0x10000000\0"		\
