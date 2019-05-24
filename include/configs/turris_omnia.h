305a82217de391bbadce1dc5faac508daa11beaa
Marek Behún <marek.behun@nic.cz>
2019-05-24 14:57:50 +0200

    arm: mvebu: turris_omnia: change environment address in SPI flash
    The U-Boot partition is 1 MiB and environment is 64 KiB. It does not
    make sense to have environment at 0xc0000 when it could be at 0xf0000
    and we can have more space for U-Boot binary.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Stefan Roese <sr@denx.de>

--- include/configs/turris_omnia.h
+++ include/configs/turris_omnia.h
@@ -22,9 +22,9 @@
 #define CONFIG_EHCI_IS_TDI
 
 /* Environment in SPI NOR flash */
-#define CONFIG_ENV_OFFSET		(3*(1 << 18)) /* 768KiB in */
 #define CONFIG_ENV_SIZE			(64 << 10) /* 64KiB */
-#define CONFIG_ENV_SECT_SIZE		(256 << 10) /* 256KiB sectors */
+#define CONFIG_ENV_OFFSET		((1 << 20) - CONFIG_ENV_SIZE)
+#define CONFIG_ENV_SECT_SIZE		(64 << 10) /* 64KiB */
 
 #define PHY_ANEG_TIMEOUT	8000	/* PHY needs a longer aneg time */
 
