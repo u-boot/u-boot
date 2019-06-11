4db6e790bab644f105fcacc98744286ac461b588
Yinbo Zhu <yinbo.zhu@nxp.com>
2019-06-11 14:29:03 +0800

    net: phy: cortina: Use block layer to read from mmc
    This patch is to use block layer to read from mmc in cortina
    
    Signed-off-by: Yinbo Zhu <yinbo.zhu@nxp.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- drivers/net/phy/cortina.c
+++ drivers/net/phy/cortina.c
@@ -176,8 +176,13 @@ void cs4340_upload_firmware(struct phy_device *phydev)
 		printf("MMC read: dev # %u, block # %u, count %u ...\n",
 		       dev, blk, cnt);
 		mmc_init(mmc);
+#ifdef CONFIG_BLK
+		(void)blk_dread(mmc_get_blk_desc(mmc), blk, cnt,
+						addr);
+#else
 		(void)mmc->block_dev.block_read(&mmc->block_dev, blk, cnt,
 						addr);
+#endif
 	}
 #endif
 
