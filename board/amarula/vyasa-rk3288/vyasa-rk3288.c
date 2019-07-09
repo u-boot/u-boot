35dfc0496d38e57a3c49d055b79a259a0585f44b
Kever Yang <kever.yang@rock-chips.com>
2019-07-09 21:58:55 +0800

    rockchip: board: rk3288: remove board_boot_order()
    Prefer to use dts config instead, remove all board_boot_order()
    for rk3288 boards.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- board/amarula/vyasa-rk3288/vyasa-rk3288.c
+++ board/amarula/vyasa-rk3288/vyasa-rk3288.c
@@ -6,14 +6,6 @@
 #include <common.h>
 
 #ifndef CONFIG_TPL_BUILD
-#include <spl.h>
-
-void board_boot_order(u32 *spl_boot_list)
-{
-	/* eMMC prior to sdcard. */
-	spl_boot_list[0] = BOOT_DEVICE_MMC2;
-	spl_boot_list[1] = BOOT_DEVICE_MMC1;
-}
 
 int spl_start_uboot(void)
 {
