535b1fdb8e5e857b9d2747e5f1a1501ec85f327c
Ye Li <ye.li@nxp.com>
2019-07-10 09:23:51 +0000

    spi: spi-mem: Fix read data size issue
    When slave drivers don't set the max_read_size, the spi-mem should directly
    use data.nbytes and not limit to any size. But current logic will limit to
    the max_write_size.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Vignesh Raghavendra <vigneshr@ti.com>
    Reviewed-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/spi/spi-mem.c
+++ drivers/spi/spi-mem.c
@@ -430,12 +430,14 @@ int spi_mem_adjust_op_size(struct spi_slave *slave, struct spi_mem_op *op)
 		if (slave->max_write_size && len > slave->max_write_size)
 			return -EINVAL;
 
-		if (op->data.dir == SPI_MEM_DATA_IN && slave->max_read_size)
-			op->data.nbytes = min(op->data.nbytes,
+		if (op->data.dir == SPI_MEM_DATA_IN) {
+			if (slave->max_read_size)
+				op->data.nbytes = min(op->data.nbytes,
 					      slave->max_read_size);
-		else if (slave->max_write_size)
+		} else if (slave->max_write_size) {
 			op->data.nbytes = min(op->data.nbytes,
 					      slave->max_write_size - len);
+		}
 
 		if (!op->data.nbytes)
 			return -EINVAL;
