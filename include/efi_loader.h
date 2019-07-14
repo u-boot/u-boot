ade317a281652354cb3d86d40a8c012b32f28497
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-14 18:51:00 +0200

    efi_loader: avoid alignment error
    In packed structures GUIDs are not aligned. Avoid a build error with
    GCC 9.1 by using const void * as argument for guidcmp().
    
    Reported-by: Ramon Fried <rfried.dev@gmail.com>
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- include/efi_loader.h
+++ include/efi_loader.h
@@ -567,7 +567,7 @@ static inline void ascii2unicode(u16 *unicode, const char *ascii)
 	*unicode = 0;
 }
 
-static inline int guidcmp(const efi_guid_t *g1, const efi_guid_t *g2)
+static inline int guidcmp(const void *g1, const void *g2)
 {
 	return memcmp(g1, g2, sizeof(efi_guid_t));
 }
