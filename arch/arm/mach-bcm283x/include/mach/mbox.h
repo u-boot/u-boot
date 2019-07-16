49822442edd910efdf12eb8070a9c9cc7c48037d
Fabian Vogt <fvogt@suse.com>
2019-07-16 13:09:47 +0200

    ARM: bcm283x mbox: Fix send status register
    Before we can send a message to the mailbox we have to check that there
    is space to do so. Therefore we poll the status register. But up to now
    the wrong status register, the one of mailbox 0, was checked. Fix this
    by polling the status regiser of mailbox 1.
    
    Signed-off-by: Fabian Vogt <fvogt@suse.com>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    [mb: rename registers and update commit message]
    Signed-off-by: Matthias Brugger <mbrugger@suse.com>

--- arch/arm/mach-bcm283x/include/mach/mbox.h
+++ arch/arm/mach-bcm283x/include/mach/mbox.h
@@ -46,9 +46,12 @@
 struct bcm2835_mbox_regs {
 	u32 read;
 	u32 rsvd0[5];
-	u32 status;
-	u32 config;
+	u32 mail0_status;
+	u32 mail0_config;
 	u32 write;
+	u32 rsvd1[5];
+	u32 mail1_status;
+	u32 mail1_config;
 };
 
 #define BCM2835_MBOX_STATUS_WR_FULL	0x80000000
