e766b2ea0aa887366f510074349b766eb5be98d6
Bin Meng <bmeng.cn@gmail.com>
2019-07-16 09:39:20 -0700

    doc: Add the U-Boot logo to the html doc
    Now we have a logo for U-Boot, we can include it in the Sphinx
    html doc.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -170,7 +170,7 @@ except ImportError:
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '../tools/logos/u-boot_logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
