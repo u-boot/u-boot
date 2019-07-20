95a0f3c6919e5586c23e41df46d7d41e401f13bb
Simon Glass <sjg@chromium.org>
2019-07-20 12:24:00 -0600

    binman: Adjust fmap to ignore CBFS files
    The FMAP is not intended to show the files inside a CBFS. The FMAP can be
    used to locate the CBFS itself, but then the CBFS must be read to find out
    what is in it.
    
    Update the FMAP to work this way and add some debugging while we are here.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/etype/fmap.py
+++ tools/binman/etype/fmap.py
@@ -8,6 +8,8 @@
 from entry import Entry
 import fmap_util
 import tools
+from tools import ToHexSize
+import tout
 
 
 class Entry_fmap(Entry):
@@ -26,7 +28,8 @@ class Entry_fmap(Entry):
 
     When used, this entry will be populated with an FMAP which reflects the
     entries in the current image. Note that any hierarchy is squashed, since
-    FMAP does not support this.
+    FMAP does not support this. Also, CBFS entries appear as a single entry -
+    the sub-entries are ignored.
     """
     def __init__(self, section, etype, node):
         Entry.__init__(self, section, etype, node)
@@ -39,7 +42,9 @@ class Entry_fmap(Entry):
         """
         def _AddEntries(areas, entry):
             entries = entry.GetEntries()
-            if entries:
+            tout.Debug("fmap: Add entry '%s' type '%s' (%s subentries)" %
+                       (entry.GetPath(), entry.etype, ToHexSize(entries)))
+            if entries and entry.etype != 'cbfs':
                 for subentry in entries.values():
                     _AddEntries(areas, subentry)
             else:
