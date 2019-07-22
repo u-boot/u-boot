3b3c623e61319ae268c5ccb55078931161bc719e
Kever Yang <kever.yang@rock-chips.com>
2019-07-22 19:59:17 +0800

    rockchip: rk3188: add boot_devices mapping to support 'same-as-spl'
    The driver need a mapping to get the dts node by boot srouce ID.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/mach-rockchip/rk3188/rk3188.c
+++ arch/arm/mach-rockchip/rk3188/rk3188.c
@@ -4,10 +4,16 @@
  */
 #include <common.h>
 #include <asm/io.h>
+#include <asm/arch-rockchip/bootrom.h>
 #include <asm/arch-rockchip/grf_rk3188.h>
 #include <asm/arch-rockchip/hardware.h>
 
 #define GRF_BASE	0x20008000
+
+const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
+	[BROM_BOOTSOURCE_EMMC] = "dwmmc@1021c000",
+	[BROM_BOOTSOURCE_SD] = "dwmmc@10214000",
+};
 
 #ifdef CONFIG_DEBUG_UART_BOARD_INIT
 void board_debug_uart_init(void)
