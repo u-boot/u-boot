544293f87850b415721690533dae2d27f77f44f3
Park, Aiden <aiden.park@intel.com>
2019-08-03 08:30:12 +0000

    x86: Add new slimbootloader CPU type
    This slimbootloader CPU type is to enable U-Boot as a payload which
    runs on top of Slim Bootloader (https://github.com/slimbootloader).
    The Slim Bootloader is designed with multi-stage architecture for
    the execution from reset vector to OS booting, and supports QEMU,
    Apollolake, Whiskeylake and Coffeelake platforms consuming Intel
    FSP (https://github.com/IntelFsp) for silicon initialization
    including CAR and memory initialization.
    The Slim Bootloader generates new HOB (Hand Off Block) which are
    serial port info, memory map info, performance data info and so on,
    and passes it to a Payload. U-Boot as a payload will use these HOB
    information for basic initialization such as serial console.
    
    As an initial commit,
    - Add CONFIG_SYS_SLIMBOOTLOADER to enable slimbootloader CPU type
    - Add new arch/x86/cpu/slimbootloader directory with minimum codes
    - Get hob_list pointer from Slim Bootloader
    
    Signed-off-by: Aiden Park <aiden.park@intel.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Tested-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ arch/x86/cpu/slimbootloader/slimbootloader.c
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2019 Intel Corporation <www.intel.com>
+ */
+
+#include <common.h>
+
+int arch_cpu_init(void)
+{
+	return x86_cpu_init_f();
+}
+
+int checkcpu(void)
+{
+	return 0;
+}
+
+int print_cpuinfo(void)
+{
+	return default_print_cpuinfo();
+}
