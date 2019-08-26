ed2ebd8629618b6964fd4bcceba3d69fe139063f
Peng Fan <peng.fan@nxp.com>
2019-08-26 08:11:53 +0000

    imx8qm: correct fdt_file
    imx8qm-mek.dtb is the correct fdt file, not fsl-imx8qxp-mek.dtb
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- include/configs/imx8qm_mek.h
+++ include/configs/imx8qm_mek.h
@@ -64,7 +64,7 @@
 	"fdt_addr=0x83000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
 	"boot_fdt=try\0" \
-	"fdt_file=fsl-imx8qxp-mek.dtb\0" \
+	"fdt_file=imx8qm-mek.dtb\0" \
 	"initrd_addr=0x83800000\0"		\
 	"initrd_high=0xffffffffffffffff\0" \
 	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
