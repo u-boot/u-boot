1c7cba9fa70c7ce5113afdf78f132d49c99e9ed2
Peng Fan <peng.fan@nxp.com>
2019-08-27 06:25:23 +0000

    imx: add i.MX8MM PE property
    i.MX8MM does not have LVTTL, it has a PE property
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/include/asm/mach-imx/iomux-v3.h
+++ arch/arm/include/asm/mach-imx/iomux-v3.h
@@ -104,7 +104,11 @@ typedef u64 iomux_v3_cfg_t;
 #define PAD_CTL_ODE		(0x1 << 5)
 #define PAD_CTL_PUE		(0x1 << 6)
 #define PAD_CTL_HYS		(0x1 << 7)
+#ifdef CONFIG_IMX8MM
+#define PAD_CTL_PE		(0x1 << 8)
+#else
 #define PAD_CTL_LVTTL		(0x1 << 8)
+#endif
 
 #elif defined CONFIG_MX7
 
