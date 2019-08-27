096d5f80151f2160f6defe7b1497b9590cbcecfa
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2019-08-27 11:04:39 +0000

    dm: pcie_fsl: Add P3041 PCIe support
    Add compatible string for P3041 PCIe.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Prabhakar Kushwaha <prabhakar.kushwaha@nxp.com>

--- drivers/pci/pcie_fsl.c
+++ drivers/pci/pcie_fsl.c
@@ -631,6 +631,7 @@ static struct fsl_pcie_data t2080_data = {
 static const struct udevice_id fsl_pcie_ids[] = {
 	{ .compatible = "fsl,pcie-p1_p2", .data = (ulong)&p1_p2_data },
 	{ .compatible = "fsl,pcie-p2041", .data = (ulong)&p2041_data },
+	{ .compatible = "fsl,pcie-p3041", .data = (ulong)&p2041_data },
 	{ .compatible = "fsl,pcie-t102x", .data = (ulong)&t2080_data },
 	{ .compatible = "fsl,pcie-t104x", .data = (ulong)&t2080_data },
 	{ .compatible = "fsl,pcie-t2080", .data = (ulong)&t2080_data },
