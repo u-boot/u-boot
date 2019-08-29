aa207cf3a6d68f39d64cd29057a4fb63943e9078
liucheng (G) <liucheng32@huawei.com>
2019-08-29 13:47:48 +0000

    CVE-2019-14194/CVE-2019-14198: nfs: fix unbounded memcpy with a failed length check at nfs_read_reply
    This patch adds a check to rpc_pkt.u.reply.data at nfs_read_reply.
    
    Signed-off-by: Cheng Liu <liucheng32@huawei.com>
    Reported-by: Fermín Serna <fermin@semmle.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- net/nfs.c
+++ net/nfs.c
@@ -701,6 +701,9 @@ static int nfs_read_reply(uchar *pkt, unsigned len)
 			&(rpc_pkt.u.reply.data[4 + nfsv3_data_offset]);
 	}
 
+	if (((uchar *)&(rpc_pkt.u.reply.data[0]) - (uchar *)(&rpc_pkt) + rlen) > len)
+			return -9999;
+
 	if (store_block(data_ptr, nfs_offset, rlen))
 			return -9999;
 
