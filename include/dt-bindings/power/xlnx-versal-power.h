366c665a28fab888ed24f2628101e7e03a7b8fac
Ravi Patel <ravi.patel@xilinx.com>
2019-09-06 04:55:44 -0700

    arm64: versal: Rename macro names in dt-bindings
    To maintain compatibility with PLM and other Versal components,
    rename power domain macros to have "PM_DEV_" prefix instead of
    "PD_" prefix.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/dt-bindings/power/xlnx-versal-power.h
+++ include/dt-bindings/power/xlnx-versal-power.h
@@ -6,35 +6,35 @@
 #ifndef _DT_BINDINGS_VERSAL_POWER_H
 #define _DT_BINDINGS_VERSAL_POWER_H
 
-#define PD_SWDT_FPD		0x18224029
-#define PD_USB_0		0x18224018
-#define PD_GEM_0		0x18224019
-#define PD_GEM_1		0x1822401a
-#define PD_SPI_0		0x1822401b
-#define PD_SPI_1		0x1822401c
-#define PD_I2C_0		0x1822401d
-#define PD_I2C_1		0x1822401e
-#define PD_CAN_FD_0		0x1822401f
-#define PD_CAN_FD_1		0x18224020
-#define PD_UART_0		0x18224021
-#define PD_UART_1		0x18224022
-#define PD_GPIO			0x18224023
-#define PD_TTC_0		0x18224024
-#define PD_TTC_1		0x18224025
-#define PD_TTC_2		0x18224026
-#define PD_TTC_3		0x18224027
-#define PD_OSPI			0x1822402a
-#define PD_QSPI			0x1822402b
-#define PD_SDIO_0		0x1822402e
-#define PD_SDIO_1		0x1822402f
-#define PD_RTC			0x18224034
-#define PD_ADMA_0		0x18224035
-#define PD_ADMA_1		0x18224036
-#define PD_ADMA_2		0x18224037
-#define PD_ADMA_3		0x18224038
-#define PD_ADMA_4		0x18224039
-#define PD_ADMA_5		0x1822403a
-#define PD_ADMA_6		0x1822403b
-#define PD_ADMA_7		0x1822403c
+#define PM_DEV_USB_0				(0x18224018U)
+#define PM_DEV_GEM_0				(0x18224019U)
+#define PM_DEV_GEM_1				(0x1822401aU)
+#define PM_DEV_SPI_0				(0x1822401bU)
+#define PM_DEV_SPI_1				(0x1822401cU)
+#define PM_DEV_I2C_0				(0x1822401dU)
+#define PM_DEV_I2C_1				(0x1822401eU)
+#define PM_DEV_CAN_FD_0				(0x1822401fU)
+#define PM_DEV_CAN_FD_1				(0x18224020U)
+#define PM_DEV_UART_0				(0x18224021U)
+#define PM_DEV_UART_1				(0x18224022U)
+#define PM_DEV_GPIO				(0x18224023U)
+#define PM_DEV_TTC_0				(0x18224024U)
+#define PM_DEV_TTC_1				(0x18224025U)
+#define PM_DEV_TTC_2				(0x18224026U)
+#define PM_DEV_TTC_3				(0x18224027U)
+#define PM_DEV_SWDT_FPD				(0x18224029U)
+#define PM_DEV_OSPI				(0x1822402aU)
+#define PM_DEV_QSPI				(0x1822402bU)
+#define PM_DEV_SDIO_0				(0x1822402eU)
+#define PM_DEV_SDIO_1				(0x1822402fU)
+#define PM_DEV_RTC				(0x18224034U)
+#define PM_DEV_ADMA_0				(0x18224035U)
+#define PM_DEV_ADMA_1				(0x18224036U)
+#define PM_DEV_ADMA_2				(0x18224037U)
+#define PM_DEV_ADMA_3				(0x18224038U)
+#define PM_DEV_ADMA_4				(0x18224039U)
+#define PM_DEV_ADMA_5				(0x1822403aU)
+#define PM_DEV_ADMA_6				(0x1822403bU)
+#define PM_DEV_ADMA_7				(0x1822403cU)
 
 #endif
