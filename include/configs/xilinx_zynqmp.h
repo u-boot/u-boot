5c16c2bdb48e8a2cbec0459165d97cbd299571b2
Michal Simek <michal.simek@xilinx.com>
2019-09-10 14:39:39 +0200

    arm64: zynqmp: Remove all Xilinx private commands
    As was said in commit 5452017f209f
    ("arm64: zynqmp: Deprecate xilinx boot commands") it is time to remove all
    xilinx private commands and use distro boot only.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/configs/xilinx_zynqmp.h
+++ include/configs/xilinx_zynqmp.h
@@ -101,89 +101,6 @@
 # define PARTS_DEFAULT
 #endif
 
-/* Xilinx initial environment variables */
-#ifndef CONFIG_EXTRA_ENV_BOARD_SETTINGS
-#define CONFIG_EXTRA_ENV_BOARD_SETTINGS \
-	"kernel_addr=0x80000\0" \
-	"initrd_addr=0xa00000\0" \
-	"initrd_size=0x2000000\0" \
-	"fdt_addr=4000000\0" \
-	"fdt_high=0x10000000\0" \
-	"loadbootenv_addr=0x100000\0" \
-	"sdbootdev=0\0"\
-	"kernel_offset=0x280000\0" \
-	"fdt_offset=0x200000\0" \
-	"kernel_size=0x1e00000\0" \
-	"fdt_size=0x80000\0" \
-	"bootenv=uEnv.txt\0" \
-	"partid=auto\0" \
-	"loadbootenv=load mmc $sdbootdev:$partid ${loadbootenv_addr} ${bootenv}\0" \
-	"importbootenv=echo Importing environment from SD ...; " \
-		"env import -t ${loadbootenv_addr} $filesize\0" \
-	"sd_uEnvtxt_existence_test=test -e mmc $sdbootdev:$partid /uEnv.txt\0" \
-	"sata_root=if test $scsidevs -gt 0; then setenv bootargs $bootargs root=/dev/sda rw rootfstype=ext4; fi\0" \
-	"sataboot=run xilinxcmd && load scsi 0 80000 boot/Image && load scsi 0 $fdt_addr boot/system.dtb && booti 80000 - $fdt_addr\0" \
-	"netboot=run xilinxcmd && tftpboot 10000000 image.ub && bootm\0" \
-	"qspiboot=run xilinxcmd && sf probe 0 0 0 && sf read $fdt_addr $fdt_offset $fdt_size && " \
-		  "sf read $kernel_addr $kernel_offset $kernel_size && " \
-		  "booti $kernel_addr - $fdt_addr\0" \
-	"uenvboot=" \
-		"if run sd_uEnvtxt_existence_test; then " \
-			"run loadbootenv; " \
-			"echo Loaded environment from ${bootenv}; " \
-			"run importbootenv; " \
-		"fi; " \
-		"if test -n $uenvcmd; then " \
-			"echo Running uenvcmd ...; " \
-			"run uenvcmd; " \
-		"fi\0" \
-	"sdboot=run xilinxcmd && mmc dev $sdbootdev && mmcinfo && run uenvboot || run sdroot$sdbootdev; " \
-		"load mmc $sdbootdev:$partid $fdt_addr system.dtb && " \
-		"load mmc $sdbootdev:$partid $kernel_addr Image && " \
-		"booti $kernel_addr - $fdt_addr\0" \
-	"emmcboot=run sdboot\0" \
-	"nandboot=run xilinxcmd && nand info && nand read $fdt_addr $fdt_offset $fdt_size && " \
-		  "nand read $kernel_addr $kernel_offset $kernel_size && " \
-		  "booti $kernel_addr - $fdt_addr\0" \
-	"xen_prepare_dt=fdt addr $fdt_addr && fdt resize 128 && " \
-		"fdt set /chosen \\\\#address-cells <1> && " \
-		"fdt set /chosen \\\\#size-cells <1> && " \
-		"fdt mknod /chosen dom0 && " \
-		"fdt set /chosen/dom0 compatible \"xen,linux-zimage\" \"xen,multiboot-module\" && " \
-		"fdt set /chosen/dom0 reg <0x80000 0x$filesize> && " \
-		"fdt set /chosen xen,xen-bootargs \"console=dtuart dtuart=serial0 dom0_mem=768M bootscrub=0 maxcpus=1 timer_slop=0\" && " \
-		"fdt set /chosen xen,dom0-bootargs \"console=hvc0 earlycon=xen earlyprintk=xen maxcpus=1 clk_ignore_unused\"\0" \
-	"xen_prepare_dt_qemu=run xen_prepare_dt && " \
-		"fdt set /cpus/cpu@1 device_type \"none\" && " \
-		"fdt set /cpus/cpu@2 device_type \"none\" && " \
-		"fdt set /cpus/cpu@3 device_type \"none\" && " \
-		"fdt rm /cpus/cpu@1 compatible && " \
-		"fdt rm /cpus/cpu@2 compatible && " \
-		"fdt rm /cpus/cpu@3 compatible\0" \
-	"xen=run xilinxcmd && tftpb $fdt_addr system.dtb &&  tftpb 0x80000 Image &&" \
-		"run xen_prepare_dt && " \
-		"tftpb 6000000 xen.ub && tftpb 0x1000000 image.ub && " \
-		"bootm 6000000 0x1000000 $fdt_addr\0" \
-	"xen_qemu=run xilinxcmd && tftpb $fdt_addr system.dtb && tftpb 0x80000 Image && " \
-		"run xen_prepare_dt_qemu && " \
-		"tftpb 6000000 xen.ub && tftpb 0x1000000 image.ub && " \
-		"bootm 6000000 0x1000000 $fdt_addr\0" \
-	"jtagboot=run xilinxcmd && tftpboot 80000 Image && tftpboot $fdt_addr system.dtb && " \
-		 "tftpboot 6000000 rootfs.cpio.ub && booti 80000 6000000 $fdt_addr\0" \
-	"nosmp=setenv bootargs $bootargs maxcpus=1\0" \
-	"nfsroot=setenv bootargs $bootargs root=/dev/nfs nfsroot=$serverip:/mnt/sata,tcp ip=$ipaddr:$serverip:$serverip:255.255.255.0:zynqmp:eth0:off rw\0" \
-	"sdroot0=setenv bootargs $bootargs root=/dev/mmcblk0p2 rw rootwait\0" \
-	"sdroot1=setenv bootargs $bootargs root=/dev/mmcblk1p2 rw rootwait\0" \
-	"android=setenv bootargs $bootargs init=/init androidboot.selinux=disabled androidboot.hardware=$board\0" \
-	"android_debug=run android && setenv bootargs $bootargs video=DP-1:1024x768@60 drm.debug=0xf\0" \
-	"usb_dfu_spl=booti $kernel_addr - $fdt_addr\0" \
-	"usbhostboot=usb start && load usb 0 $fdt_addr system.dtb && " \
-		     "load usb 0 $kernel_addr Image && " \
-		     "booti $kernel_addr - $fdt_addr\0" \
-	"xilinxcmd=echo !!! && echo !!! Booting cmd is deprecated (will be removed in 2020). && echo !!! Please move to distro bootcmd. && echo !!!\0" \
-	PARTS_DEFAULT
-#endif
-
 /* Monitor Command Prompt */
 /* Console I/O Buffer Size */
 #define CONFIG_SYS_CBSIZE		2048
@@ -253,12 +170,6 @@
 # define BOOT_TARGET_DEVICES_NAND(func)
 #endif
 
-#define BOOTENV_DEV_XILINX(devtypeu, devtypel, instance) \
-	"bootcmd_xilinx=run $modeboot\0"
-
-#define BOOTENV_DEV_NAME_XILINX(devtypeu, devtypel, instance) \
-	"xilinx "
-
 #define BOOTENV_DEV_QSPI(devtypeu, devtypel, instance) \
 	"bootcmd_" #devtypel #instance "=sf probe " #instance " 0 0 && " \
 		       "sf read $scriptaddr $script_offset_f $script_size_f && " \
@@ -291,15 +202,13 @@
 	BOOT_TARGET_DEVICES_USB(func) \
 	BOOT_TARGET_DEVICES_SCSI(func) \
 	BOOT_TARGET_DEVICES_PXE(func) \
-	BOOT_TARGET_DEVICES_DHCP(func) \
-	func(XILINX, xilinx, na)
+	BOOT_TARGET_DEVICES_DHCP(func)
 
 #include <config_distro_bootcmd.h>
 
 /* Initial environment variables */
 #ifndef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	CONFIG_EXTRA_ENV_BOARD_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
 	BOOTENV \
 	DFU_ALT_INFO
