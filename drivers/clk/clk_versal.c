34192121bd5679214a45a78b98d158d12c1cf27f
Michal Simek <michal.simek@xilinx.com>
2019-09-11 10:15:16 +0200

    clk: versal: Remove additional/unused compatible string
    xlnx,versal-clkc compatible string is not used that's why it can be
    removed.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/clk/clk_versal.c
+++ drivers/clk/clk_versal.c
@@ -733,7 +733,6 @@ static struct clk_ops versal_clk_ops = {
 
 static const struct udevice_id versal_clk_ids[] = {
 	{ .compatible = "xlnx,versal-clk" },
-	{ .compatible = "xlnx,versal-clkc" },
 	{ }
 };
 
