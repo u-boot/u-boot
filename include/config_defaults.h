4b0bcfa7c4ecd5d5e02c5e7d10d128a7da53414b
Tom Rini <trini@konsulko.com>
2019-09-20 17:36:50 -0400

    Kconfig: Migrate CONFIG_BOOTM_* options
    Migrate all of the existing OS support options that are under
    CONFIG_BOOTM_* to Kconfig.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/config_defaults.h
+++ include/config_defaults.h
@@ -9,11 +9,4 @@
 #ifndef _CONFIG_DEFAULTS_H_
 #define _CONFIG_DEFAULTS_H_
 
-/* Support bootm-ing different OSes */
-#define CONFIG_BOOTM_LINUX 1
-#define CONFIG_BOOTM_NETBSD 1
-#define CONFIG_BOOTM_PLAN9 1
-#define CONFIG_BOOTM_RTEMS 1
-#define CONFIG_BOOTM_VXWORKS 1
-
 #endif
