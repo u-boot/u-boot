beaf5a6b891957141d2b0d3bb3905cd379231f15
Michal Simek <michal.simek@xilinx.com>
2019-09-25 09:11:48 +0200

    microblaze: Fix lmb memory initialization
    Microblaze as Arm is using multiple memory banks which are read from DT
    that's why there is a need to initialized LMB based on bd->bi_dram[].
    Without this fix memory base/size is all the time 0 and image relocation is
    not possible.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- common/image.c
+++ common/image.c
@@ -473,7 +473,7 @@ ulong env_get_bootm_low(void)
 
 #if defined(CONFIG_SYS_SDRAM_BASE)
 	return CONFIG_SYS_SDRAM_BASE;
-#elif defined(CONFIG_ARM)
+#elif defined(CONFIG_ARM) || defined(CONFIG_MICROBLAZE)
 	return gd->bd->bi_dram[0].start;
 #else
 	return 0;
@@ -490,7 +490,7 @@ phys_size_t env_get_bootm_size(void)
 		return tmp;
 	}
 
-#if defined(CONFIG_ARM) && defined(CONFIG_NR_DRAM_BANKS)
+#if (defined(CONFIG_ARM) || defined(CONFIG_MICROBLAZE))  && defined(CONFIG_NR_DRAM_BANKS)
 	start = gd->bd->bi_dram[0].start;
 	size = gd->bd->bi_dram[0].size;
 #else
