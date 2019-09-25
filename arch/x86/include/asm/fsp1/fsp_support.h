79b228baae06a57d0d01358e832d5ea2f2cc02d3
Simon Glass <sjg@chromium.org>
2019-09-25 08:00:19 -0600

    x86: Move fsp_bootmode.h to the generic fsp directory
    This header file is the same for FSP v1 and v2, although there may be
    some additions to come. Move it into the generic fsp directory.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Tested-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/x86/include/asm/fsp1/fsp_support.h
+++ arch/x86/include/asm/fsp1/fsp_support.h
@@ -7,12 +7,12 @@
 #ifndef __FSP_SUPPORT_H__
 #define __FSP_SUPPORT_H__
 
+#include <asm/fsp/fsp_bootmode.h>
 #include <asm/fsp/fsp_fv.h>
 #include <asm/fsp/fsp_hob.h>
 #include <asm/fsp/fsp_infoheader.h>
 #include <asm/fsp/fsp_types.h>
 #include "fsp_ffs.h"
-#include "fsp_bootmode.h"
 #include <asm/fsp_arch.h>
 #include <asm/fsp/fsp_azalia.h>
 
