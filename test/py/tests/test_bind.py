2a43dbdf9668dc9fbfc309e085eb0c7fa64c2f15
Patrick Delaunay <patrick.delaunay@st.com>
2019-09-30 10:19:13 +0200

    dm: Tidy up dump output when there are many devices
    At present the 'Index' column of 'dm tree' assumes there is
    two digits, this patch increase it to 3 digits.
    
    It also aligns output of 'dm uclass', assuming the same 3 digits index.
    
    The boards with CONFIG_PINCTRL_FULL activated have one pinconfig
    by pin configuration, so they can have more than 100 devices
    pinconfig (for example with stm32mp157c-ev1 board we have
    106 pinconfig node).
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- test/py/tests/test_bind.py
+++ test/py/tests/test_bind.py
@@ -13,7 +13,7 @@ def in_tree(response, name, uclass, drv, depth, last_child):
 	else:
 		leaf = leaf + '`'
 	leaf = leaf + '-- ' + name
-	line = (' *{:10.10}   [0-9]*  \[ [ +] \]   {:20.20}  {}$'
+	line = (' *{:10.10}    [0-9]*  \[ [ +] \]   {:20.20}  {}$'
 	        .format(uclass, drv, leaf))
 	prog = re.compile(line)
 	for l in lines:
