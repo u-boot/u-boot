f279e1d9167fc0dcc7172c8f845a253511ac6001
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-10-06 13:58:57 +0200

    lib: errno: avoid error format-overflow
    In cmd/regulator.c an error occurs with GCC 9.2.1 if CONFIG_ERRNO_STR is
    not defined:
    
    cmd/regulator.c: In function ‘failure’:
    cmd/regulator.c:20:2: error: ‘%s’ directive argument is null
    [-Werror=format-overflow=]
       20 |  printf("Error: %d (%s)\n", ret, errno_str(ret));
          |  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    In function ‘constraint’,
        inlined from ‘constraint’ at cmd/regulator.c:111:12:
    cmd/regulator.c:115:3: error: ‘%s’ directive argument is null
    [-Werror=format-overflow=]
      115 |   printf(" %s (err: %d)\n", errno_str(val), val);
          |   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    errno_str() should return a valid string instead of NULL if
    CONFIG_ERRNO_STR is not defined.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- include/errno.h
+++ include/errno.h
@@ -12,12 +12,21 @@ extern int errno;
 
 #define __set_errno(val) do { errno = val; } while (0)
 
+/**
+ * errno_str() - get description for error number
+ *
+ * @errno:	error number (negative in case of error)
+ * Return:	string describing the error. If CONFIG_ERRNO_STR is not
+ *		defined an empty string is returned.
+ */
 #ifdef CONFIG_ERRNO_STR
 const char *errno_str(int errno);
 #else
+static const char error_message[] = "";
+
 static inline const char *errno_str(int errno)
 {
-	return 0;
+	return error_message;
 }
 #endif
 #endif /* _ERRNO_H */
