22b548044b840d67bd941c5b66602ae643db21d3
Lokesh Vutla <lokeshvutla@ti.com>
2019-10-07 19:26:38 +0530

    armv7R: K3: j721e: Add support for triggering ddr init from SPL
    In SPL, DDR should be made available by the end of board_init_f()
    so that apis in board_init_r() can use ddr. Adding support for
    triggering DDR initialization from board_init_f().
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- arch/arm/mach-k3/j721e_init.c
+++ arch/arm/mach-k3/j721e_init.c
@@ -73,7 +73,7 @@ static void store_boot_index_from_rom(void)
 
 void board_init_f(ulong dummy)
 {
-#if defined(CONFIG_K3_LOAD_SYSFW)
+#if defined(CONFIG_K3_J721E_DDRSS) || defined(CONFIG_K3_LOAD_SYSFW)
 	struct udevice *dev;
 	int ret;
 #endif
@@ -116,6 +116,12 @@ void board_init_f(ulong dummy)
 #else
 	/* Prepare console output */
 	preloader_console_init();
+#endif
+
+#if defined(CONFIG_K3_J721E_DDRSS)
+	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
+	if (ret)
+		panic("DRAM init failed: %d\n", ret);
 #endif
 }
 
