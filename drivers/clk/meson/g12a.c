2168a52e2b50f72e495143f04d81d6a786a434c8
Neil Armstrong <narmstrong@baylibre.com>
2019-10-11 17:33:53 +0200

    clk: meson-sm1: add compatible
    The SM1 clock controller is almost identical to the G12A and
    so far the differences don't matter.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- drivers/clk/meson/g12a.c
+++ drivers/clk/meson/g12a.c
@@ -992,6 +992,7 @@ static struct clk_ops meson_clk_ops = {
 static const struct udevice_id meson_clk_ids[] = {
 	{ .compatible = "amlogic,g12a-clkc" },
 	{ .compatible = "amlogic,g12b-clkc" },
+	{ .compatible = "amlogic,sm1-clkc" },
 	{ }
 };
 
