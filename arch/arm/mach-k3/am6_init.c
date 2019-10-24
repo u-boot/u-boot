27380cb7e966f779f52d13f394d7aaeed9acbe44
Keerthy <j-keerthy@ti.com>
2019-10-24 15:00:52 +0530

    arm: mach-k3: am6_init: Initialize AVS class 0
    Initialize AVS class 0 so that mpu voltage rail is
    programmed to the AVS class 0 compensated value.
    
    Signed-off-by: Keerthy <j-keerthy@ti.com>

--- arch/arm/mach-k3/am6_init.c
+++ arch/arm/mach-k3/am6_init.c
@@ -116,6 +116,13 @@ void board_init_f(ulong dummy)
 	/* Perform EEPROM-based board detection */
 	do_board_detect();
 
+#if defined(CONFIG_CPU_V7R) && defined(CONFIG_K3_AVS0)
+	ret = uclass_get_device_by_driver(UCLASS_MISC, DM_GET_DRIVER(k3_avs),
+					  &dev);
+	if (ret)
+		printf("AVS init failed: %d\n", ret);
+#endif
+
 #ifdef CONFIG_K3_AM654_DDRSS
 	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
 	if (ret)
