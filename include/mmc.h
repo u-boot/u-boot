u-boot/u-boot

commit d7a950acb065f41c18cb9ed6a2f998913646c932

mmc: sdhci: Fix issue in UHS-I mode check
This patch corrects checking SD bus modes of host & card by adding
SD bus mode mask. This fixes the incorrect checking of SD card
to switch to 1.8v even though card/host does not support UHS mode.

Signed-off-by: T Karthik Reddy <t.karthik.reddy@xilinx.com>
Signed-off-by: Michal Simek <michal.simek@xilinx.com>

include/mmc.h
@@ -65,6 +65,7 @@
 #define MMC_MODE_DDR_52MHz	MMC_CAP(MMC_DDR_52)
 #define MMC_MODE_HS200		MMC_CAP(MMC_HS_200)
 #define MMC_MODE_HS400		MMC_CAP(MMC_HS_400)
+#define MMC_MODE_MASK		0x3FFF
 
 #define MMC_MODE_8BIT		BIT(30)
 #define MMC_MODE_4BIT		BIT(29)

drivers/mmc/mmc.c
@@ -1660,8 +1660,8 @@ static int sd_select_mode_and_width(struct mmc *mmc, uint card_caps)
 #if CONFIG_IS_ENABLED(MMC_UHS_SUPPORT)
 	if (!uhs_en && !(mmc_host_is_spi(mmc)) &&
 	    mmc->scr[0] & SD_SCR0_SPEC3 &&
-	    mmc->card_caps >= UHS_SDR50 &&
-	    mmc->host_caps >= UHS_SDR50) {
+	    (mmc->card_caps & MMC_MODE_MASK) >= MMC_CAP(UHS_SDR50) &&
+	    (mmc->host_caps & MMC_MODE_MASK) >= MMC_CAP(UHS_SDR50)) {
 		uhs_en = true;
 		err = mmc_set_signal_voltage(mmc, MMC_SIGNAL_VOLTAGE_180);
 		if (err)

commit 9141381a4f58364e8eb51f2e08a5be969ab9e690

mmc: Simplify MMC_MODE_MASK macro
Using MMC_MODES_END macro for mask generation.

Fixes: d7a950acb065 ("mmc: sdhci: Fix issue in UHS-I mode check")
Signed-off-by: Michal Simek <michal.simek@xilinx.com>

include/mmc.h
@@ -65,7 +65,7 @@
 #define MMC_MODE_DDR_52MHz	MMC_CAP(MMC_DDR_52)
 #define MMC_MODE_HS200		MMC_CAP(MMC_HS_200)
 #define MMC_MODE_HS400		MMC_CAP(MMC_HS_400)
-#define MMC_MODE_MASK		0x3FFF
+#define MMC_MODE_MASK		(MMC_CAP(MMC_MODES_END) - 1)
 
 #define MMC_MODE_8BIT		BIT(30)
 #define MMC_MODE_4BIT		BIT(29)