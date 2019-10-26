264977d1c2f12c8b7bcb125405a66bb05332d3c5
Ye Li <ye.li@nxp.com>
2019-10-26 16:24:03 +0200

    mach-imx: Adding new argument for SIP call interface
    Need to pass total 5 arguments for SIP HAB call on i.MX8MQ,
    so update the interface to add new argument.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    [agust: fixed imx8m-power-domain build]
    Signed-off-by: Anatolij Gustschin <agust@denx.de>
    Reviewed-by: Patrick Wildt <patrick@blueri.se>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/include/asm/mach-imx/sys_proto.h
+++ arch/arm/include/asm/mach-imx/sys_proto.h
@@ -138,7 +138,8 @@ int mxs_wait_mask_set(struct mxs_register_32 *reg, u32 mask, u32 timeout);
 int mxs_wait_mask_clr(struct mxs_register_32 *reg, u32 mask, u32 timeout);
 
 unsigned long call_imx_sip(unsigned long id, unsigned long reg0,
-			   unsigned long reg1, unsigned long reg2);
+			   unsigned long reg1, unsigned long reg2,
+			   unsigned long reg3);
 unsigned long call_imx_sip_ret2(unsigned long id, unsigned long reg0,
 				unsigned long *reg1, unsigned long reg2,
 				unsigned long reg3);
