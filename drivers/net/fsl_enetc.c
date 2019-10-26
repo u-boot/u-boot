29a6617ada631c1f86512028218491e3c15500cc
Michael Walle <michael@walle.cc>
2019-10-26 02:39:12 +0200

    drivers: net: fsl_enetc: fix RGMII configuration
    Add the missing RGMII PHY modes in which case the MAC has configure its
    RGMII settings. The only difference between these modes is the RX and
    TX delay configuration. A user might choose any RGMII mode in the device
    tree.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Reviewed-by: Alex Marginean <alexm.osslist@gmail.com>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- drivers/net/fsl_enetc.c
+++ drivers/net/fsl_enetc.c
@@ -191,6 +191,9 @@ static void enetc_start_pcs(struct udevice *dev)
 		enetc_init_sgmii(dev);
 		break;
 	case PHY_INTERFACE_MODE_RGMII:
+	case PHY_INTERFACE_MODE_RGMII_ID:
+	case PHY_INTERFACE_MODE_RGMII_RXID:
+	case PHY_INTERFACE_MODE_RGMII_TXID:
 		enetc_init_rgmii(dev);
 		break;
 	case PHY_INTERFACE_MODE_XGMII:
