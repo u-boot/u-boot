de43ca7e3ee4b71793880b540bb3b958a02e6c04
Vasily Khoruzhick <anarsoul@gmail.com>
2019-11-09 11:24:50 -0800

    rockchip: rk3399: split rockpro64 out of evb_rk3399
    rockpro64 needs to setup I/O domains in order for USB to work in u-boot.
    Since we currently don't have a driver to do that, split it into its own
    board file and initialize I/O domains here.
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

---
+++ include/configs/rockpro64_rk3399.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2019 Vasily Khoruzhick <anarsoul@gmail.com>
+ */
+
+#ifndef __ROCKPRO64_RK3399_H
+#define __ROCKPRO64_RK3399_H
+
+#include <configs/rk3399_common.h>
+
+#define CONFIG_SYS_MMC_ENV_DEV 0
+
+#define SDRAM_BANK_SIZE			(2UL << 30)
+
+#endif
