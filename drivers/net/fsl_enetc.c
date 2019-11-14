307f8a6d1f034fc23aa7a42fa121f7fa011a33e4
Alex Marginean <alexandru.marginean@nxp.com>
2019-11-14 18:58:45 +0200

    drivers: net: fsl_enetc: Add 2.5Gbps to supported link speeds
    The original code enabled link speeds up to 1Gbps, but the interface can
    go up to 2.5G, enable that speed to in PHY AN mask.
    
    Signed-off-by: Alex Marginean <alexandru.marginean@nxp.com>
    Acked-by: Joe Hershberger <joe.hershberger@ni.com>

--- drivers/net/fsl_enetc.c
+++ drivers/net/fsl_enetc.c
@@ -215,8 +215,9 @@ static void enetc_config_phy(struct udevice *dev)
 	if (!priv->phy)
 		return;
 
-	supported = GENMASK(6, 0); /* speeds up to 1G & AN */
-	priv->phy->advertising = priv->phy->supported & supported;
+	supported = PHY_GBIT_FEATURES | SUPPORTED_2500baseX_Full;
+	priv->phy->supported &= supported;
+	priv->phy->advertising &= supported;
 
 	phy_config(priv->phy);
 }
