71094b72d7113cbb2c566eac43fc122a18e5c6f9
Holger Brunck <holger.brunck@ch.abb.com>
2019-11-26 19:09:03 +0100

    ppc/kmeter1: remove unused define
    CONFIG_CONS_INDEX is nowhere used for this board, we can drop it.
    
    Signed-off-by: Holger Brunck <holger.brunck@ch.abb.com>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>
    CC: Priyanka Jain <priyanka.jain@nxp.com>
    CC: Valentin Longchamp <valentin.longchamp@ch.abb.com>

--- include/configs/kmeter1.h
+++ include/configs/kmeter1.h
@@ -16,11 +16,6 @@
 #include "km/km-mpc83xx.h"
 #include "km/km-mpc8360.h"
 
-/*
- * Serial Port
- */
-#define CONFIG_CONS_INDEX	1
-
 #define CONFIG_SYS_DDR_CS0_CONFIG	(CSCONFIG_EN | CSCONFIG_AP | \
 					 CSCONFIG_ROW_BIT_13 | \
 					 CSCONFIG_COL_BIT_10 | \
