77dd7c6854f3bd8ddc422f0cb1953071fe00dc6c
Simon Glass <sjg@chromium.org>
2019-12-06 21:41:49 -0700

    x86: timer: use a timer base of 0
    On x86 platforms the timer is reset to 0 when the SoC is reset. Having
    this as the timer base is useful since it provides an indication of how
    long it takes before U-Boot is running.
    
    When U-Boot sets the timer base to something else, time is lost and we
    no-longer have an accurate account of the time since reset. This
    particularly affects bootstage.
    
    Change the default to not read the timer base, leaving it at 0. Add an
    option for when U-Boot is the secondary bootloader.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- drivers/timer/tsc_timer.c
+++ drivers/timer/tsc_timer.c
@@ -397,7 +397,8 @@ static void tsc_timer_ensure_setup(bool early)
 {
 	if (gd->arch.tsc_inited)
 		return;
-	gd->arch.tsc_base = rdtsc();
+	if (IS_ENABLED(CONFIG_X86_TSC_READ_BASE))
+		gd->arch.tsc_base = rdtsc();
 
 	if (!gd->arch.clock_rate) {
 		unsigned long fast_calibrate;
