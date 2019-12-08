b86f6d1e649f237849297b5ec6b5566b7a92b2b4
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2019-12-08 23:28:49 +0100

    spl: opensbi: specify main hart as preferred boot hart
    OpenSBI uses a relocation lottery to determine the hart to relocate
    OpenSBI to its link address. In the U-Boot SPL boot flow, the main hart
    schedules the secondary harts to enter OpenSBI before doing so itself.
    One of the secondary harts will therefore always be the winner of the
    relocation lottery. This is problematic if the link address ranges of
    OpenSBI and U-Boot SPL overlap. OpenSBI will be relocated and therefore
    overwrite U-Boot SPL while some harts may still run it, leading to code
    corruption.
    
    Avoid this problem by specifying the main hart as the preferred boot
    hart to perform the OpenSBI relocation. The main hart will be the last
    hart to enter OpenSBI, relocation can therefore occur safely.
    
    The boot hart field was added to version 2 of the OpenSBI FW_DYNAMIC
    info structure. The header file include/opensbi.h is synchronized with
    include/sbi/fw_dynamic.h from the OpenSBI project to update the info
    structure. The header file is recent as of commit
    7a13beb21326 ("firmware: Add preferred boot HART field in struct
    fw_dynamic_info").
    
    Reported-by: Rick Chen <rick@andestech.com>
    Suggested-by: Anup Patel <Anup.Patel@wdc.com>
    Signed-off-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>
    Reviewed-by: Rick Chen <rick@andestech.com>
    Tested-by: Rick Chen <rick@andestech.com>
    Reviewed-by: Anup Patel <anup.patel@wdc.com>

--- common/spl/spl_opensbi.c
+++ common/spl/spl_opensbi.c
@@ -70,6 +70,7 @@ void spl_invoke_opensbi(struct spl_image_info *spl_image)
 	opensbi_info.next_addr = uboot_entry;
 	opensbi_info.next_mode = FW_DYNAMIC_INFO_NEXT_MODE_S;
 	opensbi_info.options = SBI_SCRATCH_NO_BOOT_PRINTS;
+	opensbi_info.boot_hart = gd->arch.boot_hart;
 
 	opensbi_entry = (void (*)(ulong, ulong, ulong))spl_image->entry_point;
 	invalidate_icache_all();
