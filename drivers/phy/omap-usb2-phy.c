779f40bf52d0456d30d21f84f754ce29abdac915
Vignesh Raghavendra <vigneshr@ti.com>
2019-12-09 10:37:31 +0530

    phy: omap-usb2-phy: Add support for AM654 USB2 PHY
    AM654 SoC has USB2 PHY which is similar to existing USB2 PHYs on OMAP
    SoCs. Add support for the same.
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- drivers/phy/omap-usb2-phy.c
+++ drivers/phy/omap-usb2-phy.c
@@ -27,6 +27,10 @@
 #define USB2PHY_DISCON_BYP_LATCH	BIT(31)
 #define USB2PHY_ANA_CONFIG1		(0x4c)
 
+#define AM654_USB2_OTG_PD		BIT(8)
+#define AM654_USB2_VBUS_DET_EN		BIT(5)
+#define AM654_USB2_VBUSVALID_DET_EN	BIT(4)
+
 DECLARE_GLOBAL_DATA_PTR;
 
 struct omap_usb2_phy {
@@ -74,6 +78,15 @@ static const struct usb_phy_data am437x_usb2_data = {
 	.power_off = AM437X_USB2_PHY_PD | AM437X_USB2_OTG_PD,
 };
 
+static const struct usb_phy_data am654_usb2_data = {
+	.label = "am654_usb2",
+	.flags = OMAP_USB2_CALIBRATE_FALSE_DISCONNECT,
+	.mask = AM654_USB2_OTG_PD | AM654_USB2_VBUS_DET_EN |
+		AM654_USB2_VBUSVALID_DET_EN,
+	.power_on = AM654_USB2_VBUS_DET_EN | AM654_USB2_VBUSVALID_DET_EN,
+	.power_off = AM654_USB2_OTG_PD,
+};
+
 static const struct udevice_id omap_usb2_id_table[] = {
 	{
 		.compatible = "ti,omap5-usb2",
@@ -90,6 +103,10 @@ static const struct udevice_id omap_usb2_id_table[] = {
 	{
 		.compatible = "ti,am437x-usb2",
 		.data = (ulong)&am437x_usb2_data,
+	},
+	{
+		.compatible = "ti,am654-usb2",
+		.data = (ulong)&am654_usb2_data,
 	},
 	{},
 };
