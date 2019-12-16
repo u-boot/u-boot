13cb7cc9e8e48eb888b13743f79ff02420405044
Guillermo Rodríguez <guille.rodriguez@gmail.com>
2019-12-16 16:27:57 +0100

    dfu: Add option to skip empty pages when flashing UBI images to NAND
    Add a new option to enable the DROP_FFS flag when flashing UBI images to
    NAND in order to drop trailing all-0xff pages.
    
    This is similar to the existing FASTBOOT_FLASH_NAND_TRIMFFS option.
    
    Signed-off-by: Guillermo Rodriguez <guille.rodriguez@gmail.com>
    Cc: Lukasz Majewski <lukma@denx.de>

--- drivers/dfu/dfu_nand.c
+++ drivers/dfu/dfu_nand.c
@@ -50,6 +50,7 @@ static int nand_block_op(enum dfu_op op, struct dfu_entity *dfu,
 					 lim, buf);
 	} else {
 		nand_erase_options_t opts;
+		int write_flags = WITH_WR_VERIFY;
 
 		memset(&opts, 0, sizeof(opts));
 		opts.offset = start;
@@ -62,8 +63,12 @@ static int nand_block_op(enum dfu_op op, struct dfu_entity *dfu,
 		if (ret)
 			return ret;
 		/* then write */
+#ifdef CONFIG_DFU_NAND_TRIMFFS
+		if (dfu->data.nand.ubi)
+			write_flags |= WITH_DROP_FFS;
+#endif
 		ret = nand_write_skip_bad(mtd, start, &count, &actual,
-					  lim, buf, WITH_WR_VERIFY);
+					  lim, buf, write_flags);
 	}
 
 	if (ret != 0) {
