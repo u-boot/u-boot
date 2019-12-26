95f7103f943bff243df46592eca5dcb8957ab0ba
Alexey Brodkin <abrodkin@synopsys.com>
2019-12-26 14:47:42 +0300

    ARC: nsim_hs38: Add support of Virtio NET & BLK
    Given now nsim_hs38 configuration is usable on QEMU and in QEMU
    we have Virtio working perfectly fine the next logical step
    is to add support of supported & known to work net & bkl to this
    config.
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

---
+++ board/synopsys/nsim/nsim.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2020 Synopsys, Inc. All rights reserved.
+ */
+
+#include <common.h>
+#include <dm/device.h>
+#include <virtio_types.h>
+#include <virtio.h>
+
+int board_early_init_r(void)
+{
+	/*
+	 * Make sure virtio bus is enumerated so that peripherals
+	 * on the virtio bus can be discovered by their drivers
+	 */
+	virtio_init();
+
+	return 0;
+}
+
+int checkboard(void)
+{
+	printf("Board: ARC virtual or prototyping platform\n");
+	return 0;
+};
