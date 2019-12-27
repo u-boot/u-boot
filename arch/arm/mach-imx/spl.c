7df6397eb4e67a89569ce5770341f8c3fa39342a
Peng Fan <peng.fan@nxp.com>
2019-12-27 10:27:49 +0800

    imx: spl: support i.MX8MP spl_boot_device
    i.MX8MP follows i.MX8MN, so just let it use spl_board_boot_device
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/spl.c
+++ arch/arm/mach-imx/spl.c
@@ -135,7 +135,8 @@ u32 spl_boot_device(void)
 
 	enum boot_device boot_device_spl = get_boot_device();
 
-	if (IS_ENABLED(CONFIG_IMX8MM) || IS_ENABLED(CONFIG_IMX8MN))
+	if (IS_ENABLED(CONFIG_IMX8MM) || IS_ENABLED(CONFIG_IMX8MN) ||
+	    IS_ENABLED(CONFIG_IMX8MP))
 		return spl_board_boot_device(boot_device_spl);
 
 	switch (boot_device_spl) {
