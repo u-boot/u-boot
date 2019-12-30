d8293e984b51e11d4f55d4ec5efa513368786e6c
Peng Fan <peng.fan@nxp.com>
2019-12-30 09:52:15 +0800

    pinctrl: imx8m: support i.MX8MP
    Add i.MX8MP compatible to let the pinctrl driver could support
    i.MX8MP.
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- drivers/pinctrl/nxp/pinctrl-imx8m.c
+++ drivers/pinctrl/nxp/pinctrl-imx8m.c
@@ -22,6 +22,7 @@ static const struct udevice_id imx8m_pinctrl_match[] = {
 	{ .compatible = "fsl,imx8mq-iomuxc", .data = (ulong)&imx8mq_pinctrl_soc_info },
 	{ .compatible = "fsl,imx8mm-iomuxc", .data = (ulong)&imx8mq_pinctrl_soc_info },
 	{ .compatible = "fsl,imx8mn-iomuxc", .data = (ulong)&imx8mq_pinctrl_soc_info },
+	{ .compatible = "fsl,imx8mp-iomuxc", .data = (ulong)&imx8mq_pinctrl_soc_info },
 	{ /* sentinel */ }
 };
 
