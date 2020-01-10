25273dbd570541214ec6d687a4d6f3bfcdaa0441
Sam Shih <sam.shih@mediatek.com>
2020-01-10 16:30:32 +0800

    mmc: add mmc and sd support for MT7622
    This patch add mmc and sd support for Mediatek MT7622 SoCs
    
    Signed-off-by: Sam Shih <sam.shih@mediatek.com>
    Reviewed-by: Ryder Lee <ryder.lee@mediatek.com>

--- drivers/mmc/mtk-sd.c
+++ drivers/mmc/mtk-sd.c
@@ -1621,6 +1621,15 @@ static const struct msdc_compatible mt7620_compat = {
 	.enhance_rx = false
 };
 
+static const struct msdc_compatible mt7622_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+};
+
 static const struct msdc_compatible mt7623_compat = {
 	.clk_div_bits = 12,
 	.sclk_cycle_shift = 20,
@@ -1664,6 +1673,7 @@ static const struct msdc_compatible mt8183_compat = {
 
 static const struct udevice_id msdc_ids[] = {
 	{ .compatible = "mediatek,mt7620-mmc", .data = (ulong)&mt7620_compat },
+	{ .compatible = "mediatek,mt7622-mmc", .data = (ulong)&mt7622_compat },
 	{ .compatible = "mediatek,mt7623-mmc", .data = (ulong)&mt7623_compat },
 	{ .compatible = "mediatek,mt8512-mmc", .data = (ulong)&mt8512_compat },
 	{ .compatible = "mediatek,mt8516-mmc", .data = (ulong)&mt8516_compat },
