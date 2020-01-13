277a0ad8f559e6b54c0d32512697debc659d9fd5
Patrick Delaunay <patrick.delaunay@st.com>
2020-01-13 11:34:59 +0100

    gpio: remove GPIOD_REQUESTED
    Remove the define GPIOD_REQUESTED as it is never used
    and use BIT() macro for other defines.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/asm-generic/gpio.h
+++ include/asm-generic/gpio.h
@@ -117,11 +117,10 @@ struct udevice;
 struct gpio_desc {
 	struct udevice *dev;	/* Device, NULL for invalid GPIO */
 	unsigned long flags;
-#define GPIOD_REQUESTED		(1 << 0)	/* Requested/claimed */
-#define GPIOD_IS_OUT		(1 << 1)	/* GPIO is an output */
-#define GPIOD_IS_IN		(1 << 2)	/* GPIO is an input */
-#define GPIOD_ACTIVE_LOW	(1 << 3)	/* value has active low */
-#define GPIOD_IS_OUT_ACTIVE	(1 << 4)	/* set output active */
+#define GPIOD_IS_OUT		BIT(1)	/* GPIO is an output */
+#define GPIOD_IS_IN		BIT(2)	/* GPIO is an input */
+#define GPIOD_ACTIVE_LOW	BIT(3)	/* value has active low */
+#define GPIOD_IS_OUT_ACTIVE	BIT(4)	/* set output active */
 
 	uint offset;		/* GPIO offset within the device */
 	/*
