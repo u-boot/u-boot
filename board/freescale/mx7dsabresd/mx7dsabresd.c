e2b14bf4ba0a151956e89c8243c10b08485a875f
Joris Offouga <offougajoris@gmail.com>
2020-01-16 17:41:43 +0100

    mx7dsabre: Fix dm probe pmic
    With commit 0d52bab4621 ("mx7dsabre: Enable DM_ETH")
    
    Device Tree has been update and change pfuze3000 node
    
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>
    Reviewed-by: Fabio Estevam <festevam@gmail.com>

--- board/freescale/mx7dsabresd/mx7dsabresd.c
+++ board/freescale/mx7dsabresd/mx7dsabresd.c
@@ -264,7 +264,7 @@ int power_init_board(void)
 	struct udevice *dev;
 	int ret, dev_id, rev_id;
 
-	ret = pmic_get("pfuze3000@08", &dev);
+	ret = pmic_get("pfuze3000@8", &dev);
 	if (ret == -ENODEV)
 		return 0;
 	if (ret != 0)
