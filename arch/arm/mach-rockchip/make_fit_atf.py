dfa64707da22d816c8e43141f7aaaf3a3078cd06
Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
2020-01-17 21:26:04 +0100

    rockchip: make_fit_atf: use correct fdt_x references in config nodes
    The script iterates over the given devicetrees and creates both
    fdt_x node as well as a conf-node for each passed dt.
    
    But there is a slight bug in that it always references fdt_1 in each
    conf node instead of the matching fdt_x as expected.
    
    So fix that by referencing the number of the current dt similar to
    how the fdt_x nodes gets created.
    
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/mach-rockchip/make_fit_atf.py
+++ arch/arm/mach-rockchip/make_fit_atf.py
@@ -107,7 +107,7 @@ def append_conf_section(file, cnt, dtname, segments):
             file.write(';\n')
     if segments <= 1:
         file.write(';\n')
-    file.write('\t\t\tfdt = "fdt_1";\n')
+    file.write('\t\t\tfdt = "fdt_%d";\n' % cnt)
     file.write('\t\t};\n')
     file.write('\n')
 
