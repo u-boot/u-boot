e204ccaf662f63300eafa7b5cbfa525efeab1336
T Karthik Reddy <t.karthik.reddy@xilinx.com>
2020-01-21 07:17:19 -0700

    spi: cadence_ospi: Move all macros to a header file
    This patch moves all macros from cadence ospi & qspi_abp files to
    cadence_qspi.h file.
    
    Signed-off-by: T Karthik Reddy <t.karthik.reddy@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- drivers/spi/cadence_ospi_versal.c
+++ drivers/spi/cadence_ospi_versal.c
@@ -12,27 +12,6 @@
 #include <asm/io.h>
 #include "cadence_qspi.h"
 
-#define CQSPI_DMA_DST_ADDR_REG			0x1800
-#define CQSPI_DMA_DST_SIZE_REG			0x1804
-#define CQSPI_DMA_DST_STS_REG			0x1808
-#define CQSPI_DMA_DST_CTRL_REG			0x180C
-#define CQSPI_DMA_DST_I_STS_REG			0x1814
-#define CQSPI_DMA_DST_I_ENBL_REG		0x1818
-#define CQSPI_DMA_DST_I_DISBL_REG		0x181C
-#define CQSPI_DMA_DST_CTRL2_REG			0x1824
-#define CQSPI_DMA_DST_ADDR_MSB_REG		0x1828
-
-#define CQSPI_DMA_SRC_RD_ADDR_REG		0x1000
-
-#define CQSPI_REG_DMA_PERIPH_CFG		0x20
-#define CQSPI_REG_INDIR_TRIG_ADDR_RANGE		0x80
-#define CQSPI_DFLT_INDIR_TRIG_ADDR_RANGE	6
-#define CQSPI_DFLT_DMA_PERIPH_CFG		0x602
-#define CQSPI_DFLT_DST_CTRL_REG_VAL		0xF43FFA00
-
-#define CQSPI_DMA_DST_I_STS_DONE		BIT(1)
-#define CQSPI_DMA_TIMEOUT			10000000
-
 void cadence_qspi_apb_dma_read(struct cadence_spi_platdata *plat,
 			       unsigned int n_rx, u8 *rxbuf)
 {
