fb3c4d926fa52b4a4f5b887eb29d64c24eb93d40
Pedro Jardim <jardim.c.pedro@gmail.com>
2020-01-23 10:20:14 -0300

    clock_imx8mq: Delete not used init_usb_clk()
    Function init_usb_clk() is not used anywhere, so it can simply be deleted.
    This was detected by the following sparse warning:
    
    arch/arm/mach-imx/imx8m/clock_imx8mq.c:377:6: warning: no previous prototype
     for ‘init_usb_clk’ [-Wmissing-prototypes]
     void init_usb_clk(void)
          ^~~~~~~~~~~~
    
    Signed-off-by: Pedro Jardim <jardim.c.pedro@gmail.com>

--- arch/arm/mach-imx/imx8m/clock_imx8mq.c
+++ arch/arm/mach-imx/imx8m/clock_imx8mq.c
@@ -374,28 +374,6 @@ void init_wdog_clk(void)
 	clock_enable(CCGR_WDOG3, 1);
 }
 
-void init_usb_clk(void)
-{
-	if (!is_usb_boot()) {
-		clock_enable(CCGR_USB_CTRL1, 0);
-		clock_enable(CCGR_USB_CTRL2, 0);
-		clock_enable(CCGR_USB_PHY1, 0);
-		clock_enable(CCGR_USB_PHY2, 0);
-		/* 500MHz */
-		clock_set_target_val(USB_BUS_CLK_ROOT, CLK_ROOT_ON |
-				     CLK_ROOT_SOURCE_SEL(1));
-		/* 100MHz */
-		clock_set_target_val(USB_CORE_REF_CLK_ROOT, CLK_ROOT_ON |
-				     CLK_ROOT_SOURCE_SEL(1));
-		/* 100MHz */
-		clock_set_target_val(USB_PHY_REF_CLK_ROOT, CLK_ROOT_ON |
-				     CLK_ROOT_SOURCE_SEL(1));
-		clock_enable(CCGR_USB_CTRL1, 1);
-		clock_enable(CCGR_USB_CTRL2, 1);
-		clock_enable(CCGR_USB_PHY1, 1);
-		clock_enable(CCGR_USB_PHY2, 1);
-	}
-}
 
 void init_nand_clk(void)
 {
