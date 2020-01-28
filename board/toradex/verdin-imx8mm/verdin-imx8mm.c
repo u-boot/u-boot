14d5aeff776bce23a4825114a8277e1be30e545b
Igor Opaniuk <igor.opaniuk@toradex.com>
2020-01-28 14:42:25 +0100

    board: toradex: Add Verdin iMX8M Mini support
    This adds initial minimal support for the Toradex Verdin iMX8M Mini Quad
    2GB WB IT V1.0A module. They are now strapped to boot from eFuses which
    are factory fused to properly boot from their on-module eMMC. U-Boot
    supports booting from the on-module eMMC only, SDP support is disabled
    for now due to missing i.MX 8M Mini USB support.
    
    Functionality wise the following is known to be working:
    - eMMC, 8-bit and 4-bit MMC/SD card slots
    - Ethernet
    - GPIOs
    - I2C
    
    Boot sequence is:
    SPL ---> ATF (TF-A) ---> U-boot proper
    
    ATF, U-boot proper and u-boot.dtb images are packed into a FIT image,
    loaded by SPL.
    
    Boot:
    U-Boot SPL 2020.01-00187-gd411d164e5 (Jan 26 2020 - 04:47:26 +0100)
    Normal Boot
    Trying to boot from MMC1
    NOTICE:  Configuring TZASC380
    NOTICE:  RDC off
    NOTICE:  BL31: v2.0(release):rel_imx_4.14.98_2.3.0-0-g09c5cc994-dirty
    NOTICE:  BL31: Built : 01:11:41, Jan 25 2020
    NOTICE:  sip svc init
    
    U-Boot 2020.01-00187-gd411d164e5 (Jan 26 2020 - 04:47:26 +0100)
    
    CPU:   Freescale i.MX8MMQ rev1.0 at 0 MHz
    Reset cause: POR
    DRAM:  2 GiB
    MMC:   FSL_SDHC: 0, FSL_SDHC: 1, FSL_SDHC: 2
    Loading Environment from MMC... OK
    In:    serial
    Out:   serial
    Err:   serial
    Model: Toradex Verdin iMX8M Mini Quad 2GB Wi-Fi / BT IT V1.0A, Serial#
     06535149
    Net:   eth0: ethernet@30be0000
    Hit any key to stop autoboot:  0
    Verdin iMX8MM #
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    Reviewed-by: Oleksandr Suvorov <oleksandr.suvorov@toradex.com>

---
+++ board/toradex/verdin-imx8mm/verdin-imx8mm.c
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2020 Toradex
+ */
+
+#include <common.h>
+#include <asm/arch/clock.h>
+#include <asm/io.h>
+#include <miiphy.h>
+#include <netdev.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init(void)
+{
+	gd->ram_size = get_ram_size((void *)PHYS_SDRAM, PHYS_SDRAM_SIZE);
+
+	return 0;
+}
+
+#if IS_ENABLED(CONFIG_FEC_MXC)
+static int setup_fec(void)
+{
+	struct iomuxc_gpr_base_regs *gpr =
+		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+
+	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
+	clrsetbits_le32(&gpr->gpr[1], 0x2000, 0);
+
+	return 0;
+}
+
+int board_phy_config(struct phy_device *phydev)
+{
+	/* enable rgmii rxc skew and phy mode select to RGMII copper */
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x1f);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x8);
+
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x00);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x82ee);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x05);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x100);
+
+	if (phydev->drv->config)
+		phydev->drv->config(phydev);
+	return 0;
+}
+#endif
+
+int board_init(void)
+{
+	if (IS_ENABLED(CONFIG_FEC_MXC))
+		setup_fec();
+
+	return 0;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
+
+int board_late_init(void)
+{
+	return 0;
+}
+
+#if defined(CONFIG_OF_LIBFDT) && defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	return 0;
+}
+#endif
