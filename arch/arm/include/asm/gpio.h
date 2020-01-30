f267f84b93d026cfcce77fc3f9eba4c57d4ad0e0
Jason Li <jason.li@cortina-access.com>
2020-01-30 12:34:55 -0800

    gpio: do not include <asm/arch/gpio.h> for Cortina CAxxxx SoCs
    The Cortina CAxxxx GPIO driver maintains DM_GPIO support
    across different CPU ISA in the CAxxxx Soc Family; Not just ARM.
    Therefore, it is not desirable to split out and maintain separete
    gpio header file for each CPU architecture.
    
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Jason Li <jason.li@cortina-access.com>
    Signed-off-by: Alex Nemirovsky <alex.nemirovsky@cortina-access.com>

--- arch/arm/include/asm/gpio.h
+++ arch/arm/include/asm/gpio.h
@@ -4,8 +4,8 @@
 	!defined(CONFIG_ARCH_ROCKCHIP) && !defined(CONFIG_ARCH_LX2160A) && \
 	!defined(CONFIG_ARCH_LS1028A) && !defined(CONFIG_ARCH_LS2080A) && \
 	!defined(CONFIG_ARCH_LS1088A) && !defined(CONFIG_ARCH_ASPEED) && \
-	!defined(CONFIG_ARCH_LS1012A) && \
-	!defined(CONFIG_ARCH_U8500)
+	!defined(CONFIG_ARCH_LS1012A) && !defined(CONFIG_ARCH_U8500) && \
+	!defined(CONFIG_CORTINA_PLATFORM)
 #include <asm/arch/gpio.h>
 #endif
 #include <asm-generic/gpio.h>
