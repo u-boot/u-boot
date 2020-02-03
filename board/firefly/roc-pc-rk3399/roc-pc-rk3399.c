01892d230dd456278cd28c3b8de109202f910238
Suniel Mahesh <sunil@amarulasolutions.com>
2020-02-03 19:20:04 +0530

    rockchip: rk3399: split roc-pc-rk3399 out of evb_rk3399
    roc-pc-rk3399 board has one user button & three user LED's. Currently
    we don't have any code support for these devices. Since button and LED's are
    specific to roc-pc-rk3399 board, split it into its own board file and add code
    support here.
    
    Signed-off-by: Suniel Mahesh <sunil@amarulasolutions.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

---
+++ board/firefly/roc-pc-rk3399/roc-pc-rk3399.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * (C) Copyright 2016 Rockchip Electronics Co., Ltd
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <asm/arch-rockchip/periph.h>
+#include <power/regulator.h>
+
+#ifndef CONFIG_SPL_BUILD
+int board_early_init_f(void)
+{
+	struct udevice *regulator;
+	int ret;
+
+	ret = regulator_get_by_platname("vcc5v0_host", &regulator);
+	if (ret) {
+		debug("%s vcc5v0_host init fail! ret %d\n", __func__, ret);
+		goto out;
+	}
+
+	ret = regulator_set_enable(regulator, true);
+	if (ret)
+		debug("%s vcc5v0-host-en set fail! ret %d\n", __func__, ret);
+out:
+	return 0;
+}
+#endif
