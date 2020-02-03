f72bdc60b230f5e5d77eaad0b2defe07a46c02a9
Simon Glass <sjg@chromium.org>
2020-02-03 07:36:01 -0700

    sandbox: Rename strdup() functions
    These functions include calls to a memory-allocation routine and so need
    to use the system routine when called from a library.
    
    To preserve access to these functions for libraries that need it, such as
    SDL, rename these functions within U-Boot.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/linux/string.h
+++ include/linux/string.h
@@ -91,6 +91,11 @@ extern __kernel_size_t strnlen(const char *,__kernel_size_t);
 size_t strcspn(const char *s, const char *reject);
 #endif
 
+#ifdef CONFIG_SANDBOX
+# define strdup		sandbox_strdup
+# define strndup		sandbox_strndup
+#endif
+
 #ifndef __HAVE_ARCH_STRDUP
 extern char * strdup(const char *);
 extern char * strndup(const char *, size_t);
