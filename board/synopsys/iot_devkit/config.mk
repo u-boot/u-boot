c77bbc221547b9820b801dc6435451fa45ecec26
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2020-02-12 14:23:54 +0300

    ARC: rework setting of ARC CPU specific compiler options
    It's a very rare if at all existing occasion when ARC CPU template
    is used as is w/o any changes - in the end it's a beauty and competitive
    advantage of ARC cores to be tailored for a particular use-case - and
    so it doesn't make a lot of sense to offer template-based "-mcpu" selection.
    Given for each and every platform we end-up adding quite a few more flags
    it's logical to move "-mcpu" selection to platform's definition as well
    which we exactly do here.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- board/synopsys/iot_devkit/config.mk
+++ board/synopsys/iot_devkit/config.mk
@@ -1,2 +1,3 @@
-PLATFORM_CPPFLAGS += -mlittle-endian -mcode-density -mdiv-rem -mswap -mnorm -mmpy-option=6 -mbarrel-shifter
+PLATFORM_CPPFLAGS += -mcpu=arcem -mlittle-endian -mcode-density -mdiv-rem \
+                     -mswap -mnorm -mmpy-option=6 -mbarrel-shifter
 LDSCRIPT = $(srctree)/board/synopsys/iot_devkit/u-boot.lds
