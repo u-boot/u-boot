a604b67b877371e6d62b7ec30c858d964c3d4b93
Alifer Moraes <alifer.wsdm@gmail.com>
2020-02-14 16:18:50 -0300

    arm: dts: imx8mq-evk: add phy-reset-gpios for fec1
    Instead of resetting the ethernet phy through functions in imx8mq_evk.c, let the
    driver reset the phy via dts description adding a reset duration of 10 ms
    following atheros 8031's datasheet recommendation.
    
    Signed-off-by: Alifer Moraes <alifer.wsdm@gmail.com>

--- board/freescale/imx8mq_evk/imx8mq_evk.c
+++ board/freescale/imx8mq_evk/imx8mq_evk.c
@@ -64,28 +64,10 @@ int dram_init(void)
 }
 
 #ifdef CONFIG_FEC_MXC
-#define FEC_RST_PAD IMX_GPIO_NR(1, 9)
-static iomux_v3_cfg_t const fec1_rst_pads[] = {
-	IMX8MQ_PAD_GPIO1_IO09__GPIO1_IO9 | MUX_PAD_CTRL(NO_PAD_CTRL),
-};
-
-static void setup_iomux_fec(void)
-{
-	imx_iomux_v3_setup_multiple_pads(fec1_rst_pads,
-					 ARRAY_SIZE(fec1_rst_pads));
-
-	gpio_request(IMX_GPIO_NR(1, 9), "fec1_rst");
-	gpio_direction_output(IMX_GPIO_NR(1, 9), 0);
-	udelay(500);
-	gpio_direction_output(IMX_GPIO_NR(1, 9), 1);
-}
-
 static int setup_fec(void)
 {
 	struct iomuxc_gpr_base_regs *gpr =
 		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
-
-	setup_iomux_fec();
 
 	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
 	clrsetbits_le32(&gpr->gpr[1], BIT(13) | BIT(17), 0);
