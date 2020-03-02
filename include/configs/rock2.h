23cb61761b137f088ef8625a1c4e7d9af2cbd296
Peter Robinson <pbrobinson@gmail.com>
2020-03-02 07:57:55 +0000

    rockchip: Change keyboard input from CrOS EC keyboard to a USB keyboard
    These boards aren't ChromeOS devices so won't have a cros-ec-keyb
    input as it's the keyboard available via the ChromeOS Embedded
    Controller. Update them to use a USB keyboard which would actually
    be available. Also enable the usb keyboard option for those devices
    that don't have it enabled already.
    
    Signed-off-by: Peter Robinson <pbrobinson@gmail.com>
    Reviewed-by: Michael Trimarchi <michael@amarulasolutions.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- include/configs/rock2.h
+++ include/configs/rock2.h
@@ -7,7 +7,7 @@
 #define __CONFIG_H
 
 #define ROCKCHIP_DEVICE_SETTINGS \
-		"stdin=serial,cros-ec-keyb\0" \
+		"stdin=serial,usbkbd\0" \
 		"stdout=serial,vidconsole\0" \
 		"stderr=serial,vidconsole\0"
 
