b9bb1d06e2623a9e42db72ab269aac615d56a336
Claudius Heine <ch@denx.de>
2020-03-04 15:23:11 +0100

    imx: imx8m*: Remove do_reset from board files
    Use the `do_reset` implementation of `arch/arm/lib/reset.c` in SPL
    instead.  It is very close to what is done here, anyway, and plays
    more nicely with the rest of U-Boot than adding a custom `do_reset`
    implementation into board files.
    
    `do_reset` from `arch/arm/lib/reset.c` calls `reset_cpu` with 0 as the
    addr parameter while the boards are passing WDOG1_BASE_ADDR.  This is
    ok because the `reset_cpu` implementation uses WDOG1_BASE_ADDR by
    default if 0 is passed in.
    
    Co-Authored-by: Harald Seiler <hws@denx.de>
    Signed-off-by: Claudius Heine <ch@denx.de>
    Signed-off-by: Harald Seiler <hws@denx.de>
    Reviewed-by: Marek Vasut <marex@denx.de>

--- board/freescale/imx8mn_evk/spl.c
+++ board/freescale/imx8mn_evk/spl.c
@@ -114,12 +114,3 @@ void board_init_f(ulong dummy)
 
 	board_init_r(NULL, 0);
 }
-
-int do_reset(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
-{
-	puts("resetting ...\n");
-
-	reset_cpu(WDOG1_BASE_ADDR);
-
-	return 0;
-}
