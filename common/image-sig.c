472f9113dbbbed88345f3d38de3ff37ca163508e
Simon Glass <sjg@chromium.org>
2020-03-18 11:43:57 -0600

    image: Return an error message from fit_config_verify_sig()
    This function only returns an error message sometimes. Update it to always
    return an error message if one is available. This makes it easier to see
    what went wrong.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- common/image-sig.c
+++ common/image-sig.c
@@ -499,13 +499,14 @@ static int fit_config_verify_sig(const void *fit, int conf_noffset,
 		goto error;
 	}
 
-	return verified ? 0 : -EPERM;
+	if (verified)
+		return 0;
 
 error:
 	printf(" error!\n%s for '%s' hash node in '%s' config node\n",
 	       err_msg, fit_get_name(fit, noffset, NULL),
 	       fit_get_name(fit, conf_noffset, NULL));
-	return -1;
+	return -EPERM;
 }
 
 int fit_config_verify_required_sigs(const void *fit, int conf_noffset,
