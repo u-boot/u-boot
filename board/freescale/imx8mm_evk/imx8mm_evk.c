47d430baed16b35a1afc5ae2ae8c468de8d21efd
Igor Opaniuk <igor.opaniuk@toradex.com>
2020-03-27 12:28:19 +0200

    imx8mm_evk: adjust dram size in case bl32 is used
    Adjust DRAM size in case BL32 secure payload is loaded (OP-TEE/Trusty),
    so during MMU initialization U-Boot won't touch this mem area.
    
    BL32 is loaded to the end of DRAM, bl32 payload size is read from
    rom_pointer[1]. This relates to the issue described in
    59efa6b52b("imx8m: Fix MMU table issue for OPTEE memory").
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

--- board/freescale/imx8mm_evk/imx8mm_evk.c
+++ board/freescale/imx8mm_evk/imx8mm_evk.c
@@ -15,7 +15,11 @@ DECLARE_GLOBAL_DATA_PTR;
 
 int dram_init(void)
 {
-	gd->ram_size = PHYS_SDRAM_SIZE;
+	/* rom_pointer[1] contains the size of TEE occupies */
+	if (rom_pointer[1])
+		gd->ram_size = PHYS_SDRAM_SIZE - rom_pointer[1];
+	else
+		gd->ram_size = PHYS_SDRAM_SIZE;
 
 	return 0;
 }
