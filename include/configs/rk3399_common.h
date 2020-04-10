d2c19b740c48c662fe23d02ac8d6514895d8c205
Jagan Teki <jagan@amarulasolutions.com>
2020-04-10 23:56:30 +0530

    rk3399: Add boot flash script offset, size
    Most of the SPI flash devices in rockchip (rk3399)
    are 16MiB size. So, let's use the script offset at
    the end of 8K. 
    
    This way it cannot overlap any offsets being used
    by software components in flash layout.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -48,6 +48,8 @@
 
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"scriptaddr=0x00500000\0" \
+	"script_offset_f=0xffe000\0" \
+	"script_size_f=0x2000\0" \
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x01f00000\0" \
 	"kernel_addr_r=0x02080000\0" \
