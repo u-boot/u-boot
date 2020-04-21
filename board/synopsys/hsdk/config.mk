e31fdd8170df7e43d3accdcb47f7a3820e7306a3
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2020-04-22 01:57:41 +0300

    ARC: HSDK-4xD: fix headerize script for HSDK-4xD compatibility
    ARC HS CPU in HSDK-4xD has ARC ID = 0x54, so fix headerize script
    accordingly.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- board/synopsys/hsdk/config.mk
+++ board/synopsys/hsdk/config.mk
@@ -6,13 +6,6 @@ ifdef CONFIG_BOARD_HSDK
 PLATFORM_CPPFLAGS += -mcpu=hs38_linux -mlittle-endian -matomic -mll64 \
                      -mdiv-rem -mswap -mnorm -mmpy-option=9 -mbarrel-shifter \
                      -mfpu=fpud_all
-endif
-
-ifdef CONFIG_BOARD_HSDK_4XD
-PLATFORM_CPPFLAGS += -mcpu=hs4x_rel31 -mlittle-endian -matomic -mll64 \
-                     -mdiv-rem -mswap -mnorm -mmpy-option=9 -mbarrel-shifter \
-                     -mfpu=fpud_all
-endif
 
 bsp-generate: u-boot u-boot.bin
 	$(Q)python3 $(srctree)/board/$(BOARDDIR)/headerize-hsdk.py \
@@ -21,3 +14,18 @@ bsp-generate: u-boot u-boot.bin
 	$(Q)tools/mkimage -T script -C none -n 'uboot update script' \
 		-d $(srctree)/u-boot-update.txt \
 		$(srctree)/u-boot-update.scr &> /dev/null
+endif
+
+ifdef CONFIG_BOARD_HSDK_4XD
+PLATFORM_CPPFLAGS += -mcpu=hs4x_rel31 -mlittle-endian -matomic -mll64 \
+                     -mdiv-rem -mswap -mnorm -mmpy-option=9 -mbarrel-shifter \
+                     -mfpu=fpud_all
+
+bsp-generate: u-boot u-boot.bin
+	$(Q)python3 $(srctree)/board/$(BOARDDIR)/headerize-hsdk.py \
+		--arc-id 0x54 --image $(srctree)/u-boot.bin \
+		--elf $(srctree)/u-boot
+	$(Q)tools/mkimage -T script -C none -n 'uboot update script' \
+		-d $(srctree)/u-boot-update.txt \
+		$(srctree)/u-boot-update.scr &> /dev/null
+endif
