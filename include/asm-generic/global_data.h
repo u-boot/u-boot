ba974a0137edc86cff9727a83cf6be154ab18ef0
Simon Glass <sjg@chromium.org>
2020-04-26 09:12:57 -0600

    board: Add a gd flag for chain loading
    When U-Boot is run from another boot loader, much of the low-level init
    needs to be skipped.
    
    Add a flag for this and adjust ll_boot_init() to use it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- include/asm-generic/global_data.h
+++ include/asm-generic/global_data.h
@@ -166,5 +166,6 @@ typedef struct global_data {
 #define GD_FLG_SPL_EARLY_INIT	0x04000 /* Early SPL init is done	   */
 #define GD_FLG_LOG_READY	0x08000 /* Log system is ready for use	   */
 #define GD_FLG_WDT_READY	0x10000 /* Watchdog is ready for use	   */
+#define GD_FLG_SKIP_LL_INIT	0x20000	/* Don't perform low-level init	   */
 
 #endif /* __ASM_GENERIC_GBL_DATA_H */
