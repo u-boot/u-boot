7b0830d41f9e0350207ae3f5137ad8c50731374b
Dario Binacchi <dariobin@libero.it>
2020-05-02 17:38:11 +0200

    clk: fix the console output of clk_register
    The parent->name variable can be used only in case the
    uclass_get_device_by_name routine returns successfully.
    
    Signed-off-by: Dario Binacchi <dariobin@libero.it>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -22,12 +22,12 @@ int clk_register(struct clk *clk, const char *drv_name,
 
 	ret = uclass_get_device_by_name(UCLASS_CLK, parent_name, &parent);
 	if (ret) {
-		printf("%s: name: %s parent: %s [0x%p]\n",
-		       __func__, name, parent->name, parent);
+		printf("%s: failed to get %s device (parent of %s)\n",
+		       __func__, parent_name, name);
+	} else {
+		debug("%s: name: %s parent: %s [0x%p]\n", __func__, name,
+		      parent->name, parent);
 	}
-
-	debug("%s: name: %s parent: %s [0x%p]\n", __func__, name, parent->name,
-	      parent);
 
 	drv = lists_driver_lookup_name(drv_name);
 	if (!drv) {
