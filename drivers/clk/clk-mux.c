76eaa2d0ed655da19fc1ea00cab054e19be33ecc
Dario Binacchi <dariobin@libero.it>
2020-05-02 17:58:31 +0200

    clk: ccf: mux: change include order
    Apply u-boot coding style on include files order.
    
    Signed-off-by: Dario Binacchi <dariobin@libero.it>

--- drivers/clk/clk-mux.c
+++ drivers/clk/clk-mux.c
@@ -22,16 +22,16 @@
  */
 
 #include <common.h>
-#include <asm/io.h>
-#include <malloc.h>
+#include <clk.h>
 #include <clk-uclass.h>
 #include <dm/device.h>
 #include <dm/devres.h>
 #include <linux/bitops.h>
+#include <malloc.h>
+#include <asm/io.h>
 #include <linux/clk-provider.h>
-#include <clk.h>
-#include "clk.h"
 #include <linux/err.h>
+#include "clk.h"
 
 #define UBOOT_DM_CLK_CCF_MUX "ccf_clk_mux"
 
