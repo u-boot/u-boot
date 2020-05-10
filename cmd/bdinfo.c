c66981ccbda837f556ef4ed89d9f9ce93d2adf12
Simon Glass <sjg@chromium.org>
2020-05-10 14:16:34 -0600

    bdinfo: sandbox: Use the generic bd command
    Sandbox has a printout of 'FB base' but this code is not used since
    sandbox uses driver model for everything.
    
    Move sandbox over to use the generic do_bdinfo().
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- cmd/bdinfo.c
+++ cmd/bdinfo.c
@@ -335,21 +335,7 @@ static int do_bdinfo(struct cmd_tbl *cmdtp, int flag, int argc,
 
 #elif defined(CONFIG_SANDBOX)
 
-int do_bdinfo(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
-{
-	bd_t *bd = gd->bd;
-
-	print_bi_boot_params(bd);
-	print_bi_dram(bd);
-	print_eth_ip_addr();
-
-#if defined(CONFIG_LCD) || defined(CONFIG_VIDEO)
-	print_num("FB base  ", gd->fb_base);
-#endif
-	print_cpu_word_size();
-
-	return 0;
-}
+#define USE_GENERIC
 
 #elif defined(CONFIG_NDS32)
 
