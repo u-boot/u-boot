628c85aec0a2034e69f56a876132e66c9eb864ff
Simon Glass <sjg@chromium.org>
2020-05-10 14:16:36 -0600

    bdinfo: riscv: Use the generic bd command
    This arch has none of its own info to show. Move it over to use the
    generic do_bdinfo().
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Rick Chen <rick@andestech.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- cmd/bdinfo.c
+++ cmd/bdinfo.c
@@ -343,20 +343,7 @@ static int do_bdinfo(struct cmd_tbl *cmdtp, int flag, int argc,
 
 #elif defined(CONFIG_RISCV)
 
-int do_bdinfo(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
-{
-	bd_t *bd = gd->bd;
-
-	print_bi_boot_params(bd);
-	print_bi_dram(bd);
-	print_num("relocaddr", gd->relocaddr);
-	print_num("reloc off", gd->reloc_off);
-	print_eth_ip_addr();
-	print_baudrate();
-	print_cpu_word_size();
-
-	return 0;
-}
+#define USE_GENERIC
 
 #elif defined(CONFIG_ARC)
 
