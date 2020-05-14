ba78c25afe96e4038849e9716e41261b9e114ab6
Marek Vasut <marex@denx.de>
2020-05-14 18:49:10 +0200

    ARM: imx6: Fetch MAC address in board_init_late() on DH iMX6 PDK2
    This is needed to obtain the MAC from EEPROM/OTP only after the final
    env is populated, otherwise the ethaddr might be overriden.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Claudius Heine <ch@denx.de>
    Cc: Harald Seiler <hws@denx.de>
    Cc: Ludwig Zenz <lzenz@dh-electronics.com>
    Cc: Stefano Babic <sbabic@denx.de>

--- board/dhelectronics/dh_imx6/dh_imx6.c
+++ board/dhelectronics/dh_imx6/dh_imx6.c
@@ -142,8 +142,6 @@ int board_init(void)
 	/* Enable eim_slow clocks */
 	setbits_le32(&mxc_ccm->CCGR6, 0x1 << MXC_CCM_CCGR6_EMI_SLOW_OFFSET);
 
-	setup_dhcom_mac_from_fuse();
-
 	setup_fec_clock();
 
 	return 0;
@@ -188,6 +186,8 @@ int board_late_init(void)
 {
 	u32 hw_code;
 	char buf[16];
+
+	setup_dhcom_mac_from_fuse();
 
 	hw_code = board_get_hwcode();
 
