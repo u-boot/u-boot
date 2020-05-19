faf002c0ab8bf013f891ec8ae09e3c0cc2bfcb6f
Tom Rini <trini@konsulko.com>
2020-05-19 10:32:33 -0400

    Remove CROSS_COMPILE default from arch/*/config.mk
    In order to support the compiler providing information used within
    Kconfig itself we cannot have the compiler be determined by
    arch/*/config.mk as we will not be able to evaluate that yet.  Given
    that most documentation tells people to specify CROSS_COMPILE, remove
    these references.
    
    Cc: Huan Wang <alison.wang@nxp.com>
    Cc: Angelo Dureghello <angelo@sysam.it>
    Cc: Michal Simek <monstr@monstr.eu>
    Cc: Rick Chen <rick@andestech.com>
    Cc: Thomas Chou <thomas@wytron.com.tw>
    Cc: Wolfgang Denk <wd@denx.de>
    Cc: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Bin Meng <bmeng.cn@gmail.com>
    Cc: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Acked-by: Michal Simek <michal.simek@xilinx.com>

--- arch/nds32/config.mk
+++ arch/nds32/config.mk
@@ -8,10 +8,6 @@
 # Macpaul Lin, Andes Technology Corporation <macpaul@andestech.com>
 #
 
-ifeq ($(CROSS_COMPILE),)
-CROSS_COMPILE := nds32le-linux-
-endif
-
 CONFIG_STANDALONE_LOAD_ADDR = 0x300000
 LDFLAGS_STANDALONE	+= -T $(srctree)/examples/standalone/nds32.lds
 
--- arch/sh/config.mk
+++ arch/sh/config.mk
@@ -3,10 +3,6 @@
 # (C) Copyright 2000-2002
 # Wolfgang Denk, DENX Software Engineering, wd@denx.de.
 
-ifeq ($(CROSS_COMPILE),)
-CROSS_COMPILE := sh4-linux-
-endif
-
 CONFIG_STANDALONE_LOAD_ADDR ?= 0x8C000000
 ifeq ($(CPU),sh2)
 LDFLAGS_STANDALONE += -EB
