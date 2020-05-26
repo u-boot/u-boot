87f6e2f51e337623b1794e916d8b8eb654736c0d
Peng Fan <peng.fan@nxp.com>
2020-05-26 20:33:46 -0300

    imx8mp_evk: spl: drop useless code
    Drop useless getting ccm device, there is no need to explicted do this
    in board code, and we not enable SPL CLK currently.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Fabio Estevam <festevam@gmail.com>

--- board/freescale/imx8mp_evk/spl.c
+++ board/freescale/imx8mp_evk/spl.c
@@ -29,11 +29,6 @@
 #include <mmc.h>
 #include <asm/arch/ddr.h>
 
-#include <dm/uclass.h>
-#include <dm/device.h>
-#include <dm/uclass-internal.h>
-#include <dm/device-internal.h>
-
 DECLARE_GLOBAL_DATA_PTR;
 
 int spl_board_boot_device(enum boot_device boot_dev_spl)
@@ -48,16 +43,7 @@ void spl_dram_init(void)
 
 void spl_board_init(void)
 {
-	struct udevice *dev;
-	int ret;
-
 	puts("Normal Boot\n");
-
-	ret = uclass_get_device_by_name(UCLASS_CLK,
-					"clock-controller@30380000",
-					&dev);
-	if (ret < 0)
-		printf("Failed to find clock node. Check device tree\n");
 }
 
 #define I2C_PAD_CTRL (PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE | PAD_CTL_PE)
