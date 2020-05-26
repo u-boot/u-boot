559c99f62818749c77f9078af929b214a0a7f819
Tom Rini <trini@konsulko.com>
2020-05-26 15:06:14 -0400

    bcmstb: Migrate CONFIG_CMD_EXT[24]
    The common config header for bcm7260 and bcm7445 enable
    CONFIG_CMD_EXT2/EXT4 but the defconfigs do not.  This results in the
    commands being disabled.  Enable them via the defconfig.
    
    Cc: Thomas Fitzsimmons <fitzsim@fitzsim.org>
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/bcmstb.h
+++ include/configs/bcmstb.h
@@ -129,7 +129,6 @@ extern phys_addr_t prior_stage_fdt_address;
  */
 #define CONFIG_CMD_ASKENV
 #define CONFIG_CMD_CACHE
-#define CONFIG_CMD_EXT2
 
 /*
  * Flash configuration.
@@ -142,8 +141,6 @@ extern phys_addr_t prior_stage_fdt_address;
  * Filesystem configuration.
  */
 #define CONFIG_DOS_PARTITION
-#define CONFIG_CMD_EXT4
-#define CONFIG_FS_EXT4
 #define CONFIG_CMD_FS_GENERIC
 
 /*
