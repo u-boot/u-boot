05369e417fa36c00ab5e253e549e19a06644b92e
Tom Rini <trini@konsulko.com>
2020-06-16 19:06:14 -0400

    Convert CONFIG_BOARD_TYPES to Kconfig
    This converts the following to Kconfig:
       CONFIG_BOARD_TYPES
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/brsmarc1.h
+++ include/configs/brsmarc1.h
@@ -16,7 +16,6 @@
 #include <configs/bur_am335x_common.h>
 #include <linux/stringify.h>
 /* ------------------------------------------------------------------------- */
-#define CONFIG_BOARD_TYPES
 
 /* memory */
 #define CONFIG_SYS_MALLOC_LEN		(5 * 1024 * 1024)
