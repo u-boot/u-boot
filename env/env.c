8968288cb477ba69b002db01a6407cf78e3a6e06
Patrick Delaunay <patrick.delaunay@st.com>
2020-06-24 10:17:50 +0200

    env: add failing trace in env_save
    Add trace in env save to indicate any errors to end user and avoid
    silent output when the command 'env save' is not executed.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>

--- env/env.c
+++ env/env.c
@@ -240,13 +240,17 @@ int env_save(void)
 	if (drv) {
 		int ret;
 
-		if (!drv->save)
-			return -ENODEV;
-
-		if (!env_has_inited(drv->location))
-			return -ENODEV;
-
 		printf("Saving Environment to %s... ", drv->name);
+		if (!drv->save) {
+			printf("not possible\n");
+			return -ENODEV;
+		}
+
+		if (!env_has_inited(drv->location)) {
+			printf("not initialized\n");
+			return -ENODEV;
+		}
+
 		ret = drv->save();
 		if (ret)
 			printf("Failed (%d)\n", ret);
