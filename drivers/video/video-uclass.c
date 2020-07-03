138dfea86ff2b7eda936043b2acc734a1425aeaa
Simon Glass <sjg@chromium.org>
2020-07-02 21:12:22 -0600

    video: Clear the copy framebuffer when clearing the screen
    Update video_clear() to also sync to the copy framebuffer.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Anatolij Gustschin <agust@denx.de>
    Tested-by: Bin Meng <bmeng.cn@gmail.com>

--- drivers/video/video-uclass.c
+++ drivers/video/video-uclass.c
@@ -95,6 +95,7 @@ int video_reserve(ulong *addrp)
 int video_clear(struct udevice *dev)
 {
 	struct video_priv *priv = dev_get_uclass_priv(dev);
+	int ret;
 
 	switch (priv->bpix) {
 	case VIDEO_BPP16:
@@ -119,6 +120,9 @@ int video_clear(struct udevice *dev)
 		memset(priv->fb, priv->colour_bg, priv->fb_size);
 		break;
 	}
+	ret = video_sync_copy(dev, priv->fb, priv->fb + priv->fb_size);
+	if (ret)
+		return ret;
 
 	return 0;
 }
