7842749c351f3bb3976ca56c2ef5793e6b2cce97
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2020-07-03 10:37:06 -0600

    linux/kconfig.h: create two- and three-argument versions of CONFIG_IS_ENABLED
    This adds a bunch of preprocessor magic to extend the capabilities of
    CONFIG_IS_ENABLED. The existing semantics of
    
      CONFIG_IS_ENABLED(FOO)
    
    expanding to a 1 or 0 (depending on build context and the defined-ness
    or not of the appropriate CONFIG_FOO/CONFIG_SPL_FOO/CONFIG_TPL_FOO)
    are of course preserved. With this, one is also allowed a two-argument
    form
    
      CONFIG_IS_ENABLED(FOO, (something))
    
    which expands to something precisely when CONFIG_IS_ENABLED(FOO) would
    expand to 1, and expands to nothing otherwise. It is, in other words,
    completely equivalent to the three lines
    
      #if CONFIG_IS_ENABLED(FOO)
      something
      #endif
    
    The second argument must be parenthesized in order to allow any
    tokens, including a trailing comma, to appear - one use case for this
    is precisely to make it a bit more ergonomic to build an array and
    only include certain items depending on .config. That should increase
    both readability and not least "git grep"ability.
    
    A third variant is also introduced,
    
      CONFIG_IS_ENABLED(FOO, (xxx), (yyy))
    
    which corresponds to
    
      #if CONFIG_IS_ENABLED(FOO)
      xxx
      #else
      yyy
      #endif
    
    Signed-off-by: Rasmus Villemoes <rasmus.villemoes@prevas.dk>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/linux/kconfig.h
+++ include/linux/kconfig.h
@@ -56,13 +56,55 @@
 #define CONFIG_VAL(option)  config_val(option)
 
 /*
- * CONFIG_IS_ENABLED(FOO) evaluates to
+ * Count number of arguments to a variadic macro. Currently only need
+ * it for 1, 2 or 3 arguments.
+ */
+#define __arg6(a1, a2, a3, a4, a5, a6, ...) a6
+#define __count_args(...) __arg6(dummy, ##__VA_ARGS__, 4, 3, 2, 1, 0)
+
+#define __concat(a, b)   ___concat(a, b)
+#define ___concat(a, b)  a ## b
+
+#define __unwrap(...) __VA_ARGS__
+#define __unwrap1(case1, case0) __unwrap case1
+#define __unwrap0(case1, case0) __unwrap case0
+
+#define __CONFIG_IS_ENABLED_1(option)        __CONFIG_IS_ENABLED_3(option, (1), (0))
+#define __CONFIG_IS_ENABLED_2(option, case1) __CONFIG_IS_ENABLED_3(option, case1, ())
+#define __CONFIG_IS_ENABLED_3(option, case1, case0) \
+	__concat(__unwrap, config_enabled(CONFIG_VAL(option))) (case1, case0)
+
+/*
+ * CONFIG_IS_ENABLED(FOO) expands to
  *  1 if CONFIG_SPL_BUILD is undefined and CONFIG_FOO is set to 'y',
  *  1 if CONFIG_SPL_BUILD is defined and CONFIG_SPL_FOO is set to 'y',
  *  1 if CONFIG_TPL_BUILD is defined and CONFIG_TPL_FOO is set to 'y',
  *  0 otherwise.
+ *
+ * CONFIG_IS_ENABLED(FOO, (abc)) expands to
+ *  abc if CONFIG_SPL_BUILD is undefined and CONFIG_FOO is set to 'y',
+ *  abc if CONFIG_SPL_BUILD is defined and CONFIG_SPL_FOO is set to 'y',
+ *  abc if CONFIG_TPL_BUILD is defined and CONFIG_TPL_FOO is set to 'y',
+ *  nothing otherwise.
+ *
+ * CONFIG_IS_ENABLED(FOO, (abc), (def)) expands to
+ *  abc if CONFIG_SPL_BUILD is undefined and CONFIG_FOO is set to 'y',
+ *  abc if CONFIG_SPL_BUILD is defined and CONFIG_SPL_FOO is set to 'y',
+ *  abc if CONFIG_TPL_BUILD is defined and CONFIG_TPL_FOO is set to 'y',
+ *  def otherwise.
+ *
+ * The optional second and third arguments must be parenthesized; that
+ * allows one to include a trailing comma, e.g. for use in
+ *
+ * CONFIG_IS_ENABLED(ACME, ({.compatible = "acme,frobnozzle"},))
+ *
+ * which adds an entry to the array being defined if CONFIG_ACME (or
+ * CONFIG_SPL_ACME/CONFIG_TPL_ACME, depending on build context) is
+ * set, and nothing otherwise.
  */
-#define CONFIG_IS_ENABLED(option) \
-	(config_enabled(CONFIG_VAL(option)))
+
+#define CONFIG_IS_ENABLED(option, ...)					\
+	__concat(__CONFIG_IS_ENABLED_, __count_args(option, ##__VA_ARGS__)) (option, ##__VA_ARGS__)
+
 
 #endif /* __LINUX_KCONFIG_H */
