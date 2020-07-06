5a8d1f60b2505cf2ee116ac38e54e65b757a1773
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-07-06 07:48:14 +0200

    fs/fat: reduce data size for FAT_WRITE
    Allocated tmpbuf_cluster dynamically to reduce the data size added by
    compiling with CONFIG_FAT_WRITE.
    
    Reported-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- fs/fat/fat_write.c
+++ fs/fat/fat_write.c
@@ -500,8 +500,6 @@ flush_dir(fat_itr *itr)
 			   nsects * mydata->sect_size);
 }
 
-static __u8 tmpbuf_cluster[MAX_CLUSTSIZE] __aligned(ARCH_DMA_MINALIGN);
-
 /*
  * Read and modify data on existing and consecutive cluster blocks
  */
@@ -509,6 +507,7 @@ static int
 get_set_cluster(fsdata *mydata, __u32 clustnum, loff_t pos, __u8 *buffer,
 		loff_t size, loff_t *gotsize)
 {
+	static u8 *tmpbuf_cluster;
 	unsigned int bytesperclust = mydata->clust_size * mydata->sect_size;
 	__u32 startsect;
 	loff_t wsize;
@@ -517,6 +516,12 @@ get_set_cluster(fsdata *mydata, __u32 clustnum, loff_t pos, __u8 *buffer,
 	*gotsize = 0;
 	if (!size)
 		return 0;
+
+	if (!tmpbuf_cluster) {
+		tmpbuf_cluster = memalign(ARCH_DMA_MINALIGN, MAX_CLUSTSIZE);
+		if (!tmpbuf_cluster)
+			return -1;
+	}
 
 	assert(pos < bytesperclust);
 	startsect = clust_to_sect(mydata, clustnum);
