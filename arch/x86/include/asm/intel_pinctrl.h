59cf26480b6598330aba492733aec05b0a67303d
Simon Glass <sjg@chromium.org>
2020-07-07 21:32:23 -0600

    x86: pinctrl: Drop the acpi_path member
    This is in the device tree now, so drop the unnecessary field here.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Wolfgang Wallner <wolfgang.wallner@br-automation.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/x86/include/asm/intel_pinctrl.h
+++ arch/x86/include/asm/intel_pinctrl.h
@@ -99,7 +99,6 @@ struct pad_group {
  * groups exist inside a community
  *
  * @name: Community name
- * @acpi_path: ACPI path
  * @num_gpi_regs: number of gpi registers in community
  * @max_pads_per_group: number of pads in each group; number of pads bit-mapped
  *	in each GPI status/en and Host Own Reg
@@ -120,7 +119,6 @@ struct pad_group {
  */
 struct pad_community {
 	const char *name;
-	const char *acpi_path;
 	size_t num_gpi_regs;
 	size_t max_pads_per_group;
 	uint first_pad;
