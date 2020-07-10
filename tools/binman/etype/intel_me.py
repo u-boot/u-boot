c6162cf87c8896094213023266737667813a86d8
Simon Glass <sjg@chromium.org>
2020-07-09 18:39:32 -0600

    binman: Fix a few typos in the entry docs
    Some typos have been fixed in the generated entry docs but the code was
    not updated. Fix this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/etype/intel_me.py
+++ tools/binman/etype/intel_me.py
@@ -16,7 +16,7 @@ class Entry_intel_me(Entry_blob):
 
     This file contains code used by the SoC that is required to make it work.
     The Management Engine is like a background task that runs things that are
-    not clearly documented, but may include keyboard, deplay and network
+    not clearly documented, but may include keyboard, display and network
     access. For platform that use ME it is not possible to disable it. U-Boot
     does not directly execute code in the ME binary.
 
