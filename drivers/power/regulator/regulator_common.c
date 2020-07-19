ad2f4ac39b13883e6bc1595137e61a4679906e42
Simon Glass <sjg@chromium.org>
2020-07-19 10:15:44 -0600

    power: Tidy up inclusion of regulator_common.h
    This file should not include common.h and dm.h so remove them. Also move
    the inclusion of this file to after the normal includes.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>

--- drivers/power/regulator/regulator_common.c
+++ drivers/power/regulator/regulator_common.c
@@ -4,11 +4,13 @@
  * Sven Schwermer <sven.svenschwermer@disruptive-technologies.com>
  */
 
-#include "regulator_common.h"
 #include <common.h>
+#include <dm.h>
 #include <log.h>
 #include <linux/delay.h>
 #include <power/regulator.h>
+
+#include "regulator_common.h"
 
 int regulator_common_ofdata_to_platdata(struct udevice *dev,
 	struct regulator_common_platdata *dev_pdata, const char *enable_gpio_name)
--- drivers/power/regulator/regulator_common.h
+++ drivers/power/regulator/regulator_common.h
@@ -7,9 +7,7 @@
 #ifndef _REGULATOR_COMMON_H
 #define _REGULATOR_COMMON_H
 
-#include <common.h>
 #include <asm/gpio.h>
-#include <dm.h>
 
 struct regulator_common_platdata {
 	struct gpio_desc gpio; /* GPIO for regulator enable control */
