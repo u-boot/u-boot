e1174c566a61c863db1b782935269acba00e9281
AKASHI Takahiro <takahiro.akashi@linaro.org>
2020-07-21 19:35:24 +0900

    test/py: efi_secboot: add test for intermediate certificates
    In this test case, an image may have a signature with additional
    intermediate certificates. A chain of trust will be followed and all
    the certificates in the middle of chain must be verified before loading.
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>

--- test/py/tests/test_efi_secboot/defs.py
+++ test/py/tests/test_efi_secboot/defs.py
@@ -5,4 +5,10 @@ GUID = '11111111-2222-3333-4444-123456789abc'
 
 # v1.5.1 or earlier of efitools has a bug in sha256 calculation, and
 # you need build a newer version on your own.
+# The path must terminate with '/'.
 EFITOOLS_PATH = ''
+
+# "--addcert" option of sbsign must be available, otherwise
+# you need build a newer version on your own.
+# The path must terminate with '/'.
+SBSIGN_PATH = ''
