a965f4dfb522532f07f7ab18b54de1e01677ffea
Michal Simek <michal.simek@xilinx.com>
2020-07-22 13:58:54 +0200

    dt-bindings: Sync include/dt-bindings/phy/phy.h from Linux
    Add 4 new phy types which are present in Linux kernel.
    DP and SGMII types are used on Xilinx ZynqMP devices.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/dt-bindings/phy/phy.h
+++ include/dt-bindings/phy/phy.h
@@ -15,5 +15,9 @@
 #define PHY_TYPE_PCIE		2
 #define PHY_TYPE_USB2		3
 #define PHY_TYPE_USB3		4
+#define PHY_TYPE_UFS		5
+#define PHY_TYPE_DP		6
+#define PHY_TYPE_XPCS		7
+#define PHY_TYPE_SGMII		8
 
 #endif /* _DT_BINDINGS_PHY */
