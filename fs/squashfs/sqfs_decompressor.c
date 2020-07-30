3634b35089df7e6edfe034c26e4243dc708b6382
Joao Marcos Costa <joaomarcos.costa@bootlin.com>
2020-07-30 15:33:50 +0200

    fs/squashfs: add support for zlib decompression
    Add call to zlib's 'uncompress' function. Add function to display the
    right error message depending on the decompression's return value.
    
    Signed-off-by: Joao Marcos Costa <joaomarcos.costa@bootlin.com>

--- fs/squashfs/sqfs_decompressor.c
+++ fs/squashfs/sqfs_decompressor.c
@@ -9,10 +9,30 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#if IS_ENABLED(CONFIG_ZLIB)
+#include <u-boot/zlib.h>
+#endif
 
 #include "sqfs_decompressor.h"
 #include "sqfs_filesystem.h"
 #include "sqfs_utils.h"
+
+#if IS_ENABLED(CONFIG_ZLIB)
+static void zlib_decompression_status(int ret)
+{
+	switch (ret) {
+	case Z_BUF_ERROR:
+		printf("Error: 'dest' buffer is not large enough.\n");
+		break;
+	case Z_DATA_ERROR:
+		printf("Error: corrupted compressed data.\n");
+		break;
+	case Z_MEM_ERROR:
+		printf("Error: insufficient memory.\n");
+		break;
+	}
+}
+#endif
 
 int sqfs_decompress(u16 comp_type, void *dest, unsigned long *dest_len,
 		    void *source, u32 lenp)
@@ -20,6 +40,16 @@ int sqfs_decompress(u16 comp_type, void *dest, unsigned long *dest_len,
 	int ret = 0;
 
 	switch (comp_type) {
+#if IS_ENABLED(CONFIG_ZLIB)
+	case SQFS_COMP_ZLIB:
+		ret = uncompress(dest, dest_len, source, lenp);
+		if (ret) {
+			zlib_decompression_status(ret);
+			return -EINVAL;
+		}
+
+		break;
+#endif
 	default:
 		printf("Error: unknown compression type.\n");
 		return -EINVAL;
