0526460a500a94c904219f3e5c3c62ee35d6a2f3
Suman Anna <s-anna@ti.com>
2020-08-05 22:44:15 +0530

    board: ti: j721e: Update fdt fixup logic for interconnect nodes
    The DT nodes on J721E SoCs currently use a node name "interconnect" for
    the various interconnects. This name is not following the DT schema, and
    should simply be "bus". Update the fdt fixup logic to use both the current
    and the expected corrected path names so that this logic won't be broken
    with newer kernels.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- board/ti/j721e/evm.c
+++ board/ti/j721e/evm.c
@@ -92,7 +92,10 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 {
 	int ret;
 
-	ret = fdt_fixup_msmc_ram(blob, "/interconnect@100000", "sram@70000000");
+	ret = fdt_fixup_msmc_ram(blob, "/bus@100000", "sram@70000000");
+	if (ret < 0)
+		ret = fdt_fixup_msmc_ram(blob, "/interconnect@100000",
+					 "sram@70000000");
 	if (ret)
 		printf("%s: fixing up msmc ram failed %d\n", __func__, ret);
 
