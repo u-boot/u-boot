858056b0e00ba4abe6f85ed1043816d69e05a99f
Madalin Bucur <madalin.bucur@oss.nxp.com>
2020-09-11 10:24:09 +0300

    driver: net: fm: add support for XFI
    All the 10G ports that were working in XFI mode were described as
    using XGMII (as PHY_INTERFACE_MODE_XFI was not added at the time).
    Add the minimal changes required for the FMan code to support XFI.
    
    Signed-off-by: Madalin Bucur <madalin.bucur@oss.nxp.com>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- drivers/net/fm/memac.c
+++ drivers/net/fm/memac.c
@@ -98,6 +98,7 @@ static void memac_set_interface_mode(struct fsl_enet_mac *mac,
 		if_mode &= ~IF_MODE_MASK;
 		if_mode |= (IF_MODE_GMII);
 		break;
+	case PHY_INTERFACE_MODE_XFI:
 	case PHY_INTERFACE_MODE_XGMII:
 		if_mode &= ~IF_MODE_MASK;
 		if_mode |= IF_MODE_XGMII;
@@ -106,7 +107,7 @@ static void memac_set_interface_mode(struct fsl_enet_mac *mac,
 		break;
 	}
 	/* Enable automatic speed selection for Non-XGMII */
-	if (type != PHY_INTERFACE_MODE_XGMII)
+	if (type != PHY_INTERFACE_MODE_XGMII && type != PHY_INTERFACE_MODE_XFI)
 		if_mode |= IF_MODE_EN_AUTO;
 
 	if (type == PHY_INTERFACE_MODE_RGMII ||
