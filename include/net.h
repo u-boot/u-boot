c3f0278e29ffae81dc24c997523a8eafba503a0c
Sean Anderson <seanga2@gmail.com>
2020-09-12 17:45:43 -0400

    net: Expose some errors generated in net_init
    net_init does not always succeed, and there is no existing mechanism to
    discover errors. This patch allows callers of net_init (such as net_init)
    to handle errors. The root issue is that eth_get_dev can fail, but
    net_init_loop doesn't expose that. The ideal way to fix eth_get_dev would
    be to return an error with ERR_PTR, but there are a lot of callers, and all
    of them just check if it's NULL. Another approach would be to change the
    signature to something like
    
    int eth_get_dev(struct udevice **pdev)
    
    but that would require rewriting all of the many callers.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/net.h
+++ include/net.h
@@ -593,7 +593,7 @@ extern int net_ntp_time_offset;			/* offset time from UTC */
 #endif
 
 /* Initialize the network adapter */
-void net_init(void);
+int net_init(void);
 int net_loop(enum proto_t);
 
 /* Load failed.	 Start again. */
