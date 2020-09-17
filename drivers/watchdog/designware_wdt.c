d9b9c91b5d0ee1044cc638f7ea809a3836cf168e
Jack Mitchell <ml@embed.me.uk>
2020-09-17 10:30:40 +0100

    wdt: designware: fix timeout calculation due to expecting KHz
    The timeout calculation is based on the clk being in KHz but
    the clk api returns the clk value in Hz. Convert this to KHz
    to calculate the correct timeout value.
    
    Signed-off-by: Jack Mitchell <ml@embed.me.uk>

--- drivers/watchdog/designware_wdt.c
+++ drivers/watchdog/designware_wdt.c
@@ -130,7 +130,7 @@ static int designware_wdt_probe(struct udevice *dev)
 	if (ret)
 		return ret;
 
-	priv->clk_khz = clk_get_rate(&clk);
+	priv->clk_khz = clk_get_rate(&clk) / 1000;
 	if (!priv->clk_khz)
 		return -EINVAL;
 #else
