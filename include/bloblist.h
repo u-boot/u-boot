e9b6b2c77dd57b591d025f6895a779930a39a5ff
Simon Glass <sjg@chromium.org>
2020-09-19 18:49:30 -0600

    bloblist: Fix up a few comments
    Adjust a few comments to make the meaning clearer.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/bloblist.h
+++ include/bloblist.h
@@ -68,7 +68,7 @@ enum bloblist_tag_t {
  *	the bloblist can grow up to this size. This starts out as
  *	sizeof(bloblist_hdr) since we need at least that much space to store a
  *	valid bloblist
- * @spare: Space space
+ * @spare: Spare space (for future use)
  * @chksum: CRC32 for the entire bloblist allocated area. Since any of the
  *	blobs can be altered after being created, this checksum is only valid
  *	when the bloblist is finalised before jumping to the next stage of boot.
@@ -115,7 +115,7 @@ struct bloblist_rec {
  * Searches the bloblist and returns the blob with the matching tag
  *
  * @tag:	Tag to search for (enum bloblist_tag_t)
- * @size:	Expected size of the blob
+ * @size:	Expected size of the blob, or 0 for any size
  * @return pointer to blob if found, or NULL if not found, or a blob was found
  *	but it is the wrong size
  */
