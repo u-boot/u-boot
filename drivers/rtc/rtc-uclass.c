67507e4aab5b58c62cd57b855ce97a83d882479c
Simon Glass <sjg@chromium.org>
2020-10-03 11:31:34 -0600

    sandbox: Fix up building for of-platdata
    There is no devicetree with of-platdata. Update a few uclasses to allow
    them to be built for sandbox_spl. Also drop the i2c-gpio from SPL to avoid
    build errors, since it does not support of-platdata.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/rtc/rtc-uclass.c
+++ drivers/rtc/rtc-uclass.c
@@ -174,5 +174,7 @@ int rtc_write32(struct udevice *dev, unsigned int reg, u32 value)
 UCLASS_DRIVER(rtc) = {
 	.name		= "rtc",
 	.id		= UCLASS_RTC,
+#if !CONFIG_IS_ENABLED(OF_PLATDATA)
 	.post_bind	= dm_scan_fdt_dev,
+#endif
 };
