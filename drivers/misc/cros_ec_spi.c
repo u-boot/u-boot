ed0f868d501ee2bc31391210c214e9d07ae0874b
Simon Glass <sjg@chromium.org>
2020-10-05 05:27:00 -0600

    cros_ec: Fix up driver names to avoid dtoc warnings
    Fix the dtoc warning in these file by using a driver name that matches the
    compatible string.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- drivers/misc/cros_ec_spi.c
+++ drivers/misc/cros_ec_spi.c
@@ -184,8 +184,8 @@ static const struct udevice_id cros_ec_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(cros_ec_spi) = {
-	.name		= "cros_ec_spi",
+U_BOOT_DRIVER(google_cros_ec_spi) = {
+	.name		= "google_cros_ec_spi",
 	.id		= UCLASS_CROS_EC,
 	.of_match	= cros_ec_ids,
 	.probe		= cros_ec_probe,
