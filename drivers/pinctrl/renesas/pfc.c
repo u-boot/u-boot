c5f376258883947ddcd8b89bb0f458c8008eb0a4
Biju Das <biju.das.jz@bp.renesas.com>
2020-10-28 10:34:21 +0000

    pinctrl: renesas: r8a77965: Add R8A774B1 PFC support
    Renesas RZ/G2N (r8a774b1) is pin compatible with R-Car M3-N (r8a77965),
    however it doesn't have several automotive specific peripherals. Add
    a r8a77965 specific pin groups/functions along with common pin
    groups/functions for supporting both r8a77965 and r8a774b1 SoC.
    
    PFC changes are synced from mainline linux-5.9 commit
    bbf5c979011a ("Linux 5.9").
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Reviewed-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>

--- drivers/pinctrl/renesas/pfc.c
+++ drivers/pinctrl/renesas/pfc.c
@@ -33,6 +33,7 @@ enum sh_pfc_model {
 	SH_PFC_R8A7795,
 	SH_PFC_R8A7796,
 	SH_PFC_R8A774A1,
+	SH_PFC_R8A774B1,
 	SH_PFC_R8A77965,
 	SH_PFC_R8A77970,
 	SH_PFC_R8A77980,
@@ -858,6 +859,10 @@ static int sh_pfc_pinctrl_probe(struct udevice *dev)
 	if (model == SH_PFC_R8A774A1)
 		priv->pfc.info = &r8a774a1_pinmux_info;
 #endif
+#ifdef CONFIG_PINCTRL_PFC_R8A774B1
+	if (model == SH_PFC_R8A774B1)
+		priv->pfc.info = &r8a774b1_pinmux_info;
+#endif
 #ifdef CONFIG_PINCTRL_PFC_R8A77965
 	if (model == SH_PFC_R8A77965)
 		priv->pfc.info = &r8a77965_pinmux_info;
@@ -933,6 +938,12 @@ static const struct udevice_id sh_pfc_pinctrl_ids[] = {
 	{
 		.compatible = "renesas,pfc-r8a774a1",
 		.data = SH_PFC_R8A774A1,
+	},
+#endif
+#ifdef CONFIG_PINCTRL_PFC_R8A774B1
+	{
+		.compatible = "renesas,pfc-r8a774b1",
+		.data = SH_PFC_R8A774B1,
 	},
 #endif
 #ifdef CONFIG_PINCTRL_PFC_R8A77965
