d99f163792089f85d57dad5c63db12a5017766c1
Michal Simek <michal.simek@xilinx.com>
2020-11-06 15:05:57 +0100

    tpm: spi: Cleanup source code
    There is no need for GD to be used and priv variable is unused.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/tpm/tpm2_tis_spi.c
+++ drivers/tpm/tpm2_tis_spi.c
@@ -30,8 +30,6 @@
 #include "tpm_tis.h"
 #include "tpm_internal.h"
 
-DECLARE_GLOBAL_DATA_PTR;
-
 #define TPM_ACCESS(l)			(0x0000 | ((l) << 12))
 #define TPM_INT_ENABLE(l)               (0x0008 | ((l) << 12))
 #define TPM_STS(l)			(0x0018 | ((l) << 12))
@@ -520,7 +518,6 @@ static int tpm_tis_spi_cleanup(struct udevice *dev)
 static int tpm_tis_spi_open(struct udevice *dev)
 {
 	struct tpm_chip *chip = dev_get_priv(dev);
-	struct tpm_chip_priv *priv = dev_get_uclass_priv(dev);
 
 	if (chip->is_open)
 		return -EBUSY;
