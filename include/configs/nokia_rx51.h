2466643950bbc6c05f0f4421d9cb1f4f70b4b688
Pali Rohár <pali@kernel.org>
2020-11-29 16:50:33 +0100

    Nokia RX-51: Enable usbtty serial console by default
    Now when usbtty serial console is fixed in U-Boot enable CONFIG_USB_TTY for
    Nokia RX-51 board by default.
    
    Fix also USB product id as U-Boot ignores CONFIG_USBD_PRODUCTID macro and
    include U-Boot string into USB product name to indicate usage of U-Boot.
    
    CONFIG_CONSOLE_MUX is already used and U-Boot console is available for
    all in/out devices. Therefore there is no need to have separate commands
    'run sercon', 'run usbcon' and 'run vgacon', so remove them.
    
    As space for U-Boot is limited to 256kB, disable some other unused options
    so CONFIG_USB_TTY can be enabled.
    
    Nokia RX-51 does not have easily accessible UART serial console so the only
    option for easy debugging is to use device's keyboard+screen or this usbtty
    serial console over USB.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>

--- include/configs/nokia_rx51.h
+++ include/configs/nokia_rx51.h
@@ -70,10 +70,12 @@
 
 /* USB device configuration */
 #define CONFIG_USB_DEVICE
+#define CONFIG_USB_TTY
 #define CONFIG_USBD_VENDORID		0x0421
-#define CONFIG_USBD_PRODUCTID		0x01c8
+#define CONFIG_USBD_PRODUCTID_CDCACM	0x01c8
+#define CONFIG_USBD_PRODUCTID_GSERIAL	0x01c8
 #define CONFIG_USBD_MANUFACTURER	"Nokia"
-#define CONFIG_USBD_PRODUCT_NAME	"N900"
+#define CONFIG_USBD_PRODUCT_NAME	"N900 (U-Boot)"
 
 #define GPIO_SLIDE			71
 
@@ -108,15 +110,9 @@ int rx51_kp_getc(struct stdio_dev *sdev);
 /* Environment information */
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"usbtty=cdc_acm\0" \
-	"stdin=serial,vga\0" \
-	"stdout=serial,vga\0" \
-	"stderr=serial,vga\0" \
-	"setcon=setenv stdin ${con};" \
-		"setenv stdout ${con};" \
-		"setenv stderr ${con}\0" \
-	"sercon=setenv con serial; run setcon\0" \
-	"usbcon=setenv con usbtty; run setcon\0" \
-	"vgacon=setenv con vga; run setcon\0" \
+	"stdin=usbtty,serial,vga\0" \
+	"stdout=usbtty,serial,vga\0" \
+	"stderr=usbtty,serial,vga\0" \
 	"slide=gpio input " __stringify(GPIO_SLIDE) "\0" \
 	"switchmmc=mmc dev ${mmcnum}\0" \
 	"kernaddr=0x82008000\0" \
@@ -198,9 +194,6 @@ int rx51_kp_getc(struct stdio_dev *sdev);
 #define CONFIG_POSTBOOTMENU \
 	"echo;" \
 	"echo Extra commands:;" \
-	"echo run sercon - Use serial port for control.;" \
-	"echo run usbcon - Use usbtty for control.;" \
-	"echo run vgacon - Use framebuffer/keyboard.;" \
 	"echo run sdboot - Boot from SD card slot.;" \
 	"echo run emmcboot - Boot internal eMMC memory.;" \
 	"echo run attachboot - Boot attached kernel image.;" \
