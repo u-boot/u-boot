a0b74acb9b4a21405e7441c808d538a460a3678f
Jaehoon Chung <jh80.chung@samsung.com>
2020-12-07 17:18:51 +0900

    common: splash_source: fix -Wint-to-pointer-cast warning
    Fix -Wint-to-pointer-cast warning
    
    common/splash_source.c: In function 'splash_load_raw':
    common/splash_source.c:100:12: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
      100 |  bmp_hdr = (struct bmp_header *)bmp_load_addr;
          |            ^
    common/splash_source.c: In function 'splash_sf_read_raw':
    common/splash_source.c:39:47: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
       39 |  return spi_flash_read(sf, offset, read_size, (void *)bmp_load_addr);
          |                                               ^
    
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- common/splash_source.c
+++ common/splash_source.c
@@ -37,7 +37,7 @@ static int splash_sf_read_raw(u32 bmp_load_addr, int offset, size_t read_size)
 			return -ENODEV;
 	}
 
-	return spi_flash_read(sf, offset, read_size, (void *)bmp_load_addr);
+	return spi_flash_read(sf, offset, read_size, (void *)(uintptr_t)bmp_load_addr);
 }
 #else
 static int splash_sf_read_raw(u32 bmp_load_addr, int offset, size_t read_size)
@@ -98,7 +98,7 @@ static int splash_load_raw(struct splash_location *location, u32 bmp_load_addr)
 	if (res < 0)
 		return res;
 
-	bmp_hdr = (struct bmp_header *)bmp_load_addr;
+	bmp_hdr = (struct bmp_header *)(uintptr_t)bmp_load_addr;
 	bmp_size = le32_to_cpu(bmp_hdr->file_size);
 
 	if (bmp_load_addr + bmp_size >= gd->start_addr_sp)
