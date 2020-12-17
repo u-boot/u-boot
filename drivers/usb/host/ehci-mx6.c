4de51cc25b5215ef5b05dfa9d13836500810751b
Simon Glass <sjg@chromium.org>
2020-12-16 21:20:20 -0700

    usb: ehci-mx6: Drop assignment of sequence number
    This hack cannot work in the new sequence-numbering scheme. Remove it
    while we wait for the maintainer to complete DM conversion as noted in
    the existing comment.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/usb/host/ehci-mx6.c
+++ drivers/usb/host/ehci-mx6.c
@@ -569,10 +569,16 @@ static int ehci_usb_bind(struct udevice *dev)
 	 * With these changes in place, the ad-hoc indexing goes away and
 	 * the driver is fully converted to DT probing.
 	 */
-	u32 controller_spacing = is_mx7() ? 0x10000 : 0x200;
-	fdt_addr_t addr = devfdt_get_addr_index(dev, 0);
 
-	dev->req_seq = (addr - USB_BASE_ADDR) / controller_spacing;
+	/*
+	 * FIXME: This cannot work with the new sequence numbers.
+	 * Please complete the DM conversion.
+	 *
+	 * u32 controller_spacing = is_mx7() ? 0x10000 : 0x200;
+	 * fdt_addr_t addr = devfdt_get_addr_index(dev, 0);
+	 *
+	 * dev->req_seq = (addr - USB_BASE_ADDR) / controller_spacing;
+	 */
 
 	return 0;
 }
