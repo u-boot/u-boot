37648b600cd16848e689e341b20da71356e24212
Marc Ferland <ferlandm@amotus.ca>
2020-12-22 14:24:12 -0500

    arm: dart6ul: read and print SoM info from eeprom on startup
    The dart6ul has an i2c eeprom at 0x50 which contains, among other
    things, the manufacturing/revision/options info of the SoM. This patch
    replaces the current checkboard() implementation with a more
    exhaustive one based on the content of the eeprom.
    
    Since this code uses the new driver model, some changes were also
    required in the DTS to make the nodes related to i2c available before
    relocation.
    
    This code was inspired from the supported u-boot code from Variscite
    which can be found here:
    
    https://github.com/varigit/uboot-imx/tree/imx_v2018.03_4.14.78_1.0.0_ga_var02
    
    New output example:
    
    Board: PN: VSM-6UL-705B, Assy: AS1812142257, Date: 2019 Feb 17
           Storage: eMMC, Wifi: yes, DDR: 1024 MiB, Rev: 2.4G
    
    Signed-off-by: Marc Ferland <ferlandm@amotus.ca>
    Reviewed-by: Fabio Estevam <festevam@gmail.com>

--- board/variscite/dart_6ul/dart_6ul.c
+++ board/variscite/dart_6ul/dart_6ul.c
@@ -12,8 +12,11 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/mach-imx/iomux-v3.h>
 #include <asm/mach-imx/mxc_i2c.h>
+#include <dm.h>
 #include <fsl_esdhc_imx.h>
+#include <i2c_eeprom.h>
 #include <linux/bitops.h>
+#include <malloc.h>
 #include <miiphy.h>
 #include <netdev.h>
 #include <usb.h>
@@ -222,9 +225,108 @@ int board_init(void)
 	return 0;
 }
 
+/* length of strings stored in the eeprom */
+#define DART6UL_PN_LEN   16
+#define DART6UL_ASSY_LEN 16
+#define DART6UL_DATE_LEN 12
+
+/* eeprom content, 512 bytes */
+struct dart6ul_info {
+	u32 magic;
+	u8 partnumber[DART6UL_PN_LEN];
+	u8 assy[DART6UL_ASSY_LEN];
+	u8 date[DART6UL_DATE_LEN];
+	u32 custom_addr_val[32];
+	struct cmd {
+		u8 addr;
+		u8 index;
+	} custom_cmd[150];
+	u8 res[33];
+	u8 som_info;
+	u8 ddr_size;
+	u8 crc;
+} __attribute__ ((__packed__));
+
+#define DART6UL_INFO_STORAGE_GET(n) ((n) & 0x3)
+#define DART6UL_INFO_WIFI_GET(n)    ((n) >> 2 & 0x1)
+#define DART6UL_INFO_REV_GET(n)     ((n) >> 3 & 0x3)
+#define DART6UL_DDRSIZE_IN_MIB(n)   ((n) << 8)
+#define DART6UL_INFO_MAGIC          0x32524156
+
+static const char *som_info_storage_to_str(u8 som_info)
+{
+	switch (DART6UL_INFO_STORAGE_GET(som_info)) {
+	case 0x0: return "none (SD only)";
+	case 0x1: return "NAND";
+	case 0x2: return "eMMC";
+	default: return "unknown";
+	}
+}
+
+static const char *som_info_rev_to_str(u8 som_info)
+{
+	switch (DART6UL_INFO_REV_GET(som_info)) {
+	case 0x0: return "2.4G";
+	case 0x1: return "5G";
+	default: return "unknown";
+	}
+}
+
 int checkboard(void)
 {
-	puts("Board: Variscite DART-6UL Evaluation Kit\n");
+	const char *path = "eeprom0";
+	struct dart6ul_info *info;
+	struct udevice *dev;
+	int ret, off;
+
+	off = fdt_path_offset(gd->fdt_blob, path);
+	if (off < 0) {
+		printf("%s: fdt_path_offset() failed: %d\n", __func__, off);
+		return off;
+	}
+
+	ret = uclass_get_device_by_of_offset(UCLASS_I2C_EEPROM, off, &dev);
+	if (ret) {
+		printf("%s: uclass_get_device_by_of_offset() failed: %d\n", __func__, ret);
+		return ret;
+	}
+
+	info = malloc(sizeof(struct dart6ul_info));
+	if (!info)
+		return -ENOMEM;
+
+	ret = i2c_eeprom_read(dev, 0, (uint8_t *)info,
+			      sizeof(struct dart6ul_info));
+	if (ret) {
+		printf("%s: i2c_eeprom_read() failed: %d\n", __func__, ret);
+		free(info);
+		return ret;
+	}
+
+	if (info->magic != DART6UL_INFO_MAGIC) {
+		printf("Board: Invalid board info magic: 0x%08x, expected 0x%08x\n",
+		       info->magic, DART6UL_INFO_MAGIC);
+		/* do not fail if the content is invalid */
+		free(info);
+		return 0;
+	}
+
+	/* make sure strings are null terminated */
+	info->partnumber[DART6UL_PN_LEN - 1] = '\0';
+	info->assy[DART6UL_ASSY_LEN - 1] = '\0';
+	info->date[DART6UL_DATE_LEN - 1] = '\0';
+
+	printf("Board: PN: %s, Assy: %s, Date: %s\n"
+	       "       Storage: %s, Wifi: %s, DDR: %d MiB, Rev: %s\n",
+	       info->partnumber,
+	       info->assy,
+	       info->date,
+	       som_info_storage_to_str(info->som_info),
+	       DART6UL_INFO_WIFI_GET(info->som_info) ? "yes" : "no",
+	       DART6UL_DDRSIZE_IN_MIB(info->ddr_size),
+	       som_info_rev_to_str(info->som_info));
+
+	free(info);
 
 	return 0;
 }
