50c51a0570a5f133c56fd73bddff3a3b8c554fe5
T Karthik Reddy <t.karthik.reddy@xilinx.com>
2021-01-05 21:02:12 -0700

    spi: spi-mem: Move execop functionality to controller driver
    Move all xilinx specific changes from spi-mem framework.
    
    Signed-off-by: T Karthik Reddy <t.karthik.reddy@xilinx.com>
    Acked-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
    State: pending

--- drivers/spi/spi-mem.c
+++ drivers/spi/spi-mem.c
@@ -216,7 +216,7 @@ int spi_mem_exec_op(struct spi_slave *slave, const struct spi_mem_op *op)
 	const u8 *tx_buf = NULL;
 	u8 *rx_buf = NULL;
 	int op_len;
-	u32 flag = 0;
+	u32 flag;
 	int ret;
 	int i;
 
@@ -373,40 +373,26 @@ int spi_mem_exec_op(struct spi_slave *slave, const struct spi_mem_op *op)
 		pos += op->addr.nbytes;
 	}
 
-	if (op->dummy.nbytes) {
+	if (op->dummy.nbytes)
 		memset(op_buf + pos, 0xff, op->dummy.nbytes);
-		slave->dummy_bytes = op->dummy.nbytes;
-	}
-
-	if (slave->flags & SPI_XFER_U_PAGE)
-		flag |= SPI_XFER_U_PAGE;
-	if (slave->flags & SPI_XFER_LOWER)
-		flag |= SPI_XFER_LOWER;
-	if (slave->flags & SPI_XFER_UPPER)
-		flag |= SPI_XFER_UPPER;
-	if (slave->flags & SPI_XFER_STRIPE)
-		flag |= SPI_XFER_STRIPE;
 
 	/* 1st transfer: opcode + address + dummy cycles */
+	flag = SPI_XFER_BEGIN;
 	/* Make sure to set END bit if no tx or rx data messages follow */
 	if (!tx_buf && !rx_buf)
 		flag |= SPI_XFER_END;
 
-	ret = spi_xfer(slave, op_len * 8, op_buf, NULL, flag | SPI_XFER_BEGIN);
+	ret = spi_xfer(slave, op_len * 8, op_buf, NULL, flag);
 	if (ret)
 		return ret;
-
-	slave->dummy_bytes = 0;
 
 	/* 2nd transfer: rx or tx data path */
 	if (tx_buf || rx_buf) {
 		ret = spi_xfer(slave, op->data.nbytes * 8, tx_buf,
-			       rx_buf, flag | SPI_XFER_END);
+			       rx_buf, SPI_XFER_END);
 		if (ret)
 			return ret;
 	}
-
-	slave->flags &= ~SPI_XFER_MASK;
 
 	spi_release_bus(slave);
 
