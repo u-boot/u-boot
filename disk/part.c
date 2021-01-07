952d710c5b96ac8cd3778dbbec77a0e7ab1138da
Sean Anderson <sean.anderson@seco.com>
2021-01-07 10:20:58 -0500

    part: Support getting whole disk from part_get_info_by_dev_and_name_or_num
    This adds an option to part_get_info_by_dev_and_name_or_num to allow
    callers to specify whether whole-disk partitions are fine.
    
    Signed-off-by: Sean Anderson <sean.anderson@seco.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Patch-cc: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- disk/part.c
+++ disk/part.c
@@ -736,7 +736,8 @@ static int part_get_info_by_dev_and_name(const char *dev_iface,
 int part_get_info_by_dev_and_name_or_num(const char *dev_iface,
 					 const char *dev_part_str,
 					 struct blk_desc **dev_desc,
-					 struct disk_partition *part_info)
+					 struct disk_partition *part_info,
+					 int allow_whole_dev)
 {
 	int ret;
 
@@ -750,7 +751,7 @@ int part_get_info_by_dev_and_name_or_num(const char *dev_iface,
 	 * directly.
 	 */
 	ret = blk_get_device_part_str(dev_iface, dev_part_str,
-				      dev_desc, part_info, 1);
+				      dev_desc, part_info, allow_whole_dev);
 	if (ret < 0)
 		printf("Couldn't find partition %s %s\n",
 		       dev_iface, dev_part_str);
