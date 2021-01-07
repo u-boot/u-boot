4029d356e68c1d7d0061aa34065fd0598f98871f
Michael Walle <michael@walle.cc>
2021-01-08 00:08:57 +0100

    board: sl28: add network variant 1 support
    This variant has one network port connected via RGMII and doesn't have
    any TSN capabilities out-of-the-box. Instead it has all four SerDes
    lanes available for customer use.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- board/kontron/sl28/spl.c
+++ board/kontron/sl28/spl.c
@@ -17,6 +17,8 @@ int board_fit_config_name_match(const char *name)
 	int variant = sl28_variant();
 
 	switch (variant) {
+	case 1:
+		return strcmp(name, "fsl-ls1028a-kontron-sl28-var1");
 	case 3:
 		return strcmp(name, "fsl-ls1028a-kontron-sl28-var3");
 	case 4:
