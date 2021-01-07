869de44590bd86b256cbfec9e570a46407452a6a
Steffen Jaeckel <jaeckel-floss@eyet-services.de>
2021-01-07 15:42:37 +0100

    lib: add crypt subsystem
    Add the basic functionality required to support the standard crypt
    format.
    The files crypt-sha256.c and crypt-sha512.c originate from libxcrypt and
    their formatting is therefor retained.
    The integration is done via a crypt_compare() function in crypt.c.
    
    ```
    libxcrypt $ git describe --long --always --all
    tags/v4.4.17-0-g6b110bc
    ```
    
    Series-version: 5
    
    Series-changes: 1
    Added unit-tests of crypt_compare()
    Wrapped crypt functions to encapsulate errno
    
    Series-changes: 2
    Update Kconfig way of enabling, setting hashes etc.
    
    Series-changes: 3
    Add unit-tests for autoboot
    Introduce `bootstopusesha256` to allow fallback to plain SHA256-based
    hashing
    Add AUTOBOOT_FLUSH_STDIN option
    Drop the changes to bcm963158_ram_defconfig
    
    Series-changes: 4
    Fix depends for unit-tests
    
    Cover-letter:
    common: Introduce crypt-style password support
    
    This patchset introduces support for crypt-style passwords to unlock
    the console in autoboot mode.
    
    The implementation of crypt-sha256 and crypt-sha512 originate from
    libxcrypt at https://github.com/besser82/libxcrypt.git
    Version v4.4.17
    Git commit hash 6b110bc
    
    I didn't re-format those two files to make diffing to the original
    versions from libxcrypt easier, which leads to a huge load of
    checkpatch.pl warnings&errors. Please advise on whether they should be
    re-formatted or can be kept as is.
    
    The remaining warnings from checkpatch.pl are intentional resp. open for
    discussion.
    
    A sandbox defconfig with password entry has been added. I'm not sure
    whether this should be kept or not, it's just there as an example.
    
    Cheers,
    Steffen
    END
    
    Signed-off-by: Steffen Jaeckel <jaeckel-floss@eyet-services.de>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Heiko Schocher <hs@denx.de>

---
+++ lib/crypt/crypt-port.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/* Copyright (C) 2020 Steffen Jaeckel <jaeckel-floss@eyet-services.de> */
+
+#include <linux/types.h>
+#include <vsprintf.h>
+
+#define NO_GENSALT
+#define CRYPT_OUTPUT_SIZE 384
+#define ALG_SPECIFIC_SIZE 8192
+
+#define ARG_UNUSED(x) (x)
+
+#define static_assert(a, b) _Static_assert(a, b)
+
+#define strtoul(cp, endp, base) simple_strtoul(cp, endp, base)
+
+extern const unsigned char ascii64[65];
+
+#define b64t ((const char *)ascii64)
+
+void crypt_sha256crypt_rn(const char *phrase, size_t phr_size,
+			  const char *setting, size_t ARG_UNUSED(set_size),
+			  uint8_t *output, size_t out_size, void *scratch,
+			  size_t scr_size);
+void crypt_sha512crypt_rn(const char *phrase, size_t phr_size,
+			  const char *setting, size_t ARG_UNUSED(set_size),
+			  uint8_t *output, size_t out_size, void *scratch,
+			  size_t scr_size);
