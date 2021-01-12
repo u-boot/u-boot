6bfa331a6e22507ae839fb8474ce1b3fd58808df
Artem Lapkin <email2tema@gmail.com>
2021-01-12 19:42:12 +0800

    board: amlogic: vim3: fix setup ethernet mac from efuse
    Fix reading built-in ethernet MAC address from efuse
    
    NOTE: MAC is stored in ASCII format, 1bytes = 2characters by 0 offset
    
    if mac from efuse not valid we use meson_generate_serial_ethaddr
    
    NOTE: remake odroid-n2.c from Neil Armstrong <narmstrong@baylibre.com>
    
    Signed-off-by: Artem Lapkin <art@khadas.com>
    [narmstrong: remove MAC print & spurious new endline]
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- board/amlogic/vim3/vim3.c
+++ board/amlogic/vim3/vim3.c
@@ -139,25 +139,37 @@ int meson_ft_board_setup(void *blob, struct bd_info *bd)
 }
 
 #define EFUSE_MAC_OFFSET	0
-#define EFUSE_MAC_SIZE		6
+#define EFUSE_MAC_SIZE		12
+#define MAC_ADDR_LEN		6
 
 int misc_init_r(void)
 {
-	uint8_t mac_addr[EFUSE_MAC_SIZE];
+	u8 mac_addr[MAC_ADDR_LEN];
+	char efuse_mac_addr[EFUSE_MAC_SIZE], tmp[3];
 	ssize_t len;
 
 	meson_eth_init(PHY_INTERFACE_MODE_RGMII, 0);
 
 	if (!eth_env_get_enetaddr("ethaddr", mac_addr)) {
 		len = meson_sm_read_efuse(EFUSE_MAC_OFFSET,
-					  mac_addr, EFUSE_MAC_SIZE);
+					  efuse_mac_addr, EFUSE_MAC_SIZE);
 		if (len != EFUSE_MAC_SIZE)
 			return 0;
+
+		/* MAC is stored in ASCII format, 1bytes = 2characters */
+		for (int i = 0; i < 6; i++) {
+			tmp[0] = efuse_mac_addr[i * 2];
+			tmp[1] = efuse_mac_addr[i * 2 + 1];
+			tmp[2] = '\0';
+			mac_addr[i] = simple_strtoul(tmp, NULL, 16);
+		}
 
 		if (is_valid_ethaddr(mac_addr))
 			eth_env_set_enetaddr("ethaddr", mac_addr);
 		else
 			meson_generate_serial_ethaddr();
+
+		eth_env_get_enetaddr("ethaddr", mac_addr);
 	}
 
 	return 0;
