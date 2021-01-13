c661c511e9cfc86f18b1326b0f0d1e338a8c442a
Teresa Remmet <t.remmet@phytec.de>
2021-01-13 16:28:09 +0100

    board: phytec: imx8mp: Add PHYTEC phyCORE-i.MX8MP support
    Add initial support PHYTEC phyCORE-i.MX8MP SOM.
    
        Supported features:
         - 2GB LPDDR4 RAM
         - eMMC
         - external SD
         - debug UART2
         - watchdog
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Reviewed-by: Heiko Schocher <hs@denx.de>

---
+++ board/phytec/phycore_imx8mp/phycore-imx8mp.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2020 PHYTEC Messtechnik GmbH
+ * Author: Teresa Remmet <t.remmet@phytec.de>
+ */
+
+#include <common.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/io.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <env.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	return 0;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
+
+int board_late_init(void)
+{
+	switch (get_boot_device()) {
+	case SD2_BOOT:
+		env_set_ulong("mmcdev", 1);
+		break;
+	case MMC3_BOOT:
+		env_set_ulong("mmcdev", 2);
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
