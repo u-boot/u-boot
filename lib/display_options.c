ac42fe539ca991543c888e261e62b4187ec9bbee
Simon Glass <sjg@chromium.org>
2021-01-13 20:29:50 -0700

    display_options: Use USE_TINY_PRINTF for SPL check
    At present this code uses a simple printf() format if running in SPL. But
    SPL can use the full printf. Use USE_TINY_PRINTF instead.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- lib/display_options.c
+++ lib/display_options.c
@@ -169,11 +169,10 @@ int print_buffer(ulong addr, const void *data, uint width, uint count,
 				x = lb.us[i] = *(volatile uint16_t *)data;
 			else
 				x = lb.uc[i] = *(volatile uint8_t *)data;
-#if defined(CONFIG_SPL_BUILD)
-			printf(" %x", (uint)x);
-#else
-			printf(" %0*lx", width * 2, x);
-#endif
+			if (CONFIG_IS_ENABLED(USE_TINY_PRINTF))
+				printf(" %x", (uint)x);
+			else
+				printf(" %0*lx", width * 2, x);
 			data += width;
 		}
 
