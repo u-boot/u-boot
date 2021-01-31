6424fba1bc4e80c43eb5eb69b9e66c0d89d1b1cf
Bin Meng <bin.meng@windriver.com>
2021-01-31 20:36:06 +0800

    bdinfo: Change to use bdinfo_print_num_ll() where the number could be 64-bit
    There are some calls to bdinfo_print_num_l() with parameters that
    could be a 64-bit value on a 32-bit system. Change those calls to
    use bdinfo_print_num_ll() instead.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- cmd/bdinfo.c
+++ cmd/bdinfo.c
@@ -55,8 +55,8 @@ static void print_bi_dram(const struct bd_info *bd)
 	for (i = 0; i < CONFIG_NR_DRAM_BANKS; ++i) {
 		if (bd->bi_dram[i].size) {
 			bdinfo_print_num_l("DRAM bank",	i);
-			bdinfo_print_num_l("-> start",	bd->bi_dram[i].start);
-			bdinfo_print_num_l("-> size",	bd->bi_dram[i].size);
+			bdinfo_print_num_ll("-> start",	bd->bi_dram[i].start);
+			bdinfo_print_num_ll("-> size",	bd->bi_dram[i].size);
 		}
 	}
 }
