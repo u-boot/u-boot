220fa478fb1a84e51235b92506ff5d48415f0a8e
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-02-01 03:28:48 +0100

    fs/squashfs: NULL dereference in sqfs_closedir()
    sqfs_opendir() called in sqfs_size(), sqfs_read(), sqfs_exists() may fail
    leading to sqfs_closedir(NULL) being called. Do not dereference NULL.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- fs/squashfs/sqfs.c
+++ fs/squashfs/sqfs.c
@@ -1716,6 +1716,9 @@ void sqfs_closedir(struct fs_dir_stream *dirs)
 {
 	struct squashfs_dir_stream *sqfs_dirs;
 
+	if (!dirs)
+		return;
+
 	sqfs_dirs = (struct squashfs_dir_stream *)dirs;
 	free(sqfs_dirs->inode_table);
 	free(sqfs_dirs->dir_table);
