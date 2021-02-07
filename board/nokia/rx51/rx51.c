69b78cbf5e227acda0db94d2e6b6ad443d106a9c
Pali Rohár <pali@kernel.org>
2021-02-07 14:50:15 +0100

    Nokia RX-51: Move content of rx51.h to rx51.c
    After removal of MUX configuration there is no need to have extra rx51.h.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Reviewed-by: Lukasz Majewski <lukma@denx.de>
    Acked-by: Pavel Machek <pavel@ucw.cz>

--- board/nokia/rx51/rx51.c
+++ board/nokia/rx51/rx51.c
@@ -40,8 +40,23 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/mmc_host_def.h>
 
-#include "rx51.h"
 #include "tag_omap.h"
+
+/* Needed for ROM SMC call */
+struct emu_hal_params_rx51 {
+	u32 num_params;
+	u32 param1;
+	u32 param2;
+	u32 param3;
+	u32 param4;
+};
+
+#define ONENAND_GPMC_CONFIG1_RX51	0xfb001202
+#define ONENAND_GPMC_CONFIG2_RX51	0x00111100
+#define ONENAND_GPMC_CONFIG3_RX51	0x00020200
+#define ONENAND_GPMC_CONFIG4_RX51	0x11001102
+#define ONENAND_GPMC_CONFIG5_RX51	0x03101616
+#define ONENAND_GPMC_CONFIG6_RX51	0x90060000
 
 DECLARE_GLOBAL_DATA_PTR;
 
