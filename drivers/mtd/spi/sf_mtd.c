e2e748d82eeb92ec451bda1b5f8e6c9ae275ae16
Marek Behún <marek.behun@nic.cz>
2021-02-09 03:04:28 +0100

    mtd: spi-nor: allow registering multiple MTDs when DM is enabled
    Currently when the SPI_FLASH_MTD config option is enabled, only one SPI
    can be registered as MTD at any time - it is the last one probed (since
    with old non-DM model only one SPI NOR could be probed at any time).
    
    When DM is enabled, allow for registering multiple SPI NORs as MTDs by
    utilizing the nor->mtd structure, which is filled in by spi_nor_scan
    anyway, instead of filling a separate struct mtd_info.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>
    Reviewed-by: Pali Rohár <pali@kernel.org>
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Tested-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Cc: Jagan Teki <jagan@amarulasolutions.com>
    Cc: Priyanka Jain <priyanka.jain@nxp.com>
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Heiko Schocher <hs@denx.de>
    Cc: Jagan Teki <jagan@amarulasolutions.com>
    Cc: Patrick Delaunay <patrick.delaunay@st.com>

--- drivers/mtd/spi/sf_mtd.c
+++ drivers/mtd/spi/sf_mtd.c
@@ -10,6 +10,20 @@
 #include <linux/mtd/mtd.h>
 #include <spi_flash.h>
 
+#if CONFIG_IS_ENABLED(DM_SPI_FLASH)
+
+int spi_flash_mtd_register(struct spi_flash *flash)
+{
+	return add_mtd_device(&flash->mtd);
+}
+
+void spi_flash_mtd_unregister(struct spi_flash *flash)
+{
+	del_mtd_device(&flash->mtd);
+}
+
+#else /* !CONFIG_IS_ENABLED(DM_SPI_FLASH) */
+
 static struct mtd_info sf_mtd_info;
 static bool sf_mtd_registered;
 static char sf_mtd_name[8];
@@ -123,7 +137,7 @@ int spi_flash_mtd_register(struct spi_flash *flash)
 	return ret;
 }
 
-void spi_flash_mtd_unregister(void)
+void spi_flash_mtd_unregister(struct spi_flash *flash)
 {
 	int ret;
 
@@ -146,3 +160,5 @@ void spi_flash_mtd_unregister(void)
 	printf("Failed to unregister MTD %s and the spi_flash object is going away: you're in deep trouble!",
 	       sf_mtd_info.name);
 }
+
+#endif /* !CONFIG_IS_ENABLED(DM_SPI_FLASH) */
