41bacbe142024c6bb26e7ca233d6e845ba1f56ac
Tom Rini <trini@konsulko.com>
2021-02-09 21:42:53 -0500

    ppc: Remove MPC8610HPCD board
    This board relies on using CONFIG_LIBATA but does not enable CONFIG_AHCI.  The
    deadline for this conversion was the v2019.07 release.  The use of CONFIG_AHCI
    requires CONFIG_DM.  The deadline for this conversion was v2020.01.  Remove
    this board.
    
    Cc: Priyanka Jain <priyanka.jain@nxp.com>
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- board/freescale/common/pixis.h
+++ board/freescale/common/pixis.h
@@ -45,36 +45,6 @@ typedef struct pixis {
 	u8 res2[4];
 } __attribute__ ((packed)) pixis_t;
 
-#elif defined(CONFIG_TARGET_MPC8610HPCD)
-typedef struct pixis {
-	u8 id;
-	u8 ver;	/* also called arch */
-	u8 pver;
-	u8 csr;
-	u8 rst;
-	u8 pwr;
-	u8 aux;
-	u8 spd;
-	u8 brdcfg0;
-	u8 brdcfg1;
-	u8 res[4];
-	u8 led;
-	u8 serno;
-	u8 vctl;
-	u8 vstat;
-	u8 vcfgen0;
-	u8 vcfgen1;
-	u8 vcore0;
-	u8 res1;
-	u8 vboot;
-	u8 vspeed[2];
-	u8 res2;
-	u8 sclk[3];
-	u8 res3;
-	u8 watch;
-	u8 res4[33];
-} __attribute__ ((packed)) pixis_t;
-
 #elif defined(CONFIG_TARGET_MPC8641HPCN)
 typedef struct pixis {
 	u8 id;
