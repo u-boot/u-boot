23397775bc2b1ff35d4eee075ef53e16360859dc
Tom Rini <trini@konsulko.com>
2021-02-09 21:42:51 -0500

    ppc: Remove MPC8544DS board
    This board relies on using CONFIG_LIBATA but does not enable CONFIG_AHCI.  The
    deadline for this conversion was the v2019.07 release.  The use of CONFIG_AHCI
    requires CONFIG_DM.  The deadline for this conversion was v2020.01.  Remove
    this board.
    
    Cc: Priyanka Jain <priyanka.jain@nxp.com>
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- board/freescale/common/pixis.h
+++ board/freescale/common/pixis.h
@@ -45,33 +45,6 @@ typedef struct pixis {
 	u8 res2[4];
 } __attribute__ ((packed)) pixis_t;
 
-#elif defined(CONFIG_TARGET_MPC8544DS)
-typedef struct pixis {
-	u8 id;
-	u8 ver;
-	u8 pver;
-	u8 csr;
-	u8 rst;
-	u8 pwr;
-	u8 aux1;
-	u8 spd;
-	u8 res[8];
-	u8 vctl;
-	u8 vstat;
-	u8 vcfgen0;
-	u8 vcfgen1;
-	u8 vcore0;
-	u8 res1;
-	u8 vboot;
-	u8 vspeed[2];
-	u8 vclkh;
-	u8 vclkl;
-	u8 watch;
-	u8 led;
-	u8 vspeed2;
-	u8 res2[34];
-} __attribute__ ((packed)) pixis_t;
-
 #elif defined(CONFIG_TARGET_MPC8572DS)
 typedef struct pixis {
 	u8 id;
