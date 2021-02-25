c40131acc030ac9b1bf0704306aa18295d44ff2a
Bin Meng <bmeng.cn@gmail.com>
2021-02-25 17:22:32 +0800

    include: Remove extern from addr_map.h
    Remove the extern of the header because they are useless.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- include/addr_map.h
+++ include/addr_map.h
@@ -8,9 +8,9 @@
 
 #include <asm/types.h>
 
-extern phys_addr_t addrmap_virt_to_phys(void *vaddr);
-extern void *addrmap_phys_to_virt(phys_addr_t paddr);
-extern void addrmap_set_entry(unsigned long vaddr, phys_addr_t paddr,
-				phys_size_t size, int idx);
+phys_addr_t addrmap_virt_to_phys(void *vaddr);
+void *addrmap_phys_to_virt(phys_addr_t paddr);
+void addrmap_set_entry(unsigned long vaddr, phys_addr_t paddr,
+		       phys_size_t size, int idx);
 
 #endif
