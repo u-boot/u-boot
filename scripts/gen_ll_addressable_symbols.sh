86e73cba2e6fcf573a9d1d950315e6167e1ea347
Marek Behún <marek.behun@nic.cz>
2021-03-02 02:30:08 +0100

    build: support building with Link Time Optimizations
    Add plumbing for building U-Boot with Link Time Optimizations.
    
    When building with LTO, $(PLATFORM_LIBS) has to be in --whole-archive /
    --no-whole-archive group, otherwise some functions declared in assembly
    may not be resolved and linking may fail.
    
    Note: clang may throw away linker list symbols it thinks are unused when
    compiling with LTO. To force these symbols to be included, we refer to
    them via the __ADDRESSABLE macro in a C file generated from compiled
    built-in.o files before linking.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

---
+++ scripts/gen_ll_addressable_symbols.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0+
+# Copyright (C) 2020 Marek Behún <marek.behun@nic.cz>
+
+# Generate __ADDRESSABLE(symbol) for every linker list entry symbol, so that LTO
+# does not optimize these symbols away
+
+set -e
+
+echo '#include <common.h>'
+$NM "$@" 2>/dev/null | grep -oe '_u_boot_list_2_[a-zA-Z0-9_]*_2_[a-zA-Z0-9_]*' | \
+	sort -u | sed -e 's/^\(.*\)/extern char \1[];\n__ADDRESSABLE(\1);/'
