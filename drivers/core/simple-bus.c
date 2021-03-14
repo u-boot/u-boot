80279fa1295af73beba8689450ba408130053ff1
Bin Meng <bmeng.cn@gmail.com>
2021-03-14 20:15:02 +0800

    dm: core: Correctly read <ranges> of simple-bus
    At present we decode simple bus <ranges> using the following assumption:
    
    - parent #address-cells 1
    - child #address-cells 1
    - child #size-cells 1
    
    However this might not always be the case.
    
    Update to use fdt_addr_t and fdt_size_t in 'struct simple_bus_plat', and
    use fdt_read_ranges() to correctly decode it according to the actual
    parent and child #address-cells / #size-cells under a Kconfig option
    CONFIG_SIMPLE_BUS_CORRECT_RANGE which can be turned on for any board
    that needs it.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- drivers/core/simple-bus.c
+++ drivers/core/simple-bus.c
@@ -4,8 +4,12 @@
  */
 
 #include <common.h>
+#include <asm/global_data.h>
 #include <dm.h>
 #include <dm/simple_bus.h>
+#include <fdt_support.h>
+
+DECLARE_GLOBAL_DATA_PTR;
 
 fdt_addr_t simple_bus_translate(struct udevice *dev, fdt_addr_t addr)
 {
@@ -22,16 +26,30 @@ static int simple_bus_post_bind(struct udevice *dev)
 #if CONFIG_IS_ENABLED(OF_PLATDATA)
 	return 0;
 #else
-	u32 cell[3];
+	struct simple_bus_plat *plat = dev_get_uclass_plat(dev);
 	int ret;
 
-	ret = dev_read_u32_array(dev, "ranges", cell, ARRAY_SIZE(cell));
-	if (!ret) {
-		struct simple_bus_plat *plat = dev_get_uclass_plat(dev);
+	if (CONFIG_IS_ENABLED(SIMPLE_BUS_CORRECT_RANGE)) {
+		uint64_t caddr, paddr, len;
 
-		plat->base = cell[0];
-		plat->target = cell[1];
-		plat->size = cell[2];
+		/* only read range index 0 */
+		ret = fdt_read_range((void *)gd->fdt_blob, dev_of_offset(dev),
+				     0, &caddr, &paddr, &len);
+		if (!ret) {
+			plat->base = caddr;
+			plat->target = paddr;
+			plat->size = len;
+		}
+	} else {
+		u32 cell[3];
+
+		ret = dev_read_u32_array(dev, "ranges", cell,
+					 ARRAY_SIZE(cell));
+		if (!ret) {
+			plat->base = cell[0];
+			plat->target = cell[1];
+			plat->size = cell[2];
+		}
 	}
 
 	return dm_scan_fdt_dev(dev);
