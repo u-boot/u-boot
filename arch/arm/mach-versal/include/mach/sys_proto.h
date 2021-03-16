57460e4a9393d6b3bfe1e49c12d1426d9b83f9d3
Michal Simek <michal.simek@xilinx.com>
2021-03-16 13:44:58 +0100

    arm64: versal: Define zynqmp_mmio_write() for versal
    GQSPI driver is using it but this function is never called for Versal
    because it is removed by linker. But function should be declared to avoid
    this build warning:
    drivers/spi/zynqmp_gqspi.c: In function 'zynqmp_qspi_set_tapdelay':
    drivers/spi/zynqmp_gqspi.c:378:3: warning: implicit declaration of function
    'zynqmp_mmio_write' [-Wimplicit-function-declaration]
      378 |   zynqmp_mmio_write(IOU_TAPDLY_BYPASS_OFST,
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-versal/include/mach/sys_proto.h
+++ arch/arm/mach-versal/include/mach/sys_proto.h
@@ -3,6 +3,8 @@
  * Copyright 2016 - 2018 Xilinx, Inc.
  */
 
+#include <linux/build_bug.h>
+
 enum {
 	TCM_LOCK,
 	TCM_SPLIT,
@@ -10,3 +12,9 @@ enum {
 
 void tcm_init(u8 mode);
 void mem_map_fill(void);
+
+static inline int zynqmp_mmio_write(const u32 address, const u32 mask, const u32 value)
+{
+	BUILD_BUG();
+	return -EINVAL;
+}
