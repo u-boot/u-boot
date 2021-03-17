3e5f3dbe3a8556503afc0020db5dd6f6ae62fef9
Biju Das <biju.das.jz@bp.renesas.com>
2021-03-17 14:11:50 +0000

    arm: mach-rmobile: Add CPU info support for RZ/G2
    Add CPU info support for RZ/G2 SoC's.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- arch/arm/mach-rmobile/include/mach/rmobile.h
+++ arch/arm/mach-rmobile/include/mach/rmobile.h
@@ -41,6 +41,7 @@
 #define RMOBILE_CPU_TYPE_R8A77995	0x58
 
 #ifndef __ASSEMBLY__
+const u8 *rzg_get_cpu_name(void);
 u32 rmobile_get_cpu_type(void);
 u32 rmobile_get_cpu_rev_integer(void);
 u32 rmobile_get_cpu_rev_fraction(void);
