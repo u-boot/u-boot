34f8fc7c859926174b02734b35e5d5112a8c9fe7
Simon Glass <sjg@chromium.org>
2021-03-23 14:52:07 +1300

    gpio: Drop dm_gpio_set_dir()
    This function is not used. Drop it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Patrick Delaunay <patrick.delaunay@foss.st.com>

--- drivers/gpio/gpio-uclass.c
+++ drivers/gpio/gpio-uclass.c
@@ -713,17 +713,6 @@ int dm_gpio_set_dir_flags(struct gpio_desc *desc, ulong flags)
 	return dm_gpio_clrset_flags(desc, GPIOD_MASK_DIR, flags);
 }
 
-int dm_gpio_set_dir(struct gpio_desc *desc)
-{
-	int ret;
-
-	ret = check_reserved(desc, "set_dir");
-	if (ret)
-		return ret;
-
-	return _dm_gpio_set_flags(desc, desc->flags);
-}
-
 int dm_gpios_clrset_flags(struct gpio_desc *desc, int count, ulong clr,
 			  ulong set)
 {
