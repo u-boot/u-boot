96fe4f6485e92ed9da464c96c5f536698c5ee66d
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2021-03-25 14:07:45 +0800

    arm: socfpga: smc: Add function to get usercode
    Add function to send mailbox command via SMC to get usercode from SDM.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>
    Reviewed-by: Ley Foon Tan <ley.foon.tan@intel.com>

--- arch/arm/mach-socfpga/include/mach/smc_api.h
+++ arch/arm/mach-socfpga/include/mach/smc_api.h
@@ -9,5 +9,6 @@
 int invoke_smc(u32 func_id, u64 *args, int arg_len, u64 *ret_arg, int ret_len);
 int smc_send_mailbox(u32 cmd, u32 len, u32 *arg, u8 urgent, u32 *resp_buf_len,
 		     u32 *resp_buf);
+int smc_get_usercode(u32 *usercode);
 
 #endif /* _SMC_API_H_ */
