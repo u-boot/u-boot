ce62b0f8f45f1b71fc03ddee84c0529cea228e24
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2021-04-23 16:24:13 +0300

    test/py: Fix efidebug related tests
    commit cbea241e935e("efidebug: add multiple device path instances on Boot####")
    slightly tweaked the efidebug syntax adding -b, -i and -s for the boot
    image, initrd and optional data.
    The pytests using this command were adapted as well. However I completely
    missed the last "" argument, which at the time indicated the optional data
    and needed conversion as well.  This patch is adding the missing -s flag
    and the tests are back to normal.
    
    Fixes: cbea241e935e("efidebug: add multiple device path instances on Boot####")
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Reviwed-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- test/py/tests/test_efi_capsule/test_capsule_firmware.py
+++ test/py/tests/test_efi_capsule/test_capsule_firmware.py
@@ -39,7 +39,7 @@ class TestEfiCapsuleFirmwareFit(object):
         with u_boot_console.log.section('Test Case 1-a, before reboot'):
             output = u_boot_console.run_command_list([
                 'host bind 0 %s' % disk_img,
-                'efidebug boot add -b 1 TEST host 0:1 /helloworld.efi ""',
+                'efidebug boot add -b 1 TEST host 0:1 /helloworld.efi -s ""',
                 'efidebug boot order 1',
                 'env set -e OsIndications',
                 'env set dfu_alt_info "sf 0:0=u-boot-bin raw 0x100000 0x50000;u-boot-env raw 0x150000 0x200000"',
@@ -114,7 +114,7 @@ class TestEfiCapsuleFirmwareFit(object):
         with u_boot_console.log.section('Test Case 2-a, before reboot'):
             output = u_boot_console.run_command_list([
                 'host bind 0 %s' % disk_img,
-                'efidebug boot add -b 1 TEST host 0:1 /helloworld.efi ""',
+                'efidebug boot add -b 1 TEST host 0:1 /helloworld.efi -s ""',
                 'efidebug boot order 1',
                 'env set -e -nv -bs -rt OsIndications =0x0000000000000004',
                 'env set dfu_alt_info "sf 0:0=u-boot-bin raw 0x100000 0x50000;u-boot-env raw 0x150000 0x200000"',
@@ -188,7 +188,7 @@ class TestEfiCapsuleFirmwareFit(object):
         with u_boot_console.log.section('Test Case 3-a, before reboot'):
             output = u_boot_console.run_command_list([
                 'host bind 0 %s' % disk_img,
-                'efidebug boot add -b 1 TEST host 0:1 /helloworld.efi ""',
+                'efidebug boot add -b 1 TEST host 0:1 /helloworld.efi -s ""',
                 'efidebug boot order 1',
                 'env set -e -nv -bs -rt OsIndications =0x0000000000000004',
                 'env set dfu_alt_info "sf 0:0=u-boot-bin raw 0x100000 0x50000;u-boot-env raw 0x150000 0x200000"',
