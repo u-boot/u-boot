139eaa7a2b5f25e42307a672849eba0e5b4cddf0
Bin Meng <bmeng.cn@gmail.com>
2021-04-30 21:16:58 +0800

    of: addr: Translate 'dma-ranges' for parent nodes missing 'dma-ranges'
    'dma-ranges' frequently exists without parent nodes having 'dma-ranges'.
    While this is an error for 'ranges', this is fine because DMA capable
    devices always have a translatable DMA address. Also, with no
    'dma-ranges' at all, the assumption is that DMA addresses are 1:1 with
    no restrictions unless perhaps the device itself has implicit
    restrictions.
    
    This keeps in sync with Linux kernel commit:
    
      81db12ee15cb: of/address: Translate 'dma-ranges' for parent nodes missing 'dma-ranges'
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/core/of_addr.c
+++ drivers/core/of_addr.c
@@ -192,9 +192,13 @@ static int of_translate_one(const struct device_node *parent,
 	 *
 	 * As far as we know, this damage only exists on Apple machines, so
 	 * This code is only enabled on powerpc. --gcl
+	 *
+	 * This quirk also applies for 'dma-ranges' which frequently exist in
+	 * child nodes without 'dma-ranges' in the parent nodes. --RobH
 	 */
 	ranges = of_get_property(parent, rprop, &rlen);
-	if (ranges == NULL && !of_empty_ranges_quirk(parent)) {
+	if (ranges == NULL && !of_empty_ranges_quirk(parent) &&
+	    strcmp(rprop, "dma-ranges")) {
 		debug("no ranges; cannot translate\n");
 		return 1;
 	}
