a74e9d899d98037c75ca770d02367e26c179b45c
Green Wan <green.wan@sifive.com>
2021-05-27 06:52:07 -0700

    riscv: cpu: fu740: Add support for cpu fu740
    Add SiFive fu740 cpu to support RISC-V arch
    
    Signed-off-by: Green Wan <green.wan@sifive.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ arch/riscv/include/asm/arch-fu740/cache.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2020-2021 SiFive, Inc.
+ *
+ * Authors:
+ *   Pragnesh Patel <pragnesh.patel@sifve.com>
+ */
+
+#ifndef _CACHE_SIFIVE_H
+#define _CACHE_SIFIVE_H
+
+int cache_enable_ways(void);
+
+#endif /* _CACHE_SIFIVE_H */
