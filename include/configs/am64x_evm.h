ce7ad5739054a9b3a766e49e574905590753c329
Aswath Govindraju <a-govindraju@ti.com>
2021-06-04 22:00:40 +0530

    configs: am64: Enable configs to support USB host and device modes
    Enable config options required to add support for USB Mass storage boot,
    USB DFU boot, host and device modes in U-Boot.
    
    Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Link: https://lore.kernel.org/r/20210604163043.12811-11-a-govindraju@ti.com

--- include/configs/am64x_evm.h
+++ include/configs/am64x_evm.h
@@ -13,6 +13,7 @@
 #include <config_distro_bootcmd.h>
 #include <environment/ti/mmc.h>
 #include <asm/arch/am64_hardware.h>
+#include <environment/ti/k3_dfu.h>
 
 /* DDR Configuration */
 #define CONFIG_SYS_SDRAM_BASE1		0x880000000
@@ -95,15 +96,24 @@
 		"${bootdir}/${name_fit}\0"				\
 	"partitions=" PARTS_DEFAULT
 
+#define EXTRA_ENV_DFUARGS \
+	DFU_ALT_INFO_MMC \
+	DFU_ALT_INFO_EMMC \
+	DFU_ALT_INFO_RAM \
+	DFU_ALT_INFO_OSPI
+
 /* Incorporate settings into the U-Boot environment */
 #define CONFIG_EXTRA_ENV_SETTINGS					\
 	DEFAULT_LINUX_BOOT_ENV						\
 	DEFAULT_MMC_TI_ARGS						\
 	EXTRA_ENV_AM642_BOARD_SETTINGS					\
-	EXTRA_ENV_AM642_BOARD_SETTINGS_MMC
+	EXTRA_ENV_AM642_BOARD_SETTINGS_MMC				\
+	EXTRA_ENV_DFUARGS
 
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
+
+#define CONFIG_SYS_USB_FAT_BOOT_PARTITION 1
 
 /* MMC ENV related defines */
 #ifdef CONFIG_ENV_IS_IN_MMC
