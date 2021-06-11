df5363a67f357106e559a64c09f6535ddac12ec1
Tero Kristo <t-kristo@ti.com>
2021-06-11 11:45:17 +0300

    tools: k3_fit_atf: add DM binary to the FIT image
    Add DM (device manager) firmware image to the fit image that is loaded by
    R5 SPL. This is needed with the HSM rearch where the firmware allocation
    has been changed slightly.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Tero Kristo <kristo@kernel.org>

--- arch/arm/mach-k3/config.mk
+++ arch/arm/mach-k3/config.mk
@@ -49,6 +49,10 @@ endif
 
 ifdef CONFIG_ARM64
 
+ifeq ($(CONFIG_SOC_K3_J721E),)
+export DM := /dev/null
+endif
+
 ifeq ($(CONFIG_TI_SECURE_DEVICE),y)
 SPL_ITS := u-boot-spl-k3_HS.its
 $(SPL_ITS): export IS_HS=1
--- tools/k3_fit_atf.sh
+++ tools/k3_fit_atf.sh
@@ -21,6 +21,13 @@ if [ ! -f $TEE ]; then
 	TEE=/dev/null
 fi
 
+[ -z "$DM" ] && DM="dm.bin"
+
+if [ ! -e $DM ]; then
+	echo "WARNING DM file $DM NOT found, resulting might be non-functional" >&2
+	DM=/dev/null
+fi
+
 if [ ! -z "$IS_HS" ]; then
 	HS_APPEND=_HS
 fi
@@ -52,6 +59,16 @@ cat << __HEADER_EOF
 			os = "tee";
 			load = <0x9e800000>;
 			entry = <0x9e800000>;
+		};
+		dm {
+			description = "DM binary";
+			data = /incbin/("$DM");
+			type = "firmware";
+			arch = "arm32";
+			compression = "none";
+			os = "DM";
+			load = <0xa0000000>;
+			entry = <0xa0000000>;
 		};
 		spl {
 			description = "SPL (64-bit)";
@@ -94,7 +111,7 @@ do
 		$(basename $dtname) {
 			description = "$(basename $dtname .dtb)";
 			firmware = "atf";
-			loadables = "tee", "spl";
+			loadables = "tee", "dm", "spl";
 			fdt = "$(basename $dtname)";
 		};
 __CONF_SECTION_EOF
