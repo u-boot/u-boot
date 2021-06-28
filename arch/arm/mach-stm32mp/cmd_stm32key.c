e00e1f394c8cb7476fe6c1abce54674b75336b70
Patrick Delaunay <patrick.delaunay@foss.st.com>
2021-06-28 14:56:01 +0200

    stm32mp: cmd_stm32key: add get_misc_dev function
    Add a helper function to access to BSEC misc driver.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- arch/arm/mach-stm32mp/cmd_stm32key.c
+++ arch/arm/mach-stm32mp/cmd_stm32key.c
@@ -14,6 +14,17 @@
 #define STM32_OTP_HASH_KEY_START 24
 #define STM32_OTP_HASH_KEY_SIZE 8
 
+static int get_misc_dev(struct udevice **dev)
+{
+	int ret;
+
+	ret = uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(stm32mp_bsec), dev);
+	if (ret)
+		log_err("Can't find stm32mp_bsec driver\n");
+
+	return ret;
+}
+
 static void read_hash_value(u32 addr)
 {
 	int i;
@@ -31,13 +42,9 @@ static int fuse_hash_value(u32 addr, bool print)
 	u32 word, val;
 	int i, ret;
 
-	ret = uclass_get_device_by_driver(UCLASS_MISC,
-					  DM_DRIVER_GET(stm32mp_bsec),
-					  &dev);
-	if (ret) {
-		log_err("Can't find stm32mp_bsec driver\n");
+	ret = get_misc_dev(&dev);
+	if (ret)
 		return ret;
-	}
 
 	for (i = 0, word = STM32_OTP_HASH_KEY_START;
 	     i < STM32_OTP_HASH_KEY_SIZE;
