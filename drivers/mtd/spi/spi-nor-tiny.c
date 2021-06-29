5b8ec59e2a219185127fcaa640c2f5d5aba3acd6
Takahiro Kuwano <Takahiro.Kuwano@infineon.com>
2021-06-29 15:01:04 +0900

    mtd: spi-nor-tiny: Add fixups for Cypress s25hl-t/s25hs-t
    Fixes mode clocks for SPINOR_OP_READ_FAST_4B in tiny.
    
    Signed-off-by: Takahiro Kuwano <Takahiro.Kuwano@infineon.com>
    Reviewed-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/mtd/spi/spi-nor-tiny.c
+++ drivers/mtd/spi/spi-nor-tiny.c
@@ -583,6 +583,12 @@ static int spi_nor_init_params(struct spi_nor *nor,
 		spi_nor_set_read_settings(&params->reads[SNOR_CMD_READ_FAST],
 					  0, 8, SPINOR_OP_READ_FAST,
 					  SNOR_PROTO_1_1_1);
+#ifdef CONFIG_SPI_FLASH_SPANSION
+		if (JEDEC_MFR(info) == SNOR_MFR_CYPRESS &&
+		    (info->id[1] == 0x2a || info->id[1] == 0x2b))
+			/* 0x2a: S25HL (QSPI, 3.3V), 0x2b: S25HS (QSPI, 1.8V) */
+			params->reads[SNOR_CMD_READ_FAST].num_mode_clocks = 8;
+#endif
 	}
 
 	if (info->flags & SPI_NOR_QUAD_READ) {
