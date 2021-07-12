985dc5f6cd417a9086a0c961b07eaa95c2bf220b
Raju Kumar Pothuraju <raju.kumar-pothuraju@xilinx.com>
2021-07-12 20:19:04 +0530

    xilinx: Define kernel_comp_addr_r,kernel_comp_size env variables
    Add kernel_comp_addr_r, kernel_comp_size env variables for zynqmp and
    versal to be able to use the compressed kernel Image(.gz,.bz2,.lzma,.lzo)
    using booti command.
    
    Signed-off-by: Raju Kumar Pothuraju <raju.kumar-pothuraju@xilinx.com>

--- include/configs/xilinx_versal.h
+++ include/configs/xilinx_versal.h
@@ -75,6 +75,8 @@
 	"pxefile_addr_r=0x10000000\0" \
 	"kernel_addr_r=0x18000000\0" \
 	"kernel_size_r=0x10000000\0" \
+	"kernel_comp_addr_r=0x30000000\0" \
+	"kernel_comp_size=0x3C00000\0" \
 	"scriptaddr=0x20000000\0" \
 	"ramdisk_addr_r=0x02100000\0" \
 	"script_size_f=0x80000\0"
