f5bc9c25f3159db2e420cdcff974980a5258a44f
Simon Glass <sjg@chromium.org>
2021-07-14 17:05:30 -0500

    image: Rename SPL_SHAxxx_SUPPORT to SPL_FIT_SHAxxx
    These option are named inconsistently with other SPL options, thus making
    them incompatible with the CONFIG_IS_ENABLED() macro. Rename them.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- include/image.h
+++ include/image.h
@@ -68,7 +68,7 @@ struct fdt_region;
 #  ifdef CONFIG_SPL_MD5_SUPPORT
 #   define IMAGE_ENABLE_MD5	1
 #  endif
-#  ifdef CONFIG_SPL_SHA1_SUPPORT
+#  ifdef CONFIG_SPL_FIT_SHA1
 #   define IMAGE_ENABLE_SHA1	1
 #  endif
 # else
@@ -90,21 +90,21 @@ struct fdt_region;
 #endif
 
 #if defined(CONFIG_FIT_SHA256) || \
-	defined(CONFIG_SPL_SHA256_SUPPORT)
+	defined(CONFIG_SPL_FIT_SHA256)
 #define IMAGE_ENABLE_SHA256	1
 #else
 #define IMAGE_ENABLE_SHA256	0
 #endif
 
 #if defined(CONFIG_FIT_SHA384) || \
-	defined(CONFIG_SPL_SHA384_SUPPORT)
+	defined(CONFIG_SPL_FIT_SHA384)
 #define IMAGE_ENABLE_SHA384	1
 #else
 #define IMAGE_ENABLE_SHA384	0
 #endif
 
 #if defined(CONFIG_FIT_SHA512) || \
-	defined(CONFIG_SPL_SHA512_SUPPORT)
+	defined(CONFIG_SPL_FIT_SHA512)
 #define IMAGE_ENABLE_SHA512	1
 #else
 #define IMAGE_ENABLE_SHA512	0
