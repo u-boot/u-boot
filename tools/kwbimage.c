3d7b93d51c26092010626708ba9adaf9a65a764f
Pali Rohár <pali@kernel.org>
2021-07-23 11:14:35 +0200

    kwbimage: Update help message about how to extract from an existing image
    Extracting is now supported by dumpimage, so mention it in help instead
    of `kwbimage -x`.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Reviewed-by: Marek Behún <marek.behun@nic.cz>
    Reviewed-by: Stefan Roese <sr@denx.de>

--- tools/kwbimage.c
+++ tools/kwbimage.c
@@ -961,7 +961,7 @@ static size_t image_headersz_v1(int *hasext)
 			fprintf(stderr,
 				"Didn't find the file '%s' in '%s' which is mandatory to generate the image\n"
 				"This file generally contains the DDR3 training code, and should be extracted from an existing bootable\n"
-				"image for your board. See 'kwbimage -x' to extract it from an existing image.\n",
+				"image for your board. Use 'dumpimage -T kwbimage -p 0' to extract it from an existing image.\n",
 				binarye->binary.file, dir);
 			return 0;
 		}
