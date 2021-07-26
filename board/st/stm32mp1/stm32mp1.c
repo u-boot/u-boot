6de57b41ddeddf6a01a61ea14614a90aba0bcd92
Patrick Delaunay <patrick.delaunay@foss.st.com>
2021-07-26 11:21:34 +0200

    arm: stm32mp: add config for STM32IMAGE support
    By default for trusted boot with TF-A, U-Boot (u-boot-nodtb)
    is located in FIP container with its device tree and with
    the secure monitor (provided by TF-A or OP-TEE).
    The FIP file is loaded by TF-A BL2 and each components is
    extracted at the final location.
    
    This patch add CONFIG_STM32MP15x_STM32IMAGE to request the
    STM32 image generation for SOC STM32MP15x
    when FIP container is not used (u-boot.stm32 is loaded by TF-A
    as done previously to keep the backward compatibility).
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- board/st/stm32mp1/stm32mp1.c
+++ board/st/stm32mp1/stm32mp1.c
@@ -105,10 +105,14 @@ int checkboard(void)
 	const char *fdt_compat;
 	int fdt_compat_len;
 
-	if (IS_ENABLED(CONFIG_TFABOOT))
-		mode = "trusted";
-	else
+	if (IS_ENABLED(CONFIG_TFABOOT)) {
+		if (IS_ENABLED(CONFIG_STM32MP15x_STM32IMAGE))
+			mode = "trusted - stm32image";
+		else
+			mode = "trusted";
+	} else {
 		mode = "basic";
+	}
 
 	fdt_compat = fdt_getprop(gd->fdt_blob, 0, "compatible",
 				 &fdt_compat_len);
