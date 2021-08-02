33e4ab31a9647b0bacd44b579199eaae239d69e3
Bin Meng <bmeng.cn@gmail.com>
2021-08-02 17:45:22 +0800

    x86: fsp: Only FSP2 has INIT_PHASE_END_FIRMWARE
    For FSP1, there is no such INIT_PHASE_END_FIRMWARE.
    
    Move board_final_cleanup() to fsp2 directory.
    
    Fixes: 7c73cea44290 ("x86: Notify the FSP of the 'end firmware' event")
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested on chromebook_coral, chromebook_samus, chromebook_link, minnowmax
    Tested-by: Simon Glass <sjg@chromium.org>

--- arch/x86/lib/fsp/fsp_common.c
+++ arch/x86/lib/fsp/fsp_common.c
@@ -61,22 +61,6 @@ void board_final_init(void)
 		debug("OK\n");
 }
 
-void board_final_cleanup(void)
-{
-	u32 status;
-
-	/* TODO(sjg@chromium.org): This causes Linux to crash */
-	return;
-
-	/* call into FspNotify */
-	debug("Calling into FSP (notify phase INIT_PHASE_END_FIRMWARE): ");
-	status = fsp_notify(NULL, INIT_PHASE_END_FIRMWARE);
-	if (status)
-		debug("fail, error code %x\n", status);
-	else
-		debug("OK\n");
-}
-
 int fsp_save_s3_stack(void)
 {
 	struct udevice *dev;
