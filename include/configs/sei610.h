ddc8b223851af042c13261fd476dbce8d1886aaa
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-08-05 17:17:27 +0200

    configs: sei510/sei610: don't use hard-coded gpt uuids
    doc/README.gpt states:
    
    > The fields 'uuid' and 'uuid_disk' are optional if CONFIG_RANDOM_UUID is
    > enabled. A random uuid will be used if omitted or they point to an empty/
    > non-existent environment variable. The environment variable will be
    > set to the generated UUID.  The 'gpt guid' command reads the current
    > value of the uuid_disk from the GPT.
    
    Since we have CONFIG_RANDOM_UUID=y, remove the hard-coded uuids
    and use meaningful variable names instead.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- include/configs/sei610.h
+++ include/configs/sei610.h
@@ -10,20 +10,16 @@
 #define __CONFIG_H
 
 #define LOGO_UUID "43a3305d-150f-4cc9-bd3b-38fca8693846;"
-#define CACHE_UUID "99207ae6-5207-11e9-999e-6f77a3612069;"
-#define SYSTEM_UUID "99f9b7ac-5207-11e9-8507-c3c037e393f3;"
-#define VENDOR_UUID "9d082802-5207-11e9-954c-cbbce08ba108;"
-#define USERDATA_UUID "9b976e42-5207-11e9-8f16-ff47ac594b22;"
 #define ROOT_UUID "ddb8c3f6-d94d-4394-b633-3134139cc2e0;"
 
 #define PARTS_DEFAULT \
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=boot,size=64M,bootable,uuid=${uuid_gpt_boot};" \
 	"name=logo,size=2M,uuid=" LOGO_UUID \
-	"name=cache,size=256M,uuid=" CACHE_UUID \
-	"name=system,size=1536M,uuid=" SYSTEM_UUID \
-	"name=vendor,size=256M,uuid=" VENDOR_UUID \
-	"name=userdata,size=12795M,uuid=" USERDATA_UUID \
+	"name=cache,size=256M,uuid=${uuid_gpt_cache};" \
+	"name=system,size=1536M,uuid=${uuid_gpt_system};" \
+	"name=vendor,size=256M,uuid=${uuid_gpt_vendor};" \
+	"name=userdata,size=12795M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
 
 #include <configs/meson64_android.h>
