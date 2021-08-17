20815ecdcf7b6cf118229af5bb2b474ca2215158
Tom Rini <trini@konsulko.com>
2021-08-17 17:59:37 -0400

    davinci: Drop CONFIG_MAC_ADDR_IN_EEPROM
    Looking over the current boards, there are no users of
    CONFIG_MAC_ADDR_IN_EEPROM.  Further, omapl138_lcdk uses
    CONFIG_NET_RANDOM_ETHADDR.  Drop various unused code paths.
    
    Cc: Adam Ford <aford173@gmail.com>
    Cc: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- board/davinci/da8xxevm/omapl138_lcdk.c
+++ board/davinci/da8xxevm/omapl138_lcdk.c
@@ -236,12 +236,6 @@ int board_init(void)
 
 #define CFG_MAC_ADDR_OFFSET	(flash->size - SZ_64K)
 
-static int  get_mac_addr(u8 *addr)
-{
-	/* Need to find a way to get MAC ADDRESS */
-	return 0;
-}
-
 void dsp_lpsc_on(unsigned domain, unsigned int id)
 {
 	dv_reg_p mdstat, mdctl, ptstat, ptcmd;
@@ -304,29 +298,6 @@ int rmii_hw_init(void)
 
 int misc_init_r(void)
 {
-	uint8_t tmp[20], addr[10];
-
-
-	if (env_get("ethaddr") == NULL) {
-		/* Read Ethernet MAC address from EEPROM */
-		if (dvevm_read_mac_address(addr)) {
-			/* Set Ethernet MAC address from EEPROM */
-			davinci_sync_env_enetaddr(addr);
-		} else {
-			get_mac_addr(addr);
-		}
-
-		if (!is_multicast_ethaddr(addr) && !is_zero_ethaddr(addr)) {
-			sprintf((char *)tmp, "%02x:%02x:%02x:%02x:%02x:%02x",
-				addr[0], addr[1], addr[2], addr[3], addr[4],
-				addr[5]);
-
-			env_set("ethaddr", (char *)tmp);
-		} else {
-			printf("Invalid MAC address read.\n");
-		}
-	}
-
 #ifdef CONFIG_DRIVER_TI_EMAC_USE_RMII
 	/* Select RMII fucntion through the expander */
 	if (rmii_hw_init())
