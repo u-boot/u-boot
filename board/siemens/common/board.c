433eb4affc497ed9e88e1e758bc3e1f21c6fda4d
Tom Rini <trini@konsulko.com>
2021-08-17 17:59:40 -0400

    siemens: Move CONFIG_SYS_I2C_EEPROM_ADDR usage out of CONFIG namespace
    The instances of CONFIG_SYS_I2C_EEPROM_ADDR on these platforms doesn't
    match up with the rest of the EERPOM related defines in U-Boot.  Move
    these out of the CONFIG namespace rather than make complex Kconfig
    logic.
    
    Cc: Samuel Egli <samuel.egli@siemens.com>
    Cc: Anatolij Gustschin <agust@denx.de>
    Cc: Heiko Schocher <hs@denx.de>
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Heiko Schocher <hs@denx.de>
    Reviewed-by: Anatolij Gustschin <agust@denx.de>

--- board/siemens/common/board.c
+++ board/siemens/common/board.c
@@ -70,6 +70,7 @@ void sdram_init(void)
 #endif /* #ifdef CONFIG_SPL_BUILD */
 
 #ifndef CONFIG_SPL_BUILD
+#define FACTORYSET_EEPROM_ADDR		0x50
 /*
  * Basic board specific setup.  Pinmux has been handled already.
  */
@@ -87,7 +88,7 @@ int board_init(void)
 	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
 
 #ifdef CONFIG_FACTORYSET
-	factoryset_read_eeprom(CONFIG_SYS_I2C_EEPROM_ADDR);
+	factoryset_read_eeprom(FACTORYSET_EEPROM_ADDR);
 #endif
 
 	gpmc_init();
