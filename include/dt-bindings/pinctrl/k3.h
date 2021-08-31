4698bb8c9433af051cc24002f40c14d5e977d7cb
Nishanth Menon <nm@ti.com>
2021-08-31 13:12:24 -0500

    dt-bindings: pinctrl: k3: Synchronize with v5.14 kernel
    Synchronize with the upstream version as of v5.14 kernel tag
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- include/dt-bindings/pinctrl/k3.h
+++ include/dt-bindings/pinctrl/k3.h
@@ -1,36 +1,33 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * This header provides constants for TI K3-AM65 pinctrl bindings.
+ * This header provides constants for pinctrl bindings for TI's K3 SoC
+ * family.
  *
- * Copyright (C) 2018-2021 Texas Instruments
+ * Copyright (C) 2018-2021 Texas Instruments Incorporated - https://www.ti.com/
  */
-#ifndef _DT_BINDINGS_PINCTRL_TI_K3_AM65_H
-#define _DT_BINDINGS_PINCTRL_TI_K3_AM65_H
+#ifndef _DT_BINDINGS_PINCTRL_TI_K3_H
+#define _DT_BINDINGS_PINCTRL_TI_K3_H
 
-#define PULL_DISABLE		(1 << 16)
-#define PULL_UP			(1 << 17)
-#define INPUT_EN		(1 << 18)
-#define SLEWCTRL_200MHZ		0
-#define SLEWCTRL_150MHZ		(1 << 19)
-#define SLEWCTRL_100MHZ		(2 << 19)
-#define SLEWCTRL_50MHZ		(3 << 19)
-#define TX_DIS			(1 << 21)
-#define ISO_OVR			(1 << 22)
-#define ISO_BYPASS		(1 << 23)
-#define DS_EN			(1 << 24)
-#define DS_INPUT		(1 << 25)
-#define DS_FORCE_OUT_HIGH	(1 << 26)
-#define DS_PULL_UP_DOWN_EN	0
-#define DS_PULL_UP_DOWN_DIS	(1 << 27)
-#define DS_PULL_UP_SEL		(1 << 28)
-#define WAKEUP_ENABLE		(1 << 29)
+#define PULLUDEN_SHIFT		(16)
+#define PULLTYPESEL_SHIFT	(17)
+#define RXACTIVE_SHIFT		(18)
 
-#define PIN_OUTPUT		(PULL_DISABLE)
-#define PIN_OUTPUT_PULLUP	(PULL_UP)
-#define PIN_OUTPUT_PULLDOWN	0
+#define PULL_DISABLE		(1 << PULLUDEN_SHIFT)
+#define PULL_ENABLE		(0 << PULLUDEN_SHIFT)
+
+#define PULL_UP			(1 << PULLTYPESEL_SHIFT | PULL_ENABLE)
+#define PULL_DOWN		(0 << PULLTYPESEL_SHIFT | PULL_ENABLE)
+
+#define INPUT_EN		(1 << RXACTIVE_SHIFT)
+#define INPUT_DISABLE		(0 << RXACTIVE_SHIFT)
+
+/* Only these macros are expected be used directly in device tree files */
+#define PIN_OUTPUT		(INPUT_DISABLE | PULL_DISABLE)
+#define PIN_OUTPUT_PULLUP	(INPUT_DISABLE | PULL_UP)
+#define PIN_OUTPUT_PULLDOWN	(INPUT_DISABLE | PULL_DOWN)
 #define PIN_INPUT		(INPUT_EN | PULL_DISABLE)
 #define PIN_INPUT_PULLUP	(INPUT_EN | PULL_UP)
-#define PIN_INPUT_PULLDOWN	(INPUT_EN)
+#define PIN_INPUT_PULLDOWN	(INPUT_EN | PULL_DOWN)
 
 #define AM65X_IOPAD(pa, val, muxmode)		(((pa) & 0x1fff)) ((val) | (muxmode))
 #define AM65X_WKUP_IOPAD(pa, val, muxmode)	(((pa) & 0x1fff)) ((val) | (muxmode))
