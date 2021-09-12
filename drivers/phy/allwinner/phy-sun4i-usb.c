6617894b6903e1cfc829add1558143be75d6bcd0
Samuel Holland <samuel@sholland.org>
2021-09-12 09:22:40 -0500

    phy: sun4i-usb: Remove a couple of debug messages
    Both of these messages log the GPIO number of the ID detection GPIO,
    which is not terribly useful, especially in the VBUS detection function.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Reviewed-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- drivers/phy/allwinner/phy-sun4i-usb.c
+++ drivers/phy/allwinner/phy-sun4i-usb.c
@@ -393,8 +393,6 @@ int sun4i_usb_phy_vbus_detect(struct phy *phy)
 	struct sun4i_usb_phy_plat *usb_phy = &data->usb_phy[phy->id];
 	int err, retries = 3;
 
-	debug("%s: id_det = %d\n", __func__, usb_phy->gpio_id_det);
-
 	if (usb_phy->gpio_vbus_det < 0)
 		return usb_phy->gpio_vbus_det;
 
@@ -416,8 +414,6 @@ int sun4i_usb_phy_id_detect(struct phy *phy)
 {
 	struct sun4i_usb_phy_data *data = dev_get_priv(phy->dev);
 	struct sun4i_usb_phy_plat *usb_phy = &data->usb_phy[phy->id];
-
-	debug("%s: id_det = %d\n", __func__, usb_phy->gpio_id_det);
 
 	if (usb_phy->gpio_id_det < 0)
 		return usb_phy->gpio_id_det;
