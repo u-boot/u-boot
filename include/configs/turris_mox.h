ba89abd028cb4e0c248c1a11ca2c8b34a407bff9
Marek Behún <marek.behun@nic.cz>
2021-10-09 19:33:39 +0200

    arm: mvebu: turris_mox: Cosmetic update for board config header
    Reorder the definitions in Turris MOX' board config header, drop the
    comment relics from when this file was copied, fix indentation.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>
    Reviewed-by: Stefan Roese <sr@denx.de>

--- include/configs/turris_mox.h
+++ include/configs/turris_mox.h
@@ -8,13 +8,11 @@
 #ifndef _CONFIG_TURRIS_MOX_H
 #define _CONFIG_TURRIS_MOX_H
 
-#define CONFIG_SYS_BOOTM_LEN (64 << 20)
-
-/* additions for new ARM relocation support */
-#define CONFIG_SYS_SDRAM_BASE	0x00000000
-
-/* auto boot */
-
+#define CONFIG_SYS_BOOTM_LEN		(64 << 20)
+#define CONFIG_SYS_SDRAM_BASE		0x00000000
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_TEXT_BASE + 0xFF0000)
+#define CONFIG_SYS_CBSIZE		1024
+#define CONFIG_SYS_MAXARGS		32
 #define CONFIG_SYS_BAUDRATE_TABLE	{ 300, 600, 1200, 1800, 2400, 4800, \
 					  9600, 19200, 38400, 57600, 115200, \
 					  230400, 460800, 500000, 576000, \
@@ -23,25 +21,10 @@
 					  4000000, 4500000, 5000000, 5500000, \
 					  6000000 }
 
-#define	CONFIG_SYS_CBSIZE	1024	/* Console I/O Buff Size */
+#define CONFIG_ARP_TIMEOUT		200
+#define CONFIG_NET_RETRY_COUNT		50
 
-/*
- * Other required minimal configurations
- */
-#define CONFIG_SYS_MAXARGS	32	/* max number of command args */
-
-/* End of 16M scrubbed by training in bootrom */
-#define CONFIG_SYS_INIT_SP_ADDR         (CONFIG_SYS_TEXT_BASE + 0xFF0000)
-
-/* Environment in SPI NOR flash */
-
-/*
- * Ethernet Driver configuration
- */
-#define CONFIG_ARP_TIMEOUT	200
-#define CONFIG_NET_RETRY_COUNT	50
-
-#define CONFIG_USB_MAX_CONTROLLER_COUNT (3 + 3)
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	6
 
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 0) \
@@ -51,22 +34,22 @@
 
 #include <config_distro_bootcmd.h>
 
-#define TURRIS_MOX_BOOTCMD_RESCUE \
-	"setenv bootargs \"console=ttyMV0,115200 " \
-			  "earlycon=ar3700_uart,0xd0012000\" && " \
-	"sf probe && " \
-	"sf read 0x5000000 0x190000 && " \
-	"lzmadec 0x5000000 0x5800000 && " \
+#define TURRIS_MOX_BOOTCMD_RESCUE					\
+	"setenv bootargs \"console=ttyMV0,115200 "			\
+			  "earlycon=ar3700_uart,0xd0012000\" && "	\
+	"sf probe && "							\
+	"sf read 0x5000000 0x190000 && "				\
+	"lzmadec 0x5000000 0x5800000 && "				\
 	"bootm 0x5800000"
 
-#define CONFIG_EXTRA_ENV_SETTINGS	\
-	"scriptaddr=0x4d00000\0"	\
-	"pxefile_addr_r=0x4e00000\0"	\
-	"fdt_addr_r=0x4f00000\0"	\
-	"kernel_addr_r=0x5000000\0"	\
-	"ramdisk_addr_r=0x8000000\0"	\
-	"fdtfile=marvell/" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
-	"bootcmd_rescue=" TURRIS_MOX_BOOTCMD_RESCUE "\0" \
+#define CONFIG_EXTRA_ENV_SETTINGS				\
+	"scriptaddr=0x4d00000\0"				\
+	"pxefile_addr_r=0x4e00000\0"				\
+	"fdt_addr_r=0x4f00000\0"				\
+	"kernel_addr_r=0x5000000\0"				\
+	"ramdisk_addr_r=0x8000000\0"				\
+	"fdtfile=marvell/" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0"	\
+	"bootcmd_rescue=" TURRIS_MOX_BOOTCMD_RESCUE "\0"	\
 	BOOTENV
 
 #endif /* _CONFIG_TURRIS_MOX_H */
