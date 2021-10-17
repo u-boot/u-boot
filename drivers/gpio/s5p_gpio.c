69bde0410a9ce5749a4cab5ed134527c2cb8b9e4
Dzmitry Sankouski <dsankouski@gmail.com>
2021-10-17 13:45:41 +0300

    SoC: exynos: add support for exynos 78x0
    Samsung Exynos 7880 \ 7870 - SoC for mainstream smartphones and tablets
    introduced on March 2017.
    Features:
    - 8 Cortex A53 cores
    - ARM Mali-T830 MP3 GPU
    - LTE Cat. 7 (7880) or 6 (7870) modem
    
    Signed-off-by: Dzmitry Sankouski <dsankouski@gmail.com>
    Cc: Minkyu Kang <mk7.kang@samsung.com>

--- drivers/gpio/s5p_gpio.c
+++ drivers/gpio/s5p_gpio.c
@@ -357,6 +357,7 @@ static const struct udevice_id exynos_gpio_ids[] = {
 	{ .compatible = "samsung,exynos4x12-pinctrl" },
 	{ .compatible = "samsung,exynos5250-pinctrl" },
 	{ .compatible = "samsung,exynos5420-pinctrl" },
+	{ .compatible = "samsung,exynos78x0-gpio" },
 	{ }
 };
 
