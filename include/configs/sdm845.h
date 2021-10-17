80565ec6f0c666e70b0fedd63617f0811c9fcfa7
Dzmitry Sankouski <dsankouski@gmail.com>
2021-10-17 13:44:32 +0300

    board: samsung: add Samsung Galaxy S9/S9+(SM-G96x0) board
    Samsung S9 SM-G9600 - Snapdragon SDM845 version of the phone,
    for China \ Hong Kong markets.
    Has unlockable bootloader, unlike SM-G960U (American market version),
    which allows running u-boot as a chain-loaded bootloader.
    
    Signed-off-by: Dzmitry Sankouski <dsankouski@gmail.com>
    Cc: Ramon Fried <rfried.dev@gmail.com>
    Cc: Tom Rini <trini@konsulko.com>

--- include/configs/sdm845.h
+++ include/configs/sdm845.h
@@ -11,19 +11,12 @@
 #include <linux/sizes.h>
 #include <asm/arch/sysmap-sdm845.h>
 
-#define CONFIG_SYS_LOAD_ADDR	0x80000000
 #define CONFIG_SYS_BAUDRATE_TABLE	{ 115200, 230400, 460800, 921600 }
 
 /* Generic Timer Definitions */
 #define COUNTER_FREQUENCY	19000000
 
-#define EXTRA_ENV_SETTINGS \
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"loadaddr=0x80000000\0"
-
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN   (CONFIG_ENV_SIZE + SZ_8M)
 #define CONFIG_SYS_BOOTM_LEN	SZ_64M
 
 /* Monitor Command Prompt */
