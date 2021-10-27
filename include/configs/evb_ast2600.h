e3cdc2cbb1b1a773b98bd39885c9be6b809375fb
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2021-10-27 14:17:35 +0800

    configs: ast2600: Boot kernel FIT in DRAM
    AST2600 leverages the FIT hash/signature verification to fulfill
    secure boot trust chain. To improve the performance and save SW
    code size for those crypto operations, the two HW crypto engine,
    HACE and ACRY, are enabled.
    
    However, both of the engines can only access to data stored in
    DRAM space. Therefore, we need to move the FIT image into DRAM
    before the booting.
    
    This patch update the CONFIG_BOOTCOMMAND to execute the pre-defined
    ENV variable which consists of FIT image copy to memory and booting.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>

--- include/configs/evb_ast2600.h
+++ include/configs/evb_ast2600.h
@@ -14,7 +14,14 @@
 #define CONFIG_SYS_LOAD_ADDR		0x83000000
 
 /* Misc */
+#define STR_HELPER(s)	#s
+#define STR(s)		STR_HELPER(s)
+
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"bootspi=fdt addr 20100000 && fdt header get fitsize totalsize && " \
+	"cp.b 20100000 ${loadaddr} ${fitsize} && bootm; " \
+	"echo Error loading kernel FIT image\0" \
 	""
 
 #endif	/* __CONFIG_H */
