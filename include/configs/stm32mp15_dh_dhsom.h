15e20e4bdf3a247962c2274070e0b28ab6f1e2f5
Marek Vasut <marex@denx.de>
2021-11-13 03:24:44 +0100

    ARM: stm32: Increase USB power-good delay on DHSOM
    The USB hub on STM32MP1 DHCOM boards needs to wait a bit longer until
    the USB Vbus is stable. Increase the USB power-good delay to 1 s.
    
    This adds default-undefined STM32MP_BOARD_EXTRA_ENV variable into
    stm32mp15_common.h to reduce duplication in board-specific config
    files adding custom environment.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Patrice Chotard <patrice.chotard@foss.st.com>
    Cc: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- include/configs/stm32mp15_dh_dhsom.h
+++ include/configs/stm32mp15_dh_dhsom.h
@@ -8,6 +8,9 @@
 #ifndef __CONFIG_STM32MP15_DH_DHSOM_H__
 #define __CONFIG_STM32MP15_DH_DHSOM_H__
 
+#define STM32MP_BOARD_EXTRA_ENV \
+	"usb_pgood_delay=1000\0"
+
 #include <configs/stm32mp15_common.h>
 
 #define CONFIG_SPL_TARGET		"u-boot.itb"
