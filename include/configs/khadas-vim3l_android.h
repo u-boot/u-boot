425f06f86ee9812cece584b1e4c15ea3f1b7c7d6
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2021-11-22 16:22:06 +0100

    configs: prepare khadas-vim3{l}_ab_android for AOSP support
    In AOSP, both VIM3 and VIM3L have 2 bootloader flavors,
    depending on A/B enablement.
    
    For example, for vim3l, the naming is:
    - u-boot_kvim3l_noab.bin : legacy support
    - u-boot_kvim3l_ab.bin   : A/B support
    
    Prepare a defconfig to support u-boot_kvim3_ab.bin and
    u-boot_kvim3l_ab.bin.
    
    This is identical to khadas-vim3{l}_ab_android but will be updated in
    the next commit.
    
    Also update partitioning tables for A/B support.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Link: https://lore.kernel.org/r/20211122152207.219023-4-mkorpershoek@baylibre.com

--- include/configs/khadas-vim3l_android.h
+++ include/configs/khadas-vim3l_android.h
@@ -12,6 +12,21 @@
 #define LOGO_UUID "43a3305d-150f-4cc9-bd3b-38fca8693846;"
 #define ROOT_UUID "ddb8c3f6-d94d-4394-b633-3134139cc2e0;"
 
+#if defined(CONFIG_CMD_AB_SELECT)
+#define PARTS_DEFAULT \
+	"uuid_disk=${uuid_gpt_disk};" \
+	"name=logo,start=512K,size=2M,uuid=" LOGO_UUID \
+	"name=misc,size=512K,uuid=${uuid_gpt_misc};" \
+	"name=dtbo_a,size=8M,uuid=${uuid_gpt_dtbo_a};" \
+	"name=dtbo_b,size=8M,uuid=${uuid_gpt_dtbo_b};" \
+	"name=vbmeta_a,size=512K,uuid=${uuid_gpt_vbmeta_a};" \
+	"name=vbmeta_b,size=512K,uuid=${uuid_gpt_vbmeta_b};" \
+	"name=boot_a,size=32M,bootable,uuid=${uuid_gpt_boot_a};" \
+	"name=boot_b,size=32M,bootable,uuid=${uuid_gpt_boot_b};" \
+	"name=super,size=3072M,uuid=${uuid_gpt_super};" \
+	"name=userdata,size=11282M,uuid=${uuid_gpt_userdata};" \
+	"name=rootfs,size=-,uuid=" ROOT_UUID
+#else
 #define PARTS_DEFAULT \
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=logo,start=512K,size=2M,uuid=" LOGO_UUID \
@@ -24,6 +39,7 @@
 	"name=super,size=1792M,uuid=${uuid_gpt_super};" \
 	"name=userdata,size=12786M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
+#endif
 
 #define EXTRA_ANDROID_ENV_SETTINGS \
 	"board=vim3l\0" \
