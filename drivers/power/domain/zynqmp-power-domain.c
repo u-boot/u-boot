186b68590bf7a947f7334d34b3e57f016c8ee384
Michal Simek <michal.simek@xilinx.com>
2021-11-30 16:32:43 +0100

    power: zynqmp: Add power domain driver for ZynqMP
    Driver should be enabled by CONFIG_POWER_DOMAIN=y and
    CONFIG_ZYNQMP_POWER_DOMAIN=y. Power domain driver doesn't have own DT node
    but it uses zynqmp firmware DT node that's why there is a need to bind
    driver when firmware node is found.
    
    Driver itself is simple. It is sending pmufw config object overlay for
    enabling access to device which is done in ...domain_request().
    In ...domain_on() capabilities are passed and node is requested.
    This should be bare minimum of required to get power domain driver working.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ drivers/power/domain/zynqmp-power-domain.c
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2021, Xilinx. Inc.
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <dm/device_compat.h>
+#include <log.h>
+#include <malloc.h>
+#include <misc.h>
+#include <power-domain-uclass.h>
+#include <linux/bitops.h>
+
+#include <zynqmp_firmware.h>
+
+static int zynqmp_pm_request_node(const u32 node, const u32 capabilities,
+				  const u32 qos, const enum zynqmp_pm_request_ack ack)
+{
+	return xilinx_pm_request(PM_REQUEST_NODE, node, capabilities,
+				   qos, ack, NULL);
+}
+
+static int zynqmp_power_domain_request(struct power_domain *power_domain)
+{
+	dev_dbg(power_domain->dev, "Request for id: %ld\n", power_domain->id);
+
+	return zynqmp_pmufw_node(power_domain->id);
+}
+
+static int zynqmp_power_domain_free(struct power_domain *power_domain)
+{
+	/* nop now */
+	return 0;
+}
+
+static int zynqmp_power_domain_on(struct power_domain *power_domain)
+{
+	dev_dbg(power_domain->dev, "Domain ON for id: %ld\n", power_domain->id);
+
+	return zynqmp_pm_request_node(power_domain->id,
+				      ZYNQMP_PM_CAPABILITY_ACCESS,
+				      ZYNQMP_PM_MAX_QOS,
+				      ZYNQMP_PM_REQUEST_ACK_BLOCKING);
+}
+
+static int zynqmp_power_domain_off(struct power_domain *power_domain)
+{
+	/* nop now */
+	return 0;
+}
+
+struct power_domain_ops zynqmp_power_domain_ops = {
+	.request = zynqmp_power_domain_request,
+	.rfree = zynqmp_power_domain_free,
+	.on = zynqmp_power_domain_on,
+	.off = zynqmp_power_domain_off,
+};
+
+static int zynqmp_power_domain_probe(struct udevice *dev)
+{
+	return 0;
+}
+
+U_BOOT_DRIVER(zynqmp_power_domain) = {
+	.name = "zynqmp_power_domain",
+	.id = UCLASS_POWER_DOMAIN,
+	.probe = zynqmp_power_domain_probe,
+	.ops = &zynqmp_power_domain_ops,
+};
