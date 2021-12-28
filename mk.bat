a311c0f4748633fc63a67947ba3e9d3e5d9b227a
Jonathan Engdahl <engdahl@windstream.net>
2021-12-28 18:12:50 -0500

    got u-boot to run on Nucleo-H743!
    ripped out nucleo board defs, which was based on H743-disco -- used a modified H743-eval setup

--- mk.bat
+++ mk.bat
@@ -1,3 +1,4 @@
 make CROSS_COMPILE=arm-cortexm7-eabi- KCFLAGS="-fno-inline -Og" HOSTLDLIBS="-lssl -lcrypto" %*
+rem make CROSS_COMPILE=arm-cortexm7-eabi- KCFLAGS="-fno-inline -Og -DDEBUG" HOSTLDLIBS="-lssl -lcrypto" %*
 
 dtc -I dtb -O dts dts/dt.dtb >u-boot.dts
