f8a5cda5ec27fd532a218c9988aa30b2318cb13d
Ying-Chun Liu (PaulLiu) <paul.liu@linaro.org>
2022-01-12 00:38:04 +0800

    configs: imx8mm-cl-iot-gate: update dfu_alt_info for single flash.bin
    We changed to single flash.bin now. So dfu_alt_info should be modified
    to reflect this change.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paul.liu@linaro.org>
    Reviewed-by: Fabio Estevam <festevam@gmail.com>

--- include/configs/imx8mm-cl-iot-gate.h
+++ include/configs/imx8mm-cl-iot-gate.h
@@ -72,8 +72,7 @@
 	"fdt_addr=0x43000000\0"			\
 	"fdt_addr_r=0x43000000\0" \
 	"boot_fit=no\0" \
-	"dfu_alt_info=mmc 2=flash-bin raw 0x42 0x250 mmcpart 1;" \
-		"u-boot-itb raw 0x300 0x1B00 mmcpart 1\0"	 \
+	"dfu_alt_info=mmc 2=flash-bin raw 0x42 0x1D00 mmcpart 1\0" \
 	"fdt_file=sb-iotgimx8.dtb\0" \
 	"fdtfile=sb-iotgimx8.dtb\0" \
 	"initrd_addr=0x43800000\0"		\
