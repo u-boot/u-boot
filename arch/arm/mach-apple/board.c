9a8e3736da25a62449f84aef2b566e2b31ca60fd
Mark Kettenis <kettenis@openbsd.org>
2022-01-12 19:55:17 +0100

    arm: apple: Use watchdog timer for system reset
    Rely on the new watchdog timer driver and the sysreset uclass to
    reset the system.  This gets rid of hard-coded addresses and
    should work on systems based on the new M1 Pro and M1 Max SoCs
    as well.
    
    Signed-off-by: Mark Kettenis <kettenis@openbsd.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Tested-on: Apple M1 Macbook
    Tested-by: Simon Glass <sjg@chromium.org>

--- arch/arm/mach-apple/board.c
+++ arch/arm/mach-apple/board.c
@@ -119,30 +119,6 @@ int dram_init_banksize(void)
 	return fdtdec_setup_memory_banksize();
 }
 
-#define APPLE_WDT_BASE		0x23d2b0000ULL
-
-#define APPLE_WDT_SYS_CTL_ENABLE	BIT(2)
-
-typedef struct apple_wdt {
-	u32	reserved0[3];
-	u32	chip_ctl;
-	u32	sys_tmr;
-	u32	sys_cmp;
-	u32	reserved1;
-	u32	sys_ctl;
-} apple_wdt_t;
-
-void reset_cpu(void)
-{
-	apple_wdt_t *wdt = (apple_wdt_t *)APPLE_WDT_BASE;
-
-	writel(0, &wdt->sys_cmp);
-	writel(APPLE_WDT_SYS_CTL_ENABLE, &wdt->sys_ctl);
-
-	while(1)
-		wfi();
-}
-
 extern long fw_dtb_pointer;
 
 void *board_fdt_blob_setup(int *err)
