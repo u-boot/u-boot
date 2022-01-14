c750c6dbb29535efa01464d46afc6ccf999cf7c5
Michal Simek <michal.simek@xilinx.com>
2022-01-14 13:25:35 +0100

    xilinx: firmware: Introduce zynqmp_pmufw_node() for loading PMU fragments
    Introduce zynqmp_pmufw_node() for loading PMU configuration fragment for
    enabling IPs. Firmware driver has small overlay where NODE id is added and
    config fragment is sent to PMUFW. There is a need to build PMUFW with
    fragment support.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Link: https://lore.kernel.org/r/636e8150bd4e2b1f988d59795772c685ceeec083.1642163135.git.michal.simek@xilinx.com

--- include/zynqmp_firmware.h
+++ include/zynqmp_firmware.h
@@ -367,6 +367,7 @@ enum pm_ioctl_id {
 #define PAYLOAD_ARG_CNT	5U
 
 unsigned int zynqmp_firmware_version(void);
+int zynqmp_pmufw_node(u32 id);
 void zynqmp_pmufw_load_config_object(const void *cfg_obj, size_t size);
 int xilinx_pm_request(u32 api_id, u32 arg0, u32 arg1, u32 arg2,
 		      u32 arg3, u32 *ret_payload);
