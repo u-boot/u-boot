5e6e41b3bab32f2ee258a76fda68b703e8f10cc3
Andre Kalb <svc.sw.rte.linux@sma.de>
2022-01-28 09:40:32 +0100

    net: bootp: Make root path (option 17) length configurable
    to adjust the root path length.
    Eg to 256 from Linux Kernel
    
    Signed-off-by: Andre Kalb <andre.kalb@sma.de>
    Reviewed-by: Ramon Fried <rfried.dev@gmail.com>
    [trini: Guard extern so that !CONFIG_NET platforms will build]
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/net.h
+++ include/net.h
@@ -536,7 +536,9 @@ extern struct in_addr net_dns_server2;
 #endif
 extern char	net_nis_domain[32];	/* Our IS domain */
 extern char	net_hostname[32];	/* Our hostname */
-extern char	net_root_path[64];	/* Our root path */
+#ifdef CONFIG_NET
+extern char	net_root_path[CONFIG_BOOTP_MAX_ROOT_PATH_LEN];	/* Our root path */
+#endif
 /** END OF BOOTP EXTENTIONS **/
 extern u8		net_ethaddr[ARP_HLEN];		/* Our ethernet address */
 extern u8		net_server_ethaddr[ARP_HLEN];	/* Boot server enet address */
