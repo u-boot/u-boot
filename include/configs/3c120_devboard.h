1ca890d789b45ddcda73454fe2b54f5fdb10ee3b
Ovidiu Panait <ovidiu.panait@windriver.com>
2022-01-31 09:20:22 +0200

    common: drop CONFIG_SYS_RESET_ADDR
    There are no boards that define CONFIG_SYS_RESET_ADDR, so drop the
    remaining comments referencing it and also the config_whitelist.txt entry.
    
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>
    Reviewed-by: Stefan Roese <sr@denx.de>
    Acked-by: thomas@wytron.com.tw

--- include/configs/3c120_devboard.h
+++ include/configs/3c120_devboard.h
@@ -47,13 +47,4 @@
 					 CONFIG_SYS_SDRAM_SIZE - \
 					 CONFIG_SYS_MONITOR_LEN)
 
-/*
- * ENVIRONMENT -- Put environment in sector CONFIG_SYS_MONITOR_LEN above
- * CONFIG_SYS_RESET_ADDR, since we assume the monitor is stored at the
- * reset address, no? This will keep the environment in user region
- * of flash. NOTE: the monitor length must be multiple of sector size
- * (which is common practice).
- */
-
-
 #endif /* __CONFIG_H */
