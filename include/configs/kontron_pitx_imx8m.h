fee2d974f140a2f34c90243b4f455d772273f051
Heiko Thiery <heiko.thiery@gmail.com>
2022-02-16 13:25:15 +0100

    kontron-pitx-imx8m: change environment address variables
    Currently the space between kernel_addr_r and the fdt_addr_r is only 32MB.
    To have enought space to load kernel images bigger than 32MB change the
    variables to a feasible value.
    
    The new environment variables layout is based on the scheme from
    "include/configs/ti_armv7_common.h".
    
    The CONFIG_SYS_LOAD_ADDR value is set to 0x42000000. With that we have
    the same value as for the kernel_addr_r.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- include/configs/kontron_pitx_imx8m.h
+++ include/configs/kontron_pitx_imx8m.h
@@ -41,12 +41,13 @@
 #endif
 
 #define ENV_MEM_LAYOUT_SETTINGS \
-	"kernel_addr_r=0x40880000\0" \
-	"fdt_addr_r=0x43000000\0" \
-	"scriptaddr=0x43500000\0" \
-	"initrd_addr=0x43800000\0" \
-	"pxefile_addr_r=0x43500000\0" \
-	"bootm_size=0x10000000\0" \
+	"loadaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"kernel_addr_r=0x42000000\0" \
+	"fdt_addr_r=0x48000000\0" \
+	"fdtoverlay_addr_r=0x49000000\0" \
+	"ramdisk_addr_r=0x48080000\0" \
+	"scriptaddr=0x40000000\0" \
+	"pxefile_addr_r=0x40100000\0"
 
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 0) \
