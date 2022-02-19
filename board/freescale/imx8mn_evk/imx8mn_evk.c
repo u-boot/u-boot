6ebe1a283d48d4370e9fc887443a451fd105ab88
Marek Vasut <marex@denx.de>
2022-02-19 17:13:54 +0100

    ARM: imx: imx8mn-ddr4-evk: Add ethernet support
    Add support for ethernet on the imx8mn-ddr4-evk.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Fabio Estevam <festevam@gmail.com>
    Cc: Peng Fan <peng.fan@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>

--- board/freescale/imx8mn_evk/imx8mn_evk.c
+++ board/freescale/imx8mn_evk/imx8mn_evk.c
@@ -7,17 +7,47 @@
 #include <env.h>
 #include <init.h>
 #include <asm/global_data.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
-
-int board_init(void)
-{
-	return 0;
-}
 
 int board_mmc_get_env_dev(int devno)
 {
 	return devno;
+}
+
+static void setup_fec(void)
+{
+	struct iomuxc_gpr_base_regs *gpr =
+		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+
+	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
+	clrsetbits_le32(&gpr->gpr[1], 0x2000, 0);
+}
+
+int board_phy_config(struct phy_device *phydev)
+{
+	/* enable rgmii rxc skew and phy mode select to RGMII copper */
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x1f);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x8);
+
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x00);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x82ee);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1d, 0x05);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0x1e, 0x100);
+
+	if (phydev->drv->config)
+		phydev->drv->config(phydev);
+	return 0;
+}
+
+int board_init(void)
+{
+	setup_fec();
+
+	return 0;
 }
 
 int board_late_init(void)
