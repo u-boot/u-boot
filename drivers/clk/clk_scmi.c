6983710a31a0d6fb782eb0ea18b9325e4075f4c3
Etienne Carriere <etienne.carriere@linaro.org>
2022-02-21 09:22:40 +0100

    scmi: change parameter dev in devm_scmi_process_msg
    Changes devm_scmi_process_msg() first argument from target parent device
    to current SCMI device and lookup the SCMI agent device among SCMI device
    parents for find the SCMI agent operator needed for communication with
    the firmware.
    
    This change is needed in order to support CCF in clk_scmi driver unless
    what CCF will fail to find the right udevice related to exposed SCMI
    clocks.
    
    This patch allows to simplify the caller sequence, using SCMI device
    reference as parameter instead of knowing SCMI uclass topology. This
    change also adds some protection in case devm_scmi_process_msg() API
    function is called for an invalid device type.
    
    Cc: Lukasz Majewski <lukma@denx.de>
    Cc: Sean Anderson <seanga2@gmail.com>
    Cc: Jaehoon Chung <jh80.chung@samsung.com>
    Cc: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- drivers/clk/clk_scmi.c
+++ drivers/clk/clk_scmi.c
@@ -24,7 +24,7 @@ static int scmi_clk_gate(struct clk *clk, int enable)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev->parent, &msg);
+	ret = devm_scmi_process_msg(clk->dev, &msg);
 	if (ret)
 		return ret;
 
@@ -52,7 +52,7 @@ static ulong scmi_clk_get_rate(struct clk *clk)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev->parent, &msg);
+	ret = devm_scmi_process_msg(clk->dev, &msg);
 	if (ret < 0)
 		return ret;
 
@@ -77,7 +77,7 @@ static ulong scmi_clk_set_rate(struct clk *clk, ulong rate)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev->parent, &msg);
+	ret = devm_scmi_process_msg(clk->dev, &msg);
 	if (ret < 0)
 		return ret;
 
