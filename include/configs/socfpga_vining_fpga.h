25cff0c1fa83e149e4693121f8b5e287659eed71
Marek Vasut <marex@denx.de>
2022-02-21 06:16:12 +0100

    arm: socfpga: vining: Fix mtdparts for 2x256 MiB SF variant
    The 2x256 MiB SF variant of this system has 192 MiB rootfs MTD partition
    containing UBI on SF0, use the correct size in U-Boot environment, else
    U-Boot cannot mount UBI and boot on this variant.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Siew Chin Lim <elly.siew.chin.lim@intel.com>
    Cc: Simon Goldschmidt <simon.k.r.goldschmidt@gmail.com>
    Cc: Tien Fong Chee <tien.fong.chee@intel.com>
    Cc: Tom Rini <trini@konsulko.com>

--- include/configs/socfpga_vining_fpga.h
+++ include/configs/socfpga_vining_fpga.h
@@ -60,7 +60,7 @@
 		"256k(softing1),"					\
 		"256k(softing2),"					\
 		"14720k(rcvrfs),"	/* Recovery */			\
-		"64m(rootfs),"		/* Root */			\
+		"192m(rootfs),"		/* Root */			\
 		"-(userfs)\0"		/* User */			\
 	"mtdparts_1_128m=ff705000.spi.1:" /* 16MiB+128MiB SF config */	\
 		"64m(rootfs),"						\
