42595eb7067c6c076e1c98213438be727f883fe2
Michael Walle <michael@walle.cc>
2022-02-25 18:06:24 +0530

    misc: add sl28cpld base driver
    Add a multi-function device driver which will probe its children and
    provides methods to access the device.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    [Rebased]
    Signed-off-by: Priyanka Jain <priyanka.jain@nxp.com>

---
+++ include/sl28cpld.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2021 Michael Walle <michael@walle.cc>
+ */
+
+#ifndef __SL28CPLD_H
+#define __SL28CPLD_H
+
+int sl28cpld_read(struct udevice *dev, uint offset);
+int sl28cpld_write(struct udevice *dev, uint offset, uint8_t value);
+int sl28cpld_update(struct udevice *dev, uint offset, uint8_t clear,
+		    uint8_t set);
+
+#endif
