e146a2c12ff1e9138f564ae6815a131bd850d8ef
Dhananjay Phadke <dphadke@linux.microsoft.com>
2022-03-15 10:19:32 -0700

    lib/crypto: support sha384/sha512 in x509/pkcs7
    Set digest_size SHA384 and SHA512 algorithms in pkcs7 and x509,
    (not set by ported linux code, but needed by __UBOOT__ part).
    
    EFI_CAPSULE_AUTHENTICATE doesn't select these algos but required for
    correctness if certificates contain sha384WithRSAEncryption or
    sha512WithRSAEncryption OIDs.
    
    Signed-off-by: Dhananjay Phadke <dphadke@linux.microsoft.com>
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- lib/crypto/x509_public_key.c
+++ lib/crypto/x509_public_key.c
@@ -71,6 +71,10 @@ int x509_get_sig_params(struct x509_certificate *cert)
 		return -ENOPKG;
 	if (!strcmp(sig->hash_algo, "sha256"))
 		sig->digest_size = SHA256_SUM_LEN;
+	else if (!strcmp(sig->hash_algo, "sha384"))
+		sig->digest_size = SHA384_SUM_LEN;
+	else if (!strcmp(sig->hash_algo, "sha512"))
+		sig->digest_size = SHA512_SUM_LEN;
 	else if (!strcmp(sig->hash_algo, "sha1"))
 		sig->digest_size = SHA1_SUM_LEN;
 	else
