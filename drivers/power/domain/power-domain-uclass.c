847990219c04fe4d4b69deefdd29a2fb2d8171d9
Sean Anderson <seanga2@gmail.com>
2022-03-23 14:26:09 -0400

    power-domain: Fix use of uninitialized value in dev_power_domain_ctrl
    If dev_count_phandle_with_args returns 0 or another error, then pd will never
    have been initialized by power_domain_get_by_index. Avoid comparing against
    pd.dev in this situation.
    
    Fixes: 3e4fcfa4bc ("power-domain: fix hang in endless loop on i.MX8")
    Signed-off-by: Sean Anderson <seanga2@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>

--- drivers/power/domain/power-domain-uclass.c
+++ drivers/power/domain/power-domain-uclass.c
@@ -137,7 +137,7 @@ static int dev_power_domain_ctrl(struct udevice *dev, bool on)
 	 * off their power-domain parent. So we will get here again and
 	 * again and will be stuck in an endless loop.
 	 */
-	if (!on && dev_get_parent(dev) == pd.dev &&
+	if (count > 0 && !on && dev_get_parent(dev) == pd.dev &&
 	    device_get_uclass_id(dev) == UCLASS_POWER_DOMAIN)
 		return ret;
 
