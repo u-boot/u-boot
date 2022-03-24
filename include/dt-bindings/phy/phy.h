7ce9d921bd560d00a601bd751ced32f71f9bbd28
Michal Simek <michal.simek@xilinx.com>
2022-03-24 10:17:51 +0100

    dt-bindings: phy: Sync phy.h with Linux kernel
    Make sure that both files are in sync to have the same values in DTs.
    The patch is fixing SPDX license as is used in the kernel and adding new
    values for PHY_TYPE_DPHY and PHY_TYPE_CPHY.
    
    SPDX license change was done by:
    Link: https://lkml.kernel.org/r/20190528170027.447718015@linutronix.de
    and new value added by:
    Link: https://lore.kernel.org/r/20210617144349.28448-3-jonathan@marek.ca
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Link: https://lore.kernel.org/r/06dbc03d4c9ac5d621341d8fb8cc16f489062b39.1648113469.git.michal.simek@xilinx.com

--- include/dt-bindings/phy/phy.h
+++ include/dt-bindings/phy/phy.h
@@ -1,10 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  *
  * This header provides constants for the phy framework
  *
  * Copyright (C) 2014 STMicroelectronics
  * Author: Gabriel Fernandez <gabriel.fernandez@st.com>
- * License terms:  GNU General Public License (GPL), version 2
  */
 
 #ifndef _DT_BINDINGS_PHY
@@ -20,5 +20,7 @@
 #define PHY_TYPE_XPCS		7
 #define PHY_TYPE_SGMII		8
 #define PHY_TYPE_QSGMII		9
+#define PHY_TYPE_DPHY		10
+#define PHY_TYPE_CPHY		11
 
 #endif /* _DT_BINDINGS_PHY */
