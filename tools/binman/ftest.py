524bc11deb0f1170ed9bd405215aa9b86432a988
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2022-03-26 14:40:37 +0300

    binman: Refuse to replace sections for now
    Binman interfaces allow attempts to replace any entry in the image with
    arbitrary data. When trying to replace sections, the changes in the
    section entry's data are not propagated to its child entries. This,
    combined with how sections rebuild their contents from its children,
    eventually causes the replaced contents to be silently overwritten by
    rebuilt contents equivalent to the original data.
    
    Add a simple test for replacing a section that is currently failing due
    to this behaviour, and mark it as an expected failure. Also, raise an
    error when replacing a section instead of silently pretending it was
    replaced.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>
    Series-name: binman-replace-fit-subentries
    Series-to: u-boot@lists.denx.de
    Series-cc: Simon Glass <sjg@chromium.org>
    Series-cc: Jan Kiszka <jan.kiszka@siemens.com>
    Series-cc: Heiko Thiery <heiko.thiery@gmail.com>

--- tools/binman/ftest.py
+++ tools/binman/ftest.py
@@ -5641,5 +5641,14 @@ fdt         fdtmap                Extract the devicetree blob from the fdtmap
         self.assertIsNotNone(path)
         self.assertEqual(expected_fdtmap, fdtmap)
 
+    @unittest.expectedFailure
+    def testReplaceSectionSimple(self):
+        """Test replacing a simple section with arbitrary data"""
+        new_data = b'w' * len(COMPRESS_DATA + U_BOOT_DATA)
+        data, expected_fdtmap, _ = self._RunReplaceCmd(
+            'section', new_data,
+            dts='234_replace_section_simple.dts')
+        self.assertEqual(new_data, data)
+
 if __name__ == "__main__":
     unittest.main()
