a62acac12a57793d0192a8104446bf99b1d14c45
Tom Rini <trini@konsulko.com>
2022-03-30 18:07:19 -0400

    stm32f429-discovery: Migrate CONFIG_*_LED out of CONFIG namespace
    These values are only used in one file, reference them more directly.
    
    Cc: Kamil Lulko <kamil.lulko@gmail.com>
    Cc: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Cc: Patrice Chotard <patrice.chotard@foss.st.com>
    Cc: uboot-stm32@st-md-mailman.stormreply.com
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- board/st/stm32f429-discovery/led.c
+++ board/st/stm32f429-discovery/led.c
@@ -8,30 +8,33 @@
 #include <status_led.h>
 #include <asm-generic/gpio.h>
 
+#define RED_LED			110
+#define GREEN_LED		109
+
 void coloured_LED_init(void)
 {
-	gpio_request(CONFIG_RED_LED, "red led");
-	gpio_direction_output(CONFIG_RED_LED, 0);
-	gpio_request(CONFIG_GREEN_LED, "green led");
-	gpio_direction_output(CONFIG_GREEN_LED, 0);
+	gpio_request(RED_LED, "red led");
+	gpio_direction_output(RED_LED, 0);
+	gpio_request(GREEN_LED, "green led");
+	gpio_direction_output(GREEN_LED, 0);
 }
 
 void red_led_off(void)
 {
-	gpio_set_value(CONFIG_RED_LED, 0);
+	gpio_set_value(RED_LED, 0);
 }
 
 void green_led_off(void)
 {
-	gpio_set_value(CONFIG_GREEN_LED, 0);
+	gpio_set_value(GREEN_LED, 0);
 }
 
 void red_led_on(void)
 {
-	gpio_set_value(CONFIG_RED_LED, 1);
+	gpio_set_value(RED_LED, 1);
 }
 
 void green_led_on(void)
 {
-	gpio_set_value(CONFIG_GREEN_LED, 1);
+	gpio_set_value(GREEN_LED, 1);
 }
