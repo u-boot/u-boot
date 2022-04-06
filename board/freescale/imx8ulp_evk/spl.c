4ddb33dac51e23c6ab93cb8b266ccb2c24b630d3
Ye Li <ye.li@nxp.com>
2022-04-06 14:30:11 +0800

    imx: imx8ulp_evk: Remove PMIC Bucks PWM mode settings
    This workaround is not needed on i.MX8ULP proto-1B EVK as board has
    fixed the problem. Because we don't support proto-1A any longer,
    remove the PMIC settings.
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx8ulp_evk/spl.c
+++ board/freescale/imx8ulp_evk/spl.c
@@ -35,24 +35,6 @@ u32 spl_boot_device(void)
 
 int power_init_board(void)
 {
-	u32 pmic_reg;
-
-	/* PMIC set bucks1-4 to PWM mode */
-	upower_pmic_i2c_read(0x10, &pmic_reg);
-	upower_pmic_i2c_read(0x14, &pmic_reg);
-	upower_pmic_i2c_read(0x21, &pmic_reg);
-	upower_pmic_i2c_read(0x2e, &pmic_reg);
-
-	upower_pmic_i2c_write(0x10, 0x3d);
-	upower_pmic_i2c_write(0x14, 0x7d);
-	upower_pmic_i2c_write(0x21, 0x7d);
-	upower_pmic_i2c_write(0x2e, 0x3d);
-
-	upower_pmic_i2c_read(0x10, &pmic_reg);
-	upower_pmic_i2c_read(0x14, &pmic_reg);
-	upower_pmic_i2c_read(0x21, &pmic_reg);
-	upower_pmic_i2c_read(0x2e, &pmic_reg);
-
 	/* Set buck3 to 1.1v OD */
 	upower_pmic_i2c_write(0x22, 0x28);
 	return 0;
