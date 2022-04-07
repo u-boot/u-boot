9859e9f8f6600d1df9301e5a74a26ebd7115e7d4
Robert Nelson <robertcnelson@gmail.com>
2022-04-07 09:52:30 -0500

    bbb.io: add basic build script for am335x/am57xx
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build-am57xx.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sys=$(uname -m)
+
+#add: bc bison flex libssl-dev u-boot-tools binutils gcc
+
+DIR="$PWD"
+
+config="am57xx_evm_defconfig"
+
+if [ "x${sys}" = "xarmv7l" ] ; then
+	if [ ! -f ./load.menuconfig ] ; then
+		echo "Developers: too make changes: [touch load.menuconfig]"
+	fi
+fi
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+
+if [ "x${sys}" = "xarmv7l" ] ; then
+	if [ -f ./load.menuconfig ] ; then
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${config}
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
+		cp -v defconfig ./configs/${config}
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+	fi
+
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${config}
+	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
+else
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${config}
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
+
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
+	cp -v defconfig ./configs/${config}
+fi
