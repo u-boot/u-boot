71dcf8128c39c81edfc4ccb10f9463837df5d37d
Pali Rohár <pali@kernel.org>
2022-04-07 12:16:19 +0200

    board: freescale: p1_p2_rdb_pc: Move ifdef for USB/eLBC check to correct place
    Whole section about USB/eLBC configuration seems to be P1020 specific. So
    add ifdefs to not compile it on other platforms (e.g. P2020).
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Reviewed-by: Priyanka Jain <priyanka.jain@nxp.com>

--- board/freescale/p1_p2_rdb_pc/p1_p2_rdb_pc.c
+++ board/freescale/p1_p2_rdb_pc/p1_p2_rdb_pc.c
@@ -359,9 +359,9 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 #if defined(CONFIG_TARGET_P1020RDB_PD) || defined(CONFIG_TARGET_P1020RDB_PC)
 	const char *soc_usb_compat = "fsl-usb2-dr";
 	int usb_err, usb1_off, usb2_off;
-#endif
 #if defined(CONFIG_SDCARD) || defined(CONFIG_SPIFLASH)
 	int err;
+#endif
 #endif
 
 	ft_cpu_setup(blob, bd);
@@ -380,6 +380,7 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 	fsl_fdt_fixup_dr_usb(blob, bd);
 #endif
 
+#if defined(CONFIG_TARGET_P1020RDB_PD) || defined(CONFIG_TARGET_P1020RDB_PC)
 #if defined(CONFIG_SDCARD) || defined(CONFIG_SPIFLASH)
 	/* Delete eLBC node as it is muxed with USB2 controller */
 	if (hwconfig("usb2")) {
@@ -401,7 +402,6 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 	}
 #endif
 
-#if defined(CONFIG_TARGET_P1020RDB_PD) || defined(CONFIG_TARGET_P1020RDB_PC)
 /* Delete USB2 node as it is muxed with eLBC */
 	usb1_off = fdt_node_offset_by_compatible(blob, -1,
 		soc_usb_compat);
