d7f72b68307ee3323c14c1c9b27e7d3b5a460bf7
Marek Vasut <marex@denx.de>
2022-04-08 02:15:01 +0200

    ARM: dts: imx8mm: Add i.MX8M Mini Toradex Verdin based Menlo board
    Add new board based on the Toradex Verdin iMX8M Mini SoM, the MX8Menlo.
    The board is a compatible replacement for i.MX53 M53Menlo and features
    USB, multiple UARTs, ethernet, LEDs, SD and eMMC.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Fabio Estevam <festevam@denx.de>
    Cc: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    Cc: Max Krummenacher <max.krummenacher@toradex.com>
    Cc: Peng Fan <peng.fan@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>

---
+++ include/configs/imx8mm-mx8menlo.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2021-2022 Marek Vasut <marex@denx.de>
+ */
+
+#ifndef __IMX8MM_MX8MENLO_H
+#define __IMX8MM_MX8MENLO_H
+
+#include <configs/verdin-imx8mm.h>
+
+/* Custom initial environment variables */
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	BOOTENV								\
+	MEM_LAYOUT_ENV_SETTINGS						\
+	"devtype=mmc\0"							\
+	"devnum=1\0"							\
+	"distro_bootpart=1\0"						\
+	"altbootcmd="							\
+		"mmc partconf 0 mmcpart ; "				\
+		"if test ${mmcpart} -eq 1 ; then "			\
+			"mmc partconf 0 1 2 0 ; "			\
+		"else "							\
+			"mmc partconf 0 1 1 0 ; "			\
+		"fi ; "							\
+		"boot\0"						\
+	"boot_file=fitImage\0"						\
+	"console=ttymxc1\0"						\
+	"fdt_addr=0x43000000\0"						\
+	"initrd_addr=0x43800000\0"					\
+	"kernel_image=fitImage\0"
+
+#undef CONFIG_MXC_UART_BASE
+#define CONFIG_MXC_UART_BASE		UART2_BASE_ADDR
+
+#endif /* __IMX8MM_MX8MENLO_H */
