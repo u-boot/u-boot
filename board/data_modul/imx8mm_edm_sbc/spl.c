bba6cc7018f0d282c222bb38a3eeab1764f538a8
Marek Vasut <marex@denx.de>
2022-04-12 17:26:01 +0200

    ARM: dts: imx: Add support for Data Modul i.MX8M Mini eDM SBC
    Add support for Data Modul i.MX8M Mini eDM SBC board. This is an
    evaluation board for various custom display units. Currently
    supported are serial console, ethernet, eMMC, SD, SPI NOR,
    USB host and USB OTG.
    
    Reviewed-by: Fabio Estevam <festevam@denx.de>
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Fabio Estevam <festevam@gmail.com>
    Cc: Peng Fan <peng.fan@nxp.com>
    Cc: Stefano Babic <sbabic@denx.de>

---
+++ board/data_modul/imx8mm_edm_sbc/spl.c
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2022 Marek Vasut <marex@denx.de>
+ */
+
+#include <common.h>
+#include <hang.h>
+#include <image.h>
+#include <init.h>
+#include <spl.h>
+#include <asm/io.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/imx8mm_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch/ddr.h>
+#include <asm/mach-imx/boot_mode.h>
+
+#include <dm/uclass.h>
+#include <dm/device.h>
+#include <dm/uclass-internal.h>
+#include <dm/device-internal.h>
+
+#include <power/pmic.h>
+#include <power/bd71837.h>
+
+#include "lpddr4_timing.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
+#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
+
+static const iomux_v3_cfg_t uart_pads[] = {
+	IMX8MM_PAD_UART3_RXD_UART3_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	IMX8MM_PAD_UART3_TXD_UART3_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static const iomux_v3_cfg_t wdog_pads[] = {
+	IMX8MM_PAD_GPIO1_IO02_WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+};
+
+static void data_modul_imx8mm_edm_sbc_early_init_f(void)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
+
+	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
+
+	set_wdog_reset(wdog);
+
+	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
+}
+
+static int data_modul_imx8mm_edm_sbc_board_power_init(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = pmic_get("pmic@4b", &dev);
+	if (ret == -ENODEV) {
+		puts("Failed to get PMIC\n");
+		return 0;
+	}
+	if (ret != 0)
+		return ret;
+
+	/* Unlock the PMIC regs */
+	pmic_reg_write(dev, BD718XX_REGLOCK, 0x1);
+
+	/* Increase VDD_SOC to typical value 0.85V before first DRAM access */
+	pmic_reg_write(dev, BD718XX_BUCK1_VOLT_RUN, 0x0f);
+
+	/* Increase VDD_DRAM to 0.975V for 3GHz DDR */
+	pmic_reg_write(dev, BD718XX_1ST_NODVS_BUCK_VOLT, 0x83);
+
+	/* Lock the PMIC regs */
+	pmic_reg_write(dev, BD718XX_REGLOCK, 0x11);
+
+	return 0;
+}
+
+int spl_board_boot_device(enum boot_device boot_dev_spl)
+{
+	if (boot_dev_spl == MMC3_BOOT)
+		return BOOT_DEVICE_MMC2;	/* eMMC */
+	else
+		return BOOT_DEVICE_MMC1;	/* SD */
+}
+
+void board_boot_order(u32 *spl_boot_list)
+{
+	int boot_device = spl_boot_device();
+
+	spl_boot_list[0] = boot_device;		/* 1:SD 2:eMMC */
+
+	if (boot_device == BOOT_DEVICE_MMC1)
+		spl_boot_list[1] = BOOT_DEVICE_MMC2;	/* eMMC */
+	else
+		spl_boot_list[1] = BOOT_DEVICE_MMC1;	/* SD */
+
+	spl_boot_list[2] = BOOT_DEVICE_UART;	/* YModem */
+	spl_boot_list[3] = BOOT_DEVICE_NONE;
+}
+
+static struct dram_timing_info *dram_timing_info[8] = {
+	&dmo_imx8mm_sbc_dram_timing_32_32,	/* 32 Gbit x32 */
+	NULL,					/* 32 Gbit x16 */
+	&dmo_imx8mm_sbc_dram_timing_16_32,	/* 16 Gbit x32 */
+	NULL,					/* 16 Gbit x16 */
+	NULL,					/* 8 Gbit x32 */
+	NULL,					/* 8 Gbit x16 */
+	NULL,					/* INVALID */
+	NULL,					/* INVALID */
+};
+
+static void spl_dram_init(void)
+{
+	u8 memcfg = dmo_get_memcfg();
+	int i;
+
+	printf("DDR:   %d GiB x%d [0x%x]\n",
+	       /* 0..4 GiB, 1..2 GiB, 0..1 GiB */
+	       4 >> ((memcfg >> 1) & 0x3),
+	       /* 0..x32, 1..x16 */
+	       32 >> (memcfg & BIT(0)),
+	       memcfg);
+
+	if (!dram_timing_info[memcfg]) {
+		printf("Unsupported DRAM strapping, trying lowest supported. MEMCFG=0x%x\n",
+		       memcfg);
+		for (i = ARRAY_SIZE(dram_timing_info) - 1; i >= 0; i--)
+			if (dram_timing_info[i])	/* Configuration found */
+				break;
+	}
+
+	ddr_init(dram_timing_info[memcfg]);
+}
+
+void board_init_f(ulong dummy)
+{
+	struct udevice *dev;
+	int ret;
+
+	icache_enable();
+
+	arch_cpu_init();
+
+	init_uart_clk(2);
+
+	data_modul_imx8mm_edm_sbc_early_init_f();
+
+	preloader_console_init();
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	ret = spl_early_init();
+	if (ret) {
+		debug("spl_early_init() failed: %d\n", ret);
+		hang();
+	}
+
+	ret = uclass_get_device_by_name(UCLASS_CLK,
+					"clock-controller@30380000",
+					&dev);
+	if (ret < 0) {
+		printf("Failed to find clock node. Check device tree\n");
+		hang();
+	}
+
+	enable_tzc380();
+
+	data_modul_imx8mm_edm_sbc_board_power_init();
+
+	/* DDR initialization */
+	spl_dram_init();
+
+	board_init_r(NULL, 0);
+}
---
+++ include/configs/imx8mm_data_modul_edm_sbc.h
@@ -0,0 +1,129 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2022 Marek Vasut <marex@denx.de>
+ */
+
+#ifndef __IMX8MM_DATA_MODUL_EDM_SBC_H
+#define __IMX8MM_DATA_MODUL_EDM_SBC_H
+
+#include <linux/sizes.h>
+#include <linux/stringify.h>
+#include <asm/arch/imx-regs.h>
+
+#define CONFIG_SYS_BOOTM_LEN		SZ_128M
+
+#define CONFIG_SPL_MAX_SIZE		(148 * 1024)
+#define CONFIG_SYS_MONITOR_LEN		SZ_1M
+
+#define CONFIG_SPL_STACK		0x920000
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SPL_BSS_START_ADDR	0x910000
+#define CONFIG_SPL_BSS_MAX_SIZE		SZ_8K	/* 8 kiB */
+#define CONFIG_SYS_SPL_MALLOC_START	0x42200000
+#define CONFIG_SYS_SPL_MALLOC_SIZE	SZ_16M	/* 16 MiB */
+
+#define CONFIG_MALLOC_F_ADDR		0x930000
+
+/* For RAW image gives a error info not panic */
+#define CONFIG_SPL_ABORT_ON_RAW_IMAGE
+
+#endif
+
+/* Link Definitions */
+#define CONFIG_SYS_INIT_RAM_ADDR	0x40000000
+#define CONFIG_SYS_INIT_RAM_SIZE	0x200000
+#define CONFIG_SYS_INIT_SP_OFFSET \
+	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+#define CONFIG_SYS_SDRAM_BASE		0x40000000
+#define PHYS_SDRAM			0x40000000
+#define PHYS_SDRAM_SIZE			0x40000000 /* Minimum 1 GiB DDR */
+
+#define CONFIG_MXC_UART_BASE		UART3_BASE_ADDR
+
+/* Monitor Command Prompt */
+#define CONFIG_SYS_CBSIZE		2048
+#define CONFIG_SYS_MAXARGS		64
+#define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_PBSIZE		\
+	(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
+
+/* PHY needs a longer autonegotiation timeout after reset */
+#define PHY_ANEG_TIMEOUT		20000
+#define FEC_QUIRK_ENET_MAC
+
+/* USDHC */
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_SYS_FSL_ESDHC_ADDR	0
+
+#if !defined(CONFIG_SPL_BUILD)
+
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	"altbootcmd=setenv devpart 2 && run bootcmd ; reset\0"		\
+	"bootlimit=3\0"							\
+	"devtype=mmc\0"							\
+	"devpart=1\0"							\
+	/* Give slow devices beyond USB HUB chance to come up. */	\
+	"usb_pgood_delay=2000\0"					\
+	"dfu_alt_info="							\
+		/* RAM block at DRAM offset 256..768 MiB */		\
+		"ram ram0=ram ram 0x50000000 0x20000000&"		\
+		/* 16 MiB SPI NOR */					\
+		"mtd nor0=sf raw 0x0 0x1000000\0"			\
+	"dmo_preboot="							\
+		"sf probe ; " /* Scan for SPI NOR, needed by DFU */	\
+		"run dmo_usb_start_hub ; "				\
+		/* Attempt to start USB and Network console */		\
+		"run dmo_usb_cdc_acm_start ; "				\
+		"run dmo_netconsole_start\0"				\
+	"dmo_update_env="						\
+		"setenv dmo_update_env true ; saveenv ; saveenv\0"	\
+	"dmo_usb_cdc_acm_start="					\
+		"if test \"${dmo_usb_cdc_acm_enabled}\" = \"true\" ; then "\
+			/* Ungate IMX8MM_CLK_USB1_CTRL_ROOT */		\
+			"mw 0x303844d0 3 ; "				\
+			/* Read USBNC_n_PHY_STATUS BIT(4) VBUS_VLD */	\
+			"setexpr.l usbnc_n_phy_status *0x32e4023c \\\\& 0x8 ; "	\
+			/* If USB OTG has valid VBUS, enable CDC ACM */	\
+			"if test \"${usbnc_n_phy_status}\" -eq 8 ; then "\
+				"usb start && "				\
+				"setenv stderr ${stderr},usbacm && "	\
+				"setenv stdout ${stdout},usbacm && "	\
+				"setenv stdin ${stdin},usbacm ; "	\
+			"fi ; "						\
+		"fi\0"							\
+	"dmo_usb_start_hub="						\
+		"i2c dev 1 ; "						\
+		/* Reset the USB USB */					\
+		"gpio clear GPIO5_2 ; sleep 0.01 ; " /* t1 > 1us */	\
+		"gpio set GPIO5_2 ; sleep 0.01 ; " /* t5 > 3us */	\
+		/* Write chunks of descriptor into the USB HUB */	\
+		"mw.l 0x7e1000 0x14042417 ; mw.l 0x7e1004 0x9b0bb325 ; "\
+		"mw.l 0x7e1008 0x00000220 ; mw.l 0x7e100c 0x01320100 ; "\
+		"mw.l 0x7e1010 0x00003232 ; mw.l 0x7e1014 0x4d000909 ; "\
+		"i2c write 0x7e1000 0x2c 0x00 0x18 -s ; "		\
+		"mw.l 0x7e1000 0x6300690f ; mw.l 0x7e1004 0x6f007200 ; "\
+		"mw.l 0x7e1008 0x68006300 ; mw.l 0x7e100c 0x70006900 ; "\
+		"i2c write 0x7e1000 0x2c 0x18 0x10 -s ; "		\
+		"mw.l 0x7e1000 0x53005511 ; mw.l 0x7e1004 0x32004200 ; "\
+		"mw.l 0x7e1008 0x31003500 ; mw.l 0x7e100c 0x42003400 ; "\
+		"mw.l 0x7e1010 0x00006900 ; "				\
+		"i2c write 0x7e1000 0x2c 0x54 0x12 -s ; "		\
+		"mw.l 0x7e1000 0x00000101 ; "				\
+		"i2c write 0x7e1000 0x2c 0xff 0x2 -s\0"			\
+	"dmo_netconsole_start="						\
+		"if test \"${dmo_netconsole_enabled}\" = \"true\" ; then "\
+			"setenv autoload false && "			\
+			"dhcp && "					\
+			"setenv autoload && "				\
+			"setenv ncip ${serverip} && "			\
+			"setenv stderr ${stderr},nc && "		\
+			"setenv stdout ${stdout},nc && "		\
+			"setenv stdin ${stdin},nc ; "			\
+		"fi"
+
+#endif
+
+#endif
