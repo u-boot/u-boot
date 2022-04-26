u-boot/u-boot

commit 970bf8603b877e2b66170290f751f9c23c120838

Convert CONFIG_USE_BOOTCOMMAND et al to Kconfig
This converts the following to Kconfig:
   CONFIG_USE_BOOTCOMMAND
   CONFIG_BOOTCOMMAND
   CONFIG_RAMBOOTCOMMAND
   CONFIG_NFSBOOTCOMMAND

Signed-off-by: Tom Rini <trini@konsulko.com>

include/configs/at91-sama5_common.h
@@ -21,31 +21,12 @@
 #define CONFIG_BOOTP_BOOTFILESIZE
 
 #ifdef CONFIG_SD_BOOT
-#define CONFIG_BOOTCOMMAND	"if test ! -n ${dtb_name}; then "	\
-				    "setenv dtb_name at91-${board_name}.dtb; " \
-				"fi; "					\
-				"fatload mmc 0:1 0x21000000 ${dtb_name}; " \
-				"fatload mmc 0:1 0x22000000 zImage; "	\
-				"bootz 0x22000000 - 0x21000000"
-
 #else
 
 #ifdef CONFIG_NAND_BOOT
 /* u-boot env in nand flash */
-#define CONFIG_BOOTCOMMAND		"nand read 0x21000000 0x180000 0x80000;"	\
-					"nand read 0x22000000 0x200000 0x600000;"	\
-					"bootz 0x22000000 - 0x21000000"
 #elif CONFIG_SPI_BOOT
 /* u-boot env in serial flash, by default is bus 0 and cs 0 */
-#define CONFIG_BOOTCOMMAND		"sf probe 0; "				\
-					"sf read 0x21000000 0x60000 0xc000; "	\
-					"sf read 0x22000000 0x6c000 0x394000; "	\
-					"bootz 0x22000000 - 0x21000000"
-#elif CONFIG_QSPI_BOOT
-#define CONFIG_BOOTCOMMAND		"sf probe 0; "					\
-					"sf read 0x21000000 0x180000 0x80000; "		\
-					"sf read 0x22000000 0x200000 0x600000; "	\
-					"bootz 0x22000000 - 0x21000000"
 #endif
 
 #endif

include/configs/at91sam9m10g45ek.h
@@ -64,16 +64,8 @@
 
 #ifdef CONFIG_NAND_BOOT
 /* bootstrap + u-boot + env in nandflash */
-
-#define CONFIG_BOOTCOMMAND						\
-	"nand read 0x70000000 0x200000 0x300000;"			\
-	"bootm 0x70000000"
 #elif CONFIG_SD_BOOT
 /* bootstrap + u-boot + env + linux in mmc */
-
-#define CONFIG_BOOTCOMMAND	"fatload mmc 0:1 0x71000000 dtb; " \
-				"fatload mmc 0:1 0x72000000 zImage; " \
-				"bootz 0x72000000 - 0x71000000"
 #endif
 
 /* Defines for SPL */

include/configs/at91sam9n12ek.h
@@ -71,28 +71,10 @@
 #ifdef CONFIG_SPI_BOOT
 
 /* bootstrap + u-boot + env + linux in dataflash on CS0 */
-#define CONFIG_BOOTCOMMAND						\
-	"setenv bootargs ${console} ${mtdparts} ${bootargs_nand};"	\
-	"sf probe 0; sf read 0x22000000 0x100000 0x300000; "		\
-	"bootm 0x22000000"
 
 #elif defined(CONFIG_NAND_BOOT)
 
 /* bootstrap + u-boot + env + linux in nandflash */
-#define CONFIG_BOOTCOMMAND						\
-	"setenv bootargs ${console} ${mtdparts} ${bootargs_nand};"	\
-	"nand read 0x21000000 0x180000 0x080000;"			\
-	"nand read 0x22000000 0x200000 0x400000;"			\
-	"bootm 0x22000000 - 0x21000000"
-
-#else /* CONFIG_SD_BOOT */
-
-#define CONFIG_BOOTCOMMAND						\
-	"setenv bootargs ${console} ${mtdparts} ${bootargs_mmc};"	\
-	"fatload mmc 0:1 0x21000000 dtb;"				\
-	"fatload mmc 0:1 0x22000000 uImage;"				\
-	"bootm 0x22000000 - 0x21000000"
-
 #endif
 
 /* SPL */

include/configs/sam9x60ek.h
@@ -50,24 +50,12 @@
 
 #ifdef CONFIG_SD_BOOT
 /* bootstrap + u-boot + env + linux in sd card */
-#define CONFIG_BOOTCOMMAND  \
-			"fatload mmc 0:1 0x21000000 at91-sam9x60ek.dtb;" \
-			"fatload mmc 0:1 0x22000000 zImage;" \
-			"bootz 0x22000000 - 0x21000000"
 
 #elif defined(CONFIG_NAND_BOOT)
 /* bootstrap + u-boot + env + linux in nandflash */
-#define CONFIG_BOOTCOMMAND	"nand read " \
-				"0x22000000 0x200000 0x600000; " \
-				"nand read 0x21000000 0x180000 0x20000; " \
-				"bootz 0x22000000 - 0x21000000"
 
 #elif defined(CONFIG_QSPI_BOOT)
 /* bootstrap + u-boot + env + linux in SPI NOR flash */
-#define CONFIG_BOOTCOMMAND	"sf probe 0; "					\
-				"sf read 0x21000000 0x180000 0x80000; "		\
-				"sf read 0x22000000 0x200000 0x600000; "	\
-				"bootz 0x22000000 - 0x21000000"
 #endif
 
 #endif

include/configs/sama5d27_som1_ek.h
@@ -21,13 +21,8 @@
 	(0x22000000 + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
 #endif
 
-#undef CONFIG_BOOTCOMMAND
 #ifdef CONFIG_SD_BOOT
 /* bootstrap + u-boot + env in sd card */
-#define CONFIG_BOOTCOMMAND	"fatload mmc " CONFIG_ENV_FAT_DEVICE_AND_PART " 0x22000000 " \
-				CONFIG_DEFAULT_DEVICE_TREE ".dtb; " \
-				"fatload mmc " CONFIG_ENV_FAT_DEVICE_AND_PART " 0x23000000 zImage; " \
-				"bootz 0x23000000 - 0x22000000"
 #endif
 
 /* SPL */

include/configs/sama5d2_xplained.h
@@ -23,31 +23,16 @@
 #ifdef CONFIG_SD_BOOT
 
 /* bootstrap + u-boot + env in sd card */
-#undef CONFIG_BOOTCOMMAND
-
-#define CONFIG_BOOTCOMMAND	"fatload mmc " CONFIG_ENV_FAT_DEVICE_AND_PART " 0x22000000 at91-sama5d2_xplained.dtb; " \
-				"fatload mmc " CONFIG_ENV_FAT_DEVICE_AND_PART " 0x23000000 zImage; " \
-				"bootz 0x23000000 - 0x22000000"
 
 #elif CONFIG_SPI_BOOT
 
 /* bootstrap + u-boot + env in sd card, but kernel + dtb in eMMC */
-#undef CONFIG_BOOTCOMMAND
-
-#define CONFIG_BOOTCOMMAND	"ext4load mmc 0:1 0x22000000 /boot/at91-sama5d2_xplained.dtb; " \
-				"ext4load mmc 0:1 0x23000000 /boot/zImage; " \
-				"bootz 0x23000000 - 0x22000000"
 
 #endif
 
 #ifdef CONFIG_QSPI_BOOT
 #undef CONFIG_ENV_SPI_BUS
-#undef CONFIG_BOOTCOMMAND
 #define CONFIG_ENV_SPI_BUS	1
-#define CONFIG_BOOTCOMMAND	"sf probe 1:0; "			\
-				"sf read 0x22000000 0x180000 0x80000; "	\
-				"sf read 0x23000000 0x200000 0x600000; "\
-				"bootz 0x23000000 - 0x22000000"
 
 #endif
 

commit 66692917003cd77ec0271b5c197aded70d1ed14b

include: configs: at91/sam: remove stray #ifdef/#else
With the commit that moves the BOOTCOMMAND to Kconfig:

970bf8603b ("Convert CONFIG_USE_BOOTCOMMAND et al to Kconfig")

some stray ifdefs have been left in the header files which
are now useless.
Clean up the include files to remove these lines.

Fixes: 970bf8603b ("Convert CONFIG_USE_BOOTCOMMAND et al to Kconfig")
Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

include/configs/at91-sama5_common.h
@@ -15,15 +15,4 @@
 #define CONFIG_SYS_AT91_SLOW_CLOCK      32768
 #define CONFIG_SYS_AT91_MAIN_CLOCK      12000000 /* from 12 MHz crystal */
 
-#ifdef CONFIG_SD_BOOT
-#else
-
-#ifdef CONFIG_NAND_BOOT
-/* u-boot env in nand flash */
-#elif CONFIG_SPI_BOOT
-/* u-boot env in serial flash, by default is bus 0 and cs 0 */
-#endif
-
-#endif
-
 #endif

include/configs/at91sam9m10g45ek.h
@@ -40,12 +40,6 @@
 
 #endif
 
-#ifdef CONFIG_NAND_BOOT
-/* bootstrap + u-boot + env in nandflash */
-#elif CONFIG_SD_BOOT
-/* bootstrap + u-boot + env + linux in mmc */
-#endif
-
 /* Defines for SPL */
 #define CONFIG_SPL_MAX_SIZE		0x010000
 #define CONFIG_SPL_STACK		0x310000

include/configs/at91sam9n12ek.h
@@ -59,15 +59,6 @@
 #define CONFIG_SYS_USB_OHCI_MAX_ROOT_PORTS	1
 #endif
 
-#ifdef CONFIG_SPI_BOOT
-
-/* bootstrap + u-boot + env + linux in dataflash on CS0 */
-
-#elif defined(CONFIG_NAND_BOOT)
-
-/* bootstrap + u-boot + env + linux in nandflash */
-#endif
-
 /* SPL */
 #define CONFIG_SPL_MAX_SIZE		0x6000
 #define CONFIG_SPL_STACK		0x308000

include/configs/sam9x60ek.h
@@ -40,14 +40,4 @@
 #define CONFIG_SYS_NAND_READY_PIN	AT91_PIN_PD5
 #endif
 
-#ifdef CONFIG_SD_BOOT
-/* bootstrap + u-boot + env + linux in sd card */
-
-#elif defined(CONFIG_NAND_BOOT)
-/* bootstrap + u-boot + env + linux in nandflash */
-
-#elif defined(CONFIG_QSPI_BOOT)
-/* bootstrap + u-boot + env + linux in SPI NOR flash */
-#endif
-
 #endif

include/configs/sama5d27_som1_ek.h
@@ -21,10 +21,6 @@
 	(0x22000000 + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
 #endif
 
-#ifdef CONFIG_SD_BOOT
-/* bootstrap + u-boot + env in sd card */
-#endif
-
 /* SPL */
 #define CONFIG_SPL_MAX_SIZE		0x10000
 #define CONFIG_SPL_BSS_START_ADDR	0x20000000

include/configs/sama5d2_xplained.h
@@ -18,18 +18,6 @@
 	(0x22000000 + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
 #endif
 
-/* SerialFlash */
-
-#ifdef CONFIG_SD_BOOT
-
-/* bootstrap + u-boot + env in sd card */
-
-#elif CONFIG_SPI_BOOT
-
-/* bootstrap + u-boot + env in sd card, but kernel + dtb in eMMC */
-
-#endif
-
 /* SPL */
 #define CONFIG_SPL_MAX_SIZE		0x10000
 #define CONFIG_SPL_BSS_START_ADDR	0x20000000