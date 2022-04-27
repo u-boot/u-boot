ccf24a9d7739241c08e17b45d7fbe74b9baa3dcc
Simon Glass <sjg@chromium.org>
2022-04-27 13:47:57 -0600

    sandbox: Avoid binman error when run without device tree
    With sandbox, U-Boot can be run without a device tree (i.e. no -d or -T
    parameter). In this case an empty device tree is created for convenience.
    With a recent change this causes an error due to the missing '/binman'
    node.
    
    Add this node to avoid the problem, as well as a test that U-Boot can
    be run without a device tree.
    
    Fixes: 059df5624b ("arch: Kconfig: imply BINMAN for SANDBOX")
    Fixes: https://source.denx.de/u-boot/u-boot/-/issues/11
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/py/tests/test_help.py
+++ test/py/tests/test_help.py
@@ -9,6 +9,18 @@ def test_help(u_boot_console):
 
     u_boot_console.run_command('help')
 
+@pytest.mark.boardspec('sandbox')
+def test_help_no_devicetree(u_boot_console):
+    try:
+        cons = u_boot_console
+        cons.restart_uboot_with_flags([], use_dtb=False)
+        cons.run_command('help')
+        output = cons.get_spawn_output().replace('\r', '')
+        assert 'print command description/usage' in output
+    finally:
+        # Restart afterward to get the normal device tree back
+        u_boot_console.restart_uboot()
+
 @pytest.mark.boardspec('sandbox_vpl')
 def test_vpl_help(u_boot_console):
     try:
