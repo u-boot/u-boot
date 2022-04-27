39bbcc341bda435899cb623a19d20727ca98c67a
Tien Fong Chee <tien.fong.chee@intel.com>
2022-04-27 12:52:42 +0800

    ddr: altera: Stratix10: Use phys_size_t for memory size
    Replace with phys_size_t for all memory size variables declaration
    for the sake of scalability. phys_size_t is defined in
    /arch/arm/include/asm/types.h.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

--- drivers/ddr/altera/sdram_s10.c
+++ drivers/ddr/altera/sdram_s10.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2016-2018 Intel Corporation <www.intel.com>
+ * Copyright (C) 2016-2022 Intel Corporation <www.intel.com>
  *
  */
 
@@ -277,7 +277,7 @@ int sdram_mmr_init_full(struct udevice *dev)
 			DDR_SCH_DEVTODEV);
 
 	/* assigning the SDRAM size */
-	unsigned long long size = sdram_calculate_size(plat);
+	phys_size_t size = sdram_calculate_size(plat);
 	/* If the size is invalid, use default Config size */
 	if (size <= 0)
 		hw_size = PHYS_SDRAM_1_SIZE;
