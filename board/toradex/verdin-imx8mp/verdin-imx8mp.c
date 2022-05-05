a6924c95d1c0079d70ed4c9b10f69cef2aa6ceaf
Peng Fan <peng.fan@nxp.com>
2022-05-05 19:06:11 +0800

    imx: verdin-imx8mm/p: cleanup board watchdog code
    pinctrl_wdog already marked u-boot,dm-spl, so clean up board code.
    
    The set_wdog_reset() function is not necessary as this is handled by
    the imx_watchdog.c driver due to the 'fsl,ext-reset-output' property
    being set.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/toradex/verdin-imx8mp/verdin-imx8mp.c
+++ board/toradex/verdin-imx8mp/verdin-imx8mp.c
@@ -24,7 +24,6 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 #define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
-#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
 
 /* Verdin UART_3, Console/Debug UART */
 static const iomux_v3_cfg_t uart_pads[] = {
@@ -32,18 +31,8 @@ static const iomux_v3_cfg_t uart_pads[] = {
 	MX8MP_PAD_UART3_TXD__UART3_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
 
-static const iomux_v3_cfg_t wdog_pads[] = {
-	MX8MP_PAD_GPIO1_IO02__WDOG1_WDOG_B | MUX_PAD_CTRL(WDOG_PAD_CTRL),
-};
-
 int board_early_init_f(void)
 {
-	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
-
-	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
-
-	set_wdog_reset(wdog);
-
 	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
 
 	init_uart_clk(2);
