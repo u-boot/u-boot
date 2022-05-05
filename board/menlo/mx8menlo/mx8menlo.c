0726bdd4608459d658ab600308086e1d50b77286
Peng Fan <peng.fan@nxp.com>
2022-05-05 19:06:03 +0800

    imx: imx8mm-mx8menlo: drop unneeded watchdog settings
    pinctrl_wdog as u-boot,dm-spl already marked in imx8mm-verdin-u-boot.dtsi,
    
    The set_wdog_reset() function is not necessary as this is handled by
    the imx_watchdog.c driver due to the 'fsl,ext-reset-output' property
    being set.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Marek Vasut <marex@denx.de>

--- board/menlo/mx8menlo/mx8menlo.c
+++ board/menlo/mx8menlo/mx8menlo.c
@@ -12,12 +12,6 @@
 #include <asm/mach-imx/iomux-v3.h>
 #include <spl.h>
 
-#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
-
-static iomux_v3_cfg_t const wdog_pads[] = {
-	IMX8MM_PAD_GPIO1_IO02_WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
-};
-
 #define SNVS_BASE_ADDR		0x30370000
 #define SNVS_LPSR		0x4c
 #define SNVS_LPLVDR		0x64
@@ -35,12 +29,6 @@ static void setup_snvs(void)
 
 void board_early_init(void)
 {
-	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
-
-	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
-
-	set_wdog_reset(wdog);
-
 	init_uart_clk(1);
 
 	setup_snvs();
