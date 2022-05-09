f8209d30512d2e9c994d32cccfceda3a1e065cab
William Zhang <william.zhang@broadcom.com>
2022-05-09 09:28:02 -0700

    arm: bcmbca: introduce the bcmbca architecture and 47622 SOC
    This is the initial support for Broadcom's ARM-based 47622 SOC.
    
    In this change, our first SOC is an armv7 platform called 47622. The
    initial support includes a bare-bone implementation and dts with ARM
    PL011 uart.
    
    The SOC-specific code resides in arch/arm/mach-bcmbca/<soc> and board
    related code is in board/broadcom/bcmba.
    
    The u-boot image can be loaded from flash or network to the entry
    point address in the memory and boot from there.
    
    Signed-off-by: William Zhang <william.zhang@broadcom.com>
    Signed-off-by: Kursad Oney <kursad.oney@broadcom.com>
    Signed-off-by: Anand Gore <anand.gore@broadcom.com>
    Reviewed-by: Philippe Reynes <philippe.reynes@softathome.com>

---
+++ include/configs/bcm947622.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2022 Broadcom Ltd.
+ */
+
+#ifndef __BCM947622_H
+#define __BCM947622_H
+
+#define CONFIG_SYS_BOOTM_LEN		(32 * 1024 * 1024)
+
+#define CONFIG_SYS_SDRAM_BASE		0x00000000
+
+#define COUNTER_FREQUENCY		50000000
+#endif
