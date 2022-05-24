974a8df476e7b42c42e996b5dda9d9ace627481a
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2022-05-24 13:56:40 +0800

    pinctrl: aspeed: FWSPICS1 and SPI1CS1 pin support
    Add FWSPICS1 and SPI1CS1 in AST2500 pinctrl group.
    On AST2500 EVB, FWSPICS1 can be supported by default.
    An extra jumper, J45, should be configured before
    enabling SPI1CS1.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Link: https://lore.kernel.org/r/20220524055650.1115899-3-chin-ting_kuo@aspeedtech.com
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/pinctrl/aspeed/pinctrl_ast2500.c
+++ drivers/pinctrl/aspeed/pinctrl_ast2500.c
@@ -61,6 +61,8 @@ static const struct ast2500_group_config ast2500_groups[] = {
 	{ "MDIO2", 5, (1 << 2) },
 	{ "SD1", 5, (1 << 0) },
 	{ "SD2", 5, (1 << 1) },
+	{ "FWSPICS1", 3, (1 << 24) },
+	{ "SPI1CS1", 1, (1 << 15) },
 };
 
 static int ast2500_pinctrl_get_groups_count(struct udevice *dev)
