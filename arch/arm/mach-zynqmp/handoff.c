254f0c766d944ef5dbb2053340feb328f5e061aa
Michal Simek <michal.simek@amd.com>
2022-06-06 09:37:09 +0200

    arm64: zynqmp: Add debug messages to bl2_plat_get_bl31_params()
    It is useful to get information about BL type and entry address that's why
    add some debug messages.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Link: https://lore.kernel.org/r/fb023b618a009009a0b564c24223cadc10ced5b3.1652871741.git.michal.simek@amd.com

--- arch/arm/mach-zynqmp/handoff.c
+++ arch/arm/mach-zynqmp/handoff.c
@@ -79,7 +79,10 @@ struct bl31_params *bl2_plat_get_bl31_params(uintptr_t bl32_entry,
 	atfhandoffparams->magic[2] = 'N';
 	atfhandoffparams->magic[3] = 'X';
 
+	debug("Creating handoff:\n");
+
 	if (bl32_entry) {
+		debug(" to BL32 at 0x%x EL-1, Secure\n", (u32)bl32_entry);
 		atfhandoffparams->partition[index].entry_point = bl32_entry;
 		atfhandoffparams->partition[index].flags = FSBL_FLAGS_EL1 << FSBL_FLAGS_EL_SHIFT |
 							   FSBL_FLAGS_SECURE << FSBL_FLAGS_TZ_SHIFT;
@@ -87,6 +90,7 @@ struct bl31_params *bl2_plat_get_bl31_params(uintptr_t bl32_entry,
 	}
 
 	if (bl33_entry) {
+		debug(" to BL33 at 0x%x EL-2\n", (u32)bl33_entry);
 		atfhandoffparams->partition[index].entry_point = bl33_entry;
 		atfhandoffparams->partition[index].flags = FSBL_FLAGS_EL2 <<
 							   FSBL_FLAGS_EL_SHIFT;
