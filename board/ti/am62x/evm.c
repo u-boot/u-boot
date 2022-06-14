4c092bb306b0443a7386fef8d7c20cf3aa52f54c
Georgi Vlaev <g-vlaev@ti.com>
2022-06-14 17:45:33 +0300

    board: ti: am62x: Account for DDR size fixups if ECC is enabled
    Call into k3-ddrss driver to fixup device tree and resize
    the available amount of DDR if ECC is enabled.
    
    A second fixup is required from A53 SPL to take the fixup
    as done from R5 SPL and apply it to DT passed to A53 U-boot,
    which in turn passes this to the OS.
    
    Signed-off-by: Georgi Vlaev <g-vlaev@ti.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- board/ti/am62x/evm.c
+++ board/ti/am62x/evm.c
@@ -9,6 +9,8 @@
 
 #include <asm/io.h>
 #include <spl.h>
+#include <dm/uclass.h>
+#include <k3-ddrss.h>
 #include <fdt_support.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/sys_proto.h>
@@ -30,3 +32,54 @@ int dram_init_banksize(void)
 {
 	return fdtdec_setup_memory_banksize();
 }
+
+#if defined(CONFIG_SPL_BUILD)
+#if defined(CONFIG_K3_AM64_DDRSS)
+static void fixup_ddr_driver_for_ecc(struct spl_image_info *spl_image)
+{
+	struct udevice *dev;
+	int ret;
+
+	dram_init_banksize();
+
+	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
+	if (ret)
+		panic("Cannot get RAM device for ddr size fixup: %d\n", ret);
+
+	ret = k3_ddrss_ddr_fdt_fixup(dev, spl_image->fdt_addr, gd->bd);
+	if (ret)
+		printf("Error fixing up ddr node for ECC use! %d\n", ret);
+}
+#else
+static void fixup_memory_node(struct spl_image_info *spl_image)
+{
+	u64 start[CONFIG_NR_DRAM_BANKS];
+	u64 size[CONFIG_NR_DRAM_BANKS];
+	int bank;
+	int ret;
+
+	dram_init();
+	dram_init_banksize();
+
+	for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
+		start[bank] =  gd->bd->bi_dram[bank].start;
+		size[bank] = gd->bd->bi_dram[bank].size;
+	}
+
+	/* dram_init functions use SPL fdt, and we must fixup u-boot fdt */
+	ret = fdt_fixup_memory_banks(spl_image->fdt_addr, start, size,
+				     CONFIG_NR_DRAM_BANKS);
+	if (ret)
+		printf("Error fixing up memory node! %d\n", ret);
+}
+#endif
+
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+#if defined(CONFIG_K3_AM64_DDRSS)
+	fixup_ddr_driver_for_ecc(spl_image);
+#else
+	fixup_memory_node(spl_image);
+#endif
+}
+#endif
