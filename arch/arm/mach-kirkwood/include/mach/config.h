432243cee100e1d8f60399f0ae8f76cf7c83975f
Tom Rini <trini@konsulko.com>
2022-06-20 08:07:58 -0400

    Convert CONFIG_KIRKWOOD_PCIE_INIT et al to Kconfig
    This converts the following to Kconfig:
       CONFIG_KIRKWOOD_EGIGA_INIT
       CONFIG_KIRKWOOD_PCIE_INIT
       CONFIG_KIRKWOOD_RGMII_PAD_1V8
       CONFIG_KM_DISABLE_PCIE
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- arch/arm/mach-kirkwood/include/mach/config.h
+++ arch/arm/mach-kirkwood/include/mach/config.h
@@ -23,9 +23,6 @@
 #endif /* CONFIG_KW88F6281 */
 
 #include <asm/arch/soc.h>
-#define CONFIG_KIRKWOOD_EGIGA_INIT	/* Enable GbePort0/1 for kernel */
-#define CONFIG_KIRKWOOD_RGMII_PAD_1V8	/* Set RGMII Pad voltage to 1.8V */
-#define CONFIG_KIRKWOOD_PCIE_INIT       /* Enable PCIE Port0 for kernel */
 
 #define CONFIG_I2C_MVTWSI_BASE0	KW_TWSI_BASE
 #define MV_UART_CONSOLE_BASE	KW_UART0_BASE
