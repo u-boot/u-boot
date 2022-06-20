f49062330967a549d2967242c36a2fe984b16b4e
Jaehoon Chung <jh80.chung@samsung.com>
2022-06-20 20:13:54 +0900

    dfu: add CONFIG_DFU_NAME_MAX_SIZE configuration
    Add CONFIG_DFU_NAME_MAX_SIZE to change the proper size.
    If name is longer than default size, it can do wrong behavior during updating
    image. So it need to change the proper maximum size.
    
    This patch is proviced the solution to change value with configuration.
    
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Reviewed-by: Lukasz Majewski <lukma@denx.de>
    Link: https://lore.kernel.org/r/20220620111354.448512-1-jh80.chung@samsung.com
    [mkorpershoek: fixed build errors for dfu.h includes]
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- include/dfu.h
+++ include/dfu.h
@@ -98,7 +98,12 @@ struct virt_internal_data {
 	int dev_num;
 };
 
+
+#if defined(CONFIG_DFU_NAME_MAX_SIZE)
+#define DFU_NAME_SIZE			CONFIG_DFU_NAME_MAX_SIZE
+#else
 #define DFU_NAME_SIZE			32
+#endif
 #ifndef DFU_DEFAULT_POLL_TIMEOUT
 #define DFU_DEFAULT_POLL_TIMEOUT 0
 #endif
