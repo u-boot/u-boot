2b7c045aff2c3e3ac71a0e03552b22d5623a0551
Sughosh Ganu <sughosh.ganu@linaro.org>
2022-06-23 12:18:28 +0530

    FWU: STM32MP1: Add support to read boot index from backup register
    The FWU Multi Bank Update feature allows the platform to boot the
    firmware images from one of the partitions(banks). The first stage
    bootloader(fsbl) passes the value of the boot index, i.e. the bank
    from which the firmware images were booted from to U-Boot. On the
    STM32MP157C-DK2 board, this value is passed through one of the SoC's
    backup register. Add a function to read the boot index value from the
    backup register.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
    Reviewed-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Acked-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- board/st/stm32mp1/stm32mp1.c
+++ board/st/stm32mp1/stm32mp1.c
@@ -966,3 +966,26 @@ static void board_copro_image_process(ulong fw_image, size_t fw_size)
 }
 
 U_BOOT_FIT_LOADABLE_HANDLER(IH_TYPE_COPRO, board_copro_image_process);
+
+#if defined(CONFIG_FWU_MULTI_BANK_UPDATE)
+
+#include <fwu.h>
+
+/**
+ * fwu_plat_get_bootidx() - Get the value of the boot index
+ * @boot_idx: Boot index value
+ *
+ * Get the value of the bank(partition) from which the platform
+ * has booted. This value is passed to U-Boot from the earlier
+ * stage bootloader which loads and boots all the relevant
+ * firmware images
+ *
+ */
+void fwu_plat_get_bootidx(void *boot_idx)
+{
+	u32 *bootidx = boot_idx;
+
+	*bootidx = (readl(TAMP_FWU_BOOT_INFO_REG) >>
+		    TAMP_FWU_BOOT_IDX_OFFSET) & TAMP_FWU_BOOT_IDX_MASK;
+}
+#endif /* CONFIG_FWU_MULTI_BANK_UPDATE */
