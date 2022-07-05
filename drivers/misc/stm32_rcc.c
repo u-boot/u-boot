43872790d8eeb2055aebc13b09fba3dbb2f8dcaf
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-07-05 16:55:55 +0200

    clk: stm32: add support compatible st, stm32mp1-rcc-secure
    Add support for new compatible st,stm32mp1-rcc-secure used when the
    RCC resource is managed by secured world (RCC_TZCR.TZEN=1)
    iand when SCMI is used.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- drivers/misc/stm32_rcc.c
+++ drivers/misc/stm32_rcc.c
@@ -84,6 +84,7 @@ static const struct udevice_id stm32_rcc_ids[] = {
 	{.compatible = "st,stm32f746-rcc", .data = (ulong)&stm32_rcc_clk_f7 },
 	{.compatible = "st,stm32h743-rcc", .data = (ulong)&stm32_rcc_clk_h7 },
 	{.compatible = "st,stm32mp1-rcc", .data = (ulong)&stm32_rcc_clk_mp1 },
+	{.compatible = "st,stm32mp1-rcc-secure", .data = (ulong)&stm32_rcc_clk_mp1 },
 	{.compatible = "st,stm32mp13-rcc", .data = (ulong)&stm32_rcc_clk_mp13 },
 	{ }
 };
