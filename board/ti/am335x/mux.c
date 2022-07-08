21acb843db24ec8fc743bf78b8e253fc69c20e27
Paul Barker <paul.barker@sancloud.com>
2022-07-08 10:25:45 +0100

    board: ti: am335x: Enable spi0 bus on SanCloud BBE Lite
    The SanCloud BBE Lite has a Micron Authenta flash device connected to
    the spi0 bus.
    
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- board/ti/am335x/mux.c
+++ board/ti/am335x/mux.c
@@ -401,8 +401,13 @@ void enable_board_pin_mux(void)
 		configure_module_pin_mux(mmc0_pin_mux_sk_evm);
 	} else if (board_is_bone_lt()) {
 		if (board_is_bben()) {
+			char subtype_id = board_ti_get_config()[1];
+
 			/* SanCloud Beaglebone LT Enhanced pinmux */
 			configure_module_pin_mux(rgmii1_pin_mux);
+
+			if (subtype_id == 'L')
+				configure_module_pin_mux(spi0_pin_mux);
 		} else {
 			/* Beaglebone LT pinmux */
 			configure_module_pin_mux(mii1_pin_mux);
