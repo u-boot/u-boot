331b4cecc2cf5a21ce8de687dddf7d52293be202
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-07-27 10:38:11 +0200

    board: st: stm32mp2: add user button support
    Handle user button 2 to force boot with STM32CubeProgrammer.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- board/st/stm32mp2/stm32mp2.c
+++ board/st/stm32mp2/stm32mp2.c
@@ -5,6 +5,7 @@
 
 #define LOG_CATEGORY LOGC_BOARD
 
+#include <button.h>
 #include <config.h>
 #include <env_internal.h>
 #include <fdt_support.h>
@@ -13,10 +14,12 @@
 #include <misc.h>
 #include <mmc.h>
 #include <asm/global_data.h>
+#include <asm/io.h>
 #include <asm/arch/sys_proto.h>
 #include <dm/device.h>
 #include <dm/ofnode.h>
 #include <dm/uclass.h>
+#include <linux/delay.h>
 
 /*
  * Get a global data pointer
@@ -90,10 +93,33 @@ static int setup_led(enum led_state_t cmd)
 	return ret;
 }
 
+static void check_user_button(void)
+{
+	struct udevice *button;
+	int i;
+
+	if (!IS_ENABLED(CONFIG_CMD_STM32PROG) || !IS_ENABLED(CONFIG_BUTTON))
+		return;
+
+	if (button_get_by_label("User-2", &button))
+		return;
+
+	for (i = 0; i < 21; ++i) {
+		if (button_get_state(button) != BUTTON_ON)
+			return;
+		if (i < 20)
+			mdelay(50);
+	}
+
+	log_notice("entering download mode...\n");
+	clrsetbits_le32(TAMP_BOOT_CONTEXT, TAMP_BOOT_FORCED_MASK, BOOT_STM32PROG);
+}
+
 /* board dependent setup after realloc */
 int board_init(void)
 {
 	setup_led(LEDST_ON);
+	check_user_button();
 
 	return 0;
 }
