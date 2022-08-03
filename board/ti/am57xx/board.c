0b161f8e89a411fe54059cf1d0ba61354ee1e8a2
Robert Nelson <robertcnelson@gmail.com>
2022-08-03 11:28:10 -0500

    am57xx/board.c: assume on failure its a BBONE-AI
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- board/ti/am57xx/board.c
+++ board/ti/am57xx/board.c
@@ -529,8 +529,10 @@ void do_board_detect(void)
 
 	rc = ti_i2c_eeprom_am_get(CONFIG_EEPROM_BUS_ADDRESS,
 				  CONFIG_EEPROM_CHIP_ADDRESS);
-	if (rc)
+	if (rc) {
 		printf("ti_i2c_eeprom_init failed %d\n", rc);
+		ti_i2c_eeprom_am_set("BBONE-AI", "A");
+	};
 
 #ifdef CONFIG_SUPPORT_EMMC_BOOT
 	rc = board_bootmode_has_emmc();
