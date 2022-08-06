21385adf2cdf7d21703e9e90c57e648a16ee0f57
William Zhang <william.zhang@broadcom.com>
2022-08-05 18:25:13 -0700

    arm: bcmbca: add bcm63138 SoC support
    BCM63138 is an ARM A9 based DSL Broadband SoC. It is part of the BCA
    (Broadband Carrier Access origin) chipset family so it's added under
    ARCH_BCMBCA platform. This initial support includes a bare-bone
    implementation and dts with CPU subsystem, memory, ARM A9 global timer
    and Broadcom uart.
    
    This SoC is supported in the linux-next git repository so the dts and
    dtsi files are stripped down version of linux copies with mininum blocks
    needed by u-boot.
    
    The u-boot image can be loaded from flash or network to the entry point
    address in the memory and boot from there to the console.
    
    This patch applies on top of the my previous patch [1].
    
    [1] https://lists.denx.de/pipermail/u-boot/2022-August/490570.html
    
    Signed-off-by: William Zhang <william.zhang@broadcom.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Philippe Reynes <philippe.reynes@softathome.com>

---
+++ include/configs/bcm963138.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2022 Broadcom Ltd.
+ */
+
+#ifndef __BCM963138_H
+#define __BCM963138_H
+
+#define CONFIG_SYS_SDRAM_BASE		0x00000000
+#define CONFIG_SYS_HZ_CLOCK		500000000
+
+#endif
