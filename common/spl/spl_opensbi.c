aa0eda17cf98448c3ef826204f38c76bf48b3345
Nikita Shubin <n.shubin@yadro.com>
2022-08-08 13:28:52 +0300

    spl: opensbi: convert scratch options to config
    Convert hardcoded "opensbi_info.options" to config provided value, this
    allows changing options passed to OpenSBI.
    
    SPL_OPENSBI_SCRATCH_OPTIONS is defaulted to SBI_SCRATCH_NO_BOOT_PRINTS.
    
    Link: https://github.com/riscv-software-src/opensbi/blob/master/docs/firmware/fw_dynamic.md
    Signed-off-by: Nikita Shubin <n.shubin@yadro.com>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

--- common/spl/spl_opensbi.c
+++ common/spl/spl_opensbi.c
@@ -71,7 +71,7 @@ void spl_invoke_opensbi(struct spl_image_info *spl_image)
 	opensbi_info.version = FW_DYNAMIC_INFO_VERSION;
 	opensbi_info.next_addr = uboot_entry;
 	opensbi_info.next_mode = FW_DYNAMIC_INFO_NEXT_MODE_S;
-	opensbi_info.options = SBI_SCRATCH_NO_BOOT_PRINTS;
+	opensbi_info.options = CONFIG_SPL_OPENSBI_SCRATCH_OPTIONS;
 	opensbi_info.boot_hart = gd->arch.boot_hart;
 
 	opensbi_entry = (void (*)(ulong, ulong, ulong))spl_image->entry_point;
