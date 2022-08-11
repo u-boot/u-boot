349e08ef1db5174ff1574c49e55ecba931554ecc
Pali Rohár <pali@kernel.org>
2022-08-11 14:10:39 +0200

    console: Implement flush() function
    On certain places it is required to flush output print buffers to ensure
    that text strings were sent to console or serial devices. For example when
    printing message that U-Boot is going to boot kernel or when U-Boot is
    going to change baudrate of terminal device.
    
    Therefore introduce a new flush() and fflush() functions into console code.
    These functions will call .flush callback of associated stdio_dev device.
    
    As this function may increase U-Boot side, allow to compile U-Boot without
    this function. For this purpose there is a new config CONSOLE_FLUSH_SUPPORT
    which is enabled by default and can be disabled. It is a good idea to have
    this option enabled for all boards which have enough space for it.
    
    When option is disabled when U-Boot defines just empty static inline
    function fflush() to avoid ifdefs in other code.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    ---
    Changes in v3:
    * Added macro STDIO_DEV_ASSIGN_FLUSH()

--- include/stdio_dev.h
+++ include/stdio_dev.h
@@ -37,6 +37,13 @@ struct stdio_dev {
 	void (*putc)(struct stdio_dev *dev, const char c);
 	/* To put a string (accelerator) */
 	void (*puts)(struct stdio_dev *dev, const char *s);
+#ifdef CONFIG_CONSOLE_FLUSH_SUPPORT
+	/* To flush output queue */
+	void (*flush)(struct stdio_dev *dev);
+#define STDIO_DEV_ASSIGN_FLUSH(dev, flush_func) ((dev)->flush = (flush_func))
+#else
+#define STDIO_DEV_ASSIGN_FLUSH(dev, flush_func)
+#endif
 
 /* INPUT functions */
 
