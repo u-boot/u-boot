f5631b2c84dcbb225ae33532fa039763732363b8
Michael Walle <michael@walle.cc>
2022-08-17 21:38:01 +0200

    board: lsxl: use proper *_r variables
    Use the common kernel_addr_r, ramdisk_addr_r and fdt_addr_r variable
    names.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Reviewed-by: Stefan Roese <sr@denx.de>

--- include/configs/lsxl.h
+++ include/configs/lsxl.h
@@ -25,31 +25,31 @@
 #define CONFIG_EXTRA_ENV_SETTINGS					\
 	"bootsource=legacy\0"						\
 	"hdpart=0:1\0"							\
-	"kernel_addr=0x00800000\0"					\
-	"ramdisk_addr=0x01000000\0"					\
-	"fdt_addr=0x00ff0000\0"						\
+	"kernel_addr_r=0x00800000\0"					\
+	"ramdisk_addr_r=0x01000000\0"					\
+	"fdt_addr_r=0x00ff0000\0"					\
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0"				\
 	"bootcmd_legacy=sata init "					\
-		"&& load sata ${hdpart} ${kernel_addr} /uImage.buffalo "\
-		"&& load sata ${hdpart} ${ramdisk_addr} /initrd.buffalo "\
-		"&& bootm ${kernel_addr} ${ramdisk_addr}\0"		\
-	"bootcmd_net=bootp ${kernel_addr} vmlinuz "			\
-		"&& tftpboot ${fdt_addr} ${fdtfile} "			\
-		"&& tftpboot ${ramdisk_addr} initrd.img "		\
-		"&& bootz ${kernel_addr} "				\
-			"${ramdisk_addr}:${filesize} ${fdt_addr}\0"	\
+		"&& load sata ${hdpart} ${kernel_addr_r} /uImage.buffalo "\
+		"&& load sata ${hdpart} ${ramdisk_addr_r} /initrd.buffalo "\
+		"&& bootm ${kernel_addr_r} ${ramdisk_addr_r}\0"		\
+	"bootcmd_net=bootp ${kernel_addr_r} vmlinuz "			\
+		"&& tftpboot ${fdt_addr_r} ${fdtfile} "			\
+		"&& tftpboot ${ramdisk_addr_r} initrd.img "		\
+		"&& bootz ${kernel_addr_r} "				\
+			"${ramdisk_addr_r}:${filesize} ${fdt_addr_r}\0"	\
 	"bootcmd_hdd=sata init "					\
-		"&& load sata ${hdpart} ${kernel_addr} /vmlinuz "	\
-		"&& load sata ${hdpart} ${fdt_addr} /dtb "		\
-		"&& load sata ${hdpart} ${ramdisk_addr} /initrd.img "	\
-		"&& bootz ${kernel_addr} "				\
-			"${ramdisk_addr}:${filesize} ${fdt_addr}\0"	\
+		"&& load sata ${hdpart} ${kernel_addr_r} /vmlinuz "	\
+		"&& load sata ${hdpart} ${fdt_addr_r} /dtb "		\
+		"&& load sata ${hdpart} ${ramdisk_addr_r} /initrd.img "	\
+		"&& bootz ${kernel_addr_r} "				\
+			"${ramdisk_addr_r}:${filesize} ${fdt_addr_r}\0"	\
 	"bootcmd_usb=usb start "					\
-		"&& load usb 0:1 ${kernel_addr} /vmlinuz "		\
-		"&& load usb 0:1 ${fdt_addr} ${fdtfile} "		\
-		"&& load usb 0:1 ${ramdisk_addr} /initrd.img "		\
-		"&& bootz ${kernel_addr} "				\
-			"${ramdisk_addr}:${filesize} ${fdt_addr}\0"	\
+		"&& load usb 0:1 ${kernel_addr_r} /vmlinuz "		\
+		"&& load usb 0:1 ${fdt_addr_r} ${fdtfile} "		\
+		"&& load usb 0:1 ${ramdisk_addr_r} /initrd.img "	\
+		"&& bootz ${kernel_addr_r} "				\
+			"${ramdisk_addr_r}:${filesize} ${fdt_addr_r}\0"	\
 	"bootcmd_rescue=run config_nc_dhcp; run nc\0"			\
 	"config_nc_dhcp=setenv autoload_old ${autoload}; "		\
 		"setenv autoload no "					\
