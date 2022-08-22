dc244ca33a8e6363dd71edd9e2beb09c7653f78f
William Zhang <william.zhang@broadcom.com>
2022-08-22 11:31:41 -0700

    arm: bcmbca: add bcm6856 SoC support under CONFIG_ARCH_BCMBCA
    BCM6856 is a Broadcom B53 based PON Gateway SoC. It is part of the BCA
    (Broadband Carrier Access origin) chipset family. Like other Broadband
    SoC, this patch adds it under CONFIG_BCM6856 chip config and
    CONFIG_ARCH_BCMBCA platform config.
    
    This initial support includes a bare-bone implementation and dts with
    CPU subsystem, memory and Broadcom uart. This SoC is supported in the
    linux-next git repository so the dts and dtsi files are copied from
    linux.
    
    The u-boot image can be loaded from flash or network to the entry point
    address in the memory and boot from there to the console.
    
    Signed-off-by: William Zhang <william.zhang@broadcom.com>
    Reviewed-by: Philippe Reynes <philippe.reynes@softathome.com>

---
+++ include/configs/bcm96856.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2022 Broadcom Ltd.
+ */
+
+#ifndef __BCM96856_H
+#define __BCM96856_H
+
+#define CONFIG_SYS_SDRAM_BASE		0x00000000
+
+#endif
