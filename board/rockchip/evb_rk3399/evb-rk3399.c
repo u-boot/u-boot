23afdef526404bbcce4f0e4ba1fce27068f03523
Sughosh Ganu <sughosh.ganu@linaro.org>
2022-09-02 18:48:24 +0530

    rockpi4: board: Add firmware image information needed for capsule updates
    Add information that will be needed for enabling the UEFI capsule
    update feature on the RockPi4 boards. With the feature enabled, it
    would be possible to update the idbloader and u-boot.itb images on the
    RockPi4B and RockPi4C variants.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- board/rockchip/evb_rk3399/evb-rk3399.c
+++ board/rockchip/evb_rk3399/evb-rk3399.c
@@ -5,10 +5,24 @@
 
 #include <common.h>
 #include <dm.h>
+#include <efi_loader.h>
 #include <init.h>
 #include <log.h>
 #include <asm/arch-rockchip/periph.h>
+#include <linux/kernel.h>
 #include <power/regulator.h>
+
+#define ROCKPI4_UPDATABLE_IMAGES	2
+
+#if CONFIG_IS_ENABLED(EFI_HAVE_CAPSULE_SUPPORT)
+struct efi_fw_image fw_images[ROCKPI4_UPDATABLE_IMAGES] = {0};
+
+struct efi_capsule_update_info update_info = {
+	.images = fw_images,
+};
+
+u8 num_image_type_guids = ARRAY_SIZE(fw_images);
+#endif
 
 #ifndef CONFIG_SPL_BUILD
 int board_early_init_f(void)
