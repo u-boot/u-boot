3cc471d30a1f7b47f0c9d2bd9874097d4b226659
Patrice Chotard <patrice.chotard@foss.st.com>
2022-09-20 18:04:35 +0200

    configs: stm32f769-disco: Fix internal flash size
    arch-stm32f7/stm32.h file is shared between STM32F746 and STM32F769
    MCUs. But STM32F769 embeds 2MB of internal flash instead of 1MB for
    STM32F746. The flash layout is quite similar between the 2 SoCs :
    
    	STM32F746 			STM32F769
    4 *  32KB sectors 			4 *  32KB sectors
    1 * 128KB sector 			1 * 128KB sector
    3 * 256KB sectors			7 * 256KB sectors
    
    Update sect_sz_kb[] structure and SYS_MAX_FLASH_SECT accordingly.
    
    Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Reviewed-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Tested-by: Waldemar Brodkorb <wbx@openadk.org>

--- arch/arm/include/asm/arch-stm32f7/stm32.h
+++ arch/arm/include/asm/arch-stm32f7/stm32.h
@@ -10,9 +10,9 @@
 #include <asm/arch-stm32/stm32f.h>
 
 static const u32 sect_sz_kb[CONFIG_SYS_MAX_FLASH_SECT] = {
-	[0 ... 3] =	32 * 1024,
-	[4] =		128 * 1024,
-	[5 ... 7] =	256 * 1024
+	[0 ... 3] =				32 * 1024,
+	[4] =					128 * 1024,
+	[5 ... CONFIG_SYS_MAX_FLASH_SECT - 1] =	256 * 1024
 };
 
 #endif /* _ASM_ARCH_HARDWARE_H */
