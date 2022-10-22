899f291bd16fd710acb2b9b6942da8c538c4f781
Adam Ford <aford173@gmail.com>
2022-10-22 08:43:41 -0500

    configs: imx8mn_beacon: Re-align memory to standard imx8mn settings
    The imx8mn_beacon board does not use the same memory map as the reference
    design from NXP or other imx8mn boards.  As such, memory is more limited
    in SPL.
    
    Moving SPL_BSS_START_ADDR and SPL_STACK to default locations increases
    the amount of available meory for the SPL stack.  Doing this allows
    the board to no longer define CONFIG_MALLOC_F_ADDR.
    
    Since SYS_LOAD_ADDR also does not align with other boards, move it too.
    
    Signed-off-by: Adam Ford <aford173@gmail.com>
    Reviewed-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- include/configs/imx8mn_beacon.h
+++ include/configs/imx8mn_beacon.h
@@ -13,14 +13,6 @@
 #define CONFIG_SYS_UBOOT_BASE	\
 	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
 
-#ifdef CONFIG_SPL_BUILD
-/* malloc f used before GD_FLG_FULL_MALLOC_INIT set */
-#define CONFIG_MALLOC_F_ADDR		0x184000
-
-/* For RAW image gives a error info not panic */
-
-#endif /* CONFIG_SPL_BUILD */
-
 /* Initial environment variables */
 #define CONFIG_EXTRA_ENV_SETTINGS		\
 	"script=boot.scr\0" \
