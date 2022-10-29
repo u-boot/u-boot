cc1159bbfa94a60e4180846e480b887cf91fa722
Tom Rini <trini@konsulko.com>
2022-10-28 20:27:14 -0400

    global: Migrate CONFIG_HPS* symbols to the CFG namespace
    Migrate all of CONFIG_HPS* to the CFG namespace.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- arch/arm/mach-socfpga/wrap_iocsr_config.c
+++ arch/arm/mach-socfpga/wrap_iocsr_config.c
@@ -17,19 +17,19 @@ int iocsr_get_config_table(const unsigned int chain_id,
 	switch (chain_id) {
 	case 0:
 		*table = iocsr_scan_chain0_table;
-		*table_len = CONFIG_HPS_IOCSR_SCANCHAIN0_LENGTH;
+		*table_len = CFG_HPS_IOCSR_SCANCHAIN0_LENGTH;
 		break;
 	case 1:
 		*table = iocsr_scan_chain1_table;
-		*table_len = CONFIG_HPS_IOCSR_SCANCHAIN1_LENGTH;
+		*table_len = CFG_HPS_IOCSR_SCANCHAIN1_LENGTH;
 		break;
 	case 2:
 		*table = iocsr_scan_chain2_table;
-		*table_len = CONFIG_HPS_IOCSR_SCANCHAIN2_LENGTH;
+		*table_len = CFG_HPS_IOCSR_SCANCHAIN2_LENGTH;
 		break;
 	case 3:
 		*table = iocsr_scan_chain3_table;
-		*table_len = CONFIG_HPS_IOCSR_SCANCHAIN3_LENGTH;
+		*table_len = CFG_HPS_IOCSR_SCANCHAIN3_LENGTH;
 		break;
 	default:
 		return -EINVAL;
