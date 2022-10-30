9bd1aa8af2ed3efde28250987806aaeb12837c66
Simon Glass <sjg@chromium.org>
2022-10-29 19:47:15 -0600

    dm: sandbox: Create a new HOST uclass
    Sandbox supports block devices which can access files on the host machine.
    At present there is no uclass for this. The devices are attached to the
    root devic. The block-device type is therefore set to UCLASS_ROOT which
    is confusing.
    
    Block devices should be attached to a 'media' device instead, something
    which handles access to the actual media and provides the block driver
    for the block device.
    
    Create a new uclass to handle this. It supports two operations, to attach
    and detach a file on the host machine.
    
    For now this is not fully plumbed in.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -63,6 +63,7 @@ enum uclass_id {
 	UCLASS_GPIO,		/* Bank of general-purpose I/O pins */
 	UCLASS_HASH,		/* Hash device */
 	UCLASS_HWSPINLOCK,	/* Hardware semaphores */
+	UCLASS_HOST,		/* Sandbox host device */
 	UCLASS_I2C,		/* I2C bus */
 	UCLASS_I2C_EEPROM,	/* I2C EEPROM device */
 	UCLASS_I2C_GENERIC,	/* Generic I2C device */
