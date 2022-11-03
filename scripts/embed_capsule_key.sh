ff55bcbab4e94d7ca171f664419eb3ce804db754
Sughosh Ganu <sughosh.ganu@linaro.org>
2022-11-03 15:43:14 +0530

    capsule: authenticate: Embed capsule signature in platform's dtb
    Add a script for embedding the public key for capsule authentication
    in the platform's dtb. This embeds the keys for all the device tree's
    that the platform builds.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

---
+++ scripts/embed_capsule_key.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# SPDX-License-Identifier: GPL-2.0+
+#
+# Copyright (C) 2022, Linaro Limited
+#
+
+gen_capsule_signature_file() {
+cat >> $1 << EOF
+/dts-v1/;
+/plugin/;
+
+&{/} {
+	signature {
+		capsule-key = /incbin/(CONFIG_EFI_CAPSULE_ESL_FILE);
+	};
+};
+EOF
+}
+
+gen_capsule_signature_file signature.$$.dts > /dev/null 2>&1
+$CPP $dtc_cpp_flags -x assembler-with-cpp -o signature.$$.tmp signature.$$.dts > /dev/null 2>&1
+dtc -@ -O dtb -o signature.$$.dtbo signature.$$.tmp > /dev/null 2>&1
+fdtoverlay -i $1 -o temp.$$.dtb -v signature.$$.dtbo > /dev/null 2>&1
+mv temp.$$.dtb $1 > /dev/null 2>&1
+rm -f signature.$$.* > /dev/null 2>&1
