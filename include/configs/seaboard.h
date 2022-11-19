b43295a27712c136afc68b0cf272e0356474642c
Tom Rini <trini@konsulko.com>
2022-11-19 18:45:36 -0500

    Convert CONFIG_TEGRA_CLOCK_SCALING et al to Kconfig
    This converts the following to Kconfig:
       CONFIG_TEGRA_CLOCK_SCALING
       CONFIG_TEGRA_LP0
       CONFIG_TEGRA_PMU
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/seaboard.h
+++ include/configs/seaboard.h
@@ -9,11 +9,6 @@
 
 #include <linux/sizes.h>
 
-/* LP0 suspend / resume */
-#define CONFIG_TEGRA_LP0
-#define CONFIG_TEGRA_PMU
-#define CONFIG_TEGRA_CLOCK_SCALING
-
 #include "tegra20-common.h"
 
 /* High-level configuration options */
