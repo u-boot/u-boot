acdf89ec060931ae4c87780ab0e65a6783ccd94b
Tom Rini <trini@konsulko.com>
2022-12-02 16:42:32 -0500

    Convert CONFIG_KSNET_NETCP_V1_0 et al to Kconfig
    This converts the following to Kconfig:
       CONFIG_KSNET_NETCP_V1_0
       CONFIG_KSNET_NETCP_V1_5
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/k2g_evm.h
+++ include/configs/k2g_evm.h
@@ -51,7 +51,6 @@
 	"name_fs=arago-base-tisdk-image-k2g-evm.cpio\0"
 
 /* Network */
-#define CONFIG_KSNET_NETCP_V1_5
 #define CONFIG_KSNET_CPSW_NUM_PORTS	2
 #define PHY_ANEG_TIMEOUT	10000 /* PHY needs longer aneg time */
 
--- include/configs/k2l_evm.h
+++ include/configs/k2l_evm.h
@@ -37,7 +37,6 @@
 #define SPI_MTD_PARTS KEYSTONE_SPI0_MTD_PARTS
 
 /* Network */
-#define CONFIG_KSNET_NETCP_V1_5
 #define CONFIG_KSNET_CPSW_NUM_PORTS	5
 
 #endif /* __CONFIG_K2L_EVM_H */
