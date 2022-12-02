a60c5a986eedd5920cd2c2ba79ecece85dbb87b5
Mihai Sain <mihai.sain@microchip.com>
2022-12-02 09:47:19 +0200

    ARM: mach-at91: add support for sama7g5 chip id and extended id definition
    Add SAMA7G5 series chip id definitions to align with linux SoC driver.
    Add support for SAMA7G5 System-In-Package (SIP):
    SAMA7G54D1G, SAMA7G54D2G, SAMA7G54D4G.
    
    Signed-off-by: Mihai Sain <mihai.sain@microchip.com>

--- arch/arm/mach-at91/armv7/sama7g5_devices.c
+++ arch/arm/mach-at91/armv7/sama7g5_devices.c
@@ -4,7 +4,31 @@
  *		      Eugen Hristev <eugen.hristev@microchip.com>
  */
 
+#include <asm/arch/sama7g5.h>
+
 char *get_cpu_name(void)
 {
-	return "SAMA7G5";
+	unsigned int extension_id = get_extension_chip_id();
+
+	if (cpu_is_sama7g5())
+		switch (extension_id) {
+		case ARCH_EXID_SAMA7G51:
+			return "SAMA7G51";
+		case ARCH_EXID_SAMA7G52:
+			return "SAMA7G52";
+		case ARCH_EXID_SAMA7G53:
+			return "SAMA7G53";
+		case ARCH_EXID_SAMA7G54:
+			return "SAMA7G54";
+		case ARCH_EXID_SAMA7G54_D1G:
+			return "SAMA7G54 1Gb DDR3L SiP";
+		case ARCH_EXID_SAMA7G54_D2G:
+			return "SAMA7G54 2Gb DDR3L SiP";
+		case ARCH_EXID_SAMA7G54_D4G:
+			return "SAMA7G54 4Gb DDR3L SiP";
+		default:
+			return "Unknown CPU type";
+		}
+	else
+		return "Unknown CPU type";
 }
