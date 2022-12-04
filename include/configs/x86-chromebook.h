92a5c899987301f6b53ef37e46ea96ce9abb752e
Tom Rini <trini@konsulko.com>
2022-12-04 10:14:09 -0500

    global: Migrate CONFIG_X86_REFCODE_RUN_ADDR to CFG
    Perform a simple rename of CONFIG_X86_REFCODE_RUN_ADDR to CFG_X86_REFCODE_RUN_ADDR
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/x86-chromebook.h
+++ include/configs/x86-chromebook.h
@@ -8,7 +8,7 @@
 
 #define CFG_X86_MRC_ADDR			0xfffa0000
 #define CFG_X86_REFCODE_ADDR			0xffea0000
-#define CONFIG_X86_REFCODE_RUN_ADDR		0
+#define CFG_X86_REFCODE_RUN_ADDR		0
 
 #define VIDEO_IO_OFFSET				0
 
