1cd60b3cc021fb8bfa537a393151871515b512b2
Tom Rini <trini@konsulko.com>
2022-12-04 10:04:31 -0500

    global: Migrate CONFIG_LEGACY_BOOTCMD_ENV to CFG
    Perform a simple rename of CONFIG_LEGACY_BOOTCMD_ENV to CFG_LEGACY_BOOTCMD_ENV
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/pic32mzdask.h
+++ include/configs/pic32mzdask.h
@@ -52,7 +52,7 @@
 	"fdt_addr_r=0x89d00000\0"				\
 	"scriptaddr=0x88300000\0"				\
 
-#define CONFIG_LEGACY_BOOTCMD_ENV					\
+#define CFG_LEGACY_BOOTCMD_ENV					\
 	"legacy_bootcmd= "						\
 		"if load mmc 0 ${scriptaddr} uEnv.txt; then "		\
 			"env import -tr ${scriptaddr} ${filesize}; "	\
@@ -71,7 +71,7 @@
 
 #define CFG_EXTRA_ENV_SETTINGS	\
 	MEM_LAYOUT_ENV_SETTINGS		\
-	CONFIG_LEGACY_BOOTCMD_ENV	\
+	CFG_LEGACY_BOOTCMD_ENV	\
 	BOOTENV
 
 #endif	/* __PIC32MZDASK_CONFIG_H */
