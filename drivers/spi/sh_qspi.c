730fcadc67680df41dd126dc469bcb35e1993be6
Pengfei Fan <fanpengfei1@eswincomputing.com>
2022-12-09 09:39:51 +0800

    drivers: spi: sh_qspi.c: Use log_warning() instead of printf()
    Use log_warning() instead of printf() to print out driver information
    
    Signed-off-by: Pengfei Fan <fanpengfei1@eswincomputing.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/spi/sh_qspi.c
+++ drivers/spi/sh_qspi.c
@@ -6,6 +6,8 @@
  * Copyright (C) 2013 Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
  */
 
+#define LOG_CATEGORY UCLASS_SPI
+
 #include <common.h>
 #include <console.h>
 #include <malloc.h>
@@ -160,7 +162,7 @@ static int sh_qspi_xfer_common(struct sh_qspi_slave *ss, unsigned int bitlen,
 	}
 
 	if (bitlen % 8) {
-		printf("%s: bitlen is not 8bit alined %d", __func__, bitlen);
+		log_warning("bitlen is not 8bit aligned %d", bitlen);
 		return 1;
 	}
 
