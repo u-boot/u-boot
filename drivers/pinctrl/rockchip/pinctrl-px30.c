32b2ea9818c6157bfc077de487b78e78536ab4a8
Jagan Teki <jagan@edgeble.ai>
2022-12-14 23:20:56 +0530

    pinctrl: rockchip: Add pinctrl route types
    Some pins in rockchip are routed via Top GRF and PMU GRF
    instead of direct regmap.
    
    Add support to handle all these routing paths so that the
    SoC pinctrl drivers will use them accordingly.
    
    Signed-off-by: Jianqun Xu <jay.xu@rock-chips.com>
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- drivers/pinctrl/rockchip/pinctrl-px30.c
+++ drivers/pinctrl/rockchip/pinctrl-px30.c
@@ -80,7 +80,7 @@ static int px30_set_mux(struct rockchip_pin_bank *bank, int pin, int mux)
 	struct regmap *regmap;
 	int reg, ret, mask, mux_type;
 	u8 bit;
-	u32 data, route_reg, route_val;
+	u32 data;
 
 	regmap = (bank->iomux[iomux_num].type & IOMUX_SOURCE_PMU)
 				? priv->regmap_pmu : priv->regmap_base;
@@ -89,15 +89,6 @@ static int px30_set_mux(struct rockchip_pin_bank *bank, int pin, int mux)
 	mux_type = bank->iomux[iomux_num].type;
 	reg = bank->iomux[iomux_num].offset;
 	reg += rockchip_get_mux_data(mux_type, pin, &bit, &mask);
-
-	if (bank->route_mask & BIT(pin)) {
-		if (rockchip_get_mux_route(bank, pin, mux, &route_reg,
-					   &route_val)) {
-			ret = regmap_write(regmap, route_reg, route_val);
-			if (ret)
-				return ret;
-		}
-	}
 
 	data = (mask << (bit + 16));
 	data |= (mux & mask) << bit;
