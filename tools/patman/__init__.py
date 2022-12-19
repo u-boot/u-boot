579916beb133ad74377964643c65c57fb3174cc6
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-12-19 17:32:41 -0500

    patman: rename main script to __main__.py
    This allows running the package as a Python module, like e.g.:
    
        $ python -m patman
    
    It also prevents Pytest from attempting to parse main.py, which
    would cause errors.
    
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@savoirfairelinux.com>
    Fix up main.py in __init__.py:
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/patman/__init__.py
+++ tools/patman/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 __all__ = ['checkpatch', 'command', 'commit', 'control', 'cros_subprocess',
-           'func_test', 'get_maintainer', 'gitutil', 'main', 'patchstream',
+           'func_test', 'get_maintainer', 'gitutil', '__main__', 'patchstream',
            'project', 'series', 'setup', 'settings', 'terminal',
            'test_checkpatch', 'test_util', 'tools', 'tout']
