5a42fd025859371e20bf7a5f064172d02c566707
Peter Robinson <pbrobinson@gmail.com>
2022-12-31 09:24:00 +0000

    rockchip: Add initial support for the PINE64 Pinephone Pro
    The Pinephone Pro is another device by PINE64. It's closely related
    to the Pinebook Pro of which this initial support is derived from.
    
    Specification:
    - A variant of the Rockchip RK3399
    - A 6 inch 720*1440 DSI display
    - Front and rear cameras
    - Type-C interface with alt mode display (DP 1.2) and PD charging
    - 4GB LPDDR4 RAM
    - 128GB eMMC
    - mSD card slot
    - An AP6255 module for 802.11ac WiFi and Bluetooth 5
    - Quectel EG25-G 4G/LTE modem
    
    Signed-off-by: Peter Robinson <pbrobinson@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ include/configs/pinephone-pro-rk3399.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2016 Rockchip Electronics Co., Ltd
+ * Copyright (C) 2022 Peter Robinson <pbrobinson at gmail.com>
+ */
+
+#ifndef __PINEPHONE_PRO_RK3399_H
+#define __PINEPHONE_PRO_RK3399_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=serial,usbkbd\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3399_common.h>
+
+#define SDRAM_BANK_SIZE			(2UL << 30)
+
+#endif
