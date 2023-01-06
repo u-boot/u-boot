9f0ebae0eada787f57b76d3d967489106048a9c6
Neha Malcom Francis <n-francis@ti.com>
2023-01-06 13:18:40 +0530

    ti: sysfw: tiboot3: Add support for packaging sysfw.itb and tiboot3.bin
    Board config binary artifacts must be populated in the R5 output
    directory to be used by binman to package sysfw.itb and tiboot3.bin.
    
    For devices that follow combined flow, these board configuration
    binaries must again be packaged into a combined board configuration
    blobs to be used by binman to package tiboot3.bin.
    
    Modify config.mk to generate all the board configuration binaries
    needed.
    
    Signed-off-by: Neha Malcom Francis <n-francis@ti.com>

--- arch/arm/mach-k3/config.mk
+++ arch/arm/mach-k3/config.mk
@@ -39,6 +39,56 @@ $(warning "WARNING: Software revision file not found. Default may not work on HS
 endif
 endif
 
+O ?= .
+
+# Board config binary artifacts necessary for packaging of tiboot3.bin
+# and sysfw.itb by binman, currently for general purpose devices and
+# devices that require sysfw.itb in ROM boot image.
+
+ifdef CONFIG_BINMAN
+
+ifndef CONFIG_TARGET_J7200_R5_EVM
+BOARD_YAML = $(srctree)/board/ti/$(BOARD)/board-cfg.yaml
+RM_YAML = $(srctree)/board/ti/$(BOARD)/rm-cfg.yaml
+SEC_YAML = $(srctree)/board/ti/$(BOARD)/sec-cfg.yaml
+PM_YAML = $(srctree)/board/ti/$(BOARD)/pm-cfg.yaml
+else
+BOARD_YAML = $(srctree)/board/ti/$(BOARD)/board-cfg_j7200.yaml
+RM_YAML = $(srctree)/board/ti/$(BOARD)/rm-cfg_j7200.yaml
+SEC_YAML = $(srctree)/board/ti/$(BOARD)/sec-cfg_j7200.yaml
+PM_YAML = $(srctree)/board/ti/$(BOARD)/pm-cfg_j7200.yaml
+endif
+
+CUSTOMER_KEY = $(srctree)/board/ti/keys/custMpk.pem
+TI_DEGENERATE_KEY = $(srctree)/board/ti/keys/ti-degenerate-key.pem
+
+SCHEMA_YAML = $(srctree)/board/ti/common/schema.yaml
+
+board-cfg.yaml: $(BOARD_YAML)
+	@cp $< $@
+rm-cfg.yaml: $(RM_YAML)
+	@cp $< $@
+sec-cfg.yaml: $(SEC_YAML)
+	@cp $< $@
+pm-cfg.yaml: $(PM_YAML)
+	@cp $< $@
+
+schema.yaml: $(SCHEMA_YAML)
+	@cp $< $@
+custMpk.pem: $(CUSTOMER_KEY)
+	@cp $< $@
+ti-degenerate-key.pem: $(TI_DEGENERATE_KEY)
+	@cp $< $@
+
+INPUTS-y	+= board-cfg.yaml
+INPUTS-y	+= rm-cfg.yaml
+INPUTS-y	+= sec-cfg.yaml
+INPUTS-y	+= pm-cfg.yaml
+INPUTS-y	+= schema.yaml
+INPUTS-y	+= custMpk.pem
+INPUTS-y	+= ti-degenerate-key.pem
+endif
+
 # tiboot3.bin is mandated by ROM and ROM only supports R5 boot.
 # So restrict tiboot3.bin creation for CPU_V7R.
 ifdef CONFIG_CPU_V7R
