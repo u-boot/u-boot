ac7fdbc35da93be0847c4bb2c2f8d6f88c9c1414
Alexey Romanov <avromanov@sberdevices.ru>
2023-01-10 13:56:49 +0300

    cmd/arm: meson: sm: introduce efusedump command
    Using this command user can print efuse memory:
    
    $ sm efusedump 0 10
      00000000: ff 00 31 00 00 ff 66 00 00 00      ..1...f...
    
    Signed-off-by: Alexey Romanov <avromanov@sberdevices.ru>
    Reviewed-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Link: https://lore.kernel.org/r/20230110105650.54580-5-avromanov@sberdevices.ru
    [narmstrong: added display_options.h include for print_buffer()]
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- cmd/meson/sm.c
+++ cmd/meson/sm.c
@@ -5,6 +5,7 @@
  * Author: Beniamino Galvani <b.galvani@gmail.com>
  * Author: Vyacheslav Bocharov <adeep@lexina.in>
  * Author: Neil Armstrong <narmstrong@baylibre.com>
+ * Author: Alexey Romanov <avromanov@sberdevices.ru>
  */
 
 #include <command.h>
@@ -12,6 +13,7 @@
 #include <env.h>
 #include <asm/arch/sm.h>
 #include <stdlib.h>
+#include <display_options.h>
 
 static int do_sm_serial(struct cmd_tbl *cmdtp, int flag, int argc,
 			char *const argv[])
@@ -119,11 +121,43 @@ static int do_efuse_write(struct cmd_tbl *cmdtp, int flag, int argc,
 	return CMD_RET_SUCCESS;
 }
 
+static int do_efuse_dump(struct cmd_tbl *cmdtp, int flag, int argc,
+			char *const argv[])
+{
+	ulong offset, size;
+	u8 *buffer;
+	int ret;
+
+	if (argc != 3)
+		return CMD_RET_USAGE;
+
+	offset = simple_strtoul(argv[1], NULL, 0);
+	size = simple_strtoul(argv[2], NULL, 0);
+	buffer = malloc(size);
+	if (!buffer) {
+		pr_err("Failed to allocate %lu bytes\n", size);
+		return CMD_RET_FAILURE;
+	}
+
+	ret = meson_sm_read_efuse(offset, (void *)buffer, size);
+	if (ret != size) {
+		ret = CMD_RET_FAILURE;
+		goto free_buffer;
+	}
+
+	print_buffer(0, buffer, 1, size, 0);
+
+free_buffer:
+	free(buffer);
+	return ret;
+}
+
 static struct cmd_tbl cmd_sm_sub[] = {
 	U_BOOT_CMD_MKENT(serial, 2, 1, do_sm_serial, "", ""),
 	U_BOOT_CMD_MKENT(reboot_reason, 1, 1, do_sm_reboot_reason, "", ""),
 	U_BOOT_CMD_MKENT(efuseread, 4, 1, do_efuse_read, "", ""),
 	U_BOOT_CMD_MKENT(efusewrite, 4, 0, do_efuse_write, "", ""),
+	U_BOOT_CMD_MKENT(efusedump, 3, 1, do_efuse_dump, "", ""),
 };
 
 static int do_sm(struct cmd_tbl *cmdtp, int flag, int argc,
@@ -152,5 +186,6 @@ U_BOOT_CMD(
 	"serial <address> - read chip unique id to memory address\n"
 	"sm reboot_reason [name] - get reboot reason and store to environment\n"
 	"sm efuseread <offset> <size> <address> - read efuse to memory address\n"
-	"sm efusewrite <offset> <size> <address> - write into efuse from memory address"
+	"sm efusewrite <offset> <size> <address> - write into efuse from memory address\n"
+	"sm efusedump <offset> <size> - dump efuse data range to console"
 );
