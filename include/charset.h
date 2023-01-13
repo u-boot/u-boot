u-boot/u-boot

commit 7a9b366cd9b7c1c880fda82509054a06fbac208c

lib: add function u16_strcasecmp()
Provide a function for comparing UTF-16 strings in a case insensitive
manner.

Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

include/charset.h
@@ -174,6 +174,19 @@ s32 utf_to_lower(const s32 code);
 s32 utf_to_upper(const s32 code);
 
 /**
+ * u16_strcasecmp() - compare two u16 strings case insensitively
+ *
+ * @s1:		first string to compare
+ * @s2:		second string to compare
+ * @n:		maximum number of u16 to compare
+ * Return:	0  if the first n u16 are the same in s1 and s2
+ *		< 0 if the first different u16 in s1 is less than the
+ *		corresponding u16 in s2
+ *		> 0 if the first different u16 in s1 is greater than the
+ */
+int u16_strcasecmp(const u16 *s1, const u16 *s2);
+
+/**
  * u16_strncmp() - compare two u16 string
  *
  * @s1:		first string to compare

lib/charset.c
@@ -351,6 +351,32 @@ s32 utf_to_upper(const s32 code)
 }
 
 /*
+ * u16_strcasecmp() - compare two u16 strings case insensitively
+ *
+ * @s1:		first string to compare
+ * @s2:		second string to compare
+ * @n:		maximum number of u16 to compare
+ * Return:	0  if the first n u16 are the same in s1 and s2
+ *		< 0 if the first different u16 in s1 is less than the
+ *		corresponding u16 in s2
+ *		> 0 if the first different u16 in s1 is greater than the
+ */
+int u16_strcasecmp(const u16 *s1, const u16 *s2)
+{
+	int ret = 0;
+	s32 c1, c2;
+
+	for (;;) {
+		c1 = utf_to_upper(utf16_get(&s1));
+		c2 = utf_to_upper(utf16_get(&s2));
+		ret = c1 - c2;
+		if (ret || !c1 || c1 == -1 || c2 == -1)
+			break;
+	}
+	return ret;
+}
+
+/*
  * u16_strncmp() - compare two u16 string
  *
  * @s1:		first string to compare

commit 6c39d0ef1eece4d8aa966b77a1362a53abd254a8

doc: fix description of u16_strcasecmp()
Remove non-existent parameter 'n' from function description.

Fixes: 7a9b366cd9b7 ("lib: add function u16_strcasecmp()")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

include/charset.h
@@ -178,7 +178,6 @@ s32 utf_to_upper(const s32 code);
  *
  * @s1:		first string to compare
  * @s2:		second string to compare
- * @n:		maximum number of u16 to compare
  * Return:	0  if the first n u16 are the same in s1 and s2
  *		< 0 if the first different u16 in s1 is less than the
  *		corresponding u16 in s2