1006b33b3a612acbaa66970f3bb8817216fc854e
Marek Vasut <marek.vasut+renesas@mailbox.org>
2023-01-26 21:10:48 +0100

    net: ravb: Drop SoC-specific compatible support
    The current set of U-Boot upstream R-Car Gen3 DTs all contain generic
    "renesas,etheravb-rcar-gen3" compatible strings, drop the SoC specific
    compatible string support from U-Boot to reduce size and duplication.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/net/ravb.c
+++ drivers/net/ravb.c
@@ -693,12 +693,6 @@ int ravb_of_to_plat(struct udevice *dev)
 }
 
 static const struct udevice_id ravb_ids[] = {
-	{ .compatible = "renesas,etheravb-r8a7795" },
-	{ .compatible = "renesas,etheravb-r8a7796" },
-	{ .compatible = "renesas,etheravb-r8a77965" },
-	{ .compatible = "renesas,etheravb-r8a77970" },
-	{ .compatible = "renesas,etheravb-r8a77990" },
-	{ .compatible = "renesas,etheravb-r8a77995" },
 	{ .compatible = "renesas,etheravb-rcar-gen3" },
 	{ }
 };
