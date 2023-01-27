b284c925ae980a60a225188ca5b04e29f8b94ee8
Troy Kisky <troy.kisky@boundarydevices.com>
2023-01-27 11:26:35 -0800

    CONFIG_CMD_NVEDIT_EFI: change CONFIG_IS_ENABLED to IS_ENABLED

--- cmd/nvedit.c
+++ cmd/nvedit.c
@@ -232,7 +232,7 @@ static int _do_env_set(int flag, int argc, char *const argv[], int env_flag)
 
 	debug("Initial value for argc=%d\n", argc);
 
-#if CONFIG_IS_ENABLED(CMD_NVEDIT_EFI)
+#if IS_ENABLED(CONFIG_CMD_NVEDIT_EFI)
 	if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'e')
 		return do_env_set_efi(NULL, flag, --argc, ++argv);
 #endif
