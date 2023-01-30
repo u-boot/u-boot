826d0484f048e85da83b9be6bd6f54fc58feb575
Jagan Teki <jagan@edgeble.ai>
2023-01-30 20:27:32 +0530

    rockchip: mkimage: Add rk3588 support
    Add support for rk3588 package header in mkimage tool.
    
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- tools/rkcommon.c
+++ tools/rkcommon.c
@@ -135,6 +135,7 @@ static struct spl_info spl_infos[] = {
 	{ "rv1108", "RK11", 0x1800, false, RK_HEADER_V1 },
 	{ "rv1126", "110B", 0x10000 - 0x1000, false, RK_HEADER_V1 },
 	{ "rk3568", "RK35", 0x10000 - 0x1000, false, RK_HEADER_V2 },
+	{ "rk3588", "RK35", 0x100000 - 0x1000, false, RK_HEADER_V2 },
 };
 
 /**
