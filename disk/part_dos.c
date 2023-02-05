d01bf20ea654f659404f744c4db284b82e4c68ad
Simon Glass <sjg@chromium.org>
2023-02-05 15:36:33 -0700

    Correct SPL use of CMD_MBR
    This converts 1 usage of this option to the non-SPL form, since there is
    no SPL_CMD_MBR defined in Kconfig
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- disk/part_dos.c
+++ disk/part_dos.c
@@ -318,7 +318,7 @@ int is_valid_dos_buf(void *buf)
 	return test_block_type(buf) == DOS_MBR ? 0 : -1;
 }
 
-#if CONFIG_IS_ENABLED(CMD_MBR)
+#if IS_ENABLED(CONFIG_CMD_MBR)
 static void lba_to_chs(lbaint_t lba, unsigned char *rc, unsigned char *rh,
 		       unsigned char *rs)
 {
