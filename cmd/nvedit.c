ddeac15e016c4ce9992f285c26f7171e7a96a323
Simon Glass <sjg@chromium.org>
2023-02-05 15:36:36 -0700

    Correct SPL use of CMD_NVEDIT_EFI
    This converts 1 usage of this option to the non-SPL form, since there is
    no SPL_CMD_NVEDIT_EFI defined in Kconfig
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- cmd/nvedit.c
+++ cmd/nvedit.c
@@ -231,7 +231,7 @@ static int _do_env_set(int flag, int argc, char *const argv[], int env_flag)
 
 	debug("Initial value for argc=%d\n", argc);
 
-#if CONFIG_IS_ENABLED(CMD_NVEDIT_EFI)
+#if !IS_ENABLED(CONFIG_SPL_BUILD) && IS_ENABLED(CONFIG_CMD_NVEDIT_EFI)
 	if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'e')
 		return do_env_set_efi(NULL, flag, --argc, ++argv);
 #endif
