066640f3b8aec1d8463e0489cd489048996fd01d
Simon Glass <sjg@chromium.org>
2023-02-05 15:40:07 -0700

    Correct SPL uses of HW_WATCHDOG
    This converts 2 usages of this option to the non-SPL form, since there is
    no SPL_HW_WATCHDOG defined in Kconfig
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>

--- drivers/crypto/aspeed/aspeed_hace.c
+++ drivers/crypto/aspeed/aspeed_hace.c
@@ -288,7 +288,7 @@ static int aspeed_hace_digest_wd(struct udevice *dev, enum HASH_ALGO algo,
 	if (rc)
 		return rc;
 
-	if (CONFIG_IS_ENABLED(HW_WATCHDOG) || CONFIG_IS_ENABLED(WATCHDOG)) {
+	if (IS_ENABLED(CONFIG_HW_WATCHDOG) || CONFIG_IS_ENABLED(WATCHDOG)) {
 		cur = ibuf;
 		end = ibuf + ilen;
 
