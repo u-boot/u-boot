7032fa28fdad988b89127daa9241d1b9024a9994
Simon Glass <sjg@chromium.org>
2023-02-05 15:40:23 -0700

    Correct SPL use of OCTEON_SERIAL_PCIE_CONSOLE
    This converts 1 usage of this option to the non-SPL form, since there is
    no SPL_OCTEON_SERIAL_PCIE_CONSOLE defined in Kconfig
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/mips/mach-octeon/cpu.c
+++ arch/mips/mach-octeon/cpu.c
@@ -448,7 +448,7 @@ int arch_misc_init(void)
 	if (ret)
 		return ret;
 
-	if (CONFIG_IS_ENABLED(OCTEON_SERIAL_PCIE_CONSOLE))
+	if (IS_ENABLED(CONFIG_OCTEON_SERIAL_PCIE_CONSOLE))
 		init_pcie_console();
 
 	if (IS_ENABLED(CONFIG_OCTEON_SERIAL_BOOTCMD))
