8dd86205ccc9e998ff7adf014a907b66c40200f5
Simon Glass <sjg@chromium.org>
2023-02-05 15:36:26 -0700

    Correct SPL uses of CMD_CLK
    This converts 7 usages of this option to the non-SPL form, since there is
    no SPL_CMD_CLK defined in Kconfig
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Sean Anderson <seanga2@gmail.com>

--- drivers/clk/imx/clk-imx8qm.c
+++ drivers/clk/imx/clk-imx8qm.c
@@ -16,7 +16,7 @@
 
 #include "clk-imx8.h"
 
-#if CONFIG_IS_ENABLED(CMD_CLK)
+#if IS_ENABLED(CONFIG_CMD_CLK)
 struct imx8_clks imx8_clk_names[] = {
 	{ IMX8QM_A53_DIV, "A53_DIV" },
 	{ IMX8QM_UART0_CLK, "UART0" },
