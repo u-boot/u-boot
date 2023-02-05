71e3e215079f7b228e8eaec084109b0aa5672fd1
Simon Glass <sjg@chromium.org>
2023-02-05 15:36:21 -0700

    Correct SPL use of CMD_BCB
    This converts 1 usage of this option to the non-SPL form, since there is
    no SPL_CMD_BCB defined in Kconfig
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/bcb.h
+++ include/bcb.h
@@ -8,7 +8,7 @@
 #ifndef __BCB_H__
 #define __BCB_H__
 
-#if CONFIG_IS_ENABLED(CMD_BCB)
+#if IS_ENABLED(CONFIG_CMD_BCB)
 int bcb_write_reboot_reason(int devnum, char *partp, const char *reasonp);
 #else
 #include <linux/errno.h>
