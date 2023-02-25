40389c2a462256da7f2348bed791c8ba2ae6eec6
Jonas Karlman <jonas@kwiboo.se>
2023-02-25 19:01:35 +0000

    binman: Mark mkimage entry missing when its subnodes is missing
    Using the mkimage entry with the multiple-data-files prop and having a
    missing external blob result in an unexpected ValueError exception using
    the --allow-missing flag.
    
      ValueError: Filename 'missing.bin' not found in input path (...)
    
    Fix this by using _pathname that is resolved by ObtainContents for blob
    entries, ObtainContents also handles allow missing for external blobs.
    
    Mark mkimage entry as missing and return without running mkimage when
    missing entries is reported by CheckMissing.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- tools/binman/ftest.py
+++ tools/binman/ftest.py
@@ -6393,6 +6393,17 @@ fdt         fdtmap                Extract the devicetree blob from the fdtmap
         data = self._DoReadFile('277_rockchip_tpl.dts')
         self.assertEqual(ROCKCHIP_TPL_DATA, data[:len(ROCKCHIP_TPL_DATA)])
 
+    def testMkimageMissingBlobMultiple(self):
+        """Test missing blob with mkimage entry and multiple-data-files"""
+        with test_util.capture_sys_output() as (stdout, stderr):
+            self._DoTestFile('278_mkimage_missing_multiple.dts', allow_missing=True)
+        err = stderr.getvalue()
+        self.assertIn("is missing external blobs and is non-functional", err)
+
+        with self.assertRaises(ValueError) as e:
+            self._DoTestFile('278_mkimage_missing_multiple.dts', allow_missing=False)
+        self.assertIn("not found in input path", str(e.exception))
+
 
 if __name__ == "__main__":
     unittest.main()
