8f098fd623c6e0064ca5b5273225f9381630a68d
Hai Pham <hai.pham.ud@renesas.com>
2023-02-28 22:34:45 +0100

    ARM: renesas: Add R8A779F0 S4 Kconfig entry and PRR ID
    Add Kconfig entry and PRR ID to support R8A779F0 S4 SoC.
    
    Reviewed-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    [Marek: Update commit message]

--- arch/arm/mach-rmobile/include/mach/rmobile.h
+++ arch/arm/mach-rmobile/include/mach/rmobile.h
@@ -39,6 +39,7 @@
 #define RMOBILE_CPU_TYPE_R8A77990	0x57
 #define RMOBILE_CPU_TYPE_R8A77995	0x58
 #define RMOBILE_CPU_TYPE_R8A779A0	0x59
+#define RMOBILE_CPU_TYPE_R8A779F0	0x5A
 
 #ifndef __ASSEMBLY__
 const u8 *rzg_get_cpu_name(void);
