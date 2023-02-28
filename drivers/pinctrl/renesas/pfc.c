1b1834c037fe87721b8c59a5d0701f8864dc499b
LUU HOAI <hoai.luu.ub@renesas.com>
2023-02-28 22:34:40 +0100

    pinctrl: renesas: Add R8A779F0 S4 PFC tables
    Add pinctrl tables for R8A779F0 S4 SoC.
    
    Based on Linux next 20230228 PFC tables tables up to
    commit 058f4df42121 ("Add linux-next specific files for 20230228")
    
    Reviewed-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    Signed-off-by: LUU HOAI <hoai.luu.ub@renesas.com>
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    [Marek: Use RCAR_64 Kconfig, sync with Linux next 20230228]

--- drivers/pinctrl/renesas/pfc.c
+++ drivers/pinctrl/renesas/pfc.c
@@ -43,6 +43,7 @@ enum sh_pfc_model {
 	SH_PFC_R8A77990,
 	SH_PFC_R8A77995,
 	SH_PFC_R8A779A0,
+	SH_PFC_R8A779F0,
 };
 
 struct sh_pfc_pin_config {
@@ -1025,6 +1026,10 @@ static int sh_pfc_pinctrl_probe(struct udevice *dev)
 	if (model == SH_PFC_R8A779A0)
 		priv->pfc.info = &r8a779a0_pinmux_info;
 #endif
+#ifdef CONFIG_PINCTRL_PFC_R8A779F0
+	if (model == SH_PFC_R8A779F0)
+		priv->pfc.info = &r8a779f0_pinmux_info;
+#endif
 
 	priv->pmx.pfc = &priv->pfc;
 	sh_pfc_init_ranges(&priv->pfc);
@@ -1140,6 +1145,12 @@ static const struct udevice_id sh_pfc_pinctrl_ids[] = {
 	{
 		.compatible = "renesas,pfc-r8a779a0",
 		.data = SH_PFC_R8A779A0,
+	},
+#endif
+#ifdef CONFIG_PINCTRL_PFC_R8A779F0
+	{
+		.compatible = "renesas,pfc-r8a779f0",
+		.data = SH_PFC_R8A779F0,
 	},
 #endif
 
