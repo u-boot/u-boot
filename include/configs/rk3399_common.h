d00fb6421c8fad639f608f55f9291305061ffb17
Tom Rini <trini@konsulko.com>
2023-03-24 16:58:16 -0400

    rockchip: rk3399: Drop altbootcmd
    The defined altbootcmd was specific to distro_bootcmd which is not
    longer in use on these platforms, so drop it.
    
    Tested-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Jonas Karlman <jonas@kwiboo.se>
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -52,10 +52,7 @@
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	"partitions=" PARTS_DEFAULT \
 	ROCKCHIP_DEVICE_SETTINGS \
-	"boot_targets=" BOOT_TARGETS "\0" \
-	"altbootcmd=" \
-		"setenv boot_syslinux_conf extlinux/extlinux-rollback.conf;" \
-		"run distro_bootcmd\0"
+	"boot_targets=" BOOT_TARGETS "\0"
 
 #endif
 
