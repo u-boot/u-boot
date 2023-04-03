8d916e5c15451756b0b8d4542f4586eed416d40e
Andrejs Cainikovs <andrejs.cainikovs@gmail.com>
2023-04-03 13:14:26 +0200

    board: colibri-imx8x: initialize snvs
    Initialize Secure Non-Volatile Storage, aka SNVS.
    
    Signed-off-by: Andrejs Cainikovs <andrejs.cainikovs@toradex.com>

--- board/toradex/colibri-imx8x/colibri-imx8x.c
+++ board/toradex/colibri-imx8x/colibri-imx8x.c
@@ -12,6 +12,7 @@
 #include <asm/arch/imx8-pins.h>
 #include <asm/arch/iomux.h>
 #include <firmware/imx/sci/sci.h>
+#include <asm/arch/snvs_security_sc.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/gpio.h>
 #include <asm/io.h>
@@ -138,6 +139,13 @@ static void select_dt_from_module_version(void)
 int board_init(void)
 {
 	board_gpio_init();
+
+	if (IS_ENABLED(CONFIG_IMX_SNVS_SEC_SC_AUTO)) {
+		int ret = snvs_security_sc_init();
+
+		if (ret)
+			return ret;
+	}
 
 	return 0;
 }
