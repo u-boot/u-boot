u-boot/u-boot

commit 2769ddc99fd089afb4c150b027ff4217d0fe098e

mmc: tmio: Replace ifdeffery with IS_ENABLED/CONFIG_IS_ENABLED macros
Instead of #if and #ifdef, use IS_ENABLED and CONFIG_IS_ENABLED macros.
This improves build test coverage. The CONFIG_SPL_BUILD must remain an
ifdef, as CONFIG_SPL_STACK may not always be defined, e.g. in U-Boot
proper build. No functional change.

Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
Reviewed-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>

drivers/mmc/tmio-common.c
@@ -369,22 +369,23 @@ static bool tmio_sd_addr_is_dmaable(struct mmc_data *data)
 	if (!IS_ALIGNED(addr, TMIO_SD_DMA_MINALIGN))
 		return false;
 
-#if defined(CONFIG_RCAR_GEN3)
-	if (!(data->flags & MMC_DATA_READ) && !IS_ALIGNED(addr, 128))
-		return false;
-	/* Gen3 DMA has 32bit limit */
-	if (sizeof(addr) > 4 && addr >> 32)
-		return false;
-#endif
+	if (IS_ENABLED(CONFIG_RCAR_GEN3)) {
+		if (!(data->flags & MMC_DATA_READ) && !IS_ALIGNED(addr, 128))
+			return false;
+		/* Gen3 DMA has 32bit limit */
+		if (sizeof(addr) > 4 && addr >> 32)
+			return false;
+	}
 
-#if defined(CONFIG_ARCH_UNIPHIER) && !defined(CONFIG_ARM64) && \
-	defined(CONFIG_SPL_BUILD)
-	/*
-	 * For UniPhier ARMv7 SoCs, the stack is allocated in the locked ways
-	 * of L2, which is unreachable from the DMA engine.
-	 */
-	if (addr < CONFIG_SPL_STACK)
-		return false;
+#ifdef CONFIG_SPL_BUILD
+	if (IS_ENABLED(CONFIG_ARCH_UNIPHIER) && !CONFIG_IS_ENABLED(CONFIG_ARM64)) {
+		/*
+		 * For UniPhier ARMv7 SoCs, the stack is allocated in locked
+		 * ways of L2, which is unreachable from the DMA engine.
+		 */
+		if (addr < CONFIG_SPL_STACK)
+			return false;
+	}
 #endif
 
 	return true;
@@ -622,25 +623,22 @@ static void tmio_sd_set_clk_rate(struct tmio_sd_priv *priv, struct mmc *mmc)
 static void tmio_sd_set_pins(struct udevice *dev)
 {
 	__maybe_unused struct mmc *mmc = mmc_get_mmc_dev(dev);
-
-#ifdef CONFIG_DM_REGULATOR
 	struct tmio_sd_priv *priv = dev_get_priv(dev);
 
-	if (priv->vqmmc_dev) {
+	if (CONFIG_IS_ENABLED(DM_REGULATOR) && priv->vqmmc_dev) {
 		if (mmc->signal_voltage == MMC_SIGNAL_VOLTAGE_180)
 			regulator_set_value(priv->vqmmc_dev, 1800000);
 		else
 			regulator_set_value(priv->vqmmc_dev, 3300000);
 		regulator_set_enable(priv->vqmmc_dev, true);
 	}
-#endif
 
-#ifdef CONFIG_PINCTRL
-	if (mmc->signal_voltage == MMC_SIGNAL_VOLTAGE_180)
-		pinctrl_select_state(dev, "state_uhs");
-	else
-		pinctrl_select_state(dev, "default");
-#endif
+	if (CONFIG_IS_ENABLED(PINCTRL)) {
+		if (mmc->signal_voltage == MMC_SIGNAL_VOLTAGE_180)
+			pinctrl_select_state(dev, "state_uhs");
+		else
+			pinctrl_select_state(dev, "default");
+	}
 }
 
 int tmio_sd_set_ios(struct udevice *dev)
@@ -734,11 +732,12 @@ int tmio_sd_probe(struct udevice *dev, u32 quirks)
 	if (!priv->regbase)
 		return -ENOMEM;
 
-#ifdef CONFIG_DM_REGULATOR
-	device_get_supply_regulator(dev, "vqmmc-supply", &priv->vqmmc_dev);
-	if (priv->vqmmc_dev)
-		regulator_set_value(priv->vqmmc_dev, 3300000);
-#endif
+	if (CONFIG_IS_ENABLED(DM_REGULATOR)) {
+		device_get_supply_regulator(dev, "vqmmc-supply",
+					    &priv->vqmmc_dev);
+		if (priv->vqmmc_dev)
+			regulator_set_value(priv->vqmmc_dev, 3300000);
+	}
 
 	ret = mmc_of_parse(dev, &plat->cfg);
 	if (ret < 0) {

drivers/mmc/tmio-common.h
@@ -133,9 +133,7 @@ struct tmio_sd_priv {
 #define TMIO_SD_CAP_RCAR_UHS		BIT(7)	/* Renesas RCar UHS/SDR modes */
 #define TMIO_SD_CAP_RCAR		\
 	(TMIO_SD_CAP_RCAR_GEN2 | TMIO_SD_CAP_RCAR_GEN3)
-#ifdef CONFIG_DM_REGULATOR
 	struct udevice *vqmmc_dev;
-#endif
 #if CONFIG_IS_ENABLED(CLK)
 	struct clk			clk;
 	struct clk			clkh;

commit 53614bc2dc4b87979041ddf97be43b81b98d4b7e

mmc: tmio: Use IS_ENABLED() to check for CONFIG_ option
Use IS_ENABLED() instead of CONFIG_IS_ENABLED() to check for CONFIG_
option which is identical across all of U-Boot and xPL builds.

Fixes: 2769ddc99fd ("mmc: tmio: Replace ifdeffery with IS_ENABLED/CONFIG_IS_ENABLED macros")
Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

drivers/mmc/tmio-common.c
@@ -378,7 +378,7 @@ static bool tmio_sd_addr_is_dmaable(struct mmc_data *data)
 	}
 
 #ifdef CONFIG_SPL_BUILD
-	if (IS_ENABLED(CONFIG_ARCH_UNIPHIER) && !CONFIG_IS_ENABLED(CONFIG_ARM64)) {
+	if (IS_ENABLED(CONFIG_ARCH_UNIPHIER) && !IS_ENABLED(CONFIG_ARM64)) {
 		/*
 		 * For UniPhier ARMv7 SoCs, the stack is allocated in locked
 		 * ways of L2, which is unreachable from the DMA engine.