08002ffd083d0caf8eea9c038a308dd45e2fb59c
Patrick Delaunay <patrick.delaunay@foss.st.com>
2023-04-24 16:21:10 +0200

    ARM: dts: stm32mp: alignment with v6.3
    Device tree alignment with Linux kernel v6.3:
    - f5a058023239 - ARM: dts: stm32: add i2c nodes into stm32mp131.dtsi
    - 8539ebb435a5 - ARM: dts: stm32: enable i2c1 and i2c5 on
      stm32mp135f-dk.dts
    - 8539ebb435a5 - ARM: dts: stm32: add spi nodes into stm32mp131.dtsi
    - 15f72e0da4da - ARM: dts: stm32: add pinctrl and disabled spi5 node in
      stm32mp135f-dk
    - ea99a5a02ebc - ARM: dts: stm32: Create separate pinmux for qspi cs pin
      in stm32mp15-pinctrl.dtsi
    - a306d8962a24 - ARM: dts: stm32: Rename mdio0 to mdio
    - 0a5ebb1f3367 - ARM: dts: stm32: Replace SAI format with dai-format DT
      property
    - ccdab19738a6 - ARM: dts: stm32: add adc support to stm32mp13
    - 022932ab55fd - ARM: dts: stm32: add adc pins muxing on stm32mp135f-dk
    - ab2806ddad9d - ARM: dts: stm32: add dummy vdd_adc regulator on
      stm32mp135f-dk
    - e46a180c060f - ARM: dts: stm32: add adc support on stm32mp135f-dk
    - 9ebf215fbae1 - ARM: dts: stm32: add PWR fixed regulators on stm32mp131
    - 16f4ff60519a - ARM: dts: stm32: add USBPHYC and dual USB HS PHY support
      on stm32mp131
    - 4a47f0f3e936 - ARM: dts: stm32: add UBSH EHCI and OHCI support on
      stm32mp131
    - 2a46bb66c47f - ARM: dts: stm32: add USB OTG HS support on stm32mp131
    - 9ebf215fbae1 - ARM: dts: stm32: add fixed regulators to support usb on
      stm32mp135f-dk
    - 16f4ff60519a - ARM: dts: stm32: enable USB HS phys on stm32mp135f-dk
    - c4e7254cf6dc - ARM: dts: stm32: enable USB Host EHCI on stm32mp135f-dk
    - 44978e135916 - ARM: dts: stm32: add pins for stm32g0 typec controller on stm32mp13
    - 4f532403b1e5 - ARM: dts: stm32: enable USB OTG in dual role mode on
      stm32mp135f-dk
    - e1f15571c96c - ARM: dts: stm32: add mcp23017 pinctrl entry for stm32mp13
    - 6cc71374002e - ARM: dts: stm32: add mcp23017 IO expander on I2C1 on
      stm32mp135f-dk
    - 7ffd2266bd32 - ARM: dts: stm32: Fix qspi pinctrl phandle for
      stm32mp15xx-dhcor-som
    - 21d83512bf2b - ARM: dts: stm32: Fix qspi pinctrl phandle for
      stm32mp15xx-dhcom-som
    - 732dbcf52f74 - ARM: dts: stm32: Fix qspi pinctrl phandle for
      stm32mp151a-prtt1l
    - 003b7c6b24f4 - ARM: dts: stm32: remove sai kernel clock on
      stm32mp15xx-dkx
    - f2b17b39bfff - ARM: dts: stm32: rename sound card on stm32mp15xx-dkx
    - dee3cb759d3d - ARM: dts: stm32: Remove the pins-are-numbered property
    - ae8cf3b48727 - ARM: dts: stm32: add i2s nodes on stm32mp131
    - 619746a27bd0 - ARM: dts: stm32: add sai nodes on stm32mp131
    - c5e05d08ef90 - ARM: dts: stm32: add spdifrx node on stm32mp131
    - 0a5afd3ee0d0 - ARM: dts: stm32: add dfsdm node on stm32mp131
    - bf9d876bea2e - ARM: dts: stm32: add timers support on stm32mp131
    - a3183748371d - ARM: dts: stm32: add timer pins muxing for stm32mp135f-dk
    - a9060c1326bc - ARM: dts: stm32: add timers support on stm32mp135f-dk
    - a12154058f75 - ARM: dts: stm32: Fix User button on stm32mp135f-dk
    - 2f33df889e99 - ARM: dts: stm32: Use new media bus type macros
    - 366384e49551 - ARM: dts: stm32: Update part number NVMEM description on
      stm32mp131
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- include/dt-bindings/reset/stm32mp13-resets.h
+++ include/dt-bindings/reset/stm32mp13-resets.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later or BSD-3-Clause */
 /*
- * Copyright (C) STMicroelectronics 2022 - All Rights Reserved
+ * Copyright (C) STMicroelectronics 2018 - All Rights Reserved
  * Author: Gabriel Fernandez <gabriel.fernandez@foss.st.com> for STMicroelectronics.
  */
 
