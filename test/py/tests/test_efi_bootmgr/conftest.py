bd730aa05baf1a6c5e61dc1f2e38b48f48a06b05
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2023-05-03 07:08:05 +0200

    test: fix pydoc issues for EFI tests
    Fix issues reported by pydocstyle.
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Acked-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- test/py/tests/test_efi_bootmgr/conftest.py
+++ test/py/tests/test_efi_bootmgr/conftest.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier:      GPL-2.0+
 
-"""Fixture for UEFI bootmanager test
-"""
+"""Fixture for UEFI bootmanager test."""
 
 import os
 import shutil
@@ -10,8 +9,7 @@ import pytest
 
 @pytest.fixture(scope='session')
 def efi_bootmgr_data(u_boot_config):
-    """Set up a file system to be used in UEFI bootmanager
-       tests
+    """Set up a file system to be used in UEFI bootmanager tests.
 
     Args:
         u_boot_config -- U-boot configuration.
