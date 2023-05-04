50574d42cfb9d3346e6a9a7642db67aa2b24d365
Simon Glass <sjg@chromium.org>
2023-05-04 16:50:58 -0600

    spl: Commit MTRRs only in board_init_f_r()
    We don't need to commit the SPI-flash MTRR change immediately, since it is
    now done in the board_init_f_r(). Also this causes chromebook_link64 to
    hang, presumably since we are still running from CAR (Cache-as-RAM) in
    SPL. Coral handles this OK, perhaps since it is running from a different
    memory area, but it has no effect on Coral anyway.
    
    Drop the extra mtrr_commit() in the SPL implementation.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

--- arch/x86/lib/spl.c
+++ arch/x86/lib/spl.c
@@ -147,7 +147,6 @@ static int x86_spl_init(void)
 		debug("%s: SPI cache setup failed (err=%d)\n", __func__, ret);
 		return ret;
 	}
-	mtrr_commit(true);
 # else
 	ret = syscon_get_by_driver_data(X86_SYSCON_PUNIT, &punit);
 	if (ret)
