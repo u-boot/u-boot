2a7ab9982cb648dba8305ad4ec02e721426e69f6
Manorit Chawdhry <m-chawdhry@ti.com>
2023-05-05 15:53:59 +0530

    Revert "arm: mach-k3: common: don't reconfigure background firewalls"
    This reverts commit b8ebf24e7f4afb5093a31bdf122e1ed0781e5c3c.
    
    This patch seems to be fundamentally wrong and requires a different way
    on how the background firewalls should be configured so revert the patch
    
    Signed-off-by: Manorit Chawdhry <m-chawdhry@ti.com>

--- arch/arm/mach-k3/common.c
+++ arch/arm/mach-k3/common.c
@@ -580,10 +580,7 @@ void remove_fwl_configs(struct fwl_data *fwl_data, size_t fwl_data_size)
 
 			fwl_ops->get_fwl_region(ti_sci, &region);
 
-			/* Don't disable the background regions */
-			if (region.control != 0 &&
-			    ((region.control & K3_BACKGROUND_FIREWALL_BIT) ==
-			     0)) {
+			if (region.control != 0) {
 				pr_debug("Attempting to disable firewall %5d (%25s)\n",
 					 region.fwl_id, fwl_data[i].name);
 				region.control = 0;
