3c874711ba2d66361b6040df48814be666aa6b1d
Ben Dooks <ben.dooks@sifive.com>
2023-05-05 09:02:05 +0100

    riscv: add generic link for <asm/atomic.h>
    Add a link from <asm/atomic.h> to the generic one to allow
    things like ubifs to be built. This can be extended with
    riscv AMO ops at a later date.
    
    Signed-off-by: Ben Dooks <ben.dooks@sifive.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

---
+++ arch/riscv/include/asm/atomic.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2023 SiFive, Inc.
+ */
+
+#ifndef __RISCV_ATOMIC_H
+#define __RISCV_ATOMIC_H
+
+/* use the generic asm/atomic.h until we define a better one */
+
+#include <asm/system.h>
+#include <asm-generic/atomic.h>
+
+#endif
