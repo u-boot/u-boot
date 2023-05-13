e87c869db3620c9b03b4364e144c19387d7bfd7a
Ralph Siemsen <ralph.siemsen@linaro.org>
2023-05-12 21:36:56 -0400

    board: schneider: add RZN1 board support
    Add support for Schneider Electric RZ/N1D and RZ/N1S boards, which
    are based on the Reneasas RZ/N1 SoC devices.
    
    The intention is to support both boards using a single defconfig, and to
    handle the differences at runtime.
    
    Signed-off-by: Ralph Siemsen <ralph.siemsen@linaro.org>
    Reviewed-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

---
+++ board/schneider/rzn1-snarc/rzn1.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+#include <common.h>
+#include <dm.h>
+#include <ram.h>
+#include <asm/global_data.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	gd->bd->bi_boot_params = gd->ram_base + 0x100;
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	struct udevice *dev;
+	int err;
+
+	/*
+	 * This will end up calling cadence_ddr_probe(),
+	 * and will also update gd->ram_size.
+	 */
+	err = uclass_get_device(UCLASS_RAM, 0, &dev);
+	if (err)
+		debug("DRAM init failed: %d\n", err);
+
+	return err;
+}
