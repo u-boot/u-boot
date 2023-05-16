f1671205fa44084b334027a267c86e5be2ea7c1a
Tom Rini <trini@konsulko.com>
2023-05-16 12:34:47 -0400

    include: Remove unused header files
    As part of various code clean-ups we have on occasion missed removing
    unused header files.  None of these files are referenced anywhere else
    at this point.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/mii_phy.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MII_PHY_H_
-#define _MII_PHY_H_
-
-void mii_discover_phy(void);
-unsigned short mii_phy_read(unsigned short reg);
-void mii_phy_write(unsigned short reg, unsigned short val);
-
-#endif
