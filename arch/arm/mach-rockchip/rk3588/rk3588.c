fd6e425be243dce518a02710482514faccf3c211
Jonas Karlman <jonas@kwiboo.se>
2023-05-18 15:39:30 +0000

    rockchip: rk3588-rock-5b: Enable boot from SPI NOR flash
    Add sfc and flash node to device tree and config options to enable
    support for booting from SPI NOR flash on Radxa ROCK 5 Model B.
    
    Similar to RK3568 the BootRom in RK3588 can read all data and look for
    idbloader at 0x8000, same as on SD and eMMC.
    
    Use the rksd format and modify the mkimage offset to generate a bootable
    u-boot-rockchip-spi.bin that can be written to 0x0 of SPI NOR flash. The
    FIT image is loaded from 0x60000.
    
      => sf probe
      SF: Detected mx25u12835f with page size 256 Bytes, erase size 4 KiB, total 16 MiB
    
      => load mmc 1:1 10000000 u-boot-rockchip-spi.bin
      1492992 bytes read in 129 ms (11 MiB/s)
    
      => sf update $fileaddr 0 $filesize
      device 0 offset 0x0, size 0x16c800
      1300480 bytes written, 192512 bytes skipped in 11.103s, speed 137694 B/s
    
    The BROM_BOOTSOURCE_ID value read back when booting from SPI flash does
    not match the expected value of 3 (SPINOR) used by other SoCs. Instead a
    value of 6 is read back, add a new enum value to handle this new
    bootsource id.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>
    Tested-by: Eugen Hristev <eugen.hristev@collabora.com>
    Reviewed-by: Eugen Hristev <eugen.hristev@collabora.com>

--- arch/arm/mach-rockchip/rk3588/rk3588.c
+++ arch/arm/mach-rockchip/rk3588/rk3588.c
@@ -41,6 +41,7 @@ const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
 	[BROM_BOOTSOURCE_EMMC] = "/mmc@fe2e0000",
 	[BROM_BOOTSOURCE_SPINOR] = "/spi@fe2b0000/flash@0",
 	[BROM_BOOTSOURCE_SD] = "/mmc@fe2c0000",
+	[BROM_BOOTSOURCE_SPINOR_RK3588] = "/spi@fe2b0000/flash@0",
 };
 
 static struct mm_region rk3588_mem_map[] = {
