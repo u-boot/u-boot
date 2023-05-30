fd5910288f7125cd4bf38c4a0df59ce8049703cf
Marek Vasut <marek.vasut+renesas@mailbox.org>
2023-05-31 00:51:19 +0200

    net: altera_tsa: Switch to new U-Boot PHY API
    Use new U-Boot phy_connect() API which also supports fixed PHYs.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    Reviewed-by: Ramon Fried <rfried.dev@gmail.com>

--- drivers/net/altera_tse.c
+++ drivers/net/altera_tse.c
@@ -430,16 +430,10 @@ static int tse_mdio_init(const char *name, struct altera_tse_priv *priv)
 static int tse_phy_init(struct altera_tse_priv *priv, void *dev)
 {
 	struct phy_device *phydev;
-	unsigned int mask = 0xffffffff;
 
-	if (priv->phyaddr)
-		mask = 1 << priv->phyaddr;
-
-	phydev = phy_find_by_mask(priv->bus, mask);
+	phydev = phy_connect(priv->bus, -1, dev, priv->interface);
 	if (!phydev)
 		return -ENODEV;
-
-	phy_connect_dev(phydev, dev, priv->interface);
 
 	phydev->supported &= PHY_GBIT_FEATURES;
 	phydev->advertising = phydev->supported;
