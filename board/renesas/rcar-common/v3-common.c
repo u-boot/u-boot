b0866db2dcecad88ffe1d0e8195d4c5f3d615cc6
Marek Vasut <marek.vasut+renesas@mailbox.org>
2023-05-31 20:24:34 +0200

    ARM: rmobile: Factor out common R-Car V3M/V3H board code
    Pull common board initialization code from V3M Eagle board
    into rcar-common/v3-common.c so it can be re-used by other
    V3M/V3H boards.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- board/renesas/rcar-common/v3-common.c
+++ board/renesas/rcar-common/v3-common.c
@@ -1,35 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * board/renesas/eagle/eagle.c
- *     This file is Eagle board support.
- *
- * Copyright (C) 2017 Marek Vasut <marek.vasut+renesas@gmail.com>
+ * Copyright (C) 2017-2023 Marek Vasut <marek.vasut+renesas@mailbox.org>
  */
 
 #include <common.h>
 #include <clock_legacy.h>
-#include <cpu_func.h>
-#include <hang.h>
-#include <init.h>
-#include <malloc.h>
-#include <netdev.h>
-#include <dm.h>
-#include <asm/global_data.h>
-#include <dm/platform_data/serial_sh.h>
-#include <asm/processor.h>
-#include <asm/mach-types.h>
 #include <asm/io.h>
-#include <linux/errno.h>
-#include <asm/arch/sys_proto.h>
-#include <asm/gpio.h>
-#include <asm/arch/gpio.h>
-#include <asm/arch/rmobile.h>
-#include <asm/arch/rcar-mstp.h>
-#include <asm/arch/sh_sdhi.h>
-#include <i2c.h>
-#include <mmc.h>
-
-DECLARE_GLOBAL_DATA_PTR;
 
 #define CPGWPR  0xE6150900
 #define CPGWPCR	0xE6150904
