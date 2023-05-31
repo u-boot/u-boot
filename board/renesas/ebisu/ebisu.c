5b90b22407e3620fc8c4cb2019ed2d073178f6f7
Marek Vasut <marek.vasut+renesas@mailbox.org>
2023-05-31 20:16:09 +0200

    ARM: rmobile: Reduce R-Car E3 Ebisu header usage
    There is no need to pull in all those headers as the board file is
    basically empty. Drop them all.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- board/renesas/ebisu/ebisu.c
+++ board/renesas/ebisu/ebisu.c
@@ -7,28 +7,6 @@
  */
 
 #include <common.h>
-#include <cpu_func.h>
-#include <hang.h>
-#include <init.h>
-#include <malloc.h>
-#include <netdev.h>
-#include <dm.h>
-#include <asm/global_data.h>
-#include <dm/platform_data/serial_sh.h>
-#include <asm/processor.h>
-#include <asm/mach-types.h>
-#include <asm/io.h>
-#include <linux/errno.h>
-#include <asm/arch/sys_proto.h>
-#include <asm/gpio.h>
-#include <asm/arch/gpio.h>
-#include <asm/arch/rmobile.h>
-#include <asm/arch/rcar-mstp.h>
-#include <asm/arch/sh_sdhi.h>
-#include <i2c.h>
-#include <mmc.h>
-
-DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
