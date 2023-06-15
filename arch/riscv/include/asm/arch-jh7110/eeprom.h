aea1bd95b61e5596000e2aa71b32b32801ac51d4
Yanhong Wang <yanhong.wang@starfivetech.com>
2023-06-15 17:36:48 +0800

    eeprom: starfive: Enable ID EEPROM configuration
    Enabled ID_EEPROM configuration for StarFive VisionFive2 board.
    
    Signed-off-by: Yanhong Wang <yanhong.wang@starfivetech.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

---
+++ arch/riscv/include/asm/arch-jh7110/eeprom.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2023 StarFive Technology Co., Ltd.
+ * Author: Yanhong Wang<yanhong.wang@starfivetech.com>
+ */
+
+#ifndef _ASM_RISCV_EEPROM_H
+#define _ASM_RISCV_EEPROM_H
+
+u8 get_pcb_revision_from_eeprom(void);
+u32 get_ddr_size_from_eeprom(void);
+
+#endif /* _ASM_RISCV_EEPROM_H */
