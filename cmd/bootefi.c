c6ee7e2cd12cbdfb4ee111489fe13b2dc64a61b4
Sughosh Ganu <sughosh.ganu@linaro.org>
2023-06-27 16:40:08 +0530

    efi: bootefi: Remove non-compliant DT nodes and properties
    Purge certain devicetree nodes and properties before the DT gets
    passed on to the kernel as EFI configuration table. This is used for
    removing any non-compliant nodes/properties from the DT before getting
    passed onto the kernel.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- cmd/bootefi.c
+++ cmd/bootefi.c
@@ -318,6 +318,7 @@ efi_status_t efi_install_fdt(void *fdt)
 	efi_carve_out_dt_rsv(fdt);
 
 	efi_try_purge_kaslr_seed(fdt);
+	efi_dt_nodes_props_purge(fdt);
 
 	if (CONFIG_IS_ENABLED(EFI_TCG2_PROTOCOL_MEASURE_DTB)) {
 		ret = efi_tcg2_measure_dtb(fdt);
