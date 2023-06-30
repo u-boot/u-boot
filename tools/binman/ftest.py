85f0b074d54480aa3699060c64257ef3b2ede93a
Sughosh Ganu <sughosh.ganu@linaro.org>
2023-06-30 17:47:44 +0530

    binman: capsule: Add support for generating capsules
    Add support in binman for generating capsules. The capsule parameters
    can be specified either through a config file or through the capsule
    binman entry. Also add test cases in binman for capsule generation,
    and enable this testing on the sandbox_spl variant.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- tools/binman/ftest.py
+++ tools/binman/ftest.py
@@ -48,6 +48,7 @@ U_BOOT_VPL_DATA       = b'vpl76543210fedcbazywxyz_'
 BLOB_DATA             = b'89'
 ME_DATA               = b'0abcd'
 VGA_DATA              = b'vga'
+EFI_DATA              = b'efi'
 U_BOOT_DTB_DATA       = b'udtb'
 U_BOOT_SPL_DTB_DATA   = b'spldtb'
 U_BOOT_TPL_DTB_DATA   = b'tpldtb'
@@ -114,6 +115,11 @@ REPACK_DTB_PROPS = ['orig-offset', 'orig-size']
 COMP_BINTOOLS = ['bzip2', 'gzip', 'lz4', 'lzma_alone', 'lzop', 'xz', 'zstd']
 
 TEE_ADDR = 0x5678
+
+# Firmware Management Protocol(FMP) GUID
+FW_MGMT_GUID = 'edd5cb6d2de8444cbda17194199ad92a'
+# Image GUID specified in the DTS
+CAPSULE_IMAGE_GUID = '52cfd7092007104791d108469b7fe9c8'
 
 class TestFunctional(unittest.TestCase):
     """Functional tests for binman
@@ -6676,6 +6682,125 @@ fdt         fdtmap                Extract the devicetree blob from the fdtmap
                                 ['fit'])
         self.assertIn("Node '/fit': Missing tool: 'mkimage'", str(e.exception))
 
+    def _CheckCapsule(self, signed_capsule=False, version_check=False):
+        fmp_signature = "4d535331"
+        fmp_size = "10"
+        fmp_fw_version = "02"
 
+        self.capsule_data = tools.read_file(self.capsule_fname)
+
+        self.assertEqual(FW_MGMT_GUID, self.capsule_data.hex()[:32])
+        self.assertEqual(CAPSULE_IMAGE_GUID, self.capsule_data.hex()[96:128])
+
+        if version_check:
+            self.assertEqual(fmp_signature, self.capsule_data.hex()[184:192])
+            self.assertEqual(fmp_size, self.capsule_data.hex()[192:194])
+            self.assertEqual(fmp_fw_version, self.capsule_data.hex()[200:202])
+
+        if signed_capsule:
+            self.assertEqual(self.payload_data.hex(), self.capsule_data.hex()[4770:4776])
+        elif version_check:
+            self.assertEqual(self.payload_data.hex(), self.capsule_data.hex()[216:222])
+        else:
+            self.assertEqual(self.payload_data.hex(), self.capsule_data.hex()[184:190])
+
+    def _GenCapsuleCfgPayload(self, fname, contents):
+        capsule_dir = '/tmp/capsules/'
+        pathname = os.path.join(capsule_dir, fname)
+
+        tools.write_file(pathname, EFI_DATA)
+
+    def testCapsuleGen(self):
+        """Test generation of EFI capsule"""
+        self.payload_data = EFI_DATA
+
+        TestFunctional._MakeInputFile('payload.txt', self.payload_data)
+
+        self._DoReadFile('282_capsule.dts')
+
+        self.capsule_fname = tools.get_output_filename('test.capsule')
+        self.assertTrue(os.path.exists(self.capsule_fname))
+
+        self._CheckCapsule()
+
+    def testSignedCapsuleGen(self):
+        """Test generation of EFI capsule"""
+        self.payload_data = EFI_DATA
+
+        TestFunctional._MakeInputFile('payload.txt', self.payload_data)
+
+        self._DoReadFile('283_capsule_signed.dts')
+
+        self.capsule_fname = tools.get_output_filename('test.capsule')
+        self.assertTrue(os.path.exists(self.capsule_fname))
+
+        self._CheckCapsule(signed_capsule=True)
+
+    def testCapsuleGenCfgFile(self):
+        """Test generation of EFI capsule through config file"""
+        self.payload_data = EFI_DATA
+
+        self._GenCapsuleCfgPayload('payload.txt', self.payload_data)
+
+        self._DoReadFile('284_capsule_conf.dts')
+
+        self.capsule_fname = '/tmp/capsules/test.capsule'
+        self.assertTrue(os.path.exists(self.capsule_fname))
+
+        self._CheckCapsule()
+
+    def testCapsuleGenVersionSupport(self):
+        """Test generation of EFI capsule with version support"""
+        self.payload_data = EFI_DATA
+
+        TestFunctional._MakeInputFile('payload.txt', self.payload_data)
+
+        self._DoReadFile('290_capsule_version.dts')
+
+        self.capsule_fname = tools.get_output_filename('test.capsule')
+        self.assertTrue(os.path.exists(self.capsule_fname))
+
+        self._CheckCapsule(version_check=True)
+
+    def testCapsuleGenKeyMissing(self):
+        """Test that binman errors out on missing key"""
+        with self.assertRaises(ValueError) as e:
+            self._DoReadFile('285_capsule_missing_key.dts')
+
+        self.assertIn("Both private key and public key certificate need to be provided",
+                      str(e.exception))
+
+    def testCapsuleGenIndexMissing(self):
+        """Test that binman errors out on missing image index"""
+        with self.assertRaises(ValueError) as e:
+            self._DoReadFile('286_capsule_missing_index.dts')
+
+        self.assertIn("mkeficapsule must be provided an Image Index",
+                      str(e.exception))
+
+    def testCapsuleGenGuidMissing(self):
+        """Test that binman errors out on missing image GUID"""
+        with self.assertRaises(ValueError) as e:
+            self._DoReadFile('287_capsule_missing_guid.dts')
+
+        self.assertIn("mkeficapsule must be provided an Image GUID",
+                      str(e.exception))
+
+    def testCapsuleGenPayloadMissing(self):
+        """Test that binman errors out on missing input(payload)image"""
+        with self.assertRaises(ValueError) as e:
+            self._DoReadFile('288_capsule_missing_payload.dts')
+
+        self.assertIn("mkeficapsule must be provided an input filename(payload)",
+                      str(e.exception))
+
+    def testCapsuleGenCapsuleFileMissing(self):
+        """Test that binman errors out on missing output capsule file"""
+        with self.assertRaises(ValueError) as e:
+            self._DoReadFile('289_capsule_missing.dts')
+
+        self.assertIn("Specify the output capsule file",
+                      str(e.exception))
+        
 if __name__ == "__main__":
     unittest.main()
