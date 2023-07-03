95811666ae9b1ff6228410629f209721d1ca91a8
Joshua Watt <jpewhacker@gmail.com>
2023-07-03 08:39:55 -0500

    dm: test: Add test for part_get_info_by_type
    Adds a test suite to ensure that part_get_info_by_type works correctly
    by creating a hybrid GPT/MBR partition table and reading both.
    
    Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>
    [trini: Add this on the other sandbox configs]
    Signedd-off-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- test/dm/part.c
+++ test/dm/part.c
@@ -108,3 +108,90 @@ static int dm_test_part_bootable(struct unit_test_state *uts)
 	return 0;
 }
 DM_TEST(dm_test_part_bootable, UT_TESTF_SCAN_FDT);
+
+static int do_get_info_test(struct unit_test_state *uts,
+			    struct blk_desc *dev_desc, int part, int part_type,
+			    struct disk_partition const *reference)
+{
+	struct disk_partition p;
+	int ret;
+
+	memset(&p, 0, sizeof(p));
+
+	ret = part_get_info_by_type(dev_desc, part, part_type, &p);
+	printf("part_get_info_by_type(%d, 0x%x) = %d\n", part, part_type, ret);
+	if (ut_assertok(ret)) {
+		return 0;
+	}
+
+	ut_asserteq(reference->start, p.start);
+	ut_asserteq(reference->size, p.size);
+	ut_asserteq(reference->sys_ind, p.sys_ind);
+
+	return 0;
+}
+
+static int dm_test_part_get_info_by_type(struct unit_test_state *uts)
+{
+	char str_disk_guid[UUID_STR_LEN + 1];
+	struct blk_desc *mmc_dev_desc;
+	struct disk_partition gpt_parts[] = {
+		{
+			.start = 48, /* GPT data takes up the first 34 blocks or so */
+			.size = 1,
+			.name = "test1",
+			.sys_ind = 0,
+		},
+		{
+			.start = 49,
+			.size = 1,
+			.name = "test2",
+			.sys_ind = 0,
+		},
+	};
+	struct disk_partition mbr_parts[] = {
+		{
+			.start = 1,
+			.size = 33,
+			.name = "gpt",
+			.sys_ind = EFI_PMBR_OSTYPE_EFI_GPT,
+		},
+		{
+			.start = 48,
+			.size = 1,
+			.name = "test1",
+			.sys_ind = 0x83,
+		},
+	};
+
+	ut_asserteq(2, blk_get_device_by_str("mmc", "2", &mmc_dev_desc));
+	if (CONFIG_IS_ENABLED(RANDOM_UUID)) {
+		gen_rand_uuid_str(gpt_parts[0].uuid, UUID_STR_FORMAT_STD);
+		gen_rand_uuid_str(gpt_parts[1].uuid, UUID_STR_FORMAT_STD);
+		gen_rand_uuid_str(str_disk_guid, UUID_STR_FORMAT_STD);
+	}
+	ut_assertok(gpt_restore(mmc_dev_desc, str_disk_guid, gpt_parts,
+				ARRAY_SIZE(gpt_parts)));
+
+	ut_assertok(write_mbr_partitions(mmc_dev_desc, mbr_parts,
+					 ARRAY_SIZE(mbr_parts), 0));
+
+#define get_info_test(_part, _part_type, _reference) \
+	ut_assertok(do_get_info_test(uts, mmc_dev_desc, _part, _part_type, \
+				     _reference))
+
+	for (int i = 0; i < ARRAY_SIZE(gpt_parts); i++) {
+		get_info_test(i + 1, PART_TYPE_UNKNOWN, &gpt_parts[i]);
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(mbr_parts); i++) {
+		get_info_test(i + 1, PART_TYPE_DOS, &mbr_parts[i]);
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(gpt_parts); i++) {
+		get_info_test(i + 1, PART_TYPE_EFI, &gpt_parts[i]);
+	}
+
+	return 0;
+}
+DM_TEST(dm_test_part_get_info_by_type, UT_TESTF_SCAN_PDATA | UT_TESTF_SCAN_FDT);
