650e5f097302246940924858408d218ae7bcd6d8
Sughosh Ganu <sughosh.ganu@linaro.org>
2023-07-09 01:14:50 +0530

    sandbox: capsule: Allow EFI capsule generation as part of the build
    The EFI capsule files can now be generated as part of u-boot
    build. This is done through binman. Add capsule entry nodes in the
    u-boot.dtsi for the sandbox architecture for generating the
    capsules. Remove the corresponding generation of capsules from the
    capsule update conftest file.
    
    Also generate the FIT image used for testing the capsule update
    feature on the sandbox_flattree variant through binman. Remove the now
    superfluous its file which was used for generating this FIT image.
    
    The capsules are generated as part of the build when the
    CONFIG_EFI_GENERATE_CAPSULES symbol is enabled. Enable this symbol in
    the CI run for sandbox and sandbox_flattree variants.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- include/sandbox_efi_capsule.h
+++ include/sandbox_efi_capsule.h
@@ -11,4 +11,13 @@
 #define SANDBOX_FIT_IMAGE_GUID		"3673b45d-6a7c-46f3-9e60-adabb03f7937"
 #define SANDBOX_INCORRECT_GUID		"058b7d83-50d5-4c47-a195-60d86ad341c4"
 
+#define UBOOT_BIN_IMAGE			"capsule_input_files/u-boot.bin.new"
+#define UBOOT_ENV_IMAGE			"capsule_input_files/u-boot.env.new"
+#define UBOOT_FIT_IMAGE			"capsule_input_files/u-boot_bin_env.itb"
+
+#define CAPSULE_PRIV_KEY		"capsule_input_files/SIGNER.key"
+#define CAPSULE_PUB_KEY			"capsule_input_files/SIGNER.crt"
+#define CAPSULE_INVAL_KEY		"capsule_input_files/SIGNER2.key"
+#define CAPSULE_INVAL_PUB_KEY		"capsule_input_files/SIGNER2.crt"
+
 #endif /* _SANDBOX_EFI_CAPSULE_H_ */
