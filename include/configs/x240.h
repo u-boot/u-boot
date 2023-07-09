4c97c4b5901cbbf8ed08eaf8c4f0b5f401c705c6
Chris Packham <judge.packham@gmail.com>
2023-07-10 10:47:36 +1200

    arm: mvebu: Add Allied Telesis x240 board
    The x240 and SE240 are a series of L2+ switches from Allied Telesis.
    There are a number of them in the range but as far as U-Boot is
    concerned all the CPU block components are the same so there's only one
    board defined.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Reviewed-by: Stefan Roese <sr@denx.de>

---
+++ include/configs/x240.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2022 Allied Telesis
+ */
+
+#ifndef __X240_H_
+#define __X240_H_
+
+#include <asm/arch/soc.h>
+
+/* additions for new ARM relocation support */
+#define CFG_SYS_SDRAM_BASE   0x200000000
+
+#define CFG_SYS_BAUDRATE_TABLE   { 9600, 19200, 38400, 57600, \
+				      115200, 230400, 460800, 921600 }
+
+/* Default Env vars */
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(USB, usb, 0) \
+	func(DHCP, dhcp, na)
+
+#include <config_distro_bootcmd.h>
+
+#define CFG_EXTRA_ENV_SETTINGS   \
+	BOOTENV \
+	"kernel_addr_r=0x202000000\0" \
+	"fdt_addr_r=0x201000000\0"    \
+	"ramdisk_addr_r=0x206000000\0"    \
+	"fdtfile=marvell/" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0"
+
+/*
+ * High Level Configuration Options (easy to change)
+ */
+#define CFG_SYS_TCLK     325000000
+
+#endif /* __X240_H_ */
