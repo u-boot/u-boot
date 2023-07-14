53873974a4b09c47d2b4b8c2ccfa23287fd6888a
Manorit Chawdhry <m-chawdhry@ti.com>
2023-07-14 11:22:35 +0530

    include: armv7: Enable distroboot across all configs
    Since K3 devices are moving towards distroboot, remove duplicates and
    add it in common file to import from.
    
    Signed-off-by: Manorit Chawdhry <m-chawdhry@ti.com>
    [trini: Add am65x_evm to this patch]
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/j721s2_evm.h
+++ include/configs/j721s2_evm.h
@@ -23,9 +23,6 @@
 #define CFG_SYS_UBOOT_BASE		0x50080000
 #endif
 
-/* Incorporate settings into the U-Boot environment */
-#define CFG_EXTRA_ENV_SETTINGS
-
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
 
