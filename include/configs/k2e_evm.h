5182e9c607c645eb97080ebd02c4f75fc1da0066
Andrew Davis <afd@ti.com>
2023-07-25 13:15:21 -0500

    configs: keystone2: Change to using env files
    Move to using .env file for setting up environment variables for K2x_evm.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- include/configs/k2e_evm.h
+++ include/configs/k2e_evm.h
@@ -9,20 +9,6 @@
 #ifndef __CONFIG_K2E_EVM_H
 #define __CONFIG_K2E_EVM_H
 
-/* U-Boot general configuration */
-#define ENV_KS2_BOARD_SETTINGS						\
-	DEFAULT_FW_INITRAMFS_BOOT_ENV					\
-	DEFAULT_FIT_TI_ARGS						\
-	"findfdt=setenv fdtfile ${name_fdt}\0"				\
-	"boot=ubi\0"							\
-	"args_ubi=setenv bootargs ${bootargs} rootfstype=ubifs "	\
-	"root=ubi0:rootfs rootflags=sync rw ubi.mtd=ubifs,2048\0"	\
-	"name_fdt=keystone-k2e-evm.dtb\0"				\
-	"name_mon=skern-k2e.bin\0"					\
-	"name_ubi=k2e-evm-ubifs.ubi\0"					\
-	"name_uboot=u-boot-spi-k2e-evm.gph\0"				\
-	"name_fs=arago-console-image-k2e-evm.cpio.gz\0"
-
 #include <configs/ti_armv7_keystone2.h>
 
 /* Network */
--- include/configs/k2l_evm.h
+++ include/configs/k2l_evm.h
@@ -9,20 +9,6 @@
 #ifndef __CONFIG_K2L_EVM_H
 #define __CONFIG_K2L_EVM_H
 
-/* U-Boot general configuration */
-#define ENV_KS2_BOARD_SETTINGS						\
-	DEFAULT_FW_INITRAMFS_BOOT_ENV					\
-	DEFAULT_FIT_TI_ARGS						\
-	"findfdt=setenv fdtfile ${name_fdt}\0"				\
-	"boot=ubi\0"							\
-	"args_ubi=setenv bootargs ${bootargs} rootfstype=ubifs "	\
-	"root=ubi0:rootfs rootflags=sync rw ubi.mtd=ubifs,4096\0"	\
-	"name_fdt=keystone-k2l-evm.dtb\0"				\
-	"name_mon=skern-k2l.bin\0"					\
-	"name_ubi=k2l-evm-ubifs.ubi\0"					\
-	"name_uboot=u-boot-spi-k2l-evm.gph\0"				\
-	"name_fs=arago-console-image-k2l-evm.cpio.gz\0"
-
 #include <configs/ti_armv7_keystone2.h>
 
 /* Network */
