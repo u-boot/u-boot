093bd0354e5b947b0bd634bf5ed4041ba075b57d
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2023-07-26 08:05:13 +0200

    acpi: Add missing RISC-V acpi_table header
    The pci_mmc.c driver can generate ACPI info and therefore includes
    asm/acpi_table.h. This file does not exist for the RISC-V architecture
    and thus code compilation fails when using this driver on RISC-V
    
    Create an empty include file.
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Reviewed-by: Rick Chen <rick@andestech.com>

---
+++ arch/riscv/include/asm/acpi_table.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef __ASM_ACPI_TABLE_H__
+#define __ASM_ACPI_TABLE_H__
+
+/*
+ * This file is needed by some drivers.
+ * We will fill it when adding ACPI support for RISC-V.
+ */
+
+#endif /* __ASM_ACPI_TABLE_H__ */
