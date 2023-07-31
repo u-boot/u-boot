2d6ebda7560b29bcc8044b01627eb3d3267806c0
Simon Glass <sjg@chromium.org>
2023-07-30 21:02:01 -0600

    x86: slimbootloader: Convert to text environment
    Use the common include along with some additions.
    
    Drop everything from the config.h file.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com> # Intel Edison
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    [Drop common env from slimbootloader.env]
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>

--- include/configs/slimbootloader.h
+++ include/configs/slimbootloader.h
@@ -2,38 +2,3 @@
 /*
  * Copyright (C) 2019 Intel Corporation <www.intel.com>
  */
-
-#ifndef __SLIMBOOTLOADER_CONFIG_H__
-#define __SLIMBOOTLOADER_CONFIG_H__
-
-#include <configs/x86-common.h>
-
-#define CFG_STD_DEVICES_SETTINGS		\
-	"stdin=serial,i8042-kbd,usbkbd\0"	\
-	"stdout=serial\0"			\
-	"stderr=serial\0"
-
-/*
- * Override CFG_EXTRA_ENV_SETTINGS in x86-common.h
- */
-#undef CFG_EXTRA_ENV_SETTINGS
-#define CFG_EXTRA_ENV_SETTINGS		\
-	CFG_STD_DEVICES_SETTINGS		\
-	"netdev=eth0\0"				\
-	"consoledev=ttyS0\0"			\
-	"ramdiskaddr=0x4000000\0"		\
-	"ramdiskfile=initrd\0"			\
-	"bootdev=usb\0"				\
-	"bootdevnum=0\0"			\
-	"bootdevpart=0\0"			\
-	"bootfsload=fatload\0"			\
-	"bootusb=setenv bootdev usb; boot\0"	\
-	"bootscsi=setenv bootdev scsi; boot\0"	\
-	"bootmmc=setenv bootdev mmc; boot\0"	\
-	"bootargs=console=ttyS0,115200 console=tty0\0"
-
-/*
- * Override CONFIG_BOOTCOMMAND in x86-common.h
- */
-
-#endif /* __SLIMBOOTLOADER_CONFIG_H__ */
