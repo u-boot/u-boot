48698ca3d76a84f052b053866d03525606faeb13
Maxim Uvarov <maxim.uvarov@linaro.org>
2023-08-02 08:53:31 +0000

    net/lwip: implement dns cmd
    U-Boot recently got support for an alternative network stack using LWIP.
    Replace dns command with the LWIP variant while keeping the output and
    error messages identical.
    
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ include/net/lwip.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+int do_lwip_dns(struct cmd_tbl *cmdtp, int flag, int argc,
+		char *const argv[]);
+
+/**
+ * ulwip_dns() - creates the DNS request to resolve a domain host name
+ *
+ * This function creates the DNS request to resolve a domain host name. Function
+ * can return immediately if previous request was cached or it might require
+ * entering the polling loop for a request to a remote server.
+ *
+ * @name:    dns name to resolve
+ * @varname: (optional) U-Boot variable name to store the result
+ * Returns: ERR_OK(0) for fetching entry from the cache
+ *          -EINPROGRESS success, can go to the polling loop
+ *          Other value < 0, if error
+ */
+int ulwip_dns(char *name, char *varname);
