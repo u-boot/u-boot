b9634fa2cb6f9c0f7cc463e1c753a7eae26d8635
Sughosh Ganu <sughosh.ganu@linaro.org>
2023-08-05 13:16:15 +0530

    binman: capsule: Add support for generating EFI capsules
    Add support in binman for generating EFI capsules. The capsule
    parameters can be specified through the capsule binman entry. Also add
    test cases in binman for testing capsule generation.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

---
+++ include/sandbox_efi_capsule.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2023, Linaro Limited
+ */
+
+#if !defined(_SANDBOX_EFI_CAPSULE_H_)
+#define _SANDBOX_EFI_CAPSULE_H_
+
+#define SANDBOX_UBOOT_IMAGE_GUID	"09d7cf52-0720-4710-91d1-08469b7fe9c8"
+#define SANDBOX_UBOOT_ENV_IMAGE_GUID	"5a7021f5-fef2-48b4-aaba-832e777418c0"
+#define SANDBOX_FIT_IMAGE_GUID		"3673b45d-6a7c-46f3-9e60-adabb03f7937"
+#define SANDBOX_INCORRECT_GUID		"058b7d83-50d5-4c47-a195-60d86ad341c4"
+
+#endif /* _SANDBOX_EFI_CAPSULE_H_ */
