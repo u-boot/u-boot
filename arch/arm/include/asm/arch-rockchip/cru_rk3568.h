6e710897aa319cda8aaf18a09290e3fb9b6d015f
Anton <vao@asu-vei.ru>
2023-08-07 10:04:46 +0300

    rockchip: cru: Enable cpu info support for rk3568
    Add cru structure definition in head file to support cpu_info driver.
    
    Series-version: 2
    Series-changes: 2
    Format the patch header, add commit message and signature.
    
    Signed-off-by: Anton <vao@asu-vei.ru>
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/include/asm/arch-rockchip/cru_rk3568.h
+++ arch/arm/include/asm/arch-rockchip/cru_rk3568.h
@@ -106,6 +106,8 @@ struct rk3568_cru {
 	unsigned int emmc_con[2];/* Address Offset: 0x0598 */
 };
 
+#define rockchip_cru rk3568_cru
+
 check_member(rk3568_cru, mode_con00, 0xc0);
 check_member(rk3568_cru, softrst_con[0], 0x400);
 
