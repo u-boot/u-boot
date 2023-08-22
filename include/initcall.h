e7f59dea880ea25078273473c575794dac08dca2
Simon Glass <sjg@chromium.org>
2023-08-21 21:16:49 -0600

    Revert "initcall: Move to inline function"
    Somehow I do not see any inlining with initcalls now. I was sure I saw
    it when this commit went in, but now it seems to make things worse.
    
    This reverts commit 47870afab92fca6e672c03d0dea802a55e200675.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/initcall.h
+++ include/initcall.h
@@ -8,50 +8,6 @@
 
 typedef int (*init_fnc_t)(void);
 
-#include <log.h>
-#ifdef CONFIG_EFI_APP
-#include <efi.h>
-#endif
-#include <asm/global_data.h>
-
-/*
- * To enable debugging. add #define DEBUG at the top of the including file.
- *
- * To find a symbol, use grep on u-boot.map
- */
-static inline int initcall_run_list(const init_fnc_t init_sequence[])
-{
-	const init_fnc_t *init_fnc_ptr;
-
-	for (init_fnc_ptr = init_sequence; *init_fnc_ptr; ++init_fnc_ptr) {
-		unsigned long reloc_ofs = 0;
-		int ret;
-
-		/*
-		 * Sandbox is relocated by the OS, so symbols always appear at
-		 * the relocated address.
-		 */
-		if (IS_ENABLED(CONFIG_SANDBOX) || (gd->flags & GD_FLG_RELOC))
-			reloc_ofs = gd->reloc_off;
-#ifdef CONFIG_EFI_APP
-		reloc_ofs = (unsigned long)image_base;
-#endif
-		if (reloc_ofs)
-			debug("initcall: %p (relocated to %p)\n",
-					(char *)*init_fnc_ptr - reloc_ofs,
-					(char *)*init_fnc_ptr);
-		else
-			debug("initcall: %p\n", (char *)*init_fnc_ptr - reloc_ofs);
-
-		ret = (*init_fnc_ptr)();
-		if (ret) {
-			printf("initcall sequence %p failed at call %p (err=%d)\n",
-			       init_sequence,
-			       (char *)*init_fnc_ptr - reloc_ofs, ret);
-			return -1;
-		}
-	}
-	return 0;
-}
+int initcall_run_list(const init_fnc_t init_sequence[]);
 
 #endif
