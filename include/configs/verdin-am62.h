9557d46107926b8346e4dcb3a0842c87e0fcbf7a
Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
2023-08-24 10:08:49 +0200

    verdin-am62: add u-boot update wrappers
    Add update_tiboot3, update_tispl and update_uboot wrappers to update
    R5 SPL, A53 SPL and A53 U-boot respectively.
    
    Usage example:
    > tftpboot ${loadaddr} tiboot3-am62x-gp-verdin.bin
    > run update_tiboot3
    
    > tftpboot ${loadaddr} tispl.bin
    > run update_tispl
    
    > tftpboot ${loadaddr} u-boot.img
    > run update_uboot
    
    Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
    Acked-by: Francesco Dolcini <francesco.dolcini@toradex.com>

--- include/configs/verdin-am62.h
+++ include/configs/verdin-am62.h
@@ -46,10 +46,20 @@
 	"fdt_board=dev\0" \
 	"setup=setenv setupargs console=tty1 console=${console},${baudrate} " \
 		"consoleblank=0 earlycon=ns16550a,mmio32,0x02800000\0" \
-	"update_uboot=askenv confirm Did you load flash.bin (y/N)?; " \
+	"update_tiboot3=askenv confirm Did you load tiboot3.bin (y/N)?; " \
 		"if test \"$confirm\" = \"y\"; then " \
 		"setexpr blkcnt ${filesize} + 0x1ff && setexpr blkcnt " \
 		"${blkcnt} / 0x200; mmc dev 0 1; mmc write ${loadaddr} 0x0 " \
+		"${blkcnt}; fi\0" \
+	"update_tispl=askenv confirm Did you load tispl.bin (y/N)?; " \
+		"if test \"$confirm\" = \"y\"; then " \
+		"setexpr blkcnt ${filesize} + 0x1ff && setexpr blkcnt " \
+		"${blkcnt} / 0x200; mmc dev 0 1; mmc write ${loadaddr} 0x400 " \
+		"${blkcnt}; fi\0" \
+	"update_uboot=askenv confirm Did you load u-boot.img (y/N)?; " \
+		"if test \"$confirm\" = \"y\"; then " \
+		"setexpr blkcnt ${filesize} + 0x1ff && setexpr blkcnt " \
+		"${blkcnt} / 0x200; mmc dev 0 1; mmc write ${loadaddr} 0x1400 " \
 		"${blkcnt}; fi\0"
 
 #endif /* __VERDIN_AM62_H */
