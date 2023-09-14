b22c6070bb235d6ca2ce274a8b70c57c7195dc0d
AKASHI Takahiro <takahiro.akashi@linaro.org>
2023-09-14 20:07:20 +0900

    firmware: scmi: use a protocol's own channel if assigned
    SCMI specification allows any protocol to have its own channel for
    the transport. While the current SCMI driver may assign its channel
    from a device tree, the core function, devm_scmi_process_msg(), doesn't
    use a protocol's channel, but always use an agent's channel.
    
    With this commit, devm_scmi_process_msg() tries to find and use
    a protocol's channel. If it doesn't exist, use an agent's.
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
    ---
    v5
    * new commit (fixing a potential bug)

--- drivers/firmware/scmi/mailbox_agent.c
+++ drivers/firmware/scmi/mailbox_agent.c
@@ -94,13 +94,14 @@ static int setup_channel(struct udevice *dev, struct scmi_mbox_channel *chan)
 }
 
 static int scmi_mbox_get_channel(struct udevice *dev,
+				 struct udevice *protocol,
 				 struct scmi_channel **channel)
 {
 	struct scmi_mbox_channel *base_chan = dev_get_plat(dev);
 	struct scmi_mbox_channel *chan;
 	int ret;
 
-	if (!dev_read_prop(dev, "shmem", NULL)) {
+	if (!dev_read_prop(protocol, "shmem", NULL)) {
 		/* Uses agent base channel */
 		*channel = container_of(base_chan, struct scmi_channel, ref);
 
@@ -112,7 +113,7 @@ static int scmi_mbox_get_channel(struct udevice *dev,
 		return -ENOMEM;
 
 	/* Setup a dedicated channel for the protocol */
-	ret = setup_channel(dev, chan);
+	ret = setup_channel(protocol, chan);
 	if (ret) {
 		free(chan);
 		return ret;
