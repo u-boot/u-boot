ef5ccaae64b670bc3183e20378d1fa31c86c9f44
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2023-09-21 12:39:29 +0200

    cmd/exception: test RISC-V 16 bit aligned instruction
    A 16 bit aligned instruction should generated an exception if the C
    extension is not available.
    
    Provide an 'extension ialign16' command for testing exception handling.
    
    For testing build qemu-riscv64_defconfig with CONFIG_RISCV_ISA_C=n
    and run with
    
        qemu-system-riscv64 -M virt -bios u-boot -nographic -cpu rv64,c=false
    
        => exception ialign16
        Unhandled exception: Instruction address misaligned
        EPC: 0000000087719138 RA: 0000000087719218 TVAL: 000000008771913e
        EPC: 0000000080020138 RA: 0000000080020218 reloc adjusted
    
        Code: 0113 0101 8067 0000 0113 ff01 3423 0011 (006f 0060)
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

--- cmd/riscv/exception.c
+++ cmd/riscv/exception.c
@@ -24,6 +24,19 @@ static int do_ebreak(struct cmd_tbl *cmdtp, int flag, int argc,
 	return CMD_RET_FAILURE;
 }
 
+static int do_ialign16(struct cmd_tbl *cmdtp, int flag, int argc,
+		       char *const argv[])
+{
+	asm volatile (
+		/* jump skipping 2 bytes */
+		".long 0x0060006f\n"
+		".long 0x006f0000\n"
+		".long 0x00000060\n"
+	);
+	printf("The system supports 16 bit aligned instructions.\n");
+	return CMD_RET_SUCCESS;
+}
+
 static int do_unaligned(struct cmd_tbl *cmdtp, int flag, int argc,
 			char *const argv[])
 {
@@ -48,6 +61,8 @@ static struct cmd_tbl cmd_sub[] = {
 			 "", ""),
 	U_BOOT_CMD_MKENT(ebreak, CONFIG_SYS_MAXARGS, 1, do_ebreak,
 			 "", ""),
+	U_BOOT_CMD_MKENT(ialign16, CONFIG_SYS_MAXARGS, 1, do_ialign16,
+			 "", ""),
 	U_BOOT_CMD_MKENT(unaligned, CONFIG_SYS_MAXARGS, 1, do_unaligned,
 			 "", ""),
 	U_BOOT_CMD_MKENT(undefined, CONFIG_SYS_MAXARGS, 1, do_undefined,
@@ -59,6 +74,7 @@ static char exception_help_text[] =
 	"  The following exceptions are available:\n"
 	"  compressed - compressed instruction\n"
 	"  ebreak     - breakpoint\n"
+	"  ialign16   - 16 bit aligned instruction\n"
 	"  undefined  - illegal instruction\n"
 	"  unaligned  - load address misaligned\n"
 	;
