5b7c58fbba568808791b636e4ee23e2348e58c0b
Okhunjon Sobirjonov <okhunjon72@gmail.com>
2023-09-25 06:43:28 +0300

    sunxi: R528: add SMHC2 pin pull ups support
    Add support for eMMC (SMHC2) pin pull ups for R528 boards.
    
    The D1 and T113s (and even R329) SoCs do not support 8-bit eMMC anymore,
    so it's just four data pins to cover here.
    
    Signed-off-by: Okhunjon Sobirjonov <Okhunjon.Sobirjonov@Mec-electronics.com>
    Reviewed-by: Andre Przywara <andre.przywara@arm.com>
    [Andre: adjust commit message]
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- board/sunxi/board.c
+++ board/sunxi/board.c
@@ -459,6 +459,13 @@ static void mmc_pinmux_setup(int sdc)
 			sunxi_gpio_set_pull(pin, SUNXI_GPIO_PULL_UP);
 			sunxi_gpio_set_drv(pin, 2);
 		}
+#elif defined(CONFIG_MACH_SUN8I_R528)
+                /* SDC2: PC2-PC7 */
+                for (pin = SUNXI_GPC(2); pin <= SUNXI_GPC(7); pin++) {
+                        sunxi_gpio_set_cfgpin(pin, SUNXI_GPC_SDC2);
+                        sunxi_gpio_set_pull(pin, SUNXI_GPIO_PULL_UP);
+                        sunxi_gpio_set_drv(pin, 2);
+                }
 #else
 		puts("ERROR: No pinmux setup defined for MMC2!\n");
 #endif
