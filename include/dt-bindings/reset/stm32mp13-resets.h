f959118b660ca7545a527179e83f3d4bc38426e1
Patrice Chotard <patrice.chotard@foss.st.com>
2023-09-26 17:09:18 +0200

    ARM: dts: stm32mp: alignment with v6.6-rc1
    Device tree alignment with Linux kernel v6.6.rc1.
    
    Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Reviewed-by: Patrick Delaunay <patrick.delaunay@foss.st.com>

--- include/dt-bindings/reset/stm32mp13-resets.h
+++ include/dt-bindings/reset/stm32mp13-resets.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later or BSD-3-Clause */
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-3-Clause */
 /*
  * Copyright (C) STMicroelectronics 2018 - All Rights Reserved
  * Author: Gabriel Fernandez <gabriel.fernandez@foss.st.com> for STMicroelectronics.
