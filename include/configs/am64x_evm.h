174b34f4f36dfc0d460bff83c4104d4795fa6b71
Roger Quadros <rogerq@kernel.org>
2023-10-05 16:06:42 +0300

    board: ti: am64x: Switch to standard boot flow
    Switch to using bootstd. Note with this change, we will stop using
    distro_bootcmd and instead depend entirely on bootflow method of
    starting the system up.
    
    Drop header files that are no longer needed in am64x_evm.h.
    k3_dfu.h is available via k3_dfu.env in am64x.env.
    
    Drop unused macro CFG_SYS_SDRAM_BASE1.
    
    Signed-off-by: Roger Quadros <rogerq@kernel.org>
    Reviewed-by: Nishanth Menon <nm@ti.com>

--- include/configs/am64x_evm.h
+++ include/configs/am64x_evm.h
@@ -9,15 +9,6 @@
 #ifndef __CONFIG_AM642_EVM_H
 #define __CONFIG_AM642_EVM_H
 
-#include <linux/sizes.h>
-#include <config_distro_bootcmd.h>
-#include <env/ti/mmc.h>
-#include <asm/arch/am64_hardware.h>
-#include <env/ti/k3_dfu.h>
-
-/* DDR Configuration */
-#define CFG_SYS_SDRAM_BASE1		0x880000000
-
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
 
