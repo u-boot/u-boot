898d82dd2e838528bbe93b56bd2518fbbca97e0e
Sughosh Ganu <sughosh.ganu@linaro.org>
2023-10-06 16:52:56 +0530

    dt: Provide a way to remove non-compliant nodes and properties
    Add a function which is registered to spy for a EVT_DT_NODE_PROP_PURGE
    event, and removes the non upstreamed nodes and properties from the
    devicetree before it gets passed to the OS.
    
    This allows removing entire nodes, or specific properties under nodes
    from the devicetree. The required nodes and properties can be
    registered for removal through the DT_NON_COMPLIANT_PURGE and
    DT_NON_COMPLIANT_PURGE_LIST macros.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- include/dt-structs.h
+++ include/dt-structs.h
@@ -56,4 +56,15 @@ struct phandle_2_arg {
 #include <generated/dt-decl.h>
 #endif
 
+struct dt_non_compliant_purge {
+	const char *node_path;
+	const char *prop;
+};
+
+#define DT_NON_COMPLIANT_PURGE(__name)	\
+	ll_entry_declare(struct dt_non_compliant_purge, __name, dt_purge)
+
+#define DT_NON_COMPLIANT_PURGE_LIST(__name)	\
+	ll_entry_declare_list(struct dt_non_compliant_purge, __name, dt_purge)
+
 #endif
