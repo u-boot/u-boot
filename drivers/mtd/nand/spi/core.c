0f2c632b8cc3fa6986d66f1ec4daefb693e000eb
Igor Prusov <ivprusov@salutedevices.com>
2023-10-17 22:29:26 +0300

    mtd: spinand: add support for ESMT F50x1G41LB
    Adaptation of Linux commit d74c36480a67
    
    This patch adds support for ESMT F50L1G41LB and F50D1G41LB.
    It seems that ESMT likes to use random JEDEC ID from other vendors.
    Their 1G chips uses 0xc8 from GigaDevice and 2G/4G chips uses 0x2c from
    Micron. For this reason, the ESMT entry is named esmt_c8 with explicit
    JEDEC ID in variable name.
    
    Datasheets:
    https://www.esmt.com.tw/upload/pdf/ESMT/datasheets/F50L1G41LB(2M).pdf
    https://www.esmt.com.tw/upload/pdf/ESMT/datasheets/F50D1G41LB(2M).pdf
    
    Signed-off-by: Igor Prusov <ivprusov@salutedevices.com>
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>
    Signed-off-by: Martin Kurbanov <mmkurbanov@sberdevices.ru>
    Signed-off-by: Dmitry Rokosov <ddrokosov@sberdevices.ru>
    Tested-by: Martin Kurbanov <mmkurbanov@sberdevices.ru>
    Reviewed-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Reviewed-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -828,6 +828,7 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&paragon_spinand_manufacturer,
 	&toshiba_spinand_manufacturer,
 	&winbond_spinand_manufacturer,
+	&esmt_c8_spinand_manufacturer,
 };
 
 static int spinand_manufacturer_match(struct spinand_device *spinand,
