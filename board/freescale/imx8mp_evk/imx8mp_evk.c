21b30c237aa649f7de0ddf6140eb747e114a2063
Fabio Estevam <festevam@denx.de>
2023-10-19 18:51:12 -0300

    imx8mp_evk: Simplify Ethernet initialization
    With DM enabled, there is no need for board code to initialize
    the Ethernet interfaces.
    
    The RTL8211FDI Ethernet PHYs have 25MHz oscillator, so there is no
    need to enable the RGMII TX clk output.
    
    Also, there is no need for describing the deprecated phy-reset FEC
    properties, nor passing reset properties to the EQOS interface in
    u-boot.dtsi.
    
    Remove all these unneeded pieces.
    
    Tested both Ethernet interfaces after these changes.
    
    Signed-off-by: Fabio Estevam <festevam@denx.de>
    Reviewed-by: Hugo Villeneuve <hvilleneuve@dimonoff.com>

--- board/freescale/imx8mp_evk/imx8mp_evk.c
+++ board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -7,8 +7,6 @@
 #include <env.h>
 #include <errno.h>
 #include <init.h>
-#include <miiphy.h>
-#include <netdev.h>
 #include <linux/delay.h>
 #include <asm/global_data.h>
 #include <asm/mach-imx/iomux-v3.h>
@@ -20,33 +18,9 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-static void setup_fec(void)
-{
-	struct iomuxc_gpr_base_regs *gpr =
-		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
-
-	/* Enable RGMII TX clk output */
-	setbits_le32(&gpr->gpr[1], BIT(22));
-}
-
-#if CONFIG_IS_ENABLED(NET)
-int board_phy_config(struct phy_device *phydev)
-{
-	if (phydev->drv->config)
-		phydev->drv->config(phydev);
-	return 0;
-}
-#endif
-
 int board_init(void)
 {
-	int ret = 0;
-
-	if (IS_ENABLED(CONFIG_FEC_MXC)) {
-		setup_fec();
-	}
-
-	return ret;
+	return 0;
 }
 
 int board_late_init(void)
