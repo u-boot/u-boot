b6014f209f15bd49dd4ceb987d3a52e99b0305d4
Tony Dinh <mibodhi@gmail.com>
2023-10-25 15:38:37 -0700

    arm: kirkwood: Enable bootstd for Zyxel NSA310S board
    Enable bootstd for Zyxel NSA310S board, and remove distroboot.
    
    Signed-off-by: Tony Dinh <mibodhi@gmail.com>
    Reviewed-by: Stefan Roese <sr@denx.de>

--- include/configs/nsa310s.h
+++ include/configs/nsa310s.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright (C) 2015, 2021-2022 Tony Dinh <mibodhi@gmail.com>
+ * Copyright (C) 2015, 2021-2023 Tony Dinh <mibodhi@gmail.com>
  * Copyright (C) 2015
  * Gerald Kerma <dreagle@doukki.net>
  * Luka Perkov <luka.perkov@sartura.hr>
@@ -15,14 +15,6 @@
  */
 #include "mv-common.h"
 
-/* Include the common distro boot environment */
-#ifndef CONFIG_SPL_BUILD
-
-#define BOOT_TARGET_DEVICES(func) \
-	func(USB, usb, 0) \
-	func(SATA, sata, 0) \
-	func(DHCP, dhcp, na)
-
 #define KERNEL_ADDR_R	__stringify(0x800000)
 #define FDT_ADDR_R	__stringify(0x2c00000)
 #define RAMDISK_ADDR_R	__stringify(0x01100000)
@@ -34,16 +26,11 @@
 	"ramdisk_addr_r=" RAMDISK_ADDR_R "\0" \
 	"scriptaddr=" SCRIPT_ADDR_R "\0"
 
-#include <config_distro_bootcmd.h>
-
 #define CFG_EXTRA_ENV_SETTINGS \
 	"console=console=ttyS0,115200\0" \
 	"kernel=/boot/zImage\0" \
 	"fdt=/boot/nsa310s.dtb\0" \
 	"bootargs_root=ubi.mtd=3 root=ubi0:rootfs rootfstype=ubifs rw\0" \
-	LOAD_ADDRESS_ENV_SETTINGS \
-	BOOTENV
-
-#endif /* CONFIG_SPL_BUILD */
+	LOAD_ADDRESS_ENV_SETTINGS
 
 #endif /* _CONFIG_NSA310S_H */
