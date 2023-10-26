56ea7c8b7534d676120b37f70427724a1e0965e2
Tom Rini <trini@konsulko.com>
2023-10-26 14:31:38 -0400

    CI, pytest: Add a test for sandbox without LTO
    The primary motivation for having a sandbox without LTO build in CI is
    to ensure that we don't have that option break. We now have the ability
    to run tests of specific options being enabled/disabled, so drop the
    parts of CI that build and test that configuration specifically and add
    a build test instead. We still test that "NO_LTO=1" rather than editing
    the config file works via the ftrace tests.
    
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- test/py/tests/test_sandbox_opts.py
+++ test/py/tests/test_sandbox_opts.py
@@ -18,3 +18,13 @@ def test_sandbox_cmdline(u_boot_console):
     out = util.run_and_log(
         cons, ['./tools/buildman/buildman', '-m', '--board', 'sandbox',
                '-a', '~CMDLINE', '-o', TMPDIR])
+
+@pytest.mark.slow
+@pytest.mark.boardspec('sandbox')
+def test_sandbox_lto(u_boot_console):
+    """Test building sandbox without CONFIG_LTO"""
+    cons = u_boot_console
+
+    out = util.run_and_log(
+        cons, ['./tools/buildman/buildman', '-m', '--board', 'sandbox',
+               '-a', '~LTO', '-o', TMPDIR])
