b35df87ae5c812d09c82e3c329b01275ab5bd41b
Sean Anderson <seanga2@gmail.com>
2023-11-04 16:37:47 -0400

    mtd: Rename SPL_MTD_SUPPORT to SPL_MTD
    Rename SPL_MTD_SUPPORT to SPL_MTD in order to match MTD. This allows using
    CONFIG_IS_ENABLED to test for MTD support.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- include/mtd/cfi_flash.h
+++ include/mtd/cfi_flash.h
@@ -163,7 +163,7 @@ struct cfi_pri_hdr {
 #if defined(CONFIG_SYS_MAX_FLASH_BANKS_DETECT)
 /* map to cfi_flash_num_flash_banks only when supported */
 #if IS_ENABLED(CONFIG_FLASH_CFI_DRIVER) && \
-    (!IS_ENABLED(CONFIG_SPL_BUILD) || IS_ENABLED(CONFIG_SPL_MTD_SUPPORT))
+    (!IS_ENABLED(CONFIG_SPL_BUILD) || IS_ENABLED(CONFIG_SPL_MTD))
 #define CFI_FLASH_BANKS		(cfi_flash_num_flash_banks)
 /* board code can update this variable before CFI detection */
 extern int cfi_flash_num_flash_banks;
