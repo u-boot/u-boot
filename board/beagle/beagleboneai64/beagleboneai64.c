21bc6ccbdbae766ee683fc91df3fdd9a8352c8d2
Nishanth Menon <nm@ti.com>
2023-11-03 22:17:32 -0500

    board: beagle: Add BeagleBone AI-64 support
    Add base support for BeagleBone AI-64 board support.
    
    Further information at  https://beagleboard.org/ai-64
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ board/beagle/beagleboneai64/beagleboneai64.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * https://beagleboard.org/ai-64
+ *
+ * Copyright (C) 2022-2023 Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2022-2023 Jason Kridner, BeagleBoard.org Foundation
+ * Copyright (C) 2022-2023 Robert Nelson, BeagleBoard.org Foundation
+ */
+
+#include <cpu_func.h>
+#include <env.h>
+#include <fdt_support.h>
+#include <spl.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	return fdtdec_setup_mem_size_base();
+}
+
+int dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
