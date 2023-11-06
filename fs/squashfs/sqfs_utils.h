99374ff189dab2d678449d3dcf3756dccfd2acc2
David Oberhollenzer <goliath@infraroot.at>
2023-11-06 11:03:20 +0100

    fs/squashfs: remove unused declarations
    This patch removes a number of struct and macro declaration that
    were found through `git-grep` to be unused. Most of those are
    related to compressor options and super block flags.
    
    For reading a SquashFS image, we do not need the compressor options
    or the flags. Those only encode settings used for packing the image,
    mksquashfs uses them when appending data to an existing image. The
    kernel implementation does not touch those, and we don't need them
    either.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- fs/squashfs/sqfs_utils.h
+++ fs/squashfs/sqfs_utils.h
@@ -15,11 +15,8 @@
 #define SQFS_FRAGMENT_INDEX_OFFSET(A) ((A) % SQFS_MAX_ENTRIES)
 #define SQFS_FRAGMENT_INDEX(A) ((A) / SQFS_MAX_ENTRIES)
 #define SQFS_BLOCK_SIZE(A) ((A) & GENMASK(23, 0))
-#define SQFS_CHECK_FLAG(flag, bit) (((flag) >> (bit)) & 1)
 /* Useful for both fragment and data blocks */
 #define SQFS_COMPRESSED_BLOCK(A) (!((A) & BIT(24)))
-/* SQFS_COMPRESSED_DATA strictly used with super block's 'flags' member */
-#define SQFS_COMPRESSED_DATA(A) (!((A) & 0x0002))
 #define SQFS_IS_FRAGMENTED(A) ((A) != 0xFFFFFFFF)
 /*
  * These two macros work as getters for a metada block header, retrieving the
@@ -27,23 +24,5 @@
  */
 #define SQFS_COMPRESSED_METADATA(A) (!((A) & BIT(15)))
 #define SQFS_METADATA_SIZE(A) ((A) & GENMASK(14, 0))
-
-struct squashfs_super_block_flags {
-	/* check: unused
-	 * uncompressed_ids: not supported
-	 */
-	bool uncompressed_inodes;
-	bool uncompressed_data;
-	bool check;
-	bool uncompressed_frags;
-	bool no_frags;
-	bool always_frags;
-	bool duplicates;
-	bool exportable;
-	bool uncompressed_xattrs;
-	bool no_xattrs;
-	bool compressor_options;
-	bool uncompressed_ids;
-};
 
 #endif /* SQFS_UTILS_H  */
