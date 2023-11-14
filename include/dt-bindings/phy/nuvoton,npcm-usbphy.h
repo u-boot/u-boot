438d25394353d7498559d18d72492c39cf2bc8b6
Jim Liu <jim.t90615@gmail.com>
2023-11-14 16:51:56 +0800

    arm: dts: npcm845-evb: fix/add node and aliases
    Modify spi and usb aliases name.
    Add dt-binding for usb phy define and fix usb phy reset error.
    Add tpm/otpee and host_intf node.
    
    Signed-off-by: Jim Liu <JJLIU0@nuvoton.com>

---
+++ include/dt-bindings/phy/nuvoton,npcm-usbphy.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+// Copyright (c) 2023 Nuvoton Technology corporation.
+
+#ifndef _DT_BINDINGS_NPCM_USBPHY_H
+#define _DT_BINDINGS_NPCM_USBPHY_H
+
+#define NPCM_UDC0_7		0
+#define NPCM_UDC8		1
+#define NPCM_UDC9		2
+#define NPCM_USBH1		3
+#define NPCM_USBH2		4
+#define NPCM_MAX_USB_CTRL_ID	4
+
+#endif
