u-boot/u-boot

commit 415eab0655a8bdfa07464e2b3f9724a198afc81f

smbios: add parsing API
Add a very simple API to be able to access SMBIOS strings
like vendor, model and bios version.

Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

lib/smbios-parser.c
@@ -1,0 +1,96 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2020, Bachmann electronic GmbH
+ */
+
+#include <common.h>
+#include <smbios.h>
+
+static inline int verify_checksum(const struct smbios_entry *e)
+{
+	/*
+	 * Checksums for SMBIOS tables are calculated to have a value, so that
+	 * the sum over all bytes yields zero (using unsigned 8 bit arithmetic).
+	 */
+	u8 *byte = (u8 *)e;
+	u8 sum = 0;
+
+	for (int i = 0; i < e->length; i++)
+		sum += byte[i];
+
+	return sum;
+}
+
+const struct smbios_entry *smbios_entry(u64 address, u32 size)
+{
+	const struct smbios_entry *entry = (struct smbios_entry *)(uintptr_t)address;
+
+	if (!address | !size)
+		return NULL;
+
+	if (memcmp(entry->anchor, "_SM_", 4))
+		return NULL;
+
+	if (verify_checksum(entry))
+		return NULL;
+
+	return entry;
+}
+
+static const struct smbios_header *next_header(const struct smbios_header *curr)
+{
+	u8 *pos = ((u8 *)curr) + curr->length;
+
+	/* search for _double_ NULL bytes */
+	while (!((*pos == 0) && (*(pos + 1) == 0)))
+		pos++;
+
+	/* step behind the double NULL bytes */
+	pos += 2;
+
+	return (struct smbios_header *)pos;
+}
+
+const struct smbios_header *smbios_header(const struct smbios_entry *entry, int type)
+{
+	const unsigned int num_header = entry->struct_count;
+	const struct smbios_header *header = (struct smbios_header *)entry->struct_table_address;
+
+	for (unsigned int i = 0; i < num_header; i++) {
+		if (header->type == type)
+			return header;
+
+		header = next_header(header);
+	}
+
+	return NULL;
+}
+
+static const char *string_from_smbios_table(const struct smbios_header *header,
+                                           int idx)
+{
+	unsigned int i = 1;
+	u8 *pos;
+
+	if (!header)
+		return NULL;
+
+	pos = ((u8 *)header) + header->length;
+
+	while (i < idx) {
+		if (*pos == 0x0)
+			i++;
+
+		pos++;
+	}
+
+	return (const char *)pos;
+}
+
+const char *smbios_string(const struct smbios_header *header, int index)
+{
+	if (!header)
+		return NULL;
+
+	return string_from_smbios_table(header, index);
+}

include/smbios.h
@@ -237,4 +237,31 @@ typedef int (*smbios_write_type)(ulong *addr, int handle);
  */
 ulong write_smbios_table(ulong addr);
 
+/**
+ * smbios_entry() - Get a valid struct smbios_entry pointer
+ *
+ * @address:   address where smbios tables is located
+ * @size:      size of smbios table
+ * @return:    NULL or a valid pointer to a struct smbios_entry
+ */
+const struct smbios_entry *smbios_entry(u64 address, u32 size);
+
+/**
+ * smbios_header() - Search for SMBIOS header type
+ *
+ * @entry:     pointer to a struct smbios_entry
+ * @type:      SMBIOS type
+ * @return:    NULL or a valid pointer to a struct smbios_header
+ */
+const struct smbios_header *smbios_header(const struct smbios_entry *entry, int type);
+
+/**
+ * smbios_string() - Return string from SMBIOS
+ *
+ * @header:    pointer to struct smbios_header
+ * @index:     string index
+ * @return:    NULL or a valid const char pointer
+ */
+const char *smbios_string(const struct smbios_header *header, int index);
+
 #endif /* _SMBIOS_H_ */

lib/Kconfig
@@ -680,6 +680,11 @@ config OID_REGISTRY
 	help
 	  Enable fast lookup object identifier registry.
 
+config SMBIOS_PARSER
+	bool "SMBIOS parser"
+	help
+	  A simple parser for SMBIOS data.
+
 source lib/efi/Kconfig
 source lib/efi_loader/Kconfig
 source lib/optee/Kconfig

lib/Makefile
@@ -37,6 +37,7 @@ obj-$(CONFIG_FIT) += fdtdec_common.o
 obj-$(CONFIG_TEST_FDTDEC) += fdtdec_test.o
 obj-$(CONFIG_GZIP_COMPRESSED) += gzip.o
 obj-$(CONFIG_GENERATE_SMBIOS_TABLE) += smbios.o
+obj-$(CONFIG_SMBIOS_PARSER) += smbios-parser.o
 obj-$(CONFIG_IMAGE_SPARSE) += image-sparse.o
 obj-y += ldiv.o
 obj-$(CONFIG_XXHASH) += xxhash.o

commit dc2fe5d84ea1f285fa0ad1052deee742c8f593e3

lib: smbios: verify_checksum() is duplicate
The function verify_checksum() duplicates what table_compute_checksum()
does. Replace it. table_compute_checksum() is always compiled.

Fixes: 415eab0655a8 ("smbios: add parsing API")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
Reviewed-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

lib/smbios-parser.c
@@ -6,21 +6,7 @@
 #define LOG_CATEGORY	LOGC_BOOT
 
 #include <smbios.h>
-
-static inline int verify_checksum(const struct smbios_entry *e)
-{
-	/*
-	 * Checksums for SMBIOS tables are calculated to have a value, so that
-	 * the sum over all bytes yields zero (using unsigned 8 bit arithmetic).
-	 */
-	u8 *byte = (u8 *)e;
-	u8 sum = 0;
-
-	for (int i = 0; i < e->length; i++)
-		sum += byte[i];
-
-	return sum;
-}
+#include <tables_csum.h>
 
 const struct smbios_entry *smbios_entry(u64 address, u32 size)
 {
@@ -32,7 +18,7 @@ const struct smbios_entry *smbios_entry(u64 address, u32 size)
 	if (memcmp(entry->anchor, "_SM_", 4))
 		return NULL;
 
-	if (verify_checksum(entry))
+	if (table_compute_checksum(entry, entry->length))
 		return NULL;
 
 	return entry;