3da3bb9636e9fca7d96e27a9300f83fb5b190396
Tim Lunn <tim@feathertop.org>
2024-01-24 14:25:59 +1100

    rockchip: Convert rv1126 to standard boot
    RV1126 soc appears to have been missed with the conversion of
    rockchip socs to standard boot.
    
    Remove remnants of distro boot for rv1126 common and the one
    existing board.
    
    Signed-off-by: Tim Lunn <tim@feathertop.org>
    Link: https://lore.kernel.org/all/20230407223645.v8.8.I4cf7708a1ba953b9abd81375d93af34665c7b251@changeid/
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- include/configs/rv1126_common.h
+++ include/configs/rv1126_common.h
@@ -28,7 +28,6 @@
 	"kernel_addr_r=0x02008000\0" \
 	"ramdisk_addr_r=0x0a200000\0"
 
-#include <config_distro_bootcmd.h>
 #define CFG_EXTRA_ENV_SETTINGS \
 	"fdt_high=0x0fffffff\0" \
 	"initrd_high=0x0fffffff\0" \
@@ -36,6 +35,6 @@
 	"partitions=" PARTS_DEFAULT \
 	ENV_MEM_LAYOUT_SETTINGS	 \
 	ROCKCHIP_DEVICE_SETTINGS \
-	BOOTENV
+	"boot_targets=" BOOT_TARGETS "\0"
 
 #endif /* __CONFIG_RV1126_COMMON_H */
