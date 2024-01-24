b062eef0755d1f16eeddda0675e0e526747221af
Enrico Leto <enrico.leto@siemens.com>
2024-01-24 15:43:55 +0100

    siemens: factoryset: use correct config for soc specific implementation
    Adding the capricorn board family some parts diverge from draco family.
    The switches used were not pertinent and need to be enhanced for each new
    board of the capricorn family. Replace them through the SOC name 'AM33XX'
    and 'IMX8'.
    
    Signed-off-by: Enrico Leto <enrico.leto@siemens.com>

--- board/siemens/common/factoryset.c
+++ board/siemens/common/factoryset.c
@@ -11,7 +11,7 @@
 #include <g_dnl.h>
 #include <net.h>
 #include <asm/io.h>
-#if !CONFIG_IS_ENABLED(TARGET_GIEDI) && !CONFIG_IS_ENABLED(TARGET_DENEB)
+#if CONFIG_IS_ENABLED(AM33XX)
 #include <asm/arch/cpu.h>
 #endif
 #include "eeprom.h"
@@ -197,7 +197,7 @@ int factoryset_read_eeprom(int i2c_addr)
 		cp1 += 3;
 	}
 
-#if CONFIG_IS_ENABLED(TARGET_GIEDI) || CONFIG_IS_ENABLED(TARGET_DENEB)
+#if CONFIG_IS_ENABLED(IMX8)
 	/* get mac address for WLAN */
 	if (get_factory_record_val(cp, size, (uchar *)"WLAN1", (uchar *)"mac",
 				   buf, MAX_STRING_LENGTH) > 0) {
@@ -304,7 +304,7 @@ static int factoryset_mac_env_set(void)
 
 	eth_env_set_enetaddr("ethaddr", mac_addr);
 
-#if CONFIG_IS_ENABLED(TARGET_GIEDI) || CONFIG_IS_ENABLED(TARGET_DENEB)
+#if CONFIG_IS_ENABLED(IMX8)
 	eth_env_set_enetaddr("eth1addr", mac_addr);
 
 	/* wlan mac */
