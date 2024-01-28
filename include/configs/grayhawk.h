53066deccbedf02439309b0d4aca9f0be853a8da
Hai Pham <hai.pham.ud@renesas.com>
2024-01-28 16:52:09 +0100

    ARM: renesas: Add Renesas R8A779H0 V4M Gray Hawk board code
    Add board code for the Renesas R8A779H0 V4M Gray Hawk board.
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

---
+++ include/configs/grayhawk.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/grayhawk.h
+ *     This file is Gray Hawk board configuration.
+ *
+ * Copyright (C) 2023 Renesas Electronics Corp.
+ */
+
+#ifndef __GRAYHAWK_H
+#define __GRAYHAWK_H
+
+#include "rcar-gen4-common.h"
+
+#endif /* __GRAYHAWK_H */
