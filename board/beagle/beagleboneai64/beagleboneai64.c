3982d5393747e8f27e4889890e79f04e0d87ecc5
Nishanth Menon <nm@ti.com>
2024-01-30 07:06:13 -0600

    board: beagle: beagleboneai64: Set fdtfile from C code instead of findfdt script
    Stop using the findfdt script and switch to setting the fdtfile from C
    code.
    
    Reviewed-by: Jonathan Humphreys <j-humphreys@ti.com>
    Reviewed-by: Roger Quadros <rogerq@kernel.org>
    Link: https://lore.kernel.org/r/20240130130615.670783-10-nm@ti.com
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- board/beagle/beagleboneai64/beagleboneai64.c
+++ board/beagle/beagleboneai64/beagleboneai64.c
@@ -28,3 +28,17 @@ int dram_init_banksize(void)
 {
 	return fdtdec_setup_memory_banksize();
 }
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+	char fdtfile[50];
+
+	snprintf(fdtfile, sizeof(fdtfile), "%s/%s.dtb",
+		 CONFIG_TI_FDT_FOLDER_PATH, CONFIG_DEFAULT_DEVICE_TREE);
+
+	env_set("fdtfile", fdtfile);
+
+	return 0;
+}
+#endif
