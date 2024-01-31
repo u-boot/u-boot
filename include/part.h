5306e103e03d33221b504aa036a1c4ca96e49c1e
Alexey Romanov <avromanov@salutedevices.com>
2024-01-31 16:22:57 +0300

    disk: support UBI partitions
    UBI partition is abstraction over UBI volumes.
    Can be used by UBI block device.
    
    Signed-off-by: Alexey Romanov <avromanov@salutedevices.com>
    Reviewed-by: Heiko Schocher <hs@denx.de>

--- include/part.h
+++ include/part.h
@@ -31,6 +31,7 @@ struct block_drvr {
 #define PART_TYPE_AMIGA		0x04
 #define PART_TYPE_EFI		0x05
 #define PART_TYPE_MTD		0x06
+#define PART_TYPE_UBI		0x07
 
 /* maximum number of partition entries supported by search */
 #define DOS_ENTRY_NUMBERS	8
@@ -38,6 +39,7 @@ struct block_drvr {
 #define MAC_ENTRY_NUMBERS	64
 #define AMIGA_ENTRY_NUMBERS	8
 #define MTD_ENTRY_NUMBERS	64
+#define UBI_ENTRY_NUMBERS	UBI_MAX_VOLUMES
 
 /*
  * Type string for U-Boot bootable partitions
