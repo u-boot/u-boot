a59fb3dcf0f2099dcb8975a189408f01a8facea7
Fabio Estevam <festevam@denx.de>
2024-02-13 08:43:38 -0300

    msc_sm2s_imx8mp: Convert to DM_SERIAL
    The conversion to DM_SERIAL is mandatory, so do the conversion.
    
    Signed-off-by: Fabio Estevam <festevam@denx.de>
    Reviewed-by: Ian Ray <ian.ray@gehealthcare.com>

--- board/msc/sm2s_imx8mp/spl.c
+++ board/msc/sm2s_imx8mp/spl.c
@@ -168,21 +168,12 @@ static const iomux_v3_cfg_t wdog_pads[] = {
 	MX8MP_PAD_GPIO1_IO02__WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
 };
 
-#define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
-
-static const iomux_v3_cfg_t ser0_pads[] = {
-	MX8MP_PAD_UART2_RXD__UART2_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
-	MX8MP_PAD_UART2_TXD__UART2_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
-};
-
 int board_early_init_f(void)
 {
 	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
 
 	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
 	set_wdog_reset(wdog);
-
-	imx_iomux_v3_setup_multiple_pads(ser0_pads, ARRAY_SIZE(ser0_pads));
 
 	return 0;
 }
