c60b835be28a9d6d7a03a64b42a922c08e62530d
Jonas Karlman <jonas@kwiboo.se>
2024-02-17 00:22:39 +0000

    rockchip: rk3328: Add support to build bootable SPI image
    Similar to RK35xx the BootRom in RK3328 can read all data and look for
    idbloader at 0x8000, same as it does for SD and eMMC.
    
    Use the rksd format and modify the mkimage offset to generate a bootable
    u-boot-rockchip-spi.bin that can be written to 0x0 of SPI NOR flash.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/mach-rockchip/rk3328/rk3328.c
+++ arch/arm/mach-rockchip/rk3328/rk3328.c
@@ -35,6 +35,7 @@
 
 const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
 	[BROM_BOOTSOURCE_EMMC] = "/mmc@ff520000",
+	[BROM_BOOTSOURCE_SPINOR] = "/spi@ff190000/flash@0",
 	[BROM_BOOTSOURCE_SD] = "/mmc@ff500000",
 };
 
