de5ef5d3b7d1bf5b678593da93def9ccf617cc83
Apurva Nandan <a-nandan@ti.com>
2024-02-24 01:51:46 +0530

    board: ti: j784s4: Add board support for J784S4 EVM
    Add board files for J784S4 EVM.
    
    SYS_DISABLE_DCACHE_OPS is selected in the Kconfig because
    J784S4/AM69 are a coherent architecture at A72 level by
    MSMC support.
    
    Signed-off-by: Hari Nagalla <hnagalla@ti.com>
    Signed-off-by: Dasnavis Sabiya <sabiya.d@ti.com>
    Signed-off-by: Apurva Nandan <a-nandan@ti.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Tested-by: Marcel Ziswiler <marcel.ziswiler@toradex.com> # AM69-SK

---
+++ board/ti/j784s4/evm.c
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Board specific initialization for J784S4 EVM
+ *
+ * Copyright (C) 2023-2024 Texas Instruments Incorporated - https://www.ti.com/
+ *	Hari Nagalla <hnagalla@ti.com>
+ *
+ */
+
+#include <init.h>
+#include <spl.h>
+#include "../common/fdt_ops.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	return fdtdec_setup_mem_size_base();
+}
+
+int dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+	ti_set_fdt_env(NULL, NULL);
+	return 0;
+}
+#endif
+
+void spl_board_init(void)
+{
+}
---
+++ include/configs/j784s4_evm.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Configuration header file for K3 J784S4 EVM
+ *
+ * Copyright (C) 2023-2024 Texas Instruments Incorporated - https://www.ti.com/
+ *	Hari Nagalla <hnagalla@ti.com>
+ */
+
+#ifndef __CONFIG_J784S4_EVM_H
+#define __CONFIG_J784S4_EVM_H
+
+/* Now for the remaining common defines */
+#include <configs/ti_armv7_common.h>
+
+#endif /* __CONFIG_J784S4_EVM_H */
