f252dfbf4611e813e4e7dc95222797ab57f11fee
Marek Vasut <marek.vasut+renesas@mailbox.org>
2024-02-27 17:05:57 +0100

    ARM: renesas: Post rename fix ups
    Fix up remains of rename from rmobile to renesas,
    namely file paths and remaining RMOBILE strings.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    Reviewed-by: Paul Barker <paul.barker.ct@bp.renesas.com>

--- arch/arm/mach-renesas/cpu_info-rcar.c
+++ arch/arm/mach-renesas/cpu_info-rcar.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * arch/arm/cpu/armv7/rmobile/cpu_info-rcar.c
+ * arch/arm/mach-renesas/cpu_info-rcar.c
  *
  * Copyright (C) 2013,2014 Renesas Electronics Corporation
  */
