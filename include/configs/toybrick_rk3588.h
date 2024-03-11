9fdd9a546986838caf52d03a0cd21029085ce1b7
Elon Zhang <zhangzj@rock-chips.com>
2024-03-11 11:57:33 +0800

    board: rockchip: add Rockchip Toybrick TB-RK3588X board
    TB-RK3588X board is a Rockchip Toybrick RK3588 based development board.
    
    Specification:
    	Rockchip Rk3588 SoC
    	4x ARM Cortex-A76, 4x ARM Cortex-A55
    	8/16GB Memory LPDDR4x
    	Mali G610MC4 GPU
    	2× MIPI-CSI0 Connector
    	1x 2Lanes PCIe3.0 Connector
    	1x SATA3.0 Connector
    	32GB eMMC Module
    	2x USB 2.0, 2x USB 3.0
    	1x HDMI Output, 1x HDMI Input
    	2x Ethernet Port
    
    Functions work normally:
    	[1] USB2.0 Host
    	[2] Ethernet0 with PHY RTL8211F
    
    More information can be obtained from the following websites:
    	[1] https://t.rock-chips.com/en/wiki/EN/tb-rk3588x_en/index.html
    	[2] http://t.rock-chips.com/
    
    Kernel commits:
    	8ffe365f8dc7 ("arm64: dts: rockchip: Add devicetree support for TB-RK3588X board")
    	7140387ff49d ("dt-bindings: arm: rockchip: Add Toybrick TB-RK3588X")
    
    Reviewed-by: Weizhao Ouyang <weizhao.ouyang@arm.com>
    Signed-off-by: Elon Zhang <zhangzj@rock-chips.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

---
+++ include/configs/toybrick_rk3588.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ */
+
+#ifndef __TOYBRICK_RK3588_H
+#define __TOYBRICK_RK3588_H
+
+#include <configs/rk3588_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#endif
