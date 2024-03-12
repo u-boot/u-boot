95a9cb3666c4a513d03a64b7233ab32bfb042f0a
Jonas Karlman <jonas@kwiboo.se>
2024-03-12 23:36:20 +0000

    rockchip: theobroma-systems: puma: Migrate to use IO-domain driver
    Switch to use the IO-domain driver to configure IO-domain based on
    device tree instead of a setup_iodomain() function.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- board/theobroma-systems/puma_rk3399/puma-rk3399.c
+++ board/theobroma-systems/puma_rk3399/puma-rk3399.c
@@ -3,31 +3,10 @@
  * (C) Copyright 2017 Theobroma Systems Design und Consulting GmbH
  */
 
-#include <dm.h>
-#include <syscon.h>
-#include <dm/pinctrl.h>
-#include <asm/arch-rockchip/clock.h>
-#include <asm/arch-rockchip/hardware.h>
-#include <asm/arch-rockchip/grf_rk3399.h>
 #include "../common/common.h"
-
-static void setup_iodomain(void)
-{
-	const u32 GRF_IO_VSEL_GPIO4CD_SHIFT = 3;
-	struct rk3399_grf_regs *grf =
-	    syscon_get_first_range(ROCKCHIP_SYSCON_GRF);
-
-	/*
-	 * Set bit 3 in GRF_IO_VSEL so PCIE_RST# works (pin GPIO4_C6).
-	 * Linux assumes that PCIE_RST# works out of the box as it probes
-	 * PCIe before loading the iodomain driver.
-	 */
-	rk_setreg(&grf->io_vsel, 1 << GRF_IO_VSEL_GPIO4CD_SHIFT);
-}
 
 int rockchip_early_misc_init_r(void)
 {
-	setup_iodomain();
 	setup_boottargets();
 
 	return 0;
