c78210fce75af9373022ab11bb547f46bebd12b8
Caleb Connolly <caleb.connolly@linaro.org>
2024-04-08 15:06:49 +0200

    clk/qcom: add driver for qcm2290 GCC
    Add a clock driver for the QCM2290 SoC which is used in the QRB2210 RB1
    board.
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- drivers/clk/qcom/clock-qcom.h
+++ drivers/clk/qcom/clock-qcom.h
@@ -9,6 +9,9 @@
 
 #define CFG_CLK_SRC_CXO   (0 << 8)
 #define CFG_CLK_SRC_GPLL0 (1 << 8)
+#define CFG_CLK_SRC_GPLL0_AUX2 (2 << 8)
+#define CFG_CLK_SRC_GPLL6 (4 << 8)
+#define CFG_CLK_SRC_GPLL7 (3 << 8)
 #define CFG_CLK_SRC_GPLL0_EVEN (6 << 8)
 #define CFG_CLK_SRC_MASK  (7 << 8)
 
