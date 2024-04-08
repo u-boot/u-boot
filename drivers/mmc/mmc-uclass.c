e098f88f7e8e3c72c05cd9a3cf586a9d70374ba7
Jonas Karlman <jonas@kwiboo.se>
2024-04-08 21:06:15 +0000

    mmc: Imply HS200 cap with mmc-hs400 prop to match linux
    eMMC nodes in linux device tree files typically only contain a mmc-hs400
    prop to signal support for both HS400 and HS200. However, U-Boot require
    an explicit mmc-hs200 prop to signal support for the HS200 mode.
    
    Fix this by follow linux and imply HS200 cap when HS400 cap is signaled
    using a mmc-hs400 prop.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Dragan Simic <dsimic@manjaro.org>
    Reviewed-by: Quentin Schulz <quentin.schulz@theobrma-systems.com>
    Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>

--- drivers/mmc/mmc-uclass.c
+++ drivers/mmc/mmc-uclass.c
@@ -257,9 +257,9 @@ int mmc_of_parse(struct udevice *dev, struct mmc_config *cfg)
 	if (dev_read_bool(dev, "mmc-hs200-1_2v"))
 		cfg->host_caps |= MMC_CAP(MMC_HS_200);
 	if (dev_read_bool(dev, "mmc-hs400-1_8v"))
-		cfg->host_caps |= MMC_CAP(MMC_HS_400);
+		cfg->host_caps |= MMC_CAP(MMC_HS_400) | MMC_CAP(MMC_HS_200);
 	if (dev_read_bool(dev, "mmc-hs400-1_2v"))
-		cfg->host_caps |= MMC_CAP(MMC_HS_400);
+		cfg->host_caps |= MMC_CAP(MMC_HS_400) | MMC_CAP(MMC_HS_200);
 	if (dev_read_bool(dev, "mmc-hs400-enhanced-strobe"))
 		cfg->host_caps |= MMC_CAP(MMC_HS_400_ES);
 
