3c1bc9f15c18149a26fbd7f604f76f949a29bc7d
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2024-04-09 20:04:56 +0200

    fs: fat: convert change month correctly
    The month is stored in 5 - 8. We need to shift it by 5 bits.
    
    Cf. Microsoft FAT Specification, 2005-08-30
    
    Fixes: 13c11c665320 ("fs: fat: add file attributes to struct fs_dirent")
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-by: Alexander Dahl <ada@thorsis.com>
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- fs/fat/fat.c
+++ fs/fat/fat.c
@@ -1254,7 +1254,7 @@ out:
 static void __maybe_unused fat2rtc(u16 date, u16 time, struct rtc_time *tm)
 {
 	tm->tm_mday = date & 0x1f;
-	tm->tm_mon = (date & 0x1e0) >> 4;
+	tm->tm_mon = (date & 0x1e0) >> 5;
 	tm->tm_year = (date >> 9) + 1980;
 
 	tm->tm_sec = (time & 0x1f) << 1;
