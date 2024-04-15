8c352a61eef6a8a2d7ffc4159a499f78f5fae99c
Fabio Estevam <festevam@denx.de>
2024-04-15 18:57:17 -0300

    imx93: Move SoC and lifeclycle information to debug level
    The following information printed on every boot is not very
    helpful for the users:
    
    SOC: 0xa0009300
    LC: 0x40040
    
    Move them to debug() level.
    
    Signed-off-by: Fabio Estevam <festevam@denx.de>
    Reviewed-by: Mathieu Othacehe <othacehe@gnu.org>

--- board/phytec/phycore_imx93/spl.c
+++ board/phytec/phycore_imx93/spl.c
@@ -126,8 +126,8 @@ void board_init_f(ulong dummy)
 	if (ret) {
 		printf("Fail to init ELE API\n");
 	} else {
-		printf("SOC: 0x%x\n", gd->arch.soc_rev);
-		printf("LC: 0x%x\n", gd->arch.lifecycle);
+		debug("SOC: 0x%x\n", gd->arch.soc_rev);
+		debug("LC: 0x%x\n", gd->arch.lifecycle);
 	}
 
 	clock_init();
