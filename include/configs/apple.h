9d189ab3f94080839c014829b8087de028d5c3b4
Janne Grunau <j@jannau.net>
2024-04-18 21:00:26 +0200

    configs: apple: Use "vidconsole,serial" as stdout/stderr
    The display size querying in efi_console relies on this order. The
    display should be the primary output device and should be used to
    display more than 80x25 chars.
    
    Reviewed-by: Mark Kettenis <kettenis@openbsd.org>
    Reviewed-by: Neal Gompa <neal@gompa.dev>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- include/configs/apple.h
+++ include/configs/apple.h
@@ -6,8 +6,8 @@
 /* Environment */
 #define ENV_DEVICE_SETTINGS \
 	"stdin=serial,usbkbd,spikbd\0" \
-	"stdout=serial,vidconsole\0" \
-	"stderr=serial,vidconsole\0"
+	"stdout=vidconsole,serial\0" \
+	"stderr=vidconsole,serial\0"
 
 #if IS_ENABLED(CONFIG_CMD_NVME)
 	#define BOOT_TARGET_NVME(func) func(NVME, nvme, 0)
