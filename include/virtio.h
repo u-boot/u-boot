3ba56d6b232f7dcbf00552af48355f0dc2d26994
Tom Rini <trini@konsulko.com>
2024-05-01 19:31:32 -0600

    virtio: Remove <common.h> and add needed includes
    Remove <common.h> from this driver directory and when needed
    add missing include files directly.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/virtio.h
+++ include/virtio.h
@@ -23,6 +23,7 @@
 #include <virtio_types.h>
 #include <linux/bitops.h>
 #include <linux/bug.h>
+#include <linux/typecheck.h>
 #define VIRTIO_ID_NET		1 /* virtio net */
 #define VIRTIO_ID_BLOCK		2 /* virtio block */
 #define VIRTIO_ID_RNG		4 /* virtio rng */
