0fd4686df5fe7a7b1783eec6ff21ffdd1c2b0467
Jiaxun Yang <jiaxun.yang@flygoat.com>
2024-05-17 19:14:54 +0100

    MIPS: Provide dummy acpi_table.h
    Some drivers need this header.
    Provide this dummy header as riscv did.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Reviewed-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ arch/mips/include/asm/acpi_table.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef __ASM_ACPI_TABLE_H__
+#define __ASM_ACPI_TABLE_H__
+
+/*
+ * This file is needed by some drivers.
+ */
+
+#endif /* __ASM_ACPI_TABLE_H__ */
