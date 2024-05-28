76a24b0f106f18b40f17295eb9570aed2002cca4
Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
2024-05-28 11:59:39 +0200

    toradex: tdx-cfg-block: add aquila am69 sku 0088 pid4
    Add new PID4 0088 Aquila AM69 Octa 32GB WB IT to config block handling.
    
    Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

--- board/toradex/common/tdx-cfg-block.c
+++ board/toradex/common/tdx-cfg-block.c
@@ -158,6 +158,7 @@ const struct toradex_som toradex_modules[] = {
 	[85] = { "Apalis iMX6Q 2GB IT",                  TARGET_IS_ENABLED(APALIS_IMX6)     },
 	[86] = { "Verdin iMX8M Mini DualLite 2GB IT",    TARGET_IS_ENABLED(VERDIN_IMX8MM)   },
 	[87] = { "Verdin iMX8M Mini Quad 2GB IT",        TARGET_IS_ENABLED(VERDIN_IMX8MM)   },
+	[88] = { "Aquila AM69 Octa 32GB WB IT",          TARGET_IS_ENABLED(AQUILA_AM69_A72) },
 };
 
 struct pid4list {
