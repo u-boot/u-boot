95cf918f16ddaf7e6e4b1b4dfacecb9d9609d6f7
Benjamin Hahn <B.Hahn@phytec.de>
2024-05-28 15:35:15 +0200

    board: phycore_imx8mp: enable setting 2GHz timings without RAM size
    make it possible to set the RAM timing frequency statically independent
    from the RAM size. Fixed RAM timing frequency can be used while the
    RAM size is still determined by the EEPROM image.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

--- board/phytec/phycore_imx8mp/spl.c
+++ board/phytec/phycore_imx8mp/spl.c
@@ -65,11 +65,21 @@ void spl_dram_init(void)
 			size = PHYTEC_IMX8MP_DDR_4GB;
 		else if (IS_ENABLED(CONFIG_PHYCORE_IMX8MP_RAM_SIZE_8GB))
 			size = PHYTEC_IMX8MP_DDR_8GB;
+	} else {
+		size = phytec_get_imx8m_ddr_size(NULL);
+	}
+
+	if (IS_ENABLED(CONFIG_PHYCORE_IMX8MP_RAM_FREQ_FIX)) {
 		if (IS_ENABLED(CONFIG_PHYCORE_IMX8MP_USE_2GHZ_RAM_TIMINGS)) {
 			if (size == PHYTEC_IMX8MP_DDR_4GB)
 				size = PHYTEC_IMX8MP_DDR_4GB_2GHZ;
 			else
 				use_2ghz_timings = true;
+		} else if (IS_ENABLED(CONFIG_PHYCORE_IMX8MP_USE_1_5GHZ_RAM_TIMINGS)) {
+			if (size == PHYTEC_IMX8MP_DDR_4GB_2GHZ)
+				size = PHYTEC_IMX8MP_DDR_4GB;
+			else
+				use_2ghz_timings = false;
 		}
 	} else {
 		u8 rev = phytec_get_rev(NULL);
@@ -78,8 +88,6 @@ void spl_dram_init(void)
 		if (rev != PHYTEC_EEPROM_INVAL &&
 		    (rev >= 3 || (somtype == SOM_TYPE_PCL && rev >= 1)))
 			use_2ghz_timings = true;
-
-		size = phytec_get_imx8m_ddr_size(NULL);
 	}
 
 	switch (size) {
