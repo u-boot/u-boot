ce53f46f33b43f550e47ca50d7827f5b7b6d2ca5
Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
2024-05-28 11:59:40 +0200

    toradex: tdx-cfg-block: add verdin imx95 sku 0089 pid4
    Add new PID4 0089 Verdin iMX95 Hexa 16GB WB IT to config block handling.
    
    Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

--- board/toradex/common/tdx-cfg-block.c
+++ board/toradex/common/tdx-cfg-block.c
@@ -159,6 +159,7 @@ const struct toradex_som toradex_modules[] = {
 	[86] = { "Verdin iMX8M Mini DualLite 2GB IT",    TARGET_IS_ENABLED(VERDIN_IMX8MM)   },
 	[87] = { "Verdin iMX8M Mini Quad 2GB IT",        TARGET_IS_ENABLED(VERDIN_IMX8MM)   },
 	[88] = { "Aquila AM69 Octa 32GB WB IT",          TARGET_IS_ENABLED(AQUILA_AM69_A72) },
+	[89] = { "Verdin iMX95 Hexa 16GB WB IT",         TARGET_IS_ENABLED(VERDIN_IMX95)    },
 };
 
 struct pid4list {
