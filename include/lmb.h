c6da678dde63c86739ee454bbaa27ff99f4e9b51
Sughosh Ganu <sughosh.ganu@linaro.org>
2024-06-21 12:43:40 +0530

    lmb: introduce a function to add memory to the lmb memory map
    Introduce a function lmb_add_memory() to add available memory to the
    LMB memory map. Call this function during board init once the LMB data
    structures have been initialised.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- include/lmb.h
+++ include/lmb.h
@@ -63,6 +63,16 @@ struct lmb {
  */
 int lmb_init(void);
 
+/**
+ * lmb_add_memory() - Add memory range for LMB allocations
+ *
+ * Add the entire available memory range to the pool of memory that
+ * can be used by the LMB module for allocations.
+ *
+ * Return: None
+ */
+void lmb_add_memory(void);
+
 long lmb_add(phys_addr_t base, phys_size_t size);
 long lmb_reserve(phys_addr_t base, phys_size_t size);
 /**
