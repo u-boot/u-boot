34bb7ac21a0af63697aa0b7f6f9f1ced2419d516
Sughosh Ganu <sughosh.ganu@linaro.org>
2024-06-21 12:50:43 +0530

    lmb: remove the lmb_init_and_reserve() function
    With the changes to make the LMB reservations persistent, the common
    memory regions are being added during board init. Remove the
    now superfluous lmb_init_and_reserve() function.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- include/lmb.h
+++ include/lmb.h
@@ -51,7 +51,6 @@ struct lmb {
 	bool test;
 };
 
-void lmb_init_and_reserve(struct bd_info *bd, void *fdt_blob);
 void lmb_init_and_reserve_range(phys_addr_t base, phys_size_t size,
 				void *fdt_blob);
 long lmb_add(phys_addr_t base, phys_size_t size);
