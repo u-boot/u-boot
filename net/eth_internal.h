fb2f2a38b6c52d8f80d7b0a0589fd0fb93c3973c
Alexander Sverdlin <alexander.sverdlin@siemens.com>
2024-07-08 11:09:58 +0200

    net: remove duplicate eth_env_set_enetaddr_by_index() declaration
    eth_env_set_enetaddr_by_index() declaration is duplicated in eth_internal.h
    and net.h, but all units including eth_internal.h already include net.h.
    Remove the superfluous declaration.
    
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@siemens.com>

--- net/eth_internal.h
+++ net/eth_internal.h
@@ -11,22 +11,6 @@
 /* Do init that is common to driver model and legacy networking */
 void eth_common_init(void);
 
-/**
- * eth_env_set_enetaddr_by_index() - set the MAC address environment variable
- *
- * This sets up an environment variable with the given MAC address (@enetaddr).
- * The environment variable to be set is defined by <@base_name><@index>addr.
- * If @index is 0 it is omitted. For common Ethernet this means ethaddr,
- * eth1addr, etc.
- *
- * @base_name:	Base name for variable, typically "eth"
- * @index:	Index of interface being updated (>=0)
- * @enetaddr:	Pointer to MAC address to put into the variable
- * Return: 0 if OK, other value on error
- */
-int eth_env_set_enetaddr_by_index(const char *base_name, int index,
-				 uchar *enetaddr);
-
 int eth_mac_skip(int index);
 void eth_current_changed(void);
 void eth_set_dev(struct udevice *dev);
