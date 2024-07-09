f6503fad0c97c6daa33ae7a290520ac4418cf386
Adam Ford <aford173@gmail.com>
2024-07-08 21:38:57 -0500

    ti: omap: am3517evm: Move environment definition to env file
    Instead of cluttering up a header file with a bunch of defines,
    move the default environmental variables to a file called
    am3517evm.env and reference it from the defconfig.  Also
    remove dead comments.
    
    Signed-off-by: Adam Ford <aford173@gmail.com>

--- include/configs/am3517_evm.h
+++ include/configs/am3517_evm.h
@@ -15,7 +15,7 @@
 #include <configs/ti_omap3_common.h>
 
 /* Board NAND Info. */
-#ifdef CONFIG_MTD_RAW_NAND
+#if defined(CONFIG_MTD_RAW_NAND)
 #define CFG_SYS_NAND_ECCPOS		{ 2,  3,  4,  5,  6,  7,  8,  9, 10, \
 					 11, 12, 13, 14, 16, 17, 18, 19, 20, \
 					 21, 22, 23, 24, 25, 26, 27, 28, 30, \
@@ -35,61 +35,8 @@
  *  DTB                  4 * NAND_BLOCK_SIZE = 512 KiB  @ 0xAA0000
  *  RootFS              Remaining Flash Space           @ 0xB20000
  */
-#endif /* CONFIG_MTD_RAW_NAND */
 
-/* Environment information */
-#define CFG_EXTRA_ENV_SETTINGS \
-	"loadaddr=0x82000000\0" \
-	"console=ttyS2,115200n8\0" \
-	"fdtfile=am3517-evm.dtb\0" \
-	"fdtaddr=0x82C00000\0" \
-	"vram=16M\0" \
-	"bootenv=uEnv.txt\0" \
-	"cmdline=\0" \
-	"optargs=\0" \
-	"mmcdev=0\0" \
-	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk0p2 rw\0" \
-	"mmcrootfstype=ext4 rootwait fixrtc\0" \
-	"mmcargs=setenv bootargs console=${console} " \
-		"${mtdparts} " \
-		"${optargs} " \
-		"root=${mmcroot} " \
-		"rootfstype=${mmcrootfstype} " \
-		"${cmdline}\0" \
-	"nandargs=setenv bootargs console=${console} " \
-		"${mtdparts} " \
-		"${optargs} " \
-		"root=ubi0:rootfs rw ubi.mtd=rootfs " \
-		"rootfstype=ubifs rootwait " \
-		"${cmdline}\0" \
-	"loadbootenv=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootenv}\0"\
-	"importbootenv=echo Importing environment from mmc ...; " \
-		"env import -t ${loadaddr} ${filesize}\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source ${loadaddr}\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootfile}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdtaddr} ${fdtfile}\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run mmcargs; " \
-		"bootz ${loadaddr} - ${fdtaddr}\0" \
-	"nandboot=echo Booting from nand ...; " \
-		"run nandargs; " \
-		"nand read ${loadaddr} 2a0000 800000; " \
-		"nand read ${fdtaddr} aa0000 80000; " \
-		"bootm ${loadaddr} - ${fdtaddr}\0" \
-
-/* Miscellaneous configurable options */
-
-/* memtest works on */
-
-/* FLASH and environment organization */
-
-/* **** PISMO SUPPORT *** */
-						/* on one chip */
-
-#if defined(CONFIG_MTD_RAW_NAND)
 #define CFG_SYS_FLASH_BASE		NAND_BASE
-#endif
+#endif /* CONFIG_MTD_RAW_NAND */
 
 #endif /* __CONFIG_H */
