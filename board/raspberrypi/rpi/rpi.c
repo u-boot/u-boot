c06687aa0649d1ecdf42f3d09e2612b228554b4b
Francois Berder <fberder@outlook.fr>
2024-07-09 14:20:48 +0200

    board: raspberrypi: Fix format specifier for printing rev_scheme
    rev_scheme is an unsigned integer and must not be printed
    as a signed integer.
    
    Signed-off-by: Francois Berder <fberder@outlook.fr>
    Reviewed-by: Peter Robinson <pbrobinson@gmail.com>

--- board/raspberrypi/rpi/rpi.c
+++ board/raspberrypi/rpi/rpi.c
@@ -382,7 +382,7 @@ static void set_board_info(void)
 
 	snprintf(s, sizeof(s), "0x%X", revision);
 	env_set("board_revision", s);
-	snprintf(s, sizeof(s), "%d", rev_scheme);
+	snprintf(s, sizeof(s), "%u", rev_scheme);
 	env_set("board_rev_scheme", s);
 	/* Can't rename this to board_rev_type since it's an ABI for scripts */
 	snprintf(s, sizeof(s), "0x%X", rev_type);
