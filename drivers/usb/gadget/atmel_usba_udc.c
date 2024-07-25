9cfee49022e2f22ca7074d423bd23a610ed652f4
Zixun LI <admin@hifiphile.com>
2024-07-25 17:31:55 +0200

    usb: gadget: atmel: Sort includes
    Sort includes in alphabetical order.
    
    Signed-off-by: Zixun LI <admin@hifiphile.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Link: https://lore.kernel.org/r/20240725153204.358925-2-admin@hifiphile.com
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- drivers/usb/gadget/atmel_usba_udc.c
+++ drivers/usb/gadget/atmel_usba_udc.c
@@ -7,16 +7,16 @@
  *			   Bo Shen <voice.shen@atmel.com>
  */
 
-#include <linux/bitops.h>
-#include <linux/errno.h>
+#include <malloc.h>
 #include <asm/gpio.h>
 #include <asm/hardware.h>
+#include <linux/bitops.h>
+#include <linux/errno.h>
 #include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
 #include <linux/usb/atmel_usba_udc.h>
-#include <malloc.h>
 
 #include "atmel_usba_udc.h"
 
