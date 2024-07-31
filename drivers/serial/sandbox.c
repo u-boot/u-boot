521d377f7852c037e338c66756982084b1cb0132
Simon Glass <sjg@chromium.org>
2024-07-31 08:44:12 -0600

    sandbox: Drop video-sync in serial driver
    With sandbox, when U-Boot is waiting for input it syncs the video
    display, since presumably the user has finished typing.
    
    Now that cyclic is used for video syncing, we can drop this. Cyclic
    will automatically call the video_idle() function when idle.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/serial/sandbox.c
+++ drivers/serial/sandbox.c
@@ -138,8 +138,6 @@ static int sandbox_serial_pending(struct udevice *dev, bool input)
 		return 0;
 
 	os_usleep(100);
-	if (IS_ENABLED(CONFIG_VIDEO) && !IS_ENABLED(CONFIG_SPL_BUILD))
-		video_sync_all();
 	avail = membuff_putraw(&priv->buf, 100, false, &data);
 	if (!avail)
 		return 1;	/* buffer full */
