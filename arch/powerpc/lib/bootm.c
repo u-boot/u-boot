8f58264529e818bda02c51ac03d0997e2971ed09
Sughosh Ganu <sughosh.ganu@linaro.org>
2024-08-16 16:38:22 +0530

    ppc: lmb: move arch specific lmb reservations to arch_misc_init()
    All the current function definitions of arch_lmb_reserve() are doing
    the same thing -- reserve the U-Boot memory region. The powerpc(ppc)
    architecture, in addition, is making some LMB reservations for the
    bootm related image loading. Move these ppc specific reservations to
    the arch_misc_init() function. This allows to move the U-Boot memory
    region reservation to a different function, and remove
    arch_lmb_reserve() in a subsequent commit.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- arch/powerpc/lib/bootm.c
+++ arch/powerpc/lib/bootm.c
@@ -12,7 +12,6 @@
 #include <cpu_func.h>
 #include <env.h>
 #include <init.h>
-#include <lmb.h>
 #include <log.h>
 #include <watchdog.h>
 #include <command.h>
@@ -40,10 +39,6 @@ DECLARE_GLOBAL_DATA_PTR;
 static ulong get_sp (void);
 extern void ft_fixup_num_cores(void *blob);
 static void set_clocks_in_mhz (struct bd_info *kbd);
-
-#ifndef CFG_SYS_LINUX_LOWMEM_MAX_SIZE
-#define CFG_SYS_LINUX_LOWMEM_MAX_SIZE	(768*1024*1024)
-#endif
 
 static void boot_jump_linux(struct bootm_headers *images)
 {
@@ -113,41 +108,6 @@ static void boot_jump_linux(struct bootm_headers *images)
 			   cmd_start, cmd_end, 0, 0);
 		/* does not return */
 	}
-	return;
-}
-
-void arch_lmb_reserve(void)
-{
-	phys_size_t bootm_size;
-	ulong size, bootmap_base;
-
-	bootmap_base = env_get_bootm_low();
-	bootm_size = env_get_bootm_size();
-
-#ifdef DEBUG
-	if (((u64)bootmap_base + bootm_size) >
-	    (CFG_SYS_SDRAM_BASE + (u64)gd->ram_size))
-		puts("WARNING: bootm_low + bootm_size exceed total memory\n");
-	if ((bootmap_base + bootm_size) > get_effective_memsize())
-		puts("WARNING: bootm_low + bootm_size exceed eff. memory\n");
-#endif
-
-	size = min(bootm_size, get_effective_memsize());
-	size = min(size, (ulong)CFG_SYS_LINUX_LOWMEM_MAX_SIZE);
-
-	if (size < bootm_size) {
-		ulong base = bootmap_base + size;
-		printf("WARNING: adjusting available memory from 0x%lx to 0x%llx\n",
-		       size, (unsigned long long)bootm_size);
-		lmb_reserve(base, bootm_size - size);
-	}
-
-	arch_lmb_reserve_generic(get_sp(), gd->ram_top, 4096);
-
-#ifdef CONFIG_MP
-	cpu_mp_lmb_reserve();
-#endif
-
 	return;
 }
 
