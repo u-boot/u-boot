u-boot/u-boot

commit db61e70e0d2ab94f572a5ec0900c7c51fb686177

efi_loader: efi_dp_from_lo() should skip VenMedia node
The 'efidebug boot dump' command should not display the VenMedia() device
path node preceding the device path of the initial ram disk.

By letting efi_dp_from_lo() skip the VenMedia() device path node we can
simplify the coding.

Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

lib/efi_loader/efi_helper.c
@@ -68,10 +68,8 @@ out:
  */
 struct efi_device_path *efi_get_dp_from_boot(const efi_guid_t guid)
 {
-	struct efi_device_path *file_path = NULL;
-	struct efi_device_path *tmp = NULL;
 	struct efi_load_option lo;
-	void *var_value = NULL;
+	void *var_value;
 	efi_uintn_t size;
 	efi_status_t ret;
 	u16 var_name[16];
@@ -86,18 +84,11 @@ struct efi_device_path *efi_get_dp_from_boot(const efi_guid_t guid)
 
 	ret = efi_deserialize_load_option(&lo, var_value, &size);
 	if (ret != EFI_SUCCESS)
-		goto out;
+		goto err;
 
-	tmp = efi_dp_from_lo(&lo, &guid);
-	if (!tmp)
-		goto out;
+	return efi_dp_from_lo(&lo, &guid);
 
-	/* efi_dp_dup will just return NULL if efi_dp_next is NULL */
-	file_path = efi_dp_dup(efi_dp_next(tmp));
-
-out:
-	efi_free_pool(tmp);
+err:
 	free(var_value);
-
-	return file_path;
+	return NULL;
 }

lib/efi_loader/efi_device_path.c
@@ -1233,7 +1233,7 @@ efi_device_path *efi_dp_from_lo(struct efi_load_option *lo,
 
 		vendor = (struct efi_device_path_vendor *)fp;
 		if (!guidcmp(&vendor->guid, guid))
-			return efi_dp_dup(fp);
+			return efi_dp_dup(efi_dp_next(fp));
 	}
 	log_debug("VenMedia(%pUl) not found in %ls\n", &guid, lo->label);
 

commit 3a8ad050aa8b1663316952d1d4bebdb5f4911160

efi_loader: fix memory freeing in efi_get_dp_from_boot()
efi_get_var() allocates memory which must be freed after the variable is
used. Since the device path is duplicated after we deserialize the load
options free the memory used for the variable payload

Fixes: db61e70e0d2a efi_loader: efi_dp_from_lo() should skip VenMedia node
Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
Reviewed-by: Heinric Schuchardt <heinrich.schuchardt@canonical.com>

lib/efi_loader/efi_helper.c
@@ -74,6 +74,7 @@ out:
  */
 struct efi_device_path *efi_get_dp_from_boot(const efi_guid_t *guid)
 {
+	struct efi_device_path *file_path = NULL;
 	struct efi_load_option lo;
 	void *var_value;
 	efi_uintn_t size;
@@ -92,11 +93,11 @@ struct efi_device_path *efi_get_dp_from_boot(const efi_guid_t *guid)
 	if (ret != EFI_SUCCESS)
 		goto err;
 
-	return efi_dp_from_lo(&lo, guid);
+	file_path = efi_dp_from_lo(&lo, guid);
 
 err:
 	free(var_value);
-	return NULL;
+	return file_path;
 }
 
 /**