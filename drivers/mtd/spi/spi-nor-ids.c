aa7d0f575f34938b8111aaaa375f30b2945cd652
Marek Vasut <marex@denx.de>
2024-09-06 23:10:10 +0200

    mtd: spi-nor-ids: Deduplicate mx25u25635f entry
    The mx25u25635f entry exists twice in spi_nor_ids, remove the less
    complete variant of the entry and keep only one copy of it.
    
    Fixes: f0084f1dfdbc ("drivers/mtd/spi/spi-nor-ids.c: add mx25u25635f support")
    Signed-off-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Michal Simek <michal.simek@amd.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@linaro.org>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -258,7 +258,6 @@ const struct flash_info spi_nor_ids[] = {
 	{ INFO("mx25u6435f",  0xc22537, 0, 64 * 1024, 128, SECT_4K) },
 	{ INFO("mx25l12805d", 0xc22018, 0, 64 * 1024, 256, SECT_4K) },
 	{ INFO("mx25u12835f", 0xc22538, 0, 64 * 1024, 256, SECT_4K) },
-	{ INFO("mx25u25635f", 0xc22539, 0, 64 * 1024, 512, SECT_4K) },
 	{ INFO("mx25u51245g", 0xc2253a, 0, 64 * 1024, 1024, SECT_4K |
 	       SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 	{ INFO("mx25l12855e", 0xc22618, 0, 64 * 1024, 256, 0) },
