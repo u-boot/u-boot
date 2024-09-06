u-boot/u-boot

commit f3b5100aff3a7edd53fd5d15bd6db92d294865e2

regulator: fixed: add possibility to enable by clock
This commit adds the possibility to choose the compatible
"regulator-fixed-clock" in devicetree.

This is a special case of regulator-fixed where a clock has to
be used to switch the regulator on and off.

Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>
Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

drivers/power/regulator/fixed.c
@@ -6,13 +6,20 @@
  */
 
 #include <common.h>
+#include <clk.h>
 #include <errno.h>
 #include <dm.h>
+#include <linux/delay.h>
 #include <log.h>
 #include <power/pmic.h>
 #include <power/regulator.h>
 
 #include "regulator_common.h"
+
+struct fixed_clock_regulator_plat {
+	struct clk *enable_clock;
+	unsigned int clk_enable_counter;
+};
 
 static int fixed_regulator_of_to_plat(struct udevice *dev)
 {
@@ -71,6 +78,38 @@ static int fixed_regulator_set_enable(struct udevice *dev, bool enable)
 	return regulator_common_set_enable(dev, dev_get_plat(dev), enable);
 }
 
+static int fixed_clock_regulator_get_enable(struct udevice *dev)
+{
+	struct fixed_clock_regulator_plat *priv = dev_get_priv(dev);
+
+	return priv->clk_enable_counter > 0;
+}
+
+static int fixed_clock_regulator_set_enable(struct udevice *dev, bool enable)
+{
+	struct fixed_clock_regulator_plat *priv = dev_get_priv(dev);
+	struct regulator_common_plat *dev_pdata = dev_get_plat(dev);
+	int ret = 0;
+
+	if (enable) {
+		ret = clk_enable(priv->enable_clock);
+		priv->clk_enable_counter++;
+	} else {
+		ret = clk_disable(priv->enable_clock);
+		priv->clk_enable_counter--;
+	}
+	if (ret)
+		return ret;
+
+	if (enable && dev_pdata->startup_delay_us)
+		udelay(dev_pdata->startup_delay_us);
+
+	if (!enable && dev_pdata->off_on_delay_us)
+		udelay(dev_pdata->off_on_delay_us);
+
+	return ret;
+}
+
 static const struct dm_regulator_ops fixed_regulator_ops = {
 	.get_value	= fixed_regulator_get_value,
 	.get_current	= fixed_regulator_get_current,
@@ -78,8 +117,18 @@ static const struct dm_regulator_ops fixed_regulator_ops = {
 	.set_enable	= fixed_regulator_set_enable,
 };
 
+static const struct dm_regulator_ops fixed_clock_regulator_ops = {
+	.get_enable	= fixed_clock_regulator_get_enable,
+	.set_enable	= fixed_clock_regulator_set_enable,
+};
+
 static const struct udevice_id fixed_regulator_ids[] = {
 	{ .compatible = "regulator-fixed" },
+	{ },
+};
+
+static const struct udevice_id fixed_clock_regulator_ids[] = {
+	{ .compatible = "regulator-fixed-clock" },
 	{ },
 };
 
@@ -89,5 +138,14 @@ U_BOOT_DRIVER(regulator_fixed) = {
 	.ops = &fixed_regulator_ops,
 	.of_match = fixed_regulator_ids,
 	.of_to_plat = fixed_regulator_of_to_plat,
-	.plat_auto	= sizeof(struct regulator_common_plat),
+	.plat_auto = sizeof(struct regulator_common_plat),
+};
+
+U_BOOT_DRIVER(regulator_fixed_clock) = {
+	.name = "regulator_fixed_clk",
+	.id = UCLASS_REGULATOR,
+	.ops = &fixed_clock_regulator_ops,
+	.of_match = fixed_clock_regulator_ids,
+	.of_to_plat = fixed_regulator_of_to_plat,
+	.plat_auto = sizeof(struct fixed_clock_regulator_plat),
 };

commit a5d990854f2c67a8e9aa3d9ab3a11b2baff3285c

regulator: fixed: fix regulator-fixed-clock
For regulator-fixed-clock, the device's private data is never set so in
fixed_clock_regulator_set_enable() is null and the function cannot
complete successfully.

Rename the _plat structure to _priv to better represent its role and set
this as the private data.  As shown by the set_enable() function and by
using the same .of_to_plat hook as regulator-fixed, the platform data is
regulator_common_plat so also set .plat_auto correctly.

Finally, set up the private data by adding a .probe function to look up
the clock and set the member variable.

Fixes: f3b5100aff3 ("regulator: fixed: add possibility to enable by clock")
Signed-off-by: John Keeping <jkeeping@inmusicbrands.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

drivers/power/regulator/fixed.c
@@ -17,7 +17,7 @@
 
 #include "regulator_common.h"
 
-struct fixed_clock_regulator_plat {
+struct fixed_clock_regulator_priv {
 	struct clk *enable_clock;
 	unsigned int clk_enable_counter;
 };
@@ -83,14 +83,14 @@ static int fixed_regulator_set_enable(struct udevice *dev, bool enable)
 
 static int fixed_clock_regulator_get_enable(struct udevice *dev)
 {
-	struct fixed_clock_regulator_plat *priv = dev_get_priv(dev);
+	struct fixed_clock_regulator_priv *priv = dev_get_priv(dev);
 
 	return priv->clk_enable_counter > 0;
 }
 
 static int fixed_clock_regulator_set_enable(struct udevice *dev, bool enable)
 {
-	struct fixed_clock_regulator_plat *priv = dev_get_priv(dev);
+	struct fixed_clock_regulator_priv *priv = dev_get_priv(dev);
 	struct regulator_common_plat *plat = dev_get_plat(dev);
 	int ret = 0;
 
@@ -111,6 +111,17 @@ static int fixed_clock_regulator_set_enable(struct udevice *dev, bool enable)
 		udelay(plat->off_on_delay_us);
 
 	return ret;
+}
+
+static int fixed_clock_regulator_probe(struct udevice *dev)
+{
+	struct fixed_clock_regulator_priv *priv = dev_get_priv(dev);
+
+	priv->enable_clock = devm_clk_get(dev, NULL);
+	if (IS_ERR(priv->enable_clock))
+		return PTR_ERR(priv->enable_clock);
+
+	return 0;
 }
 
 static const struct dm_regulator_ops fixed_regulator_ops = {
@@ -149,6 +160,8 @@ U_BOOT_DRIVER(regulator_fixed_clock) = {
 	.id = UCLASS_REGULATOR,
 	.ops = &fixed_clock_regulator_ops,
 	.of_match = fixed_clock_regulator_ids,
+	.probe = fixed_clock_regulator_probe,
 	.of_to_plat = fixed_regulator_of_to_plat,
-	.plat_auto = sizeof(struct fixed_clock_regulator_plat),
+	.plat_auto = sizeof(struct regulator_common_plat),
+	.priv_auto = sizeof(struct fixed_clock_regulator_priv),
 };