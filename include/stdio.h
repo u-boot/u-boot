d7f1672cf78dc576c5bccb8b2eb5f6ace7fe00bf
Simon Glass <sjg@chromium.org>
2024-09-29 19:49:29 -0600

    stdio: Make use of the SERIAL define
    This is always enabled for U-Boot proper, so simplify the condition
    in the common Makefile.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- include/stdio.h
+++ include/stdio.h
@@ -9,10 +9,7 @@ int getchar(void);
 int tstc(void);
 
 /* stdout */
-#if !defined(CONFIG_SPL_BUILD) || \
-	(defined(CONFIG_TPL_BUILD) && defined(CONFIG_TPL_SERIAL)) || \
-	(defined(CONFIG_SPL_BUILD) && !defined(CONFIG_TPL_BUILD) && \
-		defined(CONFIG_SPL_SERIAL))
+#if !defined(CONFIG_SPL_BUILD) || CONFIG_IS_ENABLED(SERIAL)
 void putc(const char c);
 void puts(const char *s);
 #ifdef CONFIG_CONSOLE_FLUSH_SUPPORT
