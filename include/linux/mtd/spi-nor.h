f83e36fd83c74b4e28a45a9d56abc4ad9b7848b9
Marek Vasut <marex@denx.de>
2024-10-15 22:14:53 +0200

    mtd: spi-nor: Move SR3 WPS bit definition in the correct location
    Move the SR3 bit definition in the right place. Fix
    what is likely a rebase artifact. No functional change.
    
    Fixes: 215f1d5794c6 ("mtd: spi-nor: Clear Winbond SR3 WPS bit on boot")
    Signed-off-by: Marek Vasut <marex@denx.de>

--- include/linux/mtd/spi-nor.h
+++ include/linux/mtd/spi-nor.h
@@ -182,14 +182,14 @@
 /* Status Register 2 bits. */
 #define SR2_QUAD_EN_BIT7	BIT(7)
 
+/* Status Register 3 bits. */
+#define SR3_WPS			BIT(2)
+
 /*
  * Maximum number of flashes that can be connected
  * in stacked/parallel configuration
  */
 #define SNOR_FLASH_CNT_MAX	2
-
-/* Status Register 3 bits. */
-#define SR3_WPS			BIT(2)
 
 /* For Cypress flash. */
 #define SPINOR_OP_RD_ANY_REG			0x65	/* Read any register */
