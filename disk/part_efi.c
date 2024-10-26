4ea1deb4bfdbda6e66cbb8a10967008a35cef1ef
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2024-10-26 11:05:53 +0300

    disk: Mark static functions in part_efi.c
    Mark all the functions that are only defined locally as static and
    quiesce W=1 warnings
    
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Reviewed-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    [trini: Add __maybe_unused as it's now seen as unused in some cases]
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- disk/part_efi.c
+++ disk/part_efi.c
@@ -216,7 +216,7 @@ int get_disk_guid(struct blk_desc *desc, char *guid)
 	return 0;
 }
 
-void part_print_efi(struct blk_desc *desc)
+static void __maybe_unused part_print_efi(struct blk_desc *desc)
 {
 	ALLOC_CACHE_ALIGN_BUFFER_PAD(gpt_header, gpt_head, 1, desc->blksz);
 	gpt_entry *gpt_pte = NULL;
@@ -258,8 +258,8 @@ void part_print_efi(struct blk_desc *desc)
 	return;
 }
 
-int part_get_info_efi(struct blk_desc *desc, int part,
-		      struct disk_partition *info)
+static int __maybe_unused part_get_info_efi(struct blk_desc *desc, int part,
+					    struct disk_partition *info)
 {
 	ALLOC_CACHE_ALIGN_BUFFER_PAD(gpt_header, gpt_head, 1, desc->blksz);
 	gpt_entry *gpt_pte = NULL;
