1bf25c775010290bb4239180b17684b1657488fd
Rasmus Villemoes <ravi@prevas.dk>
2024-10-30 22:34:04 +0100

    test: env: add some test cases for new "env default -k" flag
    Check that the new -k flag works as expected.
    
    This also adds a test of the -a flag, which was previously missing,
    and as the comment says, perhaps for a good reason. At least now we
    have a test for it in combination with -k (and -f, because the ethaddr
    variables otherwise cause complaining).
    
    Signed-off-by: Rasmus Villemoes <ravi@prevas.dk>

--- test/env/cmd_ut_env.c
+++ test/env/cmd_ut_env.c
@@ -31,6 +31,46 @@ static int env_test_env_cmd(struct unit_test_state *uts)
 	ut_asserteq(0, run_command("env exists non_default_var2", 0));
 	ut_assert_console_end();
 
+	ut_assertok(run_command("setenv non_default_var1 3", 0));
+	ut_assert_console_end();
+
+	ut_assertok(run_command("env default -k non_default_var1", 0));
+	ut_assert_console_end();
+
+	ut_asserteq(0, run_command("env exists non_default_var1", 0));
+	ut_assert_console_end();
+
+	ut_asserteq(0, run_command("env exists non_default_var2", 0));
+	ut_assert_console_end();
+
+	ut_assertok(run_command("env default -k -a -f", 0));
+	ut_assert_nextline("## Resetting to default environment");
+	ut_assert_console_end();
+
+	ut_asserteq(0, run_command("env exists non_default_var1", 0));
+	ut_assert_console_end();
+
+	ut_asserteq(0, run_command("env exists non_default_var2", 0));
+	ut_assert_console_end();
+
+	/*
+	 * While the following test of "env default -a" by itself
+	 * works, it unfortunately causes an unrelated test case,
+	 * env_test_fdt_import(), to fail, because the "from_fdt"
+	 * variable would be removed.
+	 */
+#if 0
+	ut_assertok(run_command("env default -a", 0));
+	ut_assert_nextline("## Resetting to default environment");
+	ut_assert_console_end();
+
+	ut_asserteq(1, run_command("env exists non_default_var1", 0));
+	ut_assert_console_end();
+
+	ut_asserteq(1, run_command("env exists non_default_var2", 0));
+	ut_assert_console_end();
+#endif
+
 	return 0;
 }
 ENV_TEST(env_test_env_cmd, UTF_CONSOLE);
