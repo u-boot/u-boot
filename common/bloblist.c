62ae82e050e69f3c4377904ac9bdb2664587f67e
Raymond Mao <raymond.mao@linaro.org>
2024-11-15 09:22:31 -0800

    bloblist: add api to check if bloblist from previous stage is valid
    Besides fdtdec, potentially other libraries need to check if a
    valid bloblist from previous boot stage exists before extracting
    information from it, thus this patch create an api for the purpose
    of code reuse.
    
    Signed-off-by: Raymond Mao <raymond.mao@linaro.org>

--- common/bloblist.c
+++ common/bloblist.c
@@ -574,6 +574,17 @@ int bloblist_maybe_init(void)
 	return 0;
 }
 
+bool bloblist_of_isvalid(void)
+{
+	if (!CONFIG_IS_ENABLED(BLOBLIST) ||
+	    (xpl_prev_phase() == PHASE_TPL &&
+	    !IS_ENABLED(CONFIG_TPL_BLOBLIST)) ||
+	    bloblist_maybe_init())
+		return false;
+
+	return true;
+}
+
 int bloblist_check_reg_conv(ulong rfdt, ulong rzero, ulong rsig)
 {
 	ulong version = BLOBLIST_REGCONV_VER;
