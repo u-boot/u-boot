f99b549fc2e44fc5f5ac2283844cf30e784c8ff8
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2024-11-27 00:40:17 +0100

    efi_loader: avoid writing message in Exit() boot service
    We should not write messages in UEFI API functions. This may lead to
    incorrect screen layout in UEFI application.
    
    For single statements after if clause we don't need braces.
    
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- lib/efi_loader/efi_boottime.c
+++ lib/efi_loader/efi_boottime.c
@@ -3495,10 +3495,9 @@ static efi_status_t EFIAPI efi_exit(efi_handle_t image_handle,
 	if (IS_ENABLED(CONFIG_EFI_TCG2_PROTOCOL)) {
 		if (image_obj->image_type == IMAGE_SUBSYSTEM_EFI_APPLICATION) {
 			ret = efi_tcg2_measure_efi_app_exit();
-			if (ret != EFI_SUCCESS) {
-				log_warning("tcg2 measurement fails(0x%lx)\n",
-					    ret);
-			}
+			if (ret != EFI_SUCCESS)
+				log_debug("tcg2 measurement fails (0x%lx)\n",
+					  ret);
 		}
 	}
 
