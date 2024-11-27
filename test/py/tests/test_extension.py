d01720a8305f7fd3f33840eac7fb89254a127f4d
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2024-11-27 08:06:26 +0100

    test: fix test_extension.py
    test_extension.py assumes that no extension is known at test start.
    This assumption is wrong because we do not come out of reboot.
    A prior test may have already hunted for the extension bootdev.
    
    Remove the invalid assert.
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- test/py/tests/test_extension.py
+++ test/py/tests/test_extension.py
@@ -26,7 +26,9 @@ def test_extension(u_boot_console):
     load_dtb(u_boot_console)
 
     output = u_boot_console.run_command('extension list')
-    assert('No extension' in output)
+    # extension_bootdev_hunt may have already run.
+    # Without reboot we cannot make any assumption here.
+    # assert('No extension' in output)
 
     output = u_boot_console.run_command('extension scan')
     assert output == 'Found 2 extension board(s).'
