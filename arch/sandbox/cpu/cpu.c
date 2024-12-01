u-boot/u-boot

commit d8289e7dfe5541dfef59520917d81cd39e10c8f3

sandbox: fdt: Avoid overwriting an existing fdt
Since the removal of OF_HOSTFILE logic in board_fdt_blob_setup(), the
logic for obtaining the DT is handled in the OF_BOARD option. If a
devicetree comes from a bloblist it is immediately overwritten by this
function.

Fix this by skipping the function if a devicetree is already present.

This is sort-of a fix for e7fb7896 ("sandbox: Remove OF_HOSTFILE") but
it has only come to light since bloblist was added, so I have not added
a Fixes tag.

Unfortunately it is not possible to report the correct FDT source with
the current code. It might be best to use an error-return code for
board_fdt_blob_setup() so that an error can be reported if the board
does not provide the DT.

Signed-off-by: Simon Glass <sjg@chromium.org>

arch/sandbox/cpu/cpu.c
@@ -340,6 +340,8 @@ void *board_fdt_blob_setup(int *ret)
 	int err;
 	int fd;
 
+	if (gd->fdt_blob)
+		return (void *)gd->fdt_blob;
 	blob = map_sysmem(CONFIG_SYS_FDT_LOAD_ADDR, 0);
 	*ret = 0;
 	if (!state->fdt_fname) {

commit 222d9d9125b8953be07f2aad2b6d66687a23ebdc

sandbox: set retval early in board_fdt_blob_setup()
Bug:
nobody sets 'ret' to non-error value inside board_fdt_blob_setup()
in case, when gd->fdt_blob has been initialized before calling the
board_fdt_blob_setup() (say, due to CONFIG_OF_EMBED=y)

Reproduced with CONFIG_OF_EMBED=y && BLOBLIST=n.
  ```
    $ ./u-boot
    initcall: 0000000000078e05 (relocated to 000061138d2c7e05)
    initcall: 0000000000117b12 (relocated to 000061138d366b12)
      fdtdec_setup():
            gd->fdt_blob = dtb_dt_embedded()
      fdtdec_setup():
            gd->fdt_blob = board_fdt_blob_setup(&ret);

        board_fdt_blob_setup():380 ret:-2 gd->fdt_blob:0x61138d4db310
                if (gd->fdt_blob)
                        return (void *)gd->fdt_blob;

    initcall failed at call 0000000000117b12 (err=-2: No such file \
       or directory)
    ### ERROR ### Please RESET the board ###
  ```

Patch moves code, zeroizing the 'ret', before checking the fdt_blob.

Fixes: d8289e7dfe5 ("sandbox: fdt: Avoid overwriting an existing fdt")
Signed-off-by: Evgeny Bachinin <EABachinin@salutedevices.com>

arch/sandbox/cpu/cpu.c
@@ -377,10 +377,10 @@ void *board_fdt_blob_setup(int *ret)
 	int err;
 	int fd;
 
+	*ret = 0;
 	if (gd->fdt_blob)
 		return (void *)gd->fdt_blob;
 	blob = map_sysmem(CONFIG_SYS_FDT_LOAD_ADDR, 0);
-	*ret = 0;
 	if (!state->fdt_fname) {
 		err = setup_auto_tree(blob);
 		if (!err)