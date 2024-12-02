ddb3d7e635c41bd46dd386efdb0e9c21e94ba99e
FUKAUMI Naoki <naoki@radxa.com>
2024-12-02 23:40:18 +0000

    rockchip: rk3399: expand space for decompressed kernel
    fix following error by using same ENV_MEM_LAYOUT_SETTINGS with rk35xx:
    
    U-Boot 2025.01-rc3-00001-g1527c7dcdb01 (Dec 02 2024 - 22:57:18 +0000)
      :
    Retrieving file: /boot/extlinux/../nixos/dhqjsnprmzjxncil3m0g9l09a479crn3-linux-6.12.1-Image
    Retrieving file: /boot/extlinux/../nixos/6fq8fmmab31yxdwcs7zw44p78fq9fy1s-initrd-linux-6.12.1-initrd
    append: init=/nix/store/yjbxgzf1vkwbw6ab738bf4kxazhyypa1-nixos-system-rock-5b-25.05.20241201.ac35b10/init console=ttyS2,1500000n8 console=ttyAMA0,115200n8 console=tty0 loglevel=7
    Retrieving file: /boot/extlinux/../nixos/dhqjsnprmzjxncil3m0g9l09a479crn3-linux-6.12.1-dtbs/rockchip/rk3399-rock-4se.dtb
    Moving Image from 0x2080000 to 0x2200000, end=0x60d0000
    ERROR: RD image overlaps OS image (OS=2200000..60d0000)
    Boot failed (err=-14)
    
    $ ls -lh boot/nixos/
    total 84M
    -r--r--r--  1 root root  24M Jan  1  1970 6fq8fmmab31yxdwcs7zw44p78fq9fy1s-initrd-linux-6.12.1-initrd
    -r--r--r--  1 root root  62M Jan  1  1970 dhqjsnprmzjxncil3m0g9l09a479crn3-linux-6.12.1-Image
    dr-xr-xr-x 36 root root 4.0K Jan  1  1970 dhqjsnprmzjxncil3m0g9l09a479crn3-linux-6.12.1-dtbs
    
    similar problem was fixed for rk35xx by:
    
    commit 69b73877f02c ("rockchip: rk35xx: expand space for decompressed kernel")
    
    Signed-off-by: FUKAUMI Naoki <naoki@radxa.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -20,16 +20,16 @@
 #endif
 
 #define ENV_MEM_LAYOUT_SETTINGS		\
-	"scriptaddr=0x00500000\0"	\
+	"scriptaddr=0x00c00000\0"	\
 	"script_offset_f=0xffe000\0"	\
 	"script_size_f=0x2000\0"	\
-	"pxefile_addr_r=0x00600000\0"	\
-	"fdt_addr_r=0x01e00000\0"	\
-	"fdtoverlay_addr_r=0x01f00000\0"	\
-	"kernel_addr_r=0x02080000\0"	\
-	"ramdisk_addr_r=0x06000000\0"	\
-	"kernel_comp_addr_r=0x08000000\0"	\
-	"kernel_comp_size=0x2000000\0"
+	"pxefile_addr_r=0x00e00000\0"	\
+	"kernel_addr_r=0x02000000\0"	\
+	"kernel_comp_addr_r=0x0a000000\0"	\
+	"fdt_addr_r=0x12000000\0"	\
+	"fdtoverlay_addr_r=0x12100000\0"	\
+	"ramdisk_addr_r=0x12180000\0"	\
+	"kernel_comp_size=0x8000000\0"
 
 #define CFG_EXTRA_ENV_SETTINGS		\
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0"	\
