d4e0105e58f6983df2b94869da8690c3fdd4b272
Peng Fan <peng.fan@nxp.com>
2025-01-09 11:29:05 +0800

    imx: imx8mp_evk: Enable dynamic settings to mmcdev and mmcroot
    Enable dynamic settings to mmcdev and mmcroot for i.MX8MP-EVK
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx8mp_evk/imx8mp_evk.c
+++ board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -3,6 +3,7 @@
  * Copyright 2019 NXP
  */
 
+#include <asm/arch/sys_proto.h>
 #include <env.h>
 
 int board_init(void)
@@ -12,6 +13,10 @@ int board_init(void)
 
 int board_late_init(void)
 {
+#if CONFIG_IS_ENABLED(ENV_IS_IN_MMC)
+	board_late_mmc_env_init();
+#endif
+
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	env_set("board_name", "EVK");
 	env_set("board_rev", "iMX8MP");
