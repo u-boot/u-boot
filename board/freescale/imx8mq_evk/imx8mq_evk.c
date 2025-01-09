74854d77506dcf1c8a3d292778c1ce98f8c47946
Peng Fan <peng.fan@nxp.com>
2025-01-09 11:29:13 +0800

    imx: imx8mq_evk: Cleanup headers
    Drop unused headers and sort the order.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx8mq_evk/imx8mq_evk.c
+++ board/freescale/imx8mq_evk/imx8mq_evk.c
@@ -3,28 +3,12 @@
  * Copyright 2018 NXP
  */
 
-#include <env.h>
-#include <init.h>
-#include <malloc.h>
-#include <errno.h>
-#include <asm/global_data.h>
-#include <asm/io.h>
-#include <miiphy.h>
-#include <netdev.h>
-#include <asm/mach-imx/iomux-v3.h>
-#include <asm-generic/gpio.h>
-#include <fsl_esdhc_imx.h>
-#include <mmc.h>
+#include <asm/arch/clock.h>
 #include <asm/arch/imx8mq_pins.h>
 #include <asm/arch/sys_proto.h>
-#include <asm/mach-imx/gpio.h>
-#include <asm/mach-imx/mxc_i2c.h>
-#include <asm/arch/clock.h>
-#include <spl.h>
-#include <linux/bitops.h>
-#include <power/pmic.h>
-#include <power/pfuze100_pmic.h>
-#include "../common/pfuze.h"
+#include <asm/global_data.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <env.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
