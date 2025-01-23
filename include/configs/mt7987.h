c80a3fb96135394d4724bbd2c887c8a372b8f5ee
Weijie Gao <weijie.gao@mediatek.com>
2025-01-23 16:55:05 +0800

    board: mediatek: add MT7987 reference boards
    This patch adds general board files based on MT7987 SoC.
    
    MT7987 uses one mmc controller for booting from both SD and eMMC, and the
    pins of mmc controller are also shared with one spi controller.
    So three configs are need for these boot types:
    
    1. mt7987_rfb_defconfig - SPI-NOR (spi2) and SPI-NAND (spi0)
    2. mt7987_emmc_rfb_defconfig - eMMC + SPI-NOR (spi2)
    3. mt7987_sd_rfb_defconfig - SD + SPI-NOR (spi2)
    
    Note: spi2 also supports booting from SPI-NAND, but not the default option.
    
    Signed-off-by: Sam Shih <sam.shih@mediatek.com>
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

---
+++ include/configs/mt7987.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Configuration for MediaTek MT7987 SoC
+ *
+ * Copyright (C) 2025 MediaTek Inc.
+ * Author: Sam Shih <sam.shih@mediatek.com>
+ */
+
+#ifndef __MT7987_H
+#define __MT7987_H
+
+#define CFG_MAX_MEM_MAPPED		0xC0000000
+
+#endif
