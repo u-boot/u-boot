b0407ace97d4961c09b7f5784527751a390a7044
Simon Glass <sjg@chromium.org>
2025-01-26 11:43:15 -0700

    spl: Adjust debugging and xPL symbols
    The size of some malloc() fields has reduced on 64-bit machines, but
    the spl_reloc code was not updated. Fix this to avoid a compiler
    warning.
    
    Also update for the new xPL naming.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Tom Rini <trini@konsulko.com>

--- common/spl/spl_reloc.c
+++ common/spl/spl_reloc.c
@@ -154,7 +154,7 @@ int spl_reloc_jump(struct spl_image_info *image, spl_jump_to_image_t jump)
 	rcode_func loader;
 	int ret;
 
-	log_debug("malloc usage %lx bytes (%ld KB of %d KB)\n", gd->malloc_ptr,
+	log_debug("malloc usage %x bytes (%d KB of %d KB)\n", gd->malloc_ptr,
 		  gd->malloc_ptr / 1024, CONFIG_VAL(SYS_MALLOC_F_LEN) / 1024);
 
 	if (*image->stack_prot != STACK_PROT_VALUE) {
