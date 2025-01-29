46fc271b646c5e755746d348c58e380ce02122e2
Quentin Schulz <quentin.schulz@cherry.de>
2025-01-29 11:43:33 +0100

    rockchip: px30: add fdtoverlay_addr_r default value to support FDTO
    In order to be able to use Device Tree Overlays, the fdtoverlay_addr_r
    needs to be specified.
    
    Follow what's been done for other Rockchip SoCs and leave 1MiB for the
    base DTB before the address for the overlay.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- include/configs/px30_common.h
+++ include/configs/px30_common.h
@@ -20,6 +20,7 @@
 	"scriptaddr=0x00500000\0" \
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x08300000\0" \
+	"fdtoverlay_addr_r=0x08400000\0" \
 	"kernel_addr_r=0x00280000\0" \
 	"ramdisk_addr_r=0x0a200000\0" \
 	"kernel_comp_addr_r=0x03e80000\0" \
