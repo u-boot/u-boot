8881eb7317ea07d5cee1ea3ab828449ce7311d56
Quentin Schulz <quentin.schulz@cherry.de>
2025-01-31 11:31:33 +0100

    pinctrl: rockchip: constify rockchip_pin_ctrl for RK3128
    There's no need to modify private data from the controller, so let's
    make that struct const.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- drivers/pinctrl/rockchip/pinctrl-rk3128.c
+++ drivers/pinctrl/rockchip/pinctrl-rk3128.c
@@ -171,7 +171,7 @@ static struct rockchip_pin_bank rk3128_pin_banks[] = {
 	PIN_BANK(3, 32, "gpio3"),
 };
 
-static struct rockchip_pin_ctrl rk3128_pin_ctrl = {
+static const struct rockchip_pin_ctrl rk3128_pin_ctrl = {
 	.pin_banks		= rk3128_pin_banks,
 	.nr_banks		= ARRAY_SIZE(rk3128_pin_banks),
 	.grf_mux_offset		= 0xa8,
