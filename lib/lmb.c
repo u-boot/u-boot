3745ba61d27c31a35d543ac355b2de79843ab3b0
Sughosh Ganu <sughosh.ganu@linaro.org>
2025-02-05 15:55:40 +0530

    lmb: handle scenario of of encompassing overlap
    The lmb_fix_over_lap_regions() function is called if the added region
    overlaps with an existing region. The function then fixes the overlap
    and removes the redundant region. However, it makes an assumption that
    the overlap would not encompass the existing region, and in such a
    scenario, it prints a message and returns without making the
    fix. Handle the case of an encompassing overlap also in the function.
    
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>

--- lib/lmb.c
+++ lib/lmb.c
@@ -111,11 +111,9 @@ static void lmb_fix_over_lap_regions(struct alist *lmb_rgn_lst,
 	phys_addr_t base2 = rgn[r2].base;
 	phys_size_t size2 = rgn[r2].size;
 
-	if (base1 + size1 > base2 + size2) {
-		printf("This will not be a case any time\n");
-		return;
-	}
-	rgn[r1].size = base2 + size2 - base1;
+	if (base1 + size1 < base2 + size2)
+		rgn[r1].size = base2 + size2 - base1;
+
 	lmb_remove_region(lmb_rgn_lst, r2);
 }
 
