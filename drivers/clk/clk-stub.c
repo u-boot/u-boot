506f6a384c335e86c4812c5e734dea4eabc21130
Julius Lehmann <lehmanju@devpi.de>
2025-02-10 16:27:27 +0000

    clk: stub: add sm8150 compatible id
    Add support for sm8150 clock controller to clk stub driver.
    
    Signed-off-by: Julius Lehmann <lehmanju@devpi.de>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/clk/clk-stub.c
+++ drivers/clk/clk-stub.c
@@ -50,6 +50,7 @@ static struct clk_ops stub_clk_ops = {
 
 static const struct udevice_id stub_clk_ids[] = {
 	{ .compatible = "qcom,rpmcc" },
+	{ .compatible = "qcom,sm8150-rpmh-clk" },
 	{ .compatible = "qcom,sm8250-rpmh-clk" },
 	{ .compatible = "qcom,sm8550-rpmh-clk" },
 	{ .compatible = "qcom,sm8650-rpmh-clk" },
