77718437862ee849bd8818c482208aaa92363c8d
Paul HENRYS <paul.henrys_ext@softathome.com>
2025-02-12 10:31:21 +0100

    rsa: Add rsa_verify_openssl() to use openssl for host builds
    rsa_verify_openssl() is used in lib/rsa/rsa-verify.c to authenticate data
    when building host tools.
    
    Signed-off-by: Paul HENRYS <paul.henrys_ext@softathome.com>

--- lib/rsa/rsa-verify.c
+++ lib/rsa/rsa-verify.c
@@ -565,6 +565,11 @@ int rsa_verify(struct image_sign_info *info,
 	uint8_t hash[info->crypto->key_len];
 	int ret;
 
+#ifdef USE_HOSTCC
+	if (!info->fdt_blob)
+		return rsa_verify_openssl(info, region, region_count, sig, sig_len);
+#endif
+
 	/*
 	 * Verify that the checksum-length does not exceed the
 	 * rsa-signature-length
