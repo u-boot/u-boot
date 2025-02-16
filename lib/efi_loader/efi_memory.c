5ce629db2512d70f298bc1d422fefad63a74c7f5
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2025-02-16 12:12:41 +0100

    efi_loader: make efi_add_memory_map_pg() static
    The function is only used in the efi_memory.c module.
    
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- lib/efi_loader/efi_memory.c
+++ lib/efi_loader/efi_memory.c
@@ -268,6 +268,7 @@ static s64 efi_mem_carve_out(struct efi_mem_list *map,
  *				memory
  * Return:			status code
  */
+static
 efi_status_t efi_add_memory_map_pg(u64 start, u64 pages,
 				   int memory_type,
 				   bool overlap_conventional)
