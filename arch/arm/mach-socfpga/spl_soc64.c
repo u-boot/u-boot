0d2010faaca92bb5f0d20b961323d85caaafa080
Tien Fong Chee <tien.fong.chee@intel.com>
2025-02-18 16:34:59 +0800

    arm: socfpga: agilex5: Add SMMU initialization
    Allow non-secure accesses only with SMMU peripherals. This would protect
    the content in DDR secure region from accidentally modified by SMMU
    peripherals.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@altera.com>

--- arch/arm/mach-socfpga/spl_soc64.c
+++ arch/arm/mach-socfpga/spl_soc64.c
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  *  Copyright (C) 2020 Intel Corporation. All rights reserved
+ *  Copyright (C) 2025 Altera Corporation <www.altera.com>
  *
  */
 
+#include <hang.h>
 #include <spl.h>
+#include <dm/uclass.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -22,3 +25,16 @@ u32 spl_boot_mode(const u32 boot_device)
 		return MMCSD_MODE_RAW;
 }
 #endif
+
+/* board specific function prior loading SSBL / U-Boot */
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+	int ret;
+	struct udevice *dev;
+
+	ret = uclass_get_device_by_name(UCLASS_NOP, "socfpga-smmu-secure-config", &dev);
+	if (ret) {
+		printf("HPS SMMU secure settings init failed: %d\n", ret);
+		hang();
+	}
+}
