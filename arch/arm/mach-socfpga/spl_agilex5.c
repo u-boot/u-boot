1c37e59bfbbae14dcc15894c8367339d16dda95a
Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@intel.com>
2025-02-18 16:35:06 +0800

    arm: armv8: Improve SPL data save and restore implementation
    Introduce a new symbol in the beginning of .data section in
    the common ARMv8 linker script and use that as a reference
    for data save and restore.
    
    Previously, the code would rely on calculating the start of
    the .data section address via data size, however, we observed
    that the data size does not really reflect the SPL mapped
    addresses.
    
    In our case, the binman_sym section size was not included in
    the data size, which will result in a wrong address for the
    .data start section, which prevents us from properly saving
    and restoring SPL data.
    
    This approach skips the calculation for the starting address
    of the .data section, and instead just defines the beginning
    address of the .data section and calling the symbol as needed,
    in which we think as a simpler and much more robust method.
    
    Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>
    Signed-off-by: Tien Fong Chee <tien.fong.chee@altera.com>
    Reviewed-by: Tien Fong Chee <tien.fong.chee@altera.com>

--- arch/arm/mach-socfpga/spl_agilex5.c
+++ arch/arm/mach-socfpga/spl_agilex5.c
@@ -21,10 +21,31 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+u32 reset_flag(void)
+{
+	/* Check rstmgr.stat for warm reset status */
+	u32 status = readl(SOCFPGA_RSTMGR_ADDRESS);
+
+	/* Check whether any L4 watchdogs or SDM had triggered warm reset */
+	u32 warm_reset_mask = RSTMGR_L4WD_MPU_WARMRESET_MASK;
+
+	if (status & warm_reset_mask)
+		return 0;
+
+	return 1;
+}
+
 void board_init_f(ulong dummy)
 {
 	int ret;
 	struct udevice *dev;
+
+	/* Enable Async */
+	asm volatile("msr daifclr, #4");
+
+#ifdef CONFIG_SPL_BUILD
+	spl_save_restore_data();
+#endif
 
 	ret = spl_early_init();
 	if (ret)
