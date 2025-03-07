a7bc58409f509acda631b558f87f93470ac90a94
Yao Zi <ziyao@disroot.org>
2025-03-07 13:13:44 +0000

    board: sifive: Remove dead board_fdt_blob_setup
    CONFIG_OF_BOARD isn't enabled on SiFive Unleashed and Unmatched, thus
    board_fdt_blob_setup is actually dead code on these platforms. Let's
    remove it.
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

--- board/sifive/unleashed/unleashed.c
+++ board/sifive/unleashed/unleashed.c
@@ -114,17 +114,6 @@ int misc_init_r(void)
 
 #endif
 
-int board_fdt_blob_setup(void **fdtp)
-{
-	if (gd->arch.firmware_fdt_addr) {
-		*fdtp = (ulong *)(uintptr_t)gd->arch.firmware_fdt_addr;
-
-		return 0;
-	}
-
-	return -EEXIST;
-}
-
 int board_init(void)
 {
 	/* enable all cache ways */
--- board/sifive/unmatched/unmatched.c
+++ board/sifive/unmatched/unmatched.c
@@ -10,16 +10,6 @@
 #include <dm.h>
 #include <asm/sections.h>
 
-int board_fdt_blob_setup(void **fdtp)
-{
-	if (gd->arch.firmware_fdt_addr) {
-		*fdtp = (ulong *)(uintptr_t)gd->arch.firmware_fdt_addr;
-		return 0;
-	}
-
-	return -EEXIST;
-}
-
 int board_init(void)
 {
 	/* enable all cache ways */
