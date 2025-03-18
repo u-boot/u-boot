e8660b23f4929fc787e917f3b5f9cd1d09b93e84
Pawel Kochanowski <pkochanowski@sii.pl>
2025-03-18 10:22:18 +0100

    efi: Make FDT extra space configurable
    U-Boot currently reserves only 0x3000 bytes when copying the FDT
    in copy_fdt(), which may not be sufficient if additional nodes
    (such as FMAN firmware) are added later.
    
    This patch uses the exisitng SYS_FDT_PAD to reserve space for FDT fixup
    instead of hardcoded value.
    
    This change prevents potential corruption when resizing FDT after
    EFI boot, especially when firmware like FMAN requires additional
    space.
    
    Signed-off-by: Gabriel Nesteruk <gnesteruk@sii.pl>
    Signed-off-by: Pawel Kochanowski <pkochanowski@sii.pl>
    Reviewed-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- lib/efi_loader/efi_dt_fixup.c
+++ lib/efi_loader/efi_dt_fixup.c
@@ -168,7 +168,7 @@ efi_dt_fixup(struct efi_dt_fixup_protocol *this, void *dtb,
 		/* Check size */
 		required_size = fdt_off_dt_strings(dtb) +
 				fdt_size_dt_strings(dtb) +
-				0x3000;
+				CONFIG_SYS_FDT_PAD;
 		total_size = fdt_totalsize(dtb);
 		if (required_size < total_size)
 			required_size = total_size;
