99843fe42d6746a23e6b24447675c06c1a0c83e4
Adam Ford <aford173@gmail.com>
2025-03-24 18:27:25 -0500

    board: beacon: imx8mp: Fix GIC clock for Overdrive mode
    There is a config option to run the PMIC at nominal voltages
    which is not enabled on the i.MX8MP Beacon kit, so it the PMIC
    runs at overdrive voltages.   Unfortuately, the check for this
    condition to set the GIC clock parent and rate is backwards from
    what it should be, and accidentally sets the GIC clock to nominal
    if the PMIC is in overdrive, and sets the GIC clock to overdrive
    if the PMIC is in nominal.  Fix this by inverting the logic on the
    check.
    
    Fixes: ab53bd43dbde ("arm64: imx: Add support for imx8mp-beacon-kit")
    Signed-off-by: Adam Ford <aford173@gmail.com>

--- board/beacon/imx8mp/spl.c
+++ board/beacon/imx8mp/spl.c
@@ -50,7 +50,7 @@ void spl_board_init(void)
 	 * setting done. Default is 400Mhz (system_pll1_800m with div = 2)
 	 * set by ROM for ND VDD_SOC
 	 */
-	if (IS_ENABLED(CONFIG_IMX8M_VDD_SOC_850MV)) {
+	if (!IS_ENABLED(CONFIG_IMX8M_VDD_SOC_850MV)) {
 		clock_enable(CCGR_GIC, 0);
 		clock_set_target_val(GIC_CLK_ROOT, CLK_ROOT_ON | CLK_ROOT_SOURCE_SEL(5));
 		clock_enable(CCGR_GIC, 1);
