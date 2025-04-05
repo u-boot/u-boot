12f26720e58bd39c044487cd7eff1cf4e9a22264
Alexey Minnekhanov <alexeymin@postmarketos.org>
2025-04-05 16:01:19 +0300

    Revert "gpio: qcom_pmic: Add PM660 / PM660L"
    This reverts commit f99efd23497939d3192e178f3f4ba80380787c2b.

--- drivers/gpio/qcom_pmic_gpio.c
+++ drivers/gpio/qcom_pmic_gpio.c
@@ -343,8 +343,6 @@ static const struct udevice_id qcom_gpio_ids[] = {
 	{ .compatible = "qcom,pm8998-gpio" },
 	{ .compatible = "qcom,pms405-gpio" },
 	{ .compatible = "qcom,pm6125-gpio" },
-	{ .compatible = "qcom,pm660-gpio" },	/* 13 GPIOs */
-	{ .compatible = "qcom,pm660l-gpio" },	/* 12 GPIOs */
 	{ .compatible = "qcom,pm8150-gpio" },
 	{ }
 };
