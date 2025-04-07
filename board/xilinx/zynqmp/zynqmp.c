a93cc9b9c56870fe134fe2006a1f46d9557ec780
Vincent Stehlé <vincent.stehle@arm.com>
2025-04-07 19:05:29 +0200

    arm64: zynqmp: fix dfu alt buffer clearing
    The set_dfu_alt_info() function calls the ALLOC_CACHE_ALIGN_BUFFER()
    macro to declare a `buf' variable pointer into an array allocated on the
    stack. It then calls the memset() function to clear the useable portion
    of the array using the idiomatic expression `sizeof(buf)'.
    
    While this would indeed work fine for an array, in the present case we
    end up clearing only the size of a pointer.
    Fix this by specifying the explicit size `DFU_ALT_BUF_LEN' instead.
    
    Fixes: b86f43de0be0 ("xilinx: zynqmp: Add support for runtime dfu_alt_info setup")
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>
    Cc: Michal Simek <michal.simek@amd.com>
    Cc: Tom Rini <trini@konsulko.com>
    Acked-by: Padmarao Begari <padmarao.begari@amd.com>
    Link: https://lore.kernel.org/r/20250407170529.893307-6-vincent.stehle@arm.com

--- board/xilinx/zynqmp/zynqmp.c
+++ board/xilinx/zynqmp/zynqmp.c
@@ -668,7 +668,7 @@ void configure_capsule_updates(void)
 
 	ALLOC_CACHE_ALIGN_BUFFER(char, buf, DFU_ALT_BUF_LEN);
 
-	memset(buf, 0, sizeof(buf));
+	memset(buf, 0, DFU_ALT_BUF_LEN);
 
 	multiboot = multi_boot();
 	if (multiboot < 0)
