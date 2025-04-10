a876295e1bec85f8318807424bb9de8794a4d7f7
Simon Glass <sjg@chromium.org>
2025-04-10 06:42:59 -0600

    binman: Exclude dist-packages and site-packages
    Newer versions of the python3-coverage tool require a directory
    separator before and after the directory name. Add this so that system
    package are not included in the coverage report.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/u_boot_pylib/test_util.py
+++ tools/u_boot_pylib/test_util.py
@@ -56,7 +56,7 @@ def run_test_coverage(prog, filter_fname, exclude_list, build_dir,
     else:
         glob_list = []
     glob_list += exclude_list
-    glob_list += ['*libfdt.py', '*site-packages*', '*dist-packages*']
+    glob_list += ['*libfdt.py', '*/site-packages/*', '*/dist-packages/*']
     glob_list += ['*concurrencytest*']
     test_cmd = 'test' if 'binman' in prog or 'patman' in prog else '-t'
     prefix = ''
