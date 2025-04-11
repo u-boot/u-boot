05a2b6984159e83d91420af283dd204bdbe07d65
Tom Rini <trini@konsulko.com>
2025-04-11 11:04:06 -0600

    buildman: Update to grabbing gcc-14.2.0 toolchains by default
    With the switch to using GCC 14.2.0 in commit 001bac5f16ad ("Dockerfile:
    Update to gcc-14.2.0 and clang-18") in CI, we should make buildman match
    this.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- tools/buildman/toolchain.py
+++ tools/buildman/toolchain.py
@@ -531,7 +531,7 @@ class Toolchains:
         if arch == 'aarch64':
             arch = 'arm64'
         base = 'https://www.kernel.org/pub/tools/crosstool/files/bin'
-        versions = ['13.2.0', '12.2.0']
+        versions = ['14.2.0', '13.2.0']
         links = []
         for version in versions:
             url = '%s/%s/%s/' % (base, arch, version)
