4b1b07e3c6055edd29b98a7a30a5c7d062395df7
Daniel Schultz <d.schultz@phytec.de>
2025-04-15 08:12:41 -0700

    mach-k3: common_fdt: Move carveout struct
    Labels are not allowed before declarations. Move the carveout struct
    at the beginning and only update 'end' at this point.
    
    This will fix following error:
    
    arch/arm/mach-k3/common_fdt.c: In function 'fdt_fixup_reserved':
    arch/arm/mach-k3/common_fdt.c:156:2: error: a label can only be part of a statement and a declaration is not a statement
      156 |  struct fdt_memory carveout = {
          |  ^~~~~~
    make[1]: *** [scripts/Makefile.build:256: arch/arm/mach-k3/common_fdt.o] Error 1
    make: *** [Makefile:1919: arch/arm/mach-k3] Error 2
    
    Fixes: 096aa229a9e ("mach-k3: common_fdt: create a reserved memory node")
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>
    Reviewed-by: Udit Kumar <u-kumar1@ti.com>
    Reviewed-by: Bryan Brattlof <bb@ti.com>

--- arch/arm/mach-k3/common_fdt.c
+++ arch/arm/mach-k3/common_fdt.c
@@ -119,6 +119,9 @@ int fdt_fixup_reserved(void *blob, const char *name,
 {
 	int nodeoffset, subnode;
 	int ret;
+	struct fdt_memory carveout = {
+		.start = new_address,
+	};
 
 	/* Find reserved-memory */
 	nodeoffset = fdt_subnode_offset(blob, 0, "reserved-memory");
@@ -153,10 +156,7 @@ int fdt_fixup_reserved(void *blob, const char *name,
 	}
 
 add_carveout:
-	struct fdt_memory carveout = {
-		.start = new_address,
-		.end = new_address + new_size - 1,
-	};
+	carveout.end = new_address + new_size - 1;
 	ret = fdtdec_add_reserved_memory(blob, name, &carveout, NULL, 0, NULL,
 					 FDTDEC_RESERVED_MEMORY_NO_MAP);
 	if (ret < 0)
