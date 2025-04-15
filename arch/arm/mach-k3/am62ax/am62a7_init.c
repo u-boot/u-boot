3b7893145e3621574f01e0bfde1277ec730dc5b6
Anshul Dalal <anshuld@ti.com>
2025-04-15 15:22:24 +0530

    mach-k3: add eMMC FS boot support for am62[ap]
    This makes spl_mmc_boot_mode consistent across am62x, 62a and 62p.
    
    If MMCSD_MODE_EMMCBOOT is returned, FS boot fails since it checks for FS
    on the hardware partitions, not the UDA. So to allow FS boot from EMMC,
    the function should return MMCSD_MODE_FS instead which allows us to read
    from FS on the UDA.
    
    Signed-off-by: Anshul Dalal <anshuld@ti.com>

--- arch/arm/mach-k3/am62ax/am62a7_init.c
+++ arch/arm/mach-k3/am62ax/am62a7_init.c
@@ -216,6 +216,10 @@ u32 spl_mmc_boot_mode(struct mmc *mmc, const u32 boot_device)
 
 	switch (bootmode) {
 	case BOOT_DEVICE_EMMC:
+		if (IS_ENABLED(CONFIG_SUPPORT_EMMC_BOOT))
+			return MMCSD_MODE_EMMCBOOT;
+		if (IS_ENABLED(CONFIG_SPL_FS_FAT) || IS_ENABLED(CONFIG_SPL_FS_EXT4))
+			return MMCSD_MODE_FS;
 		return MMCSD_MODE_EMMCBOOT;
 	case BOOT_DEVICE_MMC:
 		if (bootmode_cfg & MAIN_DEVSTAT_PRIMARY_MMC_FS_RAW_MASK)
