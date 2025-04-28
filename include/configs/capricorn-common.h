eda18cc2c3db0448bc7a710e7f82b59638271264
Walter Schweizer <walter.schweizer@siemens.com>
2025-04-28 07:28:54 +0200

    siemens: capricorn: enable text based default environment
    enable text based default U-Boot Environment by enabling
    CONFIG_ENV_SOURCE_FILE
    
    and adding default environment file:
    
    board/siemens/capricorn/capricorn_cxg3.env
    
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Signed-off-by: Walter Schweizer <walter.schweizer@siemens.com>

--- include/configs/capricorn-common.h
+++ include/configs/capricorn-common.h
@@ -10,8 +10,6 @@
 #include <linux/sizes.h>
 #include <asm/arch/imx-regs.h>
 
-#include "siemens-env-common.h"
-
 /* SPL config */
 #ifdef CONFIG_XPL_BUILD
 #define CFG_MALLOC_F_ADDR		0x00120000
@@ -36,57 +34,9 @@
 #define AHAB_ENV "sec_boot=no\0"
 #endif
 
-#define MFG_ENV_SETTINGS_DEFAULT \
-	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
-		"rdinit=/linuxrc " \
-		"clk_ignore_unused "\
-		"\0" \
-	"kboot=booti\0"\
-	"bootcmd_mfg=run mfgtool_args;" \
-	"if iminfo ${initrd_addr}; then " \
-	"if test ${tee} = yes; then " \
-		"bootm ${tee_addr} ${initrd_addr} ${fdt_addr}; " \
-	"else " \
-		"booti ${loadaddr} ${initrd_addr} ${fdt_addr}; " \
-	"fi; " \
-	"else " \
-	    "echo \"Run fastboot ...\"; fastboot 0; "  \
-	"fi;\0"
-
-/* Boot M4 */
-#define M4_BOOT_ENV \
-	"m4_0_image=m4_0.bin\0" \
-	"loadm4image_0=fatload mmc ${mmcdev}:${mmcpart} " \
-			"${loadaddr} ${m4_0_image}\0" \
-	"m4boot_0=run loadm4image_0; dcache flush; bootaux ${loadaddr} 0\0" \
-
-#define CFG_MFG_ENV_SETTINGS \
-	MFG_ENV_SETTINGS_DEFAULT \
-	"initrd_addr=0x83100000\0" \
-	"initrd_high=0xffffffffffffffff\0" \
-	"emmc_dev=0\0"
-
 /* Initial environment variables */
 #define CFG_EXTRA_ENV_SETTINGS \
-	CFG_MFG_ENV_SETTINGS \
-	M4_BOOT_ENV \
-	AHAB_ENV \
-	ENV_COMMON \
-	"script=boot.scr\0" \
-	"image=Image\0" \
-	"panel=NULL\0" \
-	"console=ttyLP2\0" \
-	"fdt_addr=0x83000000\0" \
-	"fdt_high=0xffffffffffffffff\0" \
-	"cntr_addr=0x88000000\0" \
-	"cntr_file=os_cntr_signed.bin\0" \
-	"initrd_addr=0x83800000\0" \
-	"initrd_high=0xffffffffffffffff\0" \
-	"netdev=eth0\0" \
-	"nfsopts=vers=3,udp,rsize=4096,wsize=4096,nolock rw\0" \
-	"hostname=capricorn\0" \
-	ENV_EMMC \
-	ENV_NET
+	AHAB_ENV
 
 /* Default location for tftp and bootm */
 
