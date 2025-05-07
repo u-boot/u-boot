4f7bd6cae8fa3697d8b90a15b83c47087933f61b
Simon Glass <sjg@chromium.org>
2025-05-07 17:50:20 +0200

    patman: Add all files to __init__.py
    Some files are missing from the __all__ list, so add then. Reformat the
    list to use more of the width of each line.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/patman/__init__.py
+++ tools/patman/__init__.py
@@ -1,5 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0+
 
-__all__ = ['checkpatch', 'commit', 'control', 'func_test', 'get_maintainer',
-           '__main__', 'patchstream', 'project', 'series',
-           'settings', 'setup', 'status', 'test_checkpatch', 'test_settings']
+__all__ = [
+    'checkpatch', 'cmdline', 'commit', 'control', 'func_test',
+    'get_maintainer', '__main__', 'patchstream', 'patchwork', 'project',
+    'send', 'series', 'settings', 'setup', 'status', 'test_checkpatch',
+    'test_settings'
+]
