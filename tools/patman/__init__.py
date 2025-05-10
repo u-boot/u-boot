ce785fd9b7d6b1176349ba7717576548c95a3e2b
Simon Glass <sjg@chromium.org>
2025-05-10 13:05:04 +0200

    patman: Add a Cseries class
    This is the main class for dealing with series, across branches and the
    database.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/patman/__init__.py
+++ tools/patman/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 __all__ = [
-    'checkpatch', 'cmdline', 'commit', 'control', 'cser_helper',
+    'checkpatch', 'cmdline', 'commit', 'control', 'cser_helper', 'cseries',
     'database', 'func_test', 'get_maintainer', '__main__', 'patchstream',
     'patchwork', 'project',
     'send', 'series', 'settings', 'setup', 'status', 'test_checkpatch',
