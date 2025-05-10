24e7625085ee14a55275e2fbdaa5e595af1d632f
Simon Glass <sjg@chromium.org>
2025-05-10 13:05:03 +0200

    patman: Add a helper for managing cseries
    Add a module which includes helper functions for dealing with Cseries
    objects.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/patman/__init__.py
+++ tools/patman/__init__.py
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 __all__ = [
-    'checkpatch', 'cmdline', 'commit', 'control',
-    'database', 'func_test',
-    'get_maintainer', '__main__', 'patchstream', 'patchwork', 'project',
+    'checkpatch', 'cmdline', 'commit', 'control', 'cser_helper',
+    'database', 'func_test', 'get_maintainer', '__main__', 'patchstream',
+    'patchwork', 'project',
     'send', 'series', 'settings', 'setup', 'status', 'test_checkpatch',
     'test_common', 'test_settings'
 ]
