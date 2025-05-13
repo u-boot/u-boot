38ed760bc90722207060ff16e370e4ea3f7f3db3
Yao Zi <ziyao@disroot.org>
2025-05-13 09:05:02 +0000

    board: thead: licheepi4a: Enable SPL support
    Adjust Kconfig and defconfig and add SPL initialization code for
    Lichee Pi 4A. Then enable SPL support which we've added for TH1520 SoC
    earlier. The board devicetree is changed to use TH1520 binman
    configuration to generate bootable images.
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

---
+++ board/thead/th1520_lpi4a/spl.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2025, Yao Zi <ziyao@disroot.org>
+ */
+
+#include <asm/io.h>
+#include <asm/spl.h>
+#include <asm/arch/cpu.h>
+#include <asm/arch/spl.h>
+#include <cpu_func.h>
+#include <dm.h>
+#include <hang.h>
+#include <spl.h>
+
+u32 spl_boot_device(void)
+{
+	/*
+	 * We don't bother to load proper U-Boot from an external device as
+	 * it fits in the integrated SRAM nicely.
+	 */
+	return BOOT_DEVICE_RAM;
+}
+
+void board_init_f(ulong dummy)
+{
+	int ret = spl_early_init();
+	struct udevice *dev;
+
+	if (ret)
+		panic("spl_early_init() failed %d\n", ret);
+
+	preloader_console_init();
+
+	/*
+	 * Manually bind CPU ahead of time to make sure in-core timers are
+	 * available in SPL.
+	 */
+	ret = uclass_get_device(UCLASS_CPU, 0, &dev);
+	if (ret)
+		panic("failed to bind CPU: %d\n", ret);
+
+	spl_dram_init();
+
+	icache_enable();
+	dcache_enable();
+
+	th1520_invalidate_pmp();
+}
