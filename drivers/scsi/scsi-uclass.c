9a2010941f56929cb28432cc3f4b37a944f55b8e
Varadarajan Narayanan <quic_varada@quicinc.com>
2025-05-13 14:47:07 +0530

    scsi: Implement get_blk() function
    Add a function to obtain the block device for SCSI.
    
    Signed-off-by: Varadarajan Narayanan <quic_varada@quicinc.com>
    Acked-by: Casey Connolly <casey.connolly@linaro.org>

--- drivers/scsi/scsi-uclass.c
+++ drivers/scsi/scsi-uclass.c
@@ -10,7 +10,9 @@
 
 #define LOG_CATEGORY UCLASS_SCSI
 
+#include <blk.h>
 #include <dm.h>
+#include <part.h>
 #include <scsi.h>
 
 int scsi_exec(struct udevice *dev, struct scsi_cmd *pccb)
@@ -21,6 +23,34 @@ int scsi_exec(struct udevice *dev, struct scsi_cmd *pccb)
 		return -ENOSYS;
 
 	return ops->exec(dev, pccb);
+}
+
+int scsi_get_blk_by_uuid(const char *uuid,
+			 struct blk_desc **blk_desc_ptr,
+			 struct disk_partition *part_info_ptr)
+{
+	static int is_scsi_scanned;
+	struct blk_desc *blk;
+	int i, ret;
+
+	if (!is_scsi_scanned) {
+		scsi_scan(false /* no verbose */);
+		is_scsi_scanned = 1;
+	}
+
+	for (i = 0; i < blk_find_max_devnum(UCLASS_SCSI) + 1; i++) {
+		ret = blk_get_desc(UCLASS_SCSI, i, &blk);
+		if (ret)
+			continue;
+
+		ret = part_get_info_by_uuid(blk, uuid, part_info_ptr);
+		if (ret > 0) {
+			*blk_desc_ptr = blk;
+			return 0;
+		}
+	}
+
+	return -1;
 }
 
 int scsi_bus_reset(struct udevice *dev)
