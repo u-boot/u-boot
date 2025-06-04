95bc85931125252b1a32c6d115c62d840a3948f6
Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>
2025-06-04 17:03:11 +0800

    sysreset: socfpga: soc64: Enable L2 reset
    Put all slave CPUs (CPU1-3) into WFI mode. Master CPU (CPU0) writes
    the magic word into system manager's scratch register to indicate
    the system has performed L2 reset and request reset manager to
    perform hardware handshake and then trigger L2 reset. CPU0 put
    itself into WFI mode. L2 reset will reboot all HPS CPU cores after
    which all HPS cores are in WFI mode. L2 reset is followed by warm
    reset request by SPL via RMR_EL3 system register.
    To trigger L2 + warm reset under u-boot, set 'reset=warm' in the
    u-boot environment then input 'reset' command in the u-boot command
    prompt.
    
    Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -26,6 +26,15 @@
 #define VBAR_EL3_BASE_ADDR		0xFFD12218
 
 /*
+ * Share sysmgr.boot_scratch_cold6 & 7 (64bit) with VBAR_LE3_BASE_ADDR
+ * Indicate L2 reset is done. HPS should trigger warm reset via RMR_EL3.
+ */
+#define L2_RESET_DONE_REG		0xFFD12218
+
+/* Magic word to indicate L2 reset is completed */
+#define L2_RESET_DONE_STATUS		0x1228E5E7
+
+/*
  * U-Boot console configurations
  */
 
