u-boot/u-boot

commit dcc18ce0dbaf93d82ef7f0c6fe625c42313eca32

cli: Implement delete-word in cread_line()
The Ctrl-W option is implemented in the cread_line_simple() but not in
the full version. This seems odd, so add an implementation.

Signed-off-by: Simon Glass <sjg@chromium.org>

common/cli_readline.c
@@ -89,6 +89,14 @@ static char hist_lines[HIST_MAX][HIST_SIZE + 1];	/* Save room for NULL */
 
 #define add_idx_minus_one() ((hist_add_idx == 0) ? hist_max : hist_add_idx-1)
 
+static void getcmd_putchars(int count, int ch)
+{
+	int i;
+
+	for (i = 0; i < count; i++)
+		getcmd_putch(ch);
+}
+
 static void hist_init(void)
 {
 	int i;
@@ -336,6 +344,29 @@ static int cread_line(const char *const prompt, char *buf, unsigned int *len,
 			break;
 		case CTL_CH('o'):
 			insert = !insert;
+			break;
+		case CTL_CH('w'):
+			if (num) {
+				uint base;
+
+				for (base = num - 1;
+				     base >= 0 && buf[base] == ' ';)
+					base--;
+				for (; base > 0 && buf[base - 1] != ' ';)
+					base--;
+
+				/* now delete chars from base to num */
+				wlen = num - base;
+				eol_num -= wlen;
+				memmove(&buf[base], &buf[num],
+					eol_num - base + 1);
+				num = base;
+				getcmd_putchars(wlen, CTL_BACKSPACE);
+				puts(buf + base);
+				getcmd_putchars(wlen, ' ');
+				getcmd_putchars(wlen + eol_num - num,
+						CTL_BACKSPACE);
+			}
 			break;
 		case CTL_CH('x'):
 		case CTL_CH('u'):

commit 78e0771843d1d61d82d526db069ee6f5363164d8

common: readline: Fix always true test
The variable base is unsigned so >= 0 is always true. Fix this test
so that it is actually useful.

Fixes: dcc18ce0dbaf ("cli: Implement delete-word in cread_line()")
Signed-off-by: Andrew Goodbody <andrew.goodbody@linaro.org>

common/cli_readline.c
@@ -333,9 +333,9 @@ int cread_line_process_ch(struct cli_line_state *cls, char ichar)
 			uint base, wlen;
 
 			for (base = cls->num - 1;
-			     base >= 0 && buf[base] == ' ';)
+			     base > 0 && buf[base] == ' ';)
 				base--;
-			for (; base > 0 && buf[base - 1] != ' ';)
+			for (; base > 1 && buf[base - 1] != ' ';)
 				base--;
 
 			/* now delete chars from base to cls->num */