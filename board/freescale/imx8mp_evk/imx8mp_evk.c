441ca189e04472b4b807a02f0b0fe930ee6ca2a1
Peng Fan <peng.fan@nxp.com>
2025-06-23 15:02:29 +0800

    imx8mp: evk: Add support for capsule update
    Capsule update is EFI based firmware update which is widely
    used in various OS distributions. This feature is required
    by ARM System-Ready compliance test. So
     - Define image array and GUID
     - Select configs for EFI Capsule update
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx8mp_evk/imx8mp_evk.c
+++ board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -4,7 +4,30 @@
  */
 
 #include <asm/arch/sys_proto.h>
+#include <config.h>
+#include <efi_loader.h>
 #include <env.h>
+
+#if CONFIG_IS_ENABLED(EFI_HAVE_CAPSULE_SUPPORT)
+#define IMX_BOOT_IMAGE_GUID \
+	EFI_GUID(0x928b33bc, 0xe58b, 0x4247, 0x9f, 0x1d, \
+		 0x3b, 0xf1, 0xee, 0x1c, 0xda, 0xff)
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = IMX_BOOT_IMAGE_GUID,
+		.fw_name = u"IMX8MP-EVK-RAW",
+		.image_index = 1,
+	},
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "mmc 2=flash-bin raw 0 0x2000 mmcpart 1",
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
+#endif /* EFI_HAVE_CAPSULE_SUPPORT */
+
 
 int board_init(void)
 {
