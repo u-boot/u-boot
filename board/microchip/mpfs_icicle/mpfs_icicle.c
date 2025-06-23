7c16ebba1ed021f284f98260b378141bf7b9c59a
Conor Dooley <conor.dooley@microchip.com>
2025-06-23 12:51:36 +0100

    board: mpfs_icicle: implement board_fdt_blob_setup()/board_fit_config_name_match()
    The firmware on the Icicle is capable of providing a devicetree in a1 to
    U-Boot, but until now the devicetree has been packaged in a "payload" [1]
    alongside U-Boot (or other bootloaders/RTOSes) and appended to the image.
    The address of this appended devicetree is placed in a1 by the firmware.
    This meant that the mechanism used by OF_SEPARATE to locate the
    devicetree at the end of the image would pick up the one provided by the
    firmware when u-boot-nodtb.bin was in the payload and U-Boot's devicetree
    when u-boot.bin was.
    
    The firmware is now going to be capable of providing a minimal devicetree
    (quite cut down due to severe space constraints), but this devicetree is
    linked into the firmware that runs out of the L2 rather than at the end
    of the U-Boot image.
    
    Implement board_fdt_blob_setup() so that this devicetree can be
    optionally used, and the devicetree provided in the "payload" can be
    used without relying on "happening" to implement the same strategy as
    OF_SEPARATE expects in combination with u-boot-nodtb.bin.
    Unlike other RISC-V boards, the firmware provided devicetree is only
    used when OF_BOARD is set, so that the almost certainly more complete
    devicetree in U-Boot will be used unless explicitly requested otherwise.
    
    Implement board_fit_config_name_match(), so that, using the firmware
    provided cut-down/minimal dtb, U-Boot can select one of several
    devicetrees when MULTI_DTB_FIT is enabled.
    
    Enabling both MULTI_DTB_FIT and OF_BOARD will lead to a conflict
    between the two options, with the latter taking priority due to
    board_fdt_blob_setup() being executed before board_fit_config_name_match(),
    which causes gd->fdt_blob to be overwritten with a pointer to the
    minimal devicetree rather than the location of the fit image containing
    the multiple dtbs. Let MULTI_DTB_FIT take priority in this case, by
    explicitly blocking the override when MULTI_DTB_FIT is enabled.
    
    Link: https://github.com/polarfire-soc/hart-software-services/blob/master/tools/hss-payload-generator/README.md [1]
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>
    Acked-by: Leo Yu-Chi Liang <ycliang@andestech.com>

--- board/microchip/mpfs_icicle/mpfs_icicle.c
+++ board/microchip/mpfs_icicle/mpfs_icicle.c
@@ -9,6 +9,7 @@
 #include <init.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
+#include <asm/sections.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -48,6 +49,68 @@ static void read_device_serial_number(u8 *response, u8 response_size)
 
 	for (idx = 0; idx < response_size; idx++)
 		response_buf[idx] = readb(MPFS_SYS_SERVICE_MAILBOX + idx);
+}
+
+#if defined(CONFIG_MULTI_DTB_FIT)
+int board_fit_config_name_match(const char *name)
+{
+	const void *fdt;
+	int list_len;
+
+	/*
+	 * If there's not a HSS provided dtb, there's no point re-selecting
+	 * since we'd just end up re-selecting the same dtb again.
+	 */
+	if (!gd->arch.firmware_fdt_addr)
+		return -EINVAL;
+
+	fdt = (void *)gd->arch.firmware_fdt_addr;
+
+	list_len = fdt_stringlist_count(fdt, 0, "compatible");
+	if (list_len < 1)
+		return -EINVAL;
+
+	for (int i = 0; i < list_len; i++) {
+		int len, match;
+		const char *compat;
+		char *devendored;
+
+		compat = fdt_stringlist_get(fdt, 0, "compatible", i, &len);
+		if (!compat)
+			return -EINVAL;
+
+		strtok((char *)compat, ",");
+
+		devendored = strtok(NULL, ",");
+		if (!devendored)
+			return -EINVAL;
+
+		match = strcmp(devendored, name);
+		if (!match)
+			return 0;
+	}
+
+	return -EINVAL;
+}
+#endif
+
+int board_fdt_blob_setup(void **fdtp)
+{
+	fdtp = (void *)_end;
+
+	/*
+	 * The devicetree provided by the previous stage is very minimal due to
+	 * severe space constraints. The firmware performs no fixups etc.
+	 * U-Boot, if providing a devicetree, almost certainly has a better
+	 * more complete one than the firmware so that provided by the firmware
+	 * is ignored for OF_SEPARATE.
+	 */
+	if (IS_ENABLED(CONFIG_OF_BOARD) && !IS_ENABLED(CONFIG_MULTI_DTB_FIT)) {
+		if (gd->arch.firmware_fdt_addr)
+			fdtp = (void *)(uintptr_t)gd->arch.firmware_fdt_addr;
+	}
+
+	return 0;
 }
 
 int board_init(void)
