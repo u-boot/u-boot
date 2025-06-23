53f328663da65c6c4c62bf1312c96d363d3f5f05
Tony Dinh <mibodhi@gmail.com>
2025-06-23 13:59:43 -0700

    arm: kirkwood: Enable bootstd and other modernization for ib62x0 board
    Enable bootstd.
    Update defconfigs to the latest u-boot requirements.
    Enable SYS_THUMB_BUILD and LTO and to reduce binary size.
    Enable NET_RANDOM_ETHADDR.
    Disable JFFS2 support.
    
    Signed-off-by: Tony Dinh <mibodhi@gmail.com>
    Reviewed-by: Stefan Roese <sr@denx.de>

--- include/configs/ib62x0.h
+++ include/configs/ib62x0.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
+ * Copyright (C) 2025 Tony Dinh <mibodhi@gmail.com>
  * Copyright (C) 2011-2012
  * Gerald Kerma <dreagle@doukki.net>
  * Luka Perkov <luka@openwrt.org>
@@ -11,18 +12,33 @@
 #include "mv-common.h"
 
 /*
- * Environment variables configuration
+ * mv-common.h should be defined after CMD configs since it used them
+ * to enable certain macros
  */
+#include "mv-common.h"
 
-/*
- * Default environment variables
- */
-
-#define CFG_EXTRA_ENV_SETTINGS \
+#define EXTRA_ENV_SETTINGS_LEGACY \
 	"console=console=ttyS0,115200\0"				\
 	"kernel=/boot/zImage\0"						\
 	"fdt=/boot/ib62x0.dtb\0"					\
 	"bootargs_root=ubi.mtd=2 root=ubi0:rootfs rootfstype=ubifs rw\0"
+
+#define KERNEL_ADDR_R	__stringify(0x800000)
+#define FDT_ADDR_R	__stringify(0x2c00000)
+#define RAMDISK_ADDR_R	__stringify(0x01100000)
+#define SCRIPT_ADDR_R	__stringify(0x200000)
+
+#define LOAD_ADDRESS_ENV_SETTINGS \
+	"kernel_addr_r=" KERNEL_ADDR_R "\0" \
+	"fdt_addr_r=" FDT_ADDR_R "\0" \
+	"ramdisk_addr_r=" RAMDISK_ADDR_R "\0" \
+	"scriptaddr=" SCRIPT_ADDR_R "\0"
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	EXTRA_ENV_SETTINGS_LEGACY \
+	LOAD_ADDRESS_ENV_SETTINGS \
+	"fdtfile=" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
+	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0"
 
 /*
  * SATA driver configuration
