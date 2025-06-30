a36e87127a39734bff1896a4fa5bdab546bce6f4
Marek Vasut <marek.vasut@mailbox.org>
2025-06-30 02:10:29 +0200

    ARM: stm32: Limit early cache enablement in SPL to STM32MP15xx
    The STM32MP13xx SRAM size is half that the SRAM size on STM32MP15xx,
    disable early dcache start on STM32MP13xx as the TLB itself takes
    about a quarter of the SPL size. The dcache will be enabled later,
    once DRAM is available and TLB can be placed in DRAM.
    
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Signed-off-by: Marek Vasut <marek.vasut@mailbox.org>

--- arch/arm/mach-stm32mp/stm32mp1/spl.c
+++ arch/arm/mach-stm32mp/stm32mp1/spl.c
@@ -220,10 +220,11 @@ void board_init_f(ulong dummy)
 	 * activate cache on DDR only when DDR is fully initialized
 	 * to avoid speculative access and issue in get_ram_size()
 	 */
-	if (!CONFIG_IS_ENABLED(SYS_DCACHE_OFF))
+	if (!CONFIG_IS_ENABLED(SYS_DCACHE_OFF) && !IS_ENABLED(CONFIG_STM32MP13X)) {
 		mmu_set_region_dcache_behaviour(STM32_DDR_BASE,
 						CONFIG_DDR_CACHEABLE_SIZE,
 						DCACHE_DEFAULT_OPTION);
+	}
 }
 
 void spl_board_prepare_for_boot(void)
