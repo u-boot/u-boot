6e20aa243d0b373cfd30e5c47c357d132bf795e2
Magnus Damm <damm@opensource.se>
2025-07-02 19:13:46 +0200

    ARM: renesas: Put common r7s72100 code in board/renesas/common
    Break out SoC specific code from the GR-Peach board and put it into the
    board/renesas/common directory so it can be easily shared between the
    GR-Peach and Genmai boards.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>

--- board/renesas/grpeach/grpeach.c
+++ board/renesas/grpeach/grpeach.c
@@ -4,17 +4,9 @@
  * Copyright (C) Chris Brandt
  */
 
-#include <cpu_func.h>
 #include <errno.h>
 #include <init.h>
 #include <asm/global_data.h>
-#include <asm/io.h>
-#include <asm/arch/sys_proto.h>
-
-#define RZA1_WDT_BASE	0xfcfe0000
-#define WTCSR		0x00
-#define WTCNT		0x02
-#define WRCSR		0x04
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -38,18 +30,4 @@ int dram_init_banksize(void)
 	fdtdec_setup_memory_banksize();
 
 	return 0;
-}
-
-void reset_cpu(void)
-{
-	/* Dummy read (must read WRCSR:WOVF at least once before clearing) */
-	readb(RZA1_WDT_BASE + WRCSR);
-
-	writew(0xa500, RZA1_WDT_BASE + WRCSR);
-	writew(0x5a5f, RZA1_WDT_BASE + WRCSR);
-	writew(0x5a00, RZA1_WDT_BASE + WTCNT);
-	writew(0xa578, RZA1_WDT_BASE + WTCSR);
-
-	for (;;)
-		asm volatile("wfi");
 }
