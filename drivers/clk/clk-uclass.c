644b4650ee57c429bede77f44752cc867dac0e00
Andrew Goodbody <andrew.goodbody@linaro.org>
2025-07-03 15:40:46 +0100

    clk: Return value calculated by ERR_PTR
    In clk_set_default_get_by_id ret is passed to ERR_PTR but nothing is
    done with the value that this calculates which is obviously not the
    intention of the code. This is confirmed by the code around where this
    function is called.
    Instead return the value from ERR_PTR.
    
    This issue found by Smatch.
    
    Signed-off-by: Andrew Goodbody <andrew.goodbody@linaro.org>
    Acked-by: Quentin Schulz <quentin.schulz@cherry.de>

--- drivers/clk/clk-uclass.c
+++ drivers/clk/clk-uclass.c
@@ -199,7 +199,7 @@ static struct clk *clk_set_default_get_by_id(struct clk *clk)
 		if (ret) {
 			debug("%s(): could not get parent clock pointer, id %lu\n",
 			      __func__, clk->id);
-			ERR_PTR(ret);
+			return ERR_PTR(ret);
 		}
 	}
 
