dc323f3bee318514af9d7c6fed1e01c712bc71ae
Rasmus Villemoes <ravi@prevas.dk>
2025-07-07 22:36:53 +0200

    move more limits from kernel.h to limits.h and standardize their definitions
    In a customer project that was building a stand-alone application, I
    hit a problem related to the fact that our LONG_MAX and friends are
    not standards-compliant, in that they are not "suitable for use in #if
    preprocessing directives"
    
    ... /toolchain_none/arm-cortexa8-eabi/sys-include/machine/_default_types.h:25:31: error: missing binary operator before token "long"
       25 |   || ( defined(LLONG_MAX) && (LLONG_MAX > 0x7fffffff) )
          |                               ^~~~~~~~~
    
    So following up on commit 13de8483388 ("mbedtls: add mbedtls into the
    build system"), move the rest of the macros associated to the standard
    C types {signed,unsigned} {char, short, int, long, long long} (and of
    course bare 'char') to limits.h.
    
    Make use of the fact that both gcc and clang provide suitable
    predefined __FOO_MAX__ macros for the signed types, and use a standard
    scheme for defining the FOO_MIN and UFOO_MAX macros in terms of
    FOO_MAX.
    
    Note that suffixes like L and ULL are allowed for preprocessor
    integers; it is (casts) which are not. And using appropriate suffixes,
    we can arrange for the type of e.g. UINT_MAX to be "unsigned int" due
    to integer promotion rules.
    
    Signed-off-by: Rasmus Villemoes <ravi@prevas.dk>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -5,17 +5,6 @@
 #include <linux/printk.h> /* for printf/pr_* utilities */
 #include <limits.h>
 
-#define USHRT_MAX	((u16)(~0U))
-#define SHRT_MAX	((s16)(USHRT_MAX>>1))
-#define SHRT_MIN	((s16)(-SHRT_MAX - 1))
-#define INT_MIN		(-INT_MAX - 1)
-#define LONG_MAX	((long)(~0UL>>1))
-#define LONG_MIN	(-LONG_MAX - 1)
-#define ULONG_MAX	(~0UL)
-#define LLONG_MAX	((long long)(~0ULL>>1))
-#define LLONG_MIN	(-LLONG_MAX - 1)
-#define ULLONG_MAX	(~0ULL)
-
 #define U8_MAX		((u8)~0U)
 #define S8_MAX		((s8)(U8_MAX>>1))
 #define S8_MIN		((s8)(-S8_MAX - 1))
