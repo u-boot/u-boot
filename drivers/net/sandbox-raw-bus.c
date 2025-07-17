5c0827eededc8392472823d8e3e50cd0f5fad47b
Andrew Goodbody <andrew.goodbody@linaro.org>
2025-07-17 12:09:15 +0100

    sandbox: eth-raw: Prevent possible buffer overflow
    Instead of strcpy which is unbounded use strlcpy to ensure that the
    receiving buffer cannot be overflowed.
    
    This issue found by Smatch.
    
    Signed-off-by: Andrew Goodbody <andrew.goodbody@linaro.org>

--- drivers/net/sandbox-raw-bus.c
+++ drivers/net/sandbox-raw-bus.c
@@ -42,7 +42,7 @@ static int eth_raw_bus_post_bind(struct udevice *dev)
 		device_probe(child);
 		priv = dev_get_priv(child);
 		if (priv) {
-			strcpy(priv->host_ifname, i->if_name);
+			strlcpy(priv->host_ifname, i->if_name, IFNAMSIZ);
 			priv->host_ifindex = i->if_index;
 			priv->local = local;
 		}
