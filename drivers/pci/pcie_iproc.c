5bad0bc4f7beeba8e972005fd0901cf368315815
Tom Rini <trini@konsulko.com>
2025-07-17 19:15:21 -0600

    pci: Add missing <linux/sizes.h> to pcie_iproc.c
    This driver references the SZ_ macros while relying on an indirection
    inclusion of <linux/sizes.h>. Add the missing include directly.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- drivers/pci/pcie_iproc.c
+++ drivers/pci/pcie_iproc.c
@@ -13,6 +13,7 @@
 #include <dm/device_compat.h>
 #include <linux/delay.h>
 #include <linux/log2.h>
+#include <linux/sizes.h>
 
 #define EP_PERST_SOURCE_SELECT_SHIFT 2
 #define EP_PERST_SOURCE_SELECT       BIT(EP_PERST_SOURCE_SELECT_SHIFT)
