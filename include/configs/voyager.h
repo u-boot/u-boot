409c73ef6368b07d4dbdf8391a0b9c7682dcf4b4
Randolph Lin <randolph@andestech.com>
2025-08-21 19:31:22 +0800

    include: configs: andes: Remove fdt_high env variable
    Remove the fdt_high environment variable, as a value of all ones
    indicates using the FDT in place. This setting is incorrect for the
    current board.
    
    Signed-off-by: Randolph Lin <randolph@andestech.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

--- include/configs/voyager.h
+++ include/configs/voyager.h
@@ -26,7 +26,6 @@
 #include <config_distro_bootcmd.h>
 
 #define CFG_EXTRA_ENV_SETTINGS	\
-				"fdt_high=0xffffffffffffffff\0" \
 				"initrd_high=0xffffffffffffffff\0" \
 				"kernel_addr_r=0x400600000\0" \
 				"kernel_comp_addr_r=0x404600000\0" \
