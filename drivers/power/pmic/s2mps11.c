38443338c7d491852d21d6c5e7e742fc347b9a44
Henrik Grimler <henrik@grimler.se>
2025-08-22 20:33:23 +0200

    power: pmic: s2mps11: look for both {voltage-,}regulators
    Linux's DTSes uses regulators { }; while u-boot's DTSes uses
    voltage-regulators { };.  Look for regulators, and fallback to
    voltage-regulators if not found, so that both type of DTSes can be
    used with the driver.
    
    Signed-off-by: Henrik Grimler <henrik@grimler.se>
    Reviewed-by: Anand Moon <linux.amoon@gmail.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- drivers/power/pmic/s2mps11.c
+++ drivers/power/pmic/s2mps11.c
@@ -52,11 +52,14 @@ static int s2mps11_probe(struct udevice *dev)
 	ofnode regulators_node;
 	int children;
 
-	regulators_node = dev_read_subnode(dev, "voltage-regulators");
+	regulators_node = dev_read_subnode(dev, "regulators");
 	if (!ofnode_valid(regulators_node)) {
-		debug("%s: %s regulators subnode not found!\n", __func__,
-		      dev->name);
-		return -ENXIO;
+		regulators_node = dev_read_subnode(dev, "voltage-regulators");
+		if (!ofnode_valid(regulators_node)) {
+			debug("%s: %s regulators subnode not found!\n", __func__,
+			      dev->name);
+			return -ENXIO;
+		}
 	}
 
 	debug("%s: '%s' - found regulators subnode\n", __func__, dev->name);
