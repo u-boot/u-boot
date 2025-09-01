7e2c23eacd8e6fe76306ebf3cd6e31d52695b617
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2025-09-01 17:16:35 +0200

    bloblist: use correct types for physical addresses
    It is expected that bloblists are stored in high memory beyond 2 GiB.
    We must not use int as data type for these addresses but phys_addr_t.
    
    Fixes: f9ef9fb033d5 ("bloblist: Handle alignment with a void entry")
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- common/bloblist.c
+++ common/bloblist.c
@@ -149,7 +149,8 @@ static int bloblist_addrec(uint tag, int size, int align_log2,
 {
 	struct bloblist_hdr *hdr = gd->bloblist;
 	struct bloblist_rec *rec;
-	int data_start, aligned_start, new_alloced;
+	phys_addr_t data_start, aligned_start;
+	phys_size_t new_alloced;
 
 	if (!align_log2)
 		align_log2 = BLOBLIST_BLOB_ALIGN_LOG2;
