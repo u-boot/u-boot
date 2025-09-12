2dbde3f9b08771b8182fcbf0bb0309acaff6c2e1
Ye Li <ye.li@nxp.com>
2025-09-12 17:41:11 +0800

    arm: armv8: Fix spl recover data section broken
    SPL recover data section is broken which causes reboot failure on
    some i.MX platforms (iMX8QM/iMX95).
    The global variable cold_reboot_flag is assigned to weak reset_flag
    function which always return 1, so restore never been executed in
    warm reboot.
    
    Fixes: 1c37e59bfbba ("arm: armv8: Improve SPL data save and restore implementation")
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/cpu/armv8/spl_data.c
+++ arch/arm/cpu/armv8/spl_data.c
@@ -11,15 +11,15 @@ char __data_save_end[0] __section(".__data_save_end");
 
 u32 cold_reboot_flag = 1;
 
-u32 __weak reset_flag(void)
+u32 __weak reset_flag(u32 flag)
 {
-	return 1;
+	return flag;
 }
 
 void spl_save_restore_data(void)
 {
 	u32 data_size = __data_save_end - __data_save_start;
-	cold_reboot_flag = reset_flag();
+	cold_reboot_flag = reset_flag(cold_reboot_flag);
 
 	if (cold_reboot_flag == 1) {
 		/* Save data section to data_save section */
