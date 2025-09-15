36d9587fa8b3590ae07ca16a2fe2aa6c905fc102
Greentime Hu <greentime.hu@sifive.com>
2025-09-15 15:44:27 +0800

    arch/riscv: Remove unused macro in encoding.h
    This patch remove the unused macro DRAM_BASE.
    
    Signed-off-by: Greentime Hu <greentime.hu@sifive.com>
    Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

--- arch/riscv/include/asm/encoding.h
+++ arch/riscv/include/asm/encoding.h
@@ -95,7 +95,6 @@
 #define DEFAULT_MTVEC		0x00001010
 #define CFG_STRING_ADDR	0x0000100C
 #define EXT_IO_BASE		0x40000000
-#define DRAM_BASE		0x80000000
 
 // page table entry (PTE) fields
 #define PTE_V		0x001 // Valid
