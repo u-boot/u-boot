8f12776f1bfcb8f97afe1d0b8c754ccd5814b2b0
Luca Weiss <luca.weiss@fairphone.com>
2025-09-17 14:47:40 +0200

    gpio: qcom: Support GPIOs on PM6350 PMIC
    The GPIOs on PM6350 work fine using the qcom_spmi_gpio driver and
    enables the use of the Volume Up button Fairphone 4 smartphone.
    
    Signed-off-by: Luca Weiss <luca.weiss@fairphone.com>

--- drivers/gpio/qcom_spmi_gpio.c
+++ drivers/gpio/qcom_spmi_gpio.c
@@ -744,6 +744,7 @@ static int qcom_spmi_pmic_gpio_probe(struct udevice *dev)
 
 static const struct udevice_id qcom_spmi_pmic_gpio_ids[] = {
 	{ .compatible = "qcom,pm6150l-gpio" },
+	{ .compatible = "qcom,pm6350-gpio" },
 	{ .compatible = "qcom,pm660l-gpio" },
 	{ .compatible = "qcom,pm8550-gpio" },
 	{ .compatible = "qcom,pm8550b-gpio" },
