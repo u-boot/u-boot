50fc92938623be4a1cf1993c703812643afc9cdd
Marek Vasut <marek.vasut+renesas@mailbox.org>
2025-09-22 13:48:28 +0200

    fw_loader: Prefix the FW loader variables with the script prefix
    Add the script name as a prefix to fw_addr and fw_size variables
    to make sure they are always unique and won't easily conflict with
    user variables.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- lib/fw_loader.c
+++ lib/fw_loader.c
@@ -13,6 +13,8 @@ int request_firmware_into_buf_via_script(void *buf, size_t max_size,
 					 const char *script_name,
 					 size_t *retsize)
 {
+	char env_addr[CONFIG_SYS_CBSIZE] = { 0 };
+	char env_size[CONFIG_SYS_CBSIZE] = { 0 };
 	char *args[2] = { "run", (char *)script_name };
 	int ret, repeatable;
 	ulong addr, size;
@@ -28,13 +30,17 @@ int request_firmware_into_buf_via_script(void *buf, size_t max_size,
 		return -EINVAL;
 	}
 
+	/* Prefix the FW loader variables with the script prefix */
+	snprintf(env_addr, sizeof(env_addr), "%s_addr", script_name);
+	snprintf(env_size, sizeof(env_size), "%s_size", script_name);
+
 	/* Find out where the firmware got loaded and how long it is */
-	addr = env_get_hex("fw_addr", 0);
-	size = env_get_hex("fw_size", 0);
+	addr = env_get_hex(env_addr, 0);
+	size = env_get_hex(env_size, 0);
 
 	/* Clear the variables set by the firmware loading script */
-	env_set("fw_addr", NULL);
-	env_set("fw_size", NULL);
+	env_set(env_addr, NULL);
+	env_set(env_size, NULL);
 
 	if (!addr || !size) {
 		log_err("Firmware address (0x%lx) or size (0x%lx) are invalid.\n",
