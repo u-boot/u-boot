14dfa6b861876275e0c2015a1013bf3bdd17a1d7
Paresh Bhagat <p-bhagat@ti.com>
2025-09-23 08:58:01 +0530

    Add initial support for AM62D2-EVM
    Add initial support for AM62D2-EVM, Kconfig entries for binman, SPL,
    base DTS files for u-boot and SPL, HS-SE and HS-FS support and initial
    configs with SD-MMC, UART, eMMC and OSPI boot providing a baseline for
    further enablement.
    
    Also add labels and targets to AM62A-SK binman to enable AM62D2-EVM
    builds to reuse existing binman infrastructure.
    
    Technical Reference Manual - https://www.ti.com/lit/pdf/sprujd4
    Schematics Link - https://www.ti.com/lit/zip/sprcal5
    
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

---
+++ board/ti/am62dx/evm.c
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Board specific initialization for AM62Dx platforms
+ *
+ * Copyright (C) 2025 Texas Instruments Incorporated - https://www.ti.com/
+ *
+ */
+
+#include <asm/arch/hardware.h>
+#include <asm/io.h>
+#include <dm/uclass.h>
+#include <env.h>
+#include <fdt_support.h>
+#include <spl.h>
+#include <asm/arch/k3-ddr.h>
+
+#include "../common/fdt_ops.h"
+
+#if defined(CONFIG_XPL_BUILD)
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+	if (IS_ENABLED(CONFIG_K3_DDRSS)) {
+		if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
+			fixup_ddr_driver_for_ecc(spl_image);
+	} else {
+		fixup_memory_node(spl_image);
+	}
+}
+#endif
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+	ti_set_fdt_env(NULL, NULL);
+	return 0;
+}
+#endif
