320b191ec820705f24ab7f7981dc3b60ab9e0ee8
Ye Li <ye.li@nxp.com>
2025-09-24 10:11:27 +0800

    spi: nxp_fspi: Support i.MX8ULP flexspi
    Add i.MX8ULP flexspi compatible string and driver data.
    The flexspi on i.MX8ULP only has 16 LUT sequences and uses 1KB RX FIFO.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- drivers/spi/nxp_fspi.c
+++ drivers/spi/nxp_fspi.c
@@ -355,6 +355,15 @@ static const struct nxp_fspi_devtype_data imx8dxl_data = {
 	.little_endian = true,  /* little-endian    */
 };
 
+static const struct nxp_fspi_devtype_data imx8ulp_data = {
+	.rxfifo = SZ_1K,       /* (128  * 64 bits)  */
+	.txfifo = SZ_1K,        /* (128 * 64 bits)  */
+	.ahb_buf_size = SZ_2K,  /* (256 * 64 bits)  */
+	.quirks = 0,
+	.lut_num = 16,
+	.little_endian = true,  /* little-endian    */
+};
+
 struct nxp_fspi {
 	struct udevice *dev;
 	void __iomem *iobase;
@@ -1101,6 +1110,7 @@ static const struct udevice_id nxp_fspi_ids[] = {
 	{ .compatible = "nxp,imx8mp-fspi", .data = (ulong)&imx8mm_data, },
 	{ .compatible = "nxp,imx8qxp-fspi", .data = (ulong)&imx8qxp_data, },
 	{ .compatible = "nxp,imx8dxl-fspi", .data = (ulong)&imx8dxl_data, },
+	{ .compatible = "nxp,imx8ulp-fspi", .data = (ulong)&imx8ulp_data, },
 	{ .compatible = "nxp,imxrt1170-fspi", .data = (ulong)&imxrt1170_data, },
 	{ }
 };
