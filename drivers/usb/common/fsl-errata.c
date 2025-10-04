dc6c80056e21a27e28e1be62ac724302ef526841
Tom Rini <trini@konsulko.com>
2025-10-04 13:31:01 -0600

    global: Add expected space between '#include' and directive
    These files had '#include<file.h>' rather than '#include <file.h>' which
    while functional is not the correct style.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- drivers/usb/common/fsl-errata.c
+++ drivers/usb/common/fsl-errata.c
@@ -7,7 +7,7 @@
 
 #include <hwconfig.h>
 #include <fsl_errata.h>
-#include<fsl_usb.h>
+#include <fsl_usb.h>
 #if defined(CONFIG_FSL_LSCH2) || defined(CONFIG_FSL_LSCH3) || \
 	defined(CONFIG_ARM)
 #include <asm/arch/clock.h>
