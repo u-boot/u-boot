1fe3a731df46edaf4402535751a5c4bad3c53fe4
Marek Vasut <marek.vasut+renesas@mailbox.org>
2025-10-27 17:35:35 +0100

    gpio: renesas: Drop unused register macros
    Remove register macros for registers which are not used by this driver.
    This makes it easier to get an overview of which registers are really
    used by the driver. No functional change.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/gpio/gpio-rcar.c
+++ drivers/gpio/gpio-rcar.c
@@ -19,14 +19,7 @@
 #define GPIO_INOUTSEL	0x04	/* General Input/Output Switching Register */
 #define GPIO_OUTDT	0x08	/* General Output Register */
 #define GPIO_INDT	0x0c	/* General Input Register */
-#define GPIO_INTDT	0x10	/* Interrupt Display Register */
-#define GPIO_INTCLR	0x14	/* Interrupt Clear Register */
-#define GPIO_INTMSK	0x18	/* Interrupt Mask Register */
-#define GPIO_MSKCLR	0x1c	/* Interrupt Mask Clear Register */
 #define GPIO_POSNEG	0x20	/* Positive/Negative Logic Select Register */
-#define GPIO_EDGLEVEL	0x24	/* Edge/level Select Register */
-#define GPIO_FILONOFF	0x28	/* Chattering Prevention On/Off Register */
-#define GPIO_BOTHEDGE	0x4c	/* One Edge/Both Edge Select Register */
 #define GPIO_INEN	0x50	/* General Input Enable Register */
 
 #define RCAR_MAX_GPIO_PER_BANK		32
