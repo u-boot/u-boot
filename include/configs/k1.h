4bba3cb7266e9b0388b59a4dc1874ad55534147d
Raymond Mao <raymond.mao@riscstar.com>
2025-11-27 13:01:54 +0800

    spacemit: k1: enable SPL with debug UART
    Add SPL support featuring debug UART output for early boot
    diagnostics on K1 SoC.
    
    Signed-off-by: Raymond Mao <raymond.mao@riscstar.com>

--- include/configs/k1.h
+++ include/configs/k1.h
@@ -1,13 +1,16 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * Copyright (c) 2024, Kongyang Liu <seashell11234455@gmail.com>
+ * Copyright (C) 2025-2026, RISCstar Ltd.
  *
  */
 
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define CFG_SYS_SDRAM_BASE         0x0
-#define CFG_SYS_NS16550_IER        0x40 /* UART Unit Enable */
+#define CFG_SYS_SDRAM_BASE	    0x0
+
+#define CFG_SYS_NS16550_CLK	    14700000
+#define CFG_SYS_NS16550_IER	    0x40 /* UART Unit Enable */
 
 #endif /* __CONFIG_H */
