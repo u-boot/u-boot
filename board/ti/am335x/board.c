u-boot/u-boot

commit 0b9ff08515927e82306bacbe7f27bb74119113ff

board: ti: am335x: Do not call disabled PMIC functions
When PMIC drivers are disabled their undefined functions cannot be called.

Signed-off-by: Maarten Brock <maarten.brock@sttls.nl>

board/ti/am335x/board.c
@@ -283,7 +283,7 @@ const struct dpll_params *get_dpll_ddr_params(void)
 
 static u8 bone_not_connected_to_ac_power(void)
 {
-	if (board_is_bone()) {
+	if (IS_ENABLED(CONFIG_PMIC_TPS65217) && board_is_bone()) {
 		uchar pmic_status_reg;
 		if (tps65217_reg_read(TPS65217_STATUS,
 				      &pmic_status_reg))
@@ -331,6 +331,9 @@ const struct dpll_params *get_dpll_mpu_params(void)
 static void scale_vcores_bone(int freq)
 {
 	int usb_cur_lim, mpu_vdd;
+
+	if (!IS_ENABLED(CONFIG_PMIC_TPS65217))
+		return;
 
 	/*
 	 * Only perform PMIC configurations if board rev > A1
@@ -425,6 +428,9 @@ static void scale_vcores_bone(int freq)
 void scale_vcores_generic(int freq)
 {
 	int sil_rev, mpu_vdd;
+
+	if (!IS_ENABLED(CONFIG_DM_PMIC_TPS65910))
+		return;
 
 	/*
 	 * The GP EVM, IDK and EVM SK use a TPS65910 PMIC.  For all

commit f06e1c04bffb89cd14d559c8d3e219ab2b815093

board: ti: am335x: Fix DM_TPS65910 condition
scale_vcores_generic() calls functions implemented in
tps65910.c, not tps65910_dm.c. Change guard from CONFIG_DM_PMIC_TPS65910 to
CONFIG_SPL_POWER_TPS65910.

Fixes: 0b9ff0851592 ("board: ti: am335x: Do not call disabled PMIC functions")
Signed-off-by: Markus Schneider-Pargmann (TI.com) <msp@baylibre.com>
Signed-off-by: Maarten Brock <maarten.brock@sttls.nl>
Reviewed-by: Kory Maincent <kory.maincent@bootlin.com>
Acked-by: Maarten Brock <maarten.brock@sttls.nl>

board/ti/am335x/board.c
@@ -429,7 +429,7 @@ void scale_vcores_generic(int freq)
 {
 	int sil_rev, mpu_vdd;
 
-	if (!IS_ENABLED(CONFIG_DM_PMIC_TPS65910))
+	if (!IS_ENABLED(CONFIG_SPL_POWER_TPS65910))
 		return;
 
 	/*