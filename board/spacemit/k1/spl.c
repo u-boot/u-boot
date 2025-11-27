b426690577ca48a42fd6e27582611d309d4f69fd
Raymond Mao <raymond.mao@riscstar.com>
2025-11-27 13:01:54 +0800

    spacemit: k1: enable SPL with debug UART
    Add SPL support featuring debug UART output for early boot
    diagnostics on K1 SoC.
    
    Signed-off-by: Raymond Mao <raymond.mao@riscstar.com>

---
+++ board/spacemit/k1/spl.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2025-2026, RISCStar Ltd.
+ */
+
+#include <spl.h>
+
+void board_init_f(ulong dummy)
+{
+	int ret;
+
+	ret = spl_early_init();
+	if (ret)
+		panic("spl_early_init() failed:%d\n", ret);
+
+	riscv_cpu_setup();
+
+	preloader_console_init();
+}
+
+u32 spl_boot_device(void)
+{
+	return BOOT_DEVICE_NONE;
+}
