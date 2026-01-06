e55a57715d942c20eafcf472998a333579f4b5f4
Michal Simek <michal.simek@amd.com>
2026-01-06 08:45:39 +0100

    amd: versal2: Align distro boot variables with default memory map
    By default Versal Gen 2 is using memory map where TF-A is placed to DDR and
    there is also some space allocated for OP-TEE that's why move default
    variable setting out of this location to avoid using it when distro boot is
    used for booting.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Link: https://lore.kernel.org/r/988a6f38ed9cfbb9757b76a16cb9cfec4601de85.1767685538.git.michal.simek@amd.com

--- include/configs/amd_versal2.h
+++ include/configs/amd_versal2.h
@@ -49,12 +49,12 @@
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"fdt_addr_r=0x40000000\0" \
 	"fdt_size_r=0x400000\0" \
-	"pxefile_addr_r=0x10000000\0" \
-	"kernel_addr_r=0x18000000\0" \
+	"pxefile_addr_r=0x70000000\0" \
+	"kernel_addr_r=0x48000000\0" \
 	"kernel_size_r=0x10000000\0" \
-	"kernel_comp_addr_r=0x30000000\0" \
+	"kernel_comp_addr_r=0x50000000\0" \
 	"kernel_comp_size=0x3C00000\0" \
-	"ramdisk_addr_r=0x02100000\0" \
+	"ramdisk_addr_r=0x60000000\0" \
 	"script_size_f=0x80000\0"
 
 #if defined(CONFIG_DISTRO_DEFAULTS)
