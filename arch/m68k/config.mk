c69b6aeaa35550d386c142302b852d6b72aea76f
Kuan-Wei Chiu <visitorckw@gmail.com>
2026-01-07 20:18:34 +0000

    m68k: Add support for M68040 CPU
    Add support for the Motorola 68040 architecture. Currently, m68k
    support in U-Boot is primarily focused on ColdFire variants. Introduce
    the necessary infrastructure to support the classic M680x0 series,
    specifically targeting the M68040 as emulated by QEMU.
    
    The implementation includes exception vectors, early startup code, and
    minimal CPU initialization and relocation stubs. It also defines the
    standard m68k boot information structure used for passing hardware
    information to the operating system. To ensure compatibility, ColdFire-
    specific library objects such as cache and interrupt handling are
    excluded from the build when M68040 is selected.
    
    Additionally, apply a specific workaround during the early memory
    reservation stage. Use a manual loop to clear global data instead of
    the standard memset() function, as utilizing memset() at this point was
    observed to cause a hang on the QEMU platform.
    
    Signed-off-by: Kuan-Wei Chiu <visitorckw@gmail.com>
    Acked-by: Angelo Dureghello <angelo@kernel-space.org>
    Reviewed-by: Simon Glass <simon.glass@canonical.com>

--- arch/m68k/config.mk
+++ arch/m68k/config.mk
@@ -3,8 +3,14 @@
 # (C) Copyright 2000-2002
 # Wolfgang Denk, DENX Software Engineering, wd@denx.de.
 
-PLATFORM_CPPFLAGS += -D__M68K__ -fPIC
+PLATFORM_CPPFLAGS += -D__M68K__
+ifneq ($(CONFIG_M680x0),y)
+PLATFORM_CPPFLAGS += -fPIC
+endif
 KBUILD_LDFLAGS    += -n -pie
 PLATFORM_RELFLAGS += -ffunction-sections -fdata-sections
-PLATFORM_RELFLAGS += -ffixed-d7 -msep-data
+PLATFORM_RELFLAGS += -ffixed-d7
+ifneq ($(CONFIG_M680x0),y)
+PLATFORM_RELFLAGS += -msep-data
+endif
 LDFLAGS_FINAL     += --gc-sections -pie
