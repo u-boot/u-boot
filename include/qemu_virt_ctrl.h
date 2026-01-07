0429298a1a76b971c0b0156918f927d6329e53bc
Kuan-Wei Chiu <visitorckw@gmail.com>
2026-01-07 20:18:33 +0000

    sysreset: Add QEMU virtual system controller driver
    Introduce a new sysreset driver for the QEMU Virtual System Controller.
    This device is found on QEMU "virt" machines (such as the m68k virt
    target) and provides a mechanism to trigger system reset and power-off
    events.
    
    The driver maps U-Boot sysreset types to the corresponding controller
    commands:
    - SYSRESET_WARM / SYSRESET_COLD -> VIRT_CTRL_CMD_RESET
    - SYSRESET_POWER_OFF -> VIRT_CTRL_CMD_HALT
    
    Signed-off-by: Kuan-Wei Chiu <visitorckw@gmail.com>
    Reviewed-by: Simon Glass <simon.glass@canonical.com>

---
+++ include/qemu_virt_ctrl.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2025, Kuan-Wei Chiu <visitorckw@gmail.com>
+ */
+
+#ifndef _QEMU_VIRT_CTRL_H_
+#define _QEMU_VIRT_CTRL_H_
+
+struct qemu_virt_ctrl_plat {
+	phys_addr_t reg;
+};
+
+#endif /* _QEMU_VIRT_CTRL_H_ */
