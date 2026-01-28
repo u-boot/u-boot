97b17ecae8161bbe259d22fb50b39e549971e22a
Brian Sune <briansune@gmail.com>
2026-01-28 22:18:15 +0800

    Add CoreCourse socfpga Board - AC501
    CoreCourse Altera GEN5 Cyclone V board
    do support different size and formfactor.
    Now introducing AC501 C5 to mainstream u-boot
    This is a UBGA-484 based board with basic
    feature. More info on [1]
    
    [1] https://corecourse.cn/forum.php?mod=viewthread&tid=27704&highlight=AC501
    
    Signed-off-by: Brian Sune <briansune@gmail.com>
    Reviewed-by: Tien Fong Chee <tien.fong.chee@altera.com>

---
+++ include/configs/socfpga_ac501soc.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2025 Brian Sune <briansune@gmail.com>
+ */
+#ifndef __CONFIG_CORESOURCE_AC501SOC_H__
+#define __CONFIG_CORESOURCE_AC501SOC_H__
+
+#include <asm/arch/base_addr_ac5.h>
+
+/* The rest of the configuration is shared */
+#include <configs/socfpga_common.h>
+
+#endif	/* __CONFIG_CORESOURCE_AC501SOC_H__ */
