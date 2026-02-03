177e2a3f1b8d89c48d51b398e39243f07414152d
Joseph Guo <qijian.guo@nxp.com>
2026-02-03 11:04:41 +0900

    imx: Add FRDM-IMX95 initial support
    Add boot support and peripherals like eMMC/SD, UART, I2C, GPIO, ENETC0/1
    and PCIE0/1 for iMX95 15x15 FRDM.
    Updated doc for build instructions.
    
    Signed-off-by: Joseph Guo <qijian.guo@nxp.com>

---
+++ include/configs/imx95_frdm.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2026 NXP
+ */
+
+#ifndef __IMX95_EVK_H
+#define __IMX95_EVK_H
+
+#include <linux/sizes.h>
+#include <linux/stringify.h>
+#include <asm/arch/imx-regs.h>
+
+#define CFG_SYS_INIT_RAM_ADDR	0x90000000
+#define CFG_SYS_INIT_RAM_SIZE	0x200000
+
+#define CFG_SYS_SDRAM_BASE		0x90000000
+#define PHYS_SDRAM				0x90000000
+
+#define PHYS_SDRAM_SIZE			0x70000000 /* 2GB - 256MB DDR */
+
+#define PHYS_SDRAM_2_SIZE		0x180000000 /* 6GB (Totally 8GB) */
+
+#define WDOG_BASE_ADDR			WDG3_BASE_ADDR
+
+#endif
