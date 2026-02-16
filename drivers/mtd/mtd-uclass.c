5d8c6c4c1ead1b5623abe186aede6e0054b9c7d4
Daniel Golle <daniel@makrotopia.org>
2026-02-16 17:02:13 +0000

    boot: bootdev: add MTD boot device
    Add a boot device driver for MTD (Memory Technology Devices) that
    enables bootstd to scan MTD partitions for FIT images.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Reviewed-by: Simon Glass <simon.glass@canonical.com>
    ---
    Changes since RFC:
      - Added blank line before return -ESHUTDOWN (Simon)
      - Added error check on bootdev_bind() (Simon)

--- drivers/mtd/mtd-uclass.c
+++ drivers/mtd/mtd-uclass.c
@@ -5,10 +5,24 @@
 
 #define LOG_CATEGORY UCLASS_MTD
 
+#include <bootdev.h>
 #include <dm.h>
 #include <dm/device-internal.h>
 #include <errno.h>
 #include <mtd.h>
+
+static int mtd_post_bind(struct udevice *dev)
+{
+	if (CONFIG_IS_ENABLED(BOOTDEV_MTD)) {
+		int ret;
+
+		ret = bootdev_setup_for_dev(dev, "mtd_bootdev");
+		if (ret)
+			return log_msg_ret("bd", ret);
+	}
+
+	return 0;
+}
 
 /*
  * Implement a MTD uclass which should include most flash drivers.
@@ -18,5 +32,6 @@
 UCLASS_DRIVER(mtd) = {
 	.id		= UCLASS_MTD,
 	.name		= "mtd",
+	.post_bind	= mtd_post_bind,
 	.per_device_auto	= sizeof(struct mtd_info),
 };
---
+++ include/bootdev-mtd.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * MTD and UBI boot device accessors
+ *
+ * Copyright (C) 2026 Daniel Golle <daniel@makrotopia.org>
+ */
+
+#ifndef __BOOTDEV_MTD_H
+#define __BOOTDEV_MTD_H
+
+#include <dm/device.h>
+
+struct mtd_info;
+
+/**
+ * mtd_bootdev_get_mtd() - get the whole-flash MTD device for an mtd_bootdev
+ *
+ * @dev: mtd_bootdev udevice
+ *
+ * Return: pointer to the top-level struct mtd_info, or NULL
+ */
+struct mtd_info *mtd_bootdev_get_mtd(struct udevice *dev);
+
+/**
+ * bootdev_get_mtd() - get the backing MTD device for any MTD-based bootdev
+ *
+ * Dispatches to the appropriate backend accessor depending on the bootdev
+ * driver type:
+ *   - mtd_bootdev  -> whole-flash MTD device
+ *   - ubi_bootdev  -> MTD partition hosting UBI
+ *
+ * @dev: bootdev udevice
+ *
+ * Return: pointer to the relevant struct mtd_info, or NULL if not MTD-based
+ */
+static inline struct mtd_info *bootdev_get_mtd(struct udevice *dev)
+{
+	const char *name = dev->driver->name;
+
+	if (IS_ENABLED(CONFIG_BOOTDEV_MTD) && !strcmp(name, "mtd_bootdev"))
+		return mtd_bootdev_get_mtd(dev);
+
+	return NULL;
+}
+
+#endif /* __BOOTDEV_MTD_H */
