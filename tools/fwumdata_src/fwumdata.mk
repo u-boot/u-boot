876fc8df120bfdc40648e904662a7c6fbf1e543e
Kory Maincent <kory.maincent@bootlin.com>
2026-02-23 14:18:36 +0100

    tools: Reorganize mkfwumdata tool into fwumdata_src directory
    Update FWU metadata-related tools by moving mkfwumdata.c into a new
    tools/fwumdata_src/ directory structure. This refactoring prepares for the
    addition of the fwumdata runtime tool, which will allow editing FWU
    metadata directly from the target.
    
    The Kconfig and Makefile entries are also moved into separate files within
    the new directory (Kconfig and fwumdata.mk respectively) to keep all FWU
    metadata tool configurations together and improve maintainability.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>
    Tested-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

---
+++ tools/fwumdata_src/fwumdata.mk
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0+
+#
+# (C) Copyright 2025, Kory Maincent <kory.maincent@bootlin.com>
+
+mkfwumdata-objs := fwumdata_src/mkfwumdata.o generated/lib/crc32.o
+HOSTLDLIBS_mkfwumdata += -luuid
+hostprogs-always-$(CONFIG_TOOLS_MKFWUMDATA) += mkfwumdata
