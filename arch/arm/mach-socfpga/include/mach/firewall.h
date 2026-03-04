e2b32e241648b95d8cc909fc3f3096ee1e08517c
Chen Huei Lok <chen.huei.lok@altera.com>
2026-03-04 12:27:20 +0800

    arm: socfpga: n5x: switch firewall setup to driver model
    Move the system manager and L3 interconnect firewall configuration from
    a dedicated firewall_setup() function to the driver model, using
    uclass_get_device_by_name(). This removes hardcoded firewall setup in
    SPL and relies on the driver model, making the code more maintainable
    and aligned with upstream practices.
    
    With spl_n5x.c migrated, no in-tree SoC64 SPL file calls
    firewall_setup() directly any longer. Remove the function and its
    private helpers from firewall.c, drop firewall.c from the Makefile,
    remove the now-unused declaration from firewall.h, and drop the stale
    firewall.h include from spl_n5x.c.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@altera.com>
    Signed-off-by: Chen Huei Lok <chen.huei.lok@altera.com>

--- arch/arm/mach-socfpga/include/mach/firewall.h
+++ arch/arm/mach-socfpga/include/mach/firewall.h
@@ -10,73 +10,6 @@
 
 #include <linux/bitops.h>
 
-struct socfpga_firwall_l4_per {
-	u32	nand;		/* 0x00 */
-	u32	nand_data;
-	u32	_pad_0x8;
-	u32	usb0;
-	u32	usb1;		/* 0x10 */
-	u32	_pad_0x14;
-	u32	_pad_0x18;
-	u32	spim0;
-	u32	spim1;		/* 0x20 */
-	u32	spis0;
-	u32	spis1;
-	u32	emac0;
-	u32	emac1;		/* 0x30 */
-	u32	emac2;
-	u32	_pad_0x38;
-	u32	_pad_0x3c;
-	u32	sdmmc;		/* 0x40 */
-	u32	gpio0;
-	u32	gpio1;
-	u32	_pad_0x4c;
-	u32	i2c0;		/* 0x50 */
-	u32	i2c1;
-	u32	i2c2;
-	u32	i2c3;
-	u32	i2c4;		/* 0x60 */
-	u32	timer0;
-	u32	timer1;
-	u32	uart0;
-	u32	uart1;		/* 0x70 */
-};
-
-struct socfpga_firwall_l4_sys {
-	u32	_pad_0x00;		/* 0x00 */
-	u32	_pad_0x04;
-	u32	dma_ecc;
-	u32	emac0rx_ecc;
-	u32	emac0tx_ecc;		/* 0x10 */
-	u32	emac1rx_ecc;
-	u32	emac1tx_ecc;
-	u32	emac2rx_ecc;
-	u32	emac2tx_ecc;		/* 0x20 */
-	u32	_pad_0x24;
-	u32	_pad_0x28;
-	u32	nand_ecc;
-	u32	nand_read_ecc;		/* 0x30 */
-	u32	nand_write_ecc;
-	u32	ocram_ecc;
-	u32	_pad_0x3c;
-	u32	sdmmc_ecc;		/* 0x40 */
-	u32	usb0_ecc;
-	u32	usb1_ecc;
-	u32	clock_manager;
-	u32	_pad_0x50;		/* 0x50 */
-	u32	io_manager;
-	u32	reset_manager;
-	u32	system_manager;
-	u32	osc0_timer;		/* 0x60 */
-	u32	osc1_timer;
-	u32	watchdog0;
-	u32	watchdog1;
-	u32	watchdog2;		/* 0x70 */
-	u32	watchdog3;
-};
-
-#define FIREWALL_L4_DISABLE_ALL		(BIT(0) | BIT(24) | BIT(16))
-#define FIREWALL_BRIDGE_DISABLE_ALL	(~0)
 
 /* Cache coherency unit (CCU) registers */
 #define CCU_CPU0_MPRT_ADBASE_DDRREG		0x4400
@@ -148,7 +81,5 @@ struct socfpga_firwall_l4_sys {
 #define FW_MPU_DDR_SCR_WRITEL(data, reg)		\
 	writel(data, SOCFPGA_FW_MPU_DDR_SCR_ADDRESS + (reg))
 #endif
-
-void firewall_setup(void);
 
 #endif /* _FIREWALL_H_ */
