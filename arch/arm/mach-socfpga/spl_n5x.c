b3a5d42efe92c0a3299c0e3888800cf9c227cadf
Chen Huei Lok <chen.huei.lok@altera.com>
2026-03-13 10:34:35 +0800

    arm: socfpga: n5x: Update SPL data save and restore implementation
    Enable backup for data section to support warm reset as no SPL image
    would be reloaded in warm reset.
    
    Signed-off-by: Chen Huei Lok <chen.huei.lok@altera.com>

--- arch/arm/mach-socfpga/spl_n5x.c
+++ arch/arm/mach-socfpga/spl_n5x.c
@@ -22,10 +22,28 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+u32 reset_flag(void)
+{
+	/* Check rstmgr.stat for warm reset status */
+	u32 status = readl(SOCFPGA_RSTMGR_ADDRESS);
+
+	/* Check whether any L4 watchdogs or SDM had triggered warm reset */
+	u32 warm_reset_mask = RSTMGR_L4WD_MPU_WARMRESET_MASK;
+
+	if (status & warm_reset_mask)
+		return 0;
+
+	return 1;
+}
+
 void board_init_f(ulong dummy)
 {
 	int ret;
 	struct udevice *dev;
+
+#if defined(CONFIG_XPL_BUILD) && defined(CONFIG_SPL_RECOVER_DATA_SECTION)
+	spl_save_restore_data();
+#endif
 
 	ret = spl_early_init();
 	if (ret)
