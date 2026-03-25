2c883c4483c4450a5360b21573b0b78418e2941f
Nora Schiffer <nora.schiffer@ew.tq-group.com>
2026-03-25 10:12:20 +0100

    board: tq: common: add sysinfo setup helper
    Add a setup helper based on the tq_eeprom sysinfo driver
    and set up the U-Boot environment.
    
    Signed-off-by: Nora Schiffer <nora.schiffer@ew.tq-group.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Alexander Feilke <alexander.feilke@ew.tq-group.com>

---
+++ board/tq/common/tq_sysinfo.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Common sysinfo helpers for TQ-Systems SOMs
+ *
+ * Copyright (c) 2025-2026 TQ-Systems GmbH <u-boot@ew.tq-group.com>
+ * D-82229 Seefeld, Germany.
+ * Author: Nora Schiffer
+ */
+
+#ifndef __TQ_SYSINFO_H
+#define __TQ_SYSINFO_H
+
+int tq_common_sysinfo_setup(void);
+
+#endif /* __TQ_SYSINFO_H */
