u-boot/u-boot

commit c15a791972548150d8c0616a53e70dada1d358ca

board/raspberrypi: add bcm2712d0-rpi-5-b for Raspberry Pi 5
This commit adds an FDT entry for the d0 stepping of the BCM2712 SoC.
This entry is used by the v1.1 revision of the board
(revision & 0x0f == 1).

Signed-off-by: Filip Kokosiński <filip.kokosinski@gmail.com>
Reviewed-by: Peter Robinson <pbrobinson@gmail.com>
Tested-by: Peter Robinson <pbrobinson@gmail.com>

board/raspberrypi/rpi/rpi.c
@@ -197,7 +197,10 @@ static const struct rpi_model rpi_models_new_scheme[] = {
 	},
 	[0x17] = {
 		"5 Model B",
-		FDTFILES(DTB_DIR "bcm2712-rpi-5-b.dtb"),
+		FDTFILES(
+			[0] = DTB_DIR "bcm2712-rpi-5-b.dtb",
+			[1] = DTB_DIR "bcm2712d0-rpi-5-b.dtb"
+		),
 		true,
 	},
 	[0x18] = {

commit 4e64ea88585d9650af867718e1c629a1e57c0157

rpi: Update the naming for bcm2712 SoC RevD naming
The downstream Raspberry Pi uses two namings for the revD
SoC device trees, both bcm2712d0-rpi-5-b and bcm2712-d-rpi-5-b
but it seems upstream has settled on just the later, so lets
use that as it's the name that maps both upstream and downstream.

Fixes: c15a7919725 ("board/raspberrypi: add bcm2712d0-rpi-5-b for Raspberry Pi 5")
Signed-off-by: Peter Robinson <pbrobinson@gmail.com>
Reviewed-by: Matthias Brugger <mbrugger@suse.com>

board/raspberrypi/rpi/rpi.c
@@ -199,7 +199,7 @@ static const struct rpi_model rpi_models_new_scheme[] = {
 		"5 Model B",
 		FDTFILES(
 			[0] = DTB_DIR "bcm2712-rpi-5-b.dtb",
-			[1] = DTB_DIR "bcm2712d0-rpi-5-b.dtb"
+			[1] = DTB_DIR "bcm2712-d-rpi-5-b.dtb"
 		),
 		true,
 	},