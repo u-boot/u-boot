ca8bb3f087b08ebc8d953311e4d5f6095514a7e7
Randolph Sapp <rs@ti.com>
2026-03-30 18:08:15 -0500

    k3-am62-pocketbeagle2: add support for efi capsules
    Add support for EFI capsules for this device. One large caveat is that
    the partition holding boot binaries cannot be the ESP partition due to
    the following ROM limitations.
    
    - The boot disk must be MBR
    - The boot partition must be labeled 0x0c with a boot flag
    
    A separate ESP partition should be provided for EFI capabilities.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- board/beagle/pocketbeagle2/pocketbeagle2.c
+++ board/beagle/pocketbeagle2/pocketbeagle2.c
@@ -14,11 +14,37 @@
 #include <fdt_support.h>
 #include <spl.h>
 #include <asm/arch/k3-ddr.h>
+#include <efi_loader.h>
 
 #include "../../ti/common/board_detect.h"
 #include "pocketbeagle2_ddr.h"
 
 DECLARE_GLOBAL_DATA_PTR;
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = POCKETBEAGLE2_TIBOOT3_IMAGE_GUID,
+		.fw_name = u"POCKETBEAGLE2_TIBOOT3",
+		.image_index = 1,
+	},
+	{
+		.image_type_id = POCKETBEAGLE2_SPL_IMAGE_GUID,
+		.fw_name = u"POCKETBEAGLE2_SPL",
+		.image_index = 2,
+	},
+	{
+		.image_type_id = POCKETBEAGLE2_UBOOT_IMAGE_GUID,
+		.fw_name = u"POCKETBEAGLE2_UBOOT",
+		.image_index = 3,
+	}
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "mmc 0=tiboot3.bin raw 0 2000 mmcpart 1;"
+		      "tispl.bin fat 0 1;u-boot.img fat 0 1",
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
 
 static int pocketbeagle2_get_ddr_size(void)
 {
