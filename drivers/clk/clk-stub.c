be1dc88b4a15b10df5a446f7219da58b3795d70d
Balaji Selvanathan <balaji.selvanathan@oss.qualcomm.com>
2026-04-27 14:56:06 +0530

    clk: qcom: clk-stub: Add compatibles for QCS615/SA8775P
    Add RPMH clock compatible strings for QCS615 and SA8775P
    SoCs to enable clock framework support on these platforms.
    
    Signed-off-by: Balaji Selvanathan <balaji.selvanathan@oss.qualcomm.com>
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Reviewed-by: Casey Connolly <casey.connolly@linaro.org>
    Link: https://patch.msgid.link/20260427-ufs_clk-v2-2-36e10a7c0ef6@oss.qualcomm.com
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>

--- drivers/clk/clk-stub.c
+++ drivers/clk/clk-stub.c
@@ -49,7 +49,9 @@ static struct clk_ops stub_clk_ops = {
 };
 
 static const struct udevice_id stub_clk_ids[] = {
+	{ .compatible = "qcom,qcs615-rpmh-clk" },
 	{ .compatible = "qcom,rpmcc" },
+	{ .compatible = "qcom,sa8775p-rpmh-clk" },
 	{ .compatible = "qcom,sc7180-rpmh-clk" },
 	{ .compatible = "qcom,sc7280-rpmh-clk" },
 	{ .compatible = "qcom,sdm670-rpmh-clk" },
