ddbae5e48cb385780b78d57ef617d904f07eaee3
Casey Connolly <casey.connolly@linaro.org>
2026-05-04 20:57:38 +0200

    soc: qcom: smem: stub functions
    Allow smem to be optional for Qualcomm platforms by providing stub
    functions.
    
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

--- include/soc/qcom/smem.h
+++ include/soc/qcom/smem.h
@@ -2,8 +2,12 @@
 #ifndef __QCOM_SMEM_H__
 #define __QCOM_SMEM_H__
 
+#include <linux/err.h>
+#include <stdbool.h>
+
 #define QCOM_SMEM_HOST_ANY -1
 
+#if defined(CONFIG_QCOM_SMEM)
 int qcom_smem_init(void);
 int qcom_socinfo_init(void);
 
@@ -12,5 +16,21 @@ int qcom_smem_alloc(unsigned host, unsigned item, size_t size);
 void *qcom_smem_get(unsigned host, unsigned item, size_t *size);
 
 int qcom_smem_get_free_space(unsigned host);
+#else
+static int qcom_smem_init(void) { return -ENOSYS; }
+
+static bool qcom_smem_is_available(void) { return false; }
+int qcom_smem_alloc(unsigned host, unsigned item, size_t size)
+{
+	return -ENOSYS;
+}
+
+void *qcom_smem_get(unsigned host, unsigned item, size_t *size)
+{
+	return ERR_PTR(-ENOSYS);
+}
+
+int qcom_smem_get_free_space(unsigned host);
+#endif
 
 #endif
