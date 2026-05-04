b429ef216098abfb1f8441c937e9b9f70fd7d016
Casey Connolly <casey.connolly@linaro.org>
2026-05-04 20:57:36 +0200

    soc: qcom: smem: adjust for U-Boot
    Port over the smem code to U-Boot and ensure the smem region gets mapped
    after dcache is enabled.
    
    Tested-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260504-b4-modernise-smem-v2-8-c01ec2ff3886@linaro.org
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>

--- include/soc/qcom/smem.h
+++ include/soc/qcom/smem.h
@@ -4,17 +4,12 @@
 
 #define QCOM_SMEM_HOST_ANY -1
 
+int qcom_smem_init(void);
+
 bool qcom_smem_is_available(void);
 int qcom_smem_alloc(unsigned host, unsigned item, size_t size);
 void *qcom_smem_get(unsigned host, unsigned item, size_t *size);
 
 int qcom_smem_get_free_space(unsigned host);
-
-phys_addr_t qcom_smem_virt_to_phys(void *p);
-
-int qcom_smem_get_soc_id(u32 *id);
-int qcom_smem_get_feature_code(u32 *code);
-
-int qcom_smem_bust_hwspin_lock_by_host(unsigned int host);
 
 #endif
