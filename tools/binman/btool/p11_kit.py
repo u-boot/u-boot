4dc8f10edf90cab4d7f6b7106f5e52f6c23099ed
Simon Glass <sjg@chromium.org>
2026-05-05 12:12:52 -0600

    binman: Flesh out the p11-kit bintool docstring
    The Sphinx-generated bintools.rst currently produces an empty section
    for this bintool, since its class docstring is only a single line and
    so the body under the heading is blank.
    
    Extend the docstring with a short description of what p11-kit does and
    how binman uses it, so the generated documentation has useful content.
    
    Suggested-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/btool/p11_kit.py
+++ tools/binman/btool/p11_kit.py
@@ -7,7 +7,16 @@ from binman import bintool
 
 
 class Bintoolp11_kit(bintool.Bintool):
-    """p11-kit -- support tool for pkcs#11 libraries"""
+    """p11-kit -- support tool for pkcs#11 libraries
+
+    This bintool wraps the `p11-kit` command, a support tool for PKCS#11
+    modules. Binman uses this wrapper only to check that p11-kit is installed
+    (and to fetch it if missing); any actual PKCS#11 module discovery for
+    signing FIT images or capsules is done outside binman, by mkimage and the
+    OpenSSL PKCS#11 engine.
+
+    See https://p11-glue.github.io/p11-glue/p11-kit.html for more details.
+    """
     def __init__(self, name):
         super().__init__('p11-kit',
                          'Pkcs11 library modules tool',
