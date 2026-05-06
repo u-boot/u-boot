6af1851cb6363f7f7d60f6d7b4742644f5983aad
Michael Walle <mwalle@kernel.org>
2026-05-06 14:34:14 +0200

    boards/nxp: remove board_eth_init()
    board_eth_init() is dead code since commit e524f3a449f5 ("net: Remove
    eth_legacy.c"). Remove it.
    
    I'm not sure, all the shenanigans are covered by the new DM-version. The
    MDIO mux and iomux controls probably are. The fman configuration
    probably isn't. OTOH, nobody cared for years and the called
    fm_info_set_phy_address() was also removed years ago.
    
    This also removes fdt_fixup_board_enet() for the ls1043a and ls1046a
    because it relies on the local variable "mdio_mux" being initialized by
    the board_eth_init().
    
    Signed-off-by: Michael Walle <mwalle@kernel.org>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/nxp/ls1012afrdm/eth.c
+++ board/nxp/ls1012afrdm/eth.c
@@ -7,16 +7,6 @@
 #include <dm.h>
 #include <net.h>
 #include <asm/io.h>
-#include <netdev.h>
-#include <fm_eth.h>
-#include <fsl_mdio.h>
-#include <malloc.h>
-#include <asm/types.h>
-#include <fsl_dtsec.h>
-#include <asm/arch/soc.h>
-#include <asm/arch-fsl-layerscape/config.h>
-#include <asm/arch-fsl-layerscape/immap_lsch2.h>
-#include <asm/arch/fsl_serdes.h>
 #include <linux/delay.h>
 #include <net/pfe_eth/pfe_eth.h>
 #include <dm/platform_data/pfe_dm_eth.h>
