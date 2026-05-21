24ad31eb029e046426aba9d0f95c0bdef8204554
Michael Srba <Michael.Srba@seznam.cz>
2026-05-21 22:37:20 +0200

    mach-snapdragon: boot0.h: add sdm845_spl_boot0.h
    On sdm845, running u-boot SPL in EL3 requires escalting by using
    an unintentional feature in old builds of xbl_sec.elf. We do this
    in boot0.h so the rest of U-Boot can stay blissfully unaware
    of XBL_SEC. If we are already in EL3 for whatever reason, the
    code is skipped.
    
    Signed-off-by: Michael Srba <Michael.Srba@seznam.cz>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Casey Connolly <casey.connolly@linaro.org>
    Link: https://patch.msgid.link/20260521-qcom_spl-v9-8-c108ebe8ff4e@seznam.cz
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>

--- arch/arm/mach-snapdragon/include/mach/boot0.h
+++ arch/arm/mach-snapdragon/include/mach/boot0.h
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 #if defined(CONFIG_SPL_BUILD)
+#if defined(CONFIG_BOOT0_SDM845_WORKAROUND)
+#include "sdm845_spl_boot0.h"
+#else
 	b	reset
+#endif
 #else
 #if defined(CONFIG_BOOT0_MSM8916_PSCI_WORKAROUND)
 #include "msm8916_boot0.h"
