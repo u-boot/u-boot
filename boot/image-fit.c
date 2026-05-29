59d4e204af14582dd7ebfa536b88f5ae819041b3
Rasmus Villemoes <rv@rasmusvillemoes.dk>
2026-05-29 21:41:37 +0200

    image-fit.c: introduce CONTROL_DTB_AS_FIT config knob
    Having scripts embedded one way or the other in the U-Boot binary
    means they are automatically verified/trusted by whatever mechanism
    verifies U-Boot.
    
    Writing those scripts in the built-in environment leads to
    backslatitis and missing or wrong quoting and is generally not very
    readable or maintainable.
    
    Maintaining scripts in external files allows one
    to have both syntax highlighting and to some extent apply shellcheck
    on it (though U-Boot's shell is of course not quite POSIX sh, so some
    '#shellcheck disable' directives are needed). Getting those into the
    U-Boot binary is then a matter of having a suitable .dtsi file such as
    
    / {
    	images {
    		default = "boot";
    		boot {
    			description = "Bootscript";
    			data = /incbin/("boot.sh");
    			type = "script";
    			compression = "none";
    		};
    		factory-reset {
    			description = "Script for performing factory reset";
    			data = /incbin/("factory-reset.sh");
    			type = "script";
    			compression = "none";
    		};
    	};
    };
    
    and making that part of CONFIG_DEVICE_TREE_INCLUDES, so that U-Boot's
    control DTB effectively doubles as a FIT image containing a few
    "script" entries. At run-time, one's default bootcommand can then
    simply be
    
      source ${fdtcontroladdr}:boot
    
    Except of course that the control DTB is in fact not quite a FIT
    image. The lack of timestamp and description properties could
    potentially be worked around (by just adding those via that same
    .dtsi), but the no-@ check is not possible to get around. But since
    the control dtb is by definition trusted, we can make an exception for
    that particular address, if the new CONTROL_DTB_AS_FIT config option
    is enabled.
    
    One can of course build an ordinary FIT image with those
    scripts. However, that requires extra steps in the boot command for
    loading that script from storage, requires one to use "configurations"
    for pointing at a single script to run, and signing the FIT image
    using the same key used for verifying the kernel. Moreover, in certain
    situations, such as bootstrapping/production, there is no place to
    load that FIT image from, and it is much simpler to just have the
    necessary scripts be part of the U-Boot image itself.
    
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- boot/image-fit.c
+++ boot/image-fit.c
@@ -1664,6 +1664,16 @@ static int fdt_check_no_at(const void *fit, int parent)
 	return 0;
 }
 
+static int fit_check_images_node(const void *fit)
+{
+	if (fdt_path_offset(fit, FIT_IMAGES_PATH) < 0) {
+		log_debug("Wrong FIT format: no images parent node\n");
+		return -ENOENT;
+	}
+
+	return 0;
+}
+
 int fit_check_format(const void *fit, ulong size)
 {
 	int ret;
@@ -1675,6 +1685,13 @@ int fit_check_format(const void *fit, ulong size)
 			  ret);
 		return -ENOEXEC;
 	}
+
+	/*
+	 * For the control DTB to act as a FIT image, we only require
+	 * an /images node.
+	 */
+	if (CONFIG_IS_ENABLED(CONTROL_DTB_AS_FIT) && fit == gd_fdt_blob())
+		return fit_check_images_node(fit);
 
 	if (CONFIG_IS_ENABLED(FIT_FULL_CHECK)) {
 		/*
@@ -1724,12 +1741,7 @@ int fit_check_format(const void *fit, ulong size)
 	}
 
 	/* mandatory subimages parent '/images' node */
-	if (fdt_path_offset(fit, FIT_IMAGES_PATH) < 0) {
-		log_debug("Wrong FIT format: no images parent node\n");
-		return -ENOENT;
-	}
-
-	return 0;
+	return fit_check_images_node(fit);
 }
 
 int fit_conf_find_compat(const void *fit, const void *fdt)
