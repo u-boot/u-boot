ec217322301b306012e01796090c98af9d1f3812
Ye Li <ye.li@nxp.com>
2026-06-03 13:51:59 +0800

    nxp: imx[95,94,952]_evk: Implement board_fix_fdt
    Select the OF_BOARD_FIXUP and implement board_fix_fdt in board codes of
    iMX95/952/94 EVK to handle fuse setting on various part numbers.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/nxp/imx94_evk/imx94_evk.c
+++ board/nxp/imx94_evk/imx94_evk.c
@@ -7,7 +7,7 @@
 #include <fdt_support.h>
 #include <asm/gpio.h>
 #include <asm/arch/clock.h>
-#include <asm/mach-imx/sys_proto.h>
+#include <asm/arch/sys_proto.h>
 
 int board_init(void)
 {
@@ -26,3 +26,11 @@ int board_late_init(void)
 
 	return 0;
 }
+
+#if IS_ENABLED(CONFIG_OF_BOARD_FIXUP)
+int board_fix_fdt(void *fdt)
+{
+	/* Remove nodes based on fuses. */
+	return imx9_uboot_fixup_by_fuse(fdt);
+}
+#endif
