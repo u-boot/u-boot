9be04779061e466089d00dc908278880cf1844d0
Peng Fan <peng.fan@nxp.com>
2026-06-04 20:20:31 +0800

    power: pmic: qcom: convert ofnode API to dev_read API
    Replace ofnode_read_u32_index(dev_ofnode(dev), ...) with
    dev_read_u32_index(dev, ...).
    
    No functional change.
    
    Reviewed-by: Casey Connolly <casey.connolly@linaro.org>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- drivers/power/pmic/pmic_qcom.c
+++ drivers/power/pmic/pmic_qcom.c
@@ -72,7 +72,7 @@ static int pmic_qcom_probe(struct udevice *dev)
 	 * contains two discrete values, not a single 64-bit address.
 	 * The address is the first value.
 	 */
-	ret = ofnode_read_u32_index(dev_ofnode(dev), "reg", 0, &priv->usid);
+	ret = dev_read_u32_index(dev, "reg", 0, &priv->usid);
 	if (ret < 0)
 		return -EINVAL;
 
