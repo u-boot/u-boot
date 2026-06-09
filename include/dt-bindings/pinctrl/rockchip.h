2f783e53754b383e037ffc32e074e9b121386c9e
Johan Jonker <jbx6244@gmail.com>
2026-06-09 22:23:14 +0200

    rockchip: pinctrl: replace RK_GPIOn defines
    The use of RK_GPIOn defines is deprecated in the upstream
    rockchip.h file. Replace all them by the gpio number.
    
    Signed-off-by: Johan Jonker <jbx6244@gmail.com>
    Reviewed-by: Quentin Schulz <quentin.schulz@cherry.de>
    Link: https://patch.msgid.link/6bcf3192-82dc-464e-92f6-b9758f6c8422@gmail.com
    Signed-off-by: Quentin Schulz <u-boot@0leil.net>

--- include/dt-bindings/pinctrl/rockchip.h
+++ include/dt-bindings/pinctrl/rockchip.h
@@ -9,13 +9,6 @@
 #ifndef __DT_BINDINGS_ROCKCHIP_PINCTRL_H__
 #define __DT_BINDINGS_ROCKCHIP_PINCTRL_H__
 
-#define RK_GPIO0	0
-#define RK_GPIO1	1
-#define RK_GPIO2	2
-#define RK_GPIO3	3
-#define RK_GPIO4	4
-#define RK_GPIO6	6
-
 #define RK_PA0		0
 #define RK_PA1		1
 #define RK_PA2		2
