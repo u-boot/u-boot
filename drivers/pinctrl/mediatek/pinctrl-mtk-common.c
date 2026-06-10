u-boot/u-boot

commit 76da7482cf39633a05a3ffc2b3c77e9c576cb0c3

pinctrl: mediatek: print bias info along with pinmux
Add functionality to be able to print pin bias settings along with the
pinmux setting.

This can be useful to debug why pins might not be working correctly.

Reviewed-by: Macpaul Lin <macpaul.lin@mediatek.com>
Link: https://patch.msgid.link/20260330-pinctrl-mtk-fix-mt8189-v2-3-05a737ec623d@baylibre.com
Signed-off-by: David Lechner <dlechner@baylibre.com>

drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -237,7 +237,37 @@ static int mtk_get_pin_io_type(struct udevice *dev, int pin,
 	io_type->bias_set = priv->soc->io_type[io_n].bias_set;
 	io_type->drive_set = priv->soc->io_type[io_n].drive_set;
 	io_type->input_enable = priv->soc->io_type[io_n].input_enable;
+	io_type->get_pinconf = priv->soc->io_type[io_n].get_pinconf;
 
+	return 0;
+}
+
+static int mtk_pinconf_get(struct udevice *dev, u32 pin, char *buf, size_t size)
+{
+	struct mtk_io_type_desc io_type;
+	int err, pos;
+
+	/* If we fail to get the type, then we just don't add any more info. */
+	if (mtk_get_pin_io_type(dev, pin, &io_type))
+		return 0;
+
+	pos = snprintf(buf, size, " (%s)", io_type.name);
+	if (pos >= size)
+		return pos;
+
+	if (io_type.get_pinconf) {
+		err = io_type.get_pinconf(dev, pin, buf + pos, size - pos);
+		if (err < 0)
+			return err;
+
+		pos += err;
+	}
+
+	return pos;
+}
+#else
+static int mtk_pinconf_get(struct udevice *dev, u32 pin, char *buf, size_t size)
+{
 	return 0;
 }
 #endif
@@ -270,12 +300,20 @@ static int mtk_get_pins_count(struct udevice *dev)
 static int mtk_get_pin_muxing(struct udevice *dev, unsigned int selector,
 			      char *buf, int size)
 {
-	int val, err;
+	int val, err, pos;
+
 	err = mtk_hw_get_value(dev, selector, PINCTRL_PIN_REG_MODE, &val);
 	if (err)
 		return err;
 
-	snprintf(buf, size, "Aux Func.%d", val);
+	pos = snprintf(buf, size, "Aux Func.%d", val);
+	if (pos >= size)
+		return 0;
+
+	err = mtk_pinconf_get(dev, selector, buf + pos, size - pos);
+	if (err < 0)
+		return err;
+
 	return 0;
 }
 
@@ -669,6 +707,55 @@ static int mtk_pinconf_group_set(struct udevice *dev,
 	}
 
 	return 0;
+}
+
+int mtk_pinconf_get_pu_pd(struct udevice *dev, u32 pin, char *buf, size_t size)
+{
+	int err, pu, pd;
+
+	err = mtk_hw_get_value(dev, pin, PINCTRL_PIN_REG_PU, &pu);
+	if (err)
+		return err;
+
+	err = mtk_hw_get_value(dev, pin, PINCTRL_PIN_REG_PD, &pd);
+	if (err)
+		return err;
+
+	return snprintf(buf, size, " PU:%d PD:%d", pu, pd);
+}
+
+int mtk_pinconf_get_pupd_r1_r0(struct udevice *dev, u32 pin, char *buf, size_t size)
+{
+	int err, r0, r1, pupd;
+
+	err = mtk_hw_get_value(dev, pin, PINCTRL_PIN_REG_PUPD, &pupd);
+	if (err)
+		return err;
+
+	err = mtk_hw_get_value(dev, pin, PINCTRL_PIN_REG_R1, &r1);
+	if (err)
+		return err;
+
+	err = mtk_hw_get_value(dev, pin, PINCTRL_PIN_REG_R0, &r0);
+	if (err)
+		return err;
+
+	return snprintf(buf, size, " PUPD:%d R1:%d R0:%d", pupd, r1, r0);
+}
+
+int mtk_pinconf_get_pu_pd_rsel(struct udevice *dev, u32 pin, char *buf, size_t size)
+{
+	int pos, err, rsel;
+
+	pos = mtk_pinconf_get_pu_pd(dev, pin, buf, size);
+	if (pos < 0 || pos >= size)
+		return pos;
+
+	err = mtk_hw_get_value(dev, pin, PINCTRL_PIN_REG_RSEL, &rsel);
+	if (err)
+		return err;
+
+	return pos + snprintf(buf + pos, size - pos, " RSEL:%d", rsel);
 }
 #endif
 

drivers/pinctrl/mediatek/pinctrl-mtk-common.h
@@ -204,6 +204,7 @@ struct mtk_io_type_desc {
 			bool pullup, u32 val);
 	int (*drive_set)(struct udevice *dev, u32 pin, u32 arg);
 	int (*input_enable)(struct udevice *dev, u32 pin, u32 arg);
+	int (*get_pinconf)(struct udevice *dev, u32 pin, char *buf, size_t size);
 #endif
 };
 
@@ -263,6 +264,9 @@ int mtk_pinconf_bias_set_v1(struct udevice *dev, u32 pin, bool disable,
 int mtk_pinconf_input_enable_v1(struct udevice *dev, u32 pin, u32 arg);
 int mtk_pinconf_drive_set_v0(struct udevice *dev, u32 pin, u32 arg);
 int mtk_pinconf_drive_set_v1(struct udevice *dev, u32 pin, u32 arg);
+int mtk_pinconf_get_pu_pd(struct udevice *dev, u32 pin, char *buf, size_t size);
+int mtk_pinconf_get_pupd_r1_r0(struct udevice *dev, u32 pin, char *buf, size_t size);
+int mtk_pinconf_get_pu_pd_rsel(struct udevice *dev, u32 pin, char *buf, size_t size);
 
 #endif
 

commit 2258b6419a9fb94a62c566d7804fe4ff551fae7a

pinctrl: mediatek: use scnprintf() instead of snprintf()
Replace snprintf() with scnprintf() in the MediaTek pinctrl driver.

snprintf() returns the number of characters that _would_ have been
written if the buffer were large enough, while scnprintf() returns the
number of characters actually written to the buffer. Since we use the
return value to advance the buffer pointer, we need to use scnprintf()
to have the correct pointer arithmetic.

Fixes: 76da7482cf39 ("pinctrl: mediatek: print bias info along with pinmux")
Reviewed-by: Julien Stephan <jstephan@baylibre.com>
Link: https://patch.msgid.link/20260506-mtk-pinctrl-fix-scnprintf-v1-1-56b99d5809db@baylibre.com
Signed-off-by: David Lechner <dlechner@baylibre.com>

drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -251,7 +251,7 @@ static int mtk_pinconf_get(struct udevice *dev, u32 pin, char *buf, size_t size)
 	if (mtk_get_pin_io_type(dev, pin, &io_type))
 		return 0;
 
-	pos = snprintf(buf, size, " (%s)", io_type.name);
+	pos = scnprintf(buf, size, " (%s)", io_type.name);
 	if (pos >= size)
 		return pos;
 
@@ -306,7 +306,7 @@ static int mtk_get_pin_muxing(struct udevice *dev, unsigned int selector,
 	if (err)
 		return err;
 
-	pos = snprintf(buf, size, "Aux Func.%d", val);
+	pos = scnprintf(buf, size, "Aux Func.%d", val);
 	if (pos >= size)
 		return 0;
 
@@ -721,7 +721,7 @@ int mtk_pinconf_get_pu_pd(struct udevice *dev, u32 pin, char *buf, size_t size)
 	if (err)
 		return err;
 
-	return snprintf(buf, size, " PU:%d PD:%d", pu, pd);
+	return scnprintf(buf, size, " PU:%d PD:%d", pu, pd);
 }
 
 int mtk_pinconf_get_pupd_r1_r0(struct udevice *dev, u32 pin, char *buf, size_t size)
@@ -740,7 +740,7 @@ int mtk_pinconf_get_pupd_r1_r0(struct udevice *dev, u32 pin, char *buf, size_t s
 	if (err)
 		return err;
 
-	return snprintf(buf, size, " PUPD:%d R1:%d R0:%d", pupd, r1, r0);
+	return scnprintf(buf, size, " PUPD:%d R1:%d R0:%d", pupd, r1, r0);
 }
 
 int mtk_pinconf_get_pu_pd_rsel(struct udevice *dev, u32 pin, char *buf, size_t size)
@@ -755,7 +755,7 @@ int mtk_pinconf_get_pu_pd_rsel(struct udevice *dev, u32 pin, char *buf, size_t s
 	if (err)
 		return err;
 
-	return pos + snprintf(buf + pos, size - pos, " RSEL:%d", rsel);
+	return pos + scnprintf(buf + pos, size - pos, " RSEL:%d", rsel);
 }
 #endif
 