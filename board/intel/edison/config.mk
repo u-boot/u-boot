u-boot/u-boot

commit bd3f9ee679b4d1456d0d3c261ab76788950e6096

kbuild: Bump the build system to 6.1
Our last sync with the kernel was 5.1.

We are so out of sync now, that tracking the patches and backporting
them one by one makes little sense and it's going to take ages.

This is an attempt to sync up Makefiles to 6.1.
Unfortunately due to sheer amount of patches this is not easy to review,
but that's what we decided during a community call for the bump to 5.1,
so we are following the same guidelines here.

Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>a #rebased on -next

board/intel/edison/config.mk
@@ -11,7 +11,7 @@ cmd_mkalign_eds =							\
 	mv $@ $^
 
 INPUTS-y += u-boot-align.bin
-u-boot-align.bin: u-boot.bin
+u-boot-align.bin: u-boot.bin FORCE
 	$(call if_changed,mkalign_eds)
 
-HOSTCFLAGS_autoconf.mk.dep = -Wno-variadic-macros
+HOSTCFLAGS_include/autoconf.mk.dep = -Wno-variadic-macros

board/BuR/brppt1/config.mk
@@ -29,8 +29,8 @@ INPUTS-y += $(hw-platform-y)_prog.bin
 INPUTS-y += $(hw-platform-y)_prod.zip
 endif
 
-$(hw-platform-y)_prog.bin: u-boot-dtb.img spl/u-boot-spl.bin
+$(hw-platform-y)_prog.bin: u-boot-dtb.img spl/u-boot-spl.bin FORCE
 	$(call if_changed,prodbin)
 
-$(hw-platform-y)_prod.zip: $(hw-platform-y)_prog.bin
+$(hw-platform-y)_prod.zip: $(hw-platform-y)_prog.bin FORCE
 	$(call if_changed,prodzip)

board/BuR/brppt2/config.mk
@@ -29,8 +29,8 @@ INPUTS-y += $(hw-platform-y)_prod.zip
 endif
 endif
 
-$(hw-platform-y)_prog.bin: u-boot-dtb.img spl SPL
+$(hw-platform-y)_prog.bin: u-boot-dtb.img spl SPL FORCE
 	$(call if_changed,prodbin)
 
-$(hw-platform-y)_prod.zip: $(hw-platform-y)_prog.bin
+$(hw-platform-y)_prod.zip: $(hw-platform-y)_prog.bin FORCE
 	$(call if_changed,prodzip)

board/BuR/brsmarc1/config.mk
@@ -26,8 +26,8 @@ cmd_prodzip =					\
 INPUTS-y += $(hw-platform-y)_prog.bin
 INPUTS-y += $(hw-platform-y)_prod.zip
 
-$(hw-platform-y)_prog.bin: u-boot-dtb.img spl/u-boot-spl.bin
+$(hw-platform-y)_prog.bin: u-boot-dtb.img spl/u-boot-spl.bin FORCE
 	$(call if_changed,prodbin)
 
-$(hw-platform-y)_prod.zip: $(hw-platform-y)_prog.bin
+$(hw-platform-y)_prod.zip: $(hw-platform-y)_prog.bin FORCE
 	$(call if_changed,prodzip)

Kbuild
@@ -7,10 +7,11 @@
 
 generic-offsets-file := include/generated/generic-asm-offsets.h
 
-always  := $(generic-offsets-file)
+always-y  := $(generic-offsets-file)
 targets := lib/asm-offsets.s
 
-CFLAGS_REMOVE_asm-offsets.o := $(LTO_CFLAGS)
+CFLAGS_REMOVE_lib/asm-offsets.o := $(LTO_CFLAGS)
+CFLAGS_REMOVE_arch/$(ARCH)/lib/asm-offsets.o := $(LTO_CFLAGS)
 
 $(obj)/$(generic-offsets-file): $(obj)/lib/asm-offsets.s FORCE
 	$(call filechk,offsets,__GENERIC_ASM_OFFSETS_H__)
@@ -22,10 +23,11 @@ ifneq ($(wildcard $(srctree)/arch/$(ARCH)/lib/asm-offsets.c),)
 offsets-file := include/generated/asm-offsets.h
 endif
 
-always  += $(offsets-file)
+always-y  += $(offsets-file)
 targets += arch/$(ARCH)/lib/asm-offsets.s
 
-CFLAGS_asm-offsets.o := -DDO_DEPS_ONLY
+CFLAGS_lib/asm-offsets.o := -DDO_DEPS_ONLY
+CFLAGS_arch/$(ARCH)/lib/asm-offsets.o := -DDO_DEPS_ONLY
 
 $(obj)/$(offsets-file): $(obj)/arch/$(ARCH)/lib/asm-offsets.s FORCE
 	$(call filechk,offsets,__ASM_OFFSETS_H__)

arch/arm/cpu/arm926ejs/mxs/Makefile
@@ -53,23 +53,23 @@ quiet_cmd_mkcst_mxs = MXSCST  $@
 cmd_mkcst_mxs = cst -o $@ < $^						\
 	$(if $(KBUILD_VERBOSE:1=), >/dev/null)
 
-spl/u-boot-spl.ivt: spl/u-boot-spl.bin
+spl/u-boot-spl.ivt: spl/u-boot-spl.bin FORCE
 	$(call if_changed,mkalign_mxs)
 	$(call if_changed,mkivt_mxs,$(CONFIG_SPL_TEXT_BASE),\
 		0x00008000,0x00008040)
 
-u-boot.ivt: u-boot.bin
+u-boot.ivt: u-boot.bin FORCE
 	$(call if_changed,mkalign_mxs)
 	$(call if_changed,mkivt_mxs,$(CONFIG_TEXT_BASE),\
 		0x40001000,0x40001040)
 
-spl/u-boot-spl.csf: spl/u-boot-spl.ivt spl/u-boot-spl.bin board/$(VENDOR)/$(BOARD)/sign/u-boot-spl.csf
+spl/u-boot-spl.csf: spl/u-boot-spl.ivt spl/u-boot-spl.bin board/$(VENDOR)/$(BOARD)/sign/u-boot-spl.csf FORCE
 	$(call if_changed,mkcsfreq_mxs,$(CONFIG_SPL_TEXT_BASE),0x8000)
 
-u-boot.csf: u-boot.ivt u-boot.bin board/$(VENDOR)/$(BOARD)/sign/u-boot.csf
+u-boot.csf: u-boot.ivt u-boot.bin board/$(VENDOR)/$(BOARD)/sign/u-boot.csf FORCE
 	$(call if_changed,mkcsfreq_mxs,$(CONFIG_TEXT_BASE),0x40001000)
 
-%.sig: %.csf
+%.sig: %.csf FORCE
 	$(call if_changed,mkcst_mxs)
 
 MKIMAGEFLAGS_u-boot.sb = -n $< -T mxsimage

arch/arm/cpu/armv7/vf610/Makefile
@@ -7,5 +7,5 @@ obj-y	+= timer.o
 
 MKIMAGEFLAGS_u-boot.vyb = -T vybridimage
 
-u-boot.vyb: u-boot.imx
+u-boot.vyb: u-boot.imx FORCE
 	$(call if_changed,mkimage)

commit 895d3c67cd58fee6815d0e2799ea7d22474accb9

x86: edison: Avoid passing FORCE to the align command
Commit bd3f9ee679b4 ("kbuild: Bump the build system to 6.1") adds FORCE
as a prerequisite to the u-boot-align.bin rule, but cmd_mkalign_eds
expands $^, so FORCE leaks in as a stray operand. The dd then fails
(silently, since its stderr is discarded) and the following mv never
runs, so u-boot.bin never gains its 4096-byte zero prefix. The mask ROM
enters U-Boot 0x1000 (4KB) into the image and the board never starts,
sitting in BootROM download mode (DnX).

Use $< instead of $^, which excludes FORCE and restores the prefix.

Fixes: bd3f9ee679b4 ("kbuild: Bump the build system to 6.1")
Signed-off-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

board/intel/edison/config.mk
@@ -7,8 +7,8 @@
 # Add 4096 bytes of zeroes to u-boot.bin
 quiet_cmd_mkalign_eds = EDSALGN $@
 cmd_mkalign_eds =							\
-	dd if=$^ of=$@ bs=4k seek=1 2>/dev/null &&			\
-	mv $@ $^
+	dd if=$< of=$@ bs=4k seek=1 2>/dev/null &&			\
+	mv $@ $<
 
 INPUTS-y += u-boot-align.bin
 u-boot-align.bin: u-boot.bin FORCE