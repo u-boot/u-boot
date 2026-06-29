7156e868b45151bb4a2d322137cbe300df7a384a
James Hilliard <james.hilliard1@gmail.com>
2026-06-28 20:41:28 -0600

    clk: sunxi: add H6/H616 CE gates and reset
    The H6 and H616 devicetrees describe the Crypto Engine with module, bus
    and MBUS clocks plus a bus reset. Add the corresponding gate and reset
    entries so drivers can enable the CE through the normal clock and reset
    uclasses in U-Boot proper.
    
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    ---
    Changes v1 -> v2:
      - Add Simon's Reviewed-by tag

--- drivers/clk/sunxi/clk_h616.c
+++ drivers/clk/sunxi/clk_h616.c
@@ -19,6 +19,10 @@ static struct ccu_clk_gate h616_gates[] = {
 	[CLK_DE]		= GATE(0x600, BIT(31)),
 	[CLK_BUS_DE]		= GATE(0x60c, BIT(0)),
 
+	[CLK_CE]		= GATE(0x680, BIT(31)),
+	[CLK_BUS_CE]		= GATE(0x68c, BIT(0)),
+
+	[CLK_MBUS_CE]		= GATE(0x804, BIT(2)),
 	[CLK_MBUS_NAND]		= GATE(0x804, BIT(5)),
 
 	[CLK_NAND0]		= GATE(0x810, BIT(31)),
@@ -86,6 +90,7 @@ static struct ccu_clk_gate h616_gates[] = {
 
 static struct ccu_reset h616_resets[] = {
 	[RST_BUS_DE]		= RESET(0x60c, BIT(16)),
+	[RST_BUS_CE]		= RESET(0x68c, BIT(16)),
 	[RST_BUS_NAND]		= RESET(0x82c, BIT(16)),
 
 	[RST_BUS_MMC0]		= RESET(0x84c, BIT(16)),
