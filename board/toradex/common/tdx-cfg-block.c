2e49ea72ff5f77bfe3c4a6c0752b99eb57db8801
Vitor Soares <vitor.soares@toradex.com>
2026-07-10 15:33:05 +0100

    toradex: tdx-cfg-block: Add verdin am62 0242 pid4
    Add the new 0242 PID4 to config block handling:
    
    - 0242 Verdin AM62 Dual 1GB WB ET
    
    Update the board-specific variant selection to use the Wi-Fi variant
    for the new WB SKU.
    
    Signed-off-by: Vitor Soares <vitor.soares@toradex.com>

--- board/toradex/common/tdx-cfg-block.c
+++ board/toradex/common/tdx-cfg-block.c
@@ -200,6 +200,7 @@ const struct toradex_som toradex_modules[] = {
 	{ VERDIN_IMX8MPQ_8GB_WIFI_BT_IT_64G,     "Verdin iMX8M Plus Quad 8GB WB IT",     TARGET_IS_ENABLED(VERDIN_IMX8MP)        },
 	{ VERDIN_IMX8MMQ_2G_WIFI_BT_IT_64G,      "Verdin iMX8M Mini Quad 2GB WB IT",     TARGET_IS_ENABLED(VERDIN_IMX8MM)        },
 	{ SMARC_IMX8MPQ_4GB_WB_IT_64G,           "SMARC iMX8M Plus Quad 4GB WB IT",      TARGET_IS_ENABLED(TORADEX_SMARC_IMX8MP) },
+	{ VERDIN_AM62D_1G_WIFI_BT_ET,            "Verdin AM62 Dual 1GB WB ET",           TARGET_IS_ENABLED(VERDIN_AM62_A53)      },
 };
 
 struct pid4list {
