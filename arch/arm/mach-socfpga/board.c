u-boot/u-boot

commit 62f7a94602094617ac384839ed695c2906893a88

Replace TARGET namespace and cleanup properly
TARGET namespace is for machines / boards / what-have-you that
building U-Boot for. Simply replace from TARGET to ARCH
make things more clear and proper for ALL SoCFPGA.

Signed-off-by: Brian Sune <briansune@gmail.com>
Reviewed-by: Tien Fong Chee <tien.fong.chee@altera.com>

# Conflicts:
#	drivers/ddr/altera/Makefile

arch/arm/mach-socfpga/board.c
@@ -61,7 +61,7 @@ int board_init(void)
 
 int dram_init_banksize(void)
 {
-#if CONFIG_IS_ENABLED(HANDOFF) && IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)
+#if CONFIG_IS_ENABLED(HANDOFF) && IS_ENABLED(CONFIG_ARCH_SOCFPGA_AGILEX5)
 #ifndef CONFIG_SPL_BUILD
 	struct spl_handoff *ho;
 
@@ -72,7 +72,7 @@ int dram_init_banksize(void)
 #endif
 #else
 	fdtdec_setup_memory_banksize();
-#endif /* HANDOFF && CONFIG_TARGET_SOCFPGA_AGILEX5 */
+#endif /* HANDOFF && CONFIG_ARCH_SOCFPGA_AGILEX5 */
 
 	return 0;
 }
@@ -145,7 +145,7 @@ u8 socfpga_get_board_id(void)
 	return board_id;
 }
 
-#if IS_ENABLED(CONFIG_XPL_BUILD) && IS_ENABLED(CONFIG_TARGET_SOCFPGA_SOC64)
+#if IS_ENABLED(CONFIG_XPL_BUILD) && IS_ENABLED(CONFIG_ARCH_SOCFPGA_SOC64)
 int board_fit_config_name_match(const char *name)
 {
 	char board_name[10];

Kconfig
@@ -524,8 +524,8 @@ config BUILD_TARGET
 	default "u-boot-with-spl.bin" if MPC85xx && !E500MC && !E5500 && !E6500 && SPL
 	default "u-boot-with-spl.imx" if ARCH_MX6 && SPL
 	default "u-boot-with-spl.kwb" if ARMADA_32BIT && SPL
-	default "u-boot-with-spl.sfp" if TARGET_SOCFPGA_ARRIA10
-	default "u-boot-with-spl.sfp" if TARGET_SOCFPGA_GEN5
+	default "u-boot-with-spl.sfp" if ARCH_SOCFPGA_ARRIA10
+	default "u-boot-with-spl.sfp" if ARCH_SOCFPGA_GEN5
 	default "u-boot.itb" if !BINMAN && SPL_LOAD_FIT && (ARCH_ROCKCHIP || \
 				RISCV || ARCH_ZYNQMP)
 	default "u-boot.kwb" if (ARCH_KIRKWOOD || ARMADA_32BIT) && !SPL

arch/arm/Kconfig
@@ -30,7 +30,7 @@ config COUNTER_FREQUENCY
 			ROCKCHIP_RK3288 || ROCKCHIP_RK322X || ROCKCHIP_RK3036
 	default 25000000 if ARCH_LX2160A || ARCH_LX2162A || ARCH_LS1088A
 	default 100000000 if ARCH_ZYNQMP
-	default 200000000 if TARGET_SOCFPGA_AGILEX5 || TARGET_SOCFPGA_AGILEX7M
+	default 200000000 if ARCH_SOCFPGA_AGILEX5 || ARCH_SOCFPGA_AGILEX7M
 	default 0
 	help
 	  For platforms with ARMv8-A and ARMv7-A which features a system
@@ -1145,35 +1145,35 @@ config ARCH_SNAPDRAGON
 config ARCH_SOCFPGA
 	bool "Altera SOCFPGA family"
 	select ARCH_EARLY_INIT_R
-	select ARCH_MISC_INIT if !TARGET_SOCFPGA_ARRIA10
-	select ARM64 if TARGET_SOCFPGA_SOC64
-	select CPU_V7A if TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_ARRIA10
+	select ARCH_MISC_INIT if !ARCH_SOCFPGA_ARRIA10
+	select ARM64 if ARCH_SOCFPGA_SOC64
+	select CPU_V7A if ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_ARRIA10
 	select DM
 	select DM_SERIAL
 	select GPIO_EXTRA_HEADER
-	select ENABLE_ARM_SOC_BOOT0_HOOK if TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_ARRIA10
-	select LMB_ARCH_MEM_MAP if TARGET_SOCFPGA_SOC64
+	select ENABLE_ARM_SOC_BOOT0_HOOK if ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_ARRIA10
+	select LMB_ARCH_MEM_MAP if ARCH_SOCFPGA_SOC64
 	select OF_CONTROL
 	select SPL_DM_RESET if DM_RESET
 	select SPL_DM_SERIAL
 	select SPL_LIBCOMMON_SUPPORT
 	select SPL_LIBGENERIC_SUPPORT
 	select SPL_OF_CONTROL
-	select SPL_SEPARATE_BSS if TARGET_SOCFPGA_SOC64
-	select SPL_DRIVERS_MISC if TARGET_SOCFPGA_SOC64
-	select SPL_SOCFPGA_DT_REG if TARGET_SOCFPGA_SOC64
+	select SPL_SEPARATE_BSS if ARCH_SOCFPGA_SOC64
+	select SPL_DRIVERS_MISC if ARCH_SOCFPGA_SOC64
+	select SPL_SOCFPGA_DT_REG if ARCH_SOCFPGA_SOC64
 	select SPL_SERIAL
 	select SPL_SYSRESET
 	select SPL_WATCHDOG
 	select SUPPORT_SPL
 	select SYS_NS16550
-	select SYS_THUMB_BUILD if TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_ARRIA10
+	select SYS_THUMB_BUILD if ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_ARRIA10
 	select SYSRESET
-	select SYSRESET_SOCFPGA if TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_ARRIA10
-	select SYSRESET_SOCFPGA_SOC64 if !TARGET_SOCFPGA_AGILEX5 && \
-           TARGET_SOCFPGA_SOC64
-	select SYSRESET_PSCI if TARGET_SOCFPGA_AGILEX5
-	select USE_BOOTFILE if SPL_ATF && TARGET_SOCFPGA_SOC64
+	select SYSRESET_SOCFPGA if ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_ARRIA10
+	select SYSRESET_SOCFPGA_SOC64 if !ARCH_SOCFPGA_AGILEX5 && \
+           ARCH_SOCFPGA_SOC64
+	select SYSRESET_PSCI if ARCH_SOCFPGA_AGILEX5
+	select USE_BOOTFILE if SPL_ATF && ARCH_SOCFPGA_SOC64
 	imply CMD_DM
 	imply CMD_MTDPARTS
 	imply CRC32_VERIFY

arch/arm/mach-socfpga/Kconfig
@@ -1,15 +1,15 @@
 if ARCH_SOCFPGA
 
 config ERR_PTR_OFFSET
-	default 0xfffec000 if TARGET_SOCFPGA_GEN5 # Boot ROM range
+	default 0xfffec000 if ARCH_SOCFPGA_GEN5 # Boot ROM range
 
 config NR_DRAM_BANKS
 	default 1
 
 config SOCFPGA_SECURE_VAB_AUTH
 	bool "Enable boot image authentication with Secure Device Manager"
-	depends on TARGET_SOCFPGA_AGILEX || TARGET_SOCFPGA_N5X || \
-	  TARGET_SOCFPGA_AGILEX5
+	depends on ARCH_SOCFPGA_AGILEX || ARCH_SOCFPGA_N5X || \
+	  ARCH_SOCFPGA_AGILEX5
 	select FIT_IMAGE_POST_PROCESS
 	select SHA384
 	select SHA512
@@ -23,32 +23,32 @@ config SOCFPGA_SECURE_VAB_AUTH_ALLOW_NON_FIT_IMAGE
 	depends on SOCFPGA_SECURE_VAB_AUTH
 
 config SPL_SIZE_LIMIT
-	default 0x10000 if TARGET_SOCFPGA_GEN5
+	default 0x10000 if ARCH_SOCFPGA_GEN5
 
 config SPL_SIZE_LIMIT_PROVIDE_STACK
-	default 0x200 if TARGET_SOCFPGA_GEN5
+	default 0x200 if ARCH_SOCFPGA_GEN5
 
 config SPL_STACK_R_ADDR
-	default 0x00800000 if TARGET_SOCFPGA_GEN5
+	default 0x00800000 if ARCH_SOCFPGA_GEN5
 
 config SPL_SYS_MALLOC_F
-	default y if TARGET_SOCFPGA_GEN5
+	default y if ARCH_SOCFPGA_GEN5
 
 config SPL_SYS_MALLOC_F_LEN
-	default 0x800 if TARGET_SOCFPGA_GEN5
+	default 0x800 if ARCH_SOCFPGA_GEN5
 
 config SYS_MMCSD_RAW_MODE_U_BOOT_PARTITION_TYPE
 	default 0xa2
 
 config SYS_MALLOC_F_LEN
-	default 0x2000 if TARGET_SOCFPGA_ARRIA10
-	default 0x2000 if TARGET_SOCFPGA_GEN5
+	default 0x2000 if ARCH_SOCFPGA_ARRIA10
+	default 0x2000 if ARCH_SOCFPGA_GEN5
 
 config TEXT_BASE
-	default 0x01000040 if TARGET_SOCFPGA_ARRIA10
-	default 0x01000040 if TARGET_SOCFPGA_GEN5
+	default 0x01000040 if ARCH_SOCFPGA_ARRIA10
+	default 0x01000040 if ARCH_SOCFPGA_GEN5
 
-config TARGET_SOCFPGA_AGILEX
+config ARCH_SOCFPGA_AGILEX
 	bool
 	select ARMV8_MULTIENTRY
 	select ARMV8_SET_SMPEN
@@ -58,9 +58,9 @@ config TARGET_SOCFPGA_AGILEX
 	select GICV2
 	select NCORE_CACHE
 	select SPL_CLK if SPL
-	select TARGET_SOCFPGA_SOC64
+	select ARCH_SOCFPGA_SOC64
 
-config TARGET_SOCFPGA_AGILEX7M
+config ARCH_SOCFPGA_AGILEX7M
 	bool
 	select ARMV8_MULTIENTRY
 	select ARMV8_SET_SMPEN
@@ -70,21 +70,21 @@ config TARGET_SOCFPGA_AGILEX7M
 	select GICV2
 	select NCORE_CACHE
 	select SPL_CLK if SPL
-	select TARGET_SOCFPGA_SOC64
+	select ARCH_SOCFPGA_SOC64
 
-config TARGET_SOCFPGA_AGILEX5
+config ARCH_SOCFPGA_AGILEX5
 	bool
 	select BINMAN if SPL_ATF
 	select CLK
 	select FPGA_INTEL_SDM_MAILBOX
 	select SPL_CLK if SPL
-	select TARGET_SOCFPGA_SOC64
+	select ARCH_SOCFPGA_SOC64
 
-config TARGET_SOCFPGA_ARRIA5
+config ARCH_SOCFPGA_ARRIA5
 	bool
-	select TARGET_SOCFPGA_GEN5
+	select ARCH_SOCFPGA_GEN5
 
-config TARGET_SOCFPGA_ARRIA10
+config ARCH_SOCFPGA_ARRIA10
 	bool
 	select GICV2
 	select SPL_ALTERA_SDRAM
@@ -105,17 +105,17 @@ config TARGET_SOCFPGA_ARRIA10
 
 config SOCFPGA_ARRIA10_ALWAYS_REPROGRAM
 	bool "Always reprogram Arria 10 FPGA"
-	depends on TARGET_SOCFPGA_ARRIA10
+	depends on ARCH_SOCFPGA_ARRIA10
 	help
 	  Arria 10 FPGA is only programmed during the cold boot.
 	  This option forces the FPGA to be reprogrammed every reboot,
 	  allowing to change the bitstream and apply it with warm reboot.
 
-config TARGET_SOCFPGA_CYCLONE5
+config ARCH_SOCFPGA_CYCLONE5
 	bool
-	select TARGET_SOCFPGA_GEN5
+	select ARCH_SOCFPGA_GEN5
 
-config TARGET_SOCFPGA_GEN5
+config ARCH_SOCFPGA_GEN5
 	bool
 	select SPL_ALTERA_SDRAM
 	imply FPGA_SOCFPGA
@@ -125,7 +125,7 @@ config TARGET_SOCFPGA_GEN5
 	imply SPL_SYS_MALLOC_SIMPLE
 	imply SPL_USE_TINY_PRINTF
 
-config TARGET_SOCFPGA_N5X
+config ARCH_SOCFPGA_N5X
 	bool
 	select ARMV8_MULTIENTRY
 	select ARMV8_SET_SMPEN
@@ -135,23 +135,23 @@ config TARGET_SOCFPGA_N5X
 	select NCORE_CACHE
 	select SPL_ALTERA_SDRAM
 	select SPL_CLK if SPL
-	select TARGET_SOCFPGA_SOC64
+	select ARCH_SOCFPGA_SOC64
 
 config TARGET_SOCFPGA_N5X_SOCDK
 	bool "Intel eASIC SoCDK (N5X)"
-	select TARGET_SOCFPGA_N5X
+	select ARCH_SOCFPGA_N5X
 
-config TARGET_SOCFPGA_SOC64
+config ARCH_SOCFPGA_SOC64
 	bool
 
-config TARGET_SOCFPGA_STRATIX10
+config ARCH_SOCFPGA_STRATIX10
 	bool
 	select ARMV8_MULTIENTRY
 	select ARMV8_SET_SMPEN
 	select BINMAN if SPL_ATF
 	select FPGA_INTEL_SDM_MAILBOX
 	select GICV2
-	select TARGET_SOCFPGA_SOC64
+	select ARCH_SOCFPGA_SOC64
 
 choice
 	prompt "Altera SOCFPGA board select"
@@ -159,85 +159,85 @@ choice
 
 config TARGET_SOCFPGA_AGILEX_SOCDK
 	bool "Intel SOCFPGA SoCDK (Agilex)"
-	select TARGET_SOCFPGA_AGILEX
+	select ARCH_SOCFPGA_AGILEX
 
 config TARGET_SOCFPGA_AGILEX7M_SOCDK
 	bool "Intel SOCFPGA SoCDK (Agilex7 M-series)"
-	select TARGET_SOCFPGA_AGILEX7M
+	select ARCH_SOCFPGA_AGILEX7M
 
 config TARGET_SOCFPGA_AGILEX5_SOCDK
 	bool "Intel SOCFPGA SoCDK (Agilex5)"
-	select TARGET_SOCFPGA_AGILEX5
+	select ARCH_SOCFPGA_AGILEX5
 
 config TARGET_SOCFPGA_ARIES_MCVEVK
 	bool "Aries MCVEVK (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_ARRIA10_SOCDK
 	bool "Altera SOCFPGA SoCDK (Arria 10)"
-	select TARGET_SOCFPGA_ARRIA10
+	select ARCH_SOCFPGA_ARRIA10
 
 config TARGET_SOCFPGA_ARRIA5_SECU1
 	bool "ABB SECU1 (Arria V)"
-	select TARGET_SOCFPGA_ARRIA5
+	select ARCH_SOCFPGA_ARRIA5
 	select VENDOR_KM
 
 config TARGET_SOCFPGA_ARRIA5_SOCDK
 	bool "Altera SOCFPGA SoCDK (Arria V)"
-	select TARGET_SOCFPGA_ARRIA5
+	select ARCH_SOCFPGA_ARRIA5
 
 config TARGET_SOCFPGA_CHAMELEONV3
 	bool "Google Chameleon v3 (Arria 10)"
-	select TARGET_SOCFPGA_ARRIA10
+	select ARCH_SOCFPGA_ARRIA10
 
 config TARGET_SOCFPGA_CYCLONE5_SOCDK
 	bool "Altera SOCFPGA SoCDK (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_DEVBOARDS_DBM_SOC1
 	bool "Devboards DBM-SoC1 (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_EBV_SOCRATES
 	bool "EBV SoCrates (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_IS1
 	bool "IS1 (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_SOFTING_VINING_FPGA
 	bool "Softing VIN|ING FPGA (Cyclone V)"
 	select BOARD_LATE_INIT
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_SR1500
 	bool "SR1500 (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_STRATIX10_SOCDK
 	bool "Intel SOCFPGA SoCDK (Stratix 10)"
-	select TARGET_SOCFPGA_STRATIX10
+	select ARCH_SOCFPGA_STRATIX10
 
 config TARGET_SOCFPGA_TERASIC_DE0_NANO
 	bool "Terasic DE0-Nano-Atlas (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_TERASIC_DE10_NANO
 	bool "Terasic DE10-Nano (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_TERASIC_DE10_STANDARD
 	bool "Terasic DE10-Standard (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_TERASIC_DE1_SOC
 	bool "Terasic DE1-SoC (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_TERASIC_SOCKIT
 	bool "Terasic SoCkit (Cyclone V)"
-	select TARGET_SOCFPGA_CYCLONE5
+	select ARCH_SOCFPGA_CYCLONE5
 
 config TARGET_SOCFPGA_CORECOURSE_AC501SOC
 	bool "CoreCourse AC501SoC (Cyclone V)"

common/Kconfig
@@ -55,7 +55,7 @@ config CONSOLE_RECORD_IN_SIZE
 config SYS_CBSIZE
 	int "Console input buffer size"
 	default 2048 if ARCH_TEGRA || ARCH_VERSAL || ARCH_ZYNQ || ARCH_ZYNQMP || \
-		RCAR_GEN3 || TARGET_SOCFPGA_SOC64
+		RCAR_GEN3 || ARCH_SOCFPGA_SOC64
 	default 512 if ARCH_MX5 || ARCH_MX6 || ARCH_MX7 || FSL_LSCH2 || \
 		FSL_LSCH3 || X86
 	default 256 if M68K || PPC

common/spl/Kconfig
@@ -548,7 +548,7 @@ config SPL_SYS_MMCSD_RAW_MODE
 	depends on SPL_DM_MMC || SPL_MMC
 	default y if ARCH_SUNXI || ARCH_DAVINCI || ARCH_UNIPHIER || \
 		     ARCH_MX6 || ARCH_MX7 || \
-		     ARCH_ROCKCHIP || ARCH_MVEBU || TARGET_SOCFPGA_GEN5 || \
+		     ARCH_ROCKCHIP || ARCH_MVEBU || ARCH_SOCFPGA_GEN5 || \
 		     ARCH_AT91 || ARCH_ZYNQ || ARCH_KEYSTONE || OMAP34XX || \
 		     OMAP54XX || AM33XX || AM43XX || \
 		     TARGET_SIFIVE_UNLEASHED || TARGET_SIFIVE_UNMATCHED
@@ -593,7 +593,7 @@ config SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR
 	default 0x8a if ARCH_MX6 || ARCH_MX7
 	default 0x100 if ARCH_UNIPHIER
 	default 0x0 if ARCH_MVEBU
-	default 0x200 if TARGET_SOCFPGA_GEN5 || ARCH_AT91
+	default 0x200 if ARCH_SOCFPGA_GEN5 || ARCH_AT91
 	default 0x300 if ARCH_ZYNQ || ARCH_KEYSTONE || OMAP34XX || \
 		         OMAP54XX || AM33XX || AM43XX || ARCH_K3
 	default 0x4000 if ARCH_ROCKCHIP

drivers/ddr/altera/Kconfig
@@ -1,8 +1,8 @@
 config SPL_ALTERA_SDRAM
 	bool "SoCFPGA DDR SDRAM driver in SPL"
 	depends on SPL
-	depends on TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_ARRIA10 || TARGET_SOCFPGA_SOC64
-	select RAM if TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_SOC64
-	select SPL_RAM if TARGET_SOCFPGA_GEN5 || TARGET_SOCFPGA_SOC64
+	depends on ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_ARRIA10 || ARCH_SOCFPGA_SOC64
+	select RAM if ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_SOC64
+	select SPL_RAM if ARCH_SOCFPGA_GEN5 || ARCH_SOCFPGA_SOC64
 	help
 	  Enable DDR SDRAM controller for the SoCFPGA devices.

drivers/fpga/Kconfig
@@ -46,7 +46,7 @@ config FPGA_CYCLON2
 
 config FPGA_INTEL_SDM_MAILBOX
 	bool "Enable Intel FPGA Full Reconfiguration SDM Mailbox driver"
-	depends on TARGET_SOCFPGA_SOC64
+	depends on ARCH_SOCFPGA_SOC64
 	select FPGA_ALTERA
 	help
 	  Say Y here to enable the Intel FPGA Full Reconfig SDM Mailbox driver

commit 00b0a61bc017a3042013ad6b0eacec0a856aed32

arm: socfpga: Rename TARGET to ARCH for SoCFPGA
Commit 62f7a9460209 ("Replace TARGET namespace and cleanup properly")
replaces the TARGET namespace to ARCH for all SoCFPGA devices.

Some of the TARGET namespaces are still left out, update leftover TARGET
to ARCH following current SoCFPGA naming.

Fixes: 62f7a9460209 ("Replace TARGET namespace and cleanup properly")

Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>

arch/arm/mach-socfpga/board.c
@@ -210,8 +210,8 @@ void lmb_arch_add_memory(void)
 }
 #endif
 
-#if (defined(CONFIG_TARGET_SOCFPGA_ARRIA10) || \
-     defined(CONFIG_TARGET_SOCFPGA_GEN5)) && defined(CONFIG_XPL_BUILD)
+#if (defined(CONFIG_ARCH_SOCFPGA_ARRIA10) || \
+     defined(CONFIG_ARCH_SOCFPGA_GEN5)) && defined(CONFIG_XPL_BUILD)
 unsigned long board_spl_mmc_get_uboot_raw_sector(struct mmc *mmc,
 						 unsigned long raw_sect)
 {

cmd/Kconfig
@@ -1911,7 +1911,7 @@ menu "Shell scripting commands"
 
 config CMD_C5_PL330_DMA
 	bool "Release Reset DMA Channels for PL330 Handshake"
-	depends on TARGET_SOCFPGA_CYCLONE5
+	depends on ARCH_SOCFPGA_CYCLONE5
 	help
 	  Provides access to Reset Manager Per2ModRst. Enables DMA
 	  channels for ARM PrimeCell PL330 via reset release.