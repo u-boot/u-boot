ce220f7f6407c3c6e3cae469cf7d80ad3cba20d9
Michal Simek <michal.simek@amd.com>
2026-07-29 14:02:47 +0200

    cmd: scmi: Show names for vendor protocols 0x80 and 0x82
    The generic SCMI vendor protocol IDs (0x80, 0x82) were listed by
    "scmi info" as <NULL>. Add human readable names so the
    vendor protocols are easier to identify.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- cmd/scmi.c
+++ cmd/scmi.c
@@ -30,6 +30,8 @@ struct {
 	{SCMI_PROTOCOL_ID_RESET_DOMAIN, "Reset domain management"},
 	{SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN, "Voltage domain management"},
 	{SCMI_PROTOCOL_ID_PINCTRL, "Pin control management"},
+	{SCMI_PROTOCOL_ID_VENDOR_80, "Vendor specific (0x80)"},
+	{SCMI_PROTOCOL_ID_VENDOR_82, "Vendor specific (0x82)"},
 };
 
 /**
