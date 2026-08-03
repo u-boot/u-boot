u-boot/u-boot

commit 9c1b13b3fd271500cd8a61f86816d822e4852a90

cyclic: reduce get_timer_us() calls inside hlist_for_each_entry_safe()
On STM32MP157C-DK2, when using the "ums" command, in sleep_thread(),
ctrlc() is called every ~640ms which doesn't allows high reactivity when
user press CTRL+C in U-Boot console.

In sleep_thread() loop, ctrlc() is called every 200000 iterations.
But schedule is called on each loop iteration.

Optimize cyclic_run() in order to not call get_timer_us() on each entry.

This allow to save computation time :
  _ before : ctrlc() is called every ~640ms
  _ after  : ctrlc() is called every ~230ms

Signed-off-by: Rasmus Villemoes <rv@rasmusvillemoes.dk>
Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
Cc: Marek Vasut <marek.vasut@mailbox.org>

common/cyclic.c
@@ -67,26 +67,28 @@ static void cyclic_run(void)
 {
 	struct cyclic_info *cyclic;
 	struct hlist_node *tmp;
-	u64 now, cpu_time;
+	u64 now, after, cpu_time;
 
 	/* Prevent recursion */
 	if (gd->flags & GD_FLG_CYCLIC_RUNNING)
 		return;
 
 	gd->flags |= GD_FLG_CYCLIC_RUNNING;
+	now = get_timer_us(0);
 	hlist_for_each_entry_safe(cyclic, tmp, cyclic_get_list(), list) {
 		/*
 		 * Check if this cyclic function needs to get called, e.g.
 		 * do not call the cyclic func too often
 		 */
-		now = get_timer_us(0);
 		if (time_after_eq64(now, cyclic->next_call)) {
 			/* Call cyclic function and account it's cpu-time */
 			cyclic->next_call = now + cyclic->delay_us;
 			cyclic->func(cyclic);
+			after = get_timer_us(0);
 			cyclic->run_cnt++;
-			cpu_time = get_timer_us(0) - now;
+			cpu_time = after - now;
 			cyclic->cpu_time_us += cpu_time;
+			now = after;
 
 			/* Check if cpu-time exceeds max allowed time */
 			if ((cpu_time > CONFIG_CYCLIC_MAX_CPU_TIME_US) &&

commit 2f44bb1417bfbcd2c8722dddec76425f27c8a8f5

cyclic: return early from cyclic_run() if the list is empty
It is possible that schedule(), and hence cyclic_run(), gets called
very early, perhaps even from assembly code. With
commit 9c1b13b3fd27 ("cyclic: reduce get_timer_us() calls inside
hlist_for_each_entry_safe()"), there is now an unconditional
get_timer_us(0) done outside the loop, and depending on the platform,
the timer infrastructure may not be set up yet. In at least one case,
that has caused a divide-by-0 and hence a failure to boot.

Platforms should really ensure their timers are ready ASAP, and in the
concrete case reported, that was indeed possible to fix that
way. However, it doesn't hurt to also insert an early return here, and
that could prevent other such hard-to-debug boot failures.

Reported-by: Emanuele Ghidoli <ghidoliemanuele@gmail.com>
Link: https://marc.info/?l=u-boot&m=178481834846283&w=2
Fixes: 9c1b13b3fd27 ("cyclic: reduce get_timer_us() calls inside hlist_for_each_entry_safe()")
Signed-off-by: Rasmus Villemoes <rv@rasmusvillemoes.dk>
Reviewed-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
Reviewed-by: Stefan Roese <stefan.roese@mailbox.org>
[sr: fix Fixes: tag SHA length and return statement indentation]

common/cyclic.c
@@ -63,6 +63,19 @@ static void cyclic_run(void)
 	struct hlist_node *tmp;
 	u64 now, after, cpu_time;
 
+	/*
+	 * Nothing to do if the list is empty. Also, schedule() can be
+	 * called before timer infrastructure is ready, in which case
+	 * calling get_timer_us() before the (empty) loop could cause
+	 * a divide-by-0 or otherwise crash the system. No clients
+	 * should be registered before the timer infrastructure is up,
+	 * so the check for the list being empty should be
+	 * ok. Otherwise, we would need a new GD_FLG_TIMERS_READY
+	 * flag.
+	 */
+	if (hlist_empty(&gd->cyclic_list))
+		return;
+
 	/* Prevent recursion */
 	if (gd->flags & GD_FLG_CYCLIC_RUNNING)
 		return;