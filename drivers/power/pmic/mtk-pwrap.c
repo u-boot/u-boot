b237f66ab0500bc82c41fd687c77b91dc1f76bfa
David Lechner <dlechner@baylibre.com>
2026-08-07 11:01:30 -0500

    power: pmic: mtk-pwrap: use mt8195 fallback for mt8188/9
    Remove the duplicated info for mt8188 and mt8189 platforms in the
    MediaTek PMIC wrapper driver. These both have a devicetree fallback
    to mt8195. There are no differences relevant to U-Boot, so we can
    avoid the duplication.
    
    Reviewed-by: Julien Stephan <jstephan@baylibre.com>
    Link: https://patch.msgid.link/20260807-mtk-pmic-mt6366-v2-3-d9fdef581fb1@baylibre.com
    Signed-off-by: David Lechner <dlechner@baylibre.com>

--- drivers/power/pmic/mtk-pwrap.c
+++ drivers/power/pmic/mtk-pwrap.c
@@ -250,34 +250,6 @@ enum pwrap_regs {
 	PWRAP_WDT_SRC_EN_1,
 };
 
-static int mt8188_regs[] = {
-	[PWRAP_INIT_DONE2] =		0x0,
-	[PWRAP_STAUPD_CTRL] =		0x4C,
-	[PWRAP_TIMER_EN] =		0x3E4,
-	[PWRAP_INT_EN] =		0x420,
-	[PWRAP_INT_FLG] =		0x428,
-	[PWRAP_INT_CLR] =		0x42C,
-	[PWRAP_INT1_EN] =		0x450,
-	[PWRAP_INT1_FLG] =		0x458,
-	[PWRAP_INT1_CLR] =		0x45C,
-	[PWRAP_WACS2_CMD] =		0x880,
-	[PWRAP_SWINF_2_WDATA_31_0] =	0x884,
-	[PWRAP_SWINF_2_RDATA_31_0] =	0x894,
-	[PWRAP_WACS2_VLDCLR] =		0x8A4,
-	[PWRAP_WACS2_RDATA] =		0x8A8,
-};
-
-static int mt8189_regs[] = {
-	[PWRAP_INIT_DONE2] =		0x0,
-	[PWRAP_TIMER_EN] =		0x3E4,
-	[PWRAP_INT_EN] =		0x450,
-	[PWRAP_WACS2_CMD] =		0x880,
-	[PWRAP_SWINF_2_WDATA_31_0] =	0x884,
-	[PWRAP_SWINF_2_RDATA_31_0] =	0x894,
-	[PWRAP_WACS2_VLDCLR] =		0x8A4,
-	[PWRAP_WACS2_RDATA] =		0x8A8,
-};
-
 static int mt8195_regs[] = {
 	[PWRAP_INIT_DONE2] =		0x0,
 	[PWRAP_STAUPD_CTRL] =		0x4C,
@@ -849,22 +821,6 @@ static struct dm_pmic_ops mtk_pwrap_ops = {
 	.write = mtk_pwrap_write,
 };
 
-static struct pmic_wrapper_type pwrap_mt8188 = {
-	.regs = mt8188_regs,
-	.arb_en_all = 0x777f,
-	.spi_w = PWRAP_MAN_CMD_SPI_WRITE,
-	.wdt_src = PWRAP_WDT_SRC_MASK_ALL,
-	.caps = PWRAP_CAP_ARB,
-};
-
-static struct pmic_wrapper_type pwrap_mt8189 = {
-	.regs = mt8189_regs,
-	.arb_en_all = 0x777f,
-	.spi_w = PWRAP_MAN_CMD_SPI_WRITE,
-	.wdt_src = PWRAP_WDT_SRC_MASK_ALL,
-	.caps = PWRAP_CAP_ARB,
-};
-
 static const struct pmic_wrapper_type pwrap_mt8195 = {
 	.regs = mt8195_regs,
 	.arb_en_all = 0x777f, /* NEED CONFIRM */
@@ -882,8 +838,6 @@ static const struct pmic_wrapper_type pwrap_mt8365 = {
 };
 
 static const struct udevice_id mtk_pwrap_ids[] = {
-	{ .compatible = "mediatek,mt8188-pwrap", .data = (ulong)&pwrap_mt8188 },
-	{ .compatible = "mediatek,mt8189-pwrap", .data = (ulong)&pwrap_mt8189 },
 	{ .compatible = "mediatek,mt8195-pwrap", .data = (ulong)&pwrap_mt8195 },
 	{ .compatible = "mediatek,mt8365-pwrap", .data = (ulong)&pwrap_mt8365 },
 	{ }
