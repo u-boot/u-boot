9e4253d2dc8eb6c66fe5a429f0f6b003a7fda803
Marek Vasut <marek.vasut+renesas@mailbox.org>
2026-08-09 03:41:40 +0200

    arm64: renesas: r8a779g3: Add PCIe firmware environment on Retronix R-Car V4H Sparrow Hawk
    Add PCIe controller firmware handling environment to Retronix R-Car
    V4H Sparrow Hawk. The "renesas_rcar_gen4_load_firmware" script is used
    to load PCIe controller firmware from SPI NOR offset 0x300000. The
    "flash_pcie_fw_to_qspi_from_mmc" script is used to write the firmware
    from SD card to SPI NOR offset 0x300000 .
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- include/configs/sparrowhawk.h
+++ include/configs/sparrowhawk.h
@@ -11,4 +11,26 @@
 
 #include "rcar-gen4-common.h"
 
+/* Environment setting */
+#undef CFG_EXTRA_ENV_SETTINGS
+#define CFG_EXTRA_ENV_SETTINGS						\
+	"bootm_size=0x10000000\0"					\
+	\
+	"renesas_rcar_gen4_pcie_firmware_sf_offset=0x300000\0"		\
+	"renesas_rcar_gen4_pcie_firmware_sd_path=lib/firmware/rcar_gen4_pcie.bin\0" \
+	"renesas_rcar_gen4_load_firmware="				\
+		"env set renesas_rcar_gen4_load_firmware_addr 0x54000000 && " \
+		"env set renesas_rcar_gen4_load_firmware_size 0x8000 && " \
+		"sf probe && "						\
+		"sf read ${renesas_rcar_gen4_load_firmware_addr} "	\
+			"${renesas_rcar_gen4_pcie_firmware_sf_offset} "	\
+			"${renesas_rcar_gen4_load_firmware_size}\0"	\
+	"flash_pcie_fw_to_qspi_from_mmc="				\
+		"load mmc 0:1 ${loadaddr} "				\
+			"${renesas_rcar_gen4_pcie_firmware_sd_path} && " \
+		"sf probe && "						\
+		"sf update ${loadaddr} "				\
+			"${renesas_rcar_gen4_pcie_firmware_sf_offset} "	\
+			"${filesize}\0"
+
 #endif /* __SPARROWHAWK_H */
