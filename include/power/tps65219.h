u-boot/u-boot

commit b5cfa0c7ca4f793df26525cac0ab8e03ed433218

power: add driver for the TPS65219 PMIC
The TPS65219 I2S PMIC features 3 Buck converters and 4 linear regulators,
2 GPOs, 1 GPIO, and 3 multi-function-pin.

This adds the PMIC driver, loading the regulator sub-nodes.

Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

include/power/tps65219.h
@@ -1,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2022 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#ifndef TPS65219_H
+#define TPS65219_H
+
+/* I2C device address for pmic tps65219 */
+#define TPS65219_I2C_ADDR		0x30
+#define TPS65219_LDO_NUM		4
+#define TPS65219_BUCK_NUM		3
+
+/* Drivers name */
+#define TPS65219_LDO_DRIVER		"tps65219_ldo"
+#define TPS65219_BUCK_DRIVER		"tps65219_buck"
+
+#define TPS65219_VOLT_MASK		0x3F
+#define TPS65219_BUCK_VOLT_MAX		3400000
+
+#define TPS65219_ENABLE_CTRL_REG	0x2
+
+#define TPS65219_BUCK1_VOUT_REG		0xa
+#define TPS65219_BUCK2_VOUT_REG		0x9
+#define TPS65219_BUCK3_VOUT_REG		0x8
+
+#define TPS65219_LDO1_VOUT_REG		0x7
+#define TPS65219_LDO2_VOUT_REG		0x6
+
+#define TPS65219_LDO12_BYP_CONFIG	6
+
+#define TPS65219_LDO3_VOUT_REG		0x5
+#define TPS65219_LDO4_VOUT_REG		0x4
+
+#define TPS65219_LDO12_VOLT_BYP_MIN	1500000
+#define TPS65219_LDO12_VOLT_MIN		 600000
+#define TPS65219_LDO12_VOLT_MAX		3400000
+#define TPS65219_LDO12_VOLT_REG_MIN	0
+#define TPS65219_LDO12_VOLT_REG_MAX	0x56
+#define TPS65219_LDO34_VOLT_MIN		1200000
+#define TPS65219_LDO34_VOLT_MAX		3300000
+#define TPS65219_LDO34_VOLT_REG_MIN	0x12
+#define TPS65219_LDO34_VOLT_REG_MAX	0x54
+
+#endif /* TPS65219_H */

drivers/power/pmic/Kconfig
@@ -367,6 +367,12 @@ config PMIC_TPS65941
 	The TPS65941 is a PMIC containing a bunch of SMPS & LDOs.
 	This driver binds the pmic children.
 
+config PMIC_TPS65219
+	bool "Enable driver for Texas Instruments TPS65219 PMIC"
+	depends on DM_PMIC
+	help
+	The TPS65219 is a PMIC containing a bunch of SMPS & LDOs.
+	This driver binds the pmic children.
 endif
 
 config PMIC_TPS65217

drivers/power/pmic/Makefile
@@ -43,3 +43,4 @@ obj-$(CONFIG_POWER_TPS65910) += pmic_tps65910.o
 obj-$(CONFIG_POWER_HI6553) += pmic_hi6553.o
 obj-$(CONFIG_POWER_MC34VR500) += pmic_mc34vr500.o
 obj-$(CONFIG_PMIC_TPS65941) += tps65941.o
+obj-$(CONFIG_PMIC_TPS65219) += tps65219.o

drivers/power/pmic/tps65219.c
@@ -1,0 +1,88 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * (C) Copyright 2022 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#include <common.h>
+#include <fdtdec.h>
+#include <errno.h>
+#include <dm.h>
+#include <i2c.h>
+#include <log.h>
+#include <power/pmic.h>
+#include <power/regulator.h>
+#include <power/tps65219.h>
+#include <dm/device.h>
+
+static const struct pmic_child_info pmic_children_info[] = {
+	{ .prefix = "ldo", .driver = TPS65219_LDO_DRIVER },
+	{ .prefix = "buck", .driver = TPS65219_BUCK_DRIVER },
+	{ },
+};
+
+static int tps65219_reg_count(struct udevice *dev)
+{
+	return 0x41;
+}
+
+static int tps65219_write(struct udevice *dev, uint reg, const uint8_t *buff,
+			  int len)
+{
+	if (dm_i2c_write(dev, reg, buff, len)) {
+		pr_err("write error to device: %p register: %#x!\n", dev, reg);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int tps65219_read(struct udevice *dev, uint reg, uint8_t *buff, int len)
+{
+	if (dm_i2c_read(dev, reg, buff, len)) {
+		pr_err("read error from device: %p register: %#x!\n", dev, reg);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int tps65219_bind(struct udevice *dev)
+{
+	ofnode regulators_node;
+	int children;
+
+	regulators_node = dev_read_subnode(dev, "regulators");
+	if (!ofnode_valid(regulators_node)) {
+		debug("%s: %s regulators subnode not found!\n", __func__,
+		      dev->name);
+	}
+
+	debug("%s: '%s' - found regulators subnode\n", __func__, dev->name);
+
+	children = pmic_bind_children(dev, regulators_node, pmic_children_info);
+	if (!children)
+		printf("%s: %s - no child found\n", __func__, dev->name);
+
+	/* Probe all the child devices */
+	return dm_scan_fdt_dev(dev);
+}
+
+static struct dm_pmic_ops tps65219_ops = {
+	.reg_count = tps65219_reg_count,
+	.read = tps65219_read,
+	.write = tps65219_write,
+};
+
+static const struct udevice_id tps65219_ids[] = {
+	{ .compatible = "ti,tps65219" },
+	{ }
+};
+
+U_BOOT_DRIVER(pmic_tps65219) = {
+	.name = "tps65219_pmic",
+	.id = UCLASS_PMIC,
+	.of_match = tps65219_ids,
+	.bind = tps65219_bind,
+	.ops = &tps65219_ops,
+};

commit d57e3e4c75951a7e4b98d31bcde4a95b058b66e5

power: regulator: tps65219: Fix LDO selector boundaries
According to the TPS65219 datasheet, selectors 0x38 through 0x3f
saturate at 3.4 V for LDO1 and LDO2.

For LDO3 and LDO4, selectors 0x00 through 0x0c saturate at
1.2 V, while selectors 0x36 through 0x3f saturate at 3.3 V.

The driver currently uses 0x56, 0x12, and 0x54 as selector boundary
values. These values do not match the selector boundaries defined by
the datasheet.

Use the correct selector boundary values of 0x38, 0x0c, and 0x36
for interpolation.

Fixes: b5cfa0c7ca4f ("power: add driver for the TPS65219 PMIC")
Signed-off-by: Kumara Bhimeswararao Matsa <bhimesh.matsa@gmail.com>
Acked-by: Maarten Brock <maarten.brock@sttls.nl>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

include/power/tps65219.h
@@ -47,10 +47,10 @@
 #define TPS65219_LDO12_VOLT_MIN		 600000
 #define TPS65219_LDO12_VOLT_MAX		3400000
 #define TPS65219_LDO12_VOLT_REG_MIN	0
-#define TPS65219_LDO12_VOLT_REG_MAX	0x56
+#define TPS65219_LDO12_VOLT_REG_MAX	0x38
 #define TPS65219_LDO34_VOLT_MIN		1200000
 #define TPS65219_LDO34_VOLT_MAX		3300000
-#define TPS65219_LDO34_VOLT_REG_MIN	0x12
-#define TPS65219_LDO34_VOLT_REG_MAX	0x54
+#define TPS65219_LDO34_VOLT_REG_MIN	0x0c
+#define TPS65219_LDO34_VOLT_REG_MAX	0x36
 
 #endif /* TPS65219_H */