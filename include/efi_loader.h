8a0ef2af0ee317c08a99d7091e0353b5cb9ede44
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2026-08-31 23:42:51 +0300

    efi_loader: Remove duplicate declarations
    commit f4bbd7b9faa4 ("efi_loader: Separate device path into its own header")
    split some function declarations to their own header file. Remove two
    remaining duplicates efi_dp_from_lo() and efi_dp_concat()
    
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Reviewed-by: Balaji Selvanathan <balaji.selvanathan@oss.qualcomm.com>

--- include/efi_loader.h
+++ include/efi_loader.h
@@ -1026,11 +1026,6 @@ struct efi_load_option {
 	const u8 *optional_data;
 };
 
-struct efi_device_path *efi_dp_from_lo(struct efi_load_option *lo,
-				       const efi_guid_t *guid);
-struct efi_device_path *efi_dp_concat(const struct efi_device_path *dp1,
-				      const struct efi_device_path *dp2,
-				      size_t split_end_node);
 struct efi_device_path *search_gpt_dp_node(struct efi_device_path *device_path);
 efi_status_t efi_deserialize_load_option(struct efi_load_option *lo, u8 *data,
 					 efi_uintn_t *size);
