3ed90bd8d136cb422015c19a97e4aca3cc8aba84
David Lechner <dlechner@baylibre.com>
2026-09-03 16:35:56 -0500

    reset: ast2500: check dev_read_addr_ptr() for NULL
    Check the return value of dev_read_addr_ptr() for NULL instead of
    IS_ERR_OR_NULL(). This function only ever returns NULL on failure.
    
    Replace the now-unused <linux/err.h> include with <errno.h>, which
    is what actually provides the error code this driver uses.
    
    Signed-off-by: David Lechner <dlechner@baylibre.com>

--- drivers/reset/reset-ast2500.c
+++ drivers/reset/reset-ast2500.c
@@ -5,11 +5,11 @@
  */
 
 #include <dm.h>
+#include <errno.h>
 #include <log.h>
 #include <misc.h>
 #include <reset.h>
 #include <reset-uclass.h>
-#include <linux/err.h>
 #include <asm/io.h>
 #include <asm/arch/scu_ast2500.h>
 
@@ -78,7 +78,7 @@ static int ast2500_reset_probe(struct udevice *dev)
 	}
 
 	priv->scu = dev_read_addr_ptr(scu_dev);
-	if (IS_ERR_OR_NULL(priv->scu)) {
+	if (!priv->scu) {
 		debug("%s: invalid SCU base pointer\n", __func__);
 		return -EINVAL;
 	}
