7ae612085af409ba06372547ab81f9f1dbb05a62
Tom Rini <trini@konsulko.com>
2026-09-04 15:12:51 -0600

    board: tqma6ul: Add missing include
    This file requires <asm/arch/imx-regs.h> to know what IOMUXC_BASE_ADDR
    is, and had been getting this via an indirect include path. Correct
    this, so we can fix other bugs which happen to expose this indirect
    inclusion.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- board/tq/tqma6ul/tqma6ul_mba6ul.c
+++ board/tq/tqma6ul/tqma6ul_mba6ul.c
@@ -8,6 +8,7 @@
 #include <env.h>
 #include <malloc.h>
 #include <asm/arch/clock.h>
+#include <asm/arch/imx-regs.h>
 #include <asm/arch/iomux.h>
 #include <asm/arch/mx6-pins.h>
 #include <asm/mach-imx/sys_proto.h>
